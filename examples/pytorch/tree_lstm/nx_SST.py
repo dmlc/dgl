95a7815426bc62e4e70568cb4177e105dc66d17d
QipengGuo <qpguo12@fudan.edu.cn>
2018-06-20 19:43:33 +0800

    add data preprocess of SST

---
+++ examples/pytorch/tree_lstm/nx_SST.py
@@ -0,0 +1,40 @@
+import numpy as NP
+from nltk.tree import Tree
+from nltk.corpus.reader import BracketParseCorpusReader as CorpusReader
+import networkx as nx
+
+class nx_BCT_Reader(object):
+    # Binary Constituency Tree constructor for networkx
+    def __init__(self, fnames=['trees/train.txt', 'trees/dev.txt', 'trees/test.txt']):
+        # fnames must be three items which means the file path of train, validation, test set, respectively.
+        self.corpus = CorpusReader('.', fnames)
+        self.train = self.corpus.parsed_sents(fnames[0])
+        self.dev = self.corpus.parsed_sents(fnames[1])
+        self.test = self.corpus.parsed_sents(fnames[2])
+
+    def create_BCT(self, root):
+        self.node_cnt = 0
+        self.G = nx.DiGraph()
+        def _rec(node, nx_node, depth=0):
+            for child in node:
+                node_id = str(self.node_cnt) + '_' + str(depth+1)
+                self.node_cnt += 1
+                if isinstance(child[0], str) or isinstance(child[0], unicode):
+                    self.G.add_node(node_id, word=child[0])
+                else:
+                    self.G.add_node(node_id, word=None)
+                    if isinstance(child, Tree): #check illegal trees
+                        _rec(child, node_id)
+                self.G.add_edge(node_id, nx_node)
+
+        self.G.add_node('0_0', word=None) # add root into nx Graph
+        _rec(root, '0_0')
+
+        return self.G
+
+    def generator(self, mode='train'):
+        assert mode in ['train', 'dev', 'test']
+        for s in self.__dict__[mode]:
+            yield self.create_BCT(s)
+
+
