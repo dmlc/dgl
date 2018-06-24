acedb4fb723658e5f7fc324b7d569de7e24fd18c
gaiyu <gaiyu@geeker-4.news.cs.nyu.edu>
2018-06-24 15:35:01 +0000

    TreeLSTM completed

--- examples/pytorch/tree_lstm/nx_SST.py
+++ examples/pytorch/tree_lstm/nx_SST.py
@@ -1,16 +1,31 @@
-import numpy as NP
 from nltk.tree import Tree
 from nltk.corpus.reader import BracketParseCorpusReader as CorpusReader
 import networkx as nx
+import torch as th
 
-class nx_BCT_Reader(object):
+class nx_BCT_Reader:
     # Binary Constituency Tree constructor for networkx
-    def __init__(self, fnames=['trees/train.txt', 'trees/dev.txt', 'trees/test.txt']):
+    def __init__(self, cuda=False,
+                 fnames=['trees/train.txt', 'trees/dev.txt', 'trees/test.txt']):
         # fnames must be three items which means the file path of train, validation, test set, respectively.
         self.corpus = CorpusReader('.', fnames)
         self.train = self.corpus.parsed_sents(fnames[0])
         self.dev = self.corpus.parsed_sents(fnames[1])
         self.test = self.corpus.parsed_sents(fnames[2])
+
+        self.vocab = {}
+        def _rec(node):
+            for child in node:
+                if isinstance(child[0], str) and child[0] not in self.vocab:
+                    self.vocab[child[0]] = len(self.vocab)
+                elif isinstance(child, Tree):
+                    _rec(child)
+        for sent in self.train:
+            _rec(sent)
+        self.default = len(self.vocab) + 1
+
+        self.LongTensor = th.cuda.LongTensor if cuda else th.LongTensor
+        self.FloatTensor = th.cuda.FloatTensor if cuda else th.FloatTensor
 
     def create_BCT(self, root):
         self.node_cnt = 0
@@ -19,15 +34,19 @@ class nx_BCT_Reader(object):
             for child in node:
                 node_id = str(self.node_cnt) + '_' + str(depth+1)
                 self.node_cnt += 1
-                if isinstance(child[0], str) or isinstance(child[0], unicode):
-                    self.G.add_node(node_id, word=child[0], label=None)
+#               if isinstance(child[0], str) or isinstance(child[0], unicode):
+                if isinstance(child[0], str):
+                    word = self.LongTensor([self.vocab.get(child[0], self.default)])
+                    self.G.add_node(node_id, x=word, y=None)
                 else:
-                    self.G.add_node(node_id, word=None, lable=child.label())
+                    label = self.FloatTensor([[0] * 5])
+                    label[0, int(child.label())] = 1
+                    self.G.add_node(node_id, x=None, y=label)
                     if isinstance(child, Tree): #check illegal trees
                         _rec(child, node_id)
                 self.G.add_edge(node_id, nx_node)
 
-        self.G.add_node('0_0', word=None) # add root into nx Graph
+        self.G.add_node('0_0', x=None, y=None) # add root into nx Graph
         _rec(root, '0_0')
 
         return self.G
@@ -36,4 +55,3 @@ class nx_BCT_Reader(object):
         assert mode in ['train', 'dev', 'test']
         for s in self.__dict__[mode]:
             yield self.create_BCT(s)
-
--- examples/pytorch/tree_lstm/tree_lstm.py
+++ examples/pytorch/tree_lstm/tree_lstm.py
@@ -4,113 +4,158 @@ https://arxiv.org/abs/1503.00075
 """
 
 
+import itertools
+import networkx as nx
 import dgl.graph as G
 import torch as th
 import torch.nn as nn
-
-
-class Preprocessor(nn.Module):
-    def __init__(self, n_embeddings, embedding_size):
-        super().__init__()
-        self.embedding = nn.Embedding(n_embeddings, embedding_size)
-
-    def forward(self, g):
-        """
-        Parameters
-        ----------
-        g : networkx.DiGraph
-        """
-        g = G.DGLGraph(g)
+import torch.nn.functional as F
 
 
 class TreeLSTM(nn.Module):
-    @staticmethod
-    def message_func(src, trg, _):
-        return {'h' : src['h'], 'c' : src['c']}
+    def __init__(self, n_embeddings, x_size, h_size, n_classes):
+        super().__init__()
+
+        self.embedding = nn.Embedding(n_embeddings, x_size)
+        self.linear = nn.Linear(h_size, n_classes)
 
     @staticmethod
-    def leaf_update_func(node_reprs, edge_reprs):
-        iou = th.mm(node_reprs['x'], self.iou_x) + self.iou_b
+    def message_func(src, trg, _):
+        return {'h' : src.get('h'), 'c' : src.get('c')}
+
+    def leaf_update_func(self, node_reprs, edge_reprs):
+        x = node_reprs['x']
+        iou = th.mm(x, self.iou_w) + self.iou_b
         i, o, u = th.chunk(iou, 3, 1)
         i, o, u = th.sigmoid(i), th.sigmoid(o), th.tanh(u)
         c = i * u
         h = o * th.tanh(c)
-        return h, c
+        return {'h' : h, 'c' : c}
 
-    def forward(self, g):
+    def internal_update_func(self, node_reprs, edge_reprs):
+        raise NotImplementedError()
+
+    def readout_func(self, g, train):
+        if train:
+            h = th.cat([d['h'] for d in g.nodes.values() if d['y'] is not None], 0)
+            y = th.cat([d['y'] for d in g.nodes.values() if d['y'] is not None], 0)
+            log_p = F.log_softmax(self.linear(h), 1)
+            return -th.sum(y * log_p) / len(y)
+        else:
+            h = th.cat([reprs['h'] for reprs in g.nodes.values()], 0)
+            y_bar = th.max(self.linear(h), 1)[1]
+            # TODO
+            for reprs, z in zip(labelled_reprs, y_bar):
+                reprs['y_bar'] = z.item()
+
+    def forward(self, g, train=False):
         """
         Parameters
         ----------
         g : networkx.DiGraph
         """
+        assert any(d['y'] is not None for d in g.nodes.values()) # TODO
 
-        order = nx.topological_sort(g)
-        frontier = itertools.takewhile(lambda x: not g.pred[x], order)
-        order = order[len(frontier):]
-        assert order
+        g = G.DGLGraph(g)
 
-        g = self.preprocessor(g)
+        def update_func(node_reprs, edge_reprs):
+            node_reprs = node_reprs.copy()
+            if node_reprs['x'] is not None:
+                node_reprs['x'] = self.embedding(node_reprs['x'])
+            return node_reprs
 
-        g.register_update_func(self.update_func(leaf=True), frontier)
+        g.register_message_func(self.message_func)
+        g.register_update_func(update_func, g.nodes)
         g.update_all()
 
-        while order:
-            frontier = itertools.takewhile(lambda x: x not in order, order)
-            edge_list = sum(([(y, x) for y in g.pred[x]] for x in frontier), [])
-            g.register_message_func(self.message_func, edge_list)
-            g.register_update_func(self.update_func(leaf=False, frontier)
-            for x in frontier:
-                g.update_to(x)
-            order = order[len(frontier):]
+        g.register_update_func(self.internal_update_func, g.nodes)
+        leaves = list(filter(lambda x: g.in_degree(x) == 0, g.nodes))
+        g.register_update_func(self.leaf_update_func, leaves)
+
+        iterator = []
+        frontier = [next(filter(lambda x: g.out_degree(x) == 0, g.nodes))]
+        while frontier:
+            src = sum([list(g.pred[x]) for x in frontier], [])
+            trg = sum([[x] * len(g.pred[x]) for x in frontier], [])
+            iterator.append((src, trg))
+            frontier = src
+
+        g.recvfrom(leaves)
+        g.propagate(reversed(iterator))
+
+        return self.readout_func(g, train)
 
 
 class ChildSumTreeLSTM(TreeLSTM):
-    def __init__(self, preprocessor, x_size, h_size):
-        super().__init__()
-        self.preprocessor = preprocessor
+    def __init__(self, n_embeddings, x_size, h_size, n_classes):
+        super().__init__(n_embeddings, x_size, h_size, n_classes)
 
-        self.iou_x = nn.Parameter(th.randn(x_size, 3 * h_size)) # TODO initializer
-        self.iou_h = nn.Parameter(th.randn(h_size, 3 * h_size)) # TODO initializer
+        self.iou_w = nn.Parameter(th.randn(x_size, 3 * h_size)) # TODO initializer
+        self.iou_u = nn.Parameter(th.randn(h_size, 3 * h_size)) # TODO initializer
         self.iou_b = nn.Parameter(th.zeros(1, 3 * h_size))
 
         self.f_x = nn.Parameter(th.randn(x_size, h_size)) # TODO initializer
         self.f_h = nn.Parameter(th.randn(h_size, h_size)) # TODO initializer
         self.f_b = nn.Parameter(th.zeros(1, h_size))
 
-    def update_func(self, leaf):
-        if leaf:
-            return self.leaf_update_func
+    def internal_update_func(self, node_reprs, edge_reprs):
+        x = node_reprs['x']
+        h_bar = sum(msg['h'] for msg in edge_reprs)
+        if x is None:
+            iou = th.mm(h_bar, self.iou_u) + self.iou_b
         else:
-            def u(node_reprs, edge_reprs):
-                x = node_reprs['x']
-                h_bar = sum(msg['h'] for msg in edge_reprs)
-                iou = th.mm(x, self.iou_x) + th.mm(h_bar, self.iou_h) + self.iou_b
-                i, o, u = th.chunk(iou, 3, 1)
-                i, o, u = th.sigmoid(i), th.sigmoid(o), th.tanh(u)
+            iou = th.mm(x, self.iou_w) + th.mm(h_bar, self.iou_u) + self.iou_b
+        i, o, u = th.chunk(iou, 3, 1)
+        i, o, u = th.sigmoid(i), th.sigmoid(o), th.tanh(u)
 
-                batch_x = x.repeat(len(edge_reprs), 1)
-                batch_h = 
+        wx = th.mm(x, self.f_x).repeat(len(edge_reprs), 1) if x is not None else 0
+        uh = th.mm(th.cat([msg['h'] for msg in edge_reprs], 0), self.f_h)
+        f = th.sigmoid(wx + uh + f_b)
 
-                wx = th.mm(x, self.f_x)
-                uh = th.mm(th.cat([msg['h'] for msg in edge_reprs], 0), self.f_h)
-                f = wx.repeat(len(edge_reprs), 1) + uh + f_b
+        c = th.cat([msg['c'] for msg in edge_reprs], 0)
+        c = i * u + th.sum(f * c, 0)
+        h = o * th.tanh(c)
 
-                c = th.cat([msg['c'] for msg in edge_reprs], 0)
-                c = i * u + th.sum(f * c, 0)
-                h = o * th.tanh(c)
-
-                return h, c
-            return u
+        return {'h' : h, 'c' : c}
 
 
 class NAryTreeLSTM(TreeLSTM):
-    def __init__(self, preprocessor, x_size, h_size):
-        super().__init__()
-        self.preprocessor = preprocessor
+    def __init__(self, n_embeddings, x_size, h_size, n_ary, n_classes):
+        super().__init__(n_embeddings, x_size, h_size, n_classes)
 
-    def update_func(self, leaf):
-        if leaf:
-            return self.leaf_update_func
-        else:
-            def u(node_reprs, edge_reprs):
-                x = node_reprs['x']
+        # TODO initializer
+        self.iou_w = nn.Parameter(th.randn(x_size, 3 * h_size))
+        self.iou_u = [nn.Parameter(th.randn(1, h_size, 3 * h_size)) for i in range(n_ary)]
+        self.iou_b = nn.Parameter(th.zeros(1, 3 * h_size))
+
+        # TODO initializer
+        self.f_x = nn.Parameter(th.randn(x_size, h_size))
+        self.f_h = [[nn.Parameter(th.randn(1, h_size, h_size))
+                     for i in range(n_ary)] for i in range(n_ary)]
+        self.f_b = nn.Parameter(th.zeros(1, h_size))
+
+    def internal_update_func(self, node_reprs, edge_reprs):
+        assert len(edge_reprs) > 0
+        assert all(msg['h'] is not None and msg['c'] is not None for msg in edge_reprs)
+
+        x = node_reprs['x']
+        n_children = len(edge_reprs)
+
+        iou_wx = th.mm(x, self.iou_w) if x is not None else 0
+        iou_u = th.cat(self.iou_u[:n_children], 0)
+        iou_h = th.cat([msg['h'] for msg in edge_reprs], 0).unsqueeze(1)
+        iou_uh = th.sum(th.bmm(iou_h, iou_u), 0)
+        i, o, u = th.chunk(iou_wx + iou_uh + self.iou_b, 3, 1)
+        i, o, u = th.sigmoid(i), th.sigmoid(o), th.tanh(u)
+
+        f_wx = th.mm(x, self.f_x).repeat(n_children, 1) if x is not None else 0
+        f_h = iou_h.repeat(n_children, 1, 1)
+        f_u = th.cat(sum([self.f_h[i][:n_children] for i in range(n_children)], []), 0)
+        f_uh = th.sum(th.bmm(f_h, f_u).view(n_children, n_children, -1), 0)
+        f = th.sigmoid(f_wx + f_uh + self.f_b)
+
+        c = th.cat([msg['c'] for msg in edge_reprs], 0)
+        c = i * u + th.sum(f * c, 0, keepdim=True)
+        h = o * th.tanh(c)
+
+        return {'h' : h, 'c' : c}
