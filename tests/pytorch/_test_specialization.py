789a76cc0a697efa51e5feafb828bb4d35bfe7a7
Minjie Wang <wmjlyjemaine@gmail.com>
2018-07-28 20:33:24 -0400

    specialize test

--- tests/pytorch/_test_specialization.py
+++ tests/pytorch/_test_specialization.py
@@ -3,6 +3,10 @@ from dgl.graph import DGLGraph
 
 D = 32
 
+def update_func(hv, accum):
+    assert hv.shape == accum.shape
+    return hv + accum
+
 def generate_graph():
     g = DGLGraph()
     for i in range(10):
@@ -18,3 +22,12 @@ def generate_graph():
     g._node_frame['h'] = col
     return g
 
+def test_spmv_specialize():
+    g = generate_graph()
+    g.register_message_func('from_src', batchable=True)
+    g.register_reduce_func('sum', batchable=True)
+    g.register_update_func(update_func, batchable=True)
+    g.update_all()
+
+if __name__ == '__main__':
+    test_spmv_specialize()
