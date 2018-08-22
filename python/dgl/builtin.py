546809e540bb9d2af46968f073141f9455f0c9fc
Minjie Wang <wmjlyjemaine@gmail.com>
2018-08-22 00:13:51 -0400

    add dict type reduced msg test

--- python/dgl/builtin.py
+++ python/dgl/builtin.py
@@ -8,7 +8,10 @@ def message_from_src(src, edge):
 
 def reduce_sum(node, msgs):
     if isinstance(msgs, list):
-        return sum(msgs)
+        if isinstance(msgs[0], dict):
+            return {k : sum(m[k] for m in msgs) for k in msgs[0].keys()}
+        else:
+            return sum(msgs)
     else:
         return F.sum(msgs, 1)
 
