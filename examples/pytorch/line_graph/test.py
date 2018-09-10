2b09281178b6bec23b1f95cc650eca0ad53cfa28
GaiYu0 <yg1246@nyu.edu>
2018-09-10 21:11:57 +0800

    line graph

--- examples/pytorch/line_graph/test.py
+++ examples/pytorch/line_graph/test.py
@@ -1,15 +1,13 @@
 """
-ipython3 test.py -- --features 1 16 16 --gpu -1 --n-classes 5 --n-iterations 10 --n-nodes 10 --order 3 --radius 3
+ipython3 test.py -- --features 1 16 16 --gpu -1 --n-classes 5 --n-iterations 10 --n-nodes 10 --radius 3
 """
-
 
 import argparse
 import networkx as nx
 import torch as th
-import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import gnn
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--features', nargs='+', type=int)
@@ -17,10 +15,8 @@ parser.add_argument('--gpu', type=int)
 parser.add_argument('--n-classes', type=int)
 parser.add_argument('--n-iterations', type=int)
 parser.add_argument('--n-nodes', type=int)
-parser.add_argument('--order', type=int)
 parser.add_argument('--radius', type=int)
 args = parser.parse_args()
-
 
 if args.gpu < 0:
     cuda = False
@@ -28,23 +24,18 @@ else:
     cuda = True
     th.cuda.set_device(args.gpu)
 
-
 g = nx.barabasi_albert_graph(args.n_nodes, 1).to_directed() # TODO SBM
 y = th.multinomial(th.ones(args.n_classes), args.n_nodes, replacement=True)
-
-
-network = gnn.GNN(args.features, args.order, args.radius, args.n_classes)
+model = gnn.GNN(g, args.features, args.radius, args.n_classes)
 if cuda:
-    network.cuda()
-ce = nn.CrossEntropyLoss()
-adam = optim.Adam(network.parameters())
-
+    model.cuda()
+opt = optim.Adam(model.parameters())
 
 for i in range(args.n_iterations):
-    y_bar = network(g)
-    loss = ce(y_bar, y)
-    adam.zero_grad()
+    y_bar = model()
+    loss = F.cross_entropy(y_bar, y)
+    opt.zero_grad()
     loss.backward()
-    adam.step()
+    opt.step()
 
     print('[iteration %d]loss %f' % (i, loss))
