af03448772e859d39b543323ecf70a205ad62fee
Da Zheng <zhengda1936@gmail.com>
2018-09-19 16:11:15 -0700

    keep node embedding.

--- python/dgl/graph.py
+++ python/dgl/graph.py
@@ -490,7 +490,7 @@ class DGLGraph(object):
         """
         return self._edge_frame.schemes
 
-    def set_n_repr(self, hu, u=ALL):
+    def set_n_repr(self, hu, u=ALL, inplace=False):
         """Set node(s) representation.
 
         To set multiple node representations at once, pass `u` with a tensor or
@@ -528,9 +528,9 @@ class DGLGraph(object):
                 self._node_frame[__REPR__] = hu
         else:
             if utils.is_dict_like(hu):
-                self._node_frame[u] = hu
+                self._node_frame.update_rows(u, hu, inplace=inplace)
             else:
-                self._node_frame[u] = {__REPR__ : hu}
+                self._node_frame.update_rows(u, {__REPR__ : hu}, inplace=inplace)
 
     def get_n_repr(self, u=ALL):
         """Get node(s) representation.
