05d487522dbe186e29400c58f2a15a5b41892dff
Ivan Brugere <ivabruge@amazon.com>
2018-09-26 12:28:28 -0700

    improved sampling
    supporing sampling methods:
    1. importance sampling
    2. random walk batched
    3. fixed-sized neighbor BFS batched
    4. fixed-sized level BFS batched
    
    -- Sampling function definitions/outputs now standardized
    -- random walk sampling as in https://arxiv.org/abs/1806.01973
    -- updated GCN default params for new batching
    -- cleaned/commented

--- examples/pytorch/gcn/gcn_minibatch.py
+++ examples/pytorch/gcn/gcn_minibatch.py
@@ -45,6 +45,7 @@ class GCN(nn.Module):
         for i in G.nodes:
             G.add_edge(i,i)
         return G
+    
     def __init__(self,
                  g,
                  in_feats,
@@ -55,20 +56,27 @@ class GCN(nn.Module):
                  dropout, 
                  fn_batch,
                  fn_batch_params):
+        
         super(GCN, self).__init__()
+        
+        #add self loops
         g = self.add_self_edges_networkx(g)
+        
         #generate minibatches
         fn_batch_params["G"] = g               
-        if fn_batch == sampling.importance_sampling_networkx:
-            self.batch_type = "importance"
+        if fn_batch == sampling.importance_sampling_networkx:  #if Importance Sampling (IS)
+            self.batch_type={"IS":True, "NS":False}  #convenience flags
             self.batches, q = fn_batch(**fn_batch_params)            
             self.q = q
-        else:
-            self.batch_type = "minibatch"
+        elif fn_batch == sampling.minibatch_seed_traverse: #if neighborhood sampling
+            self.batch_type={"IS":False, "NS":True}
             self.batches = fn_batch(**fn_batch_params)
+        else:
+            raise Exception("sampling not supported") #what are you doing?
         self.g = g    
         self.dropout = dropout
         self.n_classes = n_classes
+              
         # input layer
         self.layers = nn.ModuleList([NodeApplyModule(in_feats, n_hidden, activation)])
 
@@ -80,33 +88,40 @@ class GCN(nn.Module):
         self.layers.append(NodeApplyModule(n_hidden, n_classes))
         
     def forward(self, features, labels, mask, fn_reduce=F.log_softmax, log=False):
-        if self.batch_type == "importance":
+        #set up functions and data
+        if self.batch_type["IS"]:
             q = self.q
-            q_rep = torch.unsqueeze(torch.tensor([q[i] if i in q else 0 for i in range(features.shape[0])]),1)
+            q_rep = torch.unsqueeze(torch.tensor([q[i] if i in q else 0 for i in range(features.shape[0])]),1) #store degree distribution
             gcn_reduce = self.importance_reduce
             gcn_msg = self.importance_msg
-        else:
+        elif self.batch_type["NS"]:
             self.g.set_n_repr({'h':features})
             gcn_reduce = self.gcn_reduce
             gcn_msg = self.gcn_msg
-            
-        ret = torch.zeros((features.size()[0], self.n_classes))
-        node_count = 0
+        
+        ret = torch.zeros((features.size()[0], self.n_classes)) #output layer aggregate 
+        node_count = 0 #bookkeeping
         for batch in self.batches:
             nodes_prev = None
-            for l_id, layer in enumerate(self.layers):   
-                nodes = set.union(*[nodeset for depth,nodeset in batch.items() if depth >= l_id])
-                if l_id==0:                                      
+            for l_id, layer in enumerate(self.layers):
+                
+                #handle subgraph nodes
+                if self.batch_type["IS"]: # use fixed node set
+                    nodes = batch[0]
+                elif self.batch_type["NS"]: #expand node set
+                    nodes = set.union(*[nodeset for depth,nodeset in batch.items() if depth >= l_id])
+
+                if l_id==0:  #if level 0 use input features                                     
                     g_sub = dgl.DGLSubGraph(parent=self.g, nodes=list(nodes))
                     a = torch.tensor(list(nodes))
                     features_sub = torch.index_select(features, dim=0, index=a)
-                    if self.batch_type == "importance": 
+                    if self.batch_type["IS"]:  #branch because we might need more fields
                         q_sub = torch.index_select(q_rep, dim=0, index=a)
                         g_sub.set_n_repr({'h':features_sub, 'q':q_sub})
-                    else:
+                    elif self.batch_type["NS"]:
                         g_sub.set_n_repr({'h':features_sub})
-                else:                    
-                    idx = np.array([[i, key_i] for i, key_i in enumerate(nodes_prev) if key_i in nodes])
+                else:  #else deeper level                  
+                    idx = np.array([[i, key_i] for i, key_i in enumerate(nodes_prev) if key_i in nodes])  #if subgraph shrinks, reindex. e.g. where are old [0, M] indices in [0, N] ? M >= N 
                     g_sub_new = dgl.DGLSubGraph(parent=g_sub, nodes=idx[:, 0])
                     g_sub_new.copy_from(parent=g_sub)
                     g_sub = g_sub_new
@@ -115,7 +130,6 @@ class GCN(nn.Module):
                 node_count+= len(g_sub.nodes)
             idx_sub = torch.tensor(idx[:, 1])
             ret.index_copy_(0, idx_sub, fn_reduce(g_sub.pop_n_repr(key='h'), 1))
-            
         return ret, node_count            
            
 def main(params, data = None):
@@ -167,14 +181,17 @@ def main(params, data = None):
         
         print("Epoch {:05d} | Loss {:.4f} | Time(s) {:.4f} | Nodes {:d}".format(
             epoch, loss.item(), t_delta, node_count))
-        ret["time"].append(t_delta)
+        ret["time"].append(t_delta)   #bookkeeping here
         ret["loss"].append(loss.item())
         ret["nodes"].append(node_count)
     return ret
 
-
+#build all default parameters for execution
 def default_params():
-    fn_batch_params = {"depth": 2, "fn_neighborhood": sampling.neighborhood_networkx, "max_neighbors":100, 'seed_size':10, 'percent_nodes':0.9}    
+    
+    #seed_BFS_frontier_sampling(G,  seed_size, depth, fn_neighborhood, max_level_nodes = None, seed_nodes=None, percent_nodes=.90)
+    
+    fn_batch_params = {"depth": 3, "fn_neighborhood": sampling.neighborhood_networkx, "max_level_nodes":100, 'seed_size':10, 'percent_nodes':0.9}    
     return {"dataset":"cora",
               "dropout":0,
               "gpu":-1,
@@ -182,10 +199,11 @@ def default_params():
               "epochs":20, 
               "hidden":16,
               "layers": fn_batch_params["depth"], 
-              "fn_batch": sampling.minibatch_seed_traverse,
+              "fn_batch": sampling.seed_BFS_frontier_sampling,
               "fn_batch_params":fn_batch_params,
               "fn_loss": F.nll_loss}
 
+#handle command line params
 if len(sys.argv) > 1:
     parser = argparse.ArgumentParser(description='GCN')
     dgld.register_data_args(parser)
--- python/dgl/sampling.py
+++ python/dgl/sampling.py
@@ -10,76 +10,254 @@ import collections as c
 import numpy as np
 import numpy.random as npr
 
+#####
+### Seed Methods
+####
 
-def seeds(V, seed_size, num_batches):
-    return [list(npr.choice(V, seed_size, replace=False)) for _ in range(num_batches)]
-    #return list(chunk_iter(npr.permutation(V), s))[0:k]
 
+
+
+###
+## Sample 'num_batches' lists of seeds (node IDs) each of length 'seed_size'
+## @param V list of nodes
+## @param seed_size int
+## @param num_batches int
+## @param @replace boolean
+## @return a 2-D list of [[seeds], [seeds]....]
+###
+def seeds(V, seed_size, num_batches, replace=False):
+    return [list(npr.choice(V, seed_size, replace=replace)) for _ in range(num_batches)]
+
+###
+## Sample batches of size 'seed_size' until 'percent_nodes'*len(V) of 'V' is consumed. This will produce a variable number of seed-sets over a fixed number (but random selection) of nodes. To fix the source set, pre-sample and consume this list
+## @param seed_size int
+## @param num_batches int
+## @param percent_nodes float
+## @param @replace boolean
+## @return a 2-D list of [[seeds], [seeds]....]
+###
 def seeds_consume(V, seed_size, num_nodes=None, percent_nodes=.90):
     if num_nodes is None:
         num_nodes = int(np.round(len(V)*percent_nodes))
     return list(chunk_iter(list(npr.permutation(V)[0:min([num_nodes, len(V)])]), seed_size))
+
+
+
+###
+## Graph Traversal Methods
+###
+
+
+
+###
+## This implements Importance Sampling (IS) node sampling in https://arxiv.org/abs/1801.10247
+## This is merely sampling nodes porportional to degree. The authors then induce subgraphs over this set of sampled nodes 
+## (This implements for a networkx object, implementation may vary slightly for other representations) 
+## @param G a networkx graph 
+## @param n int the size of the node sample
+## @param batches int the number of repeated node samples
+## @param fn string the the sampling criteria to weight samples, (we're using degree, implement any others)
+## @return list a list of batch dictionaries [d1, d2... di...] where dictionary d_i = {0: [nodes]} 
+##     this representation allows for multiple levels in other sampling methods. This method has only one level.
+## @return dict probability distribution where q[v] = degree(v)/|E| (for degree weighting)
+###
+def importance_sampling_networkx(G, n=100, batches=100, fn="degree"):    
+    q = {} #probability distribution for each q[v]
+    ret = [] #list of batch samples 
+    if fn == "degree":
+        degrees = G.degree
+        s = sum(dict(degrees).values()) #total edges (directed)
+        for v in G.nodes:
+            q[v] = degrees[v]/s #my degree
+        for i in range(batches):
+            r = {}
+            r[0] = set([int(i) for i in npr.choice(list(q.keys()), size=n, replace=False, p=list(q.values()))]) # select 'n' keys using probability distribution 'q.values()'  
+            ret.append(r)
+    return ret, q
+
+
+###
+## Samples 'nodes' nodes based on hitting probability of a random walker over seed sets in list 'seedset_list' (advanced: using seed selection probability list 'weights' of len(seedset), assumes fixed-length seed-set)
+## The walker randomly selects a node, walks with 'p_restart' probability of restarting, for 'iters' iterations. Each visitation is a hit. Nodes are ranked by hits selected in descending order.
+##   
+## Some walkers enforce no cycles. for speed, this one doesn't. 
+## 
+## This is the relevance sampling in the neighborhood sampling (NS) model: https://arxiv.org/abs/1806.01973 
+##    
+## @param G a graph G (in any representation we've implemented neighborhood)
+## @param seedset_list A 2-D list of seeds (see 'Seed Methods' above)
+## @param depth int the number of levels of nodes (e.g. in geodesic distance) to generate
+## @param fn_neighborhood *function a function pointer implementing the neighborhood function on G (see 'Accessory Functions' below)
+## @param max_level_nodes int, list, or None: the maximum number of nodes sampled per level. if a list, samples according to 'max_level_nodes[curr]' current node depth
+## @param p_restart float the restart probability
+## @param iters int the number of walking iterations
+## @param weights a vector specifying weights for seed selection (default: integer, uniform sampling)
+## @param max_path_length int the maximum path length, (so we can generate path lengths with a single bernoulli sampling)
+## @return list a list of batch dictionaries [d1, d2... di...] where dictionary d_i = {0: [nodes], 1:[nodes]...} 
+###
+def seed_random_walk(G, seedset_list, depth, fn_neighborhood, max_level_nodes=None, p_restart=0.1, iters=1000, weights=1, max_path_length=20):
+    
+    if max_level_nodes is None:
+        max_level_nodes = [np.inf for i in range(depth)]
+    elif isinstance(max_level_nodes, int):
+        max_level_nodes = [max_level_nodes for i in range(depth)]    
+    
+    rr = []
+    for S in seedset_list:
+        dists = c.defaultdict(lambda:np.inf) #store best distances. Note: this approximates minimum path lengths, possible that dists[v] > minimum_path_length(v)
+        hits = c.defaultdict(int) #store node hits
+        if isinstance(weights, int): ##if unweighted
+            s_iter = npr.choice(S, size=iters, replace=True)  #generate full list of seeds
+        else:
+            s_iter = npr.choice(S, size=iters, replace=True, p=weights)  #generate full list of seeds
+        
+        for s in s_iter: 
+            l = list(npr.binomial(p_restart, 1, size=max_path_length)) + [1] #generate path length (ensure termination after 'size' steps)  
+            for i in range(l.index(1)):
+                hits[s]+= 1 #increment hits
+                dists[s] = min(i, dists[s]) #check best distance
+                neigh = fn_neighborhood(G, s)
+                if not neigh: #no neighbors, end of path
+                    break               
+                s = int(npr.choice(neigh)) #choose single neighbor
+        hits = {k:v for k,v in hits.items() if dists[k] <= depth-1} #filter hits by maximum depth
+
+        d_sorted = sorted(hits, key=lambda k: hits[k])[::-1] ##sort filtered nodes by hits (descending)
+        r = c.defaultdict(set)
+        for key in d_sorted:
+            k_iter = int(depth-1-dists[key])
+            if len(r[k_iter]) < max_level_nodes[k_iter]: #enforce maximum number per level
+                r[k_iter].add(int(key))
+        rr.append(dict(r))
+    return rr   
+
+###
+## Samples neighbors around a seed set in a BFS fashion, where we ensure exactly 'max_neighbors' of each sampled node are sampled
+##
+## @param G a graph G (in any representation we've implemented neighborhood)
+## @param seedset_list A 2-D list of seeds (see 'Seed Methods' above) 
+## @param depth int the number of levels of nodes (e.g. in geodesic distance) to generate
+## @param fn_neighborhood *function a function pointer implementing the neighborhood function on G (see 'Accessory Functions' below)
+## @param max_neighbors int, list, or None: the maximum number of neighbors sampled per visited node at the current depth. if a list, samples according to 'max_neighbors[curr]' current node depth
+## @return list a list of batch dictionaries [d1, d2... di...] where dictionary d_i = {0: [nodes], 1:[nodes]...} 
+##
+###
+def seed_neighbor_expansion(G,seedset_list,depth, fn_neighborhood, max_neighbors = None):
+    
+    if max_neighbors is None: #build max_neighbor list over depth
+        max_neighbors = [np.inf for i in range(depth)]
+    elif isinstance(max_neighbors, int):
+        max_neighbors = [max_neighbors for i in range(depth)]
+    
+    rr = []
+    for S in seedset_list: #for seed-set
+        d = {} #distances
+        to_visit = set(S)  
+        curr = 1
+        
+        for s in S: #init distance 0
+            d[s] = 0
+        
+        while (len(to_visit)) and curr < depth: #while nodes to visit and more depth
+            next_visit = set()       
+            for s in to_visit: #for each node
+                n_iter=0 #number of nodes added per 's'
+                neigh = fn_neighborhood(G, s) 
+                npr.shuffle(neigh) #randomize neighbors
+                for v in neigh:               
+                    if v not in d: #if v unseen
+                        next_visit.add(v)
+                        d[v] = curr #first we've seen this node, current depth
+                        n_iter+=1 #increment number of nodes added for s
+                        if n_iter >= max_neighbors[curr]: #if we've added neighbors for s
+                            break                        
+            curr+= 1
+            to_visit = next_visit
+        
+        r = c.defaultdict(set)
+        
+        for key,d_k in d.items():
+            r[int(depth-1-d_k)].add(int(key))
+        rr.append(dict(r))
+    return rr
+    
+## Samples neighbors around a seed set in a BFS fashion, where we ensure exactly 'max_level_nodes' are randomly sampled per level in depth
+##
+## @param G a graph G (in any representation we've implemented neighborhood)
+## @param seedset_list A 2-D list of seeds (see 'Seed Methods' above) 
+## @param depth int the number of levels of nodes (e.g. in geodesic distance) to generate
+## @param fn_neighborhood *function a function pointer implementing the neighborhood function on G (see 'Accessory Functions' below)
+## @param max_level_nodes int, list, or None: the maximum number of neighbors sampled per level over *all* visited nodes at this level. if a list, samples according to 'max_neighbors[curr]' current node depth
+## @return list a list of batch dictionaries [d1, d2... di...] where dictionary d_i = {0: [nodes], 1:[nodes]...} 
+    
+def seed_BFS_frontier(G,seedset_list,depth, fn_neighborhood, max_level_nodes = None):
+    
+    if max_level_nodes is None:
+        max_level_nodes = [np.inf for i in range(depth)]
+    elif isinstance(max_level_nodes, int):
+        max_level_nodes = [max_level_nodes for i in range(depth)]
+    
+    rr = []
+    for S in seedset_list: #for seedset
+        d = {} #distances
+        to_visit = set(S) 
+        curr = 1
+        
+        for s in S: #initialize zero depth
+            d[s] = 0
+        
+        while (len(to_visit)) and curr < depth: #while to nodes visit and more depth
+            next_visit = set()       
+            for s in to_visit: #for node to visit
+                neigh = fn_neighborhood(G, s) #expand neighbors
+                for v in neigh:
+                    if v not in d: #if unseen
+                        d[v] = curr 
+                        next_visit.add(v) #add all unseen neighbors
+            
+            neigh = npr.permutation(list(next_visit)) #randomize unseen neighbors
+            neigh = set(neigh[0:min(len(neigh), max_level_nodes[curr])]) #truncate to maximum number
+            
+            #kill unsampled neighbors with infinity
+            if len(neigh):
+                for v in next_visit:
+                    if v not in neigh: #v not sampled
+                        d[v] = np.inf
+            to_visit = neigh #visit only sampled neighs
+            curr+= 1
+        r = c.defaultdict(set)
+        
+        for key,d_k in d.items():
+            if d_k != np.inf: #if we didnt kill
+                r[int(depth-1-d_k)].add(int(key))
+        rr.append(dict(r))
+    return rr
+
+###
+# Seeded Graph Traversals
+# Convenience methods combining seeding and minibatch sampling
+### 
+def seed_expansion_sampling(G, seed_size, depth, fn_neighborhood, max_neighbors = None, seed_nodes=None, percent_nodes=.90):
+    seedset_list = seeds_consume(G.nodes, seed_size =  seed_size, num_nodes=seed_nodes,percent_nodes=percent_nodes)
+    return seed_neighbor_expansion(G,seedset_list,depth, fn_neighborhood, max_neighbors=max_neighbors)
+
+def seed_BFS_frontier_sampling(G,  seed_size, depth, fn_neighborhood, max_level_nodes = None, seed_nodes=None, percent_nodes=.90):
+    seedset_list = seeds_consume(G.nodes, seed_size =  seed_size, num_nodes=seed_nodes,percent_nodes=percent_nodes)
+    return seed_BFS_frontier(G,seedset_list,depth, fn_neighborhood, max_level_nodes=max_level_nodes)
+
+def seed_random_walk_sampling(G, seed_size, depth, fn_neighborhood, max_level_nodes=None, seed_nodes=None, percent_nodes=.90, p_restart=0.3, iters=1000, weights=1, max_path_length=10):
+    seedset_list = seeds_consume(G.nodes, seed_size =  seed_size, num_nodes=seed_nodes,percent_nodes=percent_nodes)
+    return seed_random_walk(G, seedset_list, depth, fn_neighborhood, max_level_nodes=max_level_nodes, p_restart=p_restart, iters=iters, weights=weights, max_path_length=max_path_length)
+
+####
+## Accessories
+#####
 
 #http://stackoverflow.com/questions/8290397/how-to-split-an-iterable-in-constant-size-chunks
 def chunk_iter(iterable, n=1):
     l = len(iterable)
     for ndx in range(0, l, n):
         yield iterable[ndx:min(ndx + n, l)]
-
-def importance_sampling_networkx(G, n, levels, batches=100, fn="degree"):    
-    q = {}
-    ret = []
-    if fn == "degree":
-        degrees = G.degree
-        s = sum(dict(degrees).values())
-        for v in G.nodes:
-            q[v] = degrees[v]/s
-        for i in range(batches):
-            r = {}
-            for l in range(levels):
-                r[l] = set([int(i) for i in npr.choice(list(q.keys()), size=n, replace=False, p=list(q.values()))])
-            ret.append(r)
-    return ret, q
-
-def minibatch_seed_traverse(G,  seed_size, depth, fn_neighborhood, max_neighbors = np.inf, num_nodes=None, percent_nodes=.90):
-    seedset_list = seeds_consume(G.nodes, seed_size =  seed_size, num_nodes=num_nodes,percent_nodes=percent_nodes)
-    return minibatch_traverse(G,seedset_list,depth, fn_neighborhood, max_neighbors=max_neighbors)
-    
-def minibatch_traverse(G,seedset_list,depth, fn_neighborhood, max_neighbors = np.inf):
-    rr = []
-    for S in seedset_list:
-        d = {}
-        to_visit = set(S)  
-        curr = 1
-        
-        for s in S:
-            d[s] = 0
-        
-        while (len(to_visit)) and curr <= depth:
-            next_visit = set()       
-            for s in to_visit:
-                if len(next_visit) >= max_neighbors:
-                    break
-                neigh = fn_neighborhood(G, s)
-                npr.shuffle(neigh)
-                #neigh = [int(v) for v in neigh]
-                for v in neigh:
-                    if v not in d:
-                        d[v] = curr
-                        next_visit.add(v)
-                    if len(next_visit) >= max_neighbors:
-                        break
-            curr+= 1
-            to_visit = next_visit
-        
-        r = c.defaultdict(set)
-        
-        #print(d)
-        for key,d_k in d.items():
-            r[int(depth-d_k)].add(int(key))
-        rr.append(dict(r))
-        #print(dict(r))
-    return rr
 
 def nodes_networkx(G):
     return G.nodes
@@ -98,3 +276,25 @@ def neighborhood_graphtool(G, i):
 
 def neighborhood_igraph(G, i):
     return G.neighbors(i, mode="OUT")    
+
+
+####
+####
+######
+## Minimal running example:
+##
+
+
+#import dgl.data as dgld
+#import dgl.sampling as sampling
+#import gcn.gcn_minibatch as gcn 
+#import dgl
+#
+#params = gcn.default_params()
+#data = dgld.load_data_dict(params)
+#g = dgl.DGLGraph(data.graph)
+#
+#s_rw = sampling.seed_random_walk_sampling(g, params['fn_batch_params']["seed_size"], depth=params["layers"], fn_neighborhood=sampling.neighborhood_networkx, percent_nodes=params['fn_batch_params']["percent_nodes"])
+#s_fr = s2 = sampling.seed_BFS_frontier_sampling(g, params['fn_batch_params']["seed_size"], depth=params["layers"], fn_neighborhood=sampling.neighborhood_networkx, percent_nodes=params['fn_batch_params']["percent_nodes"])
+#s_se = sampling.seed_expansion_sampling(g, params['fn_batch_params']["seed_size"], depth=params["layers"], fn_neighborhood=sampling.neighborhood_networkx, percent_nodes=params['fn_batch_params']["percent_nodes"])
+#s_is, q = sampling.importance_sampling_networkx(g)
