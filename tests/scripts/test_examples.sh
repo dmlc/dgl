a269d4979372cbfc8e13b5dd8b7074b2240f3aa1
Lingfan Yu <ly979@nyu.edu>
2018-10-18 20:30:07 +0000

    use python3

--- tests/scripts/test_examples.sh
+++ tests/scripts/test_examples.sh
@@ -10,7 +10,7 @@ function fail {
 pushd $GCN_EXAMPLE_DIR> /dev/null
 
 # test CPU
-python gcn.py --dataset cora --gpu -1 || fail "run gcn.py CPU"
-python gcn_spmv.py --dataset cora --gpu -1 || fail "run gcn_spmv.py on CPU"
+python3 gcn.py --dataset cora --gpu -1 || fail "run gcn.py CPU"
+python3 gcn_spmv.py --dataset cora --gpu -1 || fail "run gcn_spmv.py on CPU"
 
 popd > /dev/null
--- tests/scripts/test_examples_gpu.sh
+++ tests/scripts/test_examples_gpu.sh
@@ -13,7 +13,7 @@ function fail {
 pushd $GCN_EXAMPLE_DIR> /dev/null
 
 # test GPU
-python gcn.py --dataset cora --gpu 0 || fail "run gcn.py GPU"
-python gcn_spmv.py --dataset cora --gpu 0 || fail "run gcn_spmv.py GPU"
+python3 gcn.py --dataset cora --gpu 0 || fail "run gcn.py GPU"
+python3 gcn_spmv.py --dataset cora --gpu 0 || fail "run gcn_spmv.py GPU"
 
 popd > /dev/null
