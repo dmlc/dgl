cd4819e31d6e81a0d8acad2bb555867cd4c840d7
GaiYu0 <yg1246@nyu.edu>
2018-10-18 09:30:26 +0800

    pretty printer

--- examples/pytorch/line_graph/gnn.py
+++ examples/pytorch/line_graph/gnn.py
@@ -1,12 +1,3 @@
-"""
-Supervised Community Detection with Hierarchical Graph Neural Networks
-https://arxiv.org/abs/1705.08415
-
-Deviations from paper:
-- Pm Pd
-"""
-
-
 import copy
 import itertools
 import dgl
@@ -16,23 +7,22 @@ import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class GNNModule(nn.Module):
     def __init__(self, in_feats, out_feats, radius):
         super().__init__()
         self.out_feats = out_feats
         self.radius = radius
 
-        new_linear = lambda: nn.Linear(in_feats, out_feats * 2)
-        new_module_list = lambda: nn.ModuleList([new_linear() for i in range(radius)])
+        new_linear = lambda: nn.Linear(in_feats, out_feats)
+        new_linear_list = lambda: nn.ModuleList([new_linear() for i in range(radius)])
 
         self.theta_x, self.theta_deg, self.theta_y = \
             new_linear(), new_linear(), new_linear()
-        self.theta_list = new_module_list()
+        self.theta_list = new_linear_list()
 
         self.gamma_y, self.gamma_deg, self.gamma_x = \
             new_linear(), new_linear(), new_linear()
-        self.gamma_list = new_module_list()
+        self.gamma_list = new_linear_list()
 
         self.bn_x = nn.BatchNorm1d(out_feats)
         self.bn_y = nn.BatchNorm1d(out_feats)
@@ -48,27 +38,27 @@ class GNNModule(nn.Module):
             z_list.append(g.get_n_repr())
         return z_list
 
-    def forward(self, g, lg, x, y, deg_g, deg_lg, eid2nid):
-        xy = F.embedding(eid2nid, x)
+    def forward(self, g, lg, x, y, deg_g, deg_lg, pm_pd):
+        pmpd_x = F.embedding(pm_pd, x)
 
-        x_list = [theta(z) for theta, z in zip(self.theta_list, self.aggregate(g, x))]
+        sum_x = sum(theta(z) for theta, z in zip(self.theta_list, self.aggregate(g, x)))
 
         g.set_e_repr(y)
         g.update_all(fn.copy_edge(), fn.sum())
-        yx = g.get_n_repr()
+        pmpd_y = g.get_n_repr()
 
-        x = self.theta_x(x) + self.theta_deg(deg_g * x) + sum(x_list) + self.theta_y(yx)
-        x = self.bn_x(x[:, :self.out_feats] + F.relu(x[:, self.out_feats:]))
+        x = self.theta_x(x) + self.theta_deg(deg_g * x) + sum_x + self.theta_y(pmpd_y)
+        n = self.out_feats // 2
+        x = th.cat([x[:, :n], F.relu(x[:, n:])], 1)
+        x = self.bn_x(x)
 
-        y_list = [gamma(z) for gamma, z in zip(self.gamma_list, self.aggregate(lg, y))]
-        lg.set_n_repr(xy)
-        lg.update_all(fn.copy_src(), fn.sum())
-        xy = lg.get_n_repr()
-        y = self.gamma_y(y) + self.gamma_deg(deg_lg * y) + sum(y_list) + self.gamma_x(xy)
-        y = self.bn_y(y[:, :self.out_feats] + F.relu(y[:, self.out_feats:]))
+        sum_y = sum(gamma(z) for gamma, z in zip(self.gamma_list, self.aggregate(lg, y)))
+
+        y = self.gamma_y(y) + self.gamma_deg(deg_lg * y) + sum_y + self.gamma_x(pmpd_x)
+        y = th.cat([y[:, :n], F.relu(y[:, n:])], 1)
+        y = self.bn_y(y)
 
         return x, y
-
 
 class GNN(nn.Module):
     def __init__(self, feats, radius, n_classes):
@@ -82,14 +72,8 @@ class GNN(nn.Module):
         self.module_list = nn.ModuleList([GNNModule(m, n, radius)
                                           for m, n in zip(feats[:-1], feats[1:])])
 
-    def forward(self, g, lg, deg_g, deg_lg, eid2nid):
-        def normalize(x):
-            x = x - th.mean(x, 0)
-            x = x / th.sqrt(th.mean(x * x, 0))
-            return x
-
-        x = normalize(deg_g)
-        y = normalize(deg_lg)
+    def forward(self, g, lg, deg_g, deg_lg, pm_pd):
+        x, y = deg_g, deg_lg
         for module in self.module_list:
-            x, y = module(g, lg, x, y, deg_g, deg_lg, eid2nid)
+            x, y = module(g, lg, x, y, deg_g, deg_lg, pm_pd)
         return self.linear(x)
