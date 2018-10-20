2816ae809b4665a4eb40a461838f1f5baa24f4f6
QipengGuo <qpguo12@fudan.edu.cn>
2018-10-20 12:41:42 +0800

    update tree lstm

--- examples/pytorch/tree_lstm/tree_lstm.py
+++ examples/pytorch/tree_lstm/tree_lstm.py
@@ -10,42 +10,55 @@ import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
 
-import dgl
+def topological_traverse(G):
+    indegree_map = {v: d for v, d in G.in_degree() if d > 0}
+    # These nodes have zero indegree and ready to be returned.
+    zero_indegree = [v for v, d in G.in_degree() if d == 0]
+    while True:
+        yield zero_indegree
+        next_zero_indegree = []
+        while zero_indegree:
+            node = zero_indegree.pop()
+            for _, child in G.edges(node):
+                indegree_map[child] -= 1
+                if indegree_map[child] == 0:
+                    next_zero_indegree.append(child)
+                    del indegree_map[child]
+        if len(next_zero_indegree) == 0:
+            break
+        zero_indegree = next_zero_indegree
 
-class ChildSumTreeLSTMCell(nn.Module):
+class TreeLSTMCell(nn.Module):
     def __init__(self, x_size, h_size):
-        super(ChildSumTreeLSTMCell, self).__init__()
+        super(TreeLSTMCell, self).__init__()
         self.W_iou = nn.Linear(x_size, 3 * h_size)
-        self.U_iou = nn.Linear(h_size, 3 * h_size)
-        self.W_f = nn.Linear(x_size, h_size)
-        self.U_f = nn.Linear(h_size, h_size)
-        self.rt = 0.
-        self.ut = 0.
+        self.U_iou = nn.Linear(2 * h_size, 3 * h_size)
+        self.U_f = nn.Linear(2 * h_size, 2 * h_size)
 
     def message_func(self, src, edge):
-        return {'h' : src['h'], 'c' : src['c']}
+        return src
 
     def reduce_func(self, node, msgs):
-        # equation (2)
-        h_tild = th.sum(msgs['h'], 1)
-        # equation (4)
-        wx = self.W_f(node['x']).unsqueeze(1)  # shape: (B, 1, H)
-        uh = self.U_f(msgs['h']) # shape: (B, deg, H)
-        f = th.sigmoid(wx + uh)  # shape: (B, deg, H)
-        # equation (7) second term
-        c_tild = th.sum(f * msgs['c'], 1)
-        return {'h_tild' : h_tild, 'c_tild' : c_tild}
-    
-    def apply_func(self, node):
-        # equation (3), (5), (6)
-        iou = self.W_iou(node['x']) + self.U_iou(node['h_tild'])
+        # msgs['h'] (B, 2, H)
+        h_cat = msgs['h'].view(msgs['h'].size(0), -1)
+        f = th.sigmoid(self.U_f(h_cat)).view(*msgs['h'].size())
+        c_f = th.sum(f * msgs['c'], 1)
+        return {'h_cat' : h_cat, 'c_f' : c_f}
+
+    def update_func(self, node, accum):
+        if accum is None: #leaf node
+            iou = self.W_iou(node['x'])
+        else: #internal node
+            iou = self.U_iou(accum['h_cat'])
         i, o, u = th.chunk(iou, 3, 1)
         i, o, u = th.sigmoid(i), th.sigmoid(o), th.tanh(u)
-        # equation (7)
-        c = i * u + node['c_tild']
-        # equation (8)
+
+        if accum is None:
+            c = i * u
+        else:
+            c = i * u + accum['c_f']
         h = o * th.tanh(c)
-        return {'h' : h, 'c' : c}
+        return {'h' : h, 'c' : c} 
 
 class TreeLSTM(nn.Module):
     def __init__(self,
@@ -54,33 +67,33 @@ class TreeLSTM(nn.Module):
                  h_size,
                  num_classes,
                  dropout,
-                 cell_type='childsum'):
+                 pretrained_emb=None):
         super(TreeLSTM, self).__init__()
         self.x_size = x_size
-        self.h_size = h_size
-        # TODO(minjie): pre-trained embedding like GLoVe
         self.embedding = nn.Embedding(num_vocabs, x_size)
+        if pretrained_emb is not None:
+            print('Using glove')
+            self.embedding.weight.data.copy_(pretrained_emb)
+            self.embedding.weight.requires_grad = True
         self.dropout = nn.Dropout(dropout)
         self.linear = nn.Linear(h_size, num_classes)
-        if cell_type == 'childsum':
-            self.cell = ChildSumTreeLSTMCell(x_size, h_size)
-        else:
-            raise RuntimeError('Unknown cell type:', cell_type)
 
-    def forward(self, graph, zero_initializer, h=None, c=None, iterator=None, train=True):
+        self.cell = TreeLSTMCell(x_size, h_size)
+
+    def forward(self, batch, x, h, c, iterator=None, train=True):
         """Compute tree-lstm prediction given a batch.
 
         Parameters
         ----------
-        graph : dgl.DGLGraph
-            The batched trees.
-        zero_initializer : callable
-            Function to return zero value tensor.
-        h : Tensor, optional
+        batch : dgl.data.SSTBatch
+            The data batch.
+        x : Tensor
+            Initial node input.
+        h : Tensor
             Initial hidden state.
-        c : Tensor, optional
+        c : Tensor
             Initial cell state.
-        iterator : graph iterator, optional
+        iterator : graph iterator
             External iterator on graph.
 
         Returns
@@ -88,77 +101,25 @@ class TreeLSTM(nn.Module):
         logits : Tensor
             The prediction of each node.
         """
-        g = graph
-        n = g.number_of_nodes()
-        g.register_message_func(self.cell.message_func)
-        g.register_reduce_func(self.cell.reduce_func)
-        g.register_apply_node_func(self.cell.apply_func)
+        g = batch.graph
+        g.register_message_func(self.cell.message_func, batchable=True)
+        g.register_reduce_func(self.cell.reduce_func, batchable=True)
+        g.register_update_func(self.cell.update_func, batchable=True)
         # feed embedding
-        wordid = g.pop_n_repr('x')
-        mask = (wordid != dgl.data.SST.PAD_WORD)
-        wordid = wordid * mask.long()
-        embeds = self.embedding(wordid)
-        x = embeds * th.unsqueeze(mask, 1).float()
-        if h is None:
-            h = zero_initializer((n, self.h_size))
-        h_tild = zero_initializer((n, self.h_size))
-        if c is None:
-            c = zero_initializer((n, self.h_size))
-        c_tild = zero_initializer((n, self.h_size))
-        g.set_n_repr({'x' : x, 'h' : h, 'c' : c, 'h_tild' : h_tild, 'c_tild' : c_tild})
+        embeds = self.embedding(batch.wordid)
+
+        x = x.index_copy(0, batch.nid_with_word, embeds)
+        g.set_n_repr({'x' : x, 'h' : h, 'c' : c})
         # TODO(minjie): potential bottleneck
         if iterator is None:
             for frontier in topological_traverse(g):
                 #print('frontier', frontier)
-                g.pull(frontier)
+                g.update_to(frontier)
         else:
             for frontier in iterator:
-                g.pull(frontier)
+                g.update_to(frontier)
         # compute logits
         h = g.pop_n_repr('h')
         h = self.dropout(h)
         logits = self.linear(h)
         return logits
-
-'''
-class NAryTreeLSTM(TreeLSTM):
-    def __init__(self, n_embeddings, x_size, h_size, n_ary, n_classes):
-        super().__init__(n_embeddings, x_size, h_size, n_classes)
-
-        # TODO initializer
-        self.iou_w = nn.Parameter(th.randn(x_size, 3 * h_size))
-        self.iou_u = [nn.Parameter(th.randn(1, h_size, 3 * h_size)) for i in range(n_ary)]
-        self.iou_b = nn.Parameter(th.zeros(1, 3 * h_size))
-
-        # TODO initializer
-        self.f_x = nn.Parameter(th.randn(x_size, h_size))
-        self.f_h = [[nn.Parameter(th.randn(1, h_size, h_size))
-                     for i in range(n_ary)] for i in range(n_ary)]
-        self.f_b = nn.Parameter(th.zeros(1, h_size))
-
-    def internal_update_func(self, node_reprs, edge_reprs):
-        assert len(edge_reprs) > 0
-        assert all(msg['h'] is not None and msg['c'] is not None for msg in edge_reprs)
-
-        x = node_reprs['x']
-        n_children = len(edge_reprs)
-
-        iou_wx = th.mm(x, self.iou_w) if x is not None else 0
-        iou_u = th.cat(self.iou_u[:n_children], 0)
-        iou_h = th.cat([msg['h'] for msg in edge_reprs], 0).unsqueeze(1)
-        iou_uh = th.sum(th.bmm(iou_h, iou_u), 0)
-        i, o, u = th.chunk(iou_wx + iou_uh + self.iou_b, 3, 1)
-        i, o, u = th.sigmoid(i), th.sigmoid(o), th.tanh(u)
-
-        f_wx = th.mm(x, self.f_x).repeat(n_children, 1) if x is not None else 0
-        f_h = iou_h.repeat(n_children, 1, 1)
-        f_u = th.cat(sum([self.f_h[i][:n_children] for i in range(n_children)], []), 0)
-        f_uh = th.sum(th.bmm(f_h, f_u).view(n_children, n_children, -1), 0)
-        f = th.sigmoid(f_wx + f_uh + self.f_b)
-
-        c = th.cat([msg['c'] for msg in edge_reprs], 0)
-        c = i * u + th.sum(f * c, 0, keepdim=True)
-        h = o * th.tanh(c)
-
-        return {'h' : h, 'c' : c}
-'''
--- python/dgl/data/tree.py
+++ python/dgl/data/tree.py
@@ -5,17 +5,19 @@ Including:
 """
 from __future__ import absolute_import
 
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 from nltk.tree import Tree
 from nltk.corpus.reader import BracketParseCorpusReader
 import networkx as nx
 
-from .. import backend as F
-from ..graph import DGLGraph
-from .utils import download, extract_archive, get_download_dir
+import numpy as np
+import os
+import dgl
+import dgl.backend as F
+from dgl.data.utils import download, extract_archive, get_download_dir
 
 _urls = {
-    'sst' : 'https://www.dropbox.com/s/dw8kr2vuq7k4dqi/sst.zip?dl=1',
+    'sst' : 'https://www.dropbox.com/s/w0b4fka64096wqz/sst.zip?dl=1',
 }
 
 SSTBatch = namedtuple('SSTBatch', ['graph', 'nid_with_word', 'wordid', 'label'])
@@ -23,10 +25,13 @@ SSTBatch = namedtuple('SSTBatch', ['graph', 'nid_with_word', 'wordid', 'label'])
 class SST(object):
     """SST"""
     PAD_WORD=-1
+    UNK_WORD=-1
     def __init__(self, mode='train', vocab_file=None):
         self.mode = mode
         self.dir = get_download_dir()
         self.zip_file_path='{}/sst.zip'.format(self.dir)
+        self.pretrained_file = 'glove.840B.300d.txt' if mode == 'train' else ''
+        self.pretrained_emb = None
         self.vocab_file = '{}/sst/vocab.txt'.format(self.dir) if vocab_file is None else vocab_file
         download(_urls['sst'], path=self.zip_file_path)
         extract_archive(self.zip_file_path, '{}/sst'.format(self.dir))
@@ -37,27 +42,50 @@ class SST(object):
         print('Dataset creation finished. #Trees:', len(self.trees))
 
     def _load(self):
-        files = ['{}.txt'.format(self.mode)]
-        corpus = BracketParseCorpusReader('{}/sst'.format(self.dir), files)
-        sents = corpus.parsed_sents(files[0])
         # load vocab file
-        self.vocab = {}
+        self.vocab = OrderedDict()
         with open(self.vocab_file) as vf:
             for line in vf.readlines():
                 line = line.strip()
                 self.vocab[line] = len(self.vocab)
+
+        # filter glove
+        if self.pretrained_file != '' and os.path.exists(self.pretrained_file):
+            glove_emb = {}
+            with open(self.pretrained_file, 'r') as pf:
+                for line in pf.readlines():
+                    sp = line.split()
+                    if self.vocab.has_key(sp[0].lower()):
+                        glove_emb[sp[0].lower()] = np.array([float(x) for x in sp[1:]])
+        files = ['{}.txt'.format(self.mode)]
+        corpus = BracketParseCorpusReader('{}/sst'.format(self.dir), files)
+        sents = corpus.parsed_sents(files[0])
+
+        #initialize with glove
+        pretrained_emb = []
+        fail_cnt = 0
+        for line in self.vocab.keys():
+            if self.pretrained_file != '' and os.path.exists(self.pretrained_file):
+                if not glove_emb.has_key(line.lower()):
+                    fail_cnt += 1
+                pretrained_emb.append(glove_emb.get(line.lower(), np.random.uniform(-0.05, 0.05, 300)))
+
+        if self.pretrained_file != '' and os.path.exists(self.pretrained_file):
+            self.pretrained_emb = F.tensor(np.stack(pretrained_emb, 0))
+            print('Miss word in GloVe {0:.4f}'.format(1.0*fail_cnt/len(self.pretrained_emb)))
         # build trees
         for sent in sents:
             self.trees.append(self._build_tree(sent))
+
 
     def _build_tree(self, root):
         g = nx.DiGraph()
         def _rec_build(nid, node):
             for child in node:
                 cid = g.number_of_nodes()
-                if isinstance(child[0], str):
+                if isinstance(child[0], str) or isinstance(child[0], unicode):
                     # leaf node
-                    word = self.vocab[child[0].lower()]
+                    word = self.vocab.get(child[0].lower(), SST.UNK_WORD)
                     g.add_node(cid, x=word, y=int(child.label()))
                 else:
                     g.add_node(cid, x=SST.PAD_WORD, y=int(child.label()))
@@ -66,9 +94,7 @@ class SST(object):
         # add root
         g.add_node(0, x=SST.PAD_WORD, y=int(root.label()))
         _rec_build(0, root)
-        ret = DGLGraph()
-        ret.from_networkx(g, node_attrs=['x', 'y'])
-        return ret
+        return dgl.DGLGraph(g)
 
     def __getitem__(self, idx):
         return self.trees[idx]
@@ -79,3 +105,22 @@ class SST(object):
     @property 
     def num_vocabs(self):
         return len(self.vocab)
+
+    @staticmethod
+    def batcher(batch):
+        nid_with_word = []
+        wordid = []
+        label = []
+        gnid = 0
+        for tree in batch:
+            for nid in range(tree.number_of_nodes()):
+                if tree.nodes[nid]['x'] != SST.PAD_WORD:
+                    nid_with_word.append(gnid)
+                    wordid.append(tree.nodes[nid]['x'])
+                label.append(tree.nodes[nid]['y'])
+                gnid += 1
+        batch_trees = dgl.batch(batch)
+        return SSTBatch(graph=batch_trees,
+                        nid_with_word=F.tensor(nid_with_word, dtype=F.int64),
+                        wordid=F.tensor(wordid, dtype=F.int64),
+                        label=F.tensor(label, dtype=F.int64))
