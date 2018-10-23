2046980231c37935b8c405db1d7b1abb80530c30
Minjie Wang <minjie.wang@nyu.edu>
2018-10-23 16:27:15 -0400

    [DOC] API tutorials (#96)
    * setup sphinx-gallery; work on graph tutorial
    
    * draft dglgraph tutorial
    
    * update readme to include document url
    
    * rm obsolete file
    
    * Draft the message passing tutorial

---
+++ tutorials/1_first.py
@@ -0,0 +1,16 @@
+"""
+.. _tutorial-first:
+
+Your first example in DGL
+=========================
+
+TODO: either a pagerank or SSSP example
+"""
+
+###############################################################################
+# Create a DGLGraph
+# -----------------
+#
+# To start with, let's first import dgl
+
+import dgl
---
+++ tutorials/README.txt
@@ -0,0 +1,4 @@
+Tutorials
+=========
+
+DGL tutorials and examples
