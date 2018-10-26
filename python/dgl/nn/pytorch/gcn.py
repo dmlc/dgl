9c7ba1d69af3bd12ac1f20db9b5536669c7f1a29
Mufei Li <mufeili1996@gmail.com>
2018-10-26 17:27:18 +0800

    Fix GCN module
    1. Update the outdated graph convolution layer class
    2. Fix a bug in the code where dropout never works. Modules like dropout/batch norm depend on whether we are in the training stage or inference stage.

--- python/dgl/nn/pytorch/gcn.py
+++ python/dgl/nn/pytorch/gcn.py
@@ -6,48 +6,56 @@ Code: https://github.com/tkipf/gcn
 GCN with SPMV specialization.
 """
 import torch.nn as nn
+import torch.nn.functional as F
 
 from ... import function as fn
 from ...base import ALL, is_all
+
 
 class NodeUpdateModule(nn.Module):
     def __init__(self, in_feats, out_feats, activation=None):
         super(NodeUpdateModule, self).__init__()
         self.linear = nn.Linear(in_feats, out_feats)
         self.activation = activation
-        self.attribute = None
 
     def forward(self, node):
-        h = self.linear(node['accum'])
+        h = self.linear(node['h'])
         if self.activation:
             h = self.activation(h)
-        if self.attribute:
-            return {self.attribute: h}
-        else:
-            return h
+        return {'h' : h}
 
-class GCN(nn.Module):
+class GraphConvolutionLayer(nn.Module):
+    """Single graph convolution layer as in https://arxiv.org/abs/1609.02907.
+    Adjacency matrix normalization not supported yet."""
     def __init__(self,
+                 g,
                  in_feats,
                  out_feats,
                  activation,
                  dropout=0):
-        super(GCN, self).__init__()
+        super(GraphConvolutionLayer, self).__init__()
+        self.g = g
         self.dropout = dropout
         # input layer
         self.update_func = NodeUpdateModule(in_feats, out_feats, activation)
 
-    def forward(self, g, u=ALL, v=ALL, attribute=None):
+    def forward(self, features, u=ALL, v=ALL, attribute=None):
+        self.g.set_n_repr({'h' : features})
+        if self.dropout:
+            if is_all(u):
+                self.g.apply_nodes(apply_node_func=lambda node: {'h': F.dropout(node['h'], p=self.dropout,
+                                                                                training=self.training)})
+            else:
+                self.g.apply_nodes(u, apply_node_func=lambda node: {'h': F.dropout(node['h'], p=self.dropout,
+                                                                                   training=self.training)})
+
         if is_all(u) and is_all(v):
-            g.update_all(fn.copy_src(src=attribute),
-                         fn.sum(out='accum'),
-                         self.update_func,
-                         batchable=True)
+            self.g.update_all(fn.copy_src(src='h', out='m'),
+                              fn.sum(msg='m', out='h'),
+                              self.update_func)
         else:
-            g.send_and_recv(u, v,
-                            fn.copy_src(src=attribute),
-                            fn.sum(out='accum'),
-                            self.update_func,
-                            batchable=True)
-        g.pop_n_repr('accum')
-        return g
+            self.g.send_and_recv(u, v,
+                                 fn.copy_src(src='h', out='m'),
+                                 fn.sum(msg='m', out='h'),
+                                 self.update_func)
+        return self.g.pop_n_repr('h')
