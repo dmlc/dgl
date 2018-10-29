b566c5d3852cc562dd0c8e801afaa8a7cd473c26
Mufei Li <mufeili1996@gmail.com>
2018-10-30 04:56:12 +0800

    Move test function

--- tests/pytorch/test_function.py
+++ tests/pytorch/test_function.py
@@ -66,7 +66,45 @@ def test_src_mul_edge():
     assert th.allclose(g.get_n_repr()['h'],
             th.tensor([100., 1., 1., 1., 1., 1., 1., 1., 1., 284.]))
 
+def test_dynamic_addition():
+    N = 3
+    D = 1
+
+    g = dgl.DGLGraph()
+
+    # Test node addition
+    g.add_nodes(N)
+    g.set_n_repr({'h1': th.randn(N, D),
+                  'h2': th.randn(N, D)})
+    print('Node representations before dynamic addition')
+    print('--------------------------------------------')
+    print(g.get_n_repr())
+    g.add_nodes(3)
+    print('Node representations after dynamic addition')
+    print('--------------------------------------------')
+    print(g.get_n_repr())
+
+    # Test edge addition
+    g.add_edge(0, 1)
+    g.add_edge(1, 0)
+    g.set_e_repr({'h1': th.randn(2, D),
+                  'h2': th.randn(2, D)})
+    print('Edge representations before dynamic addition')
+    print('--------------------------------------------')
+    print(g.get_e_repr())
+
+    g.add_edges([0, 2], [2, 0])
+    print('Edge representations after adding edge')
+    print('--------------------------------------------')
+    print(g.get_e_repr())
+
+    g.add_edge(1, 2)
+    print('Edge representations after adding edges')
+    print('--------------------------------------------')
+    print(g.get_e_repr())
+
 if __name__ == '__main__':
     test_copy_src()
     test_copy_edge()
     test_src_mul_edge()
+    test_dynamic_addition()
