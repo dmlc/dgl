d39d8546c6afe6f180d281630c69ed52234b8951
Gan Quan <coin2028@hotmail.com>
2018-10-30 16:56:32 -0400

    section 3

--- tutorials/models/gcn_gat.py
+++ tutorials/models/gcn_gat.py
@@ -9,11 +9,11 @@ In this tutorial, we will go through the basics of DGL, in the following order:
     1. Creating a graph
     2. Setting/getting node/edge states
     3. Updating node/edge states using user-defined functions
-    4. Passing information from node to adjacent edges, and edges to adjacent
-       nodes
-    5. Implementing a Graph Convolutional Network (GCN) and a Graph Attention
+    4. Passing information to edges from endpoint nodes
+    5. Passing information to nodes from adjacent nodes and edges
+    6. Implementing a Graph Convolutional Network (GCN) and a Graph Attention
        Network (GAT)
-    6. Using built-in functions to simplify your implementation
+    7. Using built-in functions to simplify your implementation
 """
 
 ##############################################################################
@@ -91,6 +91,10 @@ g.set_n_repr({'X': X, 'Y': Y})
 # at least one key.
 # TODO: do we want to fix this behavior to allow initial partial setting?
 g.set_n_repr({'X': X[0:2], 'Y': Y[0:2]}, [0, 1])
+# You can also overwrite part of the fields.  The following overwrites field
+# X while keeping Y intact.
+X = torch.randn(3, 2)
+g.set_n_repr({'X': X})
 
 
 ##############################################################################
@@ -118,17 +122,18 @@ assert torch.allclose(Y_, Y[[0, 2]])
 # Setting/getting edge states is very similar.  We provide two ways of reading
 # and writing edge states: by source-destination pairs, and by edge ID.
 
-# We are going to assign each edge a state A, which is a 3-D vector for each
-# edge.
+# We are going to assign each edge a state A and a state B, both of which are
+# 3-D vectors for each edge.
 A = torch.randn(3, 3)
+B = torch.randn(3, 3)
 
 # You can either set the states of all edges...
-g.set_e_repr({'A': A})
+g.set_e_repr({'A': A, 'B': B})
 # ... or by source-destination pair (in this case, assigning A[0] to (0 -> 1)
 # and A[2] to (2 -> 0) ...
-g.set_e_repr({'A': A[[0, 2]]}, [0, 2], [1, 0])
+g.set_e_repr({'A': A[[0, 2]], 'B': B[[0, 2]]}, [0, 2], [1, 0])
 # ... or by edge ID (#0 and #2)
-g.set_e_repr_by_id({'A': A[[0, 2]]}, [0, 2])
+g.set_e_repr_by_id({'A': A[[0, 2]], 'B': B[[0, 2]]}, [0, 2])
 # Note that the latter two options are available only if you have set at least
 # one field on all edges.
 # TODO: do we want to fix this behavior to allow initial partial setting?
@@ -149,5 +154,50 @@ assert torch.allclose(e_repr_subset['A'], A[[0]])
 # One can also remove node/edge states from the graph.  This is particularly
 # useful to save memory during inference.
 
-A_ = g.pop_e_repr('A')
-assert torch.allclose(A_, A)
+B_ = g.pop_e_repr('B')
+assert torch.allclose(B_, B)
+
+
+##############################################################################
+# Section 3. Updating node/edge states
+# ------------------------------------
+# The most direct way to update node/edge states is by getting/setting the
+# states directly.  Of course, you can update the states on a subset of
+# nodes and/or edges this way.
+
+X_new = g.get_n_repr()['X'] + 2
+g.set_n_repr({'X': X_new})
+
+##############################################################################
+# A better structured implementation would wrap the update procedure as a
+# function/module, to decouple the update logic from the rest of the system.
+
+def updateX(node_state_dict):
+    return {'X': node_state_dict['X'] + 2}
+
+g.set_n_repr(updateX(g.get_n_repr()))
+
+##############################################################################
+# If your node state update function is a **node-wise map** operation (i.e.
+# the update on a single node only depends on the current state of that
+# particular node), you can also call ``apply_nodes`` method.
+#
+# .. note::
+#  In distributed computation, 
+
+g.apply_nodes(apply_node_func=updateX)
+# You can also update node states partially
+g.apply_nodes(v=[0, 1], apply_node_func=updateX)
+
+
+##############################################################################
+# For edges, DGL also has an ``apply_edges`` method for **edge-wise map**
+# operations.
+
+def updateA(edge_state_dict):
+    return {'A': edge_state_dict['A'] + 2}
+
+g.apply_edges(apply_edge_func=updateA)
+# You can also update edge states by specifying endpoints or edge IDs
+g.apply_edges(u=[0, 2], v=[1, 0], apply_edge_func=updateA)
+g.apply_edges(eid=[0, 2], apply_edge_func=updateA)
