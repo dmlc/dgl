590717cc951bbc32679d178849d9e533e7a45c8a
Minjie Wang <wmjlyjemaine@gmail.com>
2018-11-01 21:13:46 -0400

    fix utest

--- tests/pytorch/test_basics.py
+++ tests/pytorch/test_basics.py
@@ -366,29 +366,25 @@ def test_dynamic_addition():
 
     # Test node addition
     g.add_nodes(N)
-    g.set_n_repr({'h1': th.randn(N, D),
-                  'h2': th.randn(N, D)})
+    g.ndata.update({'h1': th.randn(N, D),
+                    'h2': th.randn(N, D)})
     g.add_nodes(3)
-    n_repr = g.get_n_repr()
-    assert n_repr['h1'].shape[0] == n_repr['h2'].shape[0] == N + 3
+    assert g.ndata['h1'].shape[0] == g.ndata['h2'].shape[0] == N + 3
 
     # Test edge addition
     g.add_edge(0, 1)
     g.add_edge(1, 0)
-    g.set_e_repr({'h1': th.randn(2, D),
-                  'h2': th.randn(2, D)})
-    e_repr = g.get_e_repr()
-    assert e_repr['h1'].shape[0] == e_repr['h2'].shape[0] == 2
+    g.edata.update({'h1': th.randn(2, D),
+                    'h2': th.randn(2, D)})
+    assert g.edata['h1'].shape[0] == g.edata['h2'].shape[0] == 2
 
     g.add_edges([0, 2], [2, 0])
-    e_repr = g.get_e_repr()
-    g.set_e_repr({'h1': th.randn(4, D)})
-    assert e_repr['h1'].shape[0] == e_repr['h2'].shape[0] == 4
+    g.edata['h1'] = th.randn(4, D)
+    assert g.edata['h1'].shape[0] == g.edata['h2'].shape[0] == 4
 
     g.add_edge(1, 2)
-    g.set_e_repr_by_id({'h1': th.randn(1, D)}, eid=4)
-    e_repr = g.get_e_repr()
-    assert e_repr['h1'].shape[0] == e_repr['h2'].shape[0] == 5
+    g.edges[4].data['h1'] = th.randn(1, D)
+    assert g.edata['h1'].shape[0] == g.edata['h2'].shape[0] == 5
 
 
 if __name__ == '__main__':
