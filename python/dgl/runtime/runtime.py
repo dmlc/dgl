e936ab7addd7dacba0838b53edcbbdea3205aaa7
Lingfan Yu <ly979@nyu.edu>
2018-11-09 21:33:10 +0000

    runtime

--- python/dgl/runtime/runtime.py
+++ python/dgl/runtime/runtime.py
@@ -1,1 +1,9 @@
 """DGL mini-runtime"""
+
+class Runtime(object):
+    @staticmethod
+    def run(execs):
+        new_repr = {}
+        for exe in execs:
+            new_repr.update(exe.run())
+        return new_repr
