2027b0f91d3ff3ad80543bc921480c5a7f17185e
Minjie Wang <wmjlyjemaine@gmail.com>
2018-11-11 17:11:30 -0500

    python3.6->3.5

--- docker/install/ubuntu_install_python.sh
+++ docker/install/ubuntu_install_python.sh
@@ -1,13 +1,14 @@
 # install python and pip, don't modify this, modify install_python_package.sh
-# apt-get update && apt-get install -y python-dev python-pip
+apt-get update
+apt-get install -y python-dev python3-dev
 
-# python 3.6
-apt-get update && yes | apt-get install software-properties-common
-add-apt-repository ppa:jonathonf/python-3.6
-apt-get update && apt-get install -y python3.6 python3.6-dev
-rm -f /usr/bin/python3 && ln -s /usr/bin/python3.6 /usr/bin/python3
+# install pip
+cd /tmp && wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
+python2 get-pip.py
+python3 get-pip.py
 
-# Install pip
-cd /tmp && wget https://bootstrap.pypa.io/get-pip.py
-# python2 get-pip.py
-python3.6 get-pip.py
+# santiy check
+python2 --version
+python3 --version
+pip2 --version
+pip3 --version
