ac30ec1286d3698438ce8a098baa7f6028799a69
Minjie Wang <wmjlyjemaine@gmail.com>
2018-11-12 00:41:03 -0500

    add matplotlib in image

--- docker/install/ubuntu_install_python_package.sh
+++ docker/install/ubuntu_install_python_package.sh
@@ -1,6 +1,6 @@
 # install libraries for python package on ubuntu
-pip2 install nose numpy cython scipy networkx nltk requests[security]
-pip3 install nose numpy cython scipy networkx nltk requests[security]
+pip2 install nose numpy cython scipy networkx matplotlib nltk requests[security]
+pip3 install nose numpy cython scipy networkx matplotlib nltk requests[security]
 
 # install DL Framework
 pip2 install torch torchvision
