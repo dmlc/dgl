aaf91dd6f2a55e577ebb489b29451fa541cdd476
Minjie Wang <wmjlyjemaine@gmail.com>
2018-11-12 15:04:45 -0500

    add graphviz

--- docker/install/ubuntu_install_core.sh
+++ docker/install/ubuntu_install_core.sh
@@ -2,4 +2,4 @@
 apt update && apt install -y --no-install-recommends --force-yes \
         apt-utils git build-essential make cmake wget unzip sudo \
         libz-dev libxml2-dev libopenblas-dev libopencv-dev \
-        libgraphviz-dev ca-certificates
+        graphviz graphviz-dev libgraphviz-dev ca-certificates
