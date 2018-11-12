a95459e3a272d8d5a6bfd5a1991941b1f8582f95
Minjie Wang <minjie.wang@nyu.edu>
2018-11-12 01:06:05 -0500

    [CI] Improved CI (#141)
    * change ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * update ci
    
    * nx package
    
    * update ci
    
    * update ci
    
    * update ci
    
    * fix
    
    * mx dockerfile by zhengda
    
    * python3.6->3.5
    
    * update ci image
    
    * add tutorial test
    
    * fix ci
    
    * fix ssl problem
    
    * minor change
    
    * small fix on traversal utest
    
    * fix syntax
    
    * add matplotlib in image
    
    * fix
    
    * update ci
    
    * update ci

--- tests/pytorch/test_traversal.py
+++ tests/pytorch/test_traversal.py
@@ -16,37 +16,42 @@ np.random.seed(42)
 def toset(x):
     return set(x.tolist())
 
-def test_bfs_nodes(n=1000):
-    g_nx = nx.random_tree(n)
+def test_bfs(n=1000):
+    def _bfs_nx(g_nx, src):
+        edges = nx.bfs_edges(g_nx, src)
+        layers_nx = [set([src])]
+        edges_nx = []
+        frontier = set()
+        edge_frontier = set()
+        for u, v in edges:
+            if u in layers_nx[-1]:
+                frontier.add(v)
+                edge_frontier.add(g.edge_id(u, v))
+            else:
+                layers_nx.append(frontier)
+                edges_nx.append(edge_frontier)
+                frontier = set([v])
+                edge_frontier = set([g.edge_id(u, v)])
+        layers_nx.append(frontier)
+        edges_nx.append(edge_frontier)
+        return layers_nx, edges_nx
+
     g = dgl.DGLGraph()
-    g.from_networkx(g_nx)
-
+    a = sp.random(n, n, 10 / n, data_rvs=lambda n: np.ones(n))
+    g.from_scipy_sparse_matrix(a)
+    g_nx = g.to_networkx()
     src = random.choice(range(n))
-
-    edges = nx.bfs_edges(g_nx, src)
-    layers_nx = [set([src])]
-    edges_nx = []
-    frontier = set()
-    edge_frontier = set()
-    for u, v in edges:
-        if u in layers_nx[-1]:
-            frontier.add(v)
-            edge_frontier.add(g.edge_id(u, v))
-        else:
-            layers_nx.append(frontier)
-            edges_nx.append(edge_frontier)
-            frontier = set([v])
-            edge_frontier = set([g.edge_id(u, v)])
-    layers_nx.append(frontier)
-    edges_nx.append(edge_frontier)
-
+    layers_nx, _ = _bfs_nx(g_nx, src)
     layers_dgl = dgl.bfs_nodes_generator(g, src)
-
     assert len(layers_dgl) == len(layers_nx)
     assert all(toset(x) == y for x, y in zip(layers_dgl, layers_nx))
 
+    g_nx = nx.random_tree(n, seed=42)
+    g = dgl.DGLGraph()
+    g.from_networkx(g_nx)
+    src = 0
+    _, edges_nx = _bfs_nx(g_nx, src)
     edges_dgl = dgl.bfs_edges_generator(g, src)
-
     assert len(edges_dgl) == len(edges_nx)
     assert all(toset(x) == y for x, y in zip(edges_dgl, edges_nx))
 
@@ -114,6 +119,6 @@ def test_dfs_labeled_edges(n=1000, example=False):
 
 
 if __name__ == '__main__':
-    test_bfs_nodes()
+    test_bfs()
     test_topological_nodes()
     test_dfs_labeled_edges()
