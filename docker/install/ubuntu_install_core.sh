f8cf3d968ead9f4e98a19bfe26881a1674a08040
Minjie Wang <wmjlyjemaine@gmail.com>
2018-11-12 12:35:02 -0500

    add graphviz

--- docker/install/ubuntu_install_core.sh
+++ docker/install/ubuntu_install_core.sh
@@ -1,4 +1,5 @@
 # install libraries for building c++ core on ubuntu
 apt update && apt install -y --no-install-recommends --force-yes \
         apt-utils git build-essential make cmake wget unzip sudo \
-        libz-dev libxml2-dev libopenblas-dev libopencv-dev ca-certificates
+        libz-dev libxml2-dev libopenblas-dev libopencv-dev \
+        libgraphviz-dev ca-certificates
