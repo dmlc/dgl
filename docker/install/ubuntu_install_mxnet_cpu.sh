c36819b1c5ab1392f4743dab14a00924aa4dda8c
Da Zheng <zhengda1936@gmail.com>
2018-11-13 03:18:26 +0000

    fix.

--- docker/install/ubuntu_install_mxnet_cpu.sh
+++ docker/install/ubuntu_install_mxnet_cpu.sh
@@ -1,4 +1,4 @@
 git clone --recursive -b dgl_graph https://github.com/zheng-da/incubator-mxnet.git
 cd incubator-mxnet
-make -j4 USE_OPENCV=1 USE_BLAS=openblas USE_MKLDNN=1 USE_DIST_KVSTORE=1
+make -j $(nproc) USE_OPENCV=1 USE_BLAS=openblas USE_MKLDNN=1 USE_DIST_KVSTORE=1
 pip3 install -e python
