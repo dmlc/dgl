b1eeb934494ef1eee20cf2d35b718790cb9cb550
Minjie Wang <minjie.wang@nyu.edu>
2018-11-25 22:33:08 -0500

    [Bugfix][Runtime] Zero degree behaviors (#177)
    * fix recv nodes are all 0deg; fix hybriddict does not through keyerror properly
    
    * fallback to apply_nodes when all nodes are 0deg; WIP on pull spmv 0deg
    
    * new 0deg behavior
    
    * new 0deg behavior
    
    * update mx utest for pull-0deg
    
    * fix mx
    
    * fix mx
    
    * get rid of unnecessary sort-n-unique

--- tests/pytorch/test_specialization.py
+++ tests/pytorch/test_specialization.py
@@ -110,6 +110,52 @@ def test_v2v_snr():
     # test 2d node features
     _test('f2')
 
+
+def test_v2v_pull():
+    nodes = th.tensor([1, 2, 3, 9])
+    def _test(fld):
+        def message_func(edges):
+            return {'m' : edges.src[fld]}
+
+        def message_func_edge(edges):
+            if len(edges.src[fld].shape) == 1:
+                return {'m' : edges.src[fld] * edges.data['e1']}
+            else:
+                return {'m' : edges.src[fld] * edges.data['e2']}
+
+        def reduce_func(nodes):
+            return {fld : th.sum(nodes.mailbox['m'], 1)}
+
+        def apply_func(nodes):
+            return {fld : 2 * nodes.data[fld]}
+        g = generate_graph()
+        # send and recv
+        v1 = g.ndata[fld]
+        g.pull(nodes, fn.copy_src(src=fld, out='m'), fn.sum(msg='m', out=fld), apply_func)
+        v2 = g.ndata[fld]
+        g.ndata[fld] = v1
+        g.pull(nodes, message_func, reduce_func, apply_func)
+        v3 = g.ndata[fld]
+        assert U.allclose(v2, v3)
+        # send and recv with edge weights
+        v1 = g.ndata[fld]
+        g.pull(nodes, fn.src_mul_edge(src=fld, edge='e1', out='m'),
+               fn.sum(msg='m', out=fld), apply_func)
+        v2 = g.ndata[fld]
+        g.ndata[fld] = v1
+        g.pull(nodes, fn.src_mul_edge(src=fld, edge='e2', out='m'),
+               fn.sum(msg='m', out=fld), apply_func)
+        v3 = g.ndata[fld]
+        g.ndata[fld] = v1
+        g.pull(nodes, message_func_edge, reduce_func, apply_func)
+        v4 = g.ndata[fld]
+        assert U.allclose(v2, v3)
+        assert U.allclose(v3, v4)
+    # test 1d node features
+    _test('f1')
+    # test 2d node features
+    _test('f2')
+
 def test_v2v_update_all_multi_fn():
     def message_func(edges):
         return {'m2': edges.src['f2']}
@@ -311,7 +357,7 @@ def test_e2v_recv_multi_fn():
     # test 2d node features
     _test('f2')
 
-def test_multi_fn_fallback():
+def test_update_all_multi_fallback():
     # create a graph with zero in degree nodes
     g = dgl.DGLGraph()
     g.add_nodes(10)
@@ -383,12 +429,98 @@ def test_multi_fn_fallback():
     assert U.allclose(o2, g.ndata.pop('o2'))
     assert U.allclose(o3, g.ndata.pop('o3'))
 
+
+def test_pull_multi_fallback():
+    # create a graph with zero in degree nodes
+    g = dgl.DGLGraph()
+    g.add_nodes(10)
+    for i in range(1, 9):
+        g.add_edge(0, i)
+        g.add_edge(i, 9)
+    g.ndata['h'] = th.randn(10, D)
+    g.edata['w1'] = th.randn(16,)
+    g.edata['w2'] = th.randn(16, D)
+    def _mfunc_hxw1(edges):
+        return {'m1' : edges.src['h'] * th.unsqueeze(edges.data['w1'], 1)}
+    def _mfunc_hxw2(edges):
+        return {'m2' : edges.src['h'] * edges.data['w2']}
+    def _rfunc_m1(nodes):
+        return {'o1' : th.sum(nodes.mailbox['m1'], 1)}
+    def _rfunc_m2(nodes):
+        return {'o2' : th.sum(nodes.mailbox['m2'], 1)}
+    def _rfunc_m1max(nodes):
+        return {'o3' : th.max(nodes.mailbox['m1'], 1)[0]}
+    def _afunc(nodes):
+        ret = {}
+        for k, v in nodes.data.items():
+            if k.startswith('o'):
+                ret[k] = 2 * v
+        return ret
+    # nodes to pull
+    def _pull_nodes(nodes):
+        # compute ground truth
+        g.pull(nodes, _mfunc_hxw1, _rfunc_m1, _afunc)
+        o1 = g.ndata.pop('o1')
+        g.pull(nodes, _mfunc_hxw2, _rfunc_m2, _afunc)
+        o2 = g.ndata.pop('o2')
+        g.pull(nodes, _mfunc_hxw1, _rfunc_m1max, _afunc)
+        o3 = g.ndata.pop('o3')
+        # v2v spmv
+        g.pull(nodes, fn.src_mul_edge(src='h', edge='w1', out='m1'),
+                     fn.sum(msg='m1', out='o1'),
+                     _afunc)
+        assert U.allclose(o1, g.ndata.pop('o1'))
+        # v2v fallback to e2v
+        g.pull(nodes, fn.src_mul_edge(src='h', edge='w2', out='m2'),
+                     fn.sum(msg='m2', out='o2'),
+                     _afunc)
+        assert U.allclose(o2, g.ndata.pop('o2'))
+        # v2v fallback to degree bucketing
+        g.pull(nodes, fn.src_mul_edge(src='h', edge='w1', out='m1'),
+                     fn.max(msg='m1', out='o3'),
+                     _afunc)
+        assert U.allclose(o3, g.ndata.pop('o3'))
+        # multi builtins, both v2v spmv
+        g.pull(nodes,
+               [fn.src_mul_edge(src='h', edge='w1', out='m1'), fn.src_mul_edge(src='h', edge='w1', out='m2')],
+               [fn.sum(msg='m1', out='o1'), fn.sum(msg='m2', out='o2')],
+               _afunc)
+        assert U.allclose(o1, g.ndata.pop('o1'))
+        assert U.allclose(o1, g.ndata.pop('o2'))
+        # multi builtins, one v2v spmv, one fallback to e2v
+        g.pull(nodes,
+               [fn.src_mul_edge(src='h', edge='w1', out='m1'), fn.src_mul_edge(src='h', edge='w2', out='m2')],
+               [fn.sum(msg='m1', out='o1'), fn.sum(msg='m2', out='o2')],
+               _afunc)
+        assert U.allclose(o1, g.ndata.pop('o1'))
+        assert U.allclose(o2, g.ndata.pop('o2'))
+        # multi builtins, one v2v spmv, one fallback to e2v, one fallback to degree-bucketing
+        g.pull(nodes,
+               [fn.src_mul_edge(src='h', edge='w1', out='m1'),
+                fn.src_mul_edge(src='h', edge='w2', out='m2'),
+                fn.src_mul_edge(src='h', edge='w1', out='m3')],
+               [fn.sum(msg='m1', out='o1'),
+                fn.sum(msg='m2', out='o2'),
+                fn.max(msg='m3', out='o3')],
+               _afunc)
+        assert U.allclose(o1, g.ndata.pop('o1'))
+        assert U.allclose(o2, g.ndata.pop('o2'))
+        assert U.allclose(o3, g.ndata.pop('o3'))
+    # test#1: non-0deg nodes
+    nodes = [1, 2, 9]
+    _pull_nodes(nodes)
+    # test#2: 0deg nodes + non-0deg nodes
+    nodes = [0, 1, 2, 9]
+    _pull_nodes(nodes)
+
 if __name__ == '__main__':
     test_v2v_update_all()
     test_v2v_snr()
+    test_v2v_pull()
     test_v2v_update_all_multi_fn()
     test_v2v_snr_multi_fn()
     test_e2v_update_all_multi_fn()
     test_e2v_snr_multi_fn()
     test_e2v_recv_multi_fn()
-    test_multi_fn_fallback()
+    test_update_all_multi_fallback()
+    test_pull_multi_fallback()
