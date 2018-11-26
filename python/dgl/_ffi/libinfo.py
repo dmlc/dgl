7af64e247c2c7100f8136a132f1c4068245130c5
Minjie Wang <wmjlyjemaine@gmail.com>
2018-11-26 16:55:38 -0500

    add bdist_wheel build

--- python/dgl/_ffi/libinfo.py
+++ python/dgl/_ffi/libinfo.py
@@ -87,4 +87,4 @@ def find_lib_path(name=None, search_path=None, optional=False):
 # We use the version of the incoming release for code
 # that is under development.
 # The following line is set by tvm/python/update_version.py
-__version__ = "0.5.dev"
+__version__ = "0.0.1"
