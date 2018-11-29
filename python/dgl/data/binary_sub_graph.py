efc72ece0f02f15483281b6648c5f64414a6965d
HQ01 <qh384@nyu.edu>
2018-11-30 02:25:18 +0800

    [model]line graph new implementation + tutorial + binary sub graph dataset

--- python/dgl/data/binary_sub_graph.py
+++ python/dgl/data/binary_sub_graph.py
@@ -6,6 +6,7 @@ from ..utils import Index
 import numpy as np
 import networkx as nx
 import itertools
+import scipy as sp
 
 class CORABinary:
     """
@@ -28,6 +29,8 @@ class CORABinary:
     
     def __init__(self, g, features, labels, num_classes):
         self._g = g
+        
+        # DEBUGs
         self._features = features
         self._labels = labels
         self._n_classes = num_classes
@@ -59,17 +62,49 @@ class CORABinary:
         
         self._line_graphs = [g.line_graph(backtracking=False) 
                              for g in self._subgraphs]
+        
+        
+        
         in_degrees = lambda g: g.in_degrees(
                              Index(np.arange(0, g.number_of_nodes()))).unsqueeze(1).float()
         
         self._g_degs = [in_degrees(g) for g in self._subgraphs]
         self._lg_degs = [in_degrees(lg) for lg in self._line_graphs]
-        self._pm_pds = list(zip(*[g.edges() for g in self._subgraphs]))[0]
+        #self._pm_pds = list(zip(*[g.edges() for g in self._subgraphs]))[0]
+        
+        assert (self._subgraphs[0].number_of_edges()%2 == 0), "invalid subgraph construction"
+        
+        ## New try
+        self._new_pmpds = []
+        for i, graph in enumerate(self._subgraphs):
+            self._new_pmpds.append(self.gen_pmpd(graph))
         
         self._equi_labels = []
         for label in self._sublabels:
-            mirror_label = np.ones(label.shape) - label
+            mirror_label = (np.ones(label.shape) - label).astype(int)
             self._equi_labels.append(mirror_label)
+    
+    def gen_pmpd(self, graph):
+        row_idx = []
+        col_idx = []
+        value = []
+        src = np.array(graph.edges()[0])
+        dst = np.array(graph.edges()[1])        
+        for i, node in enumerate(src):
+            col_idx.append(i)
+            col_idx.append(i)
+            row_idx.append(node)
+            row_idx.append(dst[i])
+            if i <= int(graph.number_of_edges()/2):
+                value.append(1)
+                value.append(1)
+            else:
+                value.append(-1)
+                value.append(1)
+        pm_pd = sp.sparse.coo_matrix((value, (row_idx, col_idx)), shape=(graph.number_of_nodes(), graph.number_of_edges()))
+                
+        return pm_pd
+        
     
     def binary_subgraph_cora(self,
                              original_graph, 
@@ -100,8 +135,14 @@ class CORABinary:
         for i, node in enumerate(largest_subgraph):
             old2newindex[node] = i
             new2oldindex[i] = node
-        for src,dst in sub.edges(largest_subgraph):
+        # now undirected subgraph is crucial to ensure the
+        # desired order of edges
+        for src,dst in sub_und.edges(largest_subgraph):
             reindex_edges.append((old2newindex[src], old2newindex[dst]))
+        #print("append new...")
+        for src,dst in sub_und.edges(largest_subgraph):
+            reindex_edges.append((old2newindex[dst], old2newindex[src]))
+        
 
         #use old2newindex to manually reindex the subgraph
         reindex_graph = nx.DiGraph()
@@ -116,26 +157,22 @@ class CORABinary:
         return len(self._subgraphs)
     
     def __getitem__(self, idx):
-        return self._subgraphs[idx], self._line_graphs[idx], self._g_degs[idx], self._lg_degs[idx], self._pm_pds[idx], self._subfeatures[idx], self._sublabels[idx], self._equi_labels[idx]
+        return self._subgraphs[idx], self._line_graphs[idx], self._g_degs[idx], self._lg_degs[idx], self._new_pmpds[idx], self._subfeatures[idx], self._sublabels[idx], self._equi_labels[idx]
     
     def collate_fn(self, x):
-        subgraph, line_graph, deg_g, deg_lg, pm_pd, subfeature, sublabel, equi_label = zip(*x)
+        subgraph, line_graph, deg_g, deg_lg, new_pmpd, subfeature, sublabel, equi_label = zip(*x)
         subgraph_batch = batch(subgraph)
         line_graph_batch = batch(line_graph)
         deg_g_batch = np.concatenate(deg_g, axis=0)
         deg_lg_batch = np.concatenate(deg_lg, axis=0)
         
         self.total = 0
-        def offset(pm_pd):
-            prev_total = self.total
-            self.total += pm_pd.size(0)
-            return prev_total
-        pm_pd_batch = np.concatenate([x + offset(x) for i, x in enumerate(pm_pd)], 
-                             axis=0)
-        
+
         subfeature_batch = np.concatenate(subfeature, axis=0)
         sublabel_batch = np.concatenate(sublabel, axis=0)
         equilabel_batch = np.concatenate(equi_label, axis=0)
         
-        return subgraph_batch, line_graph_batch, deg_g_batch, deg_lg_batch, pm_pd_batch,subfeature_batch, sublabel_batch, equilabel_batch    
+        new_pmpd_batch = sp.sparse.block_diag(new_pmpd)
+        
+        return subgraph_batch, line_graph_batch, deg_g_batch, deg_lg_batch, new_pmpd_batch, subfeature_batch, sublabel_batch, equilabel_batch    
 
--- tutorials/utils.py
+++ tutorials/utils.py
@@ -3,33 +3,41 @@ import dgl
 import torch as th
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+from scipy import sparse
+from dgl import DGLGraph
 
 
 
 def graph_viz(labels,subgraph):
     value = [labels[node] for node in subgraph.nodes()]
     pos = nx.spring_layout(subgraph, random_state=1)
+    plt.figure(figsize=(10,10))
+    plt.axis('off')
     nx.draw_networkx(subgraph, 
-                     pos=pos, 
+                     pos=pos,
+                     k=1/800,
                      edge_color='k', 
-                     node_size=5, 
-                     cmap=plt.get_cmap('Set2'), 
+                     node_size=50, 
+                     cmap=plt.get_cmap('coolwarm'), 
                      node_color=value,
                      arrows=False,
-                     width=0.6,
+                     width=0.05,
+                     style='dotted',
                      with_labels=False)
 
 def linegraph_accuracy(z_list, labels):
     accu = []
     ybar_list = [th.max(z, 1)[1] for z in z_list]
     for y_bar in ybar_list:
-        accuracy = max(th.sum(y_bar == label).item() for label in labels) / len(labels[0])
+        accuracy = max(th.sum(y_bar == th.LongTensor(label)).item() for label in labels) / len(labels[0])
         accu.append(accuracy)
     return sum(accu) / len(accu)
 
 
 def linegraph_inference(g, lg, deg_g, deg_lg, pm_pd, feature, label, equi_label, model):
-    z = model(g, lg, deg_g, deg_lg, pm_pd, feature)
+    z = model(g, lg, deg_g, deg_lg, pm_pd)
     
     z_list = [z]
     
@@ -40,28 +48,82 @@ def linegraph_inference(g, lg, deg_g, deg_lg, pm_pd, feature, label, equi_label,
     return accu
 
 def linegraph_inference_viz(g, lg, deg_g, deg_lg, pm_pd, feature, model):
-    z = model(g, lg, deg_g, deg_lg, pm_pd, feature)
+    z = model(g, lg, deg_g, deg_lg, pm_pd)
     
     z_list = [z]
     
-    n_batchsize = 1
     ybar_list = [th.max(z, 1)[1] for z in z_list]
     graph_viz(ybar_list[0],g.to_networkx())
 
-def classify_animate(i):
-    ax.cla()
-    ax.axis('off')
-    ax.set_title("community detection result @ epoch %d" % i)
-    value = [validation_example_label_change[i][node] for node in g_nx.nodes()]
-    nx.draw_networkx(g_nx,
-                     pos=pos,
-                     edge_color='k',
-                     node_size=7,
-                     cmap=plt.get_cmap('Set2'),
-                     node_color=value,
-                     arrows=False,
-                     width=0.6,
-                     with_labels=False) 
-
-
+def sparse2th(mat):
+    value = mat.data
+    indices = th.LongTensor([mat.row, mat.col])
     
+    tensor = th.sparse.FloatTensor(indices, th.from_numpy(value).float(), th.Size(mat.shape))
+    
+    return tensor
+
+def matrixDebugger(mat, verbose=True):
+    print("type is", type(mat))
+    print("shape is", mat.shape)
+    if verbose:
+        print("value is", mat)
+
+def from_npsp(f, *args):
+    def wrap(*args):
+        new = [th.from_numpy(x) if isinstance(x, np.ndarray) else x for x in args]
+        new = [sparse2th(x) if isinstance(x, sparse.coo.coo_matrix) else x for x in new]
+        return f(*new)
+    return wrap
+
+def check_intra_prob(g, labels, num_classes):
+    g_nx = DGLGraph(g).to_networkx()
+    adj = nx.adjacency_matrix(g_nx).todense()
+    class_to_intra_prob = {i: 0 for i in range(num_classes)}
+    class_to_total = {i: 0 for i in range(num_classes)}
+    class_to_intra_sum = {i: 0 for i in range(num_classes)}
+    for i in range(g_nx.number_of_nodes()):
+        _, index = np.where(adj[i] !=0)
+        each_total = len(index)
+        intra_num = 0
+        for j in index:
+            if labels[i] == labels[j]:
+                intra_num += 1
+        class_to_total[int(labels[i])] += each_total
+        class_to_intra_sum[int(labels[i])] += intra_num
+    for i in range(num_classes):
+        class_to_intra_prob[i] = class_to_intra_sum[i] / class_to_total[i]
+
+
+
+
+    for cat in class_to_intra_prob:
+        print("Class {} : intra connection probability {}".format(cat, class_to_intra_prob[cat]))
+
+def animate(g, label_history):
+    fig2 = plt.figure(figsize=(8,8), dpi=150)
+    fig2.clf()
+    ax = fig2.subplots()
+    g_nx = g.to_networkx()
+    pos = nx.spring_layout(g_nx, random_state=1)
+
+    def classify_animate(i):
+        ax.cla()
+        ax.axis('off')
+        ax.set_title("community detection result @ epoch %d" % i)
+        value = [label_history[i][node] for node in g_nx.nodes()]
+        nx.draw_networkx(g_nx,
+                         pos=pos,
+                         k=1/800,
+                         edge_color='k',
+                         node_size=50,
+                         cmap=plt.get_cmap('coolwarm'),
+                         node_color=value,
+                         arrows=False,
+                         width=0.05,
+                         style='dotted',
+                         with_labels=False) 
+
+    ani = animation.FuncAnimation(fig2, classify_animate, frames=len(label_history), interval=500)
+    ani.save('./animationfromutils.gif', writer='imagemagick')
+    plt.show()
