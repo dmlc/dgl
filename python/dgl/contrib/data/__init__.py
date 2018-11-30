a0d7b302da5d44fcbfd67a6a4c629fdda5d71401
Lingfan Yu <ly979@nyu.edu>
2018-11-30 02:59:54 -0500

    data processing for rgcn

---
+++ python/dgl/contrib/data/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+from . import knowledge_graph as knwlgrh
+def load_data(dataset, bfs_level=3, relabel=False):
+    if dataset in ['aifb', 'mutag', 'bgs', 'am']:
+        return knwlgrh.load_entity(dataset, bfs_level, relabel)
+    elif dataset in ['FB15k', 'wn18', 'FB15k-237']:
+        return knwlgrh.load_link(dataset)
+    else:
+        raise ValueError('Unknown dataset: {}'.format(dataset))
