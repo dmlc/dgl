b98dc92c59911cf72e27f87df05c74ef1894bc43
Lingfan Yu <ly979@nyu.edu>
2018-12-03 13:01:27 -0500

    [Model] Relational GCN (#55)
    * data preprocessing for rgcn
    
    * edge subgraph
    
    * WIP: RGCN
    
    * use edge feature in spmv
    
    * fix bugs
    
    * match AIFB accuracy
    
    * match mutag accuracy
    
    * avoid materializing in featureless case
    
    * remove untouched nodes and relabel nodes
    
    * fix python list concatenate overhead
    
    * sparsely store edge types
    
    * refactor entity classify code for clean link prediction implementation
    
    * further refactor code
    
    * refactoring
    
    * rgcn block decompose layers
    
    * link predict dataset
    
    * link predict model and eval code
    
    * dropout, self-loop, regularization, etc, plus bug fixes
    
    * update to new api
    
    * dataset update
    
    * bugs, WIP, need to impl early stopping and filtered metrics
    
    * instruction to run, and minor
    
    * group conv and early stop
    
    * clean slow code
    
    * some code comments
    
    * use new api in model code
    
    * change data preprocessing
    
    * entity classify model
    
    * WIP
    
    * move dgl graph out of model
    
    * hot fix for extract zip
    
    * fix link predict model
    
    * use latest dgl apis
    
    * still have memory issue...
    
    * bug fix and move inference to cpu
    
    * move rgcn data processing to contrib
    
    * th.allclose -> U.allclose
    
    * minor change in readme
    
    * fix memory issue in entity classify
    
    * fix and testing code for link predict
    
    * fix entity classify
    
    * clean up
    
    * fix comments
    
    * revert erroneous git merge changes
    
    * code clean up and more comments
    
    * minor
    
    * dependent package version

---
+++ examples/pytorch/rgcn/entity_classify.py
@@ -0,0 +1,163 @@
+"""
+Modeling Relational Data with Graph Convolutional Networks
+Paper: https://arxiv.org/abs/1703.06103
+Code: https://github.com/tkipf/relational-gcn
+
+Difference compared to tkipf/relation-gcn
+* l2norm applied to all weights
+* remove nodes that won't be touched
+"""
+
+import argparse
+import numpy as np
+import time
+import torch
+import torch.nn.functional as F
+from dgl import DGLGraph
+from dgl.contrib.data import load_data
+import dgl.function as fn
+from functools import partial
+
+from layers import RGCNBasisLayer as RGCNLayer
+from model import BaseRGCN
+
+class EntityClassify(BaseRGCN):
+    def create_features(self):
+        features = torch.arange(self.num_nodes)
+        if self.use_cuda:
+            features = features.cuda()
+        return features
+
+    def build_input_layer(self):
+        return RGCNLayer(self.num_nodes, self.h_dim, self.num_rels, self.num_bases,
+                         activation=F.relu, is_input_layer=True)
+
+    def build_hidden_layer(self, idx):
+        return RGCNLayer(self.h_dim, self.h_dim, self.num_rels, self.num_bases,
+                         activation=F.relu)
+
+    def build_output_layer(self):
+        return RGCNLayer(self.h_dim, self.out_dim, self.num_rels,self.num_bases,
+                         activation=partial(F.softmax, dim=1))
+
+def main(args):
+    # load graph data
+    data = load_data(args.dataset, bfs_level=args.bfs_level, relabel=args.relabel)
+    num_nodes = data.num_nodes
+    num_rels = data.num_rels
+    num_classes = data.num_classes
+    labels = data.labels
+    train_idx = data.train_idx
+    test_idx = data.test_idx
+
+    # split dataset into train, validate, test
+    if args.validation:
+        val_idx = train_idx[:len(train_idx) // 5]
+        train_idx = train_idx[len(train_idx) // 5:]
+    else:
+        val_idx = train_idx
+
+    # edge type and normalization factor
+    edge_type = torch.from_numpy(data.edge_type)
+    edge_norm = torch.from_numpy(data.edge_norm).unsqueeze(1)
+    labels = torch.from_numpy(labels).view(-1)
+
+    # check cuda
+    use_cuda = args.gpu >= 0 and torch.cuda.is_available()
+    if use_cuda:
+        torch.cuda.set_device(args.gpu)
+        edge_type = edge_type.cuda()
+        edge_norm = edge_norm.cuda()
+        labels = labels.cuda()
+
+    # create graph
+    g = DGLGraph()
+    g.add_nodes(num_nodes)
+    g.add_edges(data.edge_src, data.edge_dst)
+    g.edata.update({'type': edge_type, 'norm': edge_norm})
+
+    # create model
+    model = EntityClassify(len(g),
+                           args.n_hidden,
+                           num_classes,
+                           num_rels,
+                           num_bases=args.n_bases,
+                           num_hidden_layers=args.n_layers - 2,
+                           dropout=args.dropout,
+                           use_cuda=use_cuda)
+
+    if use_cuda:
+        model.cuda()
+
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.l2norm)
+
+    # training loop
+    print("start training...")
+    forward_time = []
+    backward_time = []
+    model.train()
+    for epoch in range(args.n_epochs):
+        optimizer.zero_grad()
+        t0 = time.time()
+        logits = model.forward(g)
+        loss = F.cross_entropy(logits[train_idx], labels[train_idx])
+        t1 = time.time()
+        loss.backward()
+        optimizer.step()
+        t2 = time.time()
+
+        forward_time.append(t1 - t0)
+        backward_time.append(t2 - t1)
+        print("Epoch {:05d} | Train Forward Time(s) {:.4f} | Backward Time(s) {:.4f}".
+              format(epoch, forward_time[-1], backward_time[-1]))
+        train_acc = torch.sum(logits[train_idx].argmax(dim=1) == labels[train_idx]).item() / len(train_idx)
+        val_loss = F.cross_entropy(logits[val_idx], labels[val_idx])
+        val_acc = torch.sum(logits[val_idx].argmax(dim=1) == labels[val_idx]).item() / len(val_idx)
+        print("Train Accuracy: {:.4f} | Train Loss: {:.4f} | Validation Accuracy: {:.4f} | Validation loss: {:.4f}".
+              format(train_acc, loss.item(), val_acc, val_loss.item()))
+    print()
+
+    model.eval()
+    logits = model.forward(g)
+    test_loss = F.cross_entropy(logits[test_idx], labels[test_idx])
+    test_acc = torch.sum(logits[test_idx].argmax(dim=1) == labels[test_idx]).item() / len(test_idx)
+    print("Test Accuracy: {:.4f} | Test loss: {:.4f}".format(test_acc, test_loss.item()))
+    print()
+
+    print("Mean forward time: {:4f}".format(np.mean(forward_time[len(forward_time) // 4:])))
+    print("Mean backward time: {:4f}".format(np.mean(backward_time[len(backward_time) // 4:])))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='RGCN')
+    parser.add_argument("--dropout", type=float, default=0,
+            help="dropout probability")
+    parser.add_argument("--n-hidden", type=int, default=16,
+            help="number of hidden units")
+    parser.add_argument("--gpu", type=int, default=-1,
+            help="gpu")
+    parser.add_argument("--lr", type=float, default=1e-2,
+            help="learning rate")
+    parser.add_argument("--n-bases", type=int, default=-1,
+            help="number of filter weight matrices, default: -1 [use all]")
+    parser.add_argument("--n-layers", type=int, default=2,
+            help="number of propagation rounds")
+    parser.add_argument("-e", "--n-epochs", type=int, default=50,
+            help="number of training epochs")
+    parser.add_argument("-d", "--dataset", type=str, required=True,
+            help="dataset to use")
+    parser.add_argument("--l2norm", type=float, default=0,
+            help="l2 norm coef")
+    parser.add_argument("--relabel", default=False, action='store_true',
+            help="remove untouched nodes and relabel")
+    fp = parser.add_mutually_exclusive_group(required=False)
+    fp.add_argument('--validation', dest='validation', action='store_true')
+    fp.add_argument('--testing', dest='validation', action='store_false')
+    parser.set_defaults(validation=True)
+
+    args = parser.parse_args()
+    print(args)
+    args.bfs_level = args.n_layers + 1 # pruning used nodes for memory
+    main(args)
+
