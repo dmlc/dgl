d147841159667417045a5407c212fda741e43a81
VoVAllen <jz1749@nyu.edu>
2018-12-06 01:42:44 +0800

    fix ci

--- tests/scripts/task_pytorch_tutorial_test.sh
+++ tests/scripts/task_pytorch_tutorial_test.sh
@@ -14,7 +14,7 @@ pip3 install -r requirements.txt || fail "installing requirements"
 
 # Test
 export MPLBACKEND=Agg
-for f in $(find . -name "*.py")
+for f in $(find . -name "*.py" ! -name "*_mx.py")
 do
     echo "Running tutorial ${f} ..."
     python3 $f || fail "run ${f}"
