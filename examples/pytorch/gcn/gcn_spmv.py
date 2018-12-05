cf53ef7c4b37c44a8a9e0ed19a7e4edaa331f519
Lingfan Yu <ly979@nyu.edu>
2018-12-05 04:53:01 +0000

    WIP

--- examples/pytorch/gcn/gcn_spmv.py
+++ examples/pytorch/gcn/gcn_spmv.py
@@ -18,12 +18,13 @@ from dgl.data import register_data_args, load_data
 class NodeApplyModule(nn.Module):
     def __init__(self, in_feats, out_feats, activation=None):
         super(NodeApplyModule, self).__init__()
-
-        self.linear = nn.Linear(in_feats, out_feats)
+        self.linear = nn.Linear(in_feats, out_feats, bias=False)
+        nn.init.xavier_uniform_(self.linear.weight,
+                               gain=nn.init.calculate_gain('relu'))
         self.activation = activation
 
     def forward(self, nodes):
-        h = self.linear(nodes.data['h'])
+        h = self.linear(nodes.data['h'] * nodes.data['norm'])
         if self.activation:
             h = self.activation(h)
 
@@ -46,8 +47,10 @@ class GCN(nn.Module):
         else:
             self.dropout = 0.
 
+        self.layers = nn.ModuleList()
+
         # input layer
-        self.layers = nn.ModuleList([NodeApplyModule(in_feats, n_hidden, activation)])
+        self.layers.append(NodeApplyModule(in_feats, n_hidden, activation))
 
         # hidden layers
         for i in range(n_layers - 1):
@@ -59,15 +62,27 @@ class GCN(nn.Module):
     def forward(self, features):
         self.g.ndata['h'] = features
 
-        for layer in self.layers:
+        for idx, layer in enumerate(self.layers):
             # apply dropout
-            if self.dropout:
-                self.g.apply_nodes(apply_node_func=
-                               lambda nodes: {'h': self.dropout(nodes.data['h'])})
+            if idx > 0 and self.dropout:
+                self.g.apply_nodes(lambda nodes:
+                                        {'h': self.dropout(nodes.data['h'])})
+            self.g.apply_nodes(lambda nodes:
+                                {'h': nodes.data['h'] * nodes.data['norm']})
             self.g.update_all(fn.copy_src(src='h', out='m'),
                               fn.sum(msg='m', out='h'),
                               layer)
         return self.g.pop_n_repr('h')
+
+def evaluate(model, features, labels, mask):
+    model.eval()
+    with torch.no_grad():
+        logits = model(features)
+        logits = logits[mask]
+        labels = labels[mask]
+        _, indices = torch.max(logits, dim=1)
+        correct = torch.sum(indices == labels)
+        return correct.item() * 1.0 / len(labels)
 
 def main(args):
     # load and preprocess dataset
@@ -76,7 +91,9 @@ def main(args):
 
     features = torch.FloatTensor(data.features)
     labels = torch.LongTensor(data.labels)
-    mask = torch.ByteTensor(data.train_mask)
+    train_mask = torch.ByteTensor(data.train_mask)
+    val_mask = torch.ByteTensor(data.val_mask)
+    test_mask = torch.ByteTensor(data.test_mask)
     in_feats = features.shape[1]
     n_classes = data.num_labels
     n_edges = data.graph.number_of_edges()
@@ -88,10 +105,24 @@ def main(args):
         torch.cuda.set_device(args.gpu)
         features = features.cuda()
         labels = labels.cuda()
-        mask = mask.cuda()
+        train_mask = train_mask.cuda()
+        val_mask = val_mask.cuda()
+        test_mask = test_mask.cuda()
+
+    # graph preprocess and calculate normalization factor
+    g = DGLGraph(data.graph)
+    num_nodes = g.number_of_nodes()
+    # add self loop
+    g.add_edges(range(num_nodes), range(num_nodes))
+    # normalization
+    degs = g.in_degrees(range(num_nodes)).float()
+    norm = torch.pow(degs, -0.5)
+    norm[torch.isinf(norm)] = 0
+    if cuda:
+        norm = norm.cuda()
+    g.ndata['norm'] = norm.unsqueeze(1)
 
     # create GCN model
-    g = DGLGraph(data.graph)
     model = GCN(g,
                 in_feats,
                 args.n_hidden,
@@ -104,17 +135,20 @@ def main(args):
         model.cuda()
 
     # use optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=args.lr,
+                                 weight_decay=args.weight_decay)
 
     # initialize graph
     dur = []
     for epoch in range(args.n_epochs):
+        model.train()
         if epoch >= 3:
             t0 = time.time()
         # forward
         logits = model(features)
         logp = F.log_softmax(logits, 1)
-        loss = F.nll_loss(logp[mask], labels[mask])
+        loss = F.nll_loss(logp[train_mask], labels[train_mask])
 
         optimizer.zero_grad()
         loss.backward()
@@ -123,24 +157,32 @@ def main(args):
         if epoch >= 3:
             dur.append(time.time() - t0)
 
+        acc = evaluate(model, features, labels, val_mask)
+        print(acc)
         print("Epoch {:05d} | Loss {:.4f} | Time(s) {:.4f} | ETputs(KTEPS) {:.2f}".format(
             epoch, loss.item(), np.mean(dur), n_edges / np.mean(dur) / 1000))
+
+    acc = evaluate(model, features, labels, test_mask)
+    print(acc)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='GCN')
     register_data_args(parser)
-    parser.add_argument("--dropout", type=float, default=0,
+    parser.add_argument("--dropout", type=float, default=0.5,
             help="dropout probability")
     parser.add_argument("--gpu", type=int, default=-1,
             help="gpu")
     parser.add_argument("--lr", type=float, default=1e-3,
             help="learning rate")
-    parser.add_argument("--n-epochs", type=int, default=20,
+    parser.add_argument("--n-epochs", type=int, default=200,
             help="number of training epochs")
     parser.add_argument("--n-hidden", type=int, default=16,
             help="number of hidden gcn units")
     parser.add_argument("--n-layers", type=int, default=1,
             help="number of hidden gcn layers")
+    parser.add_argument("--weight-decay", type=float, default=5e-4,
+            help="Weight for L2 loss")
     args = parser.parse_args()
     print(args)
 
