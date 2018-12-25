884c31e912aa93d96209ffdd4d92bef97ac64d97
Da Zheng <zhengda1936@gmail.com>
2018-12-25 15:58:47 +0800

    use scan to search the cache.

--- src/cache/cache.cc
+++ src/cache/cache.cc
@@ -45,14 +45,51 @@ std::vector<IdArray> SearchCachedIds(IdArray cached_ids, const std::vector<IdArr
     std::vector<dgl_id_t> cache_idx;
     // If a queries data isn't in the cache, its original id.
     std::vector<dgl_id_t> global_uncached_ids;
-    for (int64_t j = 0; j < query_len; j++) {
-      dgl_id_t id = find_id(cached_data, cached_data + cached_len, query_data[j]);
-      if (id == -1) {
-        global_uncached_ids.push_back(query_data[j]);
-        uncached_query_idx.push_back(j);
-      } else {
-        cache_idx.push_back(id);
-        cached_query_idx.push_back(j);
+
+    // If the query is much smaller than the cache size, we use binary search.
+    // The complexity is k * log n, where k is the query size and n is the cache size.
+    // However, there may be a lot of cache misses.
+    if (query_len * 8 < cached_len) {
+      for (int64_t j = 0; j < query_len; j++) {
+        dgl_id_t id = find_id(cached_data, cached_data + cached_len, query_data[j]);
+        if (id == -1) {
+          global_uncached_ids.push_back(query_data[j]);
+          uncached_query_idx.push_back(j);
+        } else {
+          cache_idx.push_back(id);
+          cached_query_idx.push_back(j);
+        }
+      }
+    } else {
+      // If the query size is more or less the same size as the cache, we should consider
+      // scan the two arrays. Before scanning, we need to make sure the queries are ordered
+      // in the ascending order. The complexity is k * log k + k + n.
+      // More importantly, this implementation only requires squential memory access.
+      std::vector<dgl_id_t> tmp_data(query_data, query_data + query_len);
+      std::sort(tmp_data.begin(), tmp_data.end());
+      query_data = tmp_data.data();
+
+      int64_t i = 0, j = 0;
+      while (i < query_len && j < cached_len) {
+        // The query doesn't exist in the cache.
+        if (query_data[i] < cached_data[j]) {
+          global_uncached_ids.push_back(query_data[i]);
+          uncached_query_idx.push_back(i);
+          i++;
+        } else if (query_data[i] > cached_data[j]) {
+          // The cached data isn't queried. We don't care. ignore it.
+          j++;
+        } else {
+          cache_idx.push_back(j);
+          cached_query_idx.push_back(i);
+          i++; j++;
+        }
+      }
+      // The remaining queries aren't in the cache.
+      while (i < query_len) {
+        global_uncached_ids.push_back(query_data[i]);
+        uncached_query_idx.push_back(i);
+        i++;
       }
     }
 
