fix bugs and accum gradients for multiple batches

examples/pytorch/transformer/dataset/__init__.py
@@ -4,12 +4,12 @@ from .utils import prepare_dataset
 import os
 import numpy as np
 
-class ClassificationDataset:
+class ClassificationDataset(object):
     "Dataset class for classification task."
     def __init__(self):
         raise NotImplementedError
 
-class TranslationDataset:
+class TranslationDataset(object):
     '''
     Dataset class for translation task.
     By default, the source language shares the same vocabulary with the target language.
@@ -91,7 +91,7 @@ class TranslationDataset:
         return self.vocab[self.EOS_TOKEN]
 
     def __call__(self, graph_pool, mode='train', batch_size=32, k=1,
-                 device='cpu', ndev=1):
+                 device='cpu', dev_rank=0, ndev=1):
         '''
         Create a batched graph correspond to the mini-batch of the dataset.
         args:
@@ -104,11 +104,12 @@ class TranslationDataset:
         src_data, tgt_data = self.src[mode], self.tgt[mode]
         n = len(src_data)
         # make sure all devices have the same number of batches
-        n_ceil = (n + batch_size - 1) // batch_size * batch_size
-        sample_per_dev = batch_size * ndev
-        n = min(n, n_ceil // sample_per_dev * sample_per_dev)
+        n = n // ndev * ndev
 
-        order = np.random.permutation(n) if mode == 'train' else range(n)
+        #order = np.random.permutation(n) if mode == 'train' else range(n)
+        # FIXME: do not shuffle for mgpu
+        order = range(dev_rank, n, ndev)
+
         src_buf, tgt_buf = [], []
 
         for idx in order:

examples/pytorch/transformer/loss/__init__.py
@@ -58,35 +58,43 @@ class SimpleLossCompute(nn.Module):
     def accuracy(self):
         return (self.n_correct + self.eps) / (self.norm_term + self.eps)
 
-    def backward(self):
+    def backward_and_step(self):
         self.loss.backward()
+        self.opt.step()
+        self.opt.optimizer.zero_grad()
 
-    def __call__(self, y_pred, y, norm, is_train=True):
+    def __call__(self, y_pred, y, norm):
         y_pred = y_pred.contiguous().view(-1, y_pred.shape[-1])
         y = y.contiguous().view(-1)
         self.loss = self.criterion(
             y_pred, y
         ) / norm
-        if is_train:
-            self.backward()
-            self.opt.step()
-            self.opt.optimizer.zero_grad()
+        if self.opt is not None:
+            self.backward_and_step()
         self.n_correct += ((y_pred.max(dim=-1)[1] == y) & (y != self.criterion.padding_idx)).sum().item()
         self.acc_loss += self.loss.item() * norm
         self.norm_term += norm
         return self.loss.item() * norm
 
 class MultiGPULossCompute(SimpleLossCompute):
-    def __init__(self, criterion, dev_id, ndev, model, opt=None):
+    def __init__(self, criterion, dev_id, ndev, accum_count, model, opt=None):
         super(MultiGPULossCompute, self).__init__(criterion, opt)
         self.dev_id = dev_id
         self.ndev = ndev
+        self.accum_count = accum_count
         self.model = model
+        self.count = 0
 
-    def backward(self):
+    def backward_and_step(self):
         # multi-gpu synchronous backward
         self.loss.backward()
-        for param in self.model.parameters():
-            if param.requires_grad and param.grad is not None:
-                dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
-                param.grad.data /= self.ndev
+        self.count += 1
+        # accumulate self.accum_count times then synchronize and update
+        if self.count == self.accum_count:
+            for param in self.model.parameters():
+                if param.requires_grad and param.grad is not None:
+                    dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
+                    param.grad.data /= self.ndev
+            self.opt.step()
+            self.opt.optimizer.zero_grad()
+            self.count = 0

examples/pytorch/transformer/translation_train.py
@@ -13,12 +13,12 @@ from modules.config import *
 import numpy as np
 import argparse
 import torch
+from functools import partial
 
 def run_epoch(data_iter, dev_rank, ndev, model, loss_compute, is_train=True):
     universal = isinstance(model, UTransformer)
     for i, g in enumerate(data_iter):
-        if i % ndev != dev_rank:
-            continue
+        #print("Dev {} start batch {}".format(dev_rank, i))
         with T.set_grad_enabled(is_train):
             if isinstance(model, list):
                 model = model[:len(gs)]
@@ -33,14 +33,14 @@ def run_epoch(data_iter, dev_rank, ndev, model, loss_compute, is_train=True):
                     output = model(g)
                 tgt_y = g.tgt_y
                 n_tokens = g.n_tokens
-            loss = loss_compute(output, tgt_y, n_tokens, is_train=is_train)
+            loss = loss_compute(output, tgt_y, n_tokens)
 
     if universal:
         for step in range(1, model.MAX_DEPTH + 1):
             print("nodes entering step {}: {:.2f}%".format(step, (1.0 * model.stat[step] / model.stat[0])))
         model.reset_stat()
-    print('average loss: {}'.format(loss_compute.avg_loss))
-    print('accuracy: {}'.format(loss_compute.accuracy))
+    print('Dev {} average loss: {}'.format(dev_rank, loss_compute.avg_loss))
+    print('Dev {} accuracy: {}'.format(dev_rank, loss_compute.accuracy))
 
 def run(dev_id, args):
     # FIXME: make ip and port configurable
@@ -80,36 +80,43 @@ def main(dev_id, args):
                         T.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.98), eps=1e-9))
 
     if args.ngpu > 1:
-        loss_compute = MultiGPULossCompute(criterion, dev_id, args.ngpu,
-                                           model, opt=model_opt)
+        loss_compute = partial(MultiGPULossCompute, criterion, dev_id,
+                               args.ngpu, args.accum, model)
         dev_rank = dev_id
         ndev = args.ngpu
     else:
-        loss_compute = SimpleLossCompute(criterion, opt=model_opt)
+        loss_compute = partial(SimpleLossCompute, criterion)
         dev_rank = 0
-        ndev = 0
+        ndev = 1
 
     for epoch in range(100):
-        train_iter = dataset(graph_pool, mode='train', batch_size=args.batch, device=device, ndev=ndev)
-        valid_iter = dataset(graph_pool, mode='valid', batch_size=args.batch, device=device, ndev=ndev)
-        print('Epoch: {} Training...'.format(epoch))
+        start = time.time()
+        train_iter = dataset(graph_pool, mode='train', batch_size=args.batch,
+                             device=device, dev_rank=dev_rank, ndev=ndev)
+        valid_iter = dataset(graph_pool, mode='valid', batch_size=args.batch,
+                             device=device, dev_rank=dev_rank, ndev=ndev)
+        print('Dev {} Epoch: {} Training...'.format(dev_rank, epoch))
         model.train(True)
-        run_epoch(train_iter, dev_rank, ndev, model, loss_compute, is_train=True)
-        print('Epoch: {} Evaluating...'.format(epoch))
+        run_epoch(train_iter, dev_rank, ndev, model, loss_compute(opt=model_opt))
+        print('Dev {} Epoch: {} Evaluating...'.format(dev_rank, epoch))
         model.att_weight_map = None
         model.eval()
-        run_epoch(valid_iter, dev_rank, ndev, model, loss_compute, is_train=False)
-        """
-        # Visualize attention
-        if args.viz:
-            src_seq = dataset.get_seq_by_id(VIZ_IDX, mode='valid', field='src')
-            tgt_seq = dataset.get_seq_by_id(VIZ_IDX, mode='valid', field='tgt')[:-1]
-            draw_atts(model.att_weight_map, src_seq, tgt_seq, exp_setting, 'epoch_{}'.format(epoch))
+        run_epoch(valid_iter, dev_rank, ndev, model, loss_compute(opt=None))
+        end = time.time()
+        if dev_rank == 0:
+            print("epoch time: {}".format(end - start))
 
-        print('----------------------------------')
-        with open('checkpoints/{}-{}.pkl'.format(exp_setting, epoch), 'wb') as f:
-            th.save(model.state_dict(), f)
-        """
+            """
+            # Visualize attention
+            if args.viz:
+                src_seq = dataset.get_seq_by_id(VIZ_IDX, mode='valid', field='src')
+                tgt_seq = dataset.get_seq_by_id(VIZ_IDX, mode='valid', field='tgt')[:-1]
+                draw_atts(model.att_weight_map, src_seq, tgt_seq, exp_setting, 'epoch_{}'.format(epoch))
+
+            print('----------------------------------')
+            with open('checkpoints/{}-{}.pkl'.format(exp_setting, epoch), 'wb') as f:
+                torch.save(model.state_dict(), f)
+            """
 
 if __name__ == '__main__':
     if not os.path.exists('checkpoints'):
@@ -122,7 +129,11 @@ if __name__ == '__main__':
     argparser.add_argument('--batch', default=128, type=int, help='batch size')
     argparser.add_argument('--viz', action='store_true', help='visualize attention')
     argparser.add_argument('--universal', action='store_true', help='use universal transformer')
+    argparser.add_argument('--accum', type=int, default=1,
+                           help='accumulate gradients for this many times '
+                                'then update weights')
     args = argparser.parse_args()
+    print(args)
     #args_filter = ['batch', 'gpus', 'viz']
     #exp_setting = '-'.join('{}'.format(v) for k, v in vars(args).items() if k not in args_filter)
     #devices = ['cpu'] if args.gpus == '-1' else [int(gpu_id) for gpu_id in args.gpus.split(',')]