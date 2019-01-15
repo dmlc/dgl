18b2ea856d66fe9e833652136d951699ffbc9a61
Da Zheng <zhengda1936@gmail.com>
2019-01-15 12:52:27 +0800

    move test_graph_index to compute.

--- tests/compute/test_graph_index.py
+++ tests/compute/test_graph_index.py
@@ -1,7 +1,5 @@
-import os
-os.environ['DGLBACKEND'] = 'mxnet'
+import backend as F
 import networkx as nx
-import mxnet as mx
 import numpy as np
 import scipy as sp
 import dgl
@@ -29,9 +27,9 @@ def generate_rand_graph(n):
     return g, ig
 
 def check_graph_equal(g1, g2):
-    adj1 = g1.adjacency_matrix(False, mx.cpu())[0] != 0
-    adj2 = g2.adjacency_matrix(False, mx.cpu())[0] != 0
-    assert mx.nd.sum(adj1 - adj2).asnumpy() == 0
+    adj1 = g1.adjacency_matrix(False, F.cpu())[0] != 0
+    adj2 = g2.adjacency_matrix(False, F.cpu())[0] != 0
+    assert F.allclose(adj1, adj2)
 
 def test_graph_gen():
     g, ig = generate_from_edgelist()
@@ -42,7 +40,7 @@ def test_graph_gen():
 def sort_edges(edges):
     edges = [e.tousertensor() for e in edges]
     if np.prod(edges[2].shape) > 0:
-        idx = mx.nd.argsort(edges[2])
+        val, idx = F.sort_1d(edges[2])
         return (edges[0][idx], edges[1][idx], edges[2][idx])
     else:
         return (edges[0], edges[1], edges[2])
@@ -61,28 +59,28 @@ def check_basics(g, ig):
         assert g.has_node(i) == ig.has_node(i)
 
     for i in range(g.number_of_nodes()):
-        assert mx.nd.sum(g.predecessors(i).tousertensor()).asnumpy() == mx.nd.sum(ig.predecessors(i).tousertensor()).asnumpy()
-        assert mx.nd.sum(g.successors(i).tousertensor()).asnumpy() == mx.nd.sum(ig.successors(i).tousertensor()).asnumpy()
+        assert F.asnumpy(F.sum(g.predecessors(i).tousertensor(), 0)) == F.asnumpy(F.sum(ig.predecessors(i).tousertensor(), 0))
+        assert F.asnumpy(F.sum(g.successors(i).tousertensor(), 0)) == F.asnumpy(F.sum(ig.successors(i).tousertensor(), 0))
 
     randv = np.random.randint(0, g.number_of_nodes(), 10)
     randv = utils.toindex(randv)
     in_src1, in_dst1, in_eids1 = sort_edges(g.in_edges(randv))
     in_src2, in_dst2, in_eids2 = sort_edges(ig.in_edges(randv))
     nnz = in_src2.shape[0]
-    assert mx.nd.sum(in_src1 == in_src2).asnumpy() == nnz
-    assert mx.nd.sum(in_dst1 == in_dst2).asnumpy() == nnz
-    assert mx.nd.sum(in_eids1 == in_eids2).asnumpy() == nnz
+    assert F.asnumpy(F.sum(in_src1 == in_src2, 0)) == nnz
+    assert F.asnumpy(F.sum(in_dst1 == in_dst2, 0)) == nnz
+    assert F.asnumpy(F.sum(in_eids1 == in_eids2, 0)) == nnz
 
     out_src1, out_dst1, out_eids1 = sort_edges(g.out_edges(randv))
     out_src2, out_dst2, out_eids2 = sort_edges(ig.out_edges(randv))
     nnz = out_dst2.shape[0]
-    assert mx.nd.sum(out_dst1 == out_dst2).asnumpy() == nnz
-    assert mx.nd.sum(out_src1 == out_src2).asnumpy() == nnz
-    assert mx.nd.sum(out_eids1 == out_eids2).asnumpy() == nnz
+    assert F.asnumpy(F.sum(out_dst1 == out_dst2, 0)) == nnz
+    assert F.asnumpy(F.sum(out_src1 == out_src2, 0)) == nnz
+    assert F.asnumpy(F.sum(out_eids1 == out_eids2, 0)) == nnz
 
     num_v = len(randv)
-    assert mx.nd.sum(g.in_degrees(randv).tousertensor() == ig.in_degrees(randv).tousertensor()).asnumpy() == num_v
-    assert mx.nd.sum(g.out_degrees(randv).tousertensor() == ig.out_degrees(randv).tousertensor()).asnumpy() == num_v
+    assert F.asnumpy(F.sum(g.in_degrees(randv).tousertensor() == ig.in_degrees(randv).tousertensor(), 0)) == num_v
+    assert F.asnumpy(F.sum(g.out_degrees(randv).tousertensor() == ig.out_degrees(randv).tousertensor(), 0)) == num_v
     randv = randv.tousertensor()
     for v in randv.asnumpy():
         assert g.in_degree(v) == ig.in_degree(v)
@@ -95,8 +93,8 @@ def check_basics(g, ig):
             assert g.has_edge_between(u, v) == ig.has_edge_between(u, v)
     randv = utils.toindex(randv)
     ids = g.edge_ids(randv, randv)[2].tonumpy()
-    assert sum(ig.edge_ids(randv, randv)[2].tonumpy() == ids) == len(ids)
-    assert sum(g.has_edges_between(randv, randv).tonumpy() == ig.has_edges_between(randv, randv).tonumpy()) == len(randv)
+    assert sum(ig.edge_ids(randv, randv)[2].tonumpy() == ids, 0) == len(ids)
+    assert sum(g.has_edges_between(randv, randv).tonumpy() == ig.has_edges_between(randv, randv).tonumpy(), 0) == len(randv)
 
 
 def test_basics():
@@ -118,8 +116,8 @@ def test_node_subgraph():
     subig = ig.node_subgraph(utils.toindex(randv))
     check_basics(subg, subig)
     check_graph_equal(subg, subig)
-    assert mx.nd.sum(map_to_subgraph_nid(subg, utils.toindex(randv1[0:10])).tousertensor()
-            == map_to_subgraph_nid(subig, utils.toindex(randv1[0:10])).tousertensor()) == 10
+    assert F.sum(map_to_subgraph_nid(subg, utils.toindex(randv1[0:10])).tousertensor()
+            == map_to_subgraph_nid(subig, utils.toindex(randv1[0:10])).tousertensor(), 0) == 10
 
     # node_subgraphs
     randvs = []
