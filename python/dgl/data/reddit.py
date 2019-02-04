7b9739fc446202b8cf4b5c31c12111dc99b760e0
ZiyueHuang <zyhuang94@gmail.com>
2019-02-04 18:03:59 +0000

    update

--- python/dgl/data/reddit.py
+++ python/dgl/data/reddit.py
@@ -16,15 +16,16 @@ class RedditDataset(object):
         extract_archive(zip_file_path, extract_dir)
         # graph
         coo_adj = sp.load_npz(os.path.join(extract_dir, "reddit_graph.npz"))
-        self.graph = dgl.DGLGraph(coo_adj, readonly=True)
+        self.graph = coo_adj
         # features and labels
         reddit_data = np.load(os.path.join(extract_dir, "reddit_data.npz"))
         self.features = reddit_data["feature"]
         self.labels = reddit_data["label"]
+        self.num_labels = 41
         # tarin/val/test indices
         node_ids = reddit_data["node_ids"]
         node_types = reddit_data["node_types"]
-        self.train_indices = node_ids[node_types == 1]
-        self.val_indices = node_ids[node_types == 2]
-        self.test_indices = node_ids[node_types == 3]
+        self.train_mask = (node_types == 1)
+        self.val_mask = (node_types == 2)
+        self.test_mask = (node_types == 3)
 
