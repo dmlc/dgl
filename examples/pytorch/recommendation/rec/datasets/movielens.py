d457c5e94889b11c398806c1a5f3a60977f20d83
Gan Quan <coin2028@hotmail.com>
2019-02-08 00:40:05 -0500

    pinsage-like model on movielens

---
+++ examples/pytorch/recommendation/rec/datasets/movielens.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import dgl
+import os
+import torch
+from .. import randomwalk
+
+class MovieLens(object):
+    def __init__(self, directory):
+        '''
+        directory: path to movielens directory which should have the three
+                   files:
+                   users.dat
+                   movies.dat
+                   ratings.dat
+        '''
+        self.directory = directory
+
+        users = []
+        movies = []
+        ratings = []
+
+        # read users
+        with open(os.path.join(directory, 'users.dat')) as f:
+            for l in f:
+                id_, gender, age, occupation, zip_ = l.split('::')
+                users.append({
+                    'id': int(id_),
+                    'gender': gender,
+                    'age': age,
+                    'occupation': occupation,
+                    'zip': zip_,
+                    })
+        self.users = pd.DataFrame(users).set_index('id')
+
+        # read movies
+        with open(os.path.join(directory, 'movies.dat'), encoding='latin1') as f:
+            for l in f:
+                id_, title, genres = l.split('::')
+                genres_set = set(genres.split('|'))
+                data = {'id': int(id_), 'title': title}
+                for g in genres_set:
+                    data[g] = True
+                movies.append(data)
+        self.movies = pd.DataFrame(movies).set_index('id')
+
+        # read ratings
+        with open(os.path.join(directory, 'ratings.dat')) as f:
+            for l in f:
+                user_id, movie_id, rating, timestamp = [int(_) for _ in l.split('::')]
+                ratings.append({
+                    'user_id': user_id,
+                    'movie_id': movie_id,
+                    'rating': rating,
+                    'timestamp': timestamp,
+                    })
+        self.ratings = pd.DataFrame(ratings)
+
+        # drop users and movies which do not exist in ratings
+        self.users = self.users[self.users.index.isin(self.ratings['user_id'])]
+        self.movies = self.movies[self.movies.index.isin(self.ratings['movie_id'])]
+
+        self.data_split()
+        self.build_graph()
+        self.find_neighbors()
+
+    def data_split(self):
+        test_set = self.ratings.sample(frac=0.02, random_state=1).index
+        valid_set = self.ratings.sample(frac=0.02, random_state=2).index
+        valid_set = valid_set.difference(test_set)
+        prev_set = valid_set.union(test_set)
+        train_set = self.ratings.sample(frac=0.1, random_state=3).index
+        train_set = train_set.difference(prev_set)
+        prev_set = prev_set.union(train_set)
+
+        self.ratings['valid'] = self.ratings.index.isin(valid_set)
+        self.ratings['test'] = self.ratings.index.isin(test_set)
+        self.ratings['train'] = self.ratings.index.isin(train_set)
+        self.ratings['prior'] = ~self.ratings.index.isin(prev_set)
+
+    def build_graph(self):
+        user_ids = list(self.users.index)
+        movie_ids = list(self.movies.index)
+
+        user_ids_invmap = {id_: i for i, id_ in enumerate(user_ids)}
+        movie_ids_invmap = {id_: i for i, id_ in enumerate(movie_ids)}
+
+        g = dgl.DGLGraph()
+        g.add_nodes(len(user_ids) + len(movie_ids))
+        rating_user_vertices = [user_ids_invmap[id_] for id_ in self.ratings['user_id'].values]
+        rating_movie_vertices = [movie_ids_invmap[id_] + len(user_ids)
+                                 for id_ in self.ratings['movie_id'].values]
+
+        valid_tensor = torch.from_numpy(self.ratings['valid'].values.astype('uint8'))
+        test_tensor = torch.from_numpy(self.ratings['test'].values.astype('uint8'))
+        train_tensor = torch.from_numpy(self.ratings['train'].values.astype('uint8'))
+        prior_tensor = torch.from_numpy(self.ratings['prior'].values.astype('uint8'))
+        edge_data = {
+                'valid': valid_tensor,
+                'test': test_tensor,
+                'train': train_tensor,
+                'prior': prior_tensor,
+                }
+
+        g.add_edges(rating_user_vertices, rating_movie_vertices, data=edge_data)
+        g.add_edges(rating_movie_vertices, rating_user_vertices, data=edge_data)
+
+        self.g = g
+        self.user_ids = user_ids
+        self.movie_ids = movie_ids
+
+    def find_neighbors(self):
+        neighbors = randomwalk.random_walk_distribution_topt(
+                self.g, self.g.nodes(), 100, 10, 20)
+
+        user_neighbors = []
+        for i in range(len(self.user_ids)):
+            user_neighbor = neighbors[i].numpy()
+            user_neighbor = user_neighbor[user_neighbor < len(self.user_ids)]
+            user_neighbors.append(user_neighbor)
+
+        movie_neighbors = []
+        for i in range(len(self.user_ids), len(self.user_ids) + len(self.movie_ids)):
+            movie_neighbor = neighbors[i].numpy()
+            movie_neighbor = movie_neighbor[movie_neighbor >= len(self.user_ids)]
+            movie_neighbors.append(movie_neighbor)
+
+        self.user_neighbors = user_neighbors
+        self.movie_neighbors = movie_neighbors
---
+++ examples/pytorch/recommendation/rec/model/pinsage.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import dgl
+from .. import randomwalk
+
+def get_embeddings(h, nodeset):
+    return h[nodeset]
+
+def put_embeddings(h, nodeset, new_embeddings):
+    n_nodes = nodeset.shape[0]
+    n_features = h.shape[1]
+    return h.scatter(0, nodeset[:, None].expand(n_nodes, n_features), new_embeddings)
+
+class PinSageConv(nn.Module):
+    def __init__(self, in_features, out_features, hidden_features):
+        super(PinSageConv, self).__init__()
+
+        self.in_features = in_features
+        self.out_features = out_features
+        self.hidden_features = hidden_features
+
+        self.Q = nn.Linear(in_features, hidden_features)
+        self.W = nn.Linear(in_features + hidden_features, out_features)
+
+    def forward(self, h, nodeset, nb_nodes, nb_weights):
+        '''
+        h: node embeddings (num_total_nodes, in_features), or a container
+           of the node embeddings (for distributed computing)
+        nodeset: node IDs in this minibatch (num_nodes,)
+        nb_nodes: neighbor node IDs of each node in nodeset (num_nodes, num_neighbors)
+        nb_weights: weight of each neighbor node (num_nodes, num_neighbors)
+        return: new node embeddings (num_nodes, out_features)
+        '''
+        n_nodes, T = nb_nodes.shape[0]
+
+        h_nodeset = get_embeddings(h, nodeset)  # (n_nodes, in_features)
+        h_neighbors = get_embeddings(h, nb_nodes.view(-1)).view(n_nodes, T, self.in_features)
+
+        h_neighbors = F.relu(self.Q(h_neighbors))
+        h_agg = (nb_weights[:, :, None] * h_neighbors).sum(1) / nb_weights.sum(1, keepdim=True)
+
+        h_concat = torch.cat([h_nodeset, h_agg], 1)
+        h_new = F.relu(self.W(h_concat))
+        h_new /= h_new.norm(dim=1, keepdim=True)
+
+        return h_new
+
+class PinSage(nn.Module):
+    '''
+    Completes a multi-layer PinSage convolution
+    G: DGLGraph
+    feature_sizes: the dimensionality of input/hidden/output features
+    T: number of neighbors we pick for each node
+    n_traces: number of random walk traces to generate during sampling
+    n_hops: number of hops of each random walk trace during sampling
+    '''
+    def __init__(self, G, feature_sizes, T, n_traces, n_hops):
+        super(PinSage, self).__init__()
+
+        self.G = G
+        self.T = T
+        self.n_traces = n_traces
+        self.n_hops = n_hops
+
+        self.in_features = feature_sizes[0]
+        self.out_features = feature_sizes[-1]
+        self.n_layers = len(feature_sizes) - 1
+
+        self.convs = nn.ModuleList()
+        for i in range(self.n_layers):
+            self.convs.append(PinSageConv(
+                feature_sizes[i], feature_sizes[i+1], feature_sizes[i+1]))
+
+    def forward(self, h, nodeset):
+        '''
+        Given a complete embedding matrix h and a list of node IDs, return
+        the output embeddings of these node IDs.
+
+        h: node embeddings (num_total_nodes, in_features), or a container
+           of the node embeddings (for distributed computing)
+        nodeset: node IDs in this minibatch (num_nodes,)
+        return: new node embeddings (num_nodes, out_features)
+        '''
+        nodeflow = randomwalk.random_walk_nodeflow(
+                self.G, nodeset, self.n_layers, self.n_traces, self.n_hops, self.T)
+
+        for i, (nodeset, nb_weights, nb_nodes) in enumerate(nodeflow):
+            new_embeddings = self.convs[i](h, nodeset, nb_nodes, nb_weights)
+            h = put_embeddings(h, nodeset, new_embeddings)
+
+        return get_embeddings(h, nodeset)
