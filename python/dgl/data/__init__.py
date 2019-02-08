67b79881610e90fc109c6589614a2a3e559d91fa
ZiyueHuang <zyhuang94@gmail.com>
2019-02-08 06:45:18 +0000

    address comments

--- python/dgl/data/__init__.py
+++ python/dgl/data/__init__.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 
 from . import citation_graph as citegrh
 from .citation_graph import CoraBinary
-from .minigc import *
 from .tree import *
 from .utils import *
 from .sbm import SBMMixture
@@ -23,7 +22,7 @@ def load_data(args):
         return citegrh.load_pubmed()
     elif args.dataset == 'syn':
         return citegrh.load_synthetic(args)
-    elif args.dataset == 'reddit':
-        return RedditDataset()
+    elif args.dataset.startswith('reddit'):
+        return RedditDataset(self_loop=('self-loop' in args.dataset))
     else:
         raise ValueError('Unknown dataset: {}'.format(args.dataset))
--- python/dgl/data/reddit.py
+++ python/dgl/data/reddit.py
@@ -33,3 +33,12 @@ class RedditDataset(object):
         self.val_mask = (node_types == 2)
         self.test_mask = (node_types == 3)
 
+        print('Finished data loading.')
+        print('  NumNodes: {}'.format(self.graph.number_of_nodes()))
+        print('  NumEdges: {}'.format(self.graph.number_of_edges()))
+        print('  NumFeats: {}'.format(self.features.shape[1]))
+        print('  NumClasses: {}'.format(self.num_labels))
+        print('  NumTrainingSamples: {}'.format(len(np.nonzero(self.train_mask)[0])))
+        print('  NumValidationSamples: {}'.format(len(np.nonzero(self.val_mask)[0])))
+        print('  NumTestSamples: {}'.format(len(np.nonzero(self.test_mask)[0])))
+
