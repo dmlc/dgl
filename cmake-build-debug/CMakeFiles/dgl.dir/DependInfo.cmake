e2d506cb206e7404bff3a127d91d4614553be7c2
Haobo Sun (FA Talent) <v-haosun@microsoft.com>
2019-02-11 16:46:18 +0800

    fix the comment error of edge_id in graph.h

---
+++ cmake-build-debug/CMakeFiles/dgl.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/dgl/src/c_api_common.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/c_api_common.cc.obj"
+  "D:/dgl/src/graph/graph.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/graph.cc.obj"
+  "D:/dgl/src/graph/graph_apis.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/graph_apis.cc.obj"
+  "D:/dgl/src/graph/graph_op.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/graph_op.cc.obj"
+  "D:/dgl/src/graph/immutable_graph.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/immutable_graph.cc.obj"
+  "D:/dgl/src/graph/traversal.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/traversal.cc.obj"
+  "D:/dgl/src/runtime/c_runtime_api.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/c_runtime_api.cc.obj"
+  "D:/dgl/src/runtime/cpu_device_api.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/cpu_device_api.cc.obj"
+  "D:/dgl/src/runtime/dso_module.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/dso_module.cc.obj"
+  "D:/dgl/src/runtime/file_util.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/file_util.cc.obj"
+  "D:/dgl/src/runtime/module.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/module.cc.obj"
+  "D:/dgl/src/runtime/module_util.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/module_util.cc.obj"
+  "D:/dgl/src/runtime/ndarray.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/ndarray.cc.obj"
+  "D:/dgl/src/runtime/registry.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/registry.cc.obj"
+  "D:/dgl/src/runtime/system_lib_module.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/system_lib_module.cc.obj"
+  "D:/dgl/src/runtime/thread_pool.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/thread_pool.cc.obj"
+  "D:/dgl/src/runtime/threading_backend.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/threading_backend.cc.obj"
+  "D:/dgl/src/runtime/workspace_pool.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/workspace_pool.cc.obj"
+  "D:/dgl/src/scheduler/scheduler.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/scheduler/scheduler.cc.obj"
+  "D:/dgl/src/scheduler/scheduler_apis.cc" "D:/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/scheduler/scheduler_apis.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../third_party/dlpack/include"
+  "../third_party/dmlc-core/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/dgl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dgl.
+# This may be replaced when dependencies are built.
