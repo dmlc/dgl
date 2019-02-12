e5a1299cc76063a5aae544186c40461189b4efe5
Lingfan Yu <ly979@nyu.edu>
2019-02-11 20:10:31 -0500

    uncomment viz code

--- examples/pytorch/transformer/translation_train.py
+++ examples/pytorch/transformer/translation_train.py
@@ -105,13 +105,11 @@ def main(dev_id, args):
             end = time.time()
             print("epoch time: {}".format(end - start))
 
-            """
             # Visualize attention
             if args.viz:
                 src_seq = dataset.get_seq_by_id(VIZ_IDX, mode='valid', field='src')
                 tgt_seq = dataset.get_seq_by_id(VIZ_IDX, mode='valid', field='tgt')[:-1]
                 draw_atts(model.att_weight_map, src_seq, tgt_seq, exp_setting, 'epoch_{}'.format(epoch))
-            """
             args_filter = ['batch', 'gpus', 'viz', 'master_ip', 'master_port', 'grad_accum', 'ngpu']
             exp_setting = '-'.join('{}'.format(v) for k, v in vars(args).items() if k not in args_filter)
             with open('checkpoints/{}-{}.pkl'.format(exp_setting, epoch), 'wb') as f:
