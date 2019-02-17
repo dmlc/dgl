95634f1070229a55f022c798d7d99c084cd35c6b
Hao Zhang <1339754545@qq.com>
2019-02-18 04:15:52 +0800

    Update ppi.py

--- python/dgl/data/ppi.py
+++ python/dgl/data/ppi.py
@@ -4,7 +4,6 @@
 
 import numpy as np
 from .utils import download, extract_archive, get_download_dir, _get_dgl_url
-from sklearn.preprocessing import StandardScaler
 from dgl import DGLGraph
 import networkx as nx
 from networkx.readwrite import json_graph
@@ -12,66 +11,72 @@ import json
 
 _url = 'dataset/ppi.zip'
 
-def load_ppi():
-    """Loads input data
-    ppi_G.json => the graph data used for training, test and validation as json format;
-    ppi-feats.npy => the feature vectors of nodes as numpy.ndarry object, it's shape is [n, v],
-    n is the number of nodes, v is the feature's dimension;
-    ppi-class_map.json => the classes of the input nodes, the format of it is {"1":[0, 1, 0, 1]},
-    "1" is the node index, [0, 1, 0, 1] is label;
-    ppi-graph_id.npy => the element in it indicates which graph the nodes belong to,
-    it is a one dimensional numpy.ndarray object and the length of it is equal the number of nodes,
-    it's like [1, 1, 2, 1,,,23];
 
-    """
-    name = 'ppi'
-    dir = get_download_dir()
-    zip_file_path = '{}/{}.zip'.format(dir, name)
-    download(_get_dgl_url(_url), path=zip_file_path)
-    extract_archive(zip_file_path,
-                    '{}/{}'.format(dir, name))
-    print('Loading G...')
-    with open('{}/ppi/ppi-G.json'.format(dir)) as jsonfile:
-        g_data = json.load(jsonfile)
-
-    with open('{}/ppi/ppi-class_map.json'.format(dir)) as jsonfile:
-        class_map = json.load(jsonfile)
-    features = np.load('{}/ppi/ppi-feats.npy'.format(dir))
-    label_list = []
-    for i in range(len(class_map)):
-        label_list.append(np.expand_dims(np.array(class_map[str(i)]), axis=0))
-    labels = np.concatenate(label_list)
-    graph = DGLGraph(nx.DiGraph(json_graph.node_link_graph(g_data)))
-    graph_id = np.load('{}/ppi/graph_id.npy'.format(dir))
-    return (features, labels, graph, graph_id)
 
 class PPIDataset(object):
-    
-    def __init__(self, mode, data):
+
+    def __init__(self, mode):
         """Initialize the dataset.
 
         Paramters
         ---------
         mode : str
             ('train', 'valid', 'test').
-        data : tuple()
-            (features, labels, graph, graph_id)
-          
-        """ 
+        """
         self.mode = mode
-        self.features, self.labels, self.graph, self.graph_id = data
+        self._load()
         self._preprocess()
-        self._normalize()
-        
+
+    def _load(self):
+        """Loads input data
+        train/test/valid_graph.json => the graph data used for training, test and validation as json format;
+        train/test/valid_feats.npy => the feature vectors of nodes as numpy.ndarry object, it's shape is [n, v],
+        n is the number of nodes, v is the feature's dimension;
+        train/test/valid_labels.npy=> the labels of the input nodes, it is a numpy ndarry, it's like[[0, 0, 1, ... 0], 
+        [0, 1, 1, 0 ...1]], shape of it is n*h, n is the number of nodes, h is the label's dimension;
+        train/test/valid/_graph_id.npy => the element in it indicates which graph the nodes belong to,
+        it is a one dimensional numpy.ndarray object and the length of it is equal the number of nodes,
+        it's like [1, 1, 2, 1...20]. 
+        """
+        name = 'ppi'
+        dir = get_download_dir()
+        zip_file_path = '{}/{}.zip'.format(dir, name)
+        download(_get_dgl_url(_url), path=zip_file_path)
+        extract_archive(zip_file_path,
+                        '{}/{}'.format(dir, name))
+        print('Loading G...')
+        if self.mode == 'train':
+            with open('{}/ppi/train_graph.json'.format(dir)) as jsonfile:
+                g_data = json.load(jsonfile)
+            self.labels = np.load('{}/ppi/train_labels.npy'.format(dir))
+            self.features = np.load('{}/ppi/train_feats.npy'.format(dir))
+            self.graph = DGLGraph(nx.DiGraph(json_graph.node_link_graph(g_data)))
+            self.graph_id = np.load('{}/ppi/train_graph_id.npy'.format(dir))
+        if self.mode == 'valid':
+            with open('{}/ppi/valid_graph.json'.format(dir)) as jsonfile:
+                g_data = json.load(jsonfile)
+            self.labels = np.load('{}/ppi/valid_labels.npy'.format(dir))
+            self.features = np.load('{}/ppi/valid_feats.npy'.format(dir))
+            self.graph = DGLGraph(nx.DiGraph(json_graph.node_link_graph(g_data)))
+            self.graph_id = np.load('{}/ppi/valid_graph_id.npy'.format(dir))
+        if self.mode == 'test':
+            with open('{}/ppi/test_graph.json'.format(dir)) as jsonfile:
+                g_data = json.load(jsonfile)
+            self.labels = np.load('{}/ppi/test_labels.npy'.format(dir))
+            self.features = np.load('{}/ppi/test_feats.npy'.format(dir))
+            self.graph = DGLGraph(nx.DiGraph(json_graph.node_link_graph(g_data)))
+            self.graph_id = np.load('{}/ppi/test_graph_id.npy'.format(dir))
+
     def _preprocess(self):
-        self.train_mask_list = []
-        self.train_graphs = []
-        self.train_labels = []
-        for train_graph_id in range(1, 21):
-            train_graph_mask = np.where(self.graph_id == train_graph_id)[0]
-            self.train_mask_list.append(train_graph_mask)
-            self.train_graphs.append(self.graph.subgraph(train_graph_mask))
-            self.train_labels.append(self.labels[train_graph_mask])
+        if self.mode == 'train':
+            self.train_mask_list = []
+            self.train_graphs = []
+            self.train_labels = []
+            for train_graph_id in range(1, 21):
+                train_graph_mask = np.where(self.graph_id == train_graph_id)[0]
+                self.train_mask_list.append(train_graph_mask)
+                self.train_graphs.append(self.graph.subgraph(train_graph_mask))
+                self.train_labels.append(self.labels[train_graph_mask])
         if self.mode == 'valid':
             self.valid_mask_list = []
             self.valid_graphs = []
@@ -90,17 +95,7 @@ class PPIDataset(object):
                 self.test_mask_list.append(test_graph_mask)
                 self.test_graphs.append(self.graph.subgraph(test_graph_mask))
                 self.test_labels.append(self.labels[test_graph_mask])
-                
-    def _normalize(self):
-        """
-        Normalize the features
-        """
 
-        train_feats = self.features[np.concatenate(self.train_mask_list)]
-        scaler = StandardScaler()
-        scaler.fit(train_feats)
-        self.features = scaler.transform(self.features)
-        
     def __len__(self):
         if self.mode == 'train':
             return len(self.train_mask_list)
