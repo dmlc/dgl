5e80dcc5e343790bad96c61d062339a7994408e2
aksnzhy <mctt90@gmail.com>
2019-03-01 10:36:35 +0800

    update unittest

--- tests/compute/test_dis_sampler.py
+++ tests/compute/test_dis_sampler.py
@@ -12,7 +12,7 @@ def generate_rand_graph(n):
     return dgl.DGLGraph(arr, readonly=True)
 
 def start_trainer():
-    recv = dgl.contrib.sampling.Receiver(ip="127.0.0.1", port=50051, num_sender=1, queue_size=500*1024)
+    recv = dgl.contrib.sampling.SamplerReceiver(ip="127.0.0.1", port=50051, num_sender=1, queue_size=500)
     nodeflow = recv.Receive()
     print(nodeflow._node_mapping.todgltensor())
     print(nodeflow._edge_mapping.todgltensor())
@@ -21,7 +21,7 @@ def start_trainer():
 
 def start_sampler():
     g = generate_rand_graph(100)
-    sender = dgl.contrib.sampling.Sender(ip="127.0.0.1", port=50051)
+    sender = dgl.contrib.sampling.SamplerSender(ip="127.0.0.1", port=50051)
     for nodeflow, aux in dgl.contrib.sampling.NeighborSampler(g, 1, 4, neighbor_type='in',
                                                           num_workers=1, return_seed_id=True):
         sender.Send(nodeflow)
