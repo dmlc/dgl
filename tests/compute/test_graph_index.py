8e9ebcd989dcb742c7ccd50d653ee310aa9a4176
Ubuntu <ubuntu@ip-172-31-45-98.us-east-2.compute.internal>
2019-03-31 00:59:10 +0000

    fix test.

--- tests/compute/test_graph_index.py
+++ tests/compute/test_graph_index.py
@@ -165,8 +165,8 @@ def test_load_csr():
     src, dst, eid = idx.edges()
     src, dst, eid = src.tousertensor(), dst.tousertensor(), eid.tousertensor()
     coo = csr.tocoo()
-    assert F.array_equal(src, F.tensor(coo.row, dtype=F.dtype(src)))
-    assert F.array_equal(dst, F.tensor(coo.col, dtype=F.dtype(dst)))
+    assert np.all(F.asnumpy(src) == coo.row)
+    assert np.all(F.asnumpy(dst) == coo.col)
 
     # Load CSR to shared memory.
     idx = dgl.graph_index.GraphIndex(multigraph=False, readonly=True)
@@ -176,8 +176,8 @@ def test_load_csr():
     src, dst, eid = idx.edges()
     src, dst, eid = src.tousertensor(), dst.tousertensor(), eid.tousertensor()
     coo = csr.tocoo()
-    assert F.array_equal(src, F.tensor(coo.row, dtype=F.dtype(src)))
-    assert F.array_equal(dst, F.tensor(coo.col, dtype=F.dtype(dst)))
+    assert np.all(F.asnumpy(src) == coo.row)
+    assert np.all(F.asnumpy(dst) == coo.col)
 
 if __name__ == '__main__':
     test_basics()
