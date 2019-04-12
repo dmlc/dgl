a4b414ebef7f6604df0f16599435d47758c19e42
aymenwah <aymenwaheb@gmail.com>
2019-04-12 09:44:20 +0100

    [Model] Add edge dropout to APPNP
    [Model] Add edge dropout to APPNP
    
    update

--- examples/pytorch/appnp/appnp.py
+++ examples/pytorch/appnp/appnp.py
@@ -5,9 +5,47 @@ References
 Paper: https://arxiv.org/abs/1810.05997
 Author's code: https://github.com/klicperajo/ppnp
 """
-
+import torch
 import torch.nn as nn
 import dgl.function as fn
+
+
+class GraphPropagation(nn.Module):
+    def __init__(self,
+                 g,
+                 edge_drop,
+                 alpha,
+                 k):
+        super(GraphPropagation, self).__init__()
+        self.g = g
+        self.alpha = alpha
+        self.k = k
+        if edge_drop:
+            self.edge_drop = nn.Dropout(edge_drop)
+        else:
+            self.edge_drop = 0.
+
+    def forward(self, h):
+        self.cached_h = h
+        for _ in range(self.k):
+            # normalization by square root of src degree
+            h = h * self.g.ndata['norm']
+            self.g.ndata['h'] = h
+            if self.edge_drop:
+                # performing edge dropout
+                ed = self.edge_drop(torch.ones((self.g.number_of_edges(), 1)))
+                self.g.edata['d'] = ed
+                self.g.update_all(fn.src_mul_edge(src='h', edge='d', out='m'),
+                                  fn.sum(msg='m', out='h'))
+            else:
+                self.g.update_all(fn.copy_src(src='h', out='m'),
+                                  fn.sum(msg='m', out='h'))
+            h = self.g.ndata.pop('h')
+            # normalization by square root of dst degree
+            h = h * self.g.ndata['norm']
+            # update h using teleport probability alpha
+            h = h * (1 - self.alpha) + self.cached_h * self.alpha
+        return h
 
 
 class APPNP(nn.Module):
@@ -17,12 +55,12 @@ class APPNP(nn.Module):
                  hiddens,
                  n_classes,
                  activation,
-                 dropout,
+                 feat_drop,
+                 edge_drop,
                  alpha,
                  k):
         super(APPNP, self).__init__()
         self.layers = nn.ModuleList()
-        self.g = g
         # input layer
         self.layers.append(nn.Linear(in_feats, hiddens[0]))
         # hidden layers
@@ -31,12 +69,12 @@ class APPNP(nn.Module):
         # output layer
         self.layers.append(nn.Linear(hiddens[-1], n_classes))
         self.activation = activation
-        if dropout:
-            self.dropout = nn.Dropout(p=dropout)
+        if feat_drop:
+            self.feat_drop = nn.Dropout(feat_drop)
         else:
-            self.dropout = 0.
-        self.K = k
-        self.alpha = alpha
+            self.feat_drop = lambda x: x
+        self.propagate = GraphPropagation(g, edge_drop, alpha, k)
+        self.reset_parameters()
 
     def reset_parameters(self):
         for layer in self.layers:
@@ -45,26 +83,11 @@ class APPNP(nn.Module):
     def forward(self, features):
         # prediction step
         h = features
-        if self.dropout:
-            h = self.dropout(h)
+        h = self.feat_drop(h)
         h = self.activation(self.layers[0](h))
         for layer in self.layers[1:-1]:
             h = self.activation(layer(h))
-        if self.dropout:
-            h = self.layers[-1](self.dropout(h))
-        # propagation step without dropout on adjacency matrices
-        self.cached_h = h
-        for _ in range(self.K):
-            # normalization by square root of src degree
-            h = h * self.g.ndata['norm']
-            self.g.ndata['h'] = h
-            # message-passing without performing adjacency dropout
-            self.g.update_all(fn.copy_src(src='h', out='m'),
-                              fn.sum(msg='m', out='h'))
-            h = self.g.ndata.pop('h')
-            # normalization by square root of dst degree
-            h = h * self.g.ndata['norm']
-            # update h using teleport probability alpha
-            h = h * (1 - self.alpha) + self.cached_h * self.alpha
-
+        h = self.layers[-1](self.feat_drop(h))
+        # propagation step
+        h = self.propagate(h)
         return h
