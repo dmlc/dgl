5d3fe1e08d2a18d0961147cf1e24861da91fd175
Lingfan Yu <ly979@nyu.edu>
2019-04-25 17:27:29 +0000

    clean up plus v2e spmv schedule

--- python/dgl/runtime/spmv.py
+++ python/dgl/runtime/spmv.py
@@ -4,9 +4,13 @@ from __future__ import absolute_import
 from ..base import DGLError
 from .. import backend as F
 from .. import utils
+from .. import ndarray
 
 from . import ir
 from .ir import var
+
+import scipy.sparse as sp
+from functools import partial
 
 def analyze_v2v_spmv(graph, mfunc, rfunc):
     """Analyze if SPMV from node space to node space can be applied.
@@ -24,18 +28,9 @@ def analyze_v2v_spmv(graph, mfunc, rfunc):
     -------
     spmv_pairs : list of pair of builtin functions
         The pair of spvm applicable message/reduce functions.
-    mfunc_left: list
-        A list of message functions that can't use v2v spmv. In other
-        words, these message functions need to be materialized
-    rfunc_left: list
-        A list of reduce functions that can't use v2v spmv
     """
     spmv_pairs = []
-    mfunc_left = []
-    rfunc_left = []
-
     fld2mfunc = {fn.out_field: fn for fn in mfunc}
-    touched_mfld = set()
 
     for rfn in rfunc:
         mfld = rfn.msg_field
@@ -43,92 +38,65 @@ def analyze_v2v_spmv(graph, mfunc, rfunc):
             raise DGLError('Reduce function requires message field "%s",'
                            ' but no message function generates it.' % mfld)
         mfn = fld2mfunc[mfld]
-        # TODO(minjie): should pre-compile a look up table
-        if mfn.is_spmv_supported(graph) and rfn.is_spmv_supported():
-            spmv_pairs.append((mfn, rfn))
-        else:
-            if mfld not in touched_mfld:
-                touched_mfld.add(mfld)
-                mfunc_left.append(mfn)
-            rfunc_left.append(rfn)
+        spmv_pairs.append((mfn, rfn))
 
-    return spmv_pairs, mfunc_left, rfunc_left
+    return spmv_pairs
 
-def analyze_e2v_spmv(graph, rfunc):  # pylint: disable=unused-argument
-    """Analyze if SPMV from edge space to node space can be applied.
-
-    Parameters
-    ----------
-    graph: DGLGraph
-        DGLGraph to use
-    rfunc : list of dgl.function.BuiltinFunction
-        The reduce function list.
-
-    Returns
-    -------
-    spmv_rfunc : list
-        A list of spmv-applicable reduce builtins.
-    rfunc_left : list
-        A list of reduce builtins that are not applicable
-    """
-    spmv_rfunc = []
-    rfunc_left = []
-    for rfn in rfunc:
-        if rfn.is_spmv_supported():
-            spmv_rfunc.append(rfn)
-        else:
-            rfunc_left.append(rfn)
-    return spmv_rfunc, rfunc_left
-
-def gen_v2v_spmv_schedule(adj, spmv_pairs, nft, eft, eid, out):
+def gen_v2v_spmv_schedule(adj, spmv_pairs, src_frame, dst_frame, edge_frame,
+                          out, out_size, edge_mapping, out_mapping):
     """Generate v2v spmv schedule.
 
     Parameters
     ----------
     adj : tuple (sparse matrix, utils.Index)
     spmv_pairs : list of pair
-    nft : var.Var
-        input node features
-    eft : var.Var
+    src_frame : var.Var
+        input source node features
+    dst_frame : var.Var
+        input destination node features
+    edge_frame : var.Var
         input edge features
-    eid : var.Var
-        eid index
     out : var.Var
         output node features
+    out_mapping : var.Var
+        mapping from recv nodes to relabeled consecutive node ids
     """
-    adjmat, shuffle_idx = adj
-    adj_var = var.SPMAT(adjmat)
-    if shuffle_idx is not None:
-        new_eid = utils.reorder_index(eid.data, shuffle_idx)
-        eid = var.IDX(new_eid)
+    spmat = var.SPMAT(adj)
     for mfn, rfn in spmv_pairs:
-        if mfn.use_edge_feature:
-            ftedge = ir.READ(eft, eid, var.STR(mfn.edge_field))
-            ftsrc = ir.READ_COL(nft, var.STR(mfn.src_field))
-            ftdst = ir.SPMV_WITH_DATA(adj_var, ftedge, ftsrc)
-        else:
-            ftsrc = ir.READ_COL(nft, var.STR(mfn.src_field))
-            ftdst = ir.SPMV(adj_var, ftsrc)
+        ftdst = mfn(spmat, src_frame, dst_frame, edge_frame, reducer=rfn.name,
+                    edge_mapping=edge_mapping, out_mapping=out_mapping)
         # save for merge
         ir.WRITE_COL_(out, var.STR(rfn.out_field), ftdst)
 
-def gen_e2v_spmv_schedule(inc, spmv_rfunc, mfr, out):
+def gen_v2e_spmv_schedule(adj, mfunc, src_frame, dst_frame, edge_frame,
+                          out_frame, out_size, edge_mapping, eid=None):
+    """Generate v2e SPMV schedule"""
+    spmat = var.SPMAT(adj)
+    if eid is not None:
+        write_back = partial(ir.WRITE_, row=eid)
+    else:
+        write_back = ir.WRITE_COL_
+    for mfn in mfunc:
+        fmsg = mfn(spmat, src_frame, dst_frame, edge_frame, out_size,
+                   edge_mapping=edge_mapping)
+        write_back(out_frame, var.STR(mfn.out_field), fmsg)
+
+def gen_e2v_spmv_schedule(adj, rfunc, mfr, edge_mapping, out, out_size,
+                          out_mapping):
     """Generate e2v SPMV schedule.
 
     Parameters
     ----------
     inc : tuple (sparse matrix, utils.Index)
-    spmv_rfunc : list of builtin reducers
+    rfunc : list of builtin reducers
     mf : var.Var
         Variable for message frame.
     out : var.Var
         Variable for output reduced features.
     """
-    incmat, _ = inc
-    inc_var = var.SPMAT(incmat)
-    for rfn in spmv_rfunc:
-        ftmsg = ir.READ_COL(mfr, var.STR(rfn.msg_field))
-        ftdst = ir.SPMV(inc_var, ftmsg)
+    spmat = var.SPMAT(adj)
+    for rfn in rfunc:
+        ftdst = rfn(spmat, mfr, edge_mapping, out_mapping, out_size)
         ir.WRITE_COL_(out, var.STR(rfn.out_field), ftdst)
 
 def build_block_adj_matrix_graph(graph, block_id):
@@ -150,7 +118,7 @@ def build_block_adj_matrix_graph(graph, block_id):
         A index for data shuffling due to sparse format change. Return None
         if shuffle is not required.
     """
-    #TODO why is this constructed twice?
+    # TODO why is this constructed twice?
     _, shuffle_idx = graph.block_adjacency_matrix(block_id, F.cpu())
     shuffle_idx = utils.toindex(shuffle_idx) if shuffle_idx is not None else None
     return lambda ctx: graph.block_adjacency_matrix(block_id, ctx)[0], shuffle_idx
@@ -167,16 +135,14 @@ def build_adj_matrix_graph(graph):
     -------
     utils.CtxCachedObject
         Get be used to get adjacency matrix on the provided ctx.
-    utils.Index
-        A index for data shuffling due to sparse format change. Return None
-        if shuffle is not required.
     """
     gidx = graph._graph
-    # TODO Why invoking adjacency_matrix twice?
-    _, shuffle_idx = gidx.adjacency_matrix(False, F.cpu())
-    return lambda ctx: gidx.adjacency_matrix(False, ctx)[0], shuffle_idx
+    eid = gidx.adjacency_matrix(True)[2]
+    inv_eid = gidx.adjacency_matrix(False)[2]
+    return lambda ctx: (gidx.adjacency_matrix(True, ctx)[:2] +
+                        gidx.adjacency_matrix(False, ctx)[:2]), eid, inv_eid
 
-def _build_adj_matrix_index_uv(edges, reduce_nodes, num_sources):
+def _build_adj_matrix_index_uv(edge_tuple, num_nodes):
     """Build adj matrix index and shape using the given (u, v) edges.
 
     The matrix is of shape (len(reduce_nodes), n), where n is the number of nodes
@@ -191,9 +157,6 @@ def _build_adj_matrix_index_uv(edges, reduce_nodes, num_sources):
     ---------
     edges : tuple of utils.Index
         (u, v)
-    reduce_nodes : utils.Index
-        The nodes to reduce messages, which will be target dimension
-        of the adjmat. The nodes include unique(v) and zero-degree-nodes.
     num_sources : int
         The number of source nodes.
 
@@ -201,33 +164,34 @@ def _build_adj_matrix_index_uv(edges, reduce_nodes, num_sources):
     -------
     sparse index
         The sparse index.
-    tupe of int
+    tuple of int
         The dense shape.
     """
-    # TODO(minjie): add node frontier for this
-    _, old2new = utils.build_relabel_map(reduce_nodes, is_sorted=True)
-    u, v = edges
+    u, v, eid = edge_tuple
     u = u.tousertensor()
     v = v.tousertensor()
-    new_v = old2new[v]  # FIXME(minjie): no use []
-    n = num_sources
-    m = len(reduce_nodes)
-    row = F.unsqueeze(new_v, 0)
-    col = F.unsqueeze(u, 0)
-    idx = F.cat([row, col], dim=0)
-    return ('coo', idx), (m, n)
+    eid = eid.tousertensor()
+    u = F.zerocopy_to_numpy(u)
+    v = F.zerocopy_to_numpy(v)
+    eid = F.zerocopy_to_numpy(eid)
+    csr = sp.csr_matrix((eid, (u, v)), shape=(num_nodes, num_nodes))
+    inv_csr = sp.csr_matrix((eid, (v, u)), shape=(num_nodes, num_nodes))
+    spmat = (csr.indptr, csr.indices, csr.data)
+    inv_spmat = (inv_csr.indptr, inv_csr.indices, inv_csr.data)
+    return spmat, inv_spmat
 
-def build_adj_matrix_uv(edges, reduce_nodes, num_sources):
-    """Build adj matrix using the given (u, v) edges and target nodes.
 
-    The matrix is of shape (len(reduce_nodes), n), where n is the number of nodes
-    in the graph. Therefore, when doing SPMV, the src node data
-    should be all the node features.
+def build_adj_matrix_uv(edge_tuple, num_nodes):
+    """Build adj matrix using the given (u, v, eid) edges and target nodes.
+
+    The matrix is of shape (len(reduce_nodes), n), where n is the number of
+    nodes in the graph. Therefore, when doing SPMV, the src node data should be
+    all the node features.
 
     Parameters
     ---------
-    edges : tuple of utils.Index
-        (u, v)
+    edge_tuple : tuple of utils.Index
+        (u, v, eid)
     reduce_nodes : utils.Index
         The nodes to reduce messages, which will be target dimension
         of the adjmat. The nodes include unique(v) and zero-degree-nodes.
@@ -242,14 +206,17 @@ def build_adj_matrix_uv(edges, reduce_nodes, num_sources):
         A index for data shuffling due to sparse format change. Return None
         if shuffle is not required.
     """
-    sp_idx, shape = _build_adj_matrix_index_uv(edges, reduce_nodes, num_sources)
-    u, _ = edges
-    nnz = len(u)
-    # FIXME(minjie): data type
-    dat = F.ones((nnz,), dtype=F.float32, ctx=F.cpu())
-    mat, shuffle_idx = F.sparse_matrix(dat, sp_idx, shape)
-    shuffle_idx = utils.toindex(shuffle_idx) if shuffle_idx is not None else None
-    return utils.CtxCachedObject(lambda ctx: F.copy_to(mat, ctx)), shuffle_idx
+    u, v, eid = edge_tuple
+    spmat, inv_spmat = _build_adj_matrix_index_uv(edge_tuple, num_nodes)
+
+    def copy_to(ctx):
+        indptr = ndarray.array(spmat[0], ctx=ctx)
+        indices = ndarray.array(spmat[1], ctx=ctx)
+        inv_indptr = ndarray.array(inv_spmat[0], ctx=ctx)
+        inv_indices = ndarray.array(inv_spmat[1], ctx=ctx)
+        return indptr, indices, inv_indptr, inv_indices
+
+    return utils.CtxCachedObject(copy_to), spmat[2], inv_spmat[2]
 
 def build_block_inc_matrix_graph(graph, block_id):
     """Build incidence matrix.
@@ -273,6 +240,8 @@ def build_block_inc_matrix_graph(graph, block_id):
     # inc mat will not use data tensor so conversion index is not needed
     return lambda ctx: graph.block_incidence_matrix(block_id, 'in', ctx)[0], None
 
+
+'''
 def build_inc_matrix_graph(graph):
     """Build incidence matrix.
 
@@ -398,3 +367,5 @@ def build_inc_matrix_dst(dst, reduce_nodes):
     """
     eid = utils.toindex(F.arange(0, len(dst)))
     return build_inc_matrix_eid(len(eid), eid, dst, reduce_nodes)
+
+'''
