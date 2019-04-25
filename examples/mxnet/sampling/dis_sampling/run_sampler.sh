2ff8ecfbacce1e13b3ec3ec398fef8366e05e0fb
Chao Ma <mctt90@gmail.com>
2019-04-25 13:28:36 +0800

    update test_batch_size (#510)

--- examples/mxnet/sampling/dis_sampling/run_sampler.sh
+++ examples/mxnet/sampling/dis_sampling/run_sampler.sh
@@ -1,1 +1,1 @@
-DGLBACKEND=mxnet python3 gcn_ns_sampler.py --ip 127.0.0.1:2049 --num-sender=1 --dataset reddit-self-loop --num-neighbors 2 --batch-size 1000 --test-batch-size 500
+DGLBACKEND=mxnet python3 gcn_ns_sampler.py --ip 127.0.0.1:2049 --num-sender=1 --dataset reddit-self-loop --num-neighbors 3 --batch-size 1000 --test-batch-size 5000
