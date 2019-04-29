cc7d2d2176bba03e26bab90a47fd070c9c98397a
Minjie Wang <wmjlyjemaine@gmail.com>
2019-04-29 15:20:06 -0400

    disable cusparse

--- src/kernel/cuda/binary_reduce_sum.cu
+++ src/kernel/cuda/binary_reduce_sum.cu
@@ -3,6 +3,7 @@
 #include "../../runtime/cuda/cuda_common.h"
 #include "./binary_reduce_impl.cuh"
 #include "./backward_binary_reduce_impl.cuh"
+#include "./utils.cuh"
 
 using minigun::Csr;
 using minigun::advance::RuntimeConfig;
@@ -18,8 +19,8 @@ void CusparseCsrmm2(const RuntimeConfig& rtcfg, const Csr& csr, GData<float>* gd
   const int k = csr.row_offsets.length - 1;
   const int n = gdata->x_length;
   const int nnz = csr.column_indices.length;
-  float alpha = 1.0;
-  float beta = 0.0;
+  const float alpha = 1.0;
+  const float beta = 0.0;
   // device
   auto device = runtime::DeviceAPI::Get(rtcfg.ctx);
   auto* thr_entry = runtime::CUDAThreadEntry::ThreadLocal();
@@ -27,18 +28,24 @@ void CusparseCsrmm2(const RuntimeConfig& rtcfg, const Csr& csr, GData<float>* gd
   if (!thr_entry->cusparse_handle) {
     CUSPARSE_CALL(cusparseCreate(&(thr_entry->cusparse_handle)));
   }
+  // all one data array
+  float* valptr = static_cast<float*>(device->AllocWorkspace(rtcfg.ctx, nnz * sizeof(float)));
+  utils::Fill(rtcfg.stream, valptr, nnz, static_cast<float>(1.));
   cusparseMatDescr_t descr;
   CUSPARSE_CALL(cusparseCreateMatDescr(&descr));
   CUSPARSE_CALL(cusparseSetMatType(descr, CUSPARSE_MATRIX_TYPE_GENERAL));
   CUSPARSE_CALL(cusparseSetMatIndexBase(descr, CUSPARSE_INDEX_BASE_ZERO));
-  //cusparseScsrmm(
-      //thr_entry->cusparse_handle,
-      //CUSPARSE_OPERATION_NON_TRANSPOSE,
-      //CUSPARSE_OPERATION_TRANSPOSE,
-      //m, n, k, nnz, alpha,
-  LOG(FATAL) << "NOT IMPLEMENTED";
+  CUSPARSE_CALL(cusparseScsrmm2(
+      thr_entry->cusparse_handle,
+      CUSPARSE_OPERATION_NON_TRANSPOSE,
+      CUSPARSE_OPERATION_TRANSPOSE,
+      m, n, k, nnz, &alpha,
+      descr, valptr, csr.row_offsets.data, csr.column_indices.data,
+      gdata->lhs_data, n, &beta, gdata->out_data, m));
+  device->FreeWorkspace(rtcfg.ctx, valptr);
 }
 
+/*
 void CusparseCsrmm2(const RuntimeConfig& rtcfg, const Csr& csr, GData<double>* gdata) {
   LOG(FATAL) << "NOT IMPLEMENTED";
 }
@@ -82,6 +89,7 @@ void CallBinaryReduce<double, DirectId<kDLGPU, int64_t>,
     GData<double>* gdata) {
   CusparseCsrmm2(rtcfg, csr, gdata);
 }
+*/
 
 // generate definitions
 
