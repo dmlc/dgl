b594cc9ec61f35ede47593694320e6638e82aedc
Da Zheng <zhengda1936@gmail.com>
2019-05-08 19:16:56 -0700

    add concurrent test.

--- tests/mxnet/test_shared_mem_store.py
+++ tests/mxnet/test_shared_mem_store.py
@@ -97,6 +97,58 @@ def test_update_all():
     work_p1.join()
     work_p2.join()
 
+def server_func1(num_workers):
+    print("server starts")
+    np.random.seed(0)
+    csr = (spsp.random(10, 10, density=0.1, format='csr') != 0).astype(np.int64)
+
+    g = dgl.contrib.graph_store.create_graph_store_server(csr, "cgraph", "shared_mem",
+                                                          num_workers, False, edge_dir="in")
+    g.ndata['feat'] = mx.nd.zeros(shape=(g._graph.number_of_nodes(), 10))
+    g.run()
+
+def read_func(worker_id):
+    time.sleep(3)
+    print('reader runs: ' + str(worker_id))
+    g = dgl.contrib.graph_store.create_graph_from_store("cgraph", "shared_mem")
+    for i in range(100000):
+        idx = dgl.utils.toindex(np.random.randint(0, g.number_of_nodes(), size=10))
+        rows = g._node_frame._frame['feat'][idx].asnumpy()
+        for j in range(len(rows)):
+            assert np.all(rows[j] == rows[j][0])
+    print('reader ends: ' + str(worker_id))
+
+def write_func(worker_id):
+    time.sleep(3)
+    print('writer runs: ' + str(worker_id))
+    g = dgl.contrib.graph_store.create_graph_from_store("cgraph", "shared_mem")
+    rows = mx.nd.ones(shape=(10, 10))
+    for i in range(100000):
+        idx = dgl.utils.toindex(np.random.randint(0, g.number_of_nodes(), size=10))
+        rows = rows + random.randint(0, 100)
+        g._node_frame._frame['feat'].update(idx, rows, inplace=True)
+    print('writer ends: ' + str(worker_id))
+
+def test_concurrent():
+    serv_p = Process(target=server_func1, args=(4,))
+    work_p1 = Process(target=read_func, args=(0,))
+    work_p2 = Process(target=read_func, args=(1,))
+    work_p3 = Process(target=write_func, args=(2,))
+    work_p4 = Process(target=write_func, args=(3,))
+    serv_p.start()
+    work_p1.start()
+    work_p2.start()
+    work_p3.start()
+    work_p4.start()
+    work_p1.join()
+    work_p2.join()
+    work_p3.join()
+    work_p4.join()
+    print('wait for s')
+    serv_p.join()
+    print('end')
+
 if __name__ == '__main__':
     test_test_init()
     test_update_all()
+    test_concurrent()
