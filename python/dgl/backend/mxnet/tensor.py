98ddfe0a9e2a2196931c764b5aba1a991cf22436
Lingfan Yu <ly979@nyu.edu>
2019-05-16 22:06:39 +0000

    mxnet backend support

--- python/dgl/backend/mxnet/tensor.py
+++ python/dgl/backend/mxnet/tensor.py
@@ -4,6 +4,8 @@ import numpy as np
 import mxnet as mx
 import mxnet.ndarray as nd
 import numbers
+from ... import ndarray as dglnd
+from ... import kernel as K
 
 def data_type_dict():
     return {'float16' : np.float16,
@@ -241,3 +243,238 @@ def zerocopy_to_numpy(arr):
 def zerocopy_from_numpy(np_data):
     # NOTE: not zerocopy
     return nd.array(np_data, dtype=np_data.dtype)
+
+def zerocopy_to_dgl_ndarray_for_read(arr):
+    return dglnd.from_dlpack(arr.to_dlpack_for_read())
+
+def zerocopy_to_dgl_ndarray_for_write(arr):
+    return dglnd.from_dlpack(arr.to_dlpack_for_write())
+
+def zerocopy_from_dgl_ndarray(arr):
+    return nd.from_dlpack(arr.to_dlpack())
+
+
+class SrcOpEdgeReduce(mx.autograd.Function):
+    def __init__(self, reducer, binary_op, spmat, out_size, src_map, edge_map,
+                 out_map):
+        super(SrcOpEdgeReduce, self).__init__()
+        self.reducer = reducer
+        self.binary_op = binary_op
+        self.spmat = spmat
+        self.out_size = out_size
+        self.src_map = src_map
+        self.edge_map = edge_map
+        if reducer == "none":
+            self.forward_out_map = out_map[0]
+            self.backward_out_map = out_map[1]
+        else:
+            self.forward_out_map = out_map
+            self.backward_out_map = out_map
+
+    def forward(self, src_data, edge_data):
+        src_data_nd = zerocopy_to_dgl_ndarray_for_read(src_data)
+        edge_data_nd = zerocopy_to_dgl_ndarray_for_read(edge_data)
+        feat_shape = K.infer_binary_feature_shape(src_data_nd, edge_data_nd)
+        out_data = nd.empty((self.out_size,) + feat_shape,
+                            ctx=src_data.context, dtype=src_data.dtype)
+        out_data_nd = zerocopy_to_dgl_ndarray_for_write(out_data)
+        K.src_op_edge_reduce(
+            self.reducer, self.binary_op, self.spmat[0], self.spmat[1],
+            self.spmat[2], self.spmat[3], self.src_map, self.edge_map[0],
+            src_data_nd, edge_data_nd, self.forward_out_map, out_data_nd)
+        self.save_for_backward(src_data_nd, edge_data_nd, out_data_nd,
+                               feat_shape)
+        return out_data
+
+    def backward(self, grad_out):
+        src_data_nd, edge_data_nd, out_data_nd, feat_shape = self.saved_tensors
+        grad_out_nd = zerocopy_to_dgl_ndarray_for_read(grad_out)
+        grad_src = nd.empty((src_data_nd.shape[0],) + feat_shape,
+                            ctx=grad_out.context, dtype=grad_out.dtype)
+        K.backward_lhs_src_mul_edge_reduce(
+            self.reducer, self.binary_op, self.spmat[0], self.spmat[1],
+            self.spmat[2], self.spmat[3], self.src_map, self.edge_map[1],
+            self.backward_out_map, src_data_nd, edge_data_nd, out_data_nd,
+            grad_out_nd, zerocopy_to_dgl_ndarray_for_write(grad_src))
+        grad_src = _reduce_grad(grad_src, src_data_nd.shape)
+        grad_edge = nd.empty((edge_data_nd.shape[0],) + feat_shape,
+                             ctx=grad_out.context, dtype=grad_out.dtype)
+        K.backward_rhs_src_mul_edge_reduce(
+            self.reducer, self.binary_op, self.spmat[0], self.spmat[1],
+            self.spmat[2], self.spmat[3], self.src_map, self.edge_map[1],
+            self.backward_out_map, src_data_nd, edge_data_nd, out_data_nd,
+            grad_out_nd, zerocopy_to_dgl_ndarray_for_write(grad_edge))
+        grad_edge = _reduce_grad(grad_edge, edge_data_nd.shape)
+        return grad_src, grad_edge
+
+
+def src_op_edge_reduce(reducer, binary_op, spmat, src_data, edge_data,
+                       out_size, src_map, edge_map, out_map):
+    func = SrcOpEdgeReduce(reducer, binary_op, spmat, out_size, src_map,
+                           edge_map, out_map)
+    return func(src_data, edge_data)
+
+
+class SrcOpDstReduce(mx.autograd.Function):
+    def forward(self, reducer, binary_op, spmat, src_data, dst_data, out_size,
+                src_map, dst_map, out_map):
+        src_data_nd = zerocopy_to_dgl_ndarray_for_read(src_data)
+        dst_data_nd = zerocopy_to_dgl_ndarray_for_read(dst_data)
+        feat_shape = K.infer_binary_feature_shape(src_data_nd, dst_data_nd)
+        out_data = nd.empty((out_size,) + feat_shape, ctx=src_data.context,
+                            dtype=src_data.dtype)
+        out_data_nd = zerocopy_to_dgl_ndarray_for_write(out_data)
+        if reducer == "none":
+            forward_out_map = out_map[0]
+            backward_out_map = out_map[1]
+        else:
+            forward_out_map = out_map
+            backward_out_map = out_map
+        K.src_op_dst_reduce(
+            reducer, binary_op, spmat[0], spmat[1], spmat[2], spmat[3],
+            src_map, dst_map, src_data, dst_data, forward_out_map, out_data,
+            feat_shape)
+        self.backward_cache = (reducer, binary_op, spmat, src_map, dst_map,
+                               backward_out_map, src_data_nd, dst_data_nd,
+                               out_data_nd, feat_shape)
+        return zerocopy_from_dgl_ndarray(out_data)
+
+    def backward(self, grad_out):
+        reducer, binary_op, spmat, src_map, dst_map, backward_out_map, \
+            src_data_nd, dst_data_nd, out_data_nd, feat_shape \
+            = self.backward_cache
+        self.backward_cache = None
+        grad_src = None
+        grad_dst = None
+        grad_out_nd = zerocopy_to_dgl_ndarray_for_read(grad_out)
+        if self.needs_input_grad[3]:
+            grad_src = nd.empty((src_data_nd.shape[0],) + feat_shape,
+                                ctx=grad_out.context, dtype=grad_out.dtype)
+            K.backward_lhs_src_mul_dst_reduce(
+                reducer, binary_op, spmat[0], spmat[1], spmat[2], spmat[3],
+                src_map, dst_map, backward_out_map, src_data_nd, dst_data_nd,
+                out_data_nd, grad_out_nd, grad_src)
+            grad_src = zerocopy_from_dgl_ndarray(grad_src)
+            grad_src = _reduce_grad(grad_src, src_data_nd.shape)
+        if self.needs_input_grad[4]:
+            grad_dst = nd.empty((dst_data_nd.shape[0],) + feat_shape,
+                                ctx=grad_out.context, dtype=grad_out.dtype)
+            K.backward_rhs_src_mul_dst_reduce(
+                reducer, binary_op, spmat[0], spmat[1], spmat[2], spmat[3],
+                src_map, dst_map, backward_out_map, src_data_nd, dst_data_nd,
+                out_data_nd, grad_out_nd, grad_dst)
+            grad_dst = zerocopy_from_dgl_ndarray(grad_dst)
+            grad_dst = _reduce_grad(grad_dst, dst_data_nd.shape)
+        return None, None, None, grad_src, grad_dst, None, None, None, None
+
+
+def src_op_dst_reduce(reducer, binary_op, spmat, src_data, dst_data, out_size,
+                      src_map, dst_map, out_map):
+    func = SrcOpDstReduce()
+    return func(reducer, binary_op, spmat, src_data, dst_data, out_size,
+                src_map, dst_map, out_map)
+
+
+class CopySrcReduce(mx.autograd.Function):
+    def __init__(self, reducer, spmat, out_size, src_map, out_map):
+        super(CopySrcReduce, self).__init__()
+        self.reducer = reducer
+        self.spmat = spmat
+        self.out_size = out_size
+        self.src_map = src_map
+        if reducer == "none":
+            self.forward_out_map = out_map[0]
+            self.backward_out_map = out_map[1]
+        else:
+            self.forward_out_map = out_map
+            self.backward_out_map = out_map
+
+    def forward(self, src_data):
+        feat_shape = src_data.shape[1:]
+        out_data = nd.empty((self.out_size,) + feat_shape,
+                            ctx=src_data.context, dtype=src_data.dtype)
+        src_data_nd = zerocopy_to_dgl_ndarray_for_read(src_data)
+        out_data_nd = zerocopy_to_dgl_ndarray_for_write(out_data)
+        K.copy_src_reduce(
+            self.reducer, self.spmat[0], self.spmat[1], self.spmat[2],
+            self.spmat[3], self.src_map, src_data_nd, self.forward_out_map,
+            out_data_nd)
+        self.save_for_backward(src_data_nd, out_data_nd)
+        return out_data
+
+    def backward(self, grad_out):
+        src_data_nd, out_data_nd = self.saved_tensors
+        grad_out_nd = zerocopy_to_dgl_ndarray_for_read(grad_out)
+        grad_src = nd.empty(src_data_nd.shape, ctx=grad_out.context,
+                            dtype=grad_out.dtype)
+        K.backward_copy_src_reduce(
+            self.reducer, self.spmat[0], self.spmat[1], self.spmat[2],
+            self.spmat[3], self.src_map, self.backward_out_map,
+            src_data_nd, out_data_nd, grad_out_nd,
+            zerocopy_to_dgl_ndarray_for_write(grad_src))
+        return grad_src
+
+
+def copy_src_reduce(reducer, spmat, src_data, out_size, src_map, out_map):
+    func = CopySrcReduce(reducer, spmat, out_size, src_map, out_map)
+    return func(src_data)
+
+
+class CopyEdgeReduce(mx.autograd.Function):
+    def __init__(self, reducer, spmat, out_size, edge_map, out_map):
+        super(CopyEdgeReduce, self).__init__()
+        self.reducer = reducer
+        self.spmat = spmat
+        self.out_size = out_size
+        self.edge_map = edge_map
+        if reducer == "none":
+            self.forward_out_map = out_map[0]
+            self.backward_out_map = out_map[1]
+        else:
+            self.forward_out_map = out_map
+            self.backward_out_map = out_map
+
+    def forward(self, edge_data):
+        feat_shape = edge_data.shape[1:]
+        out_data = nd.empty((self.out_size,) + feat_shape,
+                            ctx=edge_data.context, dtype=edge_data.dtype)
+        edge_data_nd = zerocopy_to_dgl_ndarray_for_read(edge_data)
+        out_data_nd = zerocopy_to_dgl_ndarray_for_write(out_data)
+        K.copy_edge_reduce(
+            self.reducer, self.spmat[0], self.spmat[1], self.spmat[2],
+            self.spmat[3], self.edge_map[0], edge_data_nd,
+            self.forward_out_map, out_data_nd)
+        self.save_for_backward(edge_data_nd, out_data_nd)
+        return out_data
+
+    def backward(self, grad_out):
+        edge_data_nd, out_data_nd = self.saved_tensors
+        grad_out_nd = zerocopy_to_dgl_ndarray_for_read(grad_out)
+        grad_edge = nd.empty(edge_data_nd.shape, ctx=grad_out.context,
+                             dtype=grad_out.dtype)
+        K.backward_copy_edge_reduce(
+            self.reducer, self.spmat[0], self.spmat[1], self.spmat[2],
+            self.spmat[3], self.edge_map[1], self.backward_out_map,
+            edge_data_nd, out_data_nd, grad_out_nd,
+            zerocopy_to_dgl_ndarray_for_write(grad_edge))
+        return grad_edge
+
+
+def copy_edge_reduce(reducer, spmat, edge_data, out_size, edge_map, out_map):
+    func = CopyEdgeReduce(reducer, spmat, out_size, edge_map, out_map)
+    return func(edge_data)
+
+
+def _reduce_grad(grad, shape):
+    grad_shape = grad.shape[1:]
+    src_shape = shape[1:]
+    if src_shape == grad_shape:
+        # no need to reduce
+        return grad
+    num_to_squeeze = len(grad_shape) - len(src_shape)
+    # pad src_shape
+    src_shape = (1,) * num_to_squeeze + src_shape
+    reduce_idx = np.nonzero(np.array(grad_shape) - np.array(src_shape))[0]
+    reduce_idx += 1  # skip batch dim
+    grad = grad.sum(axis=tuple(reduce_idx), keepdims=True)
+    return grad.reshape(shape)
