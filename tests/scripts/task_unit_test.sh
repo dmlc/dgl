e7d4c96c22372e3e01c7a335328dcb3b37acea3b
Minjie Wang <wmjlyjemaine@gmail.com>
2019-05-21 13:22:40 -0400

    poke ci

--- tests/scripts/task_unit_test.sh
+++ tests/scripts/task_unit_test.sh
@@ -17,6 +17,11 @@ fi
 BACKEND=$1
 export PYTHONPATH=tests:$PYTHONPATH
 
+echo $PWD
+echo $DGL_LIBRARY_PATH
+ls -lh .
+ls -lh ../../build/
+
 python3 -m nose -v --with-xunit tests/$BACKEND || fail "backend-specific"
 python3 -m nose -v --with-xunit tests/graph_index || fail "graph_index"
 python3 -m nose -v --with-xunit tests/compute || fail "compute"
