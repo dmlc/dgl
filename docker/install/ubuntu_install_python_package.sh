c64405b1f6102fd950bf2fee55b7b24f41f9295f
Minjie Wang <wmjlyjemaine@gmail.com>
2019-05-27 17:04:46 -0400

    [Docker] update docker image (#575)
    * update docker image
    
    * specify lint version
    
    * rm torch import from unified tests

--- docker/install/ubuntu_install_python_package.sh
+++ docker/install/ubuntu_install_python_package.sh
@@ -1,7 +1,3 @@
 # install libraries for python package on ubuntu
 pip2 install nose numpy cython scipy networkx matplotlib nltk requests[security]
 pip3 install nose numpy cython scipy networkx matplotlib nltk requests[security]
-
-# install DL Framework
-pip2 install torch torchvision
-pip3 install torch torchvision
