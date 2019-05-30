bda63ee3f66e775a36c5c081e73aa0ef8100fe2d
Da Zheng <zhengda1936@gmail.com>
2019-05-29 18:24:09 -0700

    try to fix multi-processing test hangs when it fails.

--- tests/mxnet/test_shared_mem_store.py
+++ tests/mxnet/test_shared_mem_store.py
@@ -9,6 +9,7 @@ import mxnet as mx
 import backend as F
 import unittest
 import dgl.function as fn
+import traceback
 
 num_nodes = 100
 num_edges = int(num_nodes * num_nodes * 0.1)
@@ -25,7 +26,7 @@ def check_array_shared_memory(g, worker_id, arrays):
         for i, arr in enumerate(arrays):
             assert np.all(arr[0].asnumpy() == i)
 
-def check_init_func(worker_id, graph_name):
+def _check_init_func(worker_id, graph_name):
     time.sleep(3)
     print("worker starts")
     np.random.seed(0)
@@ -61,6 +62,15 @@ def check_init_func(worker_id, graph_name):
 
     g.destroy()
 
+def check_init_func(worker_id, graph_name, return_dict):
+    try:
+        _check_init_func(worker_id, graph_name)
+        return_dict[worker_id] = 0
+    except Exception as e:
+        return_dict[worker_id] = -1
+        print(e)
+        traceback.print_exc()
+
 def server_func(num_workers, graph_name):
     print("server starts")
     np.random.seed(0)
@@ -75,18 +85,22 @@ def server_func(num_workers, graph_name):
     g.run()
 
 def test_init():
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
     serv_p = Process(target=server_func, args=(2, 'test_graph1'))
-    work_p1 = Process(target=check_init_func, args=(0, 'test_graph1'))
-    work_p2 = Process(target=check_init_func, args=(1, 'test_graph1'))
+    work_p1 = Process(target=check_init_func, args=(0, 'test_graph1', return_dict))
+    work_p2 = Process(target=check_init_func, args=(1, 'test_graph1', return_dict))
     serv_p.start()
     work_p1.start()
     work_p2.start()
     serv_p.join()
     work_p1.join()
     work_p2.join()
+    for worker_id in return_dict:
+        assert return_dict[worker_id] == 0, "worker %d fails" % worker_id
 
 
-def check_compute_func(worker_id, graph_name):
+def _check_compute_func(worker_id, graph_name):
     time.sleep(3)
     print("worker starts")
     for _ in range(10):
@@ -131,16 +145,29 @@ def check_compute_func(worker_id, graph_name):
 
     g.destroy()
 
+def check_compute_func(worker_id, graph_name, return_dict):
+    try:
+        _check_compute_func(worker_id, graph_name)
+        return_dict[worker_id] = 0
+    except Exception as e:
+        return_dict[worker_id] = -1
+        print(e)
+        traceback.print_exc()
+
 def test_compute():
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
     serv_p = Process(target=server_func, args=(2, 'test_graph3'))
-    work_p1 = Process(target=check_compute_func, args=(0, 'test_graph3'))
-    work_p2 = Process(target=check_compute_func, args=(1, 'test_graph3'))
+    work_p1 = Process(target=check_compute_func, args=(0, 'test_graph3', return_dict))
+    work_p2 = Process(target=check_compute_func, args=(1, 'test_graph3', return_dict))
     serv_p.start()
     work_p1.start()
     work_p2.start()
     serv_p.join()
     work_p1.join()
     work_p2.join()
+    for worker_id in return_dict:
+        assert return_dict[worker_id] == 0, "worker %d fails" % worker_id
 
 if __name__ == '__main__':
     test_init()
