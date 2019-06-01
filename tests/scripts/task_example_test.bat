0718dd0cb3339df93e60448b727c550933f18617
Quan Gan <coin2028@hotmail.com>
2019-06-01 15:14:23 +0800

    missed dir

--- tests/scripts/task_example_test.bat
+++ tests/scripts/task_example_test.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
 
+SET GCN_EXAMPLE_DIR=.\examples\pytorch
+
 IF x%1x==xx (
 	ECHO Must supply CPU or GPU
 	GOTO :FAIL
@@ -19,7 +21,7 @@ SET DGL_LIBRARY_PATH=!CD!\build
 SET PYTHONPATH=!CD!\python;!PYTHONPATH!
 SET DGL_DOWNLOAD_DIR=!CD!
 
-PUSHD ..\..\examples\pytorch
+PUSHD !GCN_EXAMPLE_DIR!
 python pagerank.py || GOTO :FAIL
 python gcn\gcn.py --dataset cora --gpu !DEV! || GOTO :FAIL
 python gcn\gcn_spmv.py --dataset cora --gpu !DEV! || GOTO :FAIL
