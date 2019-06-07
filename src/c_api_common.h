dbe45203f83979940e45777203e36a928d25f18a
Wang <wanminji@88e9fe702bad.ant.amazon.com>
2019-06-07 01:38:14 -0400

    Fix mac compilation

--- src/c_api_common.h
+++ src/c_api_common.h
@@ -12,7 +12,6 @@
 #include <algorithm>
 #include <vector>
 
-namespace {
 /*! \brief Check whether two device contexts are the same.*/
 inline bool operator == (const DLContext& ctx1, const DLContext& ctx2) {
   return ctx1.device_type == ctx2.device_type && ctx1.device_id == ctx2.device_id;
@@ -22,7 +21,6 @@ inline bool operator == (const DLContext& ctx1, const DLContext& ctx2) {
 inline std::ostream& operator << (std::ostream& os, const DLContext& ctx) {
   return os << "" << ctx.device_type << ":" << ctx.device_id << "";
 }
-}  // namespace
 
 namespace dgl {
 
