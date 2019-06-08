af58dd1011dc5d925876cdd36b7afdd61617097e
HQ01 <qh384@nyu.edu>
2019-06-08 11:24:04 +0800

    formatting

--- tests/compute/test_to_device.py
+++ tests/compute/test_to_device.py
@@ -3,8 +3,8 @@ import backend as F
 
 def test_to_device():
     g = dgl.DGLGraph()
-    g.add_nodes(5, {'h': F.ones((5,2))})
-    g.add_edges([0,1],[1,2], {'m' : F.ones((2,2))})
+    g.add_nodes(5, {'h' : F.ones((5,2))})
+    g.add_edges([0,1], [1,2], {'m' : F.ones((2,2))})
     if F.is_cuda_available():
         g.to(F.cuda())
 
