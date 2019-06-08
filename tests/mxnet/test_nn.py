8cdd6d1deb49608cf886d6b7014d634a89a66383
Lingfan Yu <ly979@nyu.edu>
2019-06-08 07:51:29 +0000

    fix unittest

--- tests/mxnet/test_nn.py
+++ tests/mxnet/test_nn.py
@@ -56,5 +56,22 @@ def test_graph_conv():
     h1 = conv(h0, g)
     assert "_gconv_feat" in g.ndata
 
+def uniform_attention(g, shape):
+    a = mx.nd.ones(shape)
+    target_shape = (g.number_of_edges(),) + (1,) * (len(shape) - 1)
+    return a / g.in_degrees(g.edges()[1]).view(target_shape).float()
+
+def test_edge_softmax():
+    # Basic
+    g = dgl.DGLGraph(nx.path_graph(3))
+    edata = mx.nd.ones(g.number_of_edges(), 1)
+    a = nn.edge_softmax(g, edata)
+    assert np.allclose(a, uniform_attention(g, a.shape))
+
+    # Test higher dimension case
+    edata = mx.nd.ones(g.number_of_edges(), 3, 1)
+    a = nn.edge_softmax(g, edata)
+    assert np.allclose(a, uniform_attention(g, a.shape))
+
 if __name__ == '__main__':
     test_graph_conv()
