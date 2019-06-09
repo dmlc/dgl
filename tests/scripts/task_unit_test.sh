292add371bf39ec7fb13a7050bd02571249dd4f9
Ubuntu <ubuntu@ip-172-31-16-168.us-east-2.compute.internal>
2019-06-09 19:07:54 +0000

    fix shared-memory test.

--- tests/scripts/task_unit_test.sh
+++ tests/scripts/task_unit_test.sh
@@ -23,3 +23,5 @@ export DGL_DOWNLOAD_DIR=${PWD}
 python3 -m nose -v --with-xunit tests/compute || fail "compute"
 python3 -m nose -v --with-xunit tests/graph_index || fail "graph_index"
 python3 -m nose -v --with-xunit tests/$DGLBACKEND || fail "backend-specific"
+export OMP_NUM_THREADS=1
+python3 -m nose -v --with-xunit tests/distributed || fail "distributed"
