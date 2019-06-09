db2a856ac924e5e4bedbaa6b6431b05a4c13a23c
Minjie Wang <wmjlyjemaine@gmail.com>
2019-06-08 21:33:28 -0400

    fix mxnet nn test; relax criteria for prod reducer

--- tests/mxnet/test_nn.py
+++ tests/mxnet/test_nn.py
@@ -59,19 +59,22 @@ def test_graph_conv():
 def uniform_attention(g, shape):
     a = mx.nd.ones(shape)
     target_shape = (g.number_of_edges(),) + (1,) * (len(shape) - 1)
-    return a / g.in_degrees(g.edges()[1]).view(target_shape).float()
+    return a / g.in_degrees(g.edges()[1]).reshape(target_shape).astype('float32')
 
 def test_edge_softmax():
     # Basic
     g = dgl.DGLGraph(nx.path_graph(3))
     edata = mx.nd.ones((g.number_of_edges(), 1))
     a = nn.edge_softmax(g, edata)
-    assert np.allclose(a, uniform_attention(g, a.shape))
+    assert np.allclose(a.asnumpy(), uniform_attention(g, a.shape).asnumpy(),
+            1e-4, 1e-4)
 
     # Test higher dimension case
     edata = mx.nd.ones((g.number_of_edges(), 3, 1))
     a = nn.edge_softmax(g, edata)
-    assert np.allclose(a, uniform_attention(g, a.shape))
+    assert np.allclose(a.asnumpy(), uniform_attention(g, a.shape).asnumpy(),
+            1e-4, 1e-4)
 
 if __name__ == '__main__':
     test_graph_conv()
+    test_edge_softmax()
