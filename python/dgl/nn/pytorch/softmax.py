f3a54710dc2306507a196c72fe4862d6069948b3
Lingfan Yu <ly979@nyu.edu>
2019-06-10 21:24:17 +0000

    fix gat memory increase bug

--- python/dgl/nn/pytorch/softmax.py
+++ python/dgl/nn/pytorch/softmax.py
@@ -62,6 +62,7 @@ class EdgeSoftmax(th.autograd.Function):
         return grad_score.data
         """
         g, out = ctx.backward_cache
+        ctx.backward_cache = None
         out_name = utils.get_edata_name(g, 'out')
         accum_name = utils.get_ndata_name(g, 'accum')
         grad_score_name = utils.get_edata_name(g, 'grad_score')
