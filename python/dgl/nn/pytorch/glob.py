8514f78b650c9192428623b12d44c293cbb0ea78
yzh119 <expye@outlook.com>
2019-07-05 20:22:31 +0800

    upd

--- python/dgl/nn/pytorch/glob.py
+++ python/dgl/nn/pytorch/glob.py
@@ -2,8 +2,10 @@
 # pylint: disable= no-member, arguments-differ
 import torch as th
 import torch.nn as nn
-from torch.nn import init
+import numpy as np
 
+from torch.nn import init
+from .softmax import edge_softmax
 from ... import function as fn, BatchedDGLGraph
 from ...utils import get_ndata_name, get_edata_name
 from ...batched_graph import sum_nodes, mean_nodes, max_nodes, broadcast_nodes, softmax_nodes, topk_nodes
@@ -55,7 +57,7 @@ class MaxPooling(nn.Module):
 
 
 class SortPooling(nn.Module):
-    r"""Apply sort pooling (from paper "An End-to-End Deep Learning Architecture
+    r"""Apply sort pooling (f"An End-to-End Deep Learning Architecture
     for Graph Classification") over the graph.
     """
     _feat_name = '_gpool_sort'
@@ -85,8 +87,10 @@ class GlobAttnPooling(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        # TODO(zihao): reset parameters of gate_nn and nn
-        pass
+        for p in self.gate_nn:
+            init.xavier_uniform_(p)
+        for p in self.nn:
+            init.xavier_uniform_(p)
 
     def forward(self, feat, graph):
         feat = feat.unsqueeze(-1) if feat.dim() == 1 else feat
@@ -107,7 +111,7 @@ class GlobAttnPooling(nn.Module):
 
 
 class Set2Set(nn.Module):
-    r"""Apply Set2Set (from paper "Order Matters: Sequence to sequence for sets") over the graph.
+    r"""Apply Set2Set (f"Order Matters: Sequence to sequence for sets") over the graph.
     """
     _score_name = '_gpool_s2s_score'
     _readout_name = '_gpool_s2s_readout'
@@ -121,8 +125,8 @@ class Set2Set(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        # TODO(zihao): finish this
-        pass
+        for p in self.lstm.parameters():
+            init.xavier_uniform_(p)
 
     def forward(self, feat, graph):
         batch_size = 1
@@ -159,3 +163,117 @@ class Set2Set(nn.Module):
         summary = 'input_dim={input_dim}, out_dim={out_dim}' +\
             'n_iters={n_iters}, n_layers={n_layers}'
         return summary.format(**self.__dict__)
+
+
+class MultiHeadAttention(nn.Module):
+    _query_name = '_gpool_mha_query'
+    _key_name = '_gpool_mha_key'
+    _value_name = '_gpool_mha_value'
+    _score_name = '_gpool_mha_score'
+    _att_name = '_gpool_mha_att'
+    _out_name = '_gpool_mha_out'
+    _feat_name = '_gpool_mha_feat'
+    def __init__(self, d_model, num_heads, d_head, d_ff, dropouth=0., dropouta=0.):
+        super(MultiHeadAttention, self).__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_head = d_head
+        self.d_ff = d_ff
+        self.W_q = nn.Linear(d_model, num_heads * d_head, bias=False)
+        self.W_k = nn.Linear(d_model, num_heads * d_head, bias=False)
+        self.W_v = nn.Linear(d_model, num_heads * d_head, bias=False)
+        self.W_o = nn.Linear(num_heads * d_head, d_model, bias=False)
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, d_ff),
+            nn.ReLU(),
+            nn.Dropout(dropouth),
+            nn.Linear(d_ff, d_model)
+        )
+        self.droph = nn.Dropout(dropouth)
+        self.dropa = nn.Dropout(dropouta)
+        self.norm_in = nn.LayerNorm(d_model)
+        self.norm_inter = nn.LayerNorm(d_model)
+
+    def forward(self, graph, q_feat, kv_feat, q_nids, kv_nids):
+        feat_name = get_ndata_name(graph, self._feat_name)
+        query_name = get_ndata_name(graph, self._query_name)
+        key_name = get_ndata_name(graph, self._key_name)
+        value_name = get_ndata_name(graph, self._value_name)
+        score_name = get_ndata_name(graph, self._score_name)
+        att_name = get_ndata_name(graph, self._att_name)
+        out_name = get_ndata_name(graph, self._out_name)
+
+        # Copy q_feat and kv_feat to graph data frame
+        graph.ndata[q_nids][feat_name] = q_feat
+        graph.ndata[kv_nids][feat_name] = kv_feat
+
+        # Compute queries, keys and values.
+        graph.nodes[q_nids][query_name] =\
+            self.W_q(graph.nodes[q_nids][feat_name]).view(-1, self.num_heads, self.d_head)
+        graph.nodes[kv_nids][key_name] =\
+            self.W_k(graph.nodes[kv_nids][feat_name]).view(-1, self.num_heads, self.d_head)
+        graph.nodes[kv_nids][value_name] =\
+            self.W_v(graph.nodes[kv_nids][feat_name]).view(-1, self.num_heads, self.d_head)
+
+        # Free node features.
+        graph.ndata.pop(feat_name)
+
+        # Compute attention score.
+        graph.apply_edges(fn.u_mul_v(key_name, query_name, score_name))
+        e = graph.edata.pop(score_name).sum(dim=-1, keepdim=True) / np.sqrt(self.d_head) # Attention & Free score field.
+        graph.edata[att_name] = self.dropa(edge_softmax(graph, e))
+        graph.pull(q_nids,
+                   fn.u_mul_e(value_name, att_name, 'm'),
+                   fn.sum('m', out_name))
+        sa = self.W_o(graph.nodes[q_nids][out_name].view(-1, self.num_heads * self.d_head))
+        feat = self.norm_in(q_feat + sa)
+
+        # Free queries, keys, values, outputs and attention weights.
+        graph.ndata.pop(query_name)
+        graph.ndata.pop(key_name)
+        graph.ndata.pop(value_name)
+        graph.ndata.pop(out_name)
+        graph.edata.pop(att_name)
+
+        # Position-wise Feed Forward Network
+        feat = self.norm_inter(feat + self.ffn(feat))
+
+        return feat
+
+
+class SetAttentionBlock(nn.Module):
+    def __init__(self):
+        super(SetAttentionBlock, self).__init__()
+        pass
+
+class InducedSetAttentionBlock(nn.Module):
+    def __init__(self):
+        super(InducedSetAttentionBlock, self).__init__()
+        pass
+
+class MHAPooling(nn.Module):
+    def __init__(self):
+        super(MHAPooling, self).__init__()
+        pass
+
+class STEncoder(nn.Module):
+    def __init__(self):
+        super(STEncoder, self).__init__()
+        pass
+
+class STDecoder(nn.Module):
+    def __init__(self):
+        super(STDecoder, self).__init__()
+        pass
+
+class SetTransformer(nn.Module):
+    r"""Apply Set Transformer(f""Set Transformer: A Framework for Attention-based
+    Permutation-Invariant Neural Networks") over the graph.
+    """
+    def __init__(self):
+        pass
+
+    def forward(self, feat, graph):
+        pass
+
+
---
+++ python/dgl/nn/utils.py
@@ -0,0 +1,65 @@
+"""utilities used in dgl.nn modules"""
+# pylint: disable= no-member, arguments-differ
+
+from ..graph import DGLGraph
+from ..batched_graph import BatchedDGLGraph, batch
+from ..graph_index import from_csr
+
+import numpy as np
+
+__all__ = ['_create_fully_connected_graph', '_create_bipartite_graph', '_create_batched_graph_from_num_nodes']
+
+def _create_fully_connected_graph(graph):
+    if isinstance(graph, BatchedDGLGraph):
+        gs = []
+        for n_nodes in graph.batch_num_nodes:
+            indptr = np.arange(0, n_nodes * n_nodes + 1, n_nodes)
+            indices = np.tile(np.arange(n_nodes), n_nodes)
+            gs.append(
+                DGLGraph(from_csr(indptr, indices, False, 'out'), readonly=True)
+            )
+        return batch(gs)
+    else:
+        n_nodes = graph.number_of_nodes()
+        indptr = np.arange(0, n_nodes * n_nodes + 1, n_nodes)
+        indices = np.tile(np.arange(n_nodes), n_nodes)
+        return DGLGraph(from_csr(indptr, indices, False, 'out'), readonly=True)
+
+def _create_bipartite_graph(graph_u, graph_v):
+    if isinstance(graph_u, BatchedDGLGraph):
+        gs = []
+        if graph_u.batch_size != graph_v.batch_size:
+            raise KeyError('Batch size of graph_u does not equal that of graph_v.')
+        for n_nodes_u, n_nodes_v in zip(graph_u.batch_num_nodes, graph_v.batch_num_nodes):
+            indptr = np.concatenate([
+                np.arange(0, n_nodes_u * n_nodes_v + 1, n_nodes_v),
+                np.full(n_nodes_v, n_nodes_u * n_nodes_v, dtype=np.int64)
+            ])
+            indices = np.tile(np.arange(n_nodes_v), n_nodes_u)
+            gs.append(
+                DGLGraph(from_csr(indptr, indices, False, 'out'), readonly=True)
+            )
+        return batch(gs)
+    else:
+        n_nodes_u = graph_u.number_of_nodes()
+        n_nodes_v = graph_v.number_of_nodes()
+        indptr = np.concatenate([
+            np.arange(0, n_nodes_u * n_nodes_v + 1, n_nodes_v),
+            np.full(n_nodes_v, n_nodes_u * n_nodes_v, )
+        ])
+        indices = np.tile(np.arange(n_nodes_v), n_nodes_u)
+        return DGLGraph(from_csr(indptr, indices, False, 'out'), readonly=True)
+
+def _create_batched_graph_from_num_nodes(n_nodes_arr):
+    gs = []
+    for n_nodes in n_nodes_arr:
+        indptr = np.zeros(n_nodes + 1, dtype=np.int64)
+        indices = np.array([])
+        gs.append(
+            DGLGraph(from_csr(indptr, indices, False, 'out'), readonly=True)
+        )
+    if len(gs) == 0:
+        return gs[0]
+    return batch(gs)
+
+
