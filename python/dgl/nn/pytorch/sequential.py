a7200d2431f2401dc3554aca555761b0434fa6ef
yzh119 <expye@outlook.com>
2019-07-08 19:51:34 +0800

    upd

--- python/dgl/nn/pytorch/sequential.py
+++ python/dgl/nn/pytorch/sequential.py
@@ -10,23 +10,12 @@ class DiffPooling(nn.Module):
     r""" Differentiable Pooling (paper: Hierarchical Graph Representation Learning with
     Differentiable Pooling) layer.
     """
-    def __init__(self, k, gnn):
+    def __init__(self, k, gnn, dim_model):
         super(DiffPooling, self).__init__()
         self.k = k
         self.gnn = gnn
+        self.W_s = nn.Linear(dim_model, k) # soft assignment
 
     def forward(self, feat, graph):
-        # TODO(zihao): finish this
         pass
 
-
-class TopKPooling(nn.Module):
-    r"""Apply Top-K Pooling
-    """
-    def __init__(self, k):
-        super(TopKPooling, self).__init__()
-        self.k = k
-
-    def forward(self, *input):
-        # TODO(zihao): finish this
-        pass
