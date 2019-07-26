f994ab7dfa495a12de990a43ab9b6d9ae367286c
Minjie Wang <wmjlyjemaine@gmail.com>
2019-07-26 13:12:52 -0400

    poke ci

--- src/graph/bipartite.cc
+++ src/graph/bipartite.cc
@@ -15,6 +15,7 @@ namespace {
 inline GraphPtr CreateBipartiteMetaGraph() {
   static IdArray row = aten::VecToIdArray(std::vector<int64_t>({Bipartite::kSrcVType}));
   static IdArray col = aten::VecToIdArray(std::vector<int64_t>({Bipartite::kDstVType}));
+  LOG(INFO) << "row size: " << row->shape[0] << " col size: " << col->shape[0];
   static GraphPtr g = ImmutableGraph::CreateFromCOO(2, row, col);
   return g;
 }
