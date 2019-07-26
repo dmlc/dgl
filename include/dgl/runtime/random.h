3dc8c2df44ba0295ac6928ef6509ea8e322d9d79
Quan Gan <coin2028@hotmail.com>
2019-07-26 14:24:51 +0800

    use explicit instantiation instead of inlining

--- include/dgl/runtime/random.h
+++ include/dgl/runtime/random.h
@@ -28,21 +28,14 @@ class Random {
    * \brief Set the seed of the DGL C random number generator.
    * \param seed The seed.
    */
-  static void SetSeed(int seed) {
-    GetInstance().Seed(seed);
-  }
+  static void SetSeed(int seed);
 
   /*!
    * \brief Generate a random integer uniformly from 0 (inclusive) to upper (exclusive).
    * \param upper The upper bound (exclusive)
    */
   template<typename IntType>
-  static IntType RandInt(IntType upper) {
-    CHECK_LT(0, upper);
-    auto *rng = GetInstance().Get(omp_get_thread_num());
-    std::uniform_int_distribution<IntType> dist(0, upper - 1);
-    return dist(*rng);
-  }
+  static IntType RandInt(IntType upper);
 
   /*!
    * \brief Generate a random integer uniformly from lower (inclusive) to upper (exclusive).
@@ -50,22 +43,13 @@ class Random {
    * \param upper The upper bound (exclusive)
    */
   template<typename IntType>
-  static IntType RandInt(IntType lower, IntType upper) {
-    CHECK_LT(lower, upper);
-    auto *rng = GetInstance().Get(omp_get_thread_num());
-    std::uniform_int_distribution<IntType> dist(lower, upper - 1);
-    return dist(*rng);
-  }
+  static IntType RandInt(IntType lower, IntType upper);
 
   /*!
    * \brief Generate a random float uniformly from 0 (inclusive) to 1 (exclusive).
    */
   template<typename FloatType>
-  static FloatType Uniform() {
-    auto *rng = GetInstance().Get(omp_get_thread_num());
-    std::uniform_real_distribution<FloatType> dist(0.f, 1.f);
-    return dist(*rng);
-  }
+  static FloatType Uniform();
 
   /*!
    * \brief Generate a random float uniformly from lower (inclusive) to upper (exclusive).
@@ -73,45 +57,23 @@ class Random {
    * \param upper The upper bound (exclusive)
    */
   template<typename FloatType>
-  static FloatType Uniform(FloatType lower, FloatType upper) {
-    CHECK_LT(lower, upper);
-    auto *rng = GetInstance().Get(omp_get_thread_num());
-    std::uniform_real_distribution<FloatType> dist(lower, upper);
-    return dist(*rng);
-  }
+  static FloatType Uniform(FloatType lower, FloatType upper);
 
  private:
   /*! \brief Private constructor that initializes seed by time */
-  Random() {
-    Resize(omp_get_max_threads());
-    Seed(std::chrono::system_clock::now().time_since_epoch().count());
-  }
+  Random();
 
   /*! \brief Get the RNG for thread ID i */
-  std::mt19937_64 *Get(int i) {
-    return &rngs_[i];
-  }
+  std::mt19937_64 *Get(int i);
 
   /*! \brief Get the singleton instance */
-  static Random &GetInstance() {
-    static Random random;
-    return random;
-  }
+  static Random &GetInstance();
 
   /*! \brief Set the seed of meta RNG, then reinitialize the thread-specific RNGs */
-  void Seed(int seed) {
-    meta_rng_.seed(seed);
-    for (auto &rng : rngs_)
-      rng.seed(meta_rng_());
-  }
+  void Seed(int seed);
 
   /*! \brief Resize the RNG array */
-  void Resize(int size) {
-    if (size <= rngs_.size())
-      return;
-    for (int i = rngs_.size(); i < size; ++i)
-      rngs_.push_back(std::mt19937_64(meta_rng_()));
-  }
+  void Resize(int size);
 
   /*! \brief Meta RNG that seeds the thread-specific RNGs */
   std::mt19937 meta_rng_;
--- src/runtime/random.cc
+++ src/runtime/random.cc
@@ -10,6 +10,84 @@
 namespace dgl {
 namespace runtime {
 
+void Random::SetSeed(int seed) {
+  GetInstance().Seed(seed);
+}
+
+template<typename IntType>
+IntType Random::RandInt(IntType upper) {
+  CHECK_LT(0, upper);
+  auto *rng = GetInstance().Get(omp_get_thread_num());
+  std::uniform_int_distribution<IntType> dist(0, upper - 1);
+  return dist(*rng);
+}
+
+template int32_t Random::RandInt<int32_t>(int32_t);
+template uint32_t Random::RandInt<uint32_t>(uint32_t);
+template int64_t Random::RandInt<int64_t>(int64_t);
+template uint64_t Random::RandInt<uint64_t>(uint64_t);
+
+template<typename IntType>
+IntType Random::RandInt(IntType lower, IntType upper) {
+  CHECK_LT(lower, upper);
+  auto *rng = GetInstance().Get(omp_get_thread_num());
+  std::uniform_int_distribution<IntType> dist(lower, upper - 1);
+  return dist(*rng);
+}
+
+template int32_t Random::RandInt<int32_t>(int32_t, int32_t);
+template uint32_t Random::RandInt<uint32_t>(uint32_t, uint32_t);
+template int64_t Random::RandInt<int64_t>(int64_t, int64_t);
+template uint64_t Random::RandInt<uint64_t>(uint64_t, uint64_t);
+
+template<typename FloatType>
+FloatType Random::Uniform() {
+  auto *rng = GetInstance().Get(omp_get_thread_num());
+  std::uniform_real_distribution<FloatType> dist(0.f, 1.f);
+  return dist(*rng);
+}
+
+template float Random::Uniform<float>();
+template double Random::Uniform<double>();
+
+template<typename FloatType>
+FloatType Random::Uniform(FloatType lower, FloatType upper) {
+  CHECK_LT(lower, upper);
+  auto *rng = GetInstance().Get(omp_get_thread_num());
+  std::uniform_real_distribution<FloatType> dist(lower, upper);
+  return dist(*rng);
+}
+
+template float Random::Uniform<float>(float, float);
+template double Random::Uniform<double>(double, double);
+
+Random::Random() {
+  Resize(omp_get_max_threads());
+  Seed(std::chrono::system_clock::now().time_since_epoch().count());
+}
+
+std::mt19937_64 *Random::Get(int i) {
+  return &rngs_[i];
+}
+
+void Random::Seed(int seed) {
+  meta_rng_.seed(seed);
+  for (auto &rng : rngs_)
+    rng.seed(meta_rng_());
+}
+
+Random &Random::GetInstance() {
+  static Random random;
+  return random;
+}
+
+void Random::Resize(int size) {
+  if (size <= rngs_.size())
+    return;
+  for (int i = rngs_.size(); i < size; ++i)
+    rngs_.push_back(std::mt19937_64(meta_rng_()));
+}
+
 DGL_REGISTER_GLOBAL("rng._CAPI_SetSeed")
 .set_body([] (DGLArgs args, DGLRetValue *rv) {
     int seed = args[0];
