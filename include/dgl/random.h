5c1d6145d0c3c34c248183f498eeea8683e91f66
Quan Gan <coin2028@hotmail.com>
2019-07-31 14:17:35 +0800

    address comments

--- include/dgl/random.h
+++ include/dgl/random.h
@@ -7,10 +7,14 @@
 #ifndef DGL_RANDOM_H_
 #define DGL_RANDOM_H_
 
+#include <dmlc/thread_local.h>
+#include <dmlc/logging.h>
 #include <random>
-#include <vector>
+#include <thread>
 
 namespace dgl {
+
+using namespace dgl::runtime;
 
 /*!
  * \brief Thread-local Random Number Generator class
@@ -18,45 +22,71 @@ namespace dgl {
 class RandomEngine {
  public:
   /*! \brief Constructor with default seed */
-  RandomEngine();
+  RandomEngine() {
+    std::random_device rd;
+    SetSeed(rd());
+  }
 
   /*! \brief Constructor with given seed */
-  explicit RandomEngine(uint32_t seed);
+  explicit RandomEngine(uint32_t seed) {
+    SetSeed(seed);
+  }
 
   /*! \brief Get the thread-local random number generator instance */
-  static RandomEngine *ThreadLocal();
+  static RandomEngine *ThreadLocal() {
+    return dmlc::ThreadLocalStore<RandomEngine>::Get();
+  }
 
   /*!
    * \brief Set the seed of this random number generator
    */
-  void SetSeed(uint32_t seed);
+  void SetSeed(uint32_t seed) {
+    rng_.seed(seed + GetThreadId());
+  }
 
   /*!
    * \brief Generate a uniform random integer in [0, upper)
    */
   template<typename T>
-  T RandInt(T upper);
+  T RandInt(T upper) {
+    return RandInt<T>(0, upper);
+  }
 
   /*!
    * \brief Generate a uniform random integer in [lower, upper)
    */
   template<typename T>
-  T RandInt(T lower, T upper);
+  T RandInt(T lower, T upper) {
+    CHECK_LT(lower, upper);
+    std::uniform_int_distribution<T> dist(lower, upper - 1);
+    return dist(rng_);
+  }
 
   /*!
    * \brief Generate a uniform random float in [0, 1)
    */
   template<typename T>
-  T Uniform();
+  T Uniform() {
+    return Uniform<T>(0., 1.);
+  }
 
   /*!
    * \brief Generate a uniform random float in [lower, upper)
    */
   template<typename T>
-  T Uniform(T lower, T upper);
+  T Uniform(T lower, T upper) {
+    CHECK_LT(lower, upper);
+    std::uniform_real_distribution<T> dist(lower, upper);
+    return dist(rng_);
+  }
 
  private:
   std::mt19937 rng_;
+  static std::hash<std::thread::id> kThreadIdHasher_;
+
+  uint32_t GetThreadId() {
+    return kThreadIdHasher_(std::this_thread::get_id());
+  }
 };
 
 };  // namespace dgl
--- src/random.cc
+++ src/random.cc
@@ -5,68 +5,13 @@
  */
 
 #include <dmlc/omp.h>
-#include <dmlc/thread_local.h>
-#include <dmlc/logging.h>
 #include <dgl/runtime/registry.h>
 #include <dgl/runtime/packed_func.h>
 #include <dgl/random.h>
-#include <thread>
-#include <random>
 
 using namespace dgl::runtime;
 
 namespace dgl {
-
-namespace {
-
-static std::hash<std::thread::id> kThreadIdHasher;
-
-uint32_t GetThreadId() {
-  return kThreadIdHasher(std::this_thread::get_id());
-}
-
-};
-
-RandomEngine::RandomEngine() {
-  std::random_device rd;
-  SetSeed(rd());
-}
-
-RandomEngine::RandomEngine(uint32_t seed) {
-  SetSeed(seed);
-}
-
-void RandomEngine::SetSeed(uint32_t seed) {
-  rng_.seed(seed + GetThreadId());
-}
-
-template<typename T>
-T RandomEngine::RandInt(T upper) {
-  return RandInt<T>(0, upper);
-}
-
-template<typename T>
-T RandomEngine::RandInt(T lower, T upper) {
-  CHECK_LT(lower, upper);
-  std::uniform_int_distribution<T> dist(lower, upper - 1);
-  return dist(rng_);
-}
-
-template<typename T>
-T RandomEngine::Uniform() {
-  return Uniform<T>(0., 1.);
-}
-
-template<typename T>
-T RandomEngine::Uniform(T lower, T upper) {
-  CHECK_LT(lower, upper);
-  std::uniform_real_distribution<T> dist(lower, upper);
-  return dist(rng_);
-}
-
-RandomEngine *RandomEngine::ThreadLocal() {
-  return dmlc::ThreadLocalStore<RandomEngine>::Get();
-}
 
 DGL_REGISTER_GLOBAL("rng._CAPI_SetSeed")
 .set_body([] (DGLArgs args, DGLRetValue *rv) {
@@ -75,22 +20,5 @@ DGL_REGISTER_GLOBAL("rng._CAPI_SetSeed")
     for (int i = 0; i < omp_get_max_threads(); ++i)
       RandomEngine::ThreadLocal()->SetSeed(seed);
   });
-
-// explicit instantiations
-template int32_t RandomEngine::RandInt<int32_t>(int32_t);
-template uint32_t RandomEngine::RandInt<uint32_t>(uint32_t);
-template int64_t RandomEngine::RandInt<int64_t>(int64_t);
-template uint64_t RandomEngine::RandInt<uint64_t>(uint64_t);
-
-template int32_t RandomEngine::RandInt<int32_t>(int32_t, int32_t);
-template uint32_t RandomEngine::RandInt<uint32_t>(uint32_t, uint32_t);
-template int64_t RandomEngine::RandInt<int64_t>(int64_t, int64_t);
-template uint64_t RandomEngine::RandInt<uint64_t>(uint64_t, uint64_t);
-
-template float RandomEngine::Uniform<float>();
-template double RandomEngine::Uniform<double>();
-
-template float RandomEngine::Uniform<float>(float, float);
-template double RandomEngine::Uniform<double>(double, double);
 
 };  // namespace dgl
