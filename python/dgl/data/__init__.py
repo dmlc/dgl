b0e2c27486000fe01d1cecc39ab74ddfa1f4a4e9
VoVAllen <VoVAllen@users.noreply.github.com>
2019-08-26 12:40:27 +0800

    Fix dataset import

--- python/dgl/data/__init__.py
+++ python/dgl/data/__init__.py
@@ -11,7 +11,7 @@ from .reddit import RedditDataset
 from .ppi import PPIDataset
 from .tu import TUDataset
 from .gindt import GINDataset
-from .chem import Tox21, alchemy
+# from .chem import Tox21, alchemy
 
 
 def register_data_args(parser):
