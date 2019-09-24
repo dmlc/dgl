7cca44e6c71daef24b497fbecccdd1bbab31457e
Ubuntu <ubuntu@ip-172-31-16-75.us-east-2.compute.internal>
2019-09-24 23:56:56 +0000

    allow to disable checking false negative.

--- python/dgl/contrib/sampling/sampler.py
+++ python/dgl/contrib/sampling/sampler.py
@@ -498,7 +498,8 @@ class EdgeSampler(object):
             negative_mode="",
             neg_sample_size=0,
             exclude_positive=False,
-            relations=None):
+            relations=None,
+            check_false_neg=False):
         self._g = g
         if self.immutable_only and not g._graph.is_readonly():
             raise NotImplementedError("This loader only support read-only graphs.")
@@ -511,6 +512,7 @@ class EdgeSampler(object):
             assert g.number_of_edges() == len(relations)
         self._relations = relations
 
+        self._check_false_neg = check_false_neg
         self._batch_size = int(batch_size)
 
         if seed_edges is None:
@@ -555,6 +557,7 @@ class EdgeSampler(object):
             self._negative_mode,
             self._neg_sample_size,
             self._exclude_positive,
+            self._check_false_neg,
             self._relations)
 
         if len(subgs) == 0:
@@ -570,8 +573,9 @@ class EdgeSampler(object):
             for i in range(num_pos):
                 pos_subg = subgraph.DGLSubGraph(self.g, subgs[i])
                 neg_subg = subgraph.DGLSubGraph(self.g, subgs[i + num_pos])
-                exist = _CAPI_GetNegEdgeExistence(subgs[i + num_pos]);
-                neg_subg.edata['exist'] = utils.toindex(exist).tousertensor()
+                if self._check_false_neg:
+                    exist = _CAPI_GetNegEdgeExistence(subgs[i + num_pos]);
+                    neg_subg.edata['exist'] = utils.toindex(exist).tousertensor()
                 rets.append((pos_subg, neg_subg))
             return rets
 
--- src/graph/sampler.cc
+++ src/graph/sampler.cc
@@ -981,7 +981,8 @@ IdArray CheckExistence(GraphPtr gptr, IdArray relations,
 
 NegSubgraph NegEdgeSubgraph(GraphPtr gptr, IdArray relations, const Subgraph &pos_subg,
                             const std::string &neg_mode,
-                            int neg_sample_size, bool exclude_positive) {
+                            int neg_sample_size, bool exclude_positive,
+                            bool check_false_neg) {
   int64_t num_tot_nodes = gptr->NumVertices();
   bool is_multigraph = gptr->IsMultigraph();
   std::vector<IdArray> adj = pos_subg.graph->GetAdj(false, "coo");
@@ -1029,6 +1030,7 @@ NegSubgraph NegEdgeSubgraph(GraphPtr gptr, IdArray relations, const Subgraph &po
     // We add all nodes as negative nodes.
     for (int64_t i = 0; i < num_tot_nodes; i++) {
       neg_vids.push_back(i);
+      neg_map[i] = i;
     }
   }
 
@@ -1089,11 +1091,13 @@ NegSubgraph NegEdgeSubgraph(GraphPtr gptr, IdArray relations, const Subgraph &po
   neg_subg.induced_vertices = induced_neg_vid;
   neg_subg.induced_edges = induced_neg_eid;
   // TODO(zhengda) we should provide an array of 1s if exclude_positive
-  if (relations->shape[0] == 0) {
-    neg_subg.exist = CheckExistence(gptr, neg_src, neg_dst, induced_neg_vid);
-  } else {
-    neg_subg.exist = CheckExistence(gptr, relations, neg_src, neg_dst,
-                                    induced_neg_vid, induced_neg_eid);
+  if (check_false_neg) {
+    if (relations->shape[0] == 0) {
+      neg_subg.exist = CheckExistence(gptr, neg_src, neg_dst, induced_neg_vid);
+    } else {
+      neg_subg.exist = CheckExistence(gptr, relations, neg_src, neg_dst,
+                                      induced_neg_vid, induced_neg_eid);
+    }
   }
   return neg_subg;
 }
@@ -1101,7 +1105,7 @@ NegSubgraph NegEdgeSubgraph(GraphPtr gptr, IdArray relations, const Subgraph &po
 NegSubgraph PBGNegEdgeSubgraph(GraphPtr gptr, IdArray relations, const Subgraph &pos_subg,
                             const std::string &neg_mode,
                             int neg_sample_size, bool is_multigraph,
-                            bool exclude_positive) {
+                            bool exclude_positive, bool check_false_neg) {
   int64_t num_tot_nodes = gptr->NumVertices();
   std::vector<IdArray> adj = pos_subg.graph->GetAdj(false, "coo");
   IdArray coo = adj[0];
@@ -1212,11 +1216,13 @@ NegSubgraph PBGNegEdgeSubgraph(GraphPtr gptr, IdArray relations, const Subgraph 
   neg_subg.graph = GraphPtr(new ImmutableGraph(neg_coo));
   neg_subg.induced_vertices = induced_neg_vid;
   neg_subg.induced_edges = induced_neg_eid;
-  if (relations->shape[0] == 0) {
-    neg_subg.exist = CheckExistence(gptr, neg_src, neg_dst, induced_neg_vid);
-  } else {
-    neg_subg.exist = CheckExistence(gptr, relations, neg_src, neg_dst,
-                                    induced_neg_vid, induced_neg_eid);
+  if (check_false_neg) {
+    if (relations->shape[0] == 0) {
+      neg_subg.exist = CheckExistence(gptr, neg_src, neg_dst, induced_neg_vid);
+    } else {
+      neg_subg.exist = CheckExistence(gptr, relations, neg_src, neg_dst,
+                                      induced_neg_vid, induced_neg_eid);
+    }
   }
   return neg_subg;
 }
@@ -1242,7 +1248,8 @@ DGL_REGISTER_GLOBAL("sampling._CAPI_UniformEdgeSampling")
     const std::string neg_mode = args[5];
     const int neg_sample_size = args[6];
     const bool exclude_positive = args[7];
-    IdArray relations = args[8];
+    const bool check_false_neg = args[8];
+    IdArray relations = args[9];
     // process args
     auto gptr = std::dynamic_pointer_cast<ImmutableGraph>(g.sptr());
     CHECK(gptr) << "sampling isn't implemented in mutable graph";
@@ -1276,11 +1283,12 @@ DGL_REGISTER_GLOBAL("sampling._CAPI_UniformEdgeSampling")
       if (neg_mode.substr(0, 3) == "PBG") {
         NegSubgraph neg_subg = PBGNegEdgeSubgraph(gptr, relations, subg,
                                                   neg_mode.substr(4), neg_sample_size,
-                                                  gptr->IsMultigraph(), exclude_positive);
+                                                  gptr->IsMultigraph(), exclude_positive,
+                                                  check_false_neg);
         negative_subgs[i] = ConvertRef(neg_subg);
       } else if (neg_mode.size() > 0) {
         NegSubgraph neg_subg = NegEdgeSubgraph(gptr, relations, subg, neg_mode, neg_sample_size,
-                                               exclude_positive);
+                                               exclude_positive, check_false_neg);
         negative_subgs[i] = ConvertRef(neg_subg);
       }
     }
