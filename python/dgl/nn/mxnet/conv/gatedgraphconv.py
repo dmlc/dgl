bd2e64c1b8b02a95a3287ebf217d2cadc09d2e9f
yzh119 <expye@outlook.com>
2019-10-17 15:46:42 +0800

    upd

--- python/dgl/nn/mxnet/conv/gatedgraphconv.py
+++ python/dgl/nn/mxnet/conv/gatedgraphconv.py
@@ -46,7 +46,7 @@ class GatedGraphConv(nn.Block):
             raise KeyError('MXNet do not support disabling bias in GRUCell.')
         with self.name_scope():
             # NOTE(zihao): MXNet do not support ModuleList, use Sequential as workaround.
-            self.linears = nn.Sequential() 
+            self.linears = nn.Sequential()
             for _ in range(n_etypes):
                 self.linears.add(
                     nn.Dense(out_feats,
