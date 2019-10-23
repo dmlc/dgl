cba04d2ba15c06e2e6fc9e205cef3d4e180b6198
Ubuntu <ubuntu@ip-172-31-13-147.us-east-2.compute.internal>
2019-10-23 07:36:01 +0000

    networkx >= 2.4 will break our examples

--- python/setup.py
+++ python/setup.py
@@ -120,7 +120,7 @@ setup(
     install_requires=[
         'numpy>=1.14.0',
         'scipy>=1.1.0',
-        'networkx>=2.1',
+        'networkx>=2.1,<2.4',
     ],
     url='https://github.com/dmlc/dgl',
     distclass=BinaryDistribution,
