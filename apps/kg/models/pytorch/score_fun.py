f4b7b3bc6a0d26b54751adc3e23b3970f74f77d2
MilkshakeForReal <38997672+MilkshakeForReal@users.noreply.github.com>
2019-10-30 22:46:40 -0400

    Update score_fun.py

--- apps/kg/models/pytorch/score_fun.py
+++ apps/kg/models/pytorch/score_fun.py
@@ -199,3 +199,77 @@ class RESCALScore(nn.Module):
                 tmp = tmp.reshape(num_chunks, chunk_size, hidden_dim)
                 return th.bmm(tmp, tails)
             return fn
+
+class RotatEScore(nn.Module):
+    def __init__(self, gamma, emb_init):
+        super(RotatEScore, self).__init__()
+        self.gamma = gamma
+        self.emb_init = emb_init
+        
+    def edge_func(self, edges):
+        pi = 3.14159265358979323846
+        re_head, im_head = th.chunk(edges.src['emb'], 2, dim=-1)
+        re_tail, im_tail = th.chunk(edges.dst['emb'], 2, dim=-1)
+        
+        phase_rel = edges.data['emb']/(self.emb_init/pi)
+        re_rel, im_rel = th.cos(phase_rel),th.sin(phase_rel)
+        
+        re_score = re_head * re_rel - im_head * im_rel     
+        im_score = re_head * im_rel + im_head * re_rel
+        re_score = re_score - re_tail
+        im_score = im_score - im_tail
+        
+        score = th.stack([re_score, im_score], dim = 0)
+        score = score.norm(dim = 0)
+        
+        return {'score': self.gamma - th.sum(score, -1)}
+
+    def reset_parameters(self):
+        pass
+
+    def save(self, path, name):
+        pass
+
+    def load(self, path, name):
+        pass
+
+    def forward(self, g):
+        g.apply_edges(lambda edges: self.edge_func(edges))
+
+    def create_neg(self, neg_head):
+        pi = 3.14159265358979323846
+        gamma = self.gamma
+        if neg_head:
+            def fn(heads, relations, tails, num_chunks, chunk_size, neg_sample_size):
+                hidden_dim = heads.shape[1]
+                emb_real = tails[..., :hidden_dim // 2]
+                emb_imag = tails[..., hidden_dim // 2:]
+                
+                phase_rel = relations/(self.emb_init/pi)
+                rel_real, rel_imag = th.cos(phase_rel),-th.sin(phase_rel)
+                
+                real = emb_real * rel_real - emb_imag * rel_imag
+                imag = emb_real * rel_imag + emb_imag * rel_real
+                
+                emb_complex = th.cat((real, imag), dim=-1)
+                tmp = emb_complex.reshape(num_chunks, chunk_size, hidden_dim)
+                heads = heads.reshape(num_chunks, neg_sample_size, hidden_dim)
+                return gamma - th.cdist(tmp, heads, p=1)
+            return fn
+        else:
+            def fn(heads, relations, tails, num_chunks, chunk_size, neg_sample_size):
+                hidden_dim = heads.shape[1]
+                emb_real = heads[..., :hidden_dim // 2]
+                emb_imag = heads[..., hidden_dim // 2:]
+                
+                phase_rel = relations/(self.emb_init/pi)
+                rel_real, rel_imag = th.cos(phase_rel), th.sin(phase_rel)
+                
+                real = emb_real * rel_real - emb_imag * rel_imag
+                imag = emb_real * rel_imag + emb_imag * rel_real
+                
+                emb_complex = th.cat((real, imag), dim=-1)
+                tmp = emb_complex.reshape(num_chunks, chunk_size, hidden_dim)
+                tails = tails.reshape(num_chunks, neg_sample_size, hidden_dim)
+                return  gamma - th.cdist(tmp, tails, p=1)
+            return fn
