cf77d16b75f19dce63c92c0114978758e3b630b0
VoVAllen <jz1749@nyu.edu>
2019-11-09 13:38:03 +0000

    fix

--- tests/scripts/task_unit_test.bat
+++ tests/scripts/task_unit_test.bat
@@ -14,9 +14,9 @@ SET DGLBACKEND=!BACKEND!
 SET DGL_LIBRARY_PATH=!CD!\build
 SET DGL_DOWNLOAD_DIR=!CD!
 
-python3 -m pip install pytest || EXIT /B 1
-python3 -m pytest -v --junitxml=pytest_backend.xml tests\!DGLBACKEND! || EXIT /B 1
-python3 -m pytest -v --junitxml=pytest_gindex.xml tests\graph_index || EXIT /B 1
-python3 -m pytest -v --junitxml=pytest_compute.xml tests\compute || EXIT /B 1
+python -m pip install pytest || EXIT /B 1
+python -m pytest -v --junitxml=pytest_backend.xml tests\!DGLBACKEND! || EXIT /B 1
+python -m pytest -v --junitxml=pytest_gindex.xml tests\graph_index || EXIT /B 1
+python -m pytest -v --junitxml=pytest_compute.xml tests\compute || EXIT /B 1
 ENDLOCAL
 EXIT /B
