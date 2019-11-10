ab0303e1731a7a4e571e96daf315f72c70e3a754
aksnzhy <mctt90@gmail.com>
2019-11-10 16:14:26 +0800

    small fix

--- apps/kg/models/general_models.py
+++ apps/kg/models/general_models.py
@@ -213,7 +213,7 @@ class KEModel(object):
     def pull_model(self, client, pos_g, neg_g):
         entity_id = F.cat(seq=[pos_g.ndata['id'], neg_g.ndata['id']], dim=0)
         entity_id = F.tensor(np.unique(F.asnumpy(entity_id)))
-        relation_id = F.cat(seq=[pos_g.edata['id'], neg_g.edata('id')], dim=0)
+        relation_id = F.cat(seq=[pos_g.edata['id'], neg_g.edata['id']], dim=0)
         relation_id = F.tensor(np.unique(F.asnumpy(relation_id)))
 
     def push_gradient(self, client):
