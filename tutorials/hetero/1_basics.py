46eca749b4d2a66d025ca251d1a2eec4572bf407
John Andrilla <38335507+john-andrilla@users.noreply.github.com>
2019-11-20 12:36:43 -0800

    Update tutorials/hetero/1_basics.py
    Co-Authored-By: Aaron Markham <markhama@amazon.com>

--- tutorials/hetero/1_basics.py
+++ tutorials/hetero/1_basics.py
@@ -208,7 +208,7 @@ print(pp_g.successors(3))
 ###############################################################################
 # Create a subset of the ACM graph using the paper-author, paper-paper, 
 # and paper-subject relationships.  Meanwhile, also add the reverse
-# relations to prepare for the later sections.
+# relationship to prepare for the later sections.
 
 G = dgl.heterograph({
         ('paper', 'written-by', 'author') : data['PvsA'],
