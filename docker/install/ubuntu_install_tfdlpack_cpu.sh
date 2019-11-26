c72d34113f4134ef257944daac4d972b44579d49
VoVAllen <jz1749@nyu.edu>
2019-11-26 09:44:48 +0000

    add tfdlpack

---
+++ docker/install/ubuntu_install_tfdlpack_cpu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Install tfdlpack on cpu
+pip3 install git+https://github.com/VoVAllen/tf-dlpack.git@no_cuda
---
+++ docker/install/ubuntu_install_tfdlpack_gpu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Install tfdlpack
+pip3 install git+https://github.com/VoVAllen/tf-dlpack.git
