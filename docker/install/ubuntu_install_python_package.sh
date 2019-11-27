6bebbcfd5a52445ffa62b24f19440536ca871390
VoVAllen <jz1749@nyu.edu>
2019-11-27 09:44:35 +0000

    change ci image

--- docker/install/ubuntu_install_python_package.sh
+++ docker/install/ubuntu_install_python_package.sh
@@ -1,3 +1,4 @@
 # install libraries for python package on ubuntu
 #pip2 install nose numpy cython scipy networkx matplotlib nltk requests[security] tqdm
-pip3 install nose numpy cython scipy networkx matplotlib nltk requests[security] tqdm
+# pip3 install nose numpy cython scipy networkx matplotlib nltk requests[security] tqdm virtualenv
+echo "pip install packages"
