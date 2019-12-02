d7f69831d115e5953f0e99d2afecc023db4714c0
QipengGuo <qpguo16@fudan.edu.cn>
2019-12-02 12:31:18 +0000

    add graphwriter pytorch example

---
+++ examples/pytorch/graphwriter/test.sh
@@ -0,0 +1,5 @@
+env CUDA_VISIBLE_DEVICES=0 python -u train.py --save_model tmp_model.ptbest --test  --title --lp 1.0 --beam_size 1
+perl detokenizer.perl -l en < tmp_gold.txt > tmp_gold.txt.a
+perl detokenizer.perl -l en < tmp_pred.txt > tmp_pred.txt.a
+perl multi-bleu.perl tmp_gold.txt < tmp_pred.txt
+perl multi-bleu-detok.perl tmp_gold.txt.a < tmp_pred.txt.a
