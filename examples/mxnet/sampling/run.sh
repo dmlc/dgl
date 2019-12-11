2b1805480795293dc3438e891670f5db0a8a8944
Ubuntu <ubuntu@ip-172-31-6-131.us-east-2.compute.internal>
2019-12-11 21:10:46 +0000

    make some improvement.

--- examples/mxnet/sampling/run.sh
+++ examples/mxnet/sampling/run.sh
@@ -1,13 +1,16 @@
 export OMP_NUM_THREADS=4
-DGLBACKEND=mxnet python3 dist_train.py --id 0 --graph-name reddit --server 1 --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 &
-DGLBACKEND=mxnet python3 dist_train.py --id 1 --graph-name reddit --server 1 --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 &
-DGLBACKEND=mxnet python3 dist_train.py --id 2 --graph-name reddit --server 1 --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 &
-DGLBACKEND=mxnet python3 dist_train.py --id 3 --graph-name reddit --server 1 --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 &
+export DGLBACKEND=mxnet
+python3 dist_train.py --id 0 --graph-name reddit --server --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 --num-neighbors 10 &
+python3 dist_train.py --id 1 --graph-name reddit --server --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 --num-neighbors 10 &
+python3 dist_train.py --id 2 --graph-name reddit --server --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 --num-neighbors 10 &
+python3 dist_train.py --id 3 --graph-name reddit --server --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 --num-neighbors 10 &
 
-DGLBACKEND=mxnet python3 dist_train.py --id 0 --graph-name reddit --server 0 --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 &
-DGLBACKEND=mxnet python3 dist_train.py --id 1 --graph-name reddit --server 0 --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 &
-DGLBACKEND=mxnet python3 dist_train.py --id 2 --graph-name reddit --server 0 --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 &
-DGLBACKEND=mxnet python3 dist_train.py --id 3 --graph-name reddit --server 0 --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 &
+export OMP_NUM_THREADS=4
+~/mxnet/tools/launch.py -n 4 --launcher local python3 dist_train.py --id 0 --graph-name reddit --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 --num-neighbors 10 &
+#python3 dist_train.py --id 0 --graph-name reddit --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 --num-neighbors 10 &
+#python3 dist_train.py --id 1 --graph-name reddit --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 --num-neighbors 10 &
+#python3 dist_train.py --id 2 --graph-name reddit --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 --num-neighbors 10 &
+#python3 dist_train.py --id 3 --graph-name reddit --num-parts 4 --model gcn_ns --n-classes 41 --n-features 602 --num-neighbors 10 &
 
 #DGLBACKEND=mxnet python3 dist_train.py --id 0 --graph-name reddit --server 1 --num-parts 1 --model gcn_ns --n-classes 41 --n-features 602 --dataset reddit &
 #DGLBACKEND=mxnet python3 dist_train.py --id 0 --graph-name reddit --server 0 --num-parts 1 --model gcn_ns --n-classes 41 --n-features 602 --dataset reddit &
