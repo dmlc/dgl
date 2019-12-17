3602ed6e4fafc4ae286821d5863c9d296e7adbca
mufeili <mufeili1996@gmail.com>
2019-12-17 13:30:42 +0800

    Update

--- examples/pytorch/model_zoo/chem/protein_ligand_binding/utils.py
+++ examples/pytorch/model_zoo/chem/protein_ligand_binding/utils.py
@@ -18,6 +18,8 @@ def set_random_seed(seed=0):
     seed : int
         Random seed to use. Default to 0.
     """
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
