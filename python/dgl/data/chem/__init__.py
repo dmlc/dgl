cf9ba90fd47471b56efaa66128de78667b6f17b9
Mufei Li <mufeili1996@gmail.com>
2019-12-19 11:54:40 +0800

    [Chem] ACNN and various utilities (#1117)
    * Add several splitting methods
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Fix
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Finally
    
    * CI

--- python/dgl/data/chem/__init__.py
+++ python/dgl/data/chem/__init__.py
@@ -1,5 +1,2 @@
+from .datasets import *
 from .utils import *
-from .csv_dataset import MoleculeCSVDataset
-from .tox21 import Tox21
-from .alchemy import TencentAlchemyDataset
-from .pubchem_aromaticity import PubChemBioAssayAromaticity
