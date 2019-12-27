f7450f467c1052b70f2fc98c1eb6529d1a723f3a
Quan Gan <coin2028@hotmail.com>
2019-12-27 11:42:27 +0800

    another try

--- python/dgl/backend/pytorch/tensor.py
+++ python/dgl/backend/pytorch/tensor.py
@@ -285,7 +285,7 @@ def zerocopy_from_dgl_ndarray(input):
 
 class BinaryReduce(th.autograd.Function):
     @staticmethod
-    def forward(ctx, reducer, binary_op, graph, lhs, rhs, lhs_data, rhs_data,
+    def forward(ctx, reducer, binary_op, graph, lhs, rhs, lhs_data, rhs_data, out_data,
                 out_size, lhs_map, rhs_map, out_map):
         lhs_data_nd = zerocopy_to_dgl_ndarray(lhs_data)
         rhs_data_nd = zerocopy_to_dgl_ndarray(rhs_data)
@@ -293,7 +293,6 @@ class BinaryReduce(th.autograd.Function):
         out_shape = feat_shape
         if binary_op == 'dot':
             out_shape = feat_shape[:-1]
-        out_data = lhs_data.new_empty((out_size,) + out_shape)
         out_data_nd = zerocopy_to_dgl_ndarray(out_data)
         K.binary_op_reduce(
             reducer if reducer != 'mean' else 'sum',
@@ -361,15 +360,30 @@ class BinaryReduce(th.autograd.Function):
             None, None
 
 
+def binary_reduce(ctx, reducer, binary_op, graph, lhs, rhs, lhs_data, rhs_data,
+                  out_size, lhs_map, rhs_map, out_map):
+    lhs_data_nd = zerocopy_to_dgl_ndarray(lhs_data)
+    rhs_data_nd = zerocopy_to_dgl_ndarray(rhs_data)
+    feat_shape = K.infer_binary_feature_shape(binary_op, lhs_data_nd, rhs_data_nd)
+
+    out_shape = feat_shape
+    if binary_op == 'dot':
+        out_shape = feat_shape[:-1]
+    out_data = lhs_data.new_empty((out_size,) + out_shape)
+
+    return BinaryReduce.apply(
+            ctx, reducer, binary_op, lhs, rhs, lhs_data, rhs_data, out_data,
+            out_size, lhs_map, rhs_map, out_map)
+
+
 class CopyReduce(th.autograd.Function):
     @staticmethod
-    def forward(ctx, reducer, graph, target, in_data, out_size, in_map,
+    def forward(ctx, reducer, graph, target, in_data, out_data, out_size, in_map,
                 out_map):
-        out_data = in_data.new_empty((out_size,) + in_data.shape[1:])
         in_data_nd = zerocopy_to_dgl_ndarray(in_data)
         out_data_nd = zerocopy_to_dgl_ndarray(out_data)
         K.copy_reduce(
-            reducer if reducer != 'mean' else 'sum', 
+            reducer if reducer != 'mean' else 'sum',
             graph, target, in_data_nd, out_data_nd, in_map[0], out_map[0])
         # normalize if mean reducer
         # NOTE(zihao): this is a temporary hack and we should have better solution in the future.
@@ -379,7 +393,7 @@ class CopyReduce(th.autograd.Function):
             in_ones_nd = zerocopy_to_dgl_ndarray(in_ones)
             degs_nd = zerocopy_to_dgl_ndarray(degs)
             K.copy_reduce(
-                'sum', graph, target, in_ones_nd, degs_nd, in_map[0], out_map[0]) 
+                'sum', graph, target, in_ones_nd, degs_nd, in_map[0], out_map[0])
             # reshape
             degs = degs.reshape((out_data.shape[0],) + (1,) * (out_data.dim() - 1)).clamp(min=1)
             out_data = out_data / degs
@@ -403,14 +417,15 @@ class CopyReduce(th.autograd.Function):
         if ctx.needs_input_grad[3]:
             grad_in = grad_out.new_empty(in_data_nd.shape)
             K.backward_copy_reduce(
-                reducer if reducer != 'mean' else 'sum', 
-                graph, target, in_data_nd, out_data_nd, grad_out_nd, 
+                reducer if reducer != 'mean' else 'sum',
+                graph, target, in_data_nd, out_data_nd, grad_out_nd,
                 zerocopy_to_dgl_ndarray(grad_in), in_map[1], out_map[1])
         return None, None, None, grad_in, None, None, None
 
 
-binary_reduce = BinaryReduce.apply
-copy_reduce = CopyReduce.apply
+def copy_reduce(reducer, graph, target, in_data, out_size, in_map, out_map):
+    out_data = in_data.new_empty((out_size,) + in_data.shape[1:])
+    return CopyReduce.apply(reducer, graph, target, in_data, out_data, in_map, out_map)
 
 
 def _reduce_grad(grad, shape):
