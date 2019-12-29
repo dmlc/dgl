b6578d999143cb74d468a7693930c5ca9d0d1c8d
aksnzhy <mctt90@gmail.com>
2019-12-29 17:44:09 +0800

    update

--- tests/pytorch/test_kvstore.py
+++ tests/pytorch/test_kvstore.py
@@ -1,108 +1,119 @@
-import backend as F
-import numpy as np
-import scipy as sp
 import dgl
+import argparse
 import torch as th
-from dgl import utils
-
-import os
 import time
+import backend as F
 
-client_namebook = { 0:'127.0.0.1:50061' }
+from multiprocessing import Process
 
-server_namebook = { 0:'127.0.0.1:50062' }
+ID = []
+ID.append(th.tensor([0,1]))
+ID.append(th.tensor([2,3]))
+ID.append(th.tensor([4,5]))
+ID.append(th.tensor([6,7]))
 
-def start_server(server_embed):
-    server = dgl.contrib.KVServer(
-        server_id=0, 
-        client_namebook=client_namebook, 
-        server_addr=server_namebook[0])
+DATA = []
+DATA.append(th.tensor([[1.,1.,1.,],[1.,1.,1.,]]))
+DATA.append(th.tensor([[2.,2.,2.,],[2.,2.,2.,]]))
+DATA.append(th.tensor([[3.,3.,3.,],[3.,3.,3.,]]))
+DATA.append(th.tensor([[4.,4.,4.,],[4.,4.,4.,]]))
 
-    server.init_data(name='server_embed', data_tensor=server_embed)
+edata_partition_book = {'edata':th.tensor([0,0,1,1,2,2,3,3])}
+ndata_partition_book = {'ndata':th.tensor([0,0,1,1,2,2,3,3])}
 
-    server.start()
+ndata_g2l = []
+edata_g2l = []
 
-def start_client(server_embed):
-    client = dgl.contrib.KVClient(
-        client_id=0, 
-        server_namebook=server_namebook, 
-        client_addr=client_namebook[0])
+ndata_g2l.append({'ndata':th.tensor([0,1,0,0,0,0,0,0])})
+ndata_g2l.append({'ndata':th.tensor([0,0,0,1,0,0,0,0])})
+ndata_g2l.append({'ndata':th.tensor([0,0,0,0,0,1,0,0])})
+ndata_g2l.append({'ndata':th.tensor([0,0,0,0,0,0,0,1])})
 
-    client.connect()
+edata_g2l.append({'edata':th.tensor([0,1,0,0,0,0,0,0])})
+edata_g2l.append({'edata':th.tensor([0,0,0,1,0,0,0,0])})
+edata_g2l.append({'edata':th.tensor([0,0,0,0,0,1,0,0])})
+edata_g2l.append({'edata':th.tensor([0,0,0,0,0,0,0,1])})
 
-    # Initialize data on server
-    client.init_data(name='embed_0', server_id=0, shape=[5, 3], init_type='zero')
-    client.init_data(name='embed_1', server_id=0, shape=[5], init_type='uniform', low=0.0, high=0.0)
+def start_client(flag):
+    time.sleep(3)
 
-    data_0 = th.tensor([[0., 0., 0., ], [1., 1., 1.], [2., 2., 2.]])
-    data_1 = th.tensor([0., 1., 2.])
+    client = dgl.contrib.start_client(ip_config='ip_config.txt', 
+                                      ndata_partition_book=ndata_partition_book, 
+                                      edata_partition_book=edata_partition_book,
+                                      close_shared_mem=flag)
 
-    for i in range(5):
-        client.push(name='embed_0', server_id=0, id_tensor=th.tensor([0, 2, 4]), data_tensor=data_0)
-        client.push(name='embed_1', server_id=0, id_tensor=th.tensor([0, 2, 4]), data_tensor=data_1)
-        client.push(name='server_embed', server_id=0, id_tensor=th.tensor([0, 2, 4]), data_tensor=data_1)
+    client.push(name='edata', id_tensor=ID[client.get_id()], data_tensor=DATA[client.get_id()])
+    client.push(name='ndata', id_tensor=ID[client.get_id()], data_tensor=DATA[client.get_id()])
 
     client.barrier()
 
-    client.pull(name='embed_0', server_id=0, id_tensor=th.tensor([0, 1, 2, 3, 4]))
-    msg = client.pull_wait()
-    assert msg.rank == 0
+    tensor_edata = client.pull(name='edata', id_tensor=th.tensor([0,1,2,3,4,5,6,7]))
+    tensor_ndata = client.pull(name='ndata', id_tensor=th.tensor([0,1,2,3,4,5,6,7]))
 
-    target_tensor_0 = th.tensor(
-        [[ 0., 0., 0.],
-         [ 0., 0., 0.],
-         [ 5., 5., 5.],
-         [ 0., 0., 0.],
-         [10., 10., 10.]])
 
-    assert th.equal(msg.data, target_tensor_0) == True
+    target_tensor = th.tensor([[1., 1., 1.],
+                               [1., 1., 1.],
+                               [2., 2., 2.],
+                               [2., 2., 2.],
+                               [3., 3., 3.],
+                               [3., 3., 3.],
+                               [4., 4., 4.],
+                               [4., 4., 4.]])
 
-    client.pull(name='embed_1', server_id=0, id_tensor=th.tensor([0, 1, 2, 3, 4]))
-    msg = client.pull_wait()
+    assert F.array_equal(tensor_edata, target_tensor)
 
-    target_tensor_1 = th.tensor([ 0., 0., 5., 0., 10.])
+    assert F.array_equal(tensor_ndata, target_tensor)
 
-    assert th.equal(msg.data, target_tensor_1) == True
+    client.barrier()
 
-    client.pull(name='embed_0', server_id=0, id_tensor=th.tensor([0, 1, 2, 3, 4]))
-    client.pull(name='embed_1', server_id=0, id_tensor=th.tensor([0, 1, 2, 3, 4]))
-    client.pull(name='embed_0', server_id=0, id_tensor=th.tensor([0, 1, 2, 3, 4]))
-    client.pull(name='embed_1', server_id=0, id_tensor=th.tensor([0, 1, 2, 3, 4]))
-    client.pull(name='server_embed', server_id=0, id_tensor=th.tensor([0, 1, 2, 3, 4]))
+    if client.get_id() == 0:
+        client.shut_down()
 
-    msg_0 = client.pull_wait()
-    msg_1 = client.pull_wait()
-    msg_2 = client.pull_wait()
-    msg_3 = client.pull_wait()
-    msg_4 = client.pull_wait()
-
-    target_tensor_2 = th.tensor([ 2., 2., 7., 2., 12.])
-
-    assert th.equal(msg_0.data, target_tensor_0) == True
-    assert th.equal(msg_1.data, target_tensor_1) == True
-    assert th.equal(msg_2.data, target_tensor_0) == True
-    assert th.equal(msg_3.data, target_tensor_1) == True
-    assert th.equal(msg_4.data, target_tensor_2) == True
-
-    server_embed += target_tensor_2
-
-    client.pull(name='server_embed', server_id=0, id_tensor=th.tensor([0, 1, 2, 3, 4]))
-    msg_5 = client.pull_wait()
-
-    assert th.equal(msg_5.data, target_tensor_2 * 2) == True
-
-    client.shut_down()
+def start_server(server_id, num_client):
+    
+    dgl.contrib.start_server(
+        server_id=server_id,
+        ip_config='ip_config.txt',
+        num_client=num_client,
+        ndata={'ndata':th.tensor([[0.,0.,0.],[0.,0.,0.]])},
+        edata={'edata':th.tensor([[0.,0.,0.],[0.,0.,0.]])},
+        ndata_g2l=ndata_g2l[server_id],
+        edata_g2l=edata_g2l[server_id])
 
 if __name__ == '__main__':
-    server_embed = th.tensor([2., 2., 2., 2., 2.])
-    # use pytorch shared memory
-    server_embed.share_memory_()
 
-    pid = os.fork()
-    if pid == 0:
-        start_server(server_embed)
-    else:
-        time.sleep(2) # wait server start
-        start_client(server_embed)
+    # server process
+    p0 = Process(target=start_server, args=(0, 4))
+    p1 = Process(target=start_server, args=(1, 4))
+    p2 = Process(target=start_server, args=(2, 4))
+    p3 = Process(target=start_server, args=(3, 4))
 
-    assert th.equal(server_embed, th.tensor([ 4., 4., 14., 4., 24.])) == True
+    # client process
+    p4 = Process(target=start_client, args=(True,))
+    p5 = Process(target=start_client, args=(True,))
+    p6 = Process(target=start_client, args=(False,))
+    p7 = Process(target=start_client, args=(False,))
+
+
+    # start server process
+    p0.start()
+    p1.start()
+    p2.start()
+    p3.start()
+
+    # start client process
+    p4.start()
+    p5.start()
+    p6.start()
+    p7.start()
+
+
+    p0.join()
+    p1.join()
+    p2.join()
+    p3.join()
+
+    p4.join()
+    p5.join()
+    p6.join()
+    p7.join()
