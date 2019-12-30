17d9ff03ad952f660576a4a066d0d12c91c9fa49
VoVAllen <jz1749@nyu.edu>
2019-12-30 12:17:29 +0000

    fix

--- tests/regression/publish.sh
+++ tests/regression/publish.sh
@@ -1,4 +1,5 @@
-docker run -d --name dgl-reg --rm --runtime=nvidia -v dgl-regression:/root/regression dgllib/dgl-ci-gpu:conda
-docker cp ./tests/regression/run.sh dgl-red:/root/regression_run.sh
+docker run --name dgl-reg --rm --runtime=nvidia -v dgl-regression:/root/regression -d -it dgllib/dgl-ci-gpu:conda /bin/bash
+docker cp ./tests/regression/run.sh dgl-reg:/root/regression_run.sh
 docker exec -d dgl-reg bash /root/regression_run.sh
 docker cp /root/regression/dgl/html ~/reg_prod/html/
+docker stop dgl-reg
