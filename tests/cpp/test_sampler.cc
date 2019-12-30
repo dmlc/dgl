65036962079bb18df386f31fe99ad83db67fcac3
yzh119 <expye@outlook.com>
2019-12-31 00:19:37 +0800

    refactor

---
+++ tests/cpp/test_sampler.cc
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <dgl/sample_utils.h>
+#include "./common.h"
+
+using namespace dgl;
+using namespace dgl::runtime;
+
+TEST(SampleUtilsTest, TestAliasSampler) {
+
+};
+
+TEST(SampleUtilsTest, TestCDFSampler) {
+
+};
+
+TEST(SampleUtilsTest, TestTreeSampler) {
+
+};
