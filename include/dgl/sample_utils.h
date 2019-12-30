ade775d4c7f4df6589b48233a1f4548a6faa78cb
yzh119 <expye@outlook.com>
2019-12-31 00:27:59 +0800

    upd

--- include/dgl/sample_utils.h
+++ include/dgl/sample_utils.h
@@ -1,9 +1,8 @@
 /*!
  *  Copyright (c) 2019 by Contributors
- * \file dgl/random.h
- * \brief Random number generators
+ * \file dgl/sample_utils.h
+ * \brief Sampling utilities
  */
-
 #ifndef DGL_SAMPLE_UTILS_H_
 #define DGL_SAMPLE_UTILS_H_
 
