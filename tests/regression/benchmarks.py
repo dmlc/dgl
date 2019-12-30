d1025777498e94b917db817669dfce89808004e9
VoVAllen <jz1749@nyu.edu>
2019-12-30 10:16:40 +0000

    111

--- tests/regression/benchmarks.py
+++ tests/regression/benchmarks.py
@@ -6,37 +6,41 @@ import os
 from pathlib import Path
 import numpy as np
 
-base_path = Path("~/regression/dgl/examples/pytorch/gcn/train.py")
+base_path = Path("~/regression/dgl/")
 
-def track_gcn_time(dataset, gpu_id):
-    bashCommand = "python {} --dataset {} --gpu {} --n-epochs 50".format(base_path.expanduser(), dataset, gpu_id)
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    lines = str(output).split("\\n")
-    time_list = []
-    for line in lines:
-        print(line)
-        if 'Time' in line:
-            time_str = line.strip().split('|')[1]
-            time = float(time_str.split()[-1])
-            time_list.append(time)
-    return np.array(time_list)[-10:].mean()
+class GCNBenchmark:
 
-    
-def track_gcn_accuracy(dataset, gpu_id):
-    bashCommand = "python {} --dataset {} --gpu {} --n-epochs 100".format(base_path.expanduser(), dataset, gpu_id)
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    lines = str(output).split("\\n")
-    test_acc = -1
-    for line in lines:
-        if 'Test accuracy' in line:
-            test_acc = float(line.split()[-1][:-1])
-            print(test_acc)
-    return test_acc
+    params = [['cora', 'pubmed'], ['0', '-1']]
+    param_names = ['dataset', 'gpu_id']
+    timeout = 120
+
+    def setup(self):
+        gcn_path = base_path / "examples/pytorch/gcn/train.py"
+        bashCommand = "python {} --dataset {} --gpu {} --n-epochs 50".format(gcn_path.expanduser(), dataset, gpu_id)
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        self.output = output
+
+    def track_gcn_time(self, dataset, gpu_id):
+        lines = str(self.output).split("\\n")
+        time_list = []
+        for line in lines:
+            print(line)
+            if 'Time' in line:
+                time_str = line.strip().split('|')[1]
+                time = float(time_str.split()[-1])
+                time_list.append(time)
+        return np.array(time_list)[-10:].mean()
+        
+    def track_gcn_accuracy(self, dataset, gpu_id):
+        lines = str(self.output).split("\\n")
+        test_acc = -1
+        for line in lines:
+            if 'Test accuracy' in line:
+                test_acc = float(line.split()[-1][:-1])
+                print(test_acc)
+        return test_acc
 
 
-track_gcn_time.unit = 's'
-track_gcn_time.params = [['cora', 'pubmed'], ['0', '-1']]
-track_gcn_accuracy.unit = '%'
-track_gcn_accuracy.params = [['cora', 'pubmed'], ['0', '-1']]
+GCNBenchmark.track_gcn_time.unit = 's'
+GCNBenchmark.track_gcn_accuracy.unit = '%'
