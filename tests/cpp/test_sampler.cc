37ecf656ca1fba4325425d36b6c96b648b817335
yzh119 <expye@outlook.com>
2019-12-31 01:24:33 +0800

    fix

--- tests/cpp/test_sampler.cc
+++ tests/cpp/test_sampler.cc
@@ -6,8 +6,38 @@
 #include "./common.h"
 
 using namespace dgl;
-using namespace dgl::runtime;
 
+template <typename Idx, typename DType>
+void _TestWithReplacement(RandomEngine *re) {
+  Idx n_categories = 1000;
+  Idx n_rolls = 100000;
+  std::vector<DType> prob;
+  std::vector<bool> counter(n_categories);
+  DType accum = 0.;
+  for (Idx i = 0; i < n_categories; ++i) {
+    prob.push_back(re->Uniform<DType>());
+    accum += prob.back();
+  }
+  for (Idx i = 0; i < n_categories; ++i)
+    prob[i] /= accum;
+
+  auto _test_given_sampler = [](BaseSampler *s) {
+    std::fill(counter.begin(), counter.end(), 0);
+    for (Idx i = 0; i < n_rolls; ++i) {
+      Idx dice = s->draw();
+      counter[dice]++;
+    }
+    for (Idx i = 0; i < n_categories; ++i)
+      ASSERT_NEAR(static_cast<DType>(counter[i]) / n_rolls, prob[i], 1e-2);
+  };
+
+  AliasSampler<Idx, DType, true> as(&re, prob);
+  CDFSampler<Idx, DType, true> cs(&re, prob);
+  TreeSampler<Idx, DType, true> ts(&re, prob);
+  _test_given_sampler(&as);
+  _test_given_sampler(&cs);
+  _test_given_sampler(&ts);
+}
 
 TEST(SampleUtilsTest, TestWithReplacement) {
   RandomEngine re();
@@ -28,36 +58,3 @@ TEST(SampleUtilsTest, TestWithoutReplacementOrder) {
 TEST(SampleUtilsTest, TestWithoutReplacementUnique) {
 
 };
-
-template <typename Idx,
-    typename DType>
-void _TestWithReplacement(RandomEngine *re) {
-  Idx n_categories = 1000;
-  Idx n_rolls = 100000;
-  std::vector<DType> prob;
-  std::vector<bool> counter(n_categories);
-  DType accum = 0.;
-  for (Idx i = 0; i < n_categories; ++i) {
-    prob.push_back(re->Uniform<DType>());
-    accum += prob.back();
-  }
-  for (Idx i = 0; i < n_categories; ++i)
-    prob[i] /= accum;
-
-  void _TestGivenSampler(BaseSampler *s, std::vector<bool>& counter) {
-    std::fill(counter.begin(), counter.end(), 0);
-    for (Idx i = 0; i < n_rolls; ++i) {
-      Idx dice = s->draw();
-      counter[dice]++;
-    }
-    for (Idx i = 0; i < n_categories; ++i)
-      ASSERT_NEAR(static_cast<DType>(counter[i]) / n_rolls, prob[i], 1e-2);
-  }
-
-  AliasSampler<Idx, DType, true> as(&re, prob);
-  CDFSampler<Idx, DType, true> cs(&re, prob);
-  TreeSampler<Idx, DType, true> ts(&re, prob);
-  _TestGivenSampler(&as);
-  _TestGivenSampler(&cs);
-  _TestGivenSampler(&ts);
-}
