a00636a02b61a1e0a1d5f330bd0e6a19c28d69a3
VoVAllen <VoVAllen@users.noreply.github.com>
2020-01-19 22:48:40 +0800

    [Tensorflow] Several nn & example  (#1191)
    * several nn example
    
    * appnp
    
    * fix lint
    
    * lint
    
    * add dgi
    
    * fix
    
    * fix
    
    * fix
    
    * fff
    
    * docs
    
    * 111
    
    * fix
    
    * change init
    
    * change result
    
    * tiaocan+1
    
    * fix
    
    * fix lint
    
    * fix
    
    * fix

---
+++ examples/tensorflow/gcn/gcn.py
@@ -0,0 +1,39 @@
+"""GCN using DGL nn package
+
+References:
+- Semi-Supervised Classification with Graph Convolutional Networks
+- Paper: https://arxiv.org/abs/1609.02907
+- Code: https://github.com/tkipf/gcn
+"""
+import tensorflow as tf
+from tensorflow.keras import layers
+from dgl.nn.tensorflow import GraphConv
+
+class GCN(tf.keras.Model):
+    def __init__(self,
+                 g,
+                 in_feats,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 activation,
+                 dropout):
+        super(GCN, self).__init__()
+        self.g = g
+        self.layer_list = []
+        # input layer
+        self.layer_list.append(GraphConv(in_feats, n_hidden, activation=activation))
+        # hidden layers
+        for i in range(n_layers - 1):
+            self.layer_list.append(GraphConv(n_hidden, n_hidden, activation=activation))
+        # output layer
+        self.layer_list.append(GraphConv(n_hidden, n_classes))
+        self.dropout = layers.Dropout(dropout)
+
+    def call(self, features):
+        h = features
+        for i, layer in enumerate(self.layer_list):
+            if i != 0:
+                h = self.dropout(h)
+            h = layer(self.g, h)
+        return h
