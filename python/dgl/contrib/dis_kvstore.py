5950034c44ae7c73f72f8ae471c07daf767823ec
aksnzhy <mctt90@gmail.com>
2020-02-13 13:27:22 +0800

    update

--- python/dgl/contrib/dis_kvstore.py
+++ python/dgl/contrib/dis_kvstore.py
@@ -244,6 +244,12 @@ class KVServer(object):
           5. Service loop for listening requests from client nodes.
 
         """
+        print("------------------------")
+        print("Server ID: %d" % self.get_id())
+        for name, data in self._data_store.items():
+            print("name: " + name)
+            print(data)
+        print("------------------------")
         # Get connected with all client nodes
         server_ip, server_port = self._addr.split(':')
         _receiver_wait(self._receiver, server_ip, int(server_port), self._client_count)
