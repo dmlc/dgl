7a506570ac6a72dd0f51e5d0b722e276aeb8f9ed
aksnzhy <mctt90@gmail.com>
2020-02-17 12:17:10 +0800

    update

--- python/dgl/contrib/dis_kvstore.py
+++ python/dgl/contrib/dis_kvstore.py
@@ -614,6 +614,8 @@ class KVClient(object):
         # Used load-balance
         random.seed(time.time())
 
+        self._flag = 0
+
 
     def __del__(self):
         """Finalize KVClient
@@ -809,7 +811,15 @@ class KVClient(object):
                     data=partial_data,
                     c_ptr=None)
                 # randomly select a server node in target machine for load-balance
-                s_id = random.randint(machine[idx]*self._group_count, (machine[idx]+1)*self._group_count-1)
+                #s_id = random.randint(machine[idx]*self._group_count, (machine[idx]+1)*self._group_count-1)
+                
+                if self._flag == 0:
+                    s_id = machine[idx]*self._group_count
+                    self._flag = 1
+                else:
+                    s_id = (machine[idx]+1)*self._group_count-1
+                    self._flag = 0
+
                 _send_kv_msg(self._sender, msg, s_id)
 
             start += count[idx]
