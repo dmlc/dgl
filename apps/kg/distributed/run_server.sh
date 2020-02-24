c36fad8fd6b33737de979b86005d314d9e2dcd84
aksnzhy <mctt90@gmail.com>
2020-02-24 13:33:51 +0800

    update

--- apps/kg/distributed/run_server.sh
+++ apps/kg/distributed/run_server.sh
@@ -1,7 +1,7 @@
 COUNTER=0
 while [ $COUNTER -lt 13 ]
 do
-    MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 DGLBACKEND=pytorch python3 ../kvserver.py --model TransE_l2 --dataset Freebase \
-    --hidden_dim 400 --gamma 10 --lr 0.1 --total_client 140 --server_id $COUNTER &
+    MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 DGLBACKEND=pytorch python3 ../kvserver.py --model DistMult --dataset Freebase \
+    --hidden_dim 400 --gamma 143 --lr 0.08 --total_client 140 --server_id $COUNTER &
     let COUNTER+=1
 done
