d79e513e10ff1a00614cde39ef07e96774cd78c5
aksnzhy <mctt90@gmail.com>
2020-02-24 18:23:34 +0800

    update

--- apps/kg/distributed/luanch.sh
+++ apps/kg/distributed/luanch.sh
@@ -1,3 +1,4 @@
+# TODO (chao) re-build a luanch script
 ssh -i ~/mctt.pem ubuntu@18.221.176.194 'cd ~/dgl/apps/kg/distributed; ./server.sh & ./worker.sh' &
 ssh -i ~/mctt.pem ubuntu@3.17.157.253 'cd ~/dgl/apps/kg/distributed; ./server.sh & ./worker.sh' &
 ssh -i ~/mctt.pem ubuntu@13.59.205.163 'cd ~/dgl/apps/kg/distributed; ./server.sh & ./worker.sh' &
