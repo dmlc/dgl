69bb51e3572d5e4bf24840d7b0288ad7ebd12c56
aksnzhy <mctt90@gmail.com>
2020-02-24 17:34:59 +0800

    update

--- apps/kg/distributed/luanch.sh
+++ apps/kg/distributed/luanch.sh
@@ -1,5 +1,5 @@
-ssh -i ~/mctt.pem ubuntu@18.221.176.194 'cd ~/dgl/apps/kg/distributed; ./run_server.sh & ./run_client.sh' &
-ssh -i ~/mctt.pem ubuntu@3.17.157.253 'cd ~/dgl/apps/kg/distributed; ./run_server.sh & ./run_client.sh' &
-ssh -i ~/mctt.pem ubuntu@13.59.205.163 'cd ~/dgl/apps/kg/distributed; ./run_server.sh & ./run_client.sh' &
-./run_server.sh & 
-./run_client.sh
+ssh -i ~/mctt.pem ubuntu@18.221.176.194 'cd ~/dgl/apps/kg/distributed; ./server.sh & ./worker.sh' &
+ssh -i ~/mctt.pem ubuntu@3.17.157.253 'cd ~/dgl/apps/kg/distributed; ./server.sh & ./worker.sh' &
+ssh -i ~/mctt.pem ubuntu@13.59.205.163 'cd ~/dgl/apps/kg/distributed; ./server.sh & ./worker.sh' &
+./server.sh & 
+./worker.sh
