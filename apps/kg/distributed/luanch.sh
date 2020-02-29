a8ceb4b446b8da1dfc74dd864cabf2b2c70a04bf
aksnzhy <mctt90@gmail.com>
2020-02-29 13:24:30 +0800

    update

--- apps/kg/distributed/luanch.sh
+++ apps/kg/distributed/luanch.sh
@@ -2,7 +2,7 @@ SCRIPT_PATH=~/dgl/apps/kg/distributed
 SCRIPT_FILE=./fb15k_transe_l2.sh
 KEY=~/mctt.pem
 
-ssh -i $KEY ubuntu@18.216.60.5 'cd '$SCRIPT_PATH'; '$SCRIPT_FILE' 13 26' &
-ssh -i $KEY ubuntu@3.136.27.4 'cd '$SCRIPT_PATH'; '$SCRIPT_FILE' 26 39' &
-ssh -i $KEY ubuntu@52.14.94.4 'cd '$SCRIPT_PATH'; '$SCRIPT_FILE' 39 52' &
-$SCRIPT_FILE 0 13
+ssh -i $KEY ubuntu@18.216.60.5 'cd '$SCRIPT_PATH'; '$SCRIPT_FILE' 8 16' &
+ssh -i $KEY ubuntu@3.136.27.4 'cd '$SCRIPT_PATH'; '$SCRIPT_FILE' 16 24' &
+ssh -i $KEY ubuntu@52.14.94.4 'cd '$SCRIPT_PATH'; '$SCRIPT_FILE' 24 32' &
+$SCRIPT_FILE 0 8
