57e5670ab6ddf94576533116445ddf88258ba378
aksnzhy <mctt90@gmail.com>
2020-03-01 23:03:48 +0800

    update

--- apps/kg/distributed/launch.sh
+++ apps/kg/distributed/launch.sh
@@ -15,12 +15,14 @@ machine_count=$(awk 'END{print NR}' ip_config.txt)
 # run command on remote machine
 LINE_LOW=2
 LINE_HIGH=$machine_count
+s_id=0
 while [ $LINE_LOW -lt $LINE_HIGH ]
 do
     ip=$(awk 'NR=='$LINE_LOW' {print $1}' ip_config.txt)
     let LINE_LOW+=1
-    echo yes | ssh -i $ssh_key $user_name@$ip 'cd '$script_path'; '$script_file' '$i' '$server_count' ' &
+    let s_id+=1
+    echo yes | ssh -i $ssh_key $user_name@$ip 'cd '$script_path'; '$script_file' '$s_id' '$server_count' ' &
 done
 
 # run command on local machine
-$script_file 1 $server_count
+$script_file 0 $server_count
