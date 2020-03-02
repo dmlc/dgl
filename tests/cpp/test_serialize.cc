302d36bd3e1e2a0cac14daf8888974b8972a0dee
VoVAllen <jz1749@nyu.edu>
2020-03-02 17:06:06 +0000

    fix

--- tests/cpp/test_serialize.cc
+++ tests/cpp/test_serialize.cc
@@ -13,26 +13,44 @@ using namespace dgl;
 using namespace dgl::aten;
 using namespace dmlc;
 
-TEST(Serialize, UnitGraph) {
+TEST(Serialize, UnitGraph_COO) {
   aten::CSRMatrix csr_matrix;
   auto src = VecToIdArray<int64_t>({1, 2, 5, 3});
   auto dst = VecToIdArray<int64_t>({1, 6, 2, 6});
   auto mg = std::dynamic_pointer_cast<UnitGraph>(
-      dgl::UnitGraph::CreateFromCOO(2, 9, 8, src, dst));
+      dgl::UnitGraph::CreateFromCOO(2, 9, 8, src, dst, dgl::SparseFormat::COO));
 
   std::string blob;
   dmlc::MemoryStringStream ifs(&blob);
 
-  static_cast<dmlc::Stream*>(&ifs)->Write(mg);
+  static_cast<dmlc::Stream *>(&ifs)->Write(mg);
 
   dmlc::MemoryStringStream ofs(&blob);
   auto ug2 = Serializer::make_shared<UnitGraph>();
-  static_cast<dmlc::Stream*>(&ofs)->Read(&ug2);
+  static_cast<dmlc::Stream *>(&ofs)->Read(&ug2);
   EXPECT_EQ(ug2->NumVertices(0), 9);
   EXPECT_EQ(ug2->NumVertices(1), 8);
   EXPECT_EQ(ug2->NumEdges(0), 4);
   EXPECT_EQ(ug2->FindEdge(0, 1).first, 2);
   EXPECT_EQ(ug2->FindEdge(0, 1).second, 6);
+}
+
+TEST(Serialize, UnitGraph_CSR) {
+  aten::CSRMatrix csr_matrix;
+  auto src = VecToIdArray<int64_t>({1, 2, 5, 3});
+  auto dst = VecToIdArray<int64_t>({1, 6, 2, 6});
+  auto mg = std::dynamic_pointer_cast<UnitGraph>(
+      dgl::UnitGraph::CreateFromCOO(2, 9, 8, src, dst, dgl::SparseFormat::CSR));
+
+  std::string blob;
+  dmlc::MemoryStringStream ifs(&blob);
+
+  static_cast<dmlc::Stream *>(&ifs)->Write(mg);
+
+  dmlc::MemoryStringStream ofs(&blob);
+  auto ug2 = Serializer::make_shared<UnitGraph>();
+  static_cast<dmlc::Stream *>(&ofs)->Read(&ug2);
+  // Query operation is not supported on CSR, how to check it?
 }
 
 TEST(Serialize, ImmutableGraph) {
@@ -42,11 +60,11 @@ TEST(Serialize, ImmutableGraph) {
   std::string blob;
   dmlc::MemoryStringStream ifs(&blob);
 
-  static_cast<dmlc::Stream*>(&ifs)->Write(gptr);
+  static_cast<dmlc::Stream *>(&ifs)->Write(gptr);
 
   dmlc::MemoryStringStream ofs(&blob);
   auto rptr_read = dgl::Serializer::make_shared<ImmutableGraph>();
-  static_cast<dmlc::Stream*>(&ofs)->Read(&rptr_read);
+  static_cast<dmlc::Stream *>(&ofs)->Read(&rptr_read);
   EXPECT_EQ(rptr_read->NumEdges(), 4);
   EXPECT_EQ(rptr_read->NumVertices(), 10);
   EXPECT_EQ(rptr_read->FindEdge(2).first, 5);
@@ -70,11 +88,11 @@ TEST(Serialize, HeteroGraph) {
 
   std::string blob;
   dmlc::MemoryStringStream ifs(&blob);
-  static_cast<dmlc::Stream*>(&ifs)->Write(hrptr);
+  static_cast<dmlc::Stream *>(&ifs)->Write(hrptr);
 
   dmlc::MemoryStringStream ofs(&blob);
   auto gptr = dgl::Serializer::make_shared<HeteroGraph>();
-  static_cast<dmlc::Stream*>(&ofs)->Read(&gptr);
+  static_cast<dmlc::Stream *>(&ofs)->Read(&gptr);
   EXPECT_EQ(gptr->NumVertices(0), 9);
   EXPECT_EQ(gptr->NumVertices(1), 8);
 }
