03783e863a9c4eee15f059a35c2790a078ae3542
Quan Gan <coin2028@hotmail.com>
2020-03-04 11:03:28 +0800

    rename load_backend to private

--- python/dgl/nn/__init__.py
+++ python/dgl/nn/__init__.py
@@ -3,10 +3,10 @@ import os
 import importlib
 import sys
 
-def load_backend(mod_name):
+def _load_backend(mod_name):
     mod = importlib.import_module('.%s' % mod_name, __name__)
     thismod = sys.modules[__name__]
     for api, obj in mod.__dict__.items():
         setattr(thismod, api, obj)
 
-load_backend(os.environ.get('DGLBACKEND', 'pytorch').lower())
+_load_backend(os.environ.get('DGLBACKEND', 'pytorch').lower())
