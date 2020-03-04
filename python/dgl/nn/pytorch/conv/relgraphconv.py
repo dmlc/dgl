9b3d9be46e0e9245b61ed8eac8210625b2ea79cb
Simone Primarosa <simonepri@outlook.com>
2020-03-04 10:32:03 +0000

    Fix RelGraphConv when num_bases is None

--- python/dgl/nn/pytorch/conv/relgraphconv.py
+++ python/dgl/nn/pytorch/conv/relgraphconv.py
@@ -91,7 +91,7 @@ class RelGraphConv(nn.Module):
             # message func
             self.message_func = self.basis_message_func
         elif regularizer == "bdd":
-            if in_feat % num_bases != 0 or out_feat % num_bases != 0:
+            if in_feat % self.num_bases != 0 or out_feat % self.num_bases != 0:
                 raise ValueError('Feature size must be a multiplier of num_bases.')
             # add block diagonal weights
             self.submat_in = in_feat // self.num_bases
