b065f5dfcd39e0dbd2333011621d15ec52f5dc12
Quan Gan <coin2028@hotmail.com>
2020-03-04 10:56:55 +0800

    automatically select nn backend

--- python/dgl/nn/__init__.py
+++ python/dgl/nn/__init__.py
@@ -1,1 +1,12 @@
 """Package for neural network common components."""
+import os
+import importlib
+import sys
+
+def load_backend(mod_name):
+    mod = importlib.import_module('.%s' % mod_name, __name__)
+    thismod = sys.modules[__name__]
+    for api, obj in mod.__dict__.items():
+        setattr(thismod, api, obj)
+
+load_backend(os.environ.get('DGLBACKEND', 'pytorch').lower())
