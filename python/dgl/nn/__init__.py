8433657727c40326e05c41e261889476abb8677b
Quan Gan <coin2028@hotmail.com>
2020-03-05 19:16:07 +0800

    switch to backend_name

--- python/dgl/nn/__init__.py
+++ python/dgl/nn/__init__.py
@@ -1,7 +1,7 @@
 """Package for neural network common components."""
-import os
 import importlib
 import sys
+from ..backend import backend_name
 
 def _load_backend(mod_name):
     mod = importlib.import_module('.%s' % mod_name, __name__)
@@ -9,4 +9,4 @@ def _load_backend(mod_name):
     for api, obj in mod.__dict__.items():
         setattr(thismod, api, obj)
 
-_load_backend(os.environ.get('DGLBACKEND', 'pytorch').lower())
+_load_backend(backend_name)
