d2e708b55e35de2334b3fb562572d35d050322b6
Ubuntu <ubuntu@ip-172-31-60-78.ec2.internal>
2020-03-09 02:19:07 +0000

    upd

--- apps/kg/eval.py
+++ apps/kg/eval.py
@@ -31,7 +31,7 @@ class ArgParser(argparse.ArgumentParser):
                           help='dataset name, under data_path')
         self.add_argument('--format', type=str, default='built_in',
                           help='the format of the dataset, it can be built_in,'\
-                                'raw_udd_{{htr} and udd_{{htr}')
+                                'raw_udd_{htr} and udd_{htr}')
         self.add_argument('--data_files', type=str, default=None, nargs='+',
                           help='a list of data files, e.g. entity relation train valid test')
         self.add_argument('--model_path', type=str, default='ckpts',
