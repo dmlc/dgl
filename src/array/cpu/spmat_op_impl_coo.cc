1e2b8d0c827a67878475be713ccd88107ddb7cbc
Ubuntu <ubuntu@ip-172-31-51-214.ec2.internal>
2020-03-10 09:03:19 +0000

    Add COOSort test

--- src/array/cpu/spmat_op_impl_coo.cc
+++ src/array/cpu/spmat_op_impl_coo.cc
@@ -229,21 +229,21 @@ template COOMatrix COOTranspose<kDLCPU, int64_t>(COOMatrix coo);
 // complexity: time O(NNZ), space O(1)
 template <DLDeviceType XPU, typename IdType>
 CSRMatrix COOToCSR(COOMatrix coo) {
+  coo = COOSort(coo, false);
   const int64_t N = coo.num_rows;
   const int64_t NNZ = coo.row->shape[0];
   const IdType* row_data = static_cast<IdType*>(coo.row->data);
-  const IdType* col_data = static_cast<IdType*>(coo.col->data);
-  const IdType* data = COOHasData(coo)? static_cast<IdType*>(coo.data->data) : nullptr;
+  // const IdType* col_data = static_cast<IdType*>(coo.col->data);
+  // const IdType* data = COOHasData(coo)? static_cast<IdType*>(coo.data->data) : nullptr;
   NDArray ret_indptr = NDArray::Empty({N + 1}, coo.row->dtype, coo.row->ctx);
-  NDArray ret_indices = NDArray::Empty({NNZ}, coo.row->dtype, coo.row->ctx);
-  NDArray ret_data = NDArray::Empty({NNZ}, coo.row->dtype, coo.row->ctx);
+  NDArray ret_indices = coo.col;
+  NDArray ret_data = coo.data;
 
   IdType* Bp = static_cast<IdType*>(ret_indptr->data);
-  IdType* Bi = static_cast<IdType*>(ret_indices->data);
-  IdType* Bx = static_cast<IdType*>(ret_data->data);
+  // IdType* Bi = static_cast<IdType*>(ret_indices->data);
+  // IdType* Bx = static_cast<IdType*>(ret_data->data);
 
   std::fill(Bp, Bp + N, 0);
-
   for (int64_t i = 0; i < NNZ; ++i) {
     Bp[row_data[i]]++;
   }
@@ -255,6 +255,12 @@ CSRMatrix COOToCSR(COOMatrix coo) {
     cumsum += temp;
   }
   Bp[N] = NNZ;
+
+  /*
+  for (int64_t i = 0; i < NNZ; ++i) {
+    Bi[i] = col_data[i];
+    Bx[i] = data[i];
+  }
 
   for (int64_t i = 0; i < NNZ; ++i) {
     const IdType r = row_data[i];
@@ -269,7 +275,7 @@ CSRMatrix COOToCSR(COOMatrix coo) {
     Bp[i] = last;
     last = temp;
   }
-
+*/
   return CSRMatrix(coo.num_rows, coo.num_cols,
                    ret_indptr, ret_indices, ret_data,
                    coo.col_sorted);
