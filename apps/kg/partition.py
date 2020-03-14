4ec308de850742e89288185408c8f6d845593dba
aksnzhy <mctt90@gmail.com>
2020-03-14 11:10:27 +0800

    update

--- apps/kg/partition.py
+++ apps/kg/partition.py
@@ -87,7 +87,7 @@ def main():
 
         part.copy_from_parent()
         #save_graphs(args.data_path + '/part_' + str(part_id) + '.dgl', [part])
-    write_graph_txt(args.data_path+'FB15k/partition_', 'train.txt', part_dict, g.number_of_nodes())
+    write_graph_txt(args.data_path+'/FB15k/partition_', 'train.txt', part_dict, g.number_of_nodes())
     print('there are {} edges in the graph and {} edge cuts for {} partitions.'.format(
         g.number_of_edges(), g.number_of_edges() - tot_num_inner_edges, len(part_dict)))
 
