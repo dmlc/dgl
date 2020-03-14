6e2c2669ff32af2d08b8db43ccdb0a55fcf62fe5
Da Zheng <zhengda1936@gmail.com>
2020-03-14 04:50:15 +0000

    a temp script to save reddit.

---
+++ tools/save_reddit.py
@@ -0,0 +1,12 @@
+from dgl import backend as F
+from dgl.data import RedditDataset
+from dgl.data.utils import load_graphs, save_graphs
+
+data = RedditDataset()
+g = data.graph
+g.ndata['feats'] = F.tensor(data.features)
+g.ndata['labels'] = F.tensor(data.labels)
+g.ndata['test_mask'] = F.tensor(data.test_mask)
+g.ndata['train_mask'] = F.tensor(data.train_mask)
+g.ndata['val_mask'] = F.tensor(data.val_mask)
+save_graphs('reddit/reddit.dgl', [g])
