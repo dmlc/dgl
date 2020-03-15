db52e0ff5c45d1584543a6d013a33baab0401f89
Da Zheng <zhengda1936@gmail.com>
2020-03-15 01:44:01 +0000

    fix temp script.

--- tools/save_reddit.py
+++ tools/save_reddit.py
@@ -4,7 +4,7 @@ from dgl.data.utils import load_graphs, save_graphs
 
 data = RedditDataset()
 g = data.graph
-g.ndata['feats'] = F.tensor(data.features)
+g.ndata['features'] = F.tensor(data.features)
 g.ndata['labels'] = F.tensor(data.labels)
 g.ndata['test_mask'] = F.tensor(data.test_mask)
 g.ndata['train_mask'] = F.tensor(data.train_mask)
