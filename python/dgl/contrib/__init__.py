e5d445af9482cec081e858473510835a87c240fa
Ubuntu <ubuntu@ip-172-31-16-150.us-west-2.compute.internal>
2020-03-22 08:42:17 +0000

    fix.

--- python/dgl/contrib/__init__.py
+++ python/dgl/contrib/__init__.py
@@ -1,4 +1,5 @@
 from . import sampling
 from . import graph_store
 from .dis_kvstore import KVClient, KVServer
+from .dist_graph_store import DistGraphStoreServer, DistGraphStore
 from .dis_kvstore import read_ip_config
