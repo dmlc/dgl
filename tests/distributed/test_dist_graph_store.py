d9d5bb850bbdb0bf9d1d912ad927cdb04f539b12
Ubuntu <ubuntu@ip-172-31-16-150.us-west-2.compute.internal>
2020-03-23 19:23:33 +0000

    fix tests.

--- tests/distributed/test_dist_graph_store.py
+++ tests/distributed/test_dist_graph_store.py
@@ -1,9 +1,11 @@
 import dgl
 import sys
 import numpy as np
+import time
 from scipy import sparse as spsp
 from numpy.testing import assert_array_equal
 from multiprocessing import Process, Manager, Condition, Value
+import multiprocessing as mp
 from dgl.graph_index import create_graph_index
 from dgl.data.utils import load_graphs, save_graphs
 from dgl.contrib import DistGraphStoreServer, DistGraphStore
@@ -17,14 +19,22 @@ def create_random_graph(n):
     ig = create_graph_index(arr, multigraph=False, readonly=True)
     return dgl.DGLGraph(ig)
 
-def run_server(graph_name, server_id, num_clients):
+def run_server(graph_name, server_id, num_clients, barrier):
     server_data = './server-' + str(server_id) + '.dgl'
     client_data = './client-' + str(server_id) + '.dgl'
     g = DistGraphStoreServer(server_namebook, server_id, graph_name, server_data, client_data, num_clients)
+    barrier.wait()
+    print('start server', server_id)
     g.start()
 
-def run_client(graph_name):
+def run_client(graph_name, part, barrier):
+    barrier.wait()
     g = DistGraphStore(server_namebook, graph_name)
+    print('|V|={}, |E|={}, #local:{}'.format(g.g.number_of_nodes(), g.g.number_of_edges(), len(g.local_nids)))
+    assert part.number_of_nodes() == g.g.number_of_nodes()
+    assert part.number_of_edges() == g.g.number_of_edges()
+    assert np.all(F.asnumpy(part.ndata['part_id'] == g.g.ndata['part_id']))
+    print('end')
 
 def test_create():
     g = create_random_graph(10000)
@@ -48,14 +58,17 @@ def test_create():
     graph_name = 'test'
     # let's just test on one partition for now.
     # We cannot run multiple servers and clients on the same machine.
+    barrier = mp.Barrier(2)
     serv_ps = []
     for serv_id in range(1):
-        p = Process(target=run_server, args=(graph_name, serv_id, 1))
+        p = Process(target=run_server, args=(graph_name, serv_id, 1, barrier))
         serv_ps.append(p)
         p.start()
+
     cli_ps = []
     for cli_id in range(1):
-        p = Process(target=run_client, args=('test',))
+        print('start client', cli_id)
+        p = Process(target=run_client, args=(graph_name, client_parts[cli_id], barrier))
         p.start()
         cli_ps.append(p)
 
@@ -63,6 +76,7 @@ def test_create():
     #    p.join()
     for p in cli_ps:
         p.join()
+    print('clients have terminated')
 
 if __name__ == '__main__':
     test_create()
