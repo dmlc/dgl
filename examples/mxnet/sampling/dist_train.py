2f2471ee677335c756d7c69f2bb2bbb56c0bb31c
Ubuntu <ubuntu@ip-172-31-16-150.us-west-2.compute.internal>
2020-03-24 07:42:13 +0000

    remove code of defining DistGraphStore.

--- examples/mxnet/sampling/dist_train.py
+++ examples/mxnet/sampling/dist_train.py
@@ -12,91 +12,21 @@ from mxnet import gluon
 import dgl
 from dgl import DGLGraph
 from dgl.data import register_data_args, load_data
-from dgl.contrib import KVServer, KVClient
+from dgl.contrib import DistGraphStoreServer, DistGraphStore
 from dgl.data.utils import load_graphs
 import socket
 
 from gcn_ns_dist import gcn_ns_train
 
-class DistGraphStoreServer(KVServer):
-    def __init__(self, ip_config, server_id, server_data, num_client):
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
-        print('Server {}: host name: {}, ip: {}'.format(server_id, host_name, host_ip))
-
-        server_namebook = dgl.contrib.read_ip_config(filename=ip_config)
-        super(DistGraphStoreServer, self).__init__(server_id=server_id, server_namebook=server_namebook, num_client=num_client)
-
-        self.part_g = load_graphs(server_data)[0][0]
-        num_nodes = np.max(self.part_g.ndata[dgl.NID].asnumpy()) + 1
-        self.g2l = mx.nd.zeros((num_nodes), dtype=np.int64)
-        self.g2l[:] = -1
-        self.g2l[self.part_g.ndata[dgl.NID]] = mx.nd.arange(self.part_g.number_of_nodes())
-        if self.get_id() % self.get_group_count() == 0: # master server
-            for ndata_name in self.part_g.ndata.keys():
-                print(ndata_name)
-                self.set_global2local(name=ndata_name, global2local=self.g2l)
-                self.init_data(name=ndata_name, data_tensor=self.part_g.ndata[ndata_name])
-        else:
-            for ndata_name in self.part_g.ndata.keys():
-                self.set_global2local(name=ndata_name)
-                self.init_data(name=ndata_name)
-        # TODO Do I need synchronization?
-
-    def _pull_handler(self, name, lID, target):
-        #lID = self.g2l[gID].asnumpy()
-        #gID = gID.asnumpy()
-        #print(gID[lID == -1])
-        assert np.sum(lID == -1) == 0
-        return target[name][lID]
-
 def start_server(args):
-    serv = DistGraphStoreServer(args.ip_config, args.id, args.server_data, args.num_client)
+    server_namebook = dgl.contrib.read_ip_config(filename=args.ip_config)
+    serv = DistGraphStoreServer(server_namebook, args.id, args.graph_name,
+                                args.server_data, args.client_data, args.num_client)
     serv.start()
 
-class DistGraphStore:
-    def __init__(self, ip_config, graph_path):
-        server_namebook = dgl.contrib.read_ip_config(filename=ip_config)
-        self._client = KVClient(server_namebook=server_namebook)
-        self._client.connect()
-
-        # TODO this cannot guarantee data locality.
-        self.part_id = self._client.get_id()
-        self.g = load_graphs(graph_path + '/client-' + str(self.part_id) + '.dgl')[0][0]
-        # TODO If we don't have HALO nodes, how do we set partition?
-        num_nodes = np.max(self.g.ndata[dgl.NID].asnumpy()) + 1
-        partition = mx.nd.zeros(shape=(num_nodes,), dtype=np.int64)
-        partition[self.g.ndata[dgl.NID]] = self.g.ndata['part_id']
-        # TODO what is the node data name?
-        self._client.set_partition_book(name='features', partition_book=partition)
-        self._client.set_partition_book(name='labels', partition_book=partition)
-        self._client.set_partition_book(name='test_mask', partition_book=partition)
-        self._client.set_partition_book(name='val_mask', partition_book=partition)
-        self._client.set_partition_book(name='train_mask', partition_book=partition)
-
-        self._client.barrier()
-
-        self.local_nids = np.nonzero((self.g.ndata['part_id'] == self.part_id).asnumpy())[0]
-        self.local_gnid = self.g.ndata[dgl.NID][self.local_nids]
-
-    def number_of_nodes(self):
-        return len(self.local_gnid)
-
-    def get_local_nids(self):
-        return self.local_nids
-
-    def get_id(self):
-        return self.part_id
-
-    def get_ndata(self, name, nids=None):
-        if nids is None:
-            gnid = self.local_gnid
-        else:
-            gnid = self.g.ndata[dgl.NID][nids]
-        return self._client.pull(name=name, id_tensor=gnid)
-
 def main(args):
-    g = DistGraphStore(args.ip_config, args.graph_path)
+    server_namebook = dgl.contrib.read_ip_config(filename=args.ip_config)
+    g = DistGraphStore(args.ip_config, args.graph_name)
 
     # We need to set random seed here. Otherwise, all processes have the same mini-batches.
     mx.random.seed(g.get_id())
