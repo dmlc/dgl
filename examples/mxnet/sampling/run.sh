4ebb32f4332b501d7b4bf75bdf1715ac18f504a4
Ubuntu <ubuntu@ip-172-31-16-250.us-west-2.compute.internal>
2020-03-27 09:18:46 +0000

    update run.sh.

--- examples/mxnet/sampling/run.sh
+++ examples/mxnet/sampling/run.sh
@@ -1,9 +1,7 @@
 export OMP_NUM_THREADS=4
 export DGLBACKEND=mxnet
-python3 examples/mxnet/sampling/dist_train.py --ip_config examples/mxnet/sampling/ip_config.txt --num-client 4 --id 0 --server_data reddit/server-0.dgl --server &
-python3 examples/mxnet/sampling/dist_train.py --ip_config examples/mxnet/sampling/ip_config.txt --num-client 4 --id 1 --server_data reddit/server-1.dgl --server &
-python3 examples/mxnet/sampling/dist_train.py --ip_config examples/mxnet/sampling/ip_config.txt --num-client 4 --id 2 --server_data reddit/server-2.dgl --server &
-python3 examples/mxnet/sampling/dist_train.py --ip_config examples/mxnet/sampling/ip_config.txt --num-client 4 --id 3 --server_data reddit/server-3.dgl --server &
+python3 examples/mxnet/sampling/dist_train.py --ip_config ../ip_config.txt --num-client 2 --id 0 --server_data ../reddit/server-0.dgl --server --client_data ../reddit/client-0.dgl --graph-name reddit --model gcn_ns &
+python3 examples/mxnet/sampling/dist_train.py --ip_config ../ip_config.txt --num-client 2 --id 1 --server_data ../reddit/server-1.dgl --server --client_data ../reddit/client-1.dgl --graph-name reddit --model gcn_ns &
 
 export OMP_NUM_THREADS=4
-python3 /usr/local/lib/python3.6/dist-packages/mxnet/tools/launch.py -n 4 --launcher local python3 examples/mxnet/sampling/dist_train.py --ip_config examples/mxnet/sampling/ip_config.txt --graph-path reddit --model gcn_ns --n-classes 41 --num-neighbors 10 --n-features 602 &
+python3 /usr/local/lib/python3.6/dist-packages/mxnet/tools/launch.py -n 2 --launcher ssh -H hosts.txt python3 examples/mxnet/sampling/dist_train.py --ip_config ../ip_config.txt --model gcn_ns --n-classes 41 --num-neighbors 10 --n-features 602 --graph-name reddit &
