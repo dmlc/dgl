9b0fd9da37eac92d48aaffd9552816c93c2becc7
Jinjing Zhou <VoVAllen@users.noreply.github.com>
2020-04-02 20:31:07 +0800

    [Docs] Fix doc build doesn't work for nn.tensorflow and nn.mxnet (#1413)
    * fix
    
    * lint
    
    * fix

--- python/dgl/nn/__init__.py
+++ python/dgl/nn/__init__.py
@@ -1,1 +1,22 @@
 """Package for neural network common components."""
+import importlib
+import sys
+import os
+from ..backend import backend_name
+
+
+def _load_backend(mod_name):
+    mod = importlib.import_module('.%s' % mod_name, __name__)
+    thismod = sys.modules[__name__]
+    for api, obj in mod.__dict__.items():
+        setattr(thismod, api, obj)
+
+
+LOAD_ALL = os.getenv("DGL_LOADALL", "False")
+
+if LOAD_ALL.lower() != "false":
+    from .mxnet import *
+    from .pytorch import *
+    from .tensorflow import *
+else:
+    _load_backend(backend_name)
