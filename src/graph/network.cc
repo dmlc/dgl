5a29849b1b5b93da6246efd55b19fdce1610a2af
aksnzhy <mctt90@gmail.com>
2020-04-17 17:48:13 +0800

    update

--- src/graph/network.cc
+++ src/graph/network.cc
@@ -716,6 +716,8 @@ DGL_REGISTER_GLOBAL("network._CAPI_FastPull")
 #ifndef _WIN32  // windows does not support rand_r()
         int s_id = (rand_r(&seed) % (higher-lower+1))+lower;
         send_kv_message(sender, &kv_msg, s_id, !AUTO_FREE);
+#else
+        LOG(FATAL) << "KVStore does not support Windows yet.";
 #endif
         msg_count++;
       }
