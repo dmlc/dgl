577d69ffc2dbc1bef9ad1d8c98d39d9378c57ebe
mufeili <mufeili1996@gmail.com>
2020-04-20 21:54:56 +0800

    Update

--- apps/life_sci/python/dgllife/data/uspto.py
+++ apps/life_sci/python/dgllife/data/uspto.py
@@ -265,8 +265,13 @@ def process_file(path, num_processes):
     """
     with open(path, 'r') as input_file:
         lines = input_file.readlines()
-    with Pool(processes=num_processes) as pool:
-        results = list(tqdm(pool.imap(process_line, lines), total=len(lines)))
+    if num_processes == 1:
+        results = []
+        for li in lines:
+            results.append(process_line(li))
+    else:
+        with Pool(processes=num_processes) as pool:
+            results = list(tqdm(pool.imap(process_line, lines), total=len(lines)))
     with open(path + '.proc', 'w') as output_file:
         for line in results:
             output_file.write(line)
@@ -369,12 +374,19 @@ class WLNCenterDataset(object):
             self.reactant_mol_graphs, _ = load_graphs(mol_graph_path)
         else:
             print('Constructing graphs from scratch...')
-            torch.multiprocessing.set_sharing_strategy('file_system')
-            with Pool(processes=num_processes) as pool:
-                self.reactant_mol_graphs = list(tqdm(pool.imap(
-                    partial(mol_to_graph, node_featurizer=node_featurizer,
-                            edge_featurizer=edge_featurizer, canonical_atom_order=False),
-                    full_mols), total=len(full_mols)))
+            if num_processes == 1:
+                self.reactant_mol_graphs = []
+                for mol in full_mols:
+                    self.reactant_mol_graphs.append(mol_to_graph(
+                        mol, node_featurizer=node_featurizer,
+                        edge_featurizer=edge_featurizer, canonical_atom_order=False))
+            else:
+                torch.multiprocessing.set_sharing_strategy('file_system')
+                with Pool(processes=num_processes) as pool:
+                    self.reactant_mol_graphs = list(tqdm(pool.imap(
+                        partial(mol_to_graph, node_featurizer=node_featurizer,
+                                edge_featurizer=edge_featurizer, canonical_atom_order=False),
+                        full_mols), total=len(full_mols)))
 
             save_graphs(mol_graph_path, self.reactant_mol_graphs)
 
@@ -408,14 +420,21 @@ class WLNCenterDataset(object):
         all_graph_edits = []
         with open(file_path, 'r') as f:
             lines = f.readlines()
-        with Pool(processes=num_processes) as pool:
-            results = list(tqdm(pool.imap(load_one_reaction, lines), total=len(lines)))
-            for mol, reaction, graph_edits in results:
-                if mol is None:
-                    continue
-                all_mols.append(mol)
-                all_reactions.append(reaction)
-                all_graph_edits.append(graph_edits)
+        if num_processes == 1:
+            results = []
+            for li in lines:
+                mol, reaction, graph_edits = load_one_reaction(li)
+                results.append((mol, reaction, graph_edits))
+        else:
+            with Pool(processes=num_processes) as pool:
+                results = list(tqdm(pool.imap(load_one_reaction, lines), total=len(lines)))
+
+        for mol, reaction, graph_edits in results:
+            if mol is None:
+                continue
+            all_mols.append(mol)
+            all_reactions.append(reaction)
+            all_graph_edits.append(graph_edits)
 
         return all_mols, all_reactions, all_graph_edits
 
