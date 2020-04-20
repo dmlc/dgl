68ac0ed5be9f78b25a2095d30219f270485b87f4
aksnzhy <mctt90@gmail.com>
2020-04-20 11:35:15 +0800

    update

---
+++ tests/compute/test_kvstore.py
@@ -0,0 +1,64 @@
+import backend as F
+import numpy as np
+import scipy as sp
+import dgl
+from dgl import utils
+from dgl.contrib import KVServer
+from dgl.contrib import KVClient
+
+import os
+import time
+
+num_entries = 10
+dim_size = 10
+
+server_namebook = {0:[0, 127.0.0.1, 30070, 1]}
+
+def start_server():
+    my_server = KVServer(server_id=0, server_namebook=server_namebook, num_client=1)
+    data_0 = F.zeros((num_entries, dim_size), F.float32, F.cpu())
+    g2l_0 = F.arange(0, num_entries)
+    partition_0 = F.zeros(num_entries, F.int64, F.cpu())
+
+    data_1 = F.zeros((num_entries*2, dim_size), F.float32, F.cpu())
+    g2l_1 = F.arange(0, num_entries*2)
+    partition_1 = F.zeros(num_entries*2, F.int64, F.cpu())
+
+    my_server.set_global2local(name='data_0', global2local=g2l_0)
+    my_server.set_global2local(name='data_1', global2local=g2l_1)
+    my_server.set_partition_book(name='data_0', partition_book=partition_0)
+    my_server.set_partition_book(name='data_1', partition_book=partition_1)
+    my_server.init_data(name='data_0', data_tensor=data_0)
+    my_server.init_data(name='data_1', data_tensor=data_1)
+
+    name_list = my_server.get_data_name_list()
+    assert len(name_list) == 2
+    assert 'data_0' in name_list
+    assert 'data_1' in name_list
+
+    meta_0 = my_server.get_data_meta('data_0')
+    assert meta_0[0] == F.float32
+    assert meta_0[1] == F.shape(num_entries, dim_size)
+    assert meta_0[2] == partition_0
+
+    meta_1 = my_server.get_data_meta('data_1')
+    assert meta_1[0] == F.float32
+    assert meta_1[1] == F.shape(num_entries*2, dim_size)
+    assert meta_1[2] == partition_1
+
+    my_server.start()
+
+
+def start_client():
+    my_client = KVClient(server_namebook=server_namebook)
+    my_client.connect()
+    my_client.shut_down()
+
+
+if __name__ == '__main__':
+    pid = os.fork()
+    if pid == 0:
+        start_server()
+    else:
+        time.sleep(2) # wait trainer start
+        start_client()
