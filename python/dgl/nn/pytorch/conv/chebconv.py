4410b4e25ce1ff34966fb5543ad53d3ada5c2785
Axel Bernard Gunnar Nilsson <axel.nilsson@epfl.ch>
2020-04-22 16:34:04 +0800

    New version for the convolutional layer

--- python/dgl/nn/pytorch/conv/chebconv.py
+++ python/dgl/nn/pytorch/conv/chebconv.py
@@ -3,9 +3,23 @@
 import torch as th
 from torch import nn
 from torch.nn import init
+import torch.nn.functional as F
 
 from .... import laplacian_lambda_max, broadcast_nodes, function as fn
 
+class NodeApplyModule(nn.Module):
+    """Update the node feature hv with ReLU(Whv+b)."""
+
+    def __init__(self, in_feats, out_feats, k, activation, bias=True):
+        super(NodeApplyModule, self).__init__()
+        self.linear = nn.Linear(k * in_feats, out_feats, bias)
+        self.activation = activation
+
+    def forward(self, node):
+        h = self.linear(node.data['h'])
+        if self.activation:
+            h = self.activation(h)
+        return {'h': h}
 
 class ChebConv(nn.Module):
     r"""Chebyshev Spectral Graph Convolution layer from paper `Convolutional
@@ -31,37 +45,29 @@ class ChebConv(nn.Module):
         Number of output features.
     k : int
         Chebyshev filter size.
+    activation : function, optional
+        Activation function, default is ReLu.
     bias : bool, optional
         If True, adds a learnable bias to the output. Default: ``True``.
     """
-
+    
     def __init__(self,
                  in_feats,
                  out_feats,
                  k,
+                 activation=F.relu,
                  bias=True):
-        super(ChebConv, self).__init__()
+        super(Cheb_Conv, self).__init__()
+        self._k = k
         self._in_feats = in_feats
         self._out_feats = out_feats
-        self.fc = nn.ModuleList([
-            nn.Linear(in_feats, out_feats, bias=False) for _ in range(k)
-        ])
-        self._k = k
-        if bias:
-            self.bias = nn.Parameter(th.Tensor(out_feats))
-        else:
-            self.register_buffer('bias', None)
-        self.reset_parameters()
+        self.activation = activation
+        self.apply_mod = NodeApplyModule(
+            in_feats,
+            out_feats,
+            k=self._k,
+            activation=self.activation)
 
-    def reset_parameters(self):
-        """Reinitialize learnable parameters."""
-        if self.bias is not None:
-            init.zeros_(self.bias)
-        for module in self.fc.modules():
-            if isinstance(module, nn.Linear):
-                init.xavier_normal_(module.weight, init.calculate_gain('relu'))
-                if module.bias is not None:
-                    init.zeros_(module.bias)
 
     def forward(self, graph, feat, lambda_max=None):
         r"""Compute ChebNet layer.
@@ -86,42 +92,52 @@ class ChebConv(nn.Module):
             The output feature of shape :math:`(N, D_{out})` where :math:`D_{out}`
             is size of output feature.
         """
+        def unnLaplacian(feat, D_sqrt, graph):
+            """ Operation Feat * D^-1/2 A D^-1/2 """
+            graph.ndata['h'] = feat * D_sqrt
+            graph.update_all(fn.copy_u('h', 'm'), fn.sum('m', 'h'))
+            return graph.ndata.pop('h') * D_sqrt
+
         with graph.local_scope():
-            norm = th.pow(
-                graph.in_degrees().float().clamp(min=1), -0.5).unsqueeze(-1).to(feat.device)
+            D_sqrt = torch.pow(graph.in_degrees().float().clamp(
+                min=1), -0.5).unsqueeze(-1).to(signal.device)
+
             if lambda_max is None:
-                lambda_max = laplacian_lambda_max(graph)
+                try:
+                    lambda_max = dgl.laplacian_lambda_max(graph)
+                except BaseException:
+                    # if the largest eigonvalue is not found
+                    lambda_max = torch.Tensor(2).to(signal.device)
+
             if isinstance(lambda_max, list):
-                lambda_max = th.Tensor(lambda_max).to(feat.device)
+                lambda_max = torch.Tensor(lambda_max).to(signal.device)
             if lambda_max.dim() == 1:
                 lambda_max = lambda_max.unsqueeze(-1)  # (B,) to (B, 1)
+
             # broadcast from (B, 1) to (N, 1)
-            lambda_max = broadcast_nodes(graph, lambda_max)
-            # T0(X)
-            Tx_0 = feat
-            rst = self.fc[0](Tx_0)
-            # T1(X)
+            lambda_max = dgl.broadcast_nodes(graph, lambda_max)
+
+            # X_0(f)
+            Xt = X_0 = signal
+
+            # X_1(f)
             if self._k > 1:
-                graph.ndata['h'] = Tx_0 * norm
-                graph.update_all(fn.copy_u('h', 'm'), fn.sum('m', 'h'))
-                h = graph.ndata.pop('h') * norm
-                # Λ = 2 * (I - D ^ -1/2 A D ^ -1/2) / lambda_max - I
-                #   = - 2(D ^ -1/2 A D ^ -1/2) / lambda_max + (2 / lambda_max - 1) I
-                Tx_1 = -2. * h / lambda_max + Tx_0 * (2. / lambda_max - 1)
-                rst = rst + self.fc[1](Tx_1)
-            # Ti(x), i = 2...k
+                re_norm = 2. / lambda_max
+                h = unnLaplacian(X_0, D_sqrt, graph)
+
+                X_1 = - re_norm * h + X_0 * (re_norm - 1)
+                Xt = torch.cat((Xt, X_1), 1)
+
+            # Xi(x), i = 2...k
             for i in range(2, self._k):
-                graph.ndata['h'] = Tx_1 * norm
-                graph.update_all(fn.copy_u('h', 'm'), fn.sum('m', 'h'))
-                h = graph.ndata.pop('h') * norm
-                # Tx_k = 2 * Λ * Tx_(k-1) - Tx_(k-2)
-                #      = - 4(D ^ -1/2 A D ^ -1/2) / lambda_max Tx_(k-1) +
-                #        (4 / lambda_max - 2) Tx_(k-1) -
-                #        Tx_(k-2)
-                Tx_2 = -4. * h / lambda_max + Tx_1 * (4. / lambda_max - 2) - Tx_0
-                rst = rst + self.fc[i](Tx_2)
-                Tx_1, Tx_0 = Tx_2, Tx_1
-            # add bias
-            if self.bias is not None:
-                rst = rst + self.bias
-            return rst
+                h = unnLaplacian(X_1, D_sqrt, graph)
+                X_i = - 2 * re_norm * h + X_1 * 2 * (re_norm - 1) - X_0
+
+                Xt = torch.cat((Xt, X_i), 1)
+                X_1, X_0 = X_i, X_1
+
+            # forward pass
+            graph.ndata['h'] = Xt
+            graph.apply_nodes(func=self.apply_mod)
+
+            return graph.ndata.pop('h')
