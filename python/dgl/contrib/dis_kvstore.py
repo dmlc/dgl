5c59bb2360089157346ff98483fe2ac24c5dcb79
aksnzhy <mctt90@gmail.com>
2020-04-24 21:34:49 +0800

    update

--- python/dgl/contrib/dis_kvstore.py
+++ python/dgl/contrib/dis_kvstore.py
@@ -478,8 +478,7 @@ class KVServer(object):
                 assert msg.rank == 0
                 data_str, target_name = msg.name.split('|')
                 data_name, date_type = self._deserialize_shared_tensor(data_str)
-                dtype_dict = F.data_type_dict()
-                dtype = dtype_dict[data_type]
+                dtype = F.data_type_dict[data_type]
                 data_shape = F.asnumpy(msg.id).tolist()
                 if self._server_id % self._group_count == 0: # master server
                     data_tensor = F.zeros(data_shape, dtype, F.cpu())
