2e2910a8b49d698c816c821cf95c10d3ae8a5b15
Quan Gan <coin2028@hotmail.com>
2020-04-26 11:52:55 +0000

    change hyperparams

--- examples/pytorch/graphsage/train_sampling_unsupervised.py
+++ examples/pytorch/graphsage/train_sampling_unsupervised.py
@@ -294,11 +294,11 @@ if __name__ == '__main__':
     argparser.add_argument('--num-epochs', type=int, default=20)
     argparser.add_argument('--num-hidden', type=int, default=16)
     argparser.add_argument('--num-layers', type=int, default=2)
-    argparser.add_argument('--num-negs', type=int, default=20)
+    argparser.add_argument('--num-negs', type=int, default=1)
     argparser.add_argument('--fan-out', type=str, default='10,25')
     argparser.add_argument('--batch-size', type=int, default=10000)
     argparser.add_argument('--log-every', type=int, default=20)
-    argparser.add_argument('--eval-every', type=int, default=10000)
+    argparser.add_argument('--eval-every', type=int, default=1000)
     argparser.add_argument('--lr', type=float, default=0.003)
     argparser.add_argument('--dropout', type=float, default=0.5)
     argparser.add_argument('--num-workers', type=int, default=0,
