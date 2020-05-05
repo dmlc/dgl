70b31fc651782bd948586ca3a04945ee380ed810
aksnzhy <mctt90@gmail.com>
2020-05-05 19:25:27 +0800

    update

--- python/dgl/contrib/__init__.py
+++ python/dgl/contrib/__init__.py
@@ -2,4 +2,3 @@ from . import sampling
 from . import graph_store
 from .dis_kvstore import KVClient, KVServer
 from .dis_kvstore import read_ip_config
-from .graph_partition_book import GraphPartitionBook
