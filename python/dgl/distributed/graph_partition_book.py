5c7b3f904da667e97e1c81903be87bb1fc4f78ab
aksnzhy <mctt90@gmail.com>
2020-05-07 17:08:30 +0800

    update

--- python/dgl/distributed/graph_partition_book.py
+++ python/dgl/distributed/graph_partition_book.py
@@ -76,7 +76,11 @@ class GraphPartitionBook:
         global_id = self._graph.ndata[NID]
         max_global_id = np.amax(F.asnumpy(global_id))
         g2l = F.zeros((max_global_id+1), F.int64, F.context(global_id))
+        print("-----")
+        print(g2l)
         F.scatter_row(g2l, global_id, F.arange(0, len(global_id)))
+        print(g2l)
+        print("-----")
         self._nidg2l[self._part_id] = g2l
         # Get eidg2l
         self._eidg2l = [None] * self._num_partitions
