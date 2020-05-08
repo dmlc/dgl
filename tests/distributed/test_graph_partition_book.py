5e93b6cbec32f0d10d413da5d4b70fd5d73f7975
aksnzhy <mctt90@gmail.com>
2020-05-08 14:13:17 +0800

    update

--- tests/distributed/test_graph_partition_book.py
+++ tests/distributed/test_graph_partition_book.py
@@ -36,10 +36,10 @@ def test_graph_partition_book():
     for i in range(num_parts):
         part_g, node_feats, edge_feats, meta = load_partition('/tmp/test.json', i)
         num_nodes, num_edges, node_map, edge_map, num_partitions = meta
-        gpb = GraphPartitionBook(part_id=i, 
-                                 num_parts=num_partitions, 
-                                 node_map=node_map, 
-                                 edge_map=edge_map, 
+        gpb = GraphPartitionBook(part_id=i,
+                                 num_parts=num_partitions,
+                                 node_map=node_map,
+                                 edge_map=edge_map,
                                  part_graph=part_g)
         assert gpb.num_partitions() == num_parts
         gpb_meta = gpb.metadata()
