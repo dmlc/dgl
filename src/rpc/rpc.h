eccbf745388035afe89f745367f4941db361ed28
Minjie Wang <wmjlyjemaine@gmail.com>
2020-05-09 08:53:14 +0000

    fix compile

---
+++ src/rpc/rpc.h
@@ -0,0 +1,13 @@
+/*!
+ *  Copyright (c) 2017 by Contributors
+ * \file rpc/rpc.h
+ * \brief Common headers for remote process call (RPC).
+ */
+#ifndef DGL_RPC_RPC_H_
+#define DGL_RPC_RPC_H_
+
+struct RPCInfo {
+  int32_t rank;
+};
+
+#endif  // DGL_RPC_RPC_H_
