7b6af87a9023ac8c38b62eaadefbada54cd4ec7e
Mufei Li <mufeili1996@gmail.com>
2020-05-17 21:17:03 +0800

    Update (#1534)

--- examples/pytorch/gat/train_ppi.py
+++ examples/pytorch/gat/train_ppi.py
@@ -35,7 +35,7 @@ def evaluate(feats, model, subgraph, labels, loss_fcn):
             layer.g = subgraph
         output = model(feats.float())
         loss_data = loss_fcn(output, labels.float())
-        predict = np.where(output.data.cpu().numpy() >= 0.5, 1, 0)
+        predict = np.where(output.data.cpu().numpy() >= 0., 1, 0)
         score = f1_score(labels.data.cpu().numpy(),
                          predict, average='micro')
         return score, loss_data.item()
@@ -109,7 +109,7 @@ def main(args):
                 val_loss_list.append(val_loss)
             mean_score = np.array(score_list).mean()
             mean_val_loss = np.array(val_loss_list).mean()
-            print("F1-Score: {:.4f} ".format(mean_score))
+            print("Val F1-Score: {:.4f} ".format(mean_score))
             # early stop
             if mean_score > best_score or best_loss > mean_val_loss:
                 if mean_score > best_score and best_loss > mean_val_loss:
@@ -128,7 +128,7 @@ def main(args):
         feats = feats.to(device)
         labels = labels.to(device)
         test_score_list.append(evaluate(feats, model, subgraph, labels.float(), loss_fcn)[0])
-    print("F1-Score: {:.4f}".format(np.array(test_score_list).mean()))
+    print("Test F1-Score: {:.4f}".format(np.array(test_score_list).mean()))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='GAT')
