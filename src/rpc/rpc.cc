3da3b4dc37b5c2129642fd1024cb23eac08a49f5
aksnzhy <mctt90@gmail.com>
2020-05-19 16:36:17 +0800

    update

--- src/rpc/rpc.cc
+++ src/rpc/rpc.cc
@@ -128,8 +128,7 @@ RPCStatus SendRPCMessage(const RPCMessage& msg) {
       network::Message ndarray_data_msg;
       ndarray_data_msg.data = reinterpret_cast<char*>(ptr.data);
       ndarray_data_msg.size = ptr.size;
-      NDArray tensor = ptr.tensor;
-      ndarray_data_msg.deallocator = [tensor](network::Message*) {}
+      ndarray_data_msg.deallocator = [ptr.tensor](network::Message*) {};
     }
   }
   return kRPCSuccess;
