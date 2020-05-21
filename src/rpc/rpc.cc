1dbb76872fabf43600c099722557948ed2e27964
aksnzhy <mctt90@gmail.com>
2020-05-21 18:24:28 +0800

    update

--- src/rpc/rpc.cc
+++ src/rpc/rpc.cc
@@ -24,6 +24,7 @@ RPCStatus SendRPCMessage(const RPCMessage& msg) {
   // Copy the data for now, can be optimized later
   memcpy(rpc_meta_buffer, zerocopy_blob.data(), zerocopy_blob.size());
   int64_t rpc_meta_size = zerocopy_blob.size();
+  LOG(INFO) << zerocopy_blob;
   network::Message rpc_meta_msg;
   rpc_meta_msg.data = rpc_meta_buffer;
   rpc_meta_msg.size = rpc_meta_size;
@@ -61,6 +62,7 @@ RPCStatus RecvRPCMessage(RPCMessage* msg, int32_t timeout) {
   // Copy the data for now, can be optimized later
   std::string zerocopy_blob(rpc_meta_msg.data, rpc_meta_msg.size);
   rpc_meta_msg.deallocator(&rpc_meta_msg);
+  LOG(INFO) << zerocopy_blob;
   // Recv count
   network::Message ndarray_count_msg;
   CHECK_EQ(RPCContext::ThreadLocal()->receiver->RecvFrom(
