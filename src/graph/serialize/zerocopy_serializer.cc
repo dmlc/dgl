8b5075c780cd85e2203571007b42f5b9fe145fc2
VoVAllen <jz1749@nyu.edu>
2020-05-22 10:28:26 +0000

    refactor

--- src/graph/serialize/zerocopy_serializer.cc
+++ src/graph/serialize/zerocopy_serializer.cc
@@ -7,6 +7,7 @@
 #include <dgl/zerocopy_serializer.h>
 
 #include "dgl/runtime/ndarray.h"
+#include "dmlc/memory_io.h"
 
 namespace dgl {
 
@@ -46,17 +47,16 @@ NDArray CreateNDArrayFromRawData(std::vector<int64_t> shape, DLDataType dtype,
   return NDArray::FromDLPack(dlm_tensor);
 }
 
-void StringStreamWithBuffer::PushNDArray(const NDArray& tensor) {
+void StreamWithBuffer::PushNDArray(const NDArray& tensor) {
 #ifndef _WIN32
-  auto strm = static_cast<dmlc::Stream*>(this);
-  strm->Write(tensor->ndim);
-  strm->Write(tensor->dtype);
+  this->Write(tensor->ndim);
+  this->Write(tensor->dtype);
   int ndim = tensor->ndim;
-  strm->WriteArray(tensor->shape, ndim);
+  this->WriteArray(tensor->shape, ndim);
   CHECK(tensor.IsContiguous())
-    << "StringStreamWithBuffer only supports contiguous tensor";
+    << "StreamWithBuffer only supports contiguous tensor";
   CHECK_EQ(tensor->byte_offset, 0)
-    << "StringStreamWithBuffer only supports zero byte offset tensor";
+    << "StreamWithBuffer only supports zero byte offset tensor";
   int type_bytes = tensor->dtype.bits / 8;
   int64_t num_elems = 1;
   for (int i = 0; i < ndim; ++i) {
@@ -68,33 +68,32 @@ void StringStreamWithBuffer::PushNDArray(const NDArray& tensor) {
   if (send_to_remote_ || !mem) {
     // If the stream is for remote communication or the data is not stored in
     // shared memory, serialize the data content as a buffer.
-    strm->Write<bool>(false);
+    this->Write<bool>(false);
     buffer_list_.emplace_back(tensor, tensor->data, data_byte_size);
   } else {
     CHECK(mem) << "Tried to send non-shared-memroy tensor to local "
-                  "StringStreamWithBuffer";
+                  "StreamWithBuffer";
     // Serialize only the shared memory name.
-    strm->Write<bool>(true);
-    strm->Write(mem->GetName());
+    this->Write<bool>(true);
+    this->Write(mem->GetName());
   }
 #else
-  LOG(FATAL) << "StringStreamWithBuffer is not supported on windows";
+  LOG(FATAL) << "StreamWithBuffer is not supported on windows";
 #endif  // _WIN32
   return;
 }
 
-NDArray StringStreamWithBuffer::PopNDArray() {
+NDArray StreamWithBuffer::PopNDArray() {
 #ifndef _WIN32
-  auto strm = static_cast<dmlc::Stream*>(this);
   int ndim;
   DLDataType dtype;
 
-  CHECK(strm->Read(&ndim)) << "Invalid DLTensor file format";
-  CHECK(strm->Read(&dtype)) << "Invalid DLTensor file format";
+  CHECK(this->Read(&ndim)) << "Invalid DLTensor file format";
+  CHECK(this->Read(&dtype)) << "Invalid DLTensor file format";
 
   std::vector<int64_t> shape(ndim);
   if (ndim != 0) {
-    CHECK(strm->ReadArray(&shape[0], ndim)) << "Invalid DLTensor file format";
+    CHECK(this->ReadArray(&shape[0], ndim)) << "Invalid DLTensor file format";
   }
 
   DLContext cpu_ctx;
@@ -102,12 +101,12 @@ NDArray StringStreamWithBuffer::PopNDArray() {
   cpu_ctx.device_id = 0;
 
   bool is_shared_mem;
-  CHECK(strm->Read(&is_shared_mem)) << "Invalid stream read";
+  CHECK(this->Read(&is_shared_mem)) << "Invalid stream read";
   std::string sharedmem_name;
   if (is_shared_mem) {
     CHECK(!send_to_remote_) << "Invalid attempt to deserialize from shared "
                                "memory with send_to_remote=true";
-    CHECK(strm->Read(&sharedmem_name)) << "Invalid stream read";
+    CHECK(this->Read(&sharedmem_name)) << "Invalid stream read";
     return NDArray::EmptyShared(sharedmem_name, shape, dtype, cpu_ctx, false);
   } else {
     CHECK(send_to_remote_) << "Invalid attempt to deserialize from raw data "
@@ -118,9 +117,25 @@ NDArray StringStreamWithBuffer::PopNDArray() {
     return ret;
   }
 #else
-  LOG(FATAL) << "StringStreamWithBuffer is not supported on windows";
+  LOG(FATAL) << "StreamWithBuffer is not supported on windows";
   return NDArray();
 #endif  // _WIN32
 }
+
+// std::unique_ptr<StreamWithBuffer> StreamWithBuffer::Create(
+//   std::string* blob, bool send_to_remote) {
+//   auto strm =
+//     std::unique_ptr<dmlc::SeekStream>(new dmlc::MemoryStringStream(blob));
+//   return std::unique_ptr<StreamWithBuffer>(
+//     new StreamWithBuffer(std::move(strm), true));
+// }
+
+// std::unique_ptr<StreamWithBuffer> StreamWithBuffer::Create(
+//   char* p_buffer, size_t size, bool send_to_remote) {
+//   auto strm = std::unique_ptr<dmlc::SeekStream>(
+//     new dmlc::MemoryFixedSizeStream(p_buffer, size));
+//   return std::unique_ptr<StreamWithBuffer>(
+//     new StreamWithBuffer(std::move(strm), true));
+// }
 
 }  // namespace dgl
