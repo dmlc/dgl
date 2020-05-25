6a363378d50c4c1d6c01b1942b170050286e2923
Chao Ma <mctt90@gmail.com>
2020-05-25 15:41:45 +0800

    [RPC] Init server_state during get_server_state() (#1559)
    * init server_state
    
    * update
    
    * update

--- python/dgl/distributed/rpc_server.py
+++ python/dgl/distributed/rpc_server.py
@@ -2,6 +2,7 @@
 
 from . import rpc
 from .constants import MAX_QUEUE_SIZE
+from .server_state import get_server_state
 
 def start_server(server_id, ip_config, num_clients, \
     max_queue_size=MAX_QUEUE_SIZE, net_type='socket'):
@@ -69,8 +70,8 @@ def start_server(server_id, ip_config, num_clients, \
         for client_id, _ in client_namebook.items():
             register_res = rpc.ClientRegisterResponse(client_id)
             rpc.send_response(client_id, register_res)
+    server_state = get_server_state()
     # main service loop
-    server_state = None
     while True:
         req, client_id = rpc.recv_request()
         res = req.process_request(server_state)
