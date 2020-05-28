2c1a15b19715d515ee87911e48626fc4fa63d4d5
Da Zheng <zhengda1936@gmail.com>
2020-05-28 06:44:55 +0000

    benchmark partition with livejournal.

--- tests/regression/benchmarks/partition.py
+++ tests/regression/benchmarks/partition.py
@@ -1,18 +1,17 @@
 import dgl
 from dgl import distributed as dgl_distributed
 import argparse, time
-from ogb.nodeproppred import DglNodePropPredDataset
+from utils import get_graph
 
 parser = argparse.ArgumentParser(description='partition')
-parser.add_argument("--dataset", type=float, default=0.5,
-                    help="dropout probability")
+parser.add_argument("--dataset", type=str, default='livejournal',
+                    help="specify the graph for partitioning")
+parser.add_argument("--num_parts", type=int, default=16,
+                    help="the number of partitions")
 args = parser.parse_args()
 
-if args.dataset == 'amazon':
-    data = DglNodePropPredDataset(name='ogbn-products')
-
-graph, labels = data[0]
-
+g = get_graph(args.dataset)
+print('{}: |V|={}, |E|={}'.format(args.dataset, g.number_of_nodes(), g.number_of_edges()))
 start = time.time()
-dgl_distributed.partition_graph(graph, 'ogbn-prod', 16, '/tmp', num_hops=1, part_method="metis")
+dgl_distributed.partition_graph(g, args.dataset, args.num_parts, '/tmp', num_hops=1, part_method="metis")
 print('Time: {} seconds'.format(time.time() - start))
