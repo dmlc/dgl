5a4ce55b1bc5bb1aaa80154fd7cc0fb971618bb1
Ubuntu <ubuntu@ip-172-31-59-98.us-west-2.compute.internal>
2020-05-30 08:11:13 +0000

    add pyg.

---
+++ nips_benchmark/pytorch/pyg_ppi.py
@@ -0,0 +1,118 @@
+import os.path as osp
+
+import time
+import torch
+import torch.nn.functional as F
+from torch_geometric.datasets import PPI
+from torch_geometric.data import DataLoader
+from torch_geometric.nn import GATConv, GCNConv, SAGEConv
+from sklearn.metrics import f1_score
+
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'PPI')
+train_dataset = PPI(path, split='train')
+val_dataset = PPI(path, split='val')
+test_dataset = PPI(path, split='test')
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
+
+
+class GCNNet(torch.nn.Module):
+    def __init__(self):
+        super(GCNNet, self).__init__()
+        self.conv1 = GCNConv(train_dataset.num_features, 256)
+        self.lin1 = torch.nn.Linear(train_dataset.num_features, 256)
+        self.conv2 = GCNConv(256, 256)
+        self.lin2 = torch.nn.Linear(256, 256)
+        self.conv3 = GCNConv(256, train_dataset.num_classes)
+        self.lin3 = torch.nn.Linear(256, train_dataset.num_classes)
+
+    def forward(self, x, edge_index):
+        x = F.elu(self.conv1(x, edge_index) + self.lin1(x))
+        x = F.elu(self.conv2(x, edge_index) + self.lin2(x))
+        x = self.conv3(x, edge_index) + self.lin3(x)
+        return x
+
+
+class GraphSageNet(torch.nn.Module):
+    def __init__(self):
+        super(GraphSageNet, self).__init__()
+        self.conv1 = SAGEConv(train_dataset.num_features, 256)
+        self.lin1 = torch.nn.Linear(train_dataset.num_features, 256)
+        self.conv2 = SAGEConv(256, 256)
+        self.lin2 = torch.nn.Linear(256, 256)
+        self.conv3 = SAGEConv(256, train_dataset.num_classes)
+        self.lin3 = torch.nn.Linear(256, train_dataset.num_classes)
+
+    def forward(self, x, edge_index):
+        x = F.elu(self.conv1(x, edge_index) + self.lin1(x))
+        x = F.elu(self.conv2(x, edge_index) + self.lin2(x))
+        x = self.conv3(x, edge_index) + self.lin3(x)
+        return x
+
+
+class GATNet(torch.nn.Module):
+    def __init__(self):
+        super(GATNet, self).__init__()
+        self.conv1 = GATConv(train_dataset.num_features, 256, heads=4)
+        self.lin1 = torch.nn.Linear(train_dataset.num_features, 4 * 256)
+        self.conv2 = GATConv(4 * 256, 256, heads=4)
+        self.lin2 = torch.nn.Linear(4 * 256, 4 * 256)
+        self.conv3 = GATConv(
+            4 * 256, train_dataset.num_classes, heads=6, concat=False)
+        self.lin3 = torch.nn.Linear(4 * 256, train_dataset.num_classes)
+
+    def forward(self, x, edge_index):
+        x = F.elu(self.conv1(x, edge_index) + self.lin1(x))
+        x = F.elu(self.conv2(x, edge_index) + self.lin2(x))
+        x = self.conv3(x, edge_index) + self.lin3(x)
+        return x
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#model = GATNet().to(device)
+model = GraphSageNet().to(device)
+loss_op = torch.nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+
+
+def train():
+    model.train()
+
+    total_loss = 0
+    for data in train_loader:
+        num_graphs = data.num_graphs
+        data.batch = None
+        data = data.to(device)
+        optimizer.zero_grad()
+        loss = loss_op(model(data.x, data.edge_index), data.y)
+        total_loss += loss.item() * num_graphs
+        loss.backward()
+        optimizer.step()
+    return total_loss / len(train_loader.dataset)
+
+
+def test(loader):
+    model.eval()
+
+    ys, preds = [], []
+    for data in loader:
+        ys.append(data.y)
+        with torch.no_grad():
+            out = model(data.x.to(device), data.edge_index.to(device))
+        preds.append((out > 0).float().cpu())
+
+    y, pred = torch.cat(ys, dim=0).numpy(), torch.cat(preds, dim=0).numpy()
+    return f1_score(y, pred, average='micro') if pred.sum() > 0 else 0
+
+
+train_time = 0
+for epoch in range(1, 10):
+    start = time.time()
+    loss = train()
+    train_time += time.time() - start
+    val_f1 = test(val_loader)
+    test_f1 = test(test_loader)
+    print('Epoch: {:02d}, Loss: {:.4f}, Val: {:.4f}, Test: {:.4f}'.format(
+        epoch, loss, val_f1, test_f1))
+print('total train time:', train_time)
