0d09f9c347f4f5bc793cb79ad60a0acef756412c
aksnzhy <mctt90@gmail.com>
2020-05-31 20:27:38 +0800

    update

--- tests/compute/test_new_kvstore.py
+++ tests/compute/test_new_kvstore.py
@@ -9,9 +9,9 @@ from numpy.testing import assert_array_equal
 
 # Create an one-part Graph
 node_map = F.tensor([0,0,0,0,0,0], F.int64)
-edge_map = F.tensor([0,0,0,0,0,0], F.int64)
+edge_map = F.tensor([0,0,0,0,0,0,0], F.int64)
 global_nid = F.tensor([0,1,2,3,4,5], F.int64)
-global_eid = F.tensor([0,1,2,3,4,5], F.int64)
+global_eid = F.tensor([0,1,2,3,4,5,6], F.int64)
 
 gpb = dgl.distributed.GraphPartitionBook(part_id=0,
                                          num_parts=1,
@@ -34,13 +34,13 @@ def test_partition_policy():
     assert node_policy.part_id == 0
     assert edge_policy.part_id == 0
     local_nid = node_policy.to_local(F.tensor([0,1,2,3,4,5]))
-    local_eid = edge_policy.to_local(F.tensor([0,1,2,3,4,5]))
+    local_eid = edge_policy.to_local(F.tensor([0,1,2,3,4,5,6]))
     assert_array_equal(F.asnumpy(local_nid), F.asnumpy(F.tensor([0,1,2,3,4,5], F.int64)))
-    assert_array_equal(F.asnumpy(local_eid), F.asnumpy(F.tensor([0,1,2,3,4,5], F.int64)))
+    assert_array_equal(F.asnumpy(local_eid), F.asnumpy(F.tensor([0,1,2,3,4,5,6], F.int64)))
     nid_partid = node_policy.to_partid(F.tensor([0,1,2,3,4,5], F.int64))
-    eid_partid = edge_policy.to_partid(F.tensor([0,1,2,3,4,5], F.int64))
+    eid_partid = edge_policy.to_partid(F.tensor([0,1,2,3,4,5,6], F.int64))
     assert_array_equal(F.asnumpy(nid_partid), F.asnumpy(F.tensor([0,0,0,0,0,0], F.int64)))
-    assert_array_equal(F.asnumpy(eid_partid), F.asnumpy(F.tensor([0,0,0,0,0,0], F.int64)))
+    assert_array_equal(F.asnumpy(eid_partid), F.asnumpy(F.tensor([0,0,0,0,0,0,0], F.int64)))
     assert node_policy.get_data_size() == len(node_map)
     assert edge_policy.get_data_size() == len(edge_map)
 
