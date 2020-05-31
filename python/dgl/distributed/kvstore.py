1a9f68f804c5caed67e6827ca230b63198bd830c
aksnzhy <mctt90@gmail.com>
2020-05-31 21:23:03 +0800

    update

--- python/dgl/distributed/kvstore.py
+++ python/dgl/distributed/kvstore.py
@@ -217,10 +217,10 @@ class InitDataRequest(rpc.Request):
         kv = server_state.kv_store
         dtype = F.data_type_dict[self.dtype]
         if kv.is_backup_server() == False:
-            data_tensor = self.init_func(data_shape, dtype)
-            kv.init_data(name=name, data_tensor=data_tensor)
+            data_tensor = self.init_func(self.data_shape, dtype)
+            kv.init_data(name=self.name, data_tensor=data_tensor)
         else: # backup server will read data from shared-memory
-            kv.init_data(name=name)
+            kv.init_data(name=self.name)
         # Find the same partition policy from exsiting plolicy list.
         for _, policy in kv.part_policy.items():
             if policy.policy_str == self.policy_str:
