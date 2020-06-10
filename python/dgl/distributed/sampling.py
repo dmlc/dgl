9a1c88b3d75a8808bb3e182358915cab5917b6d5
VoVAllen <jz1749@nyu.edu>
2020-06-10 07:31:40 +0000

    111

--- python/dgl/distributed/sampling.py
+++ python/dgl/distributed/sampling.py
@@ -1,4 +1,4 @@
-from .rpc import Request, Response, remote_call
+from .rpc import Request, Response, remote_call, send_request, recv_response
 from .server_state import ServerState
 from .dist_graph import DistGraph
 from ..sampling import sample_neighbors as local_sample_neighbors
@@ -27,30 +27,34 @@ class SamplingResponse(Response):
 
 class SamplingRequest(Request):
 
-    def __init__(self, partition_book: GraphPartitionBook, nodes, fan_out, edge_dir='in', prob=None, replace=False):
+    def __init__(self, nodes, fan_out, edge_dir='in', prob=None, replace=False):
         self.seed_nodes = nodes
         self.edge_dir = edge_dir
         self.prob = prob
         self.replace = replace
-        self.partition_book = partition_book
         self.fan_out = fan_out
 
     def __setstate__(self, state):
-        
+        self.seed_nodes, self.edge_dir, self.prob, self.replace, self.fan_out = state
 
-    def process_request(self, server_state: ServerState):
-        
+    def __getstate__(self):
+        return self.seed_nodes, self.edge_dir, self.prob, self.replace, self.fan_out        
+
+    def process_request(self, server_state: ServerState):        
         local_g = server_state.graph
-        partition_book = self.partition_book
-        partition_id = partition_book.nid2partid()
-        local_ids = partition_book.nid2localnid(self.seed_nodes, partition_id)
+        partition_book = server_state.partition_book
+        print(f"Server: {server_state.rank}::{self.seed_nodes}")
+        partition_id = partition_book.nid2partid(th.tensor(self.seed_nodes))
+        print(f"Partition_id {partition_book._part_id} {server_state.rank} {partition_id} {self.seed_nodes}")
+        local_ids = partition_book.nid2localnid(th.tensor(self.seed_nodes), partition_id)
         sampled_graph = local_sample_neighbors(
             local_g, local_ids, self.fan_out, self.edge_dir, self.prob, self.replace)
-        global_nid_mapping = self.g.ndata[NID]
+        global_nid_mapping = local_g.ndata[dgl.NID]
         src, dst = sampled_graph.edges()
         global_src, global_dst = global_nid_mapping[src], global_nid_mapping[dst]
-        global_eids = sampled_graph.edata[EID]
+        global_eids = sampled_graph.edata[dgl.EID]
         res = SamplingResponse(global_src, global_dst, global_eids)
+        print(f"DONE {server_state.rank}")
         return res
 
 def merge_graphs(res_list):
@@ -62,28 +66,34 @@ def merge_graphs(res_list):
         srcs.append(res.global_src)
         dsts.append(res.global_dst)
         eids.append(res.global_eids)
-    src_tensor = th.concat(srcs)
-    dst_tensor = th.concat(dsts)
-    eid_tensor = th.concat(eids)
+    src_tensor = th.cat(srcs)
+    dst_tensor = th.cat(dsts)
+    eid_tensor = th.cat(eids)
     g = dgl.graph((src_tensor, dst_tensor), restrict_format='coo')
-    g.edata[EID] = eid_tensor
+    g.edata[dgl.EID] = eid_tensor
     return g
     
 
 def sample_neighbors(g: DistGraph, nodes, fanout, edge_dir='in', prob=None, replace=False):
-    assert fan_out == 1
+    assert fanout == 1
     assert edge_dir == 'in'
     req_list = []
     blocks = []
 
     for node in nodes:
-        req = SamplingRequest(
-            g, [node], fanout, edge_dir=edge_dir, prob=prob, replace=replace)
         partition_book: GraphPartitionBook = g.get_partition_book()
+        req = SamplingRequest(
+            [node], fanout, edge_dir=edge_dir, prob=prob, replace=replace)
+
         # Can be optimized, merge requests on the same machine
-        partition_id = partition_book.nid2partid(node)
-        req_list.append((partition_id, req))
-    res_list = remote_call(req_list)
+        partition_id = partition_book.nid2partid(th.tensor(node))
+        print(f"{partition_id}::{node}")
+        send_request(partition_id, req)
+        # req_list.append((partition_id, req))
+    print("before")
+    res_list = [recv_response() for _ in nodes]
+    print("after")
+    print(res_list)
     return merge_graphs(res_list)
 
 
--- tests/distributed/test_distributed_sampling.py
+++ tests/distributed/test_distributed_sampling.py
@@ -2,21 +2,32 @@ import dgl
 import unittest
 import os
 from dgl.data import CitationGraphDataset
+from dgl.distributed.sampling import sample_neighbors
+from dgl.distributed import partition_graph, load_partition, GraphPartitionBook
 import sys
 import multiprocessing as mp
+import torch as th
+import time
 
 
 def myexcepthook(exctype, value, traceback):
     for p in mp.active_children():
-       p.terminate()
+        p.terminate()
+
+
+class MochDistGraph:
+
+    def __init__(self, partition_book):
+        self.partition_book = partition_book
+
+    def get_partition_book(self):
+        return self.partition_book
 
 
 class MockServerState:
 
-    def __init__(self):
+    def __init__(self, g):
         self.state = dgl.distributed.ServerState(None)
-        g = CitationGraphDataset("cora")[0]
-        g.readonly()
         self.hgraph = dgl.as_heterograph(g)
 
     @property
@@ -34,7 +45,17 @@ class MockServerState:
 
 def start_server(rank):
     import dgl
-    server_state = MockServerState()
+    part_g, node_feats, edge_feats, meta = load_partition(
+        '/tmp/test.json', rank)
+    num_nodes, num_edges, node_map, edge_map, num_partitions = meta
+    gpb = GraphPartitionBook(part_id=rank,
+                             num_parts=num_partitions,
+                             node_map=node_map,
+                             edge_map=edge_map,
+                             part_graph=part_g)
+    server_state = MockServerState(part_g)
+    server_state.rank = rank
+    server_state.partition_book = gpb
     dgl.distributed.start_server(server_id=rank,
                                  ip_config='rpc_ip_config.txt',
                                  num_clients=1,
@@ -46,22 +67,48 @@ def start_client(rank):
     import dgl
     dgl.distributed.connect_to_server(ip_config='rpc_ip_config.txt')
 
+    part_g, node_feats, edge_feats, meta = load_partition(
+        '/tmp/test.json', rank)
+    num_nodes, num_edges, node_map, edge_map, num_partitions = meta
+    print(node_map)
+    gpb = GraphPartitionBook(part_id=rank,
+                             num_parts=num_partitions,
+                             node_map=node_map,
+                             edge_map=edge_map,
+                             part_graph=part_g)
+
+    g = MochDistGraph(gpb)
+    results = sample_neighbors(g, [0], 1)
+    print(results)
+    print("11111111")
     dgl.distributed.shutdown_servers()
     dgl.distributed.finalize_client()
 
 
 @unittest.skipIf(os.name == 'nt', reason='Do not support windows yet')
 def test_rpc():
-    num_server = 1
+    num_server = 3
     ip_config = open("rpc_ip_config.txt", "w")
-    ip_config.write(f'127.0.0.1 30060 {num_server}\n')
+    ip_config.write(f'127.0.0.1 30050 {num_server}\n')
     ip_config.close()
+
+    # partition graph
+    g = CitationGraphDataset("cora")[0]
+    g.readonly()
+    g.ndata['labels'] = th.arange(0, g.number_of_nodes())
+    # g.ndata['feats'] = F.tensor(np.random.randn(g.number_of_nodes(), 10))
+    num_parts = num_server
+    num_hops = 2
+
+    partition_graph(g, 'test', num_parts, '/tmp',
+                    num_hops=num_hops, part_method='metis')
 
     pserver_list = []
     mp.set_start_method("spawn")
     for i in range(num_server):
         p = mp.Process(target=start_server, args=(i,))
         p.start()
+        # time.sleep(1)
         pserver_list.append(p)
 
     pclient = mp.Process(target=start_client, args=(i,))
@@ -70,7 +117,6 @@ def test_rpc():
     pclient.join()
     for p in pserver_list:
         p.join()
-
 
 
 if __name__ == "__main__":
