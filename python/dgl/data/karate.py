55fbcd0f78dd492860ee078038cf52751374ae23
Hu <xiangkhu@MacBook-Pro-4.local>
2020-06-22 14:31:28 +0800

    KarateClubDataset inherits from DGLDataset

--- python/dgl/data/karate.py
+++ python/dgl/data/karate.py
@@ -2,30 +2,64 @@
 """
 import numpy as np
 import networkx as nx
+
+from .dgl_dataset import DGLDataset
 from ..graph import DGLGraph
 
+__all__ = ['KarateClubDataset']
 
-class KarateClub(object):
+
+class KarateClubDataset(DGLDataset):
+    """KarateClub dataset.
+
+    Zachary's karate club is a social network of a university
+    karate club, described in the paper "An Information Flow
+    Model for Conflict and Fission in Small Groups" by Wayne W. Zachary.
+    The network became a popular example of community structure in
+    networks after its use by Michelle Girvan and Mark Newman in 2002.
+    Official website: http://konect.cc/networks/ucidata-zachary/
+
+    Statistics
+    ===
+    Nodes: 34
+    Edges: 156
+    Number of Classes: 2
+
+    Returns
+    ===
+    KarateClubDataset object with two properties:
+        graph: A Homogeneous graph contain the graph structure and node labels
+        num_classes: number of node classes
+
+    Examples
+    ===
+    >>> data = KarateClubDataset()
+    >>> data.num_classes
+    2
+    >>> g = data.graph
+    >>> g.ndata
+    {'label': tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1])}
     """
-    Zachary's karate club is a social network of a university karate club, described in the paper
-    "An Information Flow Model for Conflict and Fission in Small Groups" by Wayne W. Zachary. The
-    network became a popular example of community structure in networks after its use by Michelle
-    Girvan and Mark Newman in 2002.
-
-    This dataset has only one graph, with ndata 'label' means whether the node is belong to the "Mr. Hi" club.
-    """
-
     def __init__(self):
-        kG = nx.karate_club_graph()
+        super(KarateClubDataset, self).__init__(name='karate_club')
+
+    def process(self, root_path):
+        kc_graph = nx.karate_club_graph()
         self.label = np.asarray(
-            [kG.nodes[i]['club'] != 'Mr. Hi' for i in kG.nodes]).astype(np.int64)
-        g = DGLGraph(kG)
+            [kc_graph.nodes[i]['club'] != 'Mr. Hi' for i in kc_graph.nodes]).astype(np.int64)
+        g = DGLGraph(kc_graph)
         g.ndata['label'] = self.label
-        self.data = [g]
+        self.graph = g
+
+    @property
+    def num_classes(self):
+        """Number of classes."""
+        return 2
 
     def __getitem__(self, idx):
         assert idx == 0, "This dataset has only one graph"
-        return self.data[0]
+        return self.graph
 
     def __len__(self):
-        return len(self.data)
+        return 1
