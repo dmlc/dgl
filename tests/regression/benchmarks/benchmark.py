773acf5eb8e8d80a37ce43a3d9dbf88219dfa900
Da Zheng <zhengda1936@gmail.com>
2020-06-22 08:08:20 +0000

    update benchmarks.

---
+++ tests/regression/benchmarks/benchmark.py
@@ -0,0 +1,36 @@
+import dgl
+from dgl import distributed as dgl_distributed
+import argparse, time
+from utils import get_graph
+
+parser = argparse.ArgumentParser(description='partition')
+parser.add_argument("--dataset", type=str, default='livejournal',
+                    help="specify the graph for partitioning")
+args = parser.parse_args()
+
+g = get_graph(args.dataset)
+print('{}: |V|={}, |E|={}'.format(args.dataset, g.number_of_nodes(), g.number_of_edges()))
+
+def run_partition(g):
+    dgl_distributed.partition_graph(g, args.dataset, 8, '/tmp', num_hops=1, part_method="metis")
+
+def run_reverse(g):
+    dgl.reverse(g)
+
+full_graph_tests = {
+    'partition' : run_partition,
+    'reverse' : run_reverse,
+}
+
+mini_batch_tests = {
+}
+
+for name in full_graph_tests:
+    start = time.time()
+    full_graph_tests[name](g)
+    print('full graph {}: {} seconds'.format(name, time.time() - start))
+
+for name in mini_batch_tests:
+    start = time.time()
+    mini_batch_tests[name](g)
+    print('mini batch {}: {} seconds'.format(name, time.time() - start))
--- tests/regression/benchmarks/utils.py
+++ tests/regression/benchmarks/utils.py
@@ -4,6 +4,7 @@ import requests
 import numpy as np
 import pandas
 import dgl
+from tqdm import tqdm
 
 def _download(url, path, filename):
     fn = os.path.join(path, filename)
@@ -15,7 +16,7 @@ def _download(url, path, filename):
     sz = f_remote.headers.get('content-length')
     assert f_remote.status_code == 200, 'fail to open {}'.format(url)
     with open(fn, 'wb') as writer:
-        for chunk in f_remote.iter_content(chunk_size=1024*1024):
+        for chunk in tqdm(f_remote.iter_content(chunk_size=1024*1024)):
             writer.write(chunk)
     print('Download finished.')
 
@@ -26,12 +27,24 @@ def get_livejournal():
                          names=['src', 'dst'], compression='gzip')
     src = np.array(df['src'])
     dst = np.array(df['dst'])
-    print('construct the graph')
+    print('construct soc-LiveJournal')
+    return dgl.DGLGraph((src, dst), readonly=True)
+
+def get_friendster():
+    _download('https://snap.stanford.edu/data/bigdata/communities/com-friendster.ungraph.txt.gz',
+              '/tmp', 'com-friendster.ungraph.txt.gz')
+    df = pandas.read_csv('/tmp/com-friendster.ungraph.txt.gz', sep='\t', skiprows=4, header=None,
+                         names=['src', 'dst'], compression='gzip')
+    src = np.array(df['src'])
+    dst = np.array(df['dst'])
+    print('construct friendster')
     return dgl.DGLGraph((src, dst), readonly=True)
 
 def get_graph(name):
     if name == 'livejournal':
         return get_livejournal()
+    elif name == 'friendster':
+        return get_friendster()
     else:
         print(name + " doesn't exist")
         return None
