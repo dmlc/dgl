c736bfc66d312a485502ccd33c8c3b56f437f592
Hu <xiangkhu@MacBook-Pro-4.local>
2020-06-23 15:57:17 +0800

    Keep KarateClub class and add deprecation warning

--- python/dgl/data/__init__.py
+++ python/dgl/data/__init__.py
@@ -10,8 +10,8 @@ from .sbm import SBMMixture
 from .reddit import RedditDataset
 from .ppi import PPIDataset, LegacyPPIDataset
 from .tu import TUDataset, LegacyTUDataset
-from .gnn_benckmark import AmazonCoBuy, CoraFull, Coauthor
-from .karate import KarateClubDataset
+from .gnn_benckmark import *
+from .karate import *
 from .gindt import GINDataset
 from .bitcoinotc import BitcoinOTC
 from .gdelt import GDELT
