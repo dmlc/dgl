32058504c5a615b883a276c1c70cae11d62f022e
Da Zheng <zhengda1936@gmail.com>
2020-06-27 21:32:16 +0000

    remove run.sh

--- examples/pytorch/graphsage/experimental/run.sh
+++
@@ -1,2 +0,0 @@
-numactl -N 0 python3 train_dist.py --graph-name ogb-product --ip_config ip_config.txt --num-epochs 3 --num-client 8 &
-numactl -N 1 python3 train_dist.py --graph-name ogb-product --ip_config ip_config.txt --num-epochs 3 --num-client 8 &
