d40800742f2a1aa79bc7959194a15f6314b9faaf
aksnzhy <mctt90@gmail.com>
2020-06-28 16:42:16 +0800

    update

--- python/dgl/distributed/rpc_server.py
+++ python/dgl/distributed/rpc_server.py
@@ -83,6 +83,8 @@ def start_server(server_id, ip_config, num_clients, server_state, \
     for client_id, addr in enumerate(addr_list):
         _, ip_addr = addr.split('-')
         client_namebook[client_id] = ip_addr
+    print("client_namebook: ")
+    print(client_namebook)
     for client_id, addr in client_namebook.items():
         client_ip, client_port = addr.split(':')
         rpc.add_receiver_addr(client_ip, client_port, client_id)
