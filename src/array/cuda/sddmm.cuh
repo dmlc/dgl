d7a6ffc531c601b50c5676151f096a8c1e2bdf28
yzh119 <expye@outlook.com>
2020-07-02 19:38:10 +0800

    upd

--- src/array/cuda/sddmm.cuh
+++ src/array/cuda/sddmm.cuh
@@ -20,6 +20,36 @@ using namespace cuda;
 namespace aten {
 namespace cuda {
 
+namespace {
+
+template <int target>
+struct Selector {
+  template <typename T>
+  static __device__ __forceinline__ Call(T src, T edge, T dst) {
+    return src;
+  }
+};
+
+template <>
+template <typename T>
+__device__ __forceinline__ Selector<0>::Call(T src, T edge, T dst) {
+  return src;
+}
+
+template <>
+template <typename T>
+__device__ __forceinline__ Selector<1>::Call(T src, T edge, T dst) {
+  return edge;
+}
+
+template <>
+template <typename T>
+__device__ __forceinline__ Selector<2>::Call(T src, T edge, T dst) {
+  return dst;
+}
+
+};
+
 /*!
  * \brief CUDA kernel of g-SDDMM on Coo format.
  * \note it uses edge parallel strategy, different threadblocks (on y-axis)
@@ -28,13 +58,14 @@ namespace cuda {
  *       in feature dimension.
  */
 template <typename Idx, typename DType, typename BinaryOp,
-          bool UseBcast = false, bool UseIdx = false>
+          bool UseBcast = false, bool UseIdx = false,
+          int LhsTarget = 0, int RhsTarget = 2>
 __global__ void SDDMMCooKernel(
-  const DType *ufeat, const DType *vfeat, DType *out,
+  const DType *lhs, const DType *rhs, DType *out,
   const Idx *row, const Idx *col, const Idx* edge_map,
   int64_t N, int64_t M, int64_t E, int64_t reduce_size,
-  const int64_t *ubcast_off, const int64_t *vbcast_off,
-  int64_t ufeat_len, int64_t vfeat_len, int64_t out_len) {
+  const int64_t *lhs_off, const int64_t *rhs_off,
+  int64_t lhs_len, int64_t rhs_len, int64_t out_len) {
   // SDDMM with COO.
   Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
   const Idx stride_y = blockDim.y * gridDim.y;
@@ -43,15 +74,15 @@ __global__ void SDDMMCooKernel(
     const Idx dst = _ldg(col + ty);
     const Idx eid = UseIdx ? _ldg(edge_map + ty) : ty;
     const DType* lhsoff = BinaryOp::use_lhs ?
-      (ufeat + src * ufeat_len): nullptr;
+      (lhs + Selector<LhsTarget>::Call(src, eid, dst) * lhs_len): nullptr;
     const DType* rhsoff = BinaryOp::use_rhs ?
-      (vfeat + dst * vfeat_len): nullptr;
+      (rhs + Selector<RhsTarget>::Call(src, eid, dst) * rhs_len): nullptr;
     DType* outoff = out + eid * out_len;
     int tx = blockIdx.x * blockDim.x + threadIdx.x;
     const int stride_x = blockDim.x * gridDim.x;
     while (tx < out_len) {
-      const Idx lhs_add = UseBcast ? ubcast_off[tx] : tx;
-      const Idx rhs_add = UseBcast ? vbcast_off[tx] : tx;
+      const Idx lhs_add = UseBcast ? lhs_off[tx] : tx;
+      const Idx rhs_add = UseBcast ? rhs_off[tx] : tx;
       DType val = BinaryOp::Call(
           lhsoff + lhs_add * reduce_size,
           rhsoff + rhs_add * reduce_size,
@@ -93,13 +124,14 @@ __device__ __forceinline__ Idx BinarySearchSrc(const Idx *array, Idx length, Idx
  *       given edge on Csr format, it uses binary search (time complexity O(log N)).
  */
 template <typename Idx, typename DType, typename BinaryOp,
-          bool UseBcast = false, bool UseIdx = false>
+          bool UseBcast = false, bool UseIdx = false,
+          int lhs_target = 0, int rhs_target = 2>
 __global__ void SDDMMCsrKernel(
-  const DType *ufeat, const DType *vfeat, DType *out,
+  const DType *lhs, const DType *rhs, DType *out,
   const Idx *indptr, const Idx *indices, const Idx* edge_map,
   int64_t N, int64_t M, int64_t E, int64_t reduce_size,
-  int64_t *ubcast_off, int64_t *vbcast_off,
-  int64_t ufeat_len, int64_t vfeat_len, int64_t out_len) {
+  int64_t *lhs_off, int64_t *rhs_off,
+  int64_t lhs_len, int64_t rhs_len, int64_t out_len) {
   // SDDMM with Csr.
   Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
   const Idx stride_y = blockDim.y * gridDim.y;
@@ -109,12 +141,14 @@ __global__ void SDDMMCsrKernel(
     const Idx eid = UseIdx ? _ldg(edge_map + ty) : ty;
     int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
     const int64_t stride_x = blockDim.x * gridDim.x;
-    const DType* lhsoff = BinaryOp::use_lhs ? (ufeat + src * ufeat_len): nullptr;
-    const DType* rhsoff = BinaryOp::use_rhs ? (vfeat + dst * vfeat_len): nullptr;
+    const DType* lhsoff = BinaryOp::use_lhs ?
+      (lhs + Selector<lhs_target>::Call(src, eid, dst) * lhs_len): nullptr;
+    const DType* rhsoff = BinaryOp::use_rhs ?
+      (rhs + Selector<rhs_target>::Call(src, eid, dst) * rhs_len): nullptr;
     DType* outoff = out + eid * out_len;
     while (tx < out_len) {
-      const Idx lhs_add = UseBcast ? ubcast_off[tx] : tx;
-      const Idx rhs_add = UseBcast ? vbcast_off[tx] : tx;
+      const Idx lhs_add = UseBcast ? lhs_off[tx] : tx;
+      const Idx rhs_add = UseBcast ? rhs_off[tx] : tx;
       DType val = BinaryOp::Call(
           lhsoff + lhs_add * reduce_size,
           rhsoff + rhs_add * reduce_size,
@@ -130,26 +164,32 @@ __global__ void SDDMMCsrKernel(
  * \brief CUDA implementation of g-SDDMM on Coo format.
  * \param bcast Broadcast information.
  * \param coo The Coo matrix.
- * \param ufeat The feature on source nodes.
- * \param vfeat The feature on destination nodes.
+ * \param lhs The left hand side operand feature.
+ * \param rhs The right hand size operand feature.
  * \param out The result feature on edges.
+ * \param lhs_target A integer indicates the lhs target.
+ *        0: src, 1: edge, 2: dst
+ * \param rhs_target A integer indicates the rhs target.
+ *        0: src, 1: edge, 2: dst
  */
 template <typename Idx, typename DType, typename Op>
 void SDDMMCoo(
     const BcastOff& bcast,
     const COOMatrix& coo,
-    NDArray ufeat,
-    NDArray vfeat,
-    NDArray out) {
+    NDArray lhs,
+    NDArray rhs,
+    NDArray out,
+    int lhs_target,
+    int rhs_target) {
   const Idx *row = coo.row.Ptr<Idx>();
   const Idx *col = coo.col.Ptr<Idx>();
   const Idx *edge_map = coo.data.Ptr<Idx>();
-  const DType *ufeat_data = ufeat.Ptr<DType>();
-  const DType *vfeat_data = vfeat.Ptr<DType>();
+  const DType *lhs_data = lhs.Ptr<DType>();
+  const DType *rhs_data = rhs.Ptr<DType>();
   DType *out_data = out.Ptr<DType>();
   auto* thr_entry = runtime::CUDAThreadEntry::ThreadLocal();
 
-  int64_t *ubcast_off = nullptr, *vbcast_off = nullptr;
+  int64_t *lhs_off = nullptr, *rhs_off = nullptr;
   int64_t len = bcast.out_len,
           lhs_len = bcast.lhs_len,
           rhs_len = bcast.rhs_len;
@@ -165,13 +205,13 @@ void SDDMMCoo(
   const dim3 nthrs(ntx, nty);
   const bool use_idx = !IsNullArray(coo.data);
 
-  BCAST_IDX_CTX_SWITCH(bcast, use_idx, ufeat->ctx, ubcast_off, vbcast_off, {
+  BCAST_IDX_CTX_SWITCH(bcast, use_idx, out->ctx, lhs_off, rhs_off, {
     SDDMMCooKernel<Idx, DType, Op, UseBcast, UseIdx>
       <<<nblks, nthrs, 0, thr_entry->stream>>>(
-        ufeat_data, vfeat_data, out_data,
+        lhs_data, rhs_data, out_data,
         row, col, edge_map,
         coo.num_rows, coo.num_cols, nnz, reduce_dim,
-        ubcast_off, vbcast_off,
+        lhs_off, rhs_off,
         lhs_len, rhs_len, len
       );
   });
@@ -181,26 +221,31 @@ void SDDMMCoo(
  * \brief CUDA implementation of g-SDDMM on Csr format.
  * \param bcast Broadcast information.
  * \param csr The Csr matrix.
- * \param ufeat The feature on source nodes.
- * \param vfeat The feature on destination nodes.
+ * \param lhs The left hand side operand feature.
+ * \param rhs The right hand size operand feature.
  * \param out The result feature on edges.
+ * \param lhs_target A integer indicates the lhs target.
+ *        0: src, 1: edge, 2: dst
+ * \param rhs_target A integer indicates the rhs target.
+ *        0: src, 1: edge, 2: dst
  */
 template <typename Idx, typename DType, typename Op>
 void SDDMMCsr(
     const BcastOff& bcast,
     const CSRMatrix& csr,
-    NDArray ufeat,
-    NDArray vfeat,
-    NDArray out) { const Idx *indptr = csr.indptr.Ptr<Idx>();
+    NDArray lhs,
+    NDArray rhs,
+    NDArray out) {
+  const Idx *indptr = csr.indptr.Ptr<Idx>();
   const Idx *indices = csr.indices.Ptr<Idx>();
   const Idx *edge_map = csr.data.Ptr<Idx>();
-  const DType *ufeat_data = ufeat.Ptr<DType>();
-  const DType *vfeat_data = vfeat.Ptr<DType>();
+  const DType *lhs_data = lhs.Ptr<DType>();
+  const DType *rhs_data = rhs.Ptr<DType>();
   DType *out_data = out.Ptr<DType>();
   auto* thr_entry = runtime::CUDAThreadEntry::ThreadLocal();
   int64_t N = csr.num_rows, M = csr.num_cols, E = csr.indices->shape[0];
 
-  int64_t *ubcast_off = nullptr, *vbcast_off = nullptr;
+  int64_t *lhs_off = nullptr, *rhs_off = nullptr;
   int64_t len = bcast.out_len,
           lhs_len = bcast.lhs_len,
           rhs_len = bcast.rhs_len;
@@ -214,13 +259,13 @@ void SDDMMCsr(
   const dim3 nthrs(ntx, nty);
   const bool use_idx = !IsNullArray(csr.data);
 
-  BCAST_IDX_CTX_SWITCH(bcast, use_idx, ufeat->ctx, ubcast_off, vbcast_off, {
+  BCAST_IDX_CTX_SWITCH(bcast, use_idx, out->ctx, lhs_off, rhs_off, {
     SDDMMCsrKernel<Idx, DType, Op, UseBcast, UseIdx>
       <<<nblks, nthrs, 0, thr_entry->stream>>>(
-        ufeat_data, vfeat_data, out_data,
+        lhs_data, rhs_data, out_data,
         indptr, indices, edge_map,
         N, M, E, reduce_dim,
-        ubcast_off, vbcast_off,
+        lhs_off, rhs_off,
         lhs_len, rhs_len, len
       );
   });
