2603e6c643cb99dc9c6a250579cc94ed686e0169
Ubuntu <ubuntu@ip-172-31-51-214.ec2.internal>
2020-07-03 07:37:20 +0000

    upd

--- tests/compute/test_heterograph.py
+++ tests/compute/test_heterograph.py
@@ -1936,15 +1936,19 @@ def test_linegraph(index_dtype):
     assert lg.number_of_nodes() == 5
     assert lg.number_of_edges() == 8
     row, col = lg.edges()
-    assert F.array_equal(row, F.tensor([0, 0, 1, 2, 2, 3, 4, 4], dtype=F.data_type_dict[index_dtype]))
-    assert F.array_equal(col, F.tensor([3, 4, 0, 3, 4, 0, 1, 2], dtype=F.data_type_dict[index_dtype]))
+    assert F.array_equal(F.copy_to(row, F.cpu()),
+                         F.tensor([0, 0, 1, 2, 2, 3, 4, 4], dtype=F.data_type_dict[index_dtype]))
+    assert F.array_equal(F.copy_to(col, F.cpu()),
+                         F.tensor([3, 4, 0, 3, 4, 0, 1, 2], dtype=F.data_type_dict[index_dtype]))
 
     lg = g.line_graph(backtracking=False)
     assert lg.number_of_nodes() == 5
     assert lg.number_of_edges() == 4
     row, col = lg.edges()
-    assert F.array_equal(row, F.tensor([0, 1, 2, 4], dtype=F.data_type_dict[index_dtype]))
-    assert F.array_equal(col, F.tensor([4, 0, 3, 1], dtype=F.data_type_dict[index_dtype]))
+    assert F.array_equal(F.copy_to(row, F.cpu()),
+                         F.tensor([0, 1, 2, 4], dtype=F.data_type_dict[index_dtype]))
+    assert F.array_equal(F.copy_to(col, F.cpu()),
+                         F.tensor([4, 0, 3, 1], dtype=F.data_type_dict[index_dtype]))
 
 if __name__ == '__main__':
     # test_create()
