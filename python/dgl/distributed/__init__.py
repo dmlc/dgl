4a3e2c55c609b074cf784767e710f180f47a2814
Da Zheng <zhengda1936@gmail.com>
2020-07-08 21:02:41 +0000

    rename.

--- python/dgl/distributed/__init__.py
+++ python/dgl/distributed/__init__.py
@@ -9,4 +9,4 @@ from .rpc_server import start_server
 from .rpc_client import connect_to_server, finalize_client, shutdown_servers
 from .kvstore import KVServer, KVClient
 from .server_state import ServerState
-from .sampling import sample_neighbors, in_subgraph
+from .graph_services import sample_neighbors, in_subgraph
