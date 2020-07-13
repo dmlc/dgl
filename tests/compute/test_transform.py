36c6a07395301f2d21cf1b46e1619fc57a715137
Ubuntu <ubuntu@ip-172-31-51-214.ec2.internal>
2020-07-13 10:02:24 +0000

    Fix

--- tests/compute/test_transform.py
+++ tests/compute/test_transform.py
@@ -56,7 +56,7 @@ def test_hetero_linegraph(index_dtype):
                           np.array([0, 1, 2, 4]))
     assert np.array_equal(F.asnumpy(col),
                           np.array([4, 0, 3, 1]))
-    g = dgl.graph(([0, 1, 1, 2, 2],[2, 0, 2, 0, 1]), 
+    g = dgl.graph(([0, 1, 1, 2, 2],[2, 0, 2, 0, 1]),
         'user', 'follows', restrict_format='csr', index_dtype=index_dtype)
     lg = dgl.line_heterograph(g)
     assert lg.number_of_nodes() == 5
@@ -67,7 +67,7 @@ def test_hetero_linegraph(index_dtype):
     assert np.array_equal(F.asnumpy(col),
                           np.array([3, 4, 0, 3, 4, 0, 1, 2]))
 
-    g = dgl.graph(([0, 1, 1, 2, 2],[2, 0, 2, 0, 1]), 
+    g = dgl.graph(([0, 1, 1, 2, 2],[2, 0, 2, 0, 1]),
         'user', 'follows', restrict_format='csc', index_dtype=index_dtype)
     lg = dgl.line_heterograph(g)
     assert lg.number_of_nodes() == 5
@@ -531,7 +531,7 @@ def test_compact(index_dtype):
         g3, always_preserve=F.tensor([1, 7], dtype=getattr(F, index_dtype)))
     induced_nodes = {ntype: new_g3.nodes[ntype].data[dgl.NID] for ntype in new_g3.ntypes}
     induced_nodes = {k: F.asnumpy(v) for k, v in induced_nodes.items()}
-    
+
     assert new_g3._idtype_str == index_dtype
     assert set(induced_nodes['user']) == set([0, 1, 2, 7])
     _check(g3, new_g3, induced_nodes)
@@ -551,7 +551,7 @@ def test_compact(index_dtype):
     new_g1, new_g2 = dgl.compact_graphs(
         [g1, g2], always_preserve={'game': F.tensor([4, 7], dtype=getattr(F, index_dtype))})
     induced_nodes = {ntype: new_g1.nodes[ntype].data[dgl.NID] for ntype in new_g1.ntypes}
-    induced_nodes = {k: F.asnumpy(v) for k, v in induced_nodes.items()}    
+    induced_nodes = {k: F.asnumpy(v) for k, v in induced_nodes.items()}
     assert new_g1._idtype_str == index_dtype
     assert new_g2._idtype_str == index_dtype
     assert set(induced_nodes['user']) == set([1, 3, 5, 2, 7, 8, 9])
@@ -564,7 +564,7 @@ def test_compact(index_dtype):
         [g3, g4], always_preserve=F.tensor([1, 7], dtype=getattr(F, index_dtype)))
     induced_nodes = {ntype: new_g3.nodes[ntype].data[dgl.NID] for ntype in new_g3.ntypes}
     induced_nodes = {k: F.asnumpy(v) for k, v in induced_nodes.items()}
-    
+
     assert new_g3._idtype_str == index_dtype
     assert new_g4._idtype_str == index_dtype
     assert set(induced_nodes['user']) == set([0, 1, 2, 3, 5, 7])
@@ -645,8 +645,8 @@ def test_to_simple(index_dtype):
     # share edata
     feat_idx = F.asnumpy(wb[('user', 'follow', 'user')])
     _, indices = np.unique(feat_idx, return_index=True)
-    assert F.array_equal(sg.edges['follow'].data['h'],
-                         g.edges['follow'].data['h'][indices])
+    assert F.array_equal(sg.edges['follow'].data['h'].to(F.cpu()),
+                         g.edges['follow'].data['h'].to(F.cpu())[F.tensor(indices)])
 
     sg = dgl.to_simple(g, writeback_mapping=False, copy_ndata=False)
     for ntype in g.ntypes:
