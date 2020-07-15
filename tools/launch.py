c3a78ee9bf76ec1b32ed008c4a5c882d475c8a14
aksnzhy <mctt90@gmail.com>
2020-07-15 17:05:47 +0800

    update

--- tools/launch.py
+++ tools/launch.py
@@ -46,7 +46,9 @@ def submit_jobs(args, udf_command):
         cmd = server_cmd + ' ' + 'DGL_SERVER_ID=' + str(i)
         cmd = cmd + ' ' + udf_command
         cmd = 'cd ' + str(args.workspace) + '; ' + cmd
+        print(cmd)
         execute_remote(cmd, ip, thread_list)
+    print(cmd)
     # launch client tasks
     client_cmd = 'DGL_ROLE=client'
     client_cmd = client_cmd + ' ' + 'DGL_CONF_PATH=' + str(args.conf_path)
@@ -63,6 +65,7 @@ def submit_jobs(args, udf_command):
         cmd = cmd + ' ' + '--master_addr=' + str(hosts[0][0])
         cmd = cmd + ' ' + '--master_port=1200'
         cmd = 'cd ' + str(args.workspace) + '; ' + cmd
+        print(cmd)
         execute_remote(cmd, ip, thread_list)
 
     for thread in thread_list:
