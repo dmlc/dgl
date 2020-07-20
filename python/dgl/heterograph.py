53411e209ad8b2175be3b8354c8c2554d1ab29d3
Ubuntu <ubuntu@ip-172-31-51-214.ec2.internal>
2020-07-20 06:37:46 +0000

    Fix

--- python/dgl/heterograph.py
+++ python/dgl/heterograph.py
@@ -699,8 +699,10 @@ class DGLHeteroGraph(object):
                 old_eids = self.edges(form='eid', order='eid', etype=c_etype)
                 # trick here, eid_0 is 0 and should be handled
                 old_eids = F.scatter_row(old_eids,
-                    F.copy_to(F.tensor(0, dtype=F.int64), F.context(old_eids)),
-                    F.copy_to(F.tensor(1, dtype=F.dtype(old_eids)),F.context(old_eids)))
+                                         F.copy_to(F.tensor(0, dtype=F.int64),
+                                                            F.context(old_eids)),
+                                         F.copy_to(F.tensor(1, dtype=F.dtype(old_eids)),
+                                                            F.context(old_eids)))
                 old_eids = F.scatter_row(old_eids, eids, F.full_1d(
                     len(eids), 0, F.dtype(old_eids), F.context(old_eids)))
                 edges[c_etype] = F.tensor(F.nonzero_1d(old_eids), dtype=F.dtype(old_eids))
@@ -793,8 +795,10 @@ class DGLHeteroGraph(object):
                 old_nids = self.nodes(c_ntype)
                 # trick here, nid_0 is 0 and should be handled
                 old_nids = F.scatter_row(old_nids,
-                    F.copy_to(F.tensor(0, dtype=F.int64), F.context(old_nids)),
-                    F.copy_to(F.tensor(1, dtype=F.dtype(old_nids)),F.context(old_nids)))
+                                         F.copy_to(F.tensor(0, dtype=F.int64),
+                                                            F.context(old_nids)),
+                                         F.copy_to(F.tensor(1, dtype=F.dtype(old_nids)),
+                                                            F.context(old_nids)))
                 old_nids = F.scatter_row(old_nids, nids, F.full_1d(
                     len(nids), 0, F.dtype(old_nids), F.context(old_nids)))
                 nodes[c_ntype] = F.tensor(F.nonzero_1d(old_nids), dtype=F.dtype(old_nids))
--- python/dgl/transform.py
+++ python/dgl/transform.py
@@ -1113,8 +1113,10 @@ def remove_edges(g, eids, etype=None):
             old_eids = g.edges(form='eid', order='eid', etype=c_etype)
             # trick here, eid_0 is 0 and should be handled
             old_eids = F.scatter_row(old_eids,
-                F.copy_to(F.tensor(0, dtype=F.int64), F.context(old_eids)),
-                F.copy_to(F.tensor(1, dtype=F.dtype(old_eids)),F.context(old_eids)))
+                                     F.copy_to(F.tensor(0, dtype=F.int64),
+                                               F.context(old_eids)),
+                                     F.copy_to(F.tensor(1, dtype=F.dtype(old_eids)),
+                                               F.context(old_eids)))
             old_eids = F.scatter_row(old_eids, eids, F.full_1d(
                 len(eids), 0, F.dtype(old_eids), F.context(old_eids)))
             edges[c_etype] = F.tensor(F.nonzero_1d(old_eids), dtype=F.dtype(old_eids))
@@ -1214,8 +1216,10 @@ def remove_nodes(g, nids, ntype=None):
             old_nids = g.nodes(c_ntype)
             # trick here, nid_0 is 0 and should be handled
             old_nids = F.scatter_row(old_nids,
-                    F.copy_to(F.tensor(0, dtype=F.int64), F.context(old_nids)),
-                    F.copy_to(F.tensor(1, dtype=F.dtype(old_nids)),F.context(old_nids)))
+                                     F.copy_to(F.tensor(0, dtype=F.int64),
+                                               F.context(old_nids)),
+                                     F.copy_to(F.tensor(1, dtype=F.dtype(old_nids)),
+                                               F.context(old_nids)))
             old_nids = F.scatter_row(old_nids, nids, F.full_1d(
                 len(nids), 0, F.dtype(old_nids), F.context(old_nids)))
             nodes[c_ntype] = F.tensor(F.nonzero_1d(old_nids), dtype=F.dtype(old_nids))
