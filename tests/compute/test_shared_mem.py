4d8b065e3d22d69d4a4a8cba7033124a5b848a42
Qidong Su <soodoshll@gmail.com>
2020-07-28 06:48:50 +0000

    update

--- tests/compute/test_shared_mem.py
+++ tests/compute/test_shared_mem.py
@@ -14,7 +14,7 @@ from utils import parametrize_dtype
 import multiprocessing as mp
 import os
 
-def create_test_graph(index_dtype):
+def create_test_graph(idtype):
     plays_spmat = ssp.coo_matrix(([1, 1, 1, 1], ([0, 1, 2, 1], [0, 0, 1, 1])))
     wishes_nx = nx.DiGraph()
     wishes_nx.add_nodes_from(['u0', 'u1', 'u2'], bipartite=0)
@@ -22,10 +22,10 @@ def create_test_graph(index_dtype):
     wishes_nx.add_edge('u0', 'g1', id=0)
     wishes_nx.add_edge('u2', 'g0', id=1)
 
-    follows_g = dgl.graph([(0, 1), (1, 2)], 'user', 'follows', index_dtype=index_dtype)
-    plays_g = dgl.bipartite(plays_spmat, 'user', 'plays', 'game', index_dtype=index_dtype)
-    wishes_g = dgl.bipartite(wishes_nx, 'user', 'wishes', 'game', index_dtype=index_dtype)
-    develops_g = dgl.bipartite([(0, 0), (1, 1)], 'developer', 'develops', 'game', index_dtype=index_dtype)
+    follows_g = dgl.graph([(0, 1), (1, 2)], 'user', 'follows', idtype=idtype)
+    plays_g = dgl.bipartite(plays_spmat, 'user', 'plays', 'game', idtype=idtype)
+    wishes_g = dgl.bipartite(wishes_nx, 'user', 'wishes', 'game', idtype=idtype)
+    develops_g = dgl.bipartite([(0, 0), (1, 1)], 'developer', 'develops', 'game', idtype=idtype)
     g = dgl.hetero_from_relations([follows_g, plays_g, wishes_g, develops_g])
     return g
 
@@ -51,8 +51,8 @@ def _assert_is_identical_hetero(g, g2):
 
 @unittest.skipIf(os.name == 'nt', reason='Do not support windows yet')
 @parametrize_dtype
-def test_single_process(index_dtype):
-    hg = create_test_graph(index_dtype=index_dtype)
+def test_single_process(idtype):
+    hg = create_test_graph(idtype=idtype)
     hg_share = hg.shared_memory("hg")
     hg_rebuild = dgl.hetero_from_shared_memory('hg')
     hg_save_again = hg_rebuild.shared_memory("hg")
@@ -68,8 +68,8 @@ def sub_proc(hg_origin, name):
 
 @unittest.skipIf(os.name == 'nt', reason='Do not support windows yet')
 @parametrize_dtype
-def test_multi_process(index_dtype):
-    hg = create_test_graph(index_dtype=index_dtype)
+def test_multi_process(idtype):
+    hg = create_test_graph(idtype=idtype)
     hg_share = hg.shared_memory("hg1")
     p = mp.Process(target=sub_proc, args=(hg, "hg1"))
     p.start()
@@ -78,7 +78,7 @@ def test_multi_process(index_dtype):
 @unittest.skipIf(os.name == 'nt', reason='Do not support windows yet')
 @unittest.skipIf(F._default_context_str == 'cpu', reason="Need gpu for this test")
 def test_copy_from_gpu():
-    hg = create_test_graph(index_dtype="int32")
+    hg = create_test_graph(idtype=F.int32)
     hg_gpu = hg.to(F.cuda())
     hg_share = hg_gpu.shared_memory("hg_gpu")
     p = mp.Process(target=sub_proc, args=(hg, "hg_gpu"))
@@ -87,6 +87,6 @@ def test_copy_from_gpu():
 
 # TODO: Test calling shared_memory with Blocks (a subclass of HeteroGraph)
 if __name__ == "__main__":
-    test_single_process("int64")
-    test_multi_process("int32")
+    test_single_process(F.int64)
+    test_multi_process(F.int32)
     test_copy_from_gpu()
