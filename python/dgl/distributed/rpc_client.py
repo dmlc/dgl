41d600c73abe5078cab91b7d17995eff537aa74b
VoVAllen <jz1749@nyu.edu>
2020-07-30 06:07:36 +0000

    1111

--- python/dgl/distributed/rpc_client.py
+++ python/dgl/distributed/rpc_client.py
@@ -233,7 +233,7 @@ def init_rpc(ip_config, num_workers, max_queue_size=MAX_QUEUE_SIZE, net_type='so
             #     num_workers, initializer=_init_rpc, initargs=(ip_config, max_queue_size, net_type))
             SAMPLER_POOL = ctx.Pool(
                 num_workers, initializer=debug_str, initargs=())
-            
+            SAMPLER_POOL.join()
             print("after pool")
         NUM_SAMPLER_WORKERS = num_workers
         print("before connect")
