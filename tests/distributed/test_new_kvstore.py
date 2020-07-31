3f859c32d4ce81005d46c2906991b17fbd366180
aksnzhy <mctt90@gmail.com>
2020-07-31 14:46:49 +0800

    update

--- tests/distributed/test_new_kvstore.py
+++ tests/distributed/test_new_kvstore.py
@@ -285,7 +285,6 @@ def start_client_mul_role(i, num_clients):
         kvclient = dgl.distributed.KVClient(ip_config='kv_ip_mul_config.txt', role='trainer')
     else:
         kvclient = dgl.distributed.KVClient(ip_config='kv_ip_mul_config.txt', role='sampler')
-    time.sleep(2)
     if i == 2: # block one trainer
         time.sleep(5)
     kvclient.barrier()
