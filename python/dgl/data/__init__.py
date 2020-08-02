570af758888960fec534107edde7627fc068bb1f
HuXiangkun <huxk_hit@qq.com>
2020-08-02 21:47:37 +0800

    bitcoinotc dataset

--- python/dgl/data/__init__.py
+++ python/dgl/data/__init__.py
@@ -13,7 +13,7 @@ from .tu import TUDataset, LegacyTUDataset
 from .gnn_benckmark import AmazonCoBuy, CoraFull, Coauthor
 from .karate import KarateClub
 from .gindt import GINDataset
-from .bitcoinotc import BitcoinOTC
+from .bitcoinotc import BitcoinOTC, BitcoinOTCDataset
 from .gdelt import GDELT
 from .icews18 import ICEWS18
 from .qm7b import QM7b
