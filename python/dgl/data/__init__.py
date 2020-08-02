92a3518991ffb2dbdfa6aeeb86065f95dff479fb
HuXiangkun <huxk_hit@qq.com>
2020-08-03 01:19:46 +0800

    QM7bDataset

--- python/dgl/data/__init__.py
+++ python/dgl/data/__init__.py
@@ -16,7 +16,7 @@ from .gindt import GINDataset
 from .bitcoinotc import BitcoinOTC
 from .gdelt import GDELT
 from .icews18 import ICEWS18
-from .qm7b import QM7b
+from .qm7b import QM7b, QM7bDataset
 from .dgl_dataset import DGLDataset, DGLBuiltinDataset
 from .citation_graph import CoraGraphDataset, CiteseerGraphDataset, PubmedGraphDataset
 
