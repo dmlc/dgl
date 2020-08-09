221b6beb24751bbbe2ffcffa2ff37c8b829d827f
Da Zheng <zhengda1936@gmail.com>
2020-08-09 02:44:42 +0000

    fix.

--- python/dgl/partition.py
+++ python/dgl/partition.py
@@ -175,12 +175,9 @@ def metis_partition_assignment(g, k, balance_ntypes=None, balance_edges=False):
     '''
     # METIS works only on symmetric graphs.
     # The METIS runs on the symmetric graph to generate the node assignment to partitions.
-    from .transform import to_bidirected, to_simple # avoid cyclic import
     start = time.time()
     sym_gidx = _CAPI_DGLMakeSymmetric_Hetero(g._graph)
     sym_g = DGLHeteroGraph(gidx=sym_gidx, ntypes=['_N'], etypes=['_E'])
-    print('sym G: |V|={}, |E|={}'.format(sym_g.number_of_nodes(), sym_g.number_of_edges()))
-    #sym_g = to_bidirected(g)
     print('Convert a graph into a bidirected graph: {:.3f} seconds'.format(
         time.time() - start))
     vwgt = []
