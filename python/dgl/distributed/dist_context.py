bdb1247a2940548cf88eca1a7e7f71bc9d4d13a6
aksnzhy <mctt90@gmail.com>
2020-08-10 14:45:54 +0800

    update

--- python/dgl/distributed/dist_context.py
+++ python/dgl/distributed/dist_context.py
@@ -91,8 +91,10 @@ def finalize_worker():
 
 def join_finalize_worker():
     """join the worker close process"""
+    global SAMPLER_POOL
     if SAMPLER_POOL is not None:
         SAMPLER_POOL.join()
+    SAMPLER_POOL = None
 
 def is_initialized():
     """Is RPC initialized?
