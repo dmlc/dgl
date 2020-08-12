b0b4b4db40ed1707c89f1e3fed9049b21f3d4401
Ubuntu <ubuntu@ip-172-31-51-214.ec2.internal>
2020-08-12 08:42:10 +0000

    upd

--- examples/pytorch/rgcn/experimental/entity_classify_dist.py
+++ examples/pytorch/rgcn/experimental/entity_classify_dist.py
@@ -447,7 +447,7 @@ def main(args):
     device = th.device('cpu')
     labels = g.ndata['labels'][np.arange(g.number_of_nodes())]
     global_val_nid = th.LongTensor(np.nonzero(g.ndata['val_mask'][np.arange(g.number_of_nodes())]))
-    global_test_nid = th.LongTensor(np.nonzero(g.ndata['test_mask'][np.arange(g.number_of_nodes())])
+    global_test_nid = th.LongTensor(np.nonzero(g.ndata['test_mask'][np.arange(g.number_of_nodes())]))
     n_classes = len(th.unique(labels[labels >= 0]))
     print(labels.shape)
     print('#classes:', n_classes)
