832b392deabb5f4ba48d05700861fd0ac31b6224
Quan Gan <coin2028@hotmail.com>
2020-08-18 09:22:17 +0000

    lint

--- python/dgl/sampling/neighbor.py
+++ python/dgl/sampling/neighbor.py
@@ -2,7 +2,7 @@
 
 from .._ffi.function import _init_api
 from .. import backend as F
-from ..base import DGLError, EID
+from ..base import DGLError
 from ..heterograph import DGLHeteroGraph
 from .. import ndarray as nd
 from .. import utils
