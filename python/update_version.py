ad8530a4dc2722e481f56c1d417c36cd87bbfb19
Quan (Andy) Gan <coin2028@hotmail.com>
2020-08-21 12:29:17 +0800

    [Readme] 0.5.0 release and bump to new prerelease versions (#2082)
    * [Readme] 0.5.0 release and bump to new prerelease versions
    
    * bump version

--- python/update_version.py
+++ python/update_version.py
@@ -11,7 +11,7 @@ import re
 # current version
 # We use the version of the incoming release for code
 # that is under development
-__version__ = "0.4" + os.getenv('DGL_PRERELEASE', '')
+__version__ = "0.6" + os.getenv('DGL_PRERELEASE', '')
 print(__version__)
 
 # Implementations
