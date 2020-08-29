c18c1753d72f5291cda53180d690ce53b5f972af
Quan Gan <coin2028@hotmail.com>
2020-08-29 14:51:13 +0000

    patch release

--- python/update_version.py
+++ python/update_version.py
@@ -11,7 +11,7 @@ import re
 # current version
 # We use the version of the incoming release for code
 # that is under development
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 print(__version__)
 
 # Implementations
