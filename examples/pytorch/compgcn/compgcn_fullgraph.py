3b1bdf2e2a75e1395c748f00f0d7c9f9aa458351
james zhang <6593865@qq.com>
2020-09-15 14:11:06 +0800

    [Feature] Add input embedding layer

--- examples/pytorch/compgcn/compgcn_fullgraph.py
+++ examples/pytorch/compgcn/compgcn_fullgraph.py
@@ -103,7 +103,10 @@ def main(args):
 
     # Step 3: Create training components ===================================================== #
     loss_fn = th.nn.CrossEntropyLoss().to('cuda:{}'.format(args.gpu))
-    optimizer = optim.Adam([[input_emb.parameters() for _, input_emb in input_embs.items()], compgcn_model.parameters()], lr=0.005, weight_decay=5e-4)
+
+    paras = [input_emb.parameters() for _, input_emb in input_embs.items()]
+    paras.append(compgcn_model.parameters())
+    optimizer = optim.Adam(paras, lr=0.005, weight_decay=5e-4)
 
     # Step 4: training epoches =============================================================== #
     for epoch in range(args.max_epoch):
