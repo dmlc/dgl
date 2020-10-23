973ab85e21c9e7bfc084b16b17682e11b0b80bab
Dominique LaSalle <dlasalle@nvidia.com>
2020-10-22 17:51:55 -0700

    Fix test naming

--- tests/compute/test_async_transferer.py
+++ tests/compute/test_async_transferer.py
@@ -5,7 +5,7 @@ import backend as F
 from dgl.sampling import AsyncTransferer
 
 @unittest.skipIf(F._default_context_str == 'cpu', reason="Async transfer tests require a GPU to transfer to.")
-def async_transferer_to_gpu():
+def test_async_transferer_to_gpu():
     cpu_ones = F.ones([100,75,25], dtype=F.int32).cpu()
     tran = AsyncTransferer(F.ctx())
     t = tran.async_copy(cpu_ones, F.ctx())
@@ -15,7 +15,7 @@ def async_transferer_to_gpu():
     assert F.equal(gpu_ones.cpu(), cpu_ones)
 
 @unittest.skipIf(F._default_context_str == 'cpu', reason="Async transfer tests require a GPU to transfer from.")
-def async_transferer_to_gpu():
+def test_async_transferer_from_gpu():
     gpu_ones = F.ones([100,75,25], dtype=F.int32, ctx=F.ctx())
     tran = AsyncTransferer(F.ctx())
     t = tran.async_copy(gpu_ones, F.cpu())
@@ -25,5 +25,6 @@ def async_transferer_to_gpu():
     assert F.equal(gpu_ones.cpu(), cpu_ones)
 
 if __name__ == '__main__':
-    test_async_transferer()
+    test_async_transferer_to_gpu()
+    test_async_transferer_from_gpu()
 
