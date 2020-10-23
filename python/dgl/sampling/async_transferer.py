a4e3f429276d011c4c64713da1726d369fe7d54e
Dominique LaSalle <dlasalle@nvidia.com>
2020-10-23 16:34:06 -0700

    Wrap/unwrap backend parameters

--- python/dgl/sampling/async_transferer.py
+++ python/dgl/sampling/async_transferer.py
@@ -2,6 +2,8 @@
 
 
 from .. import backend as F
+from .. import ndarray
+from .. import utils
 from .._ffi.function import _init_api
 
 class Transfer(object):
@@ -29,33 +31,38 @@ class Transfer(object):
         Tensor
             The new tensor on the target context.
         """
-        res_tensor = _CAPI_DGLAsyncSamplerWait(self._handle, self._transfer_id)
-        return F.zerocopy(F.from_dgl_ndarray(res_tensor))
+        res_tensor = ndarray._CAPI_DGLAsyncTransfererWait(self._handle, self._transfer_id)
+        return F.zerocopy_from_dgl_ndarray(res_tensor)
 
 
 
 class AsyncTransferer(object):
     """ Class for initiating asynchronous copies to/from the GPU on a second
     GPU stream. """
-    def __init__(self, ctx):
+    def __init__(self, device):
         """ Create a new AsyncTransferer object.
 
         Parameters
         ----------
-        ctx : Device context object.
+        device : Device or context object.
             The context in which the second stream will be created. Must be a
             GPU context.
         """
-        self._handle = _CAPI_DGLAsyncSamplerCreate(ctx)
+        if isinstance(device, ndarray.DGLContext):
+            ctx = device
+        else:
+            ctx = utils.to_dgl_context(device)
+        print("ctx = {}".format(ctx))
+        self._handle = ndarray._CAPI_DGLAsyncTransfererCreate(ctx)
 
-    def async_copy(self, tensor, ctx):
+    def async_copy(self, tensor, device):
         """ Initiate an asynchronous copy on the internal stream.
 
         Parameters
         ----------
         tensor : Tensor
             The tensor to transfer.
-        ctx : Device context object.
+        device : Device or context object.
             The context to transfer to.
 
         Returns
@@ -64,7 +71,14 @@ class AsyncTransferer(object):
             A Transfer object that can be waited on to get the tensor in the
             new context.
         """
-        transfer_id = _CAPI_DGLAsyncSamplerStartTransfer(self._handle, tensor, ctx)
+        if isinstance(device, ndarray.DGLContext):
+            ctx = device
+        else:
+            ctx = utils.to_dgl_context(device)
+
+        tensor = F.zerocopy_to_dgl_ndarray(tensor)
+
+        transfer_id = ndarray._CAPI_DGLAsyncTransfererStartTransfer(self._handle, tensor, ctx)
         return Transfer(transfer_id, self._handle)
 
 
