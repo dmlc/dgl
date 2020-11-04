81ab9d6847223f5c7a7ab0070c6edbdf78e35aa5
HuXiangkun <huxk_hit@qq.com>
2020-11-04 17:14:48 +0800

    test of gin dataset takes too much time

--- tests/compute/test_data.py
+++ tests/compute/test_data.py
@@ -14,23 +14,13 @@ def test_minigc():
 def test_gin():
     ds_n_graphs = {
         'MUTAG': 188,
-        'COLLAB': 5000,
         'IMDBBINARY': 1000,
         'IMDBMULTI': 1500,
-        'NCI1': 4110,
         'PROTEINS': 1113,
         'PTC': 344,
-        'REDDITBINARY': 2000,
-        'REDDITMULTI5K': 4999
     }
     for name, n_graphs in ds_n_graphs.items():
         ds = data.GINDataset(name, self_loop=False, degree_as_nlabel=False)
-        assert len(ds) == n_graphs, (len(ds), name)
-        ds = data.GINDataset(name, self_loop=True, degree_as_nlabel=False)
-        assert len(ds) == n_graphs, (len(ds), name)
-        ds = data.GINDataset(name, self_loop=False, degree_as_nlabel=True)
-        assert len(ds) == n_graphs, (len(ds), name)
-        ds = data.GINDataset(name, self_loop=True, degree_as_nlabel=True)
         assert len(ds) == n_graphs, (len(ds), name)
 
 
