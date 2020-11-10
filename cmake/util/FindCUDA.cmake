dc7402c2710d390ba99ea09e05bb97453a7acf17
Quan Gan <coin2028@hotmail.com>
2020-11-10 06:59:46 +0000

    multiple critical bug fixes

--- cmake/util/FindCUDA.cmake
+++ cmake/util/FindCUDA.cmake
@@ -41,20 +41,6 @@ macro(find_cuda use_cuda)
         ${CUDA_TOOLKIT_ROOT_DIR}/lib/x64
         ${CUDA_TOOLKIT_ROOT_DIR}/lib/Win32)
     else(MSVC)
-      find_library(_CUDA_CUDA_LIBRARY cuda
-        PATHS ${CUDA_TOOLKIT_ROOT_DIR}
-        PATH_SUFFIXES lib lib64 targets/x86_64-linux/lib targets/x86_64-linux/lib/stubs lib64/stubs
-        NO_DEFAULT_PATH)
-      if(_CUDA_CUDA_LIBRARY)
-        set(CUDA_CUDA_LIBRARY ${_CUDA_CUDA_LIBRARY})
-      endif()
-      find_library(CUDA_NVRTC_LIBRARY nvrtc
-        PATHS ${CUDA_TOOLKIT_ROOT_DIR}
-        PATH_SUFFIXES lib lib64 targets/x86_64-linux/lib targets/x86_64-linux/lib/stubs lib64/stubs lib/x86_64-linux-gnu
-        NO_DEFAULT_PATH)
-      #find_library(CUDA_CUDNN_LIBRARY cudnn
-      #  ${CUDA_TOOLKIT_ROOT_DIR}/lib64
-      #  ${CUDA_TOOLKIT_ROOT_DIR}/lib)
       find_library(CUDA_CUBLAS_LIBRARY cublas
         ${CUDA_TOOLKIT_ROOT_DIR}/lib64
         ${CUDA_TOOLKIT_ROOT_DIR}/lib)
