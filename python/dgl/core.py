98e11533e266c5b30b60d3852bc9f76c6ca2898f
Zihao Ye <expye@outlook.com>
2020-12-03 12:00:29 +0800

    [hotfix] Produce empty tensor in data frame for emtpy graphs. (#2387)
    * upd
    
    * lint
    
    * lint1

--- python/dgl/core.py
+++ python/dgl/core.py
@@ -274,11 +274,8 @@ def message_passing(g, mfunc, rfunc, afunc):
     dict[str, Tensor]
         Results from the message passing computation.
     """
-    if g.number_of_edges() == 0:
-        # No message passing is triggered.
-        ndata = {}
-    elif (is_builtin(mfunc) and is_builtin(rfunc) and
-          getattr(ops, '{}_{}'.format(mfunc.name, rfunc.name), None) is not None):
+    if (is_builtin(mfunc) and is_builtin(rfunc) and
+            getattr(ops, '{}_{}'.format(mfunc.name, rfunc.name), None) is not None):
         # invoke fused message passing
         ndata = invoke_gspmm(g, mfunc, rfunc)
     else:
