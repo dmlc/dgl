5792a6c2063edde19b05f82597929e1b28234bd1
Minjie Wang <wmjlyjemaine@gmail.com>
2020-12-10 11:34:52 +0000

    missing files

---
+++ benchmarks/build_dgl_asv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+. /opt/conda/etc/profile.d/conda.sh
+
+# build
+#CMAKE_VARS="-DUSE_CUDA=ON"
+mkdir -p build
+pushd build
+cmake $CMAKE_VARS ..
+make -j
+popd
---
+++ benchmarks/install_dgl_asv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+. /opt/conda/etc/profile.d/conda.sh
+
+pip install -r docker/install/conda_env/torch_gpu_pip.txt
+pip install pandas
+
+# install
+pushd python
+rm -rf build *.egg-info dist
+pip uninstall -y dgl
+python3 setup.py install
+popd
