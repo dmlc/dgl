a6af3fb5345ec5dc45c35242e9b95c36788ab8c1
VoVAllen <jz1749@nyu.edu>
2020-12-15 09:42:26 +0000

    regression

--- benchmarks/build_dgl_asv.sh
+++ benchmarks/build_dgl_asv.sh
@@ -4,8 +4,15 @@ set -e
 
 . /opt/conda/etc/profile.d/conda.sh
 
+# Default building only with cpu
+DEVICE=${DGL_BENCH_DEVICE:-cpu}
+
 # build
-CMAKE_VARS="-DUSE_CUDA=ON"
+if [[ $DEVICE == "cpu" ]]; then
+    CMAKE_VARS=""
+else
+    CMAKE_VARS="-DUSE_CUDA=ON"
+fi
 mkdir -p build
 pushd build
 cmake $CMAKE_VARS ..
