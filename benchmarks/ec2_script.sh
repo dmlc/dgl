8b8000497ee5a0e871bd13e43001fa56865c0e55
VoVAllen <jz1749@nyu.edu>
2020-12-22 11:21:48 +0000

    run gpu

--- benchmarks/ec2_script.sh
+++ benchmarks/ec2_script.sh
@@ -1,6 +1,12 @@
 export INSTANCE_TYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
 cd ~/git_repo/benchmarks
 python replace_branch.py --branch ${GIT_BRANCH}
-bash publish.sh ${INSTANCE_TYPE} cpu
+DEVICE=cpu
+if [[ ${INSTANCE_TYPE} == g* ]] || [[ ${INSTANCE_TYPE} == p* ]]; then
+    DEVICE=gpu
+else
+    DEVICE=cpu
+fi
+bash publish.sh ${INSTANCE_TYPE} ${DEVICE}
 aws s3 sync results s3://dgl-asv-data/ci/results/
 aws s3 sync html s3://dgl-asv-data/ci/html/ --acl public-read
