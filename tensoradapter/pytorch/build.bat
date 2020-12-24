a678332a0cea13d62043bc94f985acf3e95e3b87
Quan Gan <coin2028@hotmail.com>
2020-12-24 20:26:04 +0800

    fix windows

--- tensoradapter/pytorch/build.bat
+++ tensoradapter/pytorch/build.bat
@@ -2,6 +2,7 @@ REM Helper script to build tensor adapter libraries for PyTorch
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
 
+MD "%BINDIR%\tensoradapter\pytorch"
 DEL /S /Q build
 MD build
 PUSHD build
@@ -10,9 +11,9 @@ IF x%1x == xx GOTO single
 
 FOR %%X IN (%*) DO (
 	DEL /S /Q *
-	%CMAKE_COMMAND% -DCUDA_TOOLKIT_ROOT_DIR=%CUDA_TOOLKIT_ROOT_DIR% -DPYTHON_INTERP=%%X .. -G "Visual Studio 16 2019" || EXIT /B 1
+	"%CMAKE_COMMAND%" -DCMAKE_CONFIGURATION_TYPES=Release -DCUDA_TOOLKIT_ROOT_DIR="%CUDA_TOOLKIT_ROOT_DIR%" -DPYTHON_INTERP=%%X .. -G "Visual Studio 16 2019" || EXIT /B 1
 	msbuild tensoradapter_pytorch.sln /m /nr:false || EXIT /B 1
-	COPY /Y Release\*.dll %BINDIR%\tensoradapter\pytorch || EXIT /B 1
+	COPY /Y Release\*.dll "%BINDIR%\tensoradapter\pytorch" || EXIT /B 1
 )
 
 GOTO end
@@ -20,12 +21,11 @@ GOTO end
 :single
 
 DEL /S /Q *
-%CMAKE_COMMAND% -DCUDA_TOOLKIT_ROOT_DIR=%CUDA_TOOLKIT_ROOT_DIR% .. -G "Visual Studio 16 2019" || EXIT /B 1
+"%CMAKE_COMMAND%" -DCMAKE_CONFIGURATION_TYPES=Release -DCUDA_TOOLKIT_ROOT_DIR="%CUDA_TOOLKIT_ROOT_DIR%" .. -G "Visual Studio 16 2019" || EXIT /B 1
 msbuild tensoradapter_pytorch.sln /m /nr:false || EXIT /B 1
-COPY /Y Release\*.dll %BINDIR%\tensoradapter\pytorch || EXIT /B 1
+COPY /Y Release\*.dll "%BINDIR%\tensoradapter\pytorch" || EXIT /B 1
 
 :end
-MD %BINDIR%\tensoradapter\pytorch
 POPD
 
 ENDLOCAL
