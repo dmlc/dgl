2546a3aad9eaa476a4a2f47a309e2e812534b897
yzh119 <expye@outlook.com>
2020-12-29 06:26:32 +0000

    upd

--- featgraph/include/featgraph.h
+++ featgraph/include/featgraph.h
@@ -1,9 +1,13 @@
+#ifndef FEATGRAPH_H_
+#define FEATGRAPH_H_
 #include <dlpack/dlpack.h>
 
 namespace dgl {
 namespace featgraph {
-    void SDDMMTreeReduction(DLManagedTensor* row, DLManagedTensor* col, 
-                            DLManagedTensor* lhs, DLManagedTensor* rhs, 
-                            DLManagedTensor* out);
+void SDDMMTreeReduction(DLManagedTensor* row, DLManagedTensor* col, 
+                        DLManagedTensor* lhs, DLManagedTensor* rhs, 
+                        DLManagedTensor* out);
 }  // namespace featgraph
 }  // namespace dgl
+
+#endif  // FEATGRAPH_H_
--- featgraph/src/featgraph.cc
+++ featgraph/src/featgraph.cc
@@ -8,16 +8,20 @@
 namespace dgl {
 namespace featgraph {
 
-static tvm::runtime::Module featgraph = tvm::runtime::Module::LoadFromFile("/home/ubuntu/dgl/build/libfeatgraph_kernels.so");
+tvm::runtime::Module LoadFeatGraph() {
+  static tvm::runtime::Module mod = tvm::runtime::Module::LoadFromFile("/home/ubuntu/dgl/build/libfeatgraph_kernels.so");
+  return mod;
+}
 
 void SDDMMTreeReduction(DLManagedTensor* row, DLManagedTensor* col, 
                         DLManagedTensor* lhs, DLManagedTensor* rhs, 
                         DLManagedTensor* out) {
+  const static tvm::runtime::ModuleNode* mod = LoadFeatGraph().operator->();
   std::stringstream ss;
   ss << "SDDMMTreeReduction_int" << (int) (row->dl_tensor).dtype.bits;
   ss << "_float" << (int) (lhs->dl_tensor).dtype.bits;
   std::string f_name = ss.str();
-  tvm::runtime::PackedFunc f = featgraph.GetFunction(f_name);
+  tvm::runtime::PackedFunc f = const_cast<tvm::runtime::ModuleNode*>(mod)->GetFunction(f_name);
   if (f != nullptr)
     f(row, col, lhs, rhs, out);
 }
