67500e3b4d6e262c3927b26a4b8b29749dcdcb8b
Quan Gan <coin2028@hotmail.com>
2020-12-30 17:16:34 +0800

    remove

--- python/dgl/nn/__init__.py
+++ python/dgl/nn/__init__.py
@@ -18,7 +18,6 @@ import sys
 import os
 from ..backend import backend_name
 from ..utils import expand_as_pair
-from . import functional
 
 def _load_backend(mod_name):
     mod = importlib.import_module('.%s' % mod_name, __name__)
