5942b71a50c3d3f18dadf98e5d4fa22ad3856bc4
zhangtianqi <lygztq@gmail.com>
2021-01-14 18:39:07 +0800

    remote redundant network

--- examples/pytorch/hgp_sl/networks.py
+++ examples/pytorch/hgp_sl/networks.py
@@ -2,61 +2,7 @@ import torch
 from dgl.nn import AvgPooling, MaxPooling
 import torch.nn.functional as F
 import torch.nn
-from layers import HGPSLPool, WeightedGraphConv, ConvPoolReadout
-
-
-class SimpleModel(torch.nn.Module):
-    """Simplified version of Model. It has fixed number of layers (3)."""
-    def __init__(self, in_feat:int, out_feat:int, hid_feat:int,
-                 dropout:float=0.0, pool_ratio:float=0.5,
-                 sample:bool=True, sparse:bool=True, sl:bool=True,
-                 lamb:float=1.):
-        super(SimpleModel, self).__init__()
-        self.in_feat = in_feat
-        self.out_feat = out_feat
-        self.hid_feat = hid_feat
-        self.dropout = dropout
-        self.pool_ratio = pool_ratio
-        
-        self.conv1 = WeightedGraphConv(in_feat, hid_feat)
-        self.conv2 = WeightedGraphConv(hid_feat, hid_feat)
-        self.conv3 = WeightedGraphConv(hid_feat, hid_feat)
-
-        self.pool1 = HGPSLPool(hid_feat, ratio=pool_ratio, 
-                               sample=sample, sparse=sparse,
-                               sl=sl, lamb=lamb)
-        self.pool2 = HGPSLPool(hid_feat, ratio=pool_ratio, 
-                               sample=sample, sparse=sparse,
-                               sl=sl, lamb=lamb)
-
-        self.lin1 = torch.nn.Linear(hid_feat * 2, hid_feat)
-        self.lin2 = torch.nn.Linear(hid_feat, hid_feat // 2)
-        self.lin3 = torch.nn.Linear(hid_feat // 2, self.out_feat)
- 
-        self.avg_pool = AvgPooling()
-        self.max_pool = MaxPooling()
-
-    def forward(self, graph, n_feat):
-        final_readout = None
-
-        n_feat = F.relu(self.conv1(graph, n_feat))
-        graph, n_feat, e_feat, _ = self.pool1(graph, n_feat, None)
-        final_readout = torch.cat([self.avg_pool(graph, n_feat), self.max_pool(graph, n_feat)], dim=-1)
-
-        n_feat = F.relu(self.conv2(graph, n_feat, e_feat))
-        graph, n_feat, e_feat, _ = self.pool2(graph, n_feat, e_feat)
-        final_readout = final_readout + torch.cat([self.avg_pool(graph, n_feat), self.max_pool(graph, n_feat)], dim=-1)
-
-        n_feat = F.relu(self.conv3(graph, n_feat, e_feat))
-        final_readout = final_readout + torch.cat([self.avg_pool(graph, n_feat), self.max_pool(graph, n_feat)], dim=-1)
-
-        n_feat = F.relu(self.lin1(final_readout))
-        n_feat = F.dropout(n_feat, p=self.dropout, training=self.training)
-        n_feat = F.relu(self.lin2(n_feat))
-        n_feat = F.dropout(n_feat, p=self.dropout, training=self.training)
-        n_feat = self.lin3(n_feat)
-
-        return F.log_softmax(n_feat, dim=-1)
+from layers import ConvPoolReadout
 
 
 class Model(torch.nn.Module):
