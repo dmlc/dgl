3907b008e166d292eea3ecc0bfb17c04e7bdfc69
VoVAllen <jz1749@nyu.edu>
2021-01-15 08:08:51 +0000

    new batch

---
+++ benchmarks/benchmarks/api/bench_metis_partition.py
@@ -0,0 +1,23 @@
+import time
+import dgl
+import torch
+import numpy as np
+
+from .. import utils
+
+
+@utils.skip_if_gpu()
+@utils.benchmark('time', timeout=1200)
+@utils.parametrize('graph_name', ['reddit', 'ogbn-product'])
+@utils.parametrize('k', [2, 4, 8])
+def track_time(graph_name, k):
+    device = utils.get_bench_device()
+    data = utils.process_data(graph_name)
+    graph = data[0]
+
+    # timing
+    t0 = time.time()
+    gg = dgl.transform.metis_partition(graph, k)
+    t1 = time.time()
+
+    return t1 - t0
--- benchmarks/benchmarks/api/bench_unbatch.py
+++ benchmarks/benchmarks/api/bench_unbatch.py
@@ -11,15 +11,16 @@ def track_time(batch_size):
     ds = dgl.data.QM7bDataset()
     # prepare graph
     graphs = ds[0:batch_size][0]
+    bg = dgl.batch(graphs)
 
     # dry run
     for i in range(10):
-        g = dgl.batch(graphs)
+        glist = dgl.unbatch(bg)
 
     # timing
     t0 = time.time()
     for i in range(100):
-        g = dgl.batch(graphs)
+        glist = dgl.unbatch(bg)
     t1 = time.time()
 
     return (t1 - t0) / 100
