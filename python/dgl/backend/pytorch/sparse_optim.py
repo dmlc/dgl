d29fb5e5cbcfab65aad0a5696b84ac5d14aeccea
Ubuntu <ubuntu@ip-172-31-56-220.ec2.internal>
2021-01-15 15:00:55 +0000

    upd

--- python/dgl/backend/pytorch/sparse_optim.py
+++ python/dgl/backend/pytorch/sparse_optim.py
@@ -347,12 +347,10 @@ class SparseAdam(SparseGradOptimizer):
             state_mem[state_idx] = update_mem.to(state_dev, non_blocking=True)
             state_power[state_idx] = update_power.to(state_dev, non_blocking=True)
 
-            print(update_mem.dtype)
-            print(beta1.dtype)
-            print(state_step.dtype)
-            print(th.pow(beta1, state_step).dtype)
-            update_mem_corr = update_mem / (1. - th.pow(beta1, state_step)).unsqueeze(1)
-            update_power_corr = update_power / (1. - th.pow(beta2, state_step)).unsqueeze(1)
+            update_mem_corr = update_mem / (1. - th.pow(th.tensor(beta1, device=exec_dev),
+                                                        state_step)).unsqueeze(1)
+            update_power_corr = update_power / (1. - th.pow(th.tensor(beta2, device=exec_dev),
+                                                            state_step)).unsqueeze(1)
             std_values = clr * update_mem_corr / (th.sqrt(update_power_corr) + eps)
 
             emb.emb_tensor[state_idx] -= std_values.to(state_dev)
