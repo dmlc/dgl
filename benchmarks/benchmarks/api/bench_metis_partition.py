79478fd7ce01f936bb6fca302678804ba0650efa
VoVAllen <jz1749@nyu.edu>
2021-01-20 11:22:43 +0000

    fix warning

--- benchmarks/benchmarks/api/bench_metis_partition.py
+++ benchmarks/benchmarks/api/bench_metis_partition.py
@@ -8,16 +8,20 @@ from .. import utils
 
 @utils.skip_if_gpu()
 @utils.benchmark('time', timeout=1200)
-@utils.parametrize('graph_name', ['reddit', 'ogbn-product'])
+@utils.parametrize('graph_name', ['reddit'])
 @utils.parametrize('k', [2, 4, 8])
 def track_time(graph_name, k):
     device = utils.get_bench_device()
     data = utils.process_data(graph_name)
     graph = data[0]
+    # dry run
+    dry_run_data = utils.process_data('pubmed')
+    gg = dgl.transform.metis_partition(dry_run_data[0], k)
 
     # timing
     t0 = time.time()
-    gg = dgl.transform.metis_partition(graph, k)
+    for i in range(3):
+        gg = dgl.transform.metis_partition(graph, k)
     t1 = time.time()
 
-    return t1 - t0
+    return (t1 - t0) / 3 
