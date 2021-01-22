d7e6f23de6289f2cb37dd9bbdd7571dc88e52061
Da Zheng <zhengda1936@gmail.com>
2021-01-22 18:38:07 +0000

    explain IdMap.

--- python/dgl/id_map.py
+++ python/dgl/id_map.py
@@ -1,4 +1,4 @@
-"""Module for mapping between node/edge Ids and node/edge types."""
+"""Module for mapping between node/edge IDs and node/edge types."""
 import numpy as np
 
 from ._ffi.function import _init_api
@@ -6,20 +6,41 @@ from . import backend as F
 from . import utils
 
 class IdMap:
-    '''This maps node/edge Ids in the homogeneous form to per-type form.
+    '''Map node/edge IDs in the homogeneous form to per-type IDs and determine their types.
 
-    It stores the Id ranges for each partition and for each type. The Ids are in
-    the homogeneous form (i.e., all nodes of different types have unique Ids). For each type,
-    all Ids in a partition fall in a contiguous range. The Id ranges are stored in
-    a matrix, where each row has two elements to represent the beginning and the end of
-    the range.
+    For a heterogeneous graph, we can use a homogeneous graph format to store it. With
+    this format, all nodes have unique IDs. We refer to these IDs as homogeneous IDs.
+    In a heterogeneous graph format, IDs are only unique within the type. We refer to
+    this type of IDs as per-type IDs.
 
-    This class computes the homogeneous Ids to per-type Ids and their types.
+    When storing a heterogeneous graph in the homogeneous format, we arrange node IDs
+    and edge IDs so that all nodes of the same type have contiguous IDs. If
+    the graph is partitioned, the nodes of the same type within a partition have
+    contiguous IDs.
+
+    The table below shows an example of storing node IDs in a heterogeneous graph
+    in the homogeneous format. The heterogeneous graph has two node types and has
+    two partitions. The homogeneous ID range [0, 100) is used for nodes of type 0
+    in partition 0, [100, 200) is used for nodes of type 1 in partition 0, and so on.
+
+    ```
+    +---------+------+----------
+      range   | type | partition
+    [0, 100)  |   0  |    0
+    [100,200) |   1  |    0
+    [200,300) |   0  |    1
+    [300,400) |   1  |    1
+    ```
+
+    This class is to map any homogeneous ID to a per-type ID and its type. For example,
+    homogeneous node ID 90 is mapped to type 0 and per-type ID 90; homogeneous node ID
+    201 is mapped to type 0 and per-type ID 101.
 
     Parameters
     ----------
-    id_ranges : dict of tensors.
-        The Id ranges of each partition for each type.
+    id_ranges : dict[str, Tensor].
+        The ID ranges of each partition for each type. It stores the same information
+        as the `node_map` argument in `RangePartitionBook`.
     '''
     def __init__(self, id_ranges):
         self.num_parts = list(id_ranges.values())[0].shape[0]
@@ -32,6 +53,8 @@ class IdMap:
             ranges[i::self.num_types] = id_range
             map1 = np.cumsum(id_range[:, 1] - id_range[:, 0])
             typed_map.append(map1)
+        print(ranges)
+        print(typed_map)
 
         assert np.all(np.diff(ranges[:, 0]) >= 0)
         assert np.all(np.diff(ranges[:, 1]) >= 0)
@@ -40,12 +63,12 @@ class IdMap:
         self.typed_map = utils.toindex(np.concatenate(typed_map))
 
     def __call__(self, ids):
-        '''Map Ids in the homogeneous form to per-type Ids and types.
+        '''Map IDs in the homogeneous form to per-type IDs and types.
 
         Parameters
         ----------
         ids : 1D tensor
-            The homogeneous Id.
+            The homogeneous ID.
 
         Returns
         -------
