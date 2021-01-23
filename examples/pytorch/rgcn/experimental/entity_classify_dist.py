fb59cb579972abc636e978ba5a0a0db305e2c941
Da Zheng <zhengda1936@gmail.com>
2021-01-23 23:47:05 +0000

    Revert "temp fix."
    This reverts commit c450717b9f578b8c48769c675f2a19d6c1e64381.

--- examples/pytorch/rgcn/experimental/entity_classify_dist.py
+++ examples/pytorch/rgcn/experimental/entity_classify_dist.py
@@ -106,8 +106,7 @@ class EntityClassify(nn.Module):
         h = feats
         for layer, block in zip(self.layers, blocks):
             block = block.to(self.device)
-            #h = layer(block, h, block.edata[dgl.ETYPE], block.edata['norm'])
-            h = layer(block, h, block.edata[dgl.ETYPE])
+            h = layer(block, h, block.edata[dgl.ETYPE], block.edata['norm'])
         return h
 
 def init_emb(shape, dtype):
@@ -271,8 +270,8 @@ def evaluate(g, model, embed_layer, get_edata, labels, eval_loader, test_loader,
     with th.no_grad():
         for sample_data in tqdm.tqdm(eval_loader):
             seeds, blocks = sample_data
-            #for block in blocks:
-            #    block.edata['norm'] = get_edata(block.edata[dgl.EID], block.edata[dgl.ETYPE])
+            for block in blocks:
+                block.edata['norm'] = get_edata(block.edata[dgl.EID], block.edata[dgl.ETYPE])
             feats = embed_layer(blocks[0].srcdata[dgl.NID], blocks[0].srcdata[dgl.NTYPE])
             logits = model(blocks, feats)
             eval_logits.append(logits.cpu().detach())
@@ -287,8 +286,8 @@ def evaluate(g, model, embed_layer, get_edata, labels, eval_loader, test_loader,
     with th.no_grad():
         for sample_data in tqdm.tqdm(test_loader):
             seeds, blocks = sample_data
-            #for block in blocks:
-            #    block.edata['norm'] = get_edata(block.edata[dgl.EID], block.edata[dgl.ETYPE])
+            for block in blocks:
+                block.edata['norm'] = get_edata(block.edata[dgl.EID], block.edata[dgl.ETYPE])
             feats = embed_layer(blocks[0].srcdata[dgl.NID], blocks[0].srcdata[dgl.NTYPE])
             logits = model(blocks, feats)
             test_logits.append(logits.cpu().detach())
@@ -473,8 +472,8 @@ def run(args, device, data):
             sample_time += tic_step - start
             sample_t.append(tic_step - start)
 
-            #for block in blocks:
-            #    block.edata['norm'] = get_edata(block.edata[dgl.EID], block.edata[dgl.ETYPE])
+            for block in blocks:
+                block.edata['norm'] = get_edata(block.edata[dgl.EID], block.edata[dgl.ETYPE])
             feats = embed_layer(blocks[0].srcdata[dgl.NID], blocks[0].srcdata[dgl.NTYPE])
             label = labels[seeds]
             copy_time = time.time()
