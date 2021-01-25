5a5e930a46ccabd966c6c6c3ae0b749798f90eee
yzh119 <expye@outlook.com>
2021-01-25 07:24:21 +0000

    upd

--- src/array/cuda/spmat_op_impl_coo.cu
+++ src/array/cuda/spmat_op_impl_coo.cu
@@ -14,6 +14,7 @@
 namespace dgl {
 
 using runtime::NDArray;
+using namespace cuda;
 
 namespace aten {
 namespace impl {
@@ -39,36 +40,34 @@ __global__ void _COOGetRowNNZKernel(
   IdType tx = threadIdx.x;
   IdType bx = blockIdx.x;
   local_cnt[tx] = 0;
-  if (bx * blockDim.x + tx < nnz)
-    local_cnt[tx] = (row_indices[bx * blockDim.x + tx] == row_query);
-  __syncthreads();
-
-  if (tx < 512) {
-    local_cnt[tx] += local_cnt[tx + 512];
+  IdType start = bx * blockDim.x;
+  while (start < nnz) {
+    if (start + tx < nnz)
+      local_cnt[tx] = (row_indices[start + tx] == row_query);
     __syncthreads();
-  }
-
-  if (tx < 256) {
-    local_cnt[tx] += local_cnt[tx + 256];
-    __syncthreads();
-  }
-
-  if (tx < 128) {
-    local_cnt[tx] += local_cnt[tx + 128];
-    __syncthreads();
-  }
-
-  if (tx < 64) {
-    local_cnt[tx] += local_cnt[tx + 64];
-    __syncthreads();
-  }
-
-  if (tx < 32) {
-    _warpReduce(local_cnt, tx);
-  }
-
-  if (tx == 0) {
-    cuda::AtomicAdd(glb_cnt, local_cnt[tx]);
+    if (tx < 512) {
+      local_cnt[tx] += local_cnt[tx + 512];
+      __syncthreads();
+    }
+    if (tx < 256) {
+      local_cnt[tx] += local_cnt[tx + 256];
+      __syncthreads();
+    }
+    if (tx < 128) {
+      local_cnt[tx] += local_cnt[tx + 128];
+      __syncthreads();
+    }
+    if (tx < 64) {
+      local_cnt[tx] += local_cnt[tx + 64];
+      __syncthreads();
+    }
+    if (tx < 32) {
+      _warpReduce(local_cnt, tx);
+    }
+    if (tx == 0) {
+      cuda::AtomicAdd(glb_cnt, local_cnt[tx]);
+    }
+    start += blockDim.x * gridDim.x;
   }
 }
 
@@ -85,7 +84,7 @@ int64_t COOGetRowNNZ(COOMatrix coo, int64_t row) {
       coo.row.Ptr<IdType>(), rst.Ptr<IdType>(),
       row, nnz);
   rst = rst.CopyTo(DLContext{kDLCPU, 0});
-  return *rst.Ptr<uint64_t>();
+  return *rst.Ptr<IdType>();
 }
 
 template int64_t COOGetRowNNZ<kDLGPU, int32_t>(COOMatrix, int64_t);
@@ -94,43 +93,13 @@ template int64_t COOGetRowNNZ<kDLGPU, int64_t>(COOMatrix, int64_t);
 template <typename IdType>
 __global__ void _COOGetBatchRowNNZKernel(
     const IdType* __restrict__ row_indices,
-    const IdType* __restrict__ row_queries,
-    IdType* __restrict__ cnts,
+    IdType* __restrict__ glb_cnts,
     IdType nnz) {
-  __shared__ IdType local_cnt[1024];
-  IdType tx = threadIdx.x;
-  IdType bx = blockIdx.x;
-  IdType query = row_queries[bx];
-  local_cnt[tx] = 0;
-  for (int i = tx; i < nnz; i += blockDim.x)
-    local_cnt[tx] += (row_indices[i] == query);
-  __syncthreads();
-
-  if (tx < 512) {
-    local_cnt[tx] += local_cnt[tx + 512];
-    __syncthreads();
-  }
-
-  if (tx < 256) {
-    local_cnt[tx] += local_cnt[tx + 256];
-    __syncthreads();
-  }
-
-  if (tx < 128) {
-    local_cnt[tx] += local_cnt[tx + 128];
-    __syncthreads();
-  }
-
-  if (tx < 64) {
-    local_cnt[tx] += local_cnt[tx + 64];
-    __syncthreads();
-  }
-
-  if (tx < 32)
-    _warpReduce(local_cnt, tx);
-
-  if (tx == 0) {
-    cnts[bx] = local_cnt[tx];
+  IdType eid = blockIdx.x * blockDim.x + threadIdx.x;
+  while (eid < nnz) {
+    IdType row = row_indices[eid];
+    cuda::AtomicAdd(glb_cnts + row, IdType(1));
+    eid += blockDim.x * gridDim.x;
   }
 }
 
@@ -139,15 +108,17 @@ NDArray COOGetRowNNZ(COOMatrix coo, NDArray rows) {
   auto* thr_entry = runtime::CUDAThreadEntry::ThreadLocal();
   const auto& ctx = coo.row->ctx;
   IdType nnz = coo.row->shape[0];
+  IdType num_rows = coo.num_rows;
   IdType num_queries = rows->shape[0];
   IdType nt = 1024;
-  IdType nb = dgl::cuda::FindNumBlocks<'x'>(num_queries);
-  NDArray rst = NDArray::Empty({num_queries}, rows->dtype, rows->ctx);
+  IdType nb = dgl::cuda::FindNumBlocks<'x'>((nnz + nt - 1) / nt);
+  NDArray in_degrees = NDArray::Empty({num_rows}, rows->dtype, rows->ctx);
+  _Fill(in_degrees.Ptr<IdType>(), num_rows, IdType(0));
   CUDA_KERNEL_CALL(_COOGetBatchRowNNZKernel,
       nb, nt, 0, thr_entry->stream,
-      coo.row.Ptr<IdType>(), rows.Ptr<IdType>(), rst.Ptr<IdType>(),
+      coo.row.Ptr<IdType>(), in_degrees.Ptr<IdType>(),
       nnz);
-  return rst;
+  return IndexSelect(in_degrees, rows);
 }
 
 template NDArray COOGetRowNNZ<kDLGPU, int32_t>(COOMatrix, NDArray);
