d809ee273a866144234a4038941889c41084f67b
Minjie Wang <wmjlyjemaine@gmail.com>
2021-01-27 12:47:48 +0000

    add desc

--- benchmarks/benchmarks/kernel/bench_gspmm_u_mul_e_sum.py
+++ benchmarks/benchmarks/kernel/bench_gspmm_u_mul_e_sum.py
@@ -7,6 +7,13 @@ from .. import utils
 def calc_gflops(graph, feat_size, num_heads, time):
     return round(2 * graph.num_edges() * feat_size / 1000000000 / time, 2)  # count both mul and add
 
+# The benchmarks include broadcasting cases.
+# Given feat_size = D, num_heads = H, the node feature shape will be (H, D // H)
+#   while the edge feature shape will be (H, ), so tested operations will broadcast
+#   along the last dimension. The total FLOP is controlled by the feat_size no
+#   matter how many heads are there.
+# If num_heads = 0, it falls back to the normal element-wise operation without
+#   broadcasting.
 @utils.benchmark('flops', timeout=600)
 @utils.parametrize('graph', ['ogbn-arxiv', 'reddit', 'ogbn-proteins'])
 @utils.parametrize('feat_size', [4, 32, 256])
