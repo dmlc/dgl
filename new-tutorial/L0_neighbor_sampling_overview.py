7a6c6c4b309a493f8ddcfe3208d673af9513a4b0
Quan Gan <coin2028@hotmail.com>
2021-01-31 10:15:25 +0800

    fix

--- new-tutorial/L0_neighbor_sampling_overview.py
+++ new-tutorial/L0_neighbor_sampling_overview.py
@@ -12,54 +12,56 @@ By the end of this tutorial, you will be able to
 -  Understand the pipeline of stochastic GNN training.
 -  Understand what is neighbor sampling and why it yields a bipartite
    graph for each GNN layer.
-
-Message Passing Review
-----------------------
-
-Recall that in `Gilmer et al. <https://arxiv.org/abs/1704.01212>`__
-(also in :doc:`message passing tutorial <3_message_passing>`), the
-message passing formulation is as follows:
-
-.. math::
-
-
-   m_{u\to v}^{(l)} = M^{(l)}\left(h_v^{(l-1)}, h_u^{(l-1)}, e_{u\to v}^{(l-1)}\right)
-
-.. math::
-
-
-   m_{v}^{(l)} = \sum_{u\in\mathcal{N}(v)}m_{u\to v}^{(l)}
-
-.. math::
-
-
-   h_v^{(l)} = U^{(l)}\left(h_v^{(l-1)}, m_v^{(l)}\right)
-
-where DGL calls :math:`M^{(l)}` the *message function*, :math:`\sum` the
-*reduce function* and :math:`U^{(l)}` the *update function*. Note that
-:math:`\sum` here can represent any function and is not necessarily a
-summation.
-
-Essentially, the :math:`l`-th layer representation of a single node
-depends on the :math:`(l-1)`-th layer representation of the same node,
-as well as the :math:`(l-1)`-th layer representation of the neighboring
-nodes. Those :math:`(l-1)`-th layer representations then depend on the
-:math:`(l-2)`-th layer representation of those nodes, as well as their
-neighbors.
-
-The following animation shows how a 2-layer GNN is supposed to compute
-the output of node 5:
-
-|image1|
-
-You can see that to compute node 5 from the second layer, you will need
-its direct neighbors’ first layer representations (colored in yellow),
-which in turn needs their direct neighbors’ (i.e. node 5’s second-hop
-neighbors’) representations (colored in green).
-
-.. |image1| image:: assets/sampling.gif
-
 """
+
+
+######################################################################
+# Message Passing Review
+# ----------------------
+# 
+# Recall that in `Gilmer et al. <https://arxiv.org/abs/1704.01212>`__
+# (also in :doc:`message passing tutorial <3_message_passing>`), the
+# message passing formulation is as follows:
+# 
+# .. math::
+# 
+# 
+#    m_{u\to v}^{(l)} = M^{(l)}\left(h_v^{(l-1)}, h_u^{(l-1)}, e_{u\to v}^{(l-1)}\right)
+# 
+# .. math::
+# 
+# 
+#    m_{v}^{(l)} = \sum_{u\in\mathcal{N}(v)}m_{u\to v}^{(l)}
+# 
+# .. math::
+# 
+# 
+#    h_v^{(l)} = U^{(l)}\left(h_v^{(l-1)}, m_v^{(l)}\right)
+# 
+# where DGL calls :math:`M^{(l)}` the *message function*, :math:`\sum` the
+# *reduce function* and :math:`U^{(l)}` the *update function*. Note that
+# :math:`\sum` here can represent any function and is not necessarily a
+# summation.
+# 
+# Essentially, the :math:`l`-th layer representation of a single node
+# depends on the :math:`(l-1)`-th layer representation of the same node,
+# as well as the :math:`(l-1)`-th layer representation of the neighboring
+# nodes. Those :math:`(l-1)`-th layer representations then depend on the
+# :math:`(l-2)`-th layer representation of those nodes, as well as their
+# neighbors.
+# 
+# The following animation shows how a 2-layer GNN is supposed to compute
+# the output of node 5:
+# 
+# |image1|
+# 
+# You can see that to compute node 5 from the second layer, you will need
+# its direct neighbors’ first layer representations (colored in yellow),
+# which in turn needs their direct neighbors’ (i.e. node 5’s second-hop
+# neighbors’) representations (colored in green).
+# 
+# .. |image1| image:: https://data.dgl.ai/tutorial/img/sampling.gif
+# 
 
 
 ######################################################################
