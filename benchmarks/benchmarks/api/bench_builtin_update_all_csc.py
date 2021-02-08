e6fe58ef17502d73282c0e07fc22c61f70376337
VoVAllen <jz1749@nyu.edu>
2021-02-08 06:59:02 +0000

    lint

--- benchmarks/benchmarks/api/bench_builtin_update_all_csc.py
+++ benchmarks/benchmarks/api/bench_builtin_update_all_csc.py
@@ -12,7 +12,7 @@ from .. import utils
 @utils.parametrize('format', ['csc'])
 @utils.parametrize('feat_size', [4, 32, 256])
 @utils.parametrize('msg_type', ['copy_u', 'u_mul_e'])
-@utils.parametrize('reduce_type', ['sum', 'mean','max'])
+@utils.parametrize('reduce_type', ['sum', 'mean', 'max'])
 def track_time(graph_name, format, feat_size, msg_type, reduce_type):
     device = utils.get_bench_device()
     graph = utils.get_graph(graph_name, format)
@@ -21,10 +21,10 @@ def track_time(graph_name, format, feat_size, msg_type, reduce_type):
         (graph.num_nodes(), feat_size), device=device)
     graph.edata['e'] = torch.randn(
         (graph.num_edges(), 1), device=device)
-    
+
     msg_builtin_dict = {
         'copy_u': fn.copy_u('h', 'x'),
-        'u_mul_e': fn.u_mul_e('h', 'e','x'),
+        'u_mul_e': fn.u_mul_e('h', 'e', 'x'),
     }
 
     reduce_builtin_dict = {
@@ -34,13 +34,14 @@ def track_time(graph_name, format, feat_size, msg_type, reduce_type):
     }
 
     # dry run
-    graph.update_all(msg_builtin_dict[msg_type], reduce_builtin_dict[reduce_type])
+    graph.update_all(msg_builtin_dict[msg_type],
+                     reduce_builtin_dict[reduce_type])
 
     # timing
-   
+
     with utils.Timer() as t:
         for i in range(3):
-            graph.update_all(msg_builtin_dict[msg_type], reduce_builtin_dict[reduce_type])
+            graph.update_all(
+                msg_builtin_dict[msg_type], reduce_builtin_dict[reduce_type])
 
     return t.elapsed_secs / 3
-
