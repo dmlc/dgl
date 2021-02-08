de618705f2d796067f071f7afa8f5aa1637537f5
VoVAllen <jz1749@nyu.edu>
2021-02-08 07:55:34 +0000

    fix

--- benchmarks/benchmarks/api/bench_udf_update_all.py
+++ benchmarks/benchmarks/api/bench_udf_update_all.py
@@ -7,23 +7,21 @@ import dgl.function as fn
 from .. import utils
 
 
-@utils.benchmark('time', timeout=7200)
-@utils.parametrize('graph_name', ['pubmed', "ogbn-arxiv"])
-@utils.parametrize('format', ['csr'])  # only coo supports udf
-@utils.parametrize('feat_size', [8, 32, 128, 512])
+@utils.benchmark('time', timeout=600)
+@utils.parametrize('graph_name', ['pubmed', 'ogbn-arxiv'])
+@utils.parametrize('format', ['coo'])  # only coo supports udf
+@utils.parametrize('feat_size', [8, 64, 512])
 @utils.parametrize('msg_type', ['copy_u', 'u_mul_e'])
 @utils.parametrize('reduce_type', ['sum', 'mean', 'max'])
 def track_time(graph_name, format, feat_size, msg_type, reduce_type):
     device = utils.get_bench_device()
     graph = utils.get_graph(graph_name, format)
-    graph = graph.formats(['coo', 'csr', 'csc'])
     graph = graph.to(device)
-    graph = graph.formats(['coo', 'csr', 'csc'])
     graph.ndata['h'] = torch.randn(
         (graph.num_nodes(), feat_size), device=device)
     graph.edata['e'] = torch.randn(
         (graph.num_edges(), 1), device=device)
-    
+
     msg_udf_dict = {
         'copy_u': lambda edges: {'x': edges.src['h']},
         'u_mul_e': lambda edges: {'x': edges.src['h']*edges.data['e']},
@@ -41,7 +39,7 @@ def track_time(graph_name, format, feat_size, msg_type, reduce_type):
     # timing
     with utils.Timer() as t:
         for i in range(3):
-            graph.update_all(msg_udf_dict[msg_type], reduct_udf_dict[reduce_type])
+            graph.update_all(msg_udf_dict[msg_type],
+                             reduct_udf_dict[reduce_type])
 
     return t.elapsed_secs / 3
-
