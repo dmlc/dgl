8a07ab77376a99b7114d0850ff99331ed88a648e
Minjie Wang <wmjlyjemaine@gmail.com>
2021-02-20 17:42:45 +0800

    [Doc] Tutorials re-organization (#2683)
    * reorg
    
    * change titles
    
    * rm some stale API doc; minor fix
    
    * fix docs
    
    * add warning
    
    * rm new-tutorial run in ci
    
    * lint

--- tutorials/models/1_gnn/9_gat.py
+++ tutorials/models/1_gnn/9_gat.py
@@ -1,13 +1,20 @@
 """
 .. _model-gat:
 
-Graph attention network
-==================================
+Understand Graph Attention Network
+=======================================
 
 **Authors:** `Hao Zhang <https://github.com/sufeidechabei/>`_, `Mufei Li
 <https://github.com/mufeili>`_, `Minjie Wang
 <https://jermainewang.github.io/>`_  `Zheng Zhang
 <https://shanghai.nyu.edu/academics/faculty/directory/zheng-zhang>`_
+
+.. warning::
+
+    The tutorial aims at gaining insights into the paper, with code as a mean
+    of explanation. The implementation thus is NOT optimized for running
+    efficiency. For recommended implementation, please refer to the `official
+    examples <https://github.com/dmlc/dgl/tree/master/examples>`_.
 
 In this tutorial, you learn about a graph attention network (GAT) and how it can be 
 implemented in PyTorch. You can also learn to visualize and understand what the attention 
