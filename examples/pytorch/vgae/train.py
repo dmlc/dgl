397ed4d7db28b88a24b989a5a4ba27c220454085
JuliaSun623 <cnsdytsy@gmail.com>
2021-03-08 08:45:52 +0800

    fix bug

--- examples/pytorch/vgae/train.py
+++ examples/pytorch/vgae/train.py
@@ -23,7 +23,7 @@ parser.add_argument('--hidden1', '-h1', type=int, default=32, help='Number of un
 parser.add_argument('--hidden2', '-h2', type=int, default=16, help='Number of units in hidden layer 2.')
 parser.add_argument('--datasrc', '-s', type=str, default='dgl',
                     help='Dataset download from dgl Dataset or website.')
-parser.add_argument('--dataset', '-d', type=str, default='pubmed', help='Dataset string.')
+parser.add_argument('--dataset', '-d', type=str, default='cora', help='Dataset string.')
 # parser.add_argument('--gpu_id', type=int, default=0, help='GPU id to use.')
 args = parser.parse_args()
 
@@ -81,14 +81,6 @@ def dgl_main():
     graph = dataset[0]
 
     # check device
-    # device = torch.device("cuda:{}".format(args.gpu_id) if torch.cuda.is_available() else "cpu")
-
-    # Extract node features
-    feats = graph.ndata.pop('feat')
-    in_dim = feats.shape[-1]
-
-    # generate input
-    adj_orig = graph.adjacency_matrix().to_dense()
     # device = torch.device("cuda:{}".format(args.gpu_id) if torch.cuda.is_available() else "cpu")
 
     # Extract node features
