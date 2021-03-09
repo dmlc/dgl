eb4dba09b4aed4d87eb4f7943fd8aeb93e99916a
Quan Gan <coin2028@hotmail.com>
2021-03-09 13:02:33 +0800

    update version

--- python/update_version.py
+++ python/update_version.py
@@ -11,7 +11,7 @@ import re
 # current version
 # We use the version of the incoming release for code
 # that is under development
-__version__ = "0.6.0"
+__version__ = "0.6.0post1"
 print(__version__)
 
 # Implementations
