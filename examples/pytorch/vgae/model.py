cba5af22eb5ca96975b03556e41f4dac95a3db73
Yu Sun <cnsdytsy@gmail.com>
2021-04-03 20:02:04 +0800

    [bugfix] Fix bugs in vgae (#2727)
    * [Example]Variational Graph Auto-Encoders
    
    * change dgl dataset to single directional graph
    
    * clean code
    
    * refresh
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * add gpu
    
    Co-authored-by: Tianjun Xiao <xiaotj1990327@gmail.com>
    Co-authored-by: Minjie Wang <wmjlyjemaine@gmail.com>

--- examples/pytorch/vgae/model.py
+++ examples/pytorch/vgae/model.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from train import device
+
 
 class VGAEModel(nn.Module):
     def __init__(self, in_dim, hidden1_dim, hidden2_dim):
@@ -20,8 +22,8 @@ class VGAEModel(nn.Module):
         h = self.layers[0](g, features)
         self.mean = self.layers[1](g, h)
         self.log_std = self.layers[2](g, h)
-        gaussian_noise = torch.randn(features.size(0), self.hidden2_dim)
-        sampled_z = self.mean + gaussian_noise * torch.exp(self.log_std)
+        gaussian_noise = torch.randn(features.size(0), self.hidden2_dim).to(device)
+        sampled_z = self.mean + gaussian_noise * torch.exp(self.log_std).to(device)
         return sampled_z
 
     def decoder(self, z):
