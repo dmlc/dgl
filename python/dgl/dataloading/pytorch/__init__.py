79c25a6af4d4687a750b5c265f5edc71070099ac
Justus Schock <12886177+justusschock@users.noreply.github.com>
2021-05-04 09:14:43 +0200

    Make loader items iterator

--- python/dgl/dataloading/pytorch/__init__.py
+++ python/dgl/dataloading/pytorch/__init__.py
@@ -14,6 +14,9 @@ class _ScalarDataBatcherIter:
         self.batch_size = batch_size
         self.index = 0
         self.drop_last = drop_last
+        
+    def __iter__(self):
+        return self
 
     def __next__(self):
         num_items = self.dataset.shape[0]
@@ -213,6 +216,9 @@ class _NodeDataLoaderIter:
         self.device = node_dataloader.device
         self.node_dataloader = node_dataloader
         self.iter_ = iter(node_dataloader.dataloader)
+        
+    def __iter__(self):
+        return self
 
     def __next__(self):
         # input_nodes, output_nodes, blocks
@@ -227,6 +233,9 @@ class _EdgeDataLoaderIter:
         self.device = edge_dataloader.device
         self.edge_dataloader = edge_dataloader
         self.iter_ = iter(edge_dataloader.dataloader)
+        
+    def __iter__(self):
+        return self
 
     def __next__(self):
         result_ = next(self.iter_)
