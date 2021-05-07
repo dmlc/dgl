fbfcf1a8cc16241691dc88a3c769081aa2612199
Armin Stepanjan <ab-10@users.noreply.github.com>
2021-05-07 02:44:11 +0100

    Doc: adapt send_and_recv example (#2822)
    * adapt send_and_recv example
    
    * lint fix
    
    * Change title and example order
    
    * Update heterograph.py
    
    Co-authored-by: Quan (Andy) Gan <coin2028@hotmail.com>

--- python/dgl/heterograph.py
+++ python/dgl/heterograph.py
@@ -4400,6 +4400,29 @@ class DGLHeteroGraph(object):
         tensor([[0.],
                 [0.],
                 [1.]])
+
+        **``send_and_recv`` using user-defined functions**
+
+        >>> import torch as th
+        >>> g = dgl.graph(([0, 1], [1, 2]))
+        >>> g.ndata['x'] = th.tensor([[1.], [2.], [3.]])
+
+        >>> # Define the function for sending node features as messages.
+        >>> def send_source(edges):
+        ...     return {'m': edges.src['x']}
+        >>> # Sum the messages received and use this to replace the original node feature.
+        >>> def simple_reduce(nodes):
+        ...     return {'x': nodes.mailbox['m'].sum(1)}
+
+        Send and receive messages.
+
+        >>> g.send_and_recv(g.edges())
+        >>> g.ndata['x']
+        tensor([[1.],
+                [1.],
+                [2.]])
+
+        Note that the feature of node 0 remains the same as it has no incoming edges.
         """
         if inplace:
             raise DGLError('The `inplace` option is removed in v0.5.')
