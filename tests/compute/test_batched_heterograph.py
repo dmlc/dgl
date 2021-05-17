ad049980a91bd850a9fb5b11944f51ec2c405b1d
VoVAllen <jz1749@nyu.edu>
2021-05-17 11:26:55 +0100

    fix

--- tests/compute/test_batched_heterograph.py
+++ tests/compute/test_batched_heterograph.py
@@ -353,7 +353,7 @@ def test_batch_netypes():
     import networkx as nx
     B = nx.DiGraph()
     B.add_nodes_from([1, 2, 3, 4], bipartite=0,
-                     some_attr=F.tensor([1, 2, 3, 4], dtype=F.float32))
+                     some_attr=F.copy_to(F.tensor([1, 2, 3, 4], dtype=F.float32), F.ctx()))
     B.add_nodes_from(["a", "b", "c"], bipartite=1)
     B.add_edges_from([(1, "a"), (1, "b"), (2, "b"),
                       (2, "c"), (3, "c"), (4, "a")])
