6df821c8a3f0a61d56998e5a0b97b82f7560745a
Dominique LaSalle <dlasalle@nvidia.com>
2021-05-22 17:43:19 -0700

    Add nccl_external as a dependency

--- cmake/modules/NCCL.cmake
+++ cmake/modules/NCCL.cmake
@@ -5,16 +5,18 @@ set(NCCL_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/nccl")
 
 # NCCL doesn't have CMAKE, so build externally
 ExternalProject_Add(nccl_external
-	SOURCE_DIR ${PROJECT_SOURCE_DIR}/third_party/nccl
-	BUILD_IN_SOURCE 1
-	CONFIGURE_COMMAND ""
-	BUILD_COMMAND
-		env
-		make
-		"BUILDDIR=${NCCL_BUILD_DIR}"
-		BUILD_BYPRODUCTS "${NCCL_BUILD_DIR}/lib/libnccl_static.a"
-	INSTALL_COMMAND ""
-	)
+  SOURCE_DIR ${PROJECT_SOURCE_DIR}/third_party/nccl
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND
+    env
+    make
+    "-j"
+    "src.build"
+    "BUILDDIR=${NCCL_BUILD_DIR}"
+    BUILD_BYPRODUCTS "${NCCL_BUILD_DIR}/lib/libnccl_static.a"
+  INSTALL_COMMAND ""
+  )
 
 # set output variables
 set(NCCL_FOUND TRUE)
