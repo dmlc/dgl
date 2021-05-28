2938fcece111a70b509d4cc0472fcaa85b57ac5f
Zheng <dzzhen@3c22fba32af5.ant.amazon.com>
2021-05-28 18:11:03 +0800

    fix.

--- src/random/cpu/choice.cc
+++ src/random/cpu/choice.cc
@@ -104,8 +104,8 @@ void RandomEngine::UniformChoice(IdxType num, IdxType population, IdxType* out,
       // we should reduce the number of random number generations. Even though
       // reservior algorithm is more memory effficient (it has O(num) memory complexity),
       // it generates O(population) random numbers, which is computationally expensive.
-      // This algorithm has memory complexity of O(population) but has much smaller computation
-      // complexity O(num). In the case of `num >= population/10`, we don't need to worry about
+      // This algorithm has memory complexity of O(population) but generates much fewer random
+      // numbers O(num). In the case of `num >= population/10`, we don't need to worry about
       // memory complexity because `num` is usually small. So is `population`. Allocating a small
       // piece of memory is very efficient.
       std::vector<IdxType> seq(population);
