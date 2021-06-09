2eb1d7fb5c7326d08a54ea6bed5d106afd8ce595
Zheng <dzzhen@3c22fba32af5.ant.amazon.com>
2021-06-09 20:46:10 +0800

    fix.

--- python/dgl/distributed/dist_graph.py
+++ python/dgl/distributed/dist_graph.py
@@ -1122,7 +1122,7 @@ def _split_random_within_part(partition_book, rank, part_eles):
     rand_idx = rand_idx[offset[client_id_in_part] : offset[client_id_in_part + 1]]
     return part_eles[np.sort(rand_idx)]
 
-def node_split(nodes, partition_book=None, ntype='_N', rank=None, force_even=True):
+def node_split(nodes, partition_book=None, ntype='_N', rank=None, force_even=True, g=None):
     ''' Split nodes and return a subset for the local rank.
 
     This function splits the input nodes based on the partition book and
