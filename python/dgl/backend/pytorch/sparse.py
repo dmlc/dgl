3021711230791b28edfa74c3123f428258e92b8e
Cheng Wan <chwan@rice.edu>
2021-06-21 14:30:07 -0500

    Revert "Merge commit '5498cf051e9cf450dcb798ca09920bbde008a661'"
    This reverts commit f79be2ad777897c7025b28308454cad81ad6bb27.

--- python/dgl/backend/pytorch/sparse.py
+++ python/dgl/backend/pytorch/sparse.py
@@ -1,8 +1,30 @@
 import torch as th
+from distutils.version import LooseVersion
 from ...base import is_all, ALL
-from ...sparse import _gspmm, _gsddmm
+from ...sparse import _gspmm, _gsddmm, _segment_reduce, _bwd_segment_cmp, _scatter_add
 
-__all__ = ['gspmm', 'gsddmm', 'edge_softmax']
+if LooseVersion(th.__version__) >= LooseVersion("1.6.0"):
+    from torch.cuda.amp import custom_fwd, custom_bwd
+else:
+    import functools
+    """PyTorch natively supports automatic mixed precision in DGL 1.6, we redefine
+    the custom_fwd and custom_bwd function to be compatible with DGL 1.5.
+    """
+    def custom_fwd(**kwargs):
+        def custom_fwd_inner(fwd):
+            @functools.wraps(fwd)
+            def decorate_fwd(*args, **kwargs):
+                return fwd(*args, **kwargs)
+            return decorate_fwd
+        return custom_fwd_inner
+
+    def custom_bwd(bwd):
+        @functools.wraps(bwd)
+        def decorate_bwd(*args, **kwargs):
+            return bwd(*args, **kwargs)
+        return decorate_bwd
+
+__all__ = ['gspmm', 'gsddmm', 'edge_softmax', 'segment_reduce', 'scatter_add']
 
 
 def _reduce_grad(grad, shape):
@@ -10,12 +32,14 @@ def _reduce_grad(grad, shape):
     If there is broadcast in forward pass, gradients need to be reduced on
     broadcast dimension. This function checks the input tensor shape and
     gradient shape and perform the reduction.
+
     Parameters
     ----------
     grad: Tensor
         Gradient tensor
     shape: tuple
         Shape of input tensor
+
     Returns
     -------
     Tensor
@@ -28,7 +52,7 @@ def _reduce_grad(grad, shape):
     num_to_squeeze = len(grad_shape) - len(in_shape)
     # pad inshape
     in_shape = (1,) * num_to_squeeze + in_shape
-    reduce_idx = th.nonzero(th.tensor(grad_shape) - th.tensor(in_shape))
+    reduce_idx = th.nonzero(th.tensor(grad_shape) - th.tensor(in_shape), as_tuple=False)
     reduce_idx += 1  # skip batch dim
     if len(reduce_idx) > 0:
         grad = grad.sum(dim=tuple(reduce_idx), keepdim=True)
@@ -58,16 +82,30 @@ def _expand(x, shape):
 
 class GSpMM(th.autograd.Function):
     @staticmethod
+    @custom_fwd(cast_inputs=th.float16)
     def forward(ctx, gidx, op, reduce_op, X, Y):
         out, (argX, argY) = _gspmm(gidx, op, reduce_op, X, Y)
         ctx.backward_cache = gidx, op, reduce_op
-        ctx.save_for_backward(X, Y, argX, argY)
+        if op == 'copy_lhs' and reduce_op == 'sum':
+            ctx.save_for_backward(th.LongTensor(list(X.shape)))
+        elif op == 'copy_lhs' and reduce_op == 'max':
+            ctx.save_for_backward(th.LongTensor(list(X.shape)), argX)
+        else:
+            ctx.save_for_backward(X, Y, argX, argY)
         return out
 
     @staticmethod
+    @custom_bwd
     def backward(ctx, dZ):
         gidx, op, reduce_op = ctx.backward_cache
-        X, Y, argX, argY = ctx.saved_tensors
+        if op == 'copy_lhs' and reduce_op == 'sum':
+            x_shape, = ctx.saved_tensors
+            x_shape = th.Size(x_shape)
+        elif op == 'copy_lhs' and reduce_op == 'max':
+            x_shape, argX = ctx.saved_tensors
+            x_shape = th.Size(x_shape)
+        else:
+            X, Y, argX, argY = ctx.saved_tensors
         if op != 'copy_rhs' and ctx.needs_input_grad[3]:
             g_rev = gidx.reverse()
             if reduce_op == 'sum':
@@ -78,15 +116,15 @@ class GSpMM(th.autograd.Function):
                 elif op == 'copy_lhs':
                     dX = gspmm(g_rev, 'copy_lhs', 'sum', dZ, None)
             else:  # max/min
-                dX = th.zeros((X.shape[0],) + dZ.shape[1:],
-                              dtype=X.dtype, device=X.device)
+                dX = th.zeros((x_shape[0],) + dZ.shape[1:],
+                              dtype=dZ.dtype, device=dZ.device)
                 if op in ['mul', 'div']:
                     grad = _muldiv(op, _expand(Y, dZ.shape[1:]).gather(
                         0, argY.long())) * dZ
                     dX.scatter_add_(0, argX.long(), grad)
                 elif op in ['add', 'sub', 'copy_lhs']:
                     dX.scatter_add_(0, argX.long(), dZ)
-            dX = _reduce_grad(dX, X.shape)
+            dX = _reduce_grad(dX, x_shape)
         else:  # X has not gradient
             dX = None
         if op != 'copy_lhs' and ctx.needs_input_grad[4]:
@@ -118,6 +156,7 @@ class GSpMM(th.autograd.Function):
 
 class GSDDMM(th.autograd.Function):
     @staticmethod
+    @custom_fwd(cast_inputs=th.float16)
     def forward(ctx, gidx, op, X, Y, lhs_target, rhs_target):
         out = _gsddmm(gidx, op, X, Y, lhs_target, rhs_target)
         ctx.backward_cache = gidx, op, lhs_target, rhs_target
@@ -125,6 +164,7 @@ class GSDDMM(th.autograd.Function):
         return out
 
     @staticmethod
+    @custom_bwd
     def backward(ctx, dZ):
         gidx, op, lhs_target, rhs_target = ctx.backward_cache
         X, Y = ctx.saved_tensors
@@ -177,10 +217,14 @@ class GSDDMM(th.autograd.Function):
 
 class EdgeSoftmax(th.autograd.Function):
     @staticmethod
+    @custom_fwd(cast_inputs=th.float16)
     def forward(ctx, gidx, score, eids, norm_by):
         """Forward function.
+
         Pseudo-code:
+
         .. code:: python
+
             score = dgl.EData(g, score)
             score_max = score.dst_max()  # of type dgl.NData
             score = score - score_max  # edge_sub_dst, ret dgl.EData
@@ -191,7 +235,7 @@ class EdgeSoftmax(th.autograd.Function):
         # remember to save the graph to backward cache before making it
         # a local variable
         if not is_all(eids):
-            gidx = gidx.edge_subgraph(eids.type(gidx.dtype), True)
+            gidx = gidx.edge_subgraph([eids], True).graph
         if norm_by == 'src':
             gidx = gidx.reverse()
         score_max = _gspmm(gidx, 'copy_rhs', 'max', None, score)[0]
@@ -203,10 +247,14 @@ class EdgeSoftmax(th.autograd.Function):
         return out
 
     @staticmethod
+    @custom_bwd
     def backward(ctx, grad_out):
         """Backward function.
+
         Pseudo-code:
+
         .. code:: python
+
             g, out = ctx.backward_cache
             grad_out = dgl.EData(g, grad_out)
             out = dgl.EData(g, out)
@@ -223,6 +271,50 @@ class EdgeSoftmax(th.autograd.Function):
         return None, grad_score, None, None
 
 
+class SegmentReduce(th.autograd.Function):
+    @staticmethod
+    @custom_fwd(cast_inputs=th.float16)
+    def forward(ctx, op, x, offsets):
+        y, arg = _segment_reduce(op, x, offsets)
+        ctx.save_for_backward(arg, offsets)
+        ctx.backward_cache = op
+        return y
+
+    @staticmethod
+    @custom_bwd
+    def backward(ctx, dy):
+        op = ctx.backward_cache
+        arg, offsets = ctx.saved_tensors
+        m = offsets[-1].item()
+        if op == 'sum':
+            offsets = offsets[1:]
+            # To address the issue of trailing zeros, related issue:
+            # https://github.com/dmlc/dgl/pull/2610
+            indices = th.zeros(
+                (m + 1,), device=offsets.device, dtype=offsets.dtype)
+            indices.scatter_add_(0, offsets, th.ones_like(offsets))
+            indices = th.cumsum(indices, -1)[:-1]
+            dx = dy[indices]
+        else:
+            dx = _bwd_segment_cmp(dy, arg, m)
+        return None, dx, None
+
+
+class ScatterAdd(th.autograd.Function):
+    @staticmethod
+    @custom_fwd(cast_inputs=th.float16)
+    def forward(ctx, x, idx, m):
+        y = _scatter_add(x, idx, m)
+        ctx.save_for_backward(idx)
+        return y
+
+    @staticmethod
+    @custom_bwd
+    def backward(ctx, dy):
+        idx = ctx.saved_tensors
+        return dy[idx], None, None
+
+
 def gspmm(gidx, op, reduce_op, lhs_data, rhs_data):
     return GSpMM.apply(gidx, op, reduce_op, lhs_data, rhs_data)
 
@@ -233,3 +325,10 @@ def gsddmm(gidx, op, lhs_data, rhs_data, lhs_target='u', rhs_target='v'):
 
 def edge_softmax(gidx, logits, eids=ALL, norm_by='dst'):
     return EdgeSoftmax.apply(gidx, logits, eids, norm_by)
+
+
+def segment_reduce(op, x, offsets):
+    return SegmentReduce.apply(op, x, offsets)
+
+def scatter_add(x, idx, m):
+    return ScatterAdd.apply(x, idx, m)
