ea2cbeb48273e4805fd07157494637d3d62ffb7e
Tong He <hetong007@gmail.com>
2021-07-01 14:58:41 +0800

    update benchmark script

--- benchmarks/benchmarks/api/bench_knn_graph.py
+++ benchmarks/benchmarks/api/bench_knn_graph.py
@@ -9,14 +9,10 @@ from .. import utils
 @utils.parametrize('k', [8, 64])
 @utils.parametrize('size', [1000, 10000])
 @utils.parametrize('dim', [3, 64, 128])
-@utils.parametrize('algorithm', ['bruteforce-blas', 'bruteforce', 'kd-tree', 'bruteforce-sharemem', 'nn-descent'])
+@utils.parametrize_cpu('algorithm', ['bruteforce-blas', 'bruteforce', 'kd-tree', 'nn-descent'])
+@utils.parametrize_gpu('algorithm', ['bruteforce-blas', 'bruteforce', 'bruteforce-sharemem', 'nn-descent'])
 def track_time(size, dim, k, algorithm):
     device = utils.get_bench_device()
-    # skip unavailable pairs of (algorithms, devices)
-    if device is 'cuda' and algorithm=='kd-tree':
-        return 0
-    if device is 'cpu' and algorithm=='bruteforce-sharemem':
-        return 0
     features = np.random.RandomState(42).randn(size, dim)
     feat = torch.tensor(features, dtype=torch.float, device=device)
     # dry run
