5db72642e4c029a2b408d2dab8f83086bbfa95ff
VoVAllen <jz1749@nyu.edu>
2021-07-04 19:36:20 +0100

    fix stupid me

--- benchmarks/distributed/dist_sample/dist_sample_server.py
+++ benchmarks/distributed/dist_sample/dist_sample_server.py
@@ -4,7 +4,7 @@ from dgl.distributed import partition_graph, load_partition, load_partition_book
 import os
 from dgl.distributed.dist_graph import DistGraphServer
 from ogb.nodeproppred import DglNodePropPredDataset
-
+import argparse
 
 def start_server(server_id, num_servers, num_clients):
     os.environ['DGL_DIST_MODE'] = 'distributed'
@@ -16,3 +16,11 @@ def start_server(server_id, num_servers, num_clients):
                         graph_format='csc')
     print("After construct graph")
     g.start()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--server-id", type=int, help="Machine id")
+parser.add_argument("--num-servers", type=int, help="Number of servers per machine")
+parser.add_argument("--num-clients", type=int, help="Number of clients")
+args = parser.parse_args()
+start_server(args.server_id, args.num_servers, args.num_clients)
