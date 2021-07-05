141945c500b9b9ede5a40899da4f72ffdd509a92
VoVAllen <jz1749@nyu.edu>
2021-07-05 07:46:47 +0100

    fix

--- python/dgl/distributed/rpc_client.py
+++ python/dgl/distributed/rpc_client.py
@@ -14,6 +14,8 @@ if os.name != 'nt':
 
 def local_ip4_addr_list():
     """Return a set of IPv4 address
+
+    You can use logging.getLogger("dgl-distributed-socket").setLevel(logging.WARNING+1) to disable the warning here
     """
     assert os.name != 'nt', 'Do not support Windows rpc yet.'
     nic = set()
