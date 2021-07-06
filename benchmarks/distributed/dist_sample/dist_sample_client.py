38d2cb1cfee98eeed1529e279e8c37882721d6ac
VoVAllen <jz1749@nyu.edu>
2021-07-06 07:29:29 +0100

    fiz

--- benchmarks/distributed/dist_sample/dist_sample_client.py
+++ benchmarks/distributed/dist_sample/dist_sample_client.py
@@ -6,27 +6,40 @@ from dgl.distributed import DistGraph
 from ogb.nodeproppred import DglNodePropPredDataset
 from dgl.distributed import sample_neighbors
 import argparse
+import numpy as np
+import time
+
 
 def start_client(machine_id):
     os.environ['DGL_DIST_MODE'] = 'distributed'
-    datadir = Path(os.environ.get("PARTITION_DATA_BASE_PATH", "partition_data"))
-    print(datadir)
-    print(datadir / 'partition_data.json')
-    _, _, _, gpb, _, _, _ = load_partition(datadir / 'partition_data.json', machine_id)
-    print("Finish client loading Partition book")
+    datadir = Path(os.environ.get(
+        "PARTITION_DATA_BASE_PATH", "partition_data"))
+    _, _, _, gpb, _, _, _ = load_partition(
+        datadir / 'partition_data.json', machine_id)
     dgl.distributed.initialize("ip_config.txt")
-    
-    print("Before initialize server")
+
     dist_graph = DistGraph("partition_data", gpb=gpb)
-    print("After initialize server")
-    try:
-        sampled_graph = sample_neighbors(dist_graph, [0, 10, 99, 66, 1024, 2008], 3)
-    except Exception as e:
-        print(e)
-        sampled_graph = None
-    print(sampled_graph)
+    print("Finish initialize client")
+
+    num_nodes = dist_graph.num_nodes()
+    sample_batch_size = 2048
+    fanout = 3
+    num_iterations = 200
+    # dry run
+    for i in range(50):
+        seed_nodes = np.random.randint(0, num_nodes, sample_batch_size)
+        sampled_graph = sample_neighbors(dist_graph, seed_nodes, fanout)
+
+    seed_nodes = np.random.randint(0, num_nodes, (200, sample_batch_size))
+    t0 = time.time()
+    for i in range(num_iterations):
+        sampled_graph = sample_neighbors(dist_graph, seed_nodes[i], fanout)
+    t1 = time.time()
+    per_iter_time = (t1-t0)/num_iterations
+    print(f"Time: {per_iter_time}")
     dgl.distributed.exit_client()
-    return sampled_graph
+    return per_iter_time
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--machine-id", type=int, help="Machine id")
