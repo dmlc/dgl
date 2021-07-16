82a0bff09ae47e866aa518d865cf2654d7da2c3f
Ubuntu <ubuntu@ip-172-31-2-66.ec2.internal>
2021-07-16 09:42:33 +0000

    turn off mxnet for some dist test

--- tests/distributed/test_distributed_sampling.py
+++ tests/distributed/test_distributed_sampling.py
@@ -147,6 +147,7 @@ def check_rpc_find_edges_shuffle(tmpdir, num_server):
 # Wait non shared memory graph store
 @unittest.skipIf(os.name == 'nt', reason='Do not support windows yet')
 @unittest.skipIf(dgl.backend.backend_name == 'tensorflow', reason='Not support tensorflow for now')
+@unittest.skipIf(dgl.backend.backend_name == "mxnet", reason="Turn off Mxnet support")
 @pytest.mark.parametrize("num_server", [1, 2])
 def test_rpc_find_edges_shuffle(num_server):
     import tempfile
