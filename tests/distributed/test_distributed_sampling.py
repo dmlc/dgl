28b99a0683eb67eb79ae6f619d447ac51373e5f4
Ubuntu <ubuntu@ip-172-31-2-66.ec2.internal>
2021-07-16 14:40:24 +0000

    fix

--- tests/distributed/test_distributed_sampling.py
+++ tests/distributed/test_distributed_sampling.py
@@ -198,6 +198,7 @@ def check_rpc_get_degree_shuffle(tmpdir, num_server):
 # Wait non shared memory graph store
 @unittest.skipIf(os.name == 'nt', reason='Do not support windows yet')
 @unittest.skipIf(dgl.backend.backend_name == 'tensorflow', reason='Not support tensorflow for now')
+@unittest.skipIf(dgl.backend.backend_name == "mxnet", reason="Turn off Mxnet support")
 @pytest.mark.parametrize("num_server", [1, 2])
 def test_rpc_get_degree_shuffle(num_server):
     import tempfile
