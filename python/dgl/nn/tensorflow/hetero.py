5ddb31555eef80df969e519d862d98bfabb6fb3c
Niklas Beierl <22919961+NiklasBeierl@users.noreply.github.com>
2021-07-16 11:26:19 +0200

    Doc: HetGConv note for `edge_type_subgraph`
    For the rational behind this note see the discussion in #3003.

--- python/dgl/nn/tensorflow/hetero.py
+++ python/dgl/nn/tensorflow/hetero.py
@@ -88,6 +88,11 @@ class HeteroGraphConv(layers.Layer):
     >>> print(y_dst.keys())
     dict_keys(['user', 'game'])
 
+    .. note::
+    HeteroGraphConv requires that there is a module for every ``'etype'`` in an input graph.
+    If you want to apply HeteroGraphConv to a subset of a graphs ``'etypes'``, you must create a new graph using for
+    example: :func:`~dgl.edge_type_subgraph()`.
+
     Parameters
     ----------
     mods : dict[str, nn.Module]
