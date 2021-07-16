327f9d39da5d576cd61ae6ede0e3b607c49103c7
Ankit Garg <gaank@amazon.com>
2021-07-16 17:40:22 +0530

    move to pandas

--- examples/pytorch/preprocessing_dist_training/substitute_to_hetero.py
+++ examples/pytorch/preprocessing_dist_training/substitute_to_hetero.py
@@ -1,7 +1,7 @@
 import argparse
 import os
-from collections import defaultdict
 import glob
+import pandas as pd
 
 path = os.getcwd()
 parser = argparse.ArgumentParser()
@@ -11,26 +11,14 @@ parser.add_argument("-f", "--file", nargs="+", default=None)
 parser.add_argument("-r", "--removed", help="file where we have edges that were dropped", default="remove.csv")
 args = parser.parse_args()
 
-graph_name = args.name
-
 if args.file is None:
-    edges_file = sorted(glob.glob(os.path.join(args.path, "*-{}_edges.txt".format(graph_name))))
+    edges_file = sorted(glob.glob(os.path.join(args.path, "*-{}_edges.txt".format(args.name))))
 
-
+remove_column_index = [0, 1, 2, 3]
+remove_column_name = ["distributed_src_id", "distributed_dest_id", "src_id", "dest_id"]
+removed_df = pd.read_csv(args.removed, sep=" ", usecols=[0, 1, 2, 3], names=["src_id", "dest_id", "id", "type"])
 
 for edge_file in edges_file:
-    edges_dict = defaultdict(dict)
-    with open(edge_file) as fp:
-        for line in fp:
-            if line:
-                formatted_line = line.strip()
-                resp_array = formatted_line.split(" ")
-                edges_dict[resp_array[2]][resp_array[3]] = "{} {}".format(resp_array[0], resp_array[1])
-
-    with open(args.removed) as fp, open(edge_file,'a') as wr:
-        for line in fp:
-            if line:
-                delete_array = line.split(" ")
-                if edges_dict.get(delete_array[0], {}).__contains__(delete_array[1]):
-                    wr.write("{} {}".format(edges_dict[delete_array[0]][delete_array[1]], line))
-
+    part_df = pd.read_csv(edge_file, sep=" ", usecols=remove_column_index, names=remove_column_name).drop_duplicates(["src_id", "dest_id"])
+    merge_df = pd.merge(part_df, removed_df, how='inner', on=["src_id", "dest_id"])
+    merge_df.to_csv(edge_file, mode='a', header=False, index=False, sep=" ")
