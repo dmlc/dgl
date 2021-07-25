1080da5c52bc005fd70cf61c858798290a15e8a4
davidmin7 <min16@illinois.edu>
2021-07-25 09:51:02 -0500

    reduce number of workers

--- tests/pytorch/test_unified_tensor.py
+++ tests/pytorch/test_unified_tensor.py
@@ -6,17 +6,12 @@ import torch as th
 import dgl
 import backend as F
 
-def start_unified_tensor_worker(dev_id, input, seq_idx, rand_idx, output_seq_cpu, output_rand_cpu):
+def start_unified_tensor_worker(dev_id, input, seq_idx, rand_idx, output_seq, output_rand):
     device = th.device('cuda:'+str(dev_id))
     th.cuda.set_device(device)
     input_unified = dgl.contrib.UnifiedTensor(input, device=device)
-
-    seq_idx = seq_idx.to(device)
-    assert th.all(th.eq(output_seq_cpu, input_unified[seq_idx]))
-
-    rand_idx = rand_idx.to(device)
-    assert th.all(th.eq(output_rand_cpu, input_unified[rand_idx]))
-
+    output_seq.copy_(input_unified[seq_idx.to(device)].cpu())
+    output_rand.copy_(input_unified[rand_idx.to(device)].cpu())
 
 @unittest.skipIf(os.name == 'nt', reason='Do not support windows yet')
 @unittest.skipIf(F.ctx().type == 'cpu', reason='gpu only test')
@@ -43,11 +38,9 @@ def test_unified_tensor():
 
 @unittest.skipIf(os.name == 'nt', reason='Do not support windows yet')
 @unittest.skipIf(F.ctx().type == 'cpu', reason='gpu only test')
-def test_multi_gpu_unified_tensor():
-    if F.ctx().type == 'cuda' and th.cuda.device_count() > 1:
-        pytest.skip("Only one GPU detected, skip multi-gpu test.")
-
-    num_workers = th.cuda.device_count()
+def test_multi_gpu_unified_tensor(num_workers):
+    if F.ctx().type == 'cuda' and th.cuda.device_count() < num_workers:
+        pytest.skip("Not enough number of GPUs to do this test, skip multi-gpu test.")
 
     test_row_size = 65536
     test_col_size = 128
@@ -55,8 +48,8 @@ def test_multi_gpu_unified_tensor():
     rand_test_size = 8192
 
     input = th.rand((test_row_size, test_col_size)).share_memory_()
-    seq_idx = th.arange(0, test_row_size)
-    rand_idx = th.randint(0, test_row_size, (rand_test_size,))
+    seq_idx = th.arange(0, test_row_size).share_memory_()
+    rand_idx = th.randint(0, test_row_size, (rand_test_size,)).share_memory_()
 
     output_seq = []
     output_rand = []
@@ -68,8 +61,10 @@ def test_multi_gpu_unified_tensor():
 
     ctx = mp.get_context('spawn')
     for i in range(num_workers):
+        output_seq.append(th.zeros((test_row_size, test_col_size)).share_memory_())
+        output_rand.append(th.zeros((rand_test_size, test_col_size)).share_memory_())
         p = ctx.Process(target=start_unified_tensor_worker,
-                        args=(i, input, seq_idx, rand_idx, output_seq_cpu, output_rand_cpu,))
+                        args=(i, input, seq_idx, rand_idx, output_seq[i], output_rand[i],))
         p.start()
         worker_list.append(p)
 
@@ -77,8 +72,11 @@ def test_multi_gpu_unified_tensor():
         p.join()
     for p in worker_list:
         assert p.exitcode == 0
+    for i in range(num_workers):
+        assert th.all(th.eq(output_seq_cpu, output_seq[i]))
+        assert th.all(th.eq(output_rand_cpu, output_rand[i]))
 
 
 if __name__ == '__main__':
     test_unified_tensor()
-    test_multi_gpu_unified_tensor()
+    test_multi_gpu_unified_tensor(2)
