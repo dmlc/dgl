ba7e7cf931cbf6943e84e8b11d70657863308769
xiang song(charlie.song) <classicxsong@gmail.com>
2021-07-28 11:51:32 +0800

    [New Feature] Per edge type sampler for to_homogeneous graphs.  (#3131)
    * fix.
    
    * fix.
    
    * fix.
    
    * fix.
    
    * Fix test
    
    * Deprecate old DistEmbedding impl, use synchronized embedding impl
    
    * Basic imple of heterogeneous on homogenenous sampling
    
    * make pass
    
    * Pass C++ test
    
    * Add python test code
    
    * lint
    
    * lint
    
    * Add MultiLayerEtypeNeighborSampler
    
    * Add unitest for single machine dataloader
    
    * Add dist dataloader test for edge type sampler
    
    * Fix lint
    
    * fix
    
    * support for per etype sample
    
    * Fix some bug and enable distributed training with per edge sample
    
    * fix
    
    * Now distributed training works
    
    * turn off some mxnet
    
    * turn off mxnet for some dist test
    
    * fix
    
    * upd
    
    * upd according to the comments
    
    * Fix
    
    * Fix test and now distributed works.
    
    * upd
    
    * upd
    
    * Fix
    
    * Fix bug
    
    * remove dead code.
    
    * upd
    
    * Fix
    
    * upd
    
    * Fix
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-71-112.ec2.internal>
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-2-66.ec2.internal>
    Co-authored-by: Da Zheng <zhengda1936@gmail.com>

--- python/dgl/sampling/neighbor.py
+++ python/dgl/sampling/neighbor.py
@@ -8,9 +8,135 @@ from .. import ndarray as nd
 from .. import utils
 
 __all__ = [
+    'sample_etype_neighbors',
     'sample_neighbors',
     'sample_neighbors_biased',
     'select_topk']
+
+def sample_etype_neighbors(g, nodes, etype_field, fanout, edge_dir='in', prob=None,
+                           replace=False, copy_ndata=True, copy_edata=True, _dist_training=False):
+    """Sample neighboring edges of the given nodes and return the induced subgraph.
+
+    For each node, a number of inbound (or outbound when ``edge_dir == 'out'``) edges
+    will be randomly chosen.  The graph returned will then contain all the nodes in the
+    original graph, but only the sampled edges.
+
+    Node/edge features are not preserved. The original IDs of
+    the sampled edges are stored as the `dgl.EID` feature in the returned graph.
+
+    Parameters
+    ----------
+    g : DGLGraph
+        The graph.  Can only be in CPU. Should only have one node type and one edge type.
+    nodes : tensor or dict
+        Node IDs to sample neighbors from.
+
+        This argument can take a single ID tensor or a dictionary of node types and ID tensors.
+        If a single tensor is given, the graph must only have one type of nodes.
+    etype_field : string
+        The field in g.edata storing the edge type.
+    fanout : int
+        The number of edges to be sampled for each node on each edge type.
+
+        This argument can only take a single int. DGL will sample this number of edges for
+        each node for every edge type.
+
+        If -1 is given for a single edge type, all the neighboring edges with that edge
+        type will be selected.
+    edge_dir : str, optional
+        Determines whether to sample inbound or outbound edges.
+
+        Can take either ``in`` for inbound edges or ``out`` for outbound edges.
+    prob : str, optional
+        Feature name used as the (unnormalized) probabilities associated with each
+        neighboring edge of a node.  The feature must have only one element for each
+        edge.
+
+        The features must be non-negative floats, and the sum of the features of
+        inbound/outbound edges for every node must be positive (though they don't have
+        to sum up to one).  Otherwise, the result will be undefined.
+
+        If :attr:`prob` is not None, GPU sampling is not supported.
+    replace : bool, optional
+        If True, sample with replacement.
+    copy_ndata: bool, optional
+        If True, the node features of the new graph are copied from
+        the original graph. If False, the new graph will not have any
+        node features.
+
+        (Default: True)
+    copy_edata: bool, optional
+        If True, the edge features of the new graph are copied from
+        the original graph.  If False, the new graph will not have any
+        edge features.
+
+        (Default: True)
+    _dist_training : bool, optional
+        Internal argument.  Do not use.
+
+        (Default: False)
+
+    Returns
+    -------
+    DGLGraph
+        A sampled subgraph containing only the sampled neighboring edges, with the
+        same device as the input graph.
+
+    Notes
+    -----
+    If :attr:`copy_ndata` or :attr:`copy_edata` is True, same tensors are used as
+    the node or edge features of the original graph and the new graph.
+    As a result, users should avoid performing in-place operations
+    on the node features of the new graph to avoid feature corruption.
+    """
+    if g.device != F.cpu():
+        raise DGLError("The graph should be in cpu.")
+    if etype_field not in g.edata:
+        raise DGLError("The graph should have {} in the edge data" \
+                       "representing the edge type.".format(etype_field))
+    if isinstance(fanout, int) is False:
+        raise DGLError("The fanout should be an integer")
+    if isinstance(nodes, dict) is True:
+        assert len(nodes) == 1, "The input graph should not have node types"
+        nodes = list(nodes.values())[0]
+    nodes = F.to_dgl_nd(utils.prepare_tensor(g, nodes, 'nodes'))
+    # treat etypes as int32, it is much cheaper than int64
+    # TODO(xiangsx): int8 can be a better choice.
+    etypes = F.to_dgl_nd(F.astype(g.edata[etype_field], ty=F.int32))
+
+    if prob is None:
+        prob_array = nd.array([], ctx=nd.cpu())
+    elif isinstance(prob, nd.NDArray):
+        prob_array = prob
+    else:
+        if prob in g.edata:
+            prob_array = F.to_dgl_nd(g.edata[prob])
+        else:
+            prob_array = F.to_dgl_nd(F.tensor(prob, dtype=F.float32))
+
+    subgidx = _CAPI_DGLSampleNeighborsEType(g._graph, nodes, etypes, fanout,
+                                            edge_dir, prob_array, replace)
+    induced_edges = subgidx.induced_edges
+    ret = DGLHeteroGraph(subgidx.graph, g.ntypes, g.etypes)
+
+    # handle features
+    # (TODO) (BarclayII) DGL distributed fails with bus error, freezes, or other
+    # incomprehensible errors with lazy feature copy.
+    # So in distributed training context, we fall back to old behavior where we
+    # only set the edge IDs.
+    if not _dist_training:
+        if copy_ndata:
+            node_frames = utils.extract_node_subframes(g, None)
+            utils.set_new_frames(ret, node_frames=node_frames)
+
+        if copy_edata:
+            edge_frames = utils.extract_edge_subframes(g, induced_edges)
+            utils.set_new_frames(ret, edge_frames=edge_frames)
+    else:
+        for i, etype in enumerate(ret.canonical_etypes):
+            ret.edges[etype].data[EID] = induced_edges[i]
+
+    return ret
 
 def sample_neighbors(g, nodes, fanout, edge_dir='in', prob=None, replace=False,
                      copy_ndata=True, copy_edata=True, _dist_training=False):
