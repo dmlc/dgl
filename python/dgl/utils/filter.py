fe0bc66c13de91a9e5bf444e52e79c40b126dafc
Dominique LaSalle <dlasalle@nvidia.com>
2021-08-12 10:23:52 -0700

    Fix docs in filter.py

--- python/dgl/utils/filter.py
+++ python/dgl/utils/filter.py
@@ -6,9 +6,9 @@ from .. import backend as F
 
 
 class Filter(object):
-    """Class used to either find either find the subset of ids that are in this
-    filter, or th subset of ids that are not in this filter,
-    given a second set of ids.
+    """Class used to either find the subset of IDs that are in this
+    filter, or the subset of IDs that are not in this filter
+    given a second set of IDs.
 
     Examples
     --------
@@ -21,46 +21,46 @@ class Filter(object):
     tensor([0,2], device='cuda')
     """
     def __init__(self, ids):
-        """Create a new filter from a given set of ids. This currently is only
+        """Create a new filter from a given set of IDs. This currently is only
         implemented for the GPU.
 
         Parameters
         ----------
         ids : IdArray
-            The unique set of ids to keep in the filter.
+            The unique set of IDs to keep in the filter.
         """
         self._filter = _CAPI_DGLFilterCreateFromSet(
             F.zerocopy_to_dgl_ndarray(ids))
 
     def find_included_indices(self, test):
-        """Find the index of the ids in `test` that are in this filter.
+        """Find the index of the IDs in `test` that are in this filter.
 
         Parameters
         ----------
         test : IdArray
-            The set of ids to to test with.
+            The set of IDs to to test with.
 
         Returns
         -------
         IdArray
-            The index of ids in `test` that are also in this filter.
+            The index of IDs in `test` that are also in this filter.
         """
         return F.zerocopy_from_dgl_ndarray( \
             _CAPI_DGLFilterFindIncludedIndices( \
                 self._filter, F.zerocopy_to_dgl_ndarray(test)))
 
     def find_excluded_indices(self, test):
-        """Find the index of the ids in `test` that are not in this filter.
+        """Find the index of the IDs in `test` that are not in this filter.
 
         Parameters
         ----------
         test : IdArray
-            The set of ids to to test with.
+            The set of IDs to to test with.
 
         Returns
         -------
         IdArray
-            The index of ids in `test` that are not in this filter.
+            The index of IDs in `test` that are not in this filter.
         """
         return F.zerocopy_from_dgl_ndarray( \
             _CAPI_DGLFilterFindExcludedIndices( \
