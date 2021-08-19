d9a2d7416727b5f2d0e18dcbd53ede2d7e5ab3dd
nv-dlasalle <63612878+nv-dlasalle@users.noreply.github.com>
2021-08-19 12:24:15 -0700

    [Performance][Feature] Implement edge excluding in EdgeDataLoader on GPU (#3226)
    * Update filter code
    
    * Add unit tests
    
    * Fixes
    
    * Switch to indices
    
    * Rename functions
    
    * Fix linting
    
    * Fix whitespace
    
    * Add doc
    
    * Fix heterograph
    
    * Change workspace allocation
    
    * Fix linting
    
    * Fix docs in filter.py
    
    * Add todo
    
    Co-authored-by: Jinjing Zhou <VoVAllen@users.noreply.github.com>
    Co-authored-by: Quan (Andy) Gan <coin2028@hotmail.com>

--- python/dgl/dataloading/dataloader.py
+++ python/dgl/dataloading/dataloader.py
@@ -46,6 +46,83 @@ def _locate_eids_to_exclude(frontier_parent_eids, exclude_eids):
         result = np.isin(frontier_parent_eids, exclude_eids).nonzero()[0]
         return F.zerocopy_from_numpy(result)
 
+class _EidExcluder():
+    def __init__(self, exclude_eids):
+        device = None
+        if isinstance(exclude_eids, Mapping):
+            for _, v in exclude_eids.items():
+                if device is None:
+                    device = F.context(v)
+                    break
+        else:
+            device = F.context(exclude_eids)
+        self._exclude_eids = None
+        self._filter = None
+
+        if device == F.cpu():
+            # TODO(nv-dlasalle): Once Filter is implemented for the CPU, we
+            # should just use that irregardless of the device.
+            self._exclude_eids = (
+                _tensor_or_dict_to_numpy(exclude_eids) if exclude_eids is not None else None)
+        else:
+            if isinstance(exclude_eids, Mapping):
+                self._filter = {k: utils.Filter(v) for k, v in exclude_eids.items()}
+            else:
+                self._filter = utils.Filter(exclude_eids)
+
+    def _find_indices(self, parent_eids):
+        """ Find the set of edge indices to remove.
+        """
+        if self._exclude_eids is not None:
+            parent_eids_np = _tensor_or_dict_to_numpy(parent_eids)
+            return _locate_eids_to_exclude(parent_eids_np, self._exclude_eids)
+        else:
+            assert self._filter is not None
+            if isinstance(parent_eids, Mapping):
+                located_eids = {k: self._filter[k].find_included_indices(parent_eids[k])
+                                for k, v in parent_eids.items()}
+            else:
+                located_eids = self._filter.find_included_indices(parent_eids)
+            return located_eids
+
+    def __call__(self, frontier):
+        parent_eids = frontier.edata[EID]
+        located_eids = self._find_indices(parent_eids)
+
+        if not isinstance(located_eids, Mapping):
+            # (BarclayII) If frontier already has a EID field and located_eids is empty,
+            # the returned graph will keep EID intact.  Otherwise, EID will change
+            # to the mapping from the new graph to the old frontier.
+            # So we need to test if located_eids is empty, and do the remapping ourselves.
+            if len(located_eids) > 0:
+                frontier = transform.remove_edges(
+                    frontier, located_eids, store_ids=True)
+                frontier.edata[EID] = F.gather_row(parent_eids, frontier.edata[EID])
+        else:
+            # (BarclayII) remove_edges only accepts removing one type of edges,
+            # so I need to keep track of the edge IDs left one by one.
+            new_eids = parent_eids.copy()
+            for k, v in located_eids.items():
+                if len(v) > 0:
+                    frontier = transform.remove_edges(
+                        frontier, v, etype=k, store_ids=True)
+                    new_eids[k] = F.gather_row(parent_eids[k], frontier.edges[k].data[EID])
+            frontier.edata[EID] = new_eids
+
+
+def _create_eid_excluder(exclude_eids, device):
+    if exclude_eids is None:
+        return None
+
+    if device is not None:
+        if isinstance(exclude_eids, Mapping):
+            exclude_eids = {k: F.copy_to(v, device) \
+                for k, v in exclude_eids.items()}
+        else:
+            exclude_eids = F.copy_to(exclude_eids, device)
+
+    return _EidExcluder(exclude_eids)
+
 def _find_exclude_eids_with_reverse_id(g, eids, reverse_eid_map):
     if isinstance(eids, Mapping):
         eids = {g.to_canonical_etype(k): v for k, v in eids.items()}
@@ -230,8 +307,7 @@ class BlockSampler(object):
         :doc:`Minibatch Training Tutorials <tutorials/large/L0_neighbor_sampling_overview>`.
         """
         blocks = []
-        exclude_eids = (
-            _tensor_or_dict_to_numpy(exclude_eids) if exclude_eids is not None else None)
+        eid_excluder = _create_eid_excluder(exclude_eids, self.output_device)
 
         if isinstance(g, DistGraph):
             # TODO:(nv-dlasalle) dist graphs may not have an associated graph,
@@ -250,32 +326,6 @@ class BlockSampler(object):
                 seed_nodes_in = seed_nodes_in.to(graph_device)
             frontier = self.sample_frontier(block_id, g, seed_nodes_in)
 
-            # Removing edges from the frontier for link prediction training falls
-            # into the category of frontier postprocessing
-            if exclude_eids is not None:
-                parent_eids = frontier.edata[EID]
-                parent_eids_np = _tensor_or_dict_to_numpy(parent_eids)
-                located_eids = _locate_eids_to_exclude(parent_eids_np, exclude_eids)
-                if not isinstance(located_eids, Mapping):
-                    # (BarclayII) If frontier already has a EID field and located_eids is empty,
-                    # the returned graph will keep EID intact.  Otherwise, EID will change
-                    # to the mapping from the new graph to the old frontier.
-                    # So we need to test if located_eids is empty, and do the remapping ourselves.
-                    if len(located_eids) > 0:
-                        frontier = transform.remove_edges(
-                            frontier, located_eids, store_ids=True)
-                        frontier.edata[EID] = F.gather_row(parent_eids, frontier.edata[EID])
-                else:
-                    # (BarclayII) remove_edges only accepts removing one type of edges,
-                    # so I need to keep track of the edge IDs left one by one.
-                    new_eids = parent_eids.copy()
-                    for k, v in located_eids.items():
-                        if len(v) > 0:
-                            frontier = transform.remove_edges(
-                                frontier, v, etype=k, store_ids=True)
-                            new_eids[k] = F.gather_row(parent_eids[k], frontier.edges[k].data[EID])
-                    frontier.edata[EID] = new_eids
-
             if self.output_device is not None:
                 frontier = frontier.to(self.output_device)
                 if isinstance(seed_nodes, dict):
@@ -285,6 +335,11 @@ class BlockSampler(object):
                     seed_nodes_out = seed_nodes.to(self.output_device)
             else:
                 seed_nodes_out = seed_nodes
+
+            # Removing edges from the frontier for link prediction training falls
+            # into the category of frontier postprocessing
+            if eid_excluder is not None:
+                eid_excluder(frontier)
 
             block = transform.to_block(frontier, seed_nodes_out)
             if self.return_eids:
