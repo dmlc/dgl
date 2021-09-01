2cef2a255551cacf100337f635d3922c51b109cc
Israt Nisa <nisisrat@amazon.com>
2021-09-01 15:38:31 +0000

    enabled GPU test

--- tests/compute/test_new_update_all_hetero.py
+++ tests/compute/test_new_update_all_hetero.py
@@ -18,7 +18,6 @@ fill_value = {'sum': 0, 'max': float("-inf")}
 feat_size = 2
 
 @unittest.skipIf(dgl.backend.backend_name != 'pytorch', reason='Only support PyTorch for now')
-@unittest.skipIf(F._default_context_str == 'gpu', reason="Need to resolve ctx issue.")
 
 def create_test_heterograph(idtype):
     # test heterograph from the docstring, plus a user -- wishes -- game relation
