6c9cea273ebb13f6b2882eb1cff0e0babdc84a80
Dominique LaSalle <dlasalle@nvidia.com>
2021-09-09 11:00:46 -0700

    Update docs

--- src/graph/transform/to_bipartite.h
+++ src/graph/transform/to_bipartite.h
@@ -1,7 +1,21 @@
 /*!
- *  Copyright (c) 2021 by Contributors
- * \file graph/transform/to_bipartite.h
- * \brief Array operator templates
+ *  Copyright 2021 Contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * \file graph/transform/cuda_to_block.cu
+ * \brief Functions to convert a set of edges into a graph block with local
+ * ids.
  */
 
 #ifndef DGL_GRAPH_TRANSFORM_TO_BIPARTITE_H_
