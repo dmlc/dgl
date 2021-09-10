f77bee328aa08bf21c5b37f151cac213f81a7f69
esang <32364921+sangyx@users.noreply.github.com>
2021-09-10 17:50:06 +0800

    [Bugfix] Fix bugs of `farthest_point_sampler` (#3327)
    * fix start_idx
    
    * fix the bug when cuda > 0
    
    Co-authored-by: Tong He <hetong007@gmail.com>

--- python/dgl/geometry/fps.py
+++ python/dgl/geometry/fps.py
@@ -8,6 +8,7 @@ from .capi import _farthest_point_sampler
 
 __all__ = ['farthest_point_sampler']
 
+
 def farthest_point_sampler(pos, npoints, start_idx=None):
     """Farthest Point Sampler without the need to compute all pairs of distance.
 
@@ -49,12 +50,13 @@ def farthest_point_sampler(pos, npoints, start_idx=None):
     pos = pos.reshape(-1, C)
     dist = F.zeros((B * N), dtype=pos.dtype, ctx=ctx)
     if start_idx is None:
-        start_idx = F.randint(shape=(B, ), dtype=F.int64, ctx=ctx, low=0, high=N-1)
+        start_idx = F.randint(shape=(B, ), dtype=F.int64,
+                              ctx=ctx, low=0, high=N-1)
     else:
         if start_idx >= N or start_idx < 0:
             raise DGLError("Invalid start_idx, expected 0 <= start_idx < {}, got {}".format(
                 N, start_idx))
-        start_idx = F.full_1d((B, ), start_idx, dtype=F.int64, ctx=ctx)
+        start_idx = F.full_1d(B, start_idx, dtype=F.int64, ctx=ctx)
     result = F.zeros((npoints * B), dtype=F.int64, ctx=ctx)
     _farthest_point_sampler(pos, B, npoints, dist, start_idx, result)
     return result.reshape(B, npoints)
