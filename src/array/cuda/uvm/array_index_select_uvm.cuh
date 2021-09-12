632943fb4a544d529bfa387c1da8c972855a80b4
davidmin7 <min16@illinois.edu>
2021-09-11 20:43:09 -0500

    Add a comment for the uvm version

--- src/array/cuda/uvm/array_index_select_uvm.cuh
+++ src/array/cuda/uvm/array_index_select_uvm.cuh
@@ -13,6 +13,10 @@ namespace dgl {
 namespace aten {
 namespace impl {
 
+/*  This is a cross-device access version of IndexSelectMultiKernel.
+*   Since the memory access over PCIe is more sensitive to the
+*   data access aligment (cacheline), we need a separate version here.
+*/
 template <typename DType, typename IdType>
 __global__ void IndexSelectMultiKernelAligned(
         const DType* const array,
