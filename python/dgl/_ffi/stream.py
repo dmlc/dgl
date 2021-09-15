e0c8a58af62e09fa741027bec54d3a032bbdce8e
RhettYing <rhett_ying@qq.com>
2021-09-15 03:35:31 +0000

    refactor stream wrapper

--- python/dgl/_ffi/stream.py
+++ python/dgl/_ffi/stream.py
@@ -6,121 +6,53 @@ import ctypes
 from .base import _LIB, check_call, _FFI_MODE
 from .runtime_ctypes import DGLStreamHandle
 from .ndarray import context
+from ..utils import to_dgl_context
 
 
 IMPORT_EXCEPT = RuntimeError if _FFI_MODE == "cython" else ImportError
 
 
-class StreamBase(object):
-    """ A stream base class which wraps cuda stream and corresponding context. """
+class StreamContext(object):
+    """ Context-manager that selects a given stream.
 
-    def __init__(self, ctx, stream):
-        """ init
+    All CUDA kernels queued within its context will be enqueued
+    on a selected stream.
+
+    """
+
+    def __init__(self, stream):
+        """ create stream context instance
+
         Parameters
         ----------
-        ctx : DGLContext
-            context on which the stream is.
-        stream : DGLStreamHandle
-            a pointer which could be converted into cudaStream_t directly.
+        stream : torch.cuda.Stream
+            target stream will be set.
         """
-        self.ctx = ctx
-        self.stream = stream
+        self.ctx = to_dgl_context(stream.device)
+        self.curr_cuda_stream = stream.cuda_stream
 
-
-class Stream(StreamBase):
-    """ A wrapper of internal stream. It's created/freed via underlying DGL stream calls. """
-
-    def __init__(self, ctx=None):
-        """ create stream
-        Parameters
-        ----------
-        ctx : DGLContext
-            context on which the stream lies.
+    def __enter__(self):
+        """ get previous stream and set target stream as current.
         """
-        if ctx is None:
-            ctx = context('cuda')
-        stream = DGLStreamHandle()
-        check_call(_LIB.DGLStreamCreate(
-            ctx.device_type, ctx.device_id, ctypes.byref(stream)))
-        super().__init__(ctx, stream)
+        self.prev_cuda_stream = DGLStreamHandle()
+        check_call(_LIB.DGLGetStream(
+            self.ctx.device_type, self.ctx.device_id, ctypes.byref(self.prev_cuda_stream)))
+        check_call(_LIB.DGLSetStream(
+            self.ctx.device_type, self.ctx.device_id, ctypes.c_void_p(self.curr_cuda_stream)))
 
-    def __del__(self):
-        """ free created stream
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """ restore previous stream when exiting.
         """
-        ctx = self.ctx
-        check_call(_LIB.DGLStreamFree(
-            ctx.device_type, ctx.device_id, self.stream))
+        check_call(_LIB.DGLSetStream(
+            self.ctx.device_type, self.ctx.device_id, self.prev_cuda_stream))
 
 
-class ExternalStream(StreamBase):
-    """ A wrapper of external stream. As the stream is created externally,
-        it's user's responsibility to keep the referenced stream alive while
-        this class is being used.
-    """
-
-
-def create_stream(ctx=None):
-    """ create a stream via DGL stream calls.
+def stream(stream):
+    """ Wrapper of StreamContext
 
     Parameters
     ----------
-    ctx : DGLContext
-        context on which the stream is.
-
-    Returns
-    -------
-    stream : Stream
-        a stream wrapper.
+    stream : torch.cuda.Stream
+        target stream will be set.
     """
-    return Stream(ctx)
-
-
-def set_stream(stream):
-    """ set a stream as current via DGL stream calls.
-
-    Parameters
-    ----------
-    stream : StreamBase
-        a stream wrapper which includes context and underlying stream.
-
-    Returns
-    -------
-    """
-    ctx = stream.ctx
-    check_call(_LIB.DGLSetStream(
-        ctx.device_type, ctx.device_id, stream.stream))
-
-
-def current_stream(ctx):
-    """ get current stream via DGL stream calls.
-
-    Parameters
-    ----------
-    ctx : DGLContext
-        context on which the stream is.
-
-    Returns
-    -------
-    stream : StreamBase
-        a stream wrapper.
-    """
-    stream = DGLStreamHandle()
-    check_call(_LIB.DGLGetStream(
-        ctx.device_type, ctx.device_id, ctypes.byref(stream)))
-    return StreamBase(ctx, stream)
-
-
-def synchronize_stream(stream):
-    """ synchronize stream via DGL stream calls.
-
-    Parameters
-    ----------
-    stream : StreamBase
-        target stream to be synchronized.
-
-    Returns
-    -------
-    """
-    ctx = stream.ctx
-    check_call(_LIB.DGLSynchronize(
-        ctx.device_type, ctx.device_id, stream.stream))
+    return StreamContext(stream)
