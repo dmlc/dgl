fee511c5350f774bb8bee4268b17aafbff799a46
VoVAllen <jz1749@nyu.edu>
2021-10-27 08:23:30 +0100

    add tests

--- tests/compute/test_edata_fixture.py
+++ tests/compute/test_edata_fixture.py
@@ -3,6 +3,7 @@ from dgl.edata_fixture import use_edata_for_update
 import dgl.function as fn
 import dgl.nn
 from dgl.data import CoraGraphDataset
+import pytest
 
 import backend as F
 
@@ -18,6 +19,30 @@ def test_edata_nnmodule():
     assert F.grad(g.edata['ee']) is not None
 
 
+@pytest.mark.parametrize('reduce_func', [fn.sum('h', 'out'), fn.max('h', 'out')])
+def test_edata_reduce_func(reduce_func):
+    g = CoraGraphDataset()[0]
+    with F.record_grad():
+        g.edata['ee'] = F.attach_grad(F.randn((g.num_edges(),)))
+        with use_edata_for_update("ee"):
+            g.update_all(fn.copy_u('feat', 'h'), reduce_func)
+        F.backward(F.reduce_sum(g.ndata['out']))
+        assert F.grad(g.edata['ee']) is not None
+
+
+@pytest.mark.parametrize('msg_func', [fn.copy_e('e', 'h')])
+@pytest.mark.parametrize('reduce_func', [fn.sum('h', 'out'), fn.max('h', 'out')])
+def test_edata_func_should_not_change(msg_func, reduce_func):
+    g = CoraGraphDataset()[0]
+    with F.record_grad():
+        g.edata['ee'] = F.attach_grad(F.randn((g.num_edges(),)))
+        g.edata['e'] = F.attach_grad(F.randn((g.num_edges(),)))
+        with use_edata_for_update("ee"):
+            g.update_all(msg_func, reduce_func)
+        F.backward(F.reduce_sum(g.ndata['out']))
+        assert F.grad(g.edata['ee']) is None
+
+
 def test_edata_exit_ctx_manager():
     g = CoraGraphDataset()[0]
     with F.record_grad():
@@ -31,7 +56,7 @@ def test_edata_exit_ctx_manager():
 
         # Exit context manager
         g.edata['ee'] = F.attach_grad(F.randn((g.num_edges(),)))
-        out = conv(g, g.ndata["feat"])        
+        out = conv(g, g.ndata["feat"])
         F.backward(F.reduce_sum(out))
         assert F.grad(g.edata['ee']) is None
 
