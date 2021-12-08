277c14f103a5c0570cde47e1e5a0e63bfa7357ef
mufeili <mufeili1996@gmail.com>
2021-12-08 14:49:37 +0800

    Update

--- examples/pytorch/rgcn/README.md
+++ examples/pytorch/rgcn/README.md
@@ -1,6 +1,6 @@
 # Relational-GCN
 
-* Paper: [https://arxiv.org/abs/1703.06103](https://arxiv.org/abs/1703.06103)
+* Paper: [Modeling Relational Data with Graph Convolutional Networks](https://arxiv.org/abs/1703.06103)
 * Author's code for entity classification: [https://github.com/tkipf/relational-gcn](https://github.com/tkipf/relational-gcn)
 * Author's code for link prediction: [https://github.com/MichSchli/RelationPrediction](https://github.com/MichSchli/RelationPrediction)
 
@@ -18,22 +18,22 @@ Example code was tested with rdflib 4.2.2 and pandas 0.23.4
 ### Entity Classification
 AIFB: accuracy 96.29% (3 runs, DGL), 95.83% (paper)
 ```
-python3 entity_classify.py -d aifb --testing --gpu 0
+python entity_classify.py -d aifb --l2norm 0 --gpu 0
 ```
 
 MUTAG: accuracy 70.59% (3 runs, DGL), 73.23% (paper)
 ```
-python3 entity_classify.py -d mutag --l2norm 5e-4 --n-bases 30 --testing --gpu 0
+python entity_classify.py -d mutag --n-bases 30 --gpu 0
 ```
 
 BGS: accuracy 93.10% (3 runs, DGL), 83.10% (paper)
 ```
-python3 entity_classify.py -d bgs --l2norm 5e-4 --n-bases 40 --testing --gpu 0
+python entity_classify.py -d bgs --n-bases 40 --gpu 0
 ```
 
 AM: accuracy 89.22% (3 runs, DGL), 89.29% (paper)
 ```
-python3 entity_classify.py -d am --n-bases=40 --n-hidden=10 --l2norm=5e-4 --testing
+python entity_classify.py -d am --n-bases 40 --n-hidden 10
 ```
 
 ### Entity Classification with minibatch
--- examples/pytorch/rgcn/entity_classify.py
+++ examples/pytorch/rgcn/entity_classify.py
@@ -1,9 +1,5 @@
 """
-Modeling Relational Data with Graph Convolutional Networks
-Paper: https://arxiv.org/abs/1703.06103
-Code: https://github.com/tkipf/relational-gcn
-
-Difference compared to tkipf/relation-gcn
+Differences compared to tkipf/relation-gcn
 * l2norm applied to all weights
 * remove nodes that won't be touched
 """
@@ -117,9 +113,7 @@ def main(args):
                            num_classes,
                            num_rels,
                            num_bases=args.n_bases,
-                           num_hidden_layers=args.n_layers - 2,
-                           dropout=args.dropout,
-                           use_self_loop=args.use_self_loop,
+                           num_hidden_layers=0,
                            use_cuda=use_cuda)
 
     if use_cuda:
@@ -127,14 +121,14 @@ def main(args):
         g = g.to('cuda:%d' % args.gpu)
 
     # optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.l2norm)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=args.l2norm)
 
     # training loop
     print("start training...")
     forward_time = []
     backward_time = []
     model.train()
-    for epoch in range(args.n_epochs):
+    for epoch in range(50):
         optimizer.zero_grad()
         t0 = time.time()
         logits = model(g, feats, edge_type, edge_norm)
@@ -170,31 +164,16 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RGCN')
-    parser.add_argument("--dropout", type=float, default=0,
-            help="dropout probability")
     parser.add_argument("--n-hidden", type=int, default=16,
-            help="number of hidden units")
+                        help="number of hidden units")
     parser.add_argument("--gpu", type=int, default=-1,
-            help="gpu")
-    parser.add_argument("--lr", type=float, default=1e-2,
-            help="learning rate")
+                        help="gpu")
     parser.add_argument("--n-bases", type=int, default=-1,
-            help="number of filter weight matrices, default: -1 [use all]")
-    parser.add_argument("--n-layers", type=int, default=2,
-            help="number of propagation rounds")
-    parser.add_argument("-e", "--n-epochs", type=int, default=50,
-            help="number of training epochs")
+                        help="number of filter weight matrices, default: -1 [use all]")
     parser.add_argument("-d", "--dataset", type=str, required=True,
-            help="dataset to use")
-    parser.add_argument("--l2norm", type=float, default=0,
-            help="l2 norm coef")
-    parser.add_argument("--use-self-loop", default=False, action='store_true',
-            help="include self feature as a special relation")
-    fp = parser.add_mutually_exclusive_group(required=False)
-    fp.add_argument('--validation', dest='validation', action='store_true')
-    fp.add_argument('--testing', dest='validation', action='store_false')
-    parser.set_defaults(validation=True)
+                        help="dataset to use")
+    parser.add_argument("--l2norm", type=float, default=5e-4,
+                        help="l2 norm coef")
 
     args = parser.parse_args()
-    print(args)
     main(args)
