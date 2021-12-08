f4848e79117343f450183575e443be9e8327763a
mufeili <mufeili1996@gmail.com>
2021-12-08 17:06:41 +0800

    Update

--- examples/pytorch/rgcn/entity_classify.py
+++ examples/pytorch/rgcn/entity_classify.py
@@ -5,8 +5,6 @@ Differences compared to tkipf/relation-gcn
 """
 
 import argparse
-import numpy as np
-import time
 import torch
 import torch.nn.functional as F
 import dgl
@@ -36,10 +34,6 @@ def main(args):
     train_idx = torch.nonzero(train_mask, as_tuple=False).squeeze()
     test_idx = torch.nonzero(test_mask, as_tuple=False).squeeze()
     labels = hg.nodes[category].data.pop('labels')
-
-    # As the datasets do not have a validation subset,
-    # use the training set for validation
-    val_idx = train_idx
 
     # Calculate normalization weight for each edge,
     # 1. / d, d is the degree of the destination node
@@ -91,42 +85,27 @@ def main(args):
 
     # training loop
     print("start training...")
-    forward_time = []
-    backward_time = []
     model.train()
     for epoch in range(50):
-        optimizer.zero_grad()
-        t0 = time.time()
         logits = model(g, feats, edge_type, edge_norm)
         logits = logits[target_idx]
         loss = F.cross_entropy(logits[train_idx], labels[train_idx])
-        t1 = time.time()
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        t2 = time.time()
 
-        forward_time.append(t1 - t0)
-        backward_time.append(t2 - t1)
-        print("Epoch {:05d} | Train Forward Time(s) {:.4f} | Backward Time(s) {:.4f}".
-              format(epoch, forward_time[-1], backward_time[-1]))
         train_acc = torch.sum(logits[train_idx].argmax(dim=1) == labels[train_idx]).item() / len(train_idx)
-        val_loss = F.cross_entropy(logits[val_idx], labels[val_idx])
-        val_acc = torch.sum(logits[val_idx].argmax(dim=1) == labels[val_idx]).item() / len(val_idx)
-        print("Train Accuracy: {:.4f} | Train Loss: {:.4f} | Validation Accuracy: {:.4f} | Validation loss: {:.4f}".
-              format(train_acc, loss.item(), val_acc, val_loss.item()))
+        print("Epoch {:05d} | Train Accuracy: {:.4f} | Train Loss: {:.4f}".format(
+            epoch, train_acc, loss.item()))
     print()
 
     model.eval()
-    logits = model(g, feats, edge_type, edge_norm)
+    with torch.no_grad():
+        logits = model(g, feats, edge_type, edge_norm)
     logits = logits[target_idx]
     test_loss = F.cross_entropy(logits[test_idx], labels[test_idx])
     test_acc = torch.sum(logits[test_idx].argmax(dim=1) == labels[test_idx]).item() / len(test_idx)
     print("Test Accuracy: {:.4f} | Test loss: {:.4f}".format(test_acc, test_loss.item()))
-    print()
-
-    print("Mean forward time: {:4f}".format(np.mean(forward_time[len(forward_time) // 4:])))
-    print("Mean backward time: {:4f}".format(np.mean(backward_time[len(backward_time) // 4:])))
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RGCN')
