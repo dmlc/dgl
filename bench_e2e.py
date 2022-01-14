13aa9ccfdd70c3e9502fddd65f4a337b5efec66a
Israt Nisa <nisisrat@amazon.com>
2022-01-14 17:09:37 +0000

    benchmark code from Minjie

---
+++ bench_e2e.py
@@ -0,0 +1,260 @@
+from timeit import default_timer
+import functools
+import torch
+import torch.nn as nn
+import dgl
+import dgl.backend as F
+import dgl.function as fn
+from dgl.data.rdf import AIFBDataset, MUTAGDataset, BGSDataset, AMDataset
+import torch.nn as nn
+import time
+import numpy as np
+
+#####################################
+# Timer code from benchmarks folder
+#####################################
+class Timer:
+    def __init__(self, device):
+        self.timer = default_timer
+        self.device = device
+
+    def __enter__(self):
+        if self.device == 'cuda:0':
+            self.start_event = torch.cuda.Event(enable_timing=True)
+            self.end_event = torch.cuda.Event(enable_timing=True)
+            self.start_event.record()
+        else:
+            self.tic = self.timer()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self.device == 'cuda:0':
+            self.end_event.record()
+            torch.cuda.synchronize()  # Wait for the events to be recorded!
+            self.elapsed_secs = self.start_event.elapsed_time(
+                self.end_event) / 1e3
+        else:
+            self.elapsed_secs = self.timer() - self.tic
+
+
+class RGCNHighMem(nn.Module):
+    def __init__(self, weight):
+        # weight : (|R|, D_in, D_out)
+        super().__init__()
+        self.weight = nn.Parameter(weight)
+
+    def forward(self, g, feat, etypes):
+        # g : DGLGraph
+        # feat : (|V|, D)
+        # etypes : (|E|,)
+        g.ndata['h'] = feat
+        g.update_all(functools.partial(self.message, etypes=etypes), fn.sum('m', 'h'))
+        return g.ndata['h']
+
+    def message(self, edges, etypes):
+        return {'m' : torch.bmm(edges.src['h'].unsqueeze(1), self.weight[etypes]).squeeze(1)}
+
+class RGCNLowMem(nn.Module):
+    def __init__(self, weight):
+        # weight : (|R|, D_in, D_out)
+        super().__init__()
+        self.weight = nn.Parameter(weight)
+
+    def forward(self, g, feat, etypes):
+        # g : DGLGraph
+        # feat : (|V|, D)
+        # etypes : (|E|,)
+
+        # sort etypes
+        sorted_etypes, index = torch.sort(etypes)
+        g = dgl.edge_subgraph(g, index, relabel_nodes=False)
+        # Create a new etypes to be an integer list of number of edges.
+        num_rels = self.weight.shape[0]
+        pos = torch.searchsorted(sorted_etypes, torch.arange(num_rels, device=g.device))
+        num = torch.tensor([len(etypes)], device=g.device)
+        etypes = (torch.cat([pos[1:], num]) - pos).tolist()
+
+        # message passing
+        g.ndata['h'] = feat
+        g.update_all(functools.partial(self.message, etypes=etypes), fn.sum('m', 'h'))
+        return g.ndata['h']
+
+    def message(self, edges, etypes):
+        h_t = torch.split(edges.src['h'], etypes)
+        msg = []
+        for r in range(self.weight.shape[0]):
+            msg.append(torch.matmul(h_t[r], self.weight[r]))
+        return {'m' : torch.cat(msg)}
+
+
+class RGCNGatherMMSorted(nn.Module):
+    def __init__(self, weight):
+        # weight : (|R|, D_in, D_out)
+        super().__init__()
+        self.weight = nn.Parameter(weight)
+
+    def forward(self, g, feat, etypes, E_per_rel):
+        # g : DGLGraph
+        # feat : (|V|, D)
+        # etypes : (|E|,)
+
+        # sort etypes
+        etypes, index = torch.sort(etypes)
+        g = dgl.edge_subgraph(g, index, relabel_nodes=False)
+
+        # message passing
+        g.ndata['h'] = feat
+        g.update_all(functools.partial(self.message, etypes=etypes, E_per_rel=E_per_rel),
+                     fn.sum('m', 'h'))
+        return g.ndata['h']
+
+    def message(self, edges, etypes, E_per_rel):
+        h = edges.src['h']
+        w = self.weight.view(-1, self.weight.shape[2])
+        out = torch.zeros((h.shape[0], self.weight.shape[2]), dtype=torch.float32, device=h.device)
+        dgl.sparse._gather_mm(h, w, out, E_per_rel, etypes, sortedE=True)
+        return {'m' : out}
+
+class RGCNGatherMMUnsorted(nn.Module):
+    def __init__(self, weight):
+        # weight : (|R|, D_in, D_out)
+        super().__init__()
+        self.weight = nn.Parameter(weight)
+
+    def forward(self, g, feat, etypes, E_per_rel):
+        # g : DGLGraph
+        # feat : (|V|, D)
+        # etypes : (|E|,)
+        g.ndata['h'] = feat
+        g.update_all(functools.partial(self.message, etypes=etypes, E_per_rel=E_per_rel),
+                     fn.sum('m', 'h'))
+        return g.ndata['h']
+
+    def message(self, edges, etypes, E_per_rel):
+        h = edges.src['h']
+        w = self.weight.view(-1, self.weight.shape[2])
+        out = torch.zeros((h.shape[0], self.weight.shape[2]), dtype=torch.float32, device=h.device)
+        dgl.sparse._gather_mm(h, w, out, E_per_rel, etypes, sortedE=False)
+        return {'m' : out}
+
+class RGCNHetero(nn.Module):
+    def __init__(self, weight, etypes):
+        # weight : (|R|, D_in, D_out)
+        super().__init__()
+        self.weight = nn.ParameterDict({
+            etype : nn.Parameter(weight[i]) for i, etype in enumerate(etypes)})
+
+    def forward(self, hg, feat_dict):
+        # hg : DGLGraph hetero
+        # feat : dict of tensors
+        for ntype in hg.ntypes:
+            hg.nodes[ntype].data['h'] = feat_dict[ntype]
+        fns = {}
+        for rel in hg.canonical_etypes:
+            fns[rel] = (
+                functools.partial(self.message, weight=self.weight[rel[1]]),
+                fn.sum('m', 'h')
+            )
+        hg.multi_update_all(fns, 'sum')
+        return {ntype : hg.nodes[ntype].data['h'] for ntype in hg.ntypes}
+
+    def message(self, edges, weight):
+        return {'m' : edges.src['h'] @ weight}
+
+
+iters = 20
+feat_scale = 1
+
+in_feat = 16 * feat_scale
+out_feat = 16 * feat_scale
+dev = "cuda:0"
+torch.cuda.set_device(dev)
+
+name, dataset = 'aifb', AIFBDataset()
+#name, dataset = 'mutag', MUTAGDataset()
+#name, dataset = 'bgs', BGSDataset()
+#name, dataset = 'am', AMDataset()
+
+g = dgl.to_homogeneous(dataset[0]).to(dev)
+etypes = g.edata[dgl.ETYPE].long().to(dev)
+num_rels = len(dataset[0].etypes)
+E_per_rel = torch.histogram(etypes.float().cpu(), bins=num_rels).hist.long()
+
+print(f"""Dataset: {name}
+num_nodes: {g.num_nodes()}
+num_edges: {g.num_edges()}
+num_rels: {num_rels}
+in_feat: {in_feat}
+out_feat: {out_feat}
+E_per_rel: {E_per_rel}
+""")
+
+feat = torch.randn(g.num_nodes(), in_feat).to(dev)
+weight = torch.randn(num_rels, in_feat, out_feat).to(dev)
+
+# **** low-mem ******
+conv = RGCNLowMem(weight).to(dev)
+# dry run
+for i in range(3):
+    h = conv(g, feat, etypes)
+torch.cuda.synchronize()
+# test
+with Timer(dev) as t:
+    for i in range(iters):
+        h_lowmem = conv(g, feat, etypes)
+print("low-mem rgcn:", t.elapsed_secs / iters * 1000, "ms")
+
+# **** high-mem ******
+conv = RGCNHighMem(weight).to(dev)
+# dry run
+for i in range(3):
+    h = conv(g, feat, etypes)
+torch.cuda.synchronize()
+# test
+with Timer(dev) as t:
+    for i in range(iters):
+        h_highmem = conv(g, feat, etypes)
+print("high-mem rgcn:", t.elapsed_secs / iters * 1000, "ms")
+
+# **** hetero ****
+hg = dataset[0].to(dev)
+conv = RGCNHetero(weight, hg.etypes).to(dev)
+feat_dict = {ntype : torch.randn(hg.num_nodes(ntype), in_feat).to(dev) for ntype in hg.ntypes}
+# dry run
+for i in range(3):
+    h_dict = conv(hg, feat_dict)
+torch.cuda.synchronize()
+# test
+with Timer(dev) as t:
+    for i in range(iters):
+        h_dict = conv(hg, feat_dict)
+print("hetero rgcn:", t.elapsed_secs / iters * 1000, "ms")
+
+# **** gather_mm sorted ****
+conv = RGCNGatherMMSorted(weight).to(dev)
+# dry run
+for i in range(3):
+    h = conv(g, feat, etypes, E_per_rel)
+torch.cuda.synchronize()
+# test
+with Timer(dev) as t:
+    for i in range(iters):
+        h_gmm_sorted = conv(g, feat, etypes, E_per_rel)
+print("gather_mm_sorted rgcn:", t.elapsed_secs / iters * 1000, "ms")
+
+# **** gather_mm unsorted ****
+conv = RGCNGatherMMUnsorted(weight).to(dev)
+# dry run
+for i in range(3):
+    h = conv(g, feat, etypes, E_per_rel)
+torch.cuda.synchronize()
+# test
+with Timer(dev) as t:
+    for i in range(iters):
+        h_gmm_unsorted = conv(g, feat, etypes, E_per_rel)
+print("gather_mm_unsorted rgcn:", t.elapsed_secs / iters * 1000, "ms")
+
+# **** correctness ****
+assert torch.allclose(h_lowmem, h_highmem, atol=1e-3, rtol=1e-3)
+assert torch.allclose(h_lowmem, h_gmm_unsorted, atol=1e-3, rtol=1e-3)
+ 
--- bench_gathermm.py
+++ bench_gathermm.py
@@ -1,3 +1,4 @@
+from timeit import default_timer
 import torch as th
 import dgl
 import dgl.backend as F
@@ -6,20 +7,49 @@ import torch.nn as nn
 import time
 import numpy as np
 
-iters = 15
-in_feat = 16
-out_feat = 4
+#####################################
+# Timer code from benchmarks folder
+#####################################
+class Timer:
+    def __init__(self, device):
+        self.timer = default_timer
+        self.device = device
 
+    def __enter__(self):
+        if self.device == 'cuda:0':
+            self.start_event = th.cuda.Event(enable_timing=True)
+            self.end_event = th.cuda.Event(enable_timing=True)
+            self.start_event.record()
+        else:
+            self.tic = self.timer()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self.device == 'cuda:0':
+            self.end_event.record()
+            th.cuda.synchronize()  # Wait for the events to be recorded!
+            self.elapsed_secs = self.start_event.elapsed_time(
+                self.end_event) / 1e3
+        else:
+            self.elapsed_secs = self.timer() - self.tic
+
+iters = 35
+feat_scale = 1
+n_edge_scale = 1
+num_rel_scale = 1
+
+in_feat = 16 * feat_scale
+out_feat = 16 * feat_scale
 th.cuda.set_device("cuda:0")
 
-# E_per_rel = th.tensor([50, 100, 20, 284, 89, 10, 82, 9200, 10, 20, 30, 100, 128, 20, 284, 89, 10, 82, 92, 10, 20, 30,
-#                   100, 1280, 20, 284, 89, 1000, 82, 92, 10, 2000, 30, 100, 128, 20, 284, 89, 10, 82, 92, 10, 20, 30])
-
-
-E_per_rel = th.tensor([500, 1000, 2000, 28400, 8900, 10000, 8020, 9200, 100, 2000, 30000, 10000, 12800, 20000, 284, 8900, 10, 82000, 92000, 10, 20000, 30,
-                  10000, 1280, 2000, 28400, 89000, 10000, 82000, 90200, 100000, 2000, 30000, 10000, 12800, 20000, 28400, 8900, 10000, 82000, 92000, 10000, 2000000, 30000])
+E_per_rel = th.tensor([50, 100, 20, 284, 89, 10, 82, 9200, 10, 20, 30, 100,
+    128, 20, 284, 89, 10, 82, 92, 10, 20, 30, 100, 1280, 20, 284, 89, 1000, 82,
+    92, 10, 2000, 30, 100, 128, 20, 284, 89, 10, 82, 92, 10, 20, 30])
+E_per_rel *= n_edge_scale
+#E_per_rel = E_per_rel[:11].repeat(num_rel_scale)
 
 num_rel = len(E_per_rel)
+print('num_rel', num_rel)
 
 H_arr = []
 W_arr = []
@@ -32,21 +62,28 @@ for eid in range(num_rel):
 
 H = th.cat([h for h in H_arr], 0)
 W = th.cat([w for w in W_arr], 0)
+W_3D = W.reshape(num_rel, in_feat, out_feat)
 Out = th.cat([out for out in Out_arr], 0)
+
+print('H.shape', H.shape)
+print('W.shape', W.shape)
+print('W_3D.shape', W_3D.shape)
+print('Out.shape', Out.shape)
 
 # **** low-mem ******
 
 th.cuda.synchronize()
 low_mem_time = []
 for it in range(iters):
-        tic = time.time()
+    with Timer('cuda:0') as t:
+        out = []
         for i in range(len(E_per_rel)):
-                Hi = H_arr[i]
-                Wi = W_arr[i]
-                Outi = th.matmul(Hi, Wi)
-        if it > 5:
-               low_mem_time.append(time.time() - tic)
-        th.cuda.synchronize()
+            Hi = H_arr[i]
+            Wi = W_arr[i]
+            out.append(th.matmul(Hi, Wi))
+        out_low_mem = th.cat(out, 0)
+    if it > 5:
+        low_mem_time.append(t.elapsed_secs)
 print("low-mem matmul:", np.average(low_mem_time)*1000, "ms")
 
 # **** high-mem ******
@@ -54,19 +91,16 @@ print("low-mem matmul:", np.average(low_mem_time)*1000, "ms")
 th.cuda.synchronize()
 etype_arr = []
 for eid in range(num_rel):
-        etype_arr.append(th.full((E_per_rel[eid],), eid).to("cuda:0"))
+    etype_arr.append(th.full((E_per_rel[eid],), eid).to("cuda:0"))
 etypes = th.cat([etype for etype in etype_arr], 0)
-W_3D = W.reshape(num_rel, in_feat, out_feat)
 
 high_mem_time = []
 for i in range(iters):
-
+    with Timer('cuda:0') as t:
         W_high_mem = W_3D.index_select(0, etypes)
-        tic = time.time()
         out_high_mem = th.bmm(H.unsqueeze(1), W_high_mem).squeeze(1)
-        if i > 5:
-               high_mem_time.append(time.time() - tic)
-        th.cuda.synchronize()
+    if i > 5:
+        high_mem_time.append(t.elapsed_secs)
 print("high-mem matmul:", np.average(high_mem_time)*1000, "ms")
 
 # g.gather_mm(edge_udf, weight)
@@ -74,22 +108,26 @@ print("high-mem matmul:", np.average(high_mem_time)*1000, "ms")
 th.cuda.synchronize()
 _gather_mm_time = []
 for i in range(iters):
-        tic = time.time()
-        dgl.sparse._gather_mm(H, W, Out, E_per_rel, etypes, sortedE=True)
-        if i > 5:
-               _gather_mm_time.append(time.time() - tic)
-        th.cuda.synchronize()
+    with Timer('cuda:0') as t:
+        out_gmm_sorted = th.zeros(Out.shape, dtype=th.float32, device='cuda:0')
+        dgl.sparse._gather_mm(H, W, out_gmm_sorted, E_per_rel, etypes, sortedE=True)
+    if i > 5:
+        _gather_mm_time.append(t.elapsed_secs)
 print("gather_mm Sorted:", np.average(_gather_mm_time)*1000, "ms")
+
 
 # **** Unsorted Gather mm ******
 th.cuda.synchronize()
 _gather_mm_time_un = []
 for i in range(iters):
-        tic = time.time()
-        dgl.sparse._gather_mm(H, W, Out, E_per_rel, etypes, sortedE=False)
-        if i > 5:
-               _gather_mm_time_un.append(time.time() - tic)
-        th.cuda.synchronize()
+    with Timer('cuda:0') as t:
+        out_gmm_unsorted = th.zeros(Out.shape, dtype=th.float32, device='cuda:0')
+        dgl.sparse._gather_mm(H, W, out_gmm_unsorted, E_per_rel, etypes, sortedE=False)
+    if i > 5:
+        _gather_mm_time_un.append(t.elapsed_secs)
 print("gather_mm Unsorted:", np.average(_gather_mm_time_un)*1000, "ms")
 
 
+assert th.allclose(out_low_mem, out_high_mem, atol=1e-3, rtol=1e-3)
+#assert th.allclose(out_low_mem, out_gmm_sorted, atol=1e-3, rtol=1e-3)
+assert th.allclose(out_low_mem, out_gmm_unsorted, atol=1e-3, rtol=1e-3)
