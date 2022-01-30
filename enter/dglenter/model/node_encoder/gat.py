eebf7c96f26b73655bee96ee324da8b5f10151e9
VoVAllen <jz1749@nyu.edu>
2022-01-30 07:14:54 +0000

    fix

--- enter/dglenter/model/node_encoder/gat.py
+++ enter/dglenter/model/node_encoder/gat.py
@@ -1,5 +1,6 @@
 
 
+from typing import List
 import torch
 import torch.nn as nn
 import dgl.function as fn
@@ -12,13 +13,39 @@ class GAT(nn.Module):
                  in_size,
                  out_size,
                  num_layers : int = 2,
-                 num_hidden: int = 8,
-                 heads = [8, 8],
+                 hidden_size: int = 8,
+                 heads: List[int] = [8, 8],
                  activation = "elu",
                  feat_drop : float = 0.6,
                  attn_drop: float = 0.6,
                  negative_slope: float = 0.2,
                  residual: bool = False):
+        
+        """Graph Attention Networks
+
+        Parameters
+        ----------
+        in_size : int 
+            Number of input features.
+        out_size : int
+            Output size.
+        hidden_size : int
+            Hidden size.
+        num_layers : int
+            Number of layers.
+        norm : str
+            GCN normalization type. Can be 'both', 'right', 'left', 'none'.
+        activation : str
+            Activation function.
+        feat_drop : float
+            Dropout rate for features.
+        attn_drop : float
+            Dropout rate for attentions.
+        negative_slope: float
+            Negative slope for leaky relu in GATConv
+        residual : bool
+            If true, the GATConv will use residule connection
+        """
         super(GAT, self).__init__()
         self.in_size = in_size
         self.out_size = out_size
@@ -27,17 +54,17 @@ class GAT(nn.Module):
         self.activation = getattr(torch.nn.functional, activation)
         # input projection (no residual)
         self.gat_layers.append(GATConv(
-            in_size, num_hidden, heads[0],
+            in_size, hidden_size, heads[0],
             feat_drop, attn_drop, negative_slope, False, self.activation))
         # hidden layers
         for l in range(1, self.num_layers):
             # due to multi-head, the in_size = num_hidden * num_heads
             self.gat_layers.append(GATConv(
-                num_hidden * heads[l-1], num_hidden, heads[l],
+                hidden_size * heads[l-1], hidden_size, heads[l],
                 feat_drop, attn_drop, negative_slope, residual, self.activation))
         # output projection
         self.gat_layers.append(GATConv(
-            num_hidden * heads[-2], out_size, heads[-1],
+            hidden_size * heads[-2], out_size, heads[-1],
             feat_drop, attn_drop, negative_slope, residual, None))
 
     def forward(self, graph, node_feat, edge_feat = None):
