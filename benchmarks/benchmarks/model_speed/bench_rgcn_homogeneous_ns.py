e40a860b5a59be97915c8099bb3689c2845975cf
Daniil Sizov <daniil.sizov@intel.com>
2022-02-09 06:56:11 +0000

    PR3355 + CSR conversion workaround

--- benchmarks/benchmarks/model_speed/bench_rgcn_homogeneous_ns.py
+++ benchmarks/benchmarks/model_speed/bench_rgcn_homogeneous_ns.py
@@ -249,7 +249,8 @@ def track_time(data):
         batch_size=batch_size,
         shuffle=True,
         drop_last=False,
-        num_workers=num_workers)
+        num_workers=num_workers,
+        use_cpu_worker_affinity=True)
 
     # node features
     # None for one-hot feature, if not none, it should be the feature tensor.
