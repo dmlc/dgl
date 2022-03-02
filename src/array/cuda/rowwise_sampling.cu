3c062c5c312f92556fb69fe8006f39626a8abda0
ljh <1435709051@qq.com>
2022-03-02 10:45:43 +0800

    Change the curand_init parameter

--- src/array/cuda/rowwise_sampling.cu
+++ src/array/cuda/rowwise_sampling.cu
@@ -132,7 +132,7 @@ __global__ void _CSRRowWiseSampleKernel(
   const int64_t last_row = min(static_cast<int64_t>(blockIdx.x+1)*TILE_SIZE, num_rows);
 
   curandState rng;
- curand_init((rand_seed*gridDim.x+blockIdx.x)*blockDim.y+threadIdx.y, threadIdx.x, 0, &rng);
+  curand_init((rand_seed*gridDim.x+blockIdx.x)*blockDim.y+threadIdx.y, threadIdx.x, 0, &rng);
 
   while (out_row < last_row) {
     const int64_t row = in_rows[out_row];
