5f00b2d954538664d5566d86d2f1d42b74eeaad5
mufeili <mufeili1996@gmail.com>
2022-03-14 21:49:45 +0800

    Update

--- python/dgl/nn/pytorch/conv/grouprevres.py
+++ python/dgl/nn/pytorch/conv/grouprevres.py
@@ -7,69 +7,87 @@ import torch.nn as nn
 
 class InvertibleCheckpoint(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, fn, fn_inverse, inputs, weights):
+    def forward(ctx, fn, fn_inverse, num_inputs, *inputs_and_weights):
         # store in context
         ctx.fn = fn
         ctx.fn_inverse = fn_inverse
-        ctx.weights = weights
-        g, x = inputs
-        ctx.input_requires_grad = x.requires_grad
+        ctx.weights = inputs_and_weights[num_inputs:]
+        inputs = inputs_and_weights[:num_inputs]
+
+        ctx.input_requires_grad = [element.requires_grad for element in inputs]
 
         with torch.no_grad():
-            output = ctx.fn(g, x.detach())
-        # Detach y in-place (computations in between the input and output can now be discarded)
-        output = output.detach_()
+            # Makes a detached copy which shares the storage
+            x = []
+            for element in inputs:
+                if isinstance(element, torch.Tensor):
+                    x.append(element.detach())
+                else:
+                    x.append(element)
+            # Detach y in-place (inbetween computations can now be discarded)
+            outputs = ctx.fn(*x).detach_()
 
-        # clear memory of node features
-        x.storage().resize_(0)
+        # clear memory from inputs
+        # only clear memory of node features
+        inputs[0].storage().resize_(0)
 
         # store these tensor nodes for backward pass
         ctx.inputs = [inputs]
-        ctx.outputs = [output]
+        ctx.outputs = [outputs]
 
-        return output
+        return outputs
 
     @staticmethod
-    def backward(ctx, *grad_outputs):
+    def backward(ctx, *grad_outputs):  # pragma: no cover
         if not torch.autograd._is_checkpoint_valid():
-            raise RuntimeError("InvertibleCheckpoint is not compatible with .grad(), \
-                               please use .backward() if possible")
+            raise RuntimeError("InvertibleCheckpoint is not compatible with .grad(), please use .backward() if possible")
         # retrieve input and output tensor nodes
         if len(ctx.outputs) == 0:
-            raise RuntimeError("Trying to perform backward for more than once.")
+            raise RuntimeError("Trying to perform backward on the InvertibleCheckpoint for more than once.")
         inputs = ctx.inputs.pop()
-        output = ctx.outputs.pop()
-        g, x = inputs
+        outputs = ctx.outputs.pop()
 
         # recompute input
         with torch.no_grad():
-            x_inverted = ctx.fn_inverse(g, output)
-            output.storage().resize_(0)
+            inputs_inverted = ctx.fn_inverse(*((outputs,)+inputs[1:]))
+            # clear memory from outputs
+            outputs.storage().resize_(0)
 
+            x = inputs[0]
             x.storage().resize_(int(np.prod(x.size())))
-            x.set_(x_inverted)
+            x.set_(inputs_inverted)
 
         # compute gradients
         with torch.set_grad_enabled(True):
-            x = x.detach()
-            x.requires_grad = ctx.input_requires_grad
-            tmp_output = ctx.fn(g, x)
+            detached_inputs = []
+            for element in inputs:
+                if isinstance(element, torch.Tensor):
+                    detached_inputs.append(element.detach())
+                else:
+                    detached_inputs.append(element)
+            detached_inputs = tuple(detached_inputs)
+            for det_input, requires_grad in zip(detached_inputs, ctx.input_requires_grad):
+                det_input.requires_grad = requires_grad
+            temp_output = ctx.fn(*detached_inputs)
 
-        filtered_inputs = (x,) if x.requires_grad else ()
-        gradients = torch.autograd.grad(outputs=(tmp_output,),
-                                        inputs=filtered_inputs + ctx.weights,
+        filtered_detached_inputs = tuple(filter(lambda x: x.requires_grad, detached_inputs))
+        gradients = torch.autograd.grad(outputs=(temp_output,),
+                                        inputs=filtered_detached_inputs + ctx.weights,
                                         grad_outputs=grad_outputs)
 
-        # None for g
-        input_gradients = [None]
-        if x.requires_grad:
-            input_gradients.append(gradients[0])
-        else:
-            input_gradients.append(None)
+        input_gradients = []
+        i = 0
+        for rg in ctx.input_requires_grad:
+            if rg:
+                input_gradients.append(gradients[i])
+                i += 1
+            else:
+                input_gradients.append(None)
+
         gradients = tuple(input_gradients) + gradients[-len(ctx.weights):]
 
-        # None for fn, fn_inverse
-        return (None, None) + gradients
+        return (None, None, None) + gradients
+
 
 class GroupRevRes(nn.Module):
     r"""Grouped reversible residual connections for GNNs, as introduced in
@@ -78,7 +96,8 @@ class GroupRevRes(nn.Module):
     Parameters
     ----------
     gnn_module : nn.Module
-        GNN module for message passing.
+        GNN module for message passing. The input and output node representation size need
+        to be the same.
     group : int, optional
         The input node features will be partitioned into the specified number of groups.
 
@@ -91,65 +110,67 @@ class GroupRevRes(nn.Module):
 
     >>> num_nodes = 5
     >>> num_edges = 20
-    >>> in_feats = 32
-    >>> out_feats = 64
+    >>> feats = 32
+    >>> group = 2
     >>> g = dgl.rand_graph(num_nodes, num_edges)
-    >>> h = torch.randn(num_nodes, in_feats)
-    >>> conv = GraphConv(in_feats, out_feats)
-    >>> model = GroupRevRes(conv)
+    >>> h = torch.randn(num_nodes, feats)
+    >>> conv = GraphConv(feats // group, feats // group)
+    >>> model = GroupRevRes(conv, group)
     >>> out = model(g, h)
     """
     def __init__(self, gnn_module, group=2):
-        super().__init__()
+        super(GroupRevRes, self).__init__()
         self.gnn_modules = nn.ModuleList()
-        self.group = group
         for i in range(group):
             if i == 0:
                 self.gnn_modules.append(gnn_module)
             else:
                 self.gnn_modules.append(deepcopy(gnn_module))
+        self.group = group
 
-    def _forward(self, g, x):
-        x_chunks = torch.chunk(x, self.group, dim=-1)
-        y_in = sum(x_chunks[1:])
+    def _forward(self, x, g, *args):
+        xs = torch.chunk(x, self.group, dim=-1)
+        chunked_args = list(map(lambda arg: torch.chunk(arg, self.group, dim=-1), args))
+        args_chunks = list(zip(*chunked_args))
+        y_in = sum(xs[1:])
 
-        y_chunks = []
+        ys = []
         for i in range(self.group):
-            tmp_out = self.gnn_modules[i](g, y_in)
-            y = x_chunks[i] + tmp_out
+            tmp_out = self.gnn_modules[i](g, y_in, *args_chunks[i])
+            y = xs[i] + tmp_out
             y_in = y
-            y_chunks.append(y)
+            ys.append(y)
 
-        out = torch.cat(y_chunks, dim=-1)
+        out = torch.cat(ys, dim=-1)
 
         return out
 
-    def _inverse(self, g, y):
-        y_chunks = torch.chunk(y, self.group, dim=-1)
+    def _inverse(self, y, g, *args):
+        ys = torch.chunk(y, self.group, dim=-1)
+        chunked_args = list(map(lambda arg: torch.chunk(arg, self.group, dim=-1), args))
+        args_chunks = list(zip(*chunked_args))
 
-        x_chunks = []
-        # self.group-1, ..., 0
+        xs = []
         for i in range(self.group-1, -1, -1):
             if i != 0:
-                y_in = y_chunks[i-1]
+                y_in = ys[i-1]
             else:
-                y_in = sum(x_chunks)
+                y_in = sum(xs)
 
-            tmp_out = self.gnn_modules[i](g, y_in)
-            x = y_chunks[i] - tmp_out
-            x_chunks.append(x)
+            tmp_out = self.gnn_modules[i](g, y_in, *args_chunks[i])
+            x = ys[i] - tmp_out
+            xs.append(x)
 
-        x = torch.cat(x_chunks[::-1], dim=-1)
+        x = torch.cat(xs[::-1], dim=-1)
 
         return x
 
-    def forward(self, g, x):
+    def forward(self, g, x, *args):
+        args = (x, g) + args
         y = InvertibleCheckpoint.apply(
             self._forward,
             self._inverse,
-            (g, x),
-            tuple([p for p in self.parameters() if p.requires_grad])
-        )
-        if isinstance(y, tuple) and len(y) == 1:
-            return y[0]
+            len(args),
+            *(args + tuple([p for p in self.parameters() if p.requires_grad])))
+
         return y
