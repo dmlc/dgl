9093662e4d31d87e41dbd9036a9f84a85bf7d2a2
xiny <xiny@nvidia.com>
2022-03-21 15:04:32 +0800

    remove legacy random walk code

--- python/dgl/contrib/sampling/__init__.py
+++ python/dgl/contrib/sampling/__init__.py
@@ -1,4 +1,3 @@
 from .sampler import NeighborSampler, LayerSampler, EdgeSampler
-from .randomwalk import *
 from .dis_sampler import SamplerSender, SamplerReceiver
 from .dis_sampler import SamplerPool
