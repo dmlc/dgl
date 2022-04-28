04c4d0eb2bab145eb463a177e599611031b1e934
mufeili <mufeili1996@gmail.com>
2022-04-28 14:21:43 +0800

    Update

---
+++ tests/go/test_config.py
@@ -0,0 +1,2 @@
+def test_nodepred():
+    pass
--- tests/scripts/task_go_test.sh
+++ tests/scripts/task_go_test.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 . /opt/conda/etc/profile.d/conda.sh
+
+export DGLBACKEND=pytorch
+export DGL_LIBRARY_PATH=${PWD}/build
+export PYTHONPATH=tests:${PWD}/python:$PYTHONPATH
+export DGL_DOWNLOAD_DIR=${PWD}
+
 conda activate pytorch-ci
+
+python3 -m pytest -v --junitxml=pytest_go.xml tests/go || fail "go"
+
+export PYTHONUNBUFFERED=1
+export OMP_NUM_THREADS=1
+export DMLC_LOG_DEBUG=1
