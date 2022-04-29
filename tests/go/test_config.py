7f2368da25fdc6eceb22523e96cfb44e745d2f3d
mufeili <mufeili1996@gmail.com>
2022-04-29 17:56:26 +0800

    Update

--- tests/go/test_config.py
+++ tests/go/test_config.py
@@ -1,2 +1,14 @@
-def test_nodepred():
-    pass
+import os
+import pytest
+
+@pytest.mark.parametrize('data', ['cora', 'citeseer', 'pubmed', 'csv', 'reddit',
+                                  'co-buy-computer', 'ogbn-arxiv', 'ogbn-products'])
+@pytest.mark.parametrize('model', ['gcn', 'gat', 'sage', 'sgc', 'gin'])
+def test_nodepred(data, model):
+    os.system('dgl configure nodepred --data {} --model {}'.format(data, model))
+    assert os.path.exists('nodepred_{}_{}.yaml'.format(data, model))
+
+    custom_config_file = 'custom_{}_{}.yaml'.format(data, model)
+    os.system('dgl configure nodepred --data {} --model {} --cfg {}'.format(data, model,
+                                                                            custom_config_file))
+    assert os.path.exists(custom_config_file)
