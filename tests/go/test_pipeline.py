d2f20258b1e05d88c7ba9934065a7ccc73d714d7
mufeili <mufeili1996@gmail.com>
2022-05-09 18:33:30 +0800

    Update

--- tests/go/test_pipeline.py
+++ tests/go/test_pipeline.py
@@ -60,8 +60,26 @@ def test_linkpred_default_neg_sampler(data, node_model, edge_model):
         data, node_model, edge_model, custom_config_file))
     assert os.path.exists(custom_config_file)
 
+@pytest.mark.parametrize('data', ['csv', 'ogbg-molhiv', 'ogbg-molpcba'])
+@pytest.mark.parametrize('model', ['gin', 'pna'])
+def test_graphpred(data, model):
+    os.system('dgl configure graphpred --data {} --model {}'.format(data, model))
+    assert os.path.exists('graphpred_{}_{}.yaml'.format(data, model))
+
+    custom_config_file = 'custom_{}_{}.yaml'.format(data, model)
+    os.system('dgl configure graphpred --data {} --model {} --cfg {}'.format(data, model,
+                                                                             custom_config_file))
+    assert os.path.exists(custom_config_file)
+
+    custom_script = '_'.join([data, model]) + '.py'
+    os.system('dgl export --cfg {} --output {}'.format(custom_config_file, custom_script))
+    assert os.path.exists(custom_script)
+
 @pytest.mark.parametrize('recipe',
-                         ['linkpred_cora_sage.yaml',
+                         ['graphpred_hiv_gin.yaml',
+                          'graphpred_hiv_pna.yaml',
+                          'graphpred_pcba_gin.yaml',
+                          'linkpred_cora_sage.yaml',
                           'linkpred_citation2_sage.yaml',
                           'linkpred_collab_sage.yaml',
                           'nodepred_citeseer_gat.yaml',
