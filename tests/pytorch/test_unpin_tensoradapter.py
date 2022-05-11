b59a75c1dc6cdc5099d79bedaf6c11eba2919af7
Dominique LaSalle <dlasalle@nvidia.com>
2022-05-11 16:17:13 -0700

    Add unit test

---
+++ tests/pytorch/test_unpin_tensoradapter.py
@@ -0,0 +1,21 @@
+import backend as F
+import dgl
+import gc
+import unittest
+import torch
+
+
+@unittest.skipIf(F.ctx().type == 'cpu', reason='Pinning memory tests require GPU.')
+def test_unpin_tensoradapater():
+    # run a sufficient number of iterations such that the memory pool should be
+    # re-used
+    for i in range(25):
+        t = F.zerocopy_from_dlpack(dgl.ndarray.empty(
+            [100, 10],
+            F.reverse_data_type_dict[F.float32],
+            ctx=dgl.utils.to_dgl_context(torch.device('cpu'))).to_dlpack()).zero_()
+        assert not F.is_pinned(t)
+        dgl.utils.pin_memory_inplace(t)
+        assert F.is_pinned(t)
+
+        del t
