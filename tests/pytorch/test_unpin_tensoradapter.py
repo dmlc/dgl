e2a2f3c40590632ef04fc6a74a98bf653d9cf93d
Dominique LaSalle <dlasalle@nvidia.com>
2022-05-11 17:05:04 -0700

    update unit test

--- tests/pytorch/test_unpin_tensoradapter.py
+++ tests/pytorch/test_unpin_tensoradapter.py
@@ -9,13 +9,12 @@ import torch
 def test_unpin_tensoradapater():
     # run a sufficient number of iterations such that the memory pool should be
     # re-used
-    for i in range(25):
+    for j in range(3):
         t = F.zerocopy_from_dlpack(dgl.ndarray.empty(
-            [100, 10],
+            [10000, 10],
             F.reverse_data_type_dict[F.float32],
             ctx=dgl.utils.to_dgl_context(torch.device('cpu'))).to_dlpack()).zero_()
         assert not F.is_pinned(t)
         dgl.utils.pin_memory_inplace(t)
         assert F.is_pinned(t)
-
         del t
