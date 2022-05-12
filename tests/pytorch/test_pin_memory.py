6961d660b17aa9a2cedad6b55a7960e1dda03f2b
Dominique LaSalle <dlasalle@nvidia.com>
2022-05-12 08:58:27 -0700

    Switch to pytest function

--- tests/pytorch/test_pin_memory.py
+++ tests/pytorch/test_pin_memory.py
@@ -1,23 +1,19 @@
 import backend as F
 import dgl
-import unittest
+import pytest
 import torch
 
-@unittest.skipIf(F._default_context_str == 'cpu', reason="Need gpu for this test")
+@pytest.mark.skipIf(F._default_context_str == 'cpu', reason="Need gpu for this test")
 def test_pin_noncontiguous():
     t = torch.empty([10, 100]).transpose(0, 1)
 
     assert not t.is_contiguous()
     assert not F.is_pinned(t)
 
-    try:
+    with pytest.raises(dgl.DGLError):
         dgl.utils.pin_memory_inplace(t) 
-    except dgl.DGLError as e:
-        # passed
-        return
-    assert False, "Expected an exception"
 
-@unittest.skipIf(F._default_context_str == 'cpu', reason="Need gpu for this test")
+@pytest.mark.skipIf(F._default_context_str == 'cpu', reason="Need gpu for this test")
 def test_pin_view():
     t = torch.empty([100, 10])
     v = t[10:20]
@@ -25,12 +21,8 @@ def test_pin_view():
     assert v.is_contiguous()
     assert not F.is_pinned(t)
 
-    try:
+    with pytest.raises(dgl.DGLError):
         dgl.utils.pin_memory_inplace(v) 
-    except dgl.DGLError as e:
-        # passed
-        return
-    assert False, "Expected an exception"
 
 
 if __name__ == "__main__":
