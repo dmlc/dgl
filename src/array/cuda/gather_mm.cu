0f5ef66a6785e875690334601429191d810ea0f7
ndickson <ndickson@nvidia.com>
2022-05-18 19:08:31 -0400

    * Added specialization of cublasGemm function for `__half` type, to try to address https://github.com/dmlc/dgl/issues/3988

--- src/array/cuda/gather_mm.cu
+++ src/array/cuda/gather_mm.cu
@@ -27,6 +27,16 @@ cublasStatus_t cublasGemm(cublasHandle_t handle, cublasOperation_t transa,
 }
 
 template <>
+cublasStatus_t cublasGemm<__half>(cublasHandle_t handle, cublasOperation_t transa,
+    cublasOperation_t transb, int m, int n, int k,
+    const __half* alpha, const __half* A, int lda,
+    const __half* B, int ldb, const __half* beta,
+    __half* C, int ldc) {
+  return cublasHgemm(handle, transa, transb, m, n, k, alpha, A, lda,
+      B, ldb, beta, C, ldc);
+}
+
+template <>
 cublasStatus_t cublasGemm<float>(cublasHandle_t handle, cublasOperation_t transa,
     cublasOperation_t transb, int m, int n, int k,
     const float* alpha, const float* A, int lda,
