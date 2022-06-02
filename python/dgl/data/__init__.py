89655cfda245911c1c834013d9befc5571565349
RecLusIve-F <53204271+RecLusIve-F@users.noreply.github.com>
2022-06-02 23:03:17 +0800

    [Dataset] Add WikiCS Dataset (#4035)
    * Fix bugs & Update dataset
    
    * Update
    
    * Update wikics.py
    
    * Update wikics.py
    
    * Update test_data.py
    
    * Update wikics.py
    
    * Update wikics.py
    
    * Update wikics.py
    
    * update
    
    * Update module.py
    
    * Update dgl.data.rst
    
    * Update wikics.py
    
    * Update wikics.py
    
    Co-authored-by: Mufei Li <mufeili1996@gmail.com>

--- python/dgl/data/__init__.py
+++ python/dgl/data/__init__.py
@@ -32,6 +32,7 @@ from .fakenews import FakeNewsDataset
 from .csv_dataset import CSVDataset
 from .adapter import *
 from .synthetic import BAShapeDataset, BACommunityDataset, TreeCycleDataset, TreeGridDataset, BA2MotifDataset
+from .wikics import WikiCSDataset
 
 def register_data_args(parser):
     parser.add_argument(
