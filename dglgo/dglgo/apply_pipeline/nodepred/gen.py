559735d43f980dc84670b1ea441ea660e3e00076
mufeili <mufeili1996@gmail.com>
2022-06-05 16:36:34 +0800

    Update

--- dglgo/dglgo/apply_pipeline/nodepred/gen.py
+++ dglgo/dglgo/apply_pipeline/nodepred/gen.py
@@ -10,7 +10,7 @@ class ApplyNodepredPipeline(PipelineBase):
 
     def get_cfg_func(self):
         def config(
-            data: DataFactory.filter("nodepred").get_dataset_enum() = typer.Option(..., help="input data name"),
+            data: DataFactory.filter("nodepred").get_dataset_enum() = typer.Option(None, help="input data name"),
             cfg: Optional[str] = typer.Option(None, help="output configuration file path"),
             cpt: str = typer.Option(..., help="input checkpoint file path")
         ):
