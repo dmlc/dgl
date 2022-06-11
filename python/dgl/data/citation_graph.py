858340b172926a2f7798e2f761312f4abbe41304
hhr114 <1142220273@qq.com>
2022-06-11 15:53:43 +0800

    add argument reorder=False for citation_graph

--- python/dgl/data/citation_graph.py
+++ python/dgl/data/citation_graph.py
@@ -59,7 +59,8 @@ class CitationGraphDataset(DGLBuiltinDataset):
     }
 
     def __init__(self, name, raw_dir=None, force_reload=False,
-                 verbose=True, reverse_edge=True, transform=None):
+                 verbose=True, reverse_edge=True, transform=None, 
+                 reorder=False):
         assert name.lower() in ['cora', 'citeseer', 'pubmed']
 
         # Previously we use the pre-processing in pygcn (https://github.com/tkipf/pygcn)
@@ -69,6 +70,7 @@ class CitationGraphDataset(DGLBuiltinDataset):
 
         url = _get_dgl_url(self._urls[name])
         self._reverse_edge = reverse_edge
+        self._reorder = reorder
 
         super(CitationGraphDataset, self).__init__(name,
                                                    url=url,
@@ -143,8 +145,11 @@ class CitationGraphDataset(DGLBuiltinDataset):
         g.ndata['feat'] = F.tensor(_preprocess_features(features), dtype=F.data_type_dict['float32'])
         self._num_classes = onehot_labels.shape[1]
         self._labels = labels
-        self._g = reorder_graph(
-            g, node_permute_algo='rcmk', edge_permute_algo='dst', store_ids=False)
+        if self._reorder:
+            self._g = reorder_graph(
+                g, node_permute_algo='rcmk', edge_permute_algo='dst', store_ids=False)
+        else:
+            self._g = g
 
         if self.verbose:
             print('Finished data loading and preprocessing.')
@@ -414,11 +419,11 @@ class CoraGraphDataset(CitationGraphDataset):
 
     """
     def __init__(self, raw_dir=None, force_reload=False, verbose=True,
-                 reverse_edge=True, transform=None):
+                 reverse_edge=True, transform=None, reorder=False):
         name = 'cora'
 
         super(CoraGraphDataset, self).__init__(name, raw_dir, force_reload,
-                                               verbose, reverse_edge, transform)
+                                               verbose, reverse_edge, transform, reorder)
 
     def __getitem__(self, idx):
         r"""Gets the graph object
@@ -563,11 +568,11 @@ class CiteseerGraphDataset(CitationGraphDataset):
 
     """
     def __init__(self, raw_dir=None, force_reload=False,
-                 verbose=True, reverse_edge=True, transform=None):
+                 verbose=True, reverse_edge=True, transform=None, reorder=False):
         name = 'citeseer'
 
         super(CiteseerGraphDataset, self).__init__(name, raw_dir, force_reload,
-                                                   verbose, reverse_edge, transform)
+                                                   verbose, reverse_edge, transform, reorder)
 
     def __getitem__(self, idx):
         r"""Gets the graph object
@@ -709,11 +714,11 @@ class PubmedGraphDataset(CitationGraphDataset):
 
     """
     def __init__(self, raw_dir=None, force_reload=False, verbose=True,
-                 reverse_edge=True, transform=None):
+                 reverse_edge=True, transform=None, reorder=False):
         name = 'pubmed'
 
         super(PubmedGraphDataset, self).__init__(name, raw_dir, force_reload,
-                                                 verbose, reverse_edge, transform)
+                                                 verbose, reverse_edge, transform, reorder)
 
     def __getitem__(self, idx):
         r"""Gets the graph object
