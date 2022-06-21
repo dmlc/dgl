8b6781831d4eff36e0a70c7840eae04357858c45
maqy1995 <maqy1995@163.com>
2022-06-21 11:00:13 +0800

    reformat

--- python/dgl/dataloading/dataloader.py
+++ python/dgl/dataloading/dataloader.py
@@ -78,8 +78,7 @@ class _TensorizedDatasetIter(object):
             dgl_warning(
                 'The current output_nodes are out of order even if set shuffle to False in Dataloader, '
                 'the reason is that the current version of torch dose not support stable sort. '
-                'Please update torch to 1.10.0 or higher to fix it.'
-            )
+                'Please update torch to 1.10.0 or higher to fix it.')
             type_ids_sortidx = torch.argsort(type_ids)
         type_ids = type_ids[type_ids_sortidx]
         indices = indices[type_ids_sortidx]
