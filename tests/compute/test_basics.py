910886193ad15a0d21a07b660d9603d24139f6f4
Vibhu Jawa <vibhujawa@gmail.com>
2022-06-23 16:30:00 -0700

    Added to_cugraph and from_cugraph functionality

--- tests/compute/test_basics.py
+++ tests/compute/test_basics.py
@@ -6,7 +6,13 @@ import networkx as nx
 from dgl import DGLGraph
 from collections import defaultdict as ddict
 import unittest
+import pytest
 from test_utils import parametrize_idtype
+
+try:
+    import cugraph
+except:
+    cugraph = None
 
 D = 5
 reduce_msg_shapes = set()
@@ -289,6 +295,68 @@ def _test_nx_conversion():
     assert F.allclose(g.ndata['h'], F.tensor([[1.], [2.], [3.]]))
     assert F.allclose(g.edata['h'], F.tensor([[1., 2.], [1., 2.],
                                               [2., 3.], [2., 3.]]))
+
+def test_to_cugraph_conversion():
+    if cugraph is None:
+        pytest.skip("Skipping to_cugraph test because cugraph is not installed")
+
+    g = dgl.graph((F.tensor([0, 1, 2, 3]), F.tensor([1, 0, 3, 2]))).to('cuda')
+    cugraph_g = g.to_cugraph()
+
+    assert cugraph_g.number_of_nodes()==g.number_of_nodes()
+    assert cugraph_g.number_of_edges()==g.number_of_edges()
+
+    assert cugraph_g.has_edge(0, 1)
+    assert cugraph_g.has_edge(1, 0)
+    assert cugraph_g.has_edge(3, 2)
+
+def test_from_cugraph_conversion():
+    if cugraph is None:
+        pytest.skip("Skipping from_cugraph test because cugraph is not installed")
+
+    # cudf is a dependency of cugraph
+    import cudf
+
+    # directed graph conversion test
+    cugraph_g = cugraph.Graph(directed=True)
+    df = cudf.DataFrame({"source":[0, 1, 2, 3],
+                     "destination":[1, 2, 3, 2]})
+
+    cugraph_g.from_cudf_edgelist(df)
+
+    g = dgl.from_cugraph(cugraph_g)
+
+    assert g.device.type == 'cuda'
+    assert g.number_of_nodes() == cugraph_g.number_of_nodes()
+    assert g.number_of_edges() == cugraph_g.number_of_edges()
+
+    # assert reverse edges are not present
+    assert g.has_edges_between(0, 1)
+    assert not g.has_edges_between(1, 0)
+    assert g.has_edges_between(1, 2)
+    assert not g.has_edges_between(2, 1)
+    assert g.has_edges_between(2, 3)
+
+    # undirected graph conversion test
+    cugraph_g = cugraph.Graph(directed=False)
+    df = cudf.DataFrame({"source":[0, 1, 2, 3],
+                     "destination":[1, 2, 3, 2]})
+
+    cugraph_g.from_cudf_edgelist(df)
+
+    g = dgl.from_cugraph(cugraph_g)
+
+    assert g.device.type == 'cuda'
+    assert g.number_of_nodes() == cugraph_g.number_of_nodes()
+    assert g.number_of_edges() == cugraph_g.number_of_edges()
+
+    # assert reverse edges are present
+    assert g.has_edges_between(0, 1)
+    assert g.has_edges_between(1, 0)
+    assert g.has_edges_between(1, 2)
+    assert g.has_edges_between(2, 1)
+    assert g.has_edges_between(2, 3)
+
 
 @parametrize_idtype
 def test_apply_nodes(idtype):
