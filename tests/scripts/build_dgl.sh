ab457fe5e95bd2a17ff425e5f530559df2694268
RhettYing <rhett_ying@qq.com>
2022-07-05 01:51:57 +0000

    refactor build dgl shell

--- tests/scripts/build_dgl.sh
+++ tests/scripts/build_dgl.sh
@@ -38,20 +38,23 @@ make -j
 popd
 
 pushd python
-for backend in pytorch mxnet tensorflow
-do
 if [[ $1 == "cugraph" ]]; then
-    if [[ ${backend} != "pytorch" ]]; then
-        continue
-    fi
+    rm -rf build *.egg-info dist
+    pip uninstall -y dgl
+    # test install
+    python3 setup.py install
+    # test inplace build (for cython)
+    python3 setup.py build_ext --inplace
 else
+    for backend in pytorch mxnet tensorflow
+    do
     conda activate "${backend}-ci"
+    rm -rf build *.egg-info dist
+    pip uninstall -y dgl
+    # test install
+    python3 setup.py install
+    # test inplace build (for cython)
+    python3 setup.py build_ext --inplace
+    done
 fi
-rm -rf build *.egg-info dist
-pip uninstall -y dgl
-# test install
-python3 setup.py install
-# test inplace build (for cython)
-python3 setup.py build_ext --inplace
-done
 popd
