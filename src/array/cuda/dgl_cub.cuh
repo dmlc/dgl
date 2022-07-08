0d58b42d3b3f91b8a3252f9248323f12fa563b87
xiny <xiny@nvidia.com>
2022-07-08 18:38:33 +0800

    revert the change to cub and thrust

--- src/array/cuda/dgl_cub.cuh
+++ src/array/cuda/dgl_cub.cuh
@@ -7,14 +7,13 @@
 #ifndef DGL_ARRAY_CUDA_DGL_CUB_CUH_
 #define DGL_ARRAY_CUDA_DGL_CUB_CUH_
 
-// This should be defined in CMakeLists.txt
-#ifndef THRUST_CUB_WRAPPED_NAMESPACE
-static_assert(false, "THRUST_CUB_WRAPPED_NAMESPACE must be defined for DGL.");
-#endif
-
+// include cub in a safe manner
+#define CUB_NS_PREFIX namespace dgl {
+#define CUB_NS_POSTFIX }
+#define CUB_NS_QUALIFIER ::dgl::cub
 #include "cub/cub.cuh"
-
-static_assert(CUB_MAJOR_VERSION == 1 && CUB_MINOR_VERSION >= 14,
-    "CUB version is too old. Please update to 1.14 or later.");
+#undef CUB_NS_QUALIFIER
+#undef CUB_NS_POSTFIX
+#undef CUB_NS_PREFIX
 
 #endif
