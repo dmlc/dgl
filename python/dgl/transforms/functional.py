4d1ddbf40808ded6c670dc79315949afc997fa4c
RecLusIve-F <1365004806@qq.com>
2022-07-26 16:24:44 +0800

    Update functional.py

--- python/dgl/transforms/functional.py
+++ python/dgl/transforms/functional.py
@@ -1864,10 +1864,13 @@ def add_self_loop(g, edge_feat_names=None, fill_data=1., etype=None):
     for feat_name in edge_feat_names:
         if isinstance(fill_data, (int, float)):
             dtype = g.edges[etype].data[feat_name].dtype
-            tmp_fill_data = F.copy_to(F.tensor([fill_data], dtype), g.device())
-            data[feat_name] = F.zeros(
-                (g.num_nodes(etype[0]), g.edges[etype].data[feat_name].shape[1]), dtype,
-                g.device) + tmp_fill_data
+            tmp_fill_data = F.copy_to(F.tensor([fill_data], dtype), g.device)
+            try:
+                data[feat_name] = F.zeros(
+                    (g.num_nodes(etype[0]), g.edges[etype].data[feat_name].shape[1]), dtype,
+                    g.device) + tmp_fill_data
+            except IndexError:
+                data[feat_name] = F.zeros((g.num_nodes(etype[0]),), dtype, g.device) + tmp_fill_data
 
         elif isinstance(fill_data, str):
             if fill_data not in reduce_funcs.keys():
