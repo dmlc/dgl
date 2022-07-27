f697a536f57f05d66c4896809953211d7c414695
Wey Gu <weyl.gu@gmail.com>
2022-07-27 18:11:43 +0800

    Fixed device type in inference
    the device here should be typed in string or pin_memory will be false anyway
    
    pin_memory = (buffer_device != device)

--- examples/pytorch/graphsage/node_classification.py
+++ examples/pytorch/graphsage/node_classification.py
@@ -141,5 +141,5 @@ if __name__ == '__main__':
 
     # test the model
     print('Testing...')
-    acc = layerwise_infer(device, g, dataset.test_idx, model, batch_size=4096)
+    acc = layerwise_infer(device.type, g, dataset.test_idx, model, batch_size=4096)
     print("Test Accuracy {:.4f}".format(acc.item()))
