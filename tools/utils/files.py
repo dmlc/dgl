1a2ca10b1c3c1b542a1296e6202125845065ba2e
Quan Gan <coin2028@hotmail.com>
2022-08-10 05:12:11 +0000

    remove

--- tools/utils/files.py
+++ tools/utils/files.py
@@ -15,19 +15,3 @@ def setdir(path):
     finally:
         logging.info('Restoring directory to %s' % cwd)
         os.chdir(cwd)
-
-
-def txt2npy(input_path, output_path):
-    """Convert a one-dimensional array stored in text, one line per number, into
-    a numpy array for fast random access.
-    """
-    import dask.dataframe as dd
-    logging.info('Reading text file %s...' % input_path)
-    dask_arr = dd.read_csv(input_path, names=['v'])
-    logging.info('Counting lines...')
-    dask_arr = dask_arr['v'].to_dask_array(True)    # evaluate shape
-    size = dask_arr.shape[0]
-    dtype = dask_arr.dtype
-    np_arr = open_memmap(output_path, mode='w+', shape=(size,), dtype=dtype)
-    logging.info('Saving content to %s...' % output_path)
-    dask_arr.store(np_arr)
