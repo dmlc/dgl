a64ad1b974ce5a009c1a427b78c5a4710a2a1ba8
Quan Gan <coin2028@hotmail.com>
2022-08-23 09:40:45 +0000

    update with default build options

--- conda/dgl/build.sh
+++ conda/dgl/build.sh
@@ -5,26 +5,34 @@ mkdir -p build/tensoradapter/pytorch
 
 CACHEDIR=$1
 
-if [[ "$OSTYPE" = "darwin"* ]]; then
-        echo Copying prebuilt CPU binary...
-        cp $CACHEDIR/libdgl.dylib build
-	if [[ -e $CACHEDIR/tensoradapter_pytorch_cpu ]]; then
-		cp -v $CACHEDIR/tensoradapter_pytorch_cpu/*.so build/tensoradapter/pytorch
-	fi
+if [[ "$CACHEDIR" = "" ]]; then
+	echo No prebuilt binary directory specified, building with default options...
+	cd build
+	cmake -DUSE_OPENMP=ON -DUSE_CUDA=$USE_CUDA -DBUILD_TORCH=ON ..
+	make -j4
+	cd ..
 else
-        if [[ $USE_CUDA = 'ON' ]]; then
-                echo Copying prebuilt CUDA $CUDA_VER binary...
-                cp -v $CACHEDIR/libdgl.so.cu$CUDA_VER build/libdgl.so
-		if [[ -e $CACHEDIR/tensoradapter_pytorch_cu$CUDA_VER ]]; then
-	                cp -v $CACHEDIR/tensoradapter_pytorch_cu$CUDA_VER/*.so build/tensoradapter/pytorch
-		fi
-        else
-                echo Copying prebuilt CPU binary...
-                cp -v $CACHEDIR/libdgl.so.cpu build/libdgl.so
+	if [[ "$OSTYPE" = "darwin"* ]]; then
+		echo Copying prebuilt CPU binary...
+		cp $CACHEDIR/libdgl.dylib build
 		if [[ -e $CACHEDIR/tensoradapter_pytorch_cpu ]]; then
-	                cp -v $CACHEDIR/tensoradapter_pytorch_cpu/*.so build/tensoradapter/pytorch
+			cp -v $CACHEDIR/tensoradapter_pytorch_cpu/*.so build/tensoradapter/pytorch
 		fi
-        fi
+	else
+		if [[ $USE_CUDA = 'ON' ]]; then
+			echo Copying prebuilt CUDA $CUDA_VER binary...
+			cp -v $CACHEDIR/libdgl.so.cu$CUDA_VER build/libdgl.so
+			if [[ -e $CACHEDIR/tensoradapter_pytorch_cu$CUDA_VER ]]; then
+				cp -v $CACHEDIR/tensoradapter_pytorch_cu$CUDA_VER/*.so build/tensoradapter/pytorch
+			fi
+		else
+			echo Copying prebuilt CPU binary...
+			cp -v $CACHEDIR/libdgl.so.cpu build/libdgl.so
+			if [[ -e $CACHEDIR/tensoradapter_pytorch_cpu ]]; then
+				cp -v $CACHEDIR/tensoradapter_pytorch_cpu/*.so build/tensoradapter/pytorch
+			fi
+		fi
+	fi
 fi
 cd python
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
