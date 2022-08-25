4b33cebbd94d9e8d940c7e77caff5227f9212a33
ndickson <ndickson@nvidia.com>
2022-08-25 12:50:37 -0400

    * Updated doc comments for output_batch parameters for clarity

--- python/dgl/nn/pytorch/factory.py
+++ python/dgl/nn/pytorch/factory.py
@@ -119,8 +119,9 @@ class KNNGraph(nn.Module):
             be counted as one of its own k neighbors.  If False, the output graph will contain
             self loop edges, and a node will be counted as one of its own k neighbors.
         output_batch : bool, optional
-            If True, the output graph will have batch information set according to the
-            shape of ``x``. If False, the output graph will not have batch information set.
+            If True, the output will be a batched graph representing just 1 KNN graph if :attr:`x` is 2D,
+            or representing :math:`shape(x)[0]` KNN graphs if :attr:`x` is 3D, as described above.
+            If False, the output graph will be unbatched.
 
         Returns
         -------
@@ -237,8 +238,9 @@ class SegmentedKNNGraph(nn.Module):
             be counted as one of its own k neighbors.  If False, the output graph will contain
             self loop edges, and a node will be counted as one of its own k neighbors.
         output_batch : bool, optional
-            If True, the output graph will have batch information set according to ``segs``.
-            If False, the output graph will not have batch information set.
+            If True, the output will be a batched graph representing :math:`len(segs)` KNN graphs,
+            as described above.
+            If False, the output graph will be unbatched.
 
         Returns
         -------
