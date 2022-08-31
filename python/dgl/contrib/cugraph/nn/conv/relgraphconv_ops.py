46878dea831bdd53be32d2aae50c46467e581d78
Tingyu Wang <tingyuw@nvidia.com>
2022-08-30 23:55:23 -0400

    detach tensors before calling cugraph-ops function

--- python/dgl/contrib/cugraph/nn/conv/relgraphconv_ops.py
+++ python/dgl/contrib/cugraph/nn/conv/relgraphconv_ops.py
@@ -1,7 +1,6 @@
 """Torch Module for Relational graph convolution layer using cugraph"""
 # pylint: disable= no-member, arguments-differ, invalid-name
 import torch as th
-from torch import nn
 from pylibcugraphops.aggregators.node_level import agg_hg_basis_post_fwd_int64, agg_hg_basis_post_bwd_int64
 from pylibcugraphops.structure.graph_types import message_flow_graph_hg_csr_int64
 
@@ -24,13 +23,13 @@ class RgcnFunction(th.autograd.Function):
         in_node_types : ndarray or torch.Tensor, dtype=int32, device='cuda'
         edge_types : ndarray or torch.Tensor, dtype=int32, device='cuda'
 
-        coeff : torch.Tensor, dtype=torch.float32, device='cuda'
+        coeff : torch.Tensor, dtype=torch.float32, device='cuda', requires_grad=True
             Coefficient matrix in basis-decomposition for regularization, shape = n_edge_types * n_bases
         
-        in_feat : torch.Tensor, dtype=torch.float32, device='cuda'
+        in_feat : torch.Tensor, dtype=torch.float32, device='cuda', requires_grad=True
             Input feature, shape = n_in_nodes * in_feat_dim
         
-        W : torch.Tensor, dtype=torch.float32, device='cuda'
+        W : torch.Tensor, dtype=torch.float32, device='cuda', requires_grad=True
             shape = (n_bases+1) * in_feat_dim * out_feat_dim
             leading_dimension = (n_bases+1) * in_feat_dim
 
@@ -61,7 +60,7 @@ class RgcnFunction(th.autograd.Function):
         leading_dimension = (_n_bases+1) * in_feat.shape[-1]
 
         agg_out = th.empty(_n_out_nodes, leading_dimension, dtype=th.float32, device='cuda')
-        agg_hg_basis_post_fwd_int64(agg_out, in_feat, mfg, weights_combination=coeff)
+        agg_hg_basis_post_fwd_int64(agg_out, in_feat.detach(), mfg, weights_combination=coeff.detach())
 
         out_feat_dim = W.shape[-1]
         output = th.as_tensor(agg_out, device='cuda') @ W.view(leading_dimension, out_feat_dim)
@@ -81,13 +80,13 @@ class RgcnFunction(th.autograd.Function):
 
         # dense backward
         _out_feat_dim = W.shape[-1]
-        g_W = agg_out.transpose(0,1) @ grad_output
-        agg_out = grad_output @ W.view(-1, _out_feat_dim).transpose(0,1)   # gradient w.r.t input, reuse buffer
+        g_W = agg_out.t() @ grad_output
+        agg_out = grad_output @ W.view(-1, _out_feat_dim).t()   # gradient w.r.t input, reuse buffer
         
         # backward aggregation
         g_in = th.empty_like(in_feat, dtype=th.float32, device='cuda')
         g_coeff = th.empty_like(coeff, dtype=th.float32, device='cuda')
-        agg_hg_basis_post_bwd_int64(g_in, agg_out, in_feat, mfg,
-            output_weight_gradient=g_coeff, weights_combination=coeff)
+        agg_hg_basis_post_bwd_int64(g_in, agg_out, in_feat.detach(), mfg,
+            output_weight_gradient=g_coeff, weights_combination=coeff.detach())
 
         return None, None, None, None, None, None, None, g_coeff, g_in, g_W.view_as(W)
