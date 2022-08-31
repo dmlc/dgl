6a577a8356536572eb345f6b5df03e5e86a4ed83
Quan Gan <coin2028@hotmail.com>
2022-08-31 08:55:18 +0000

    add test

---
+++ tests/pytorch/test_storage.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+import dgl
+import pytest
+import torch
+
+class CustomGraphStorageWrapper(object):
+    def __init__(self, g, ndata, edata):
+        self.g = g
+        self.ndata = ndata
+        self.edata = edata
+
+    def get_node_storage(self, key, ntype=None):
+        return self.ndata[ntype][key]
+
+    def get_edge_storage(self, key, etype=None):
+        return self.edata[self.g.to_canonical_etype(etype)][key]
+
+    def __getattr__(self, attr, default=None):
+        return getattr(self.g, attr, default)
+
+def test_custom_graph_storage():
+    g = dgl.data.CoraFullDataset()[0]
+    ndata = {
+            g.ntypes[0]:
+            {key: dgl.storages.TensorStorage(value) for key, value in g.ndata.items()}}
+    edata = {
+            g.canonical_etypes[0]:
+            {key: dgl.storages.TensorStorage(value) for key, value in g.edata.items()}}
+    g_wrapped = CustomGraphStorageWrapper(g, ndata, edata)
+    sampler = dgl.dataloading.NeighborSampler(
+            [2, 2], prefetch_node_feats=['feat'], prefetch_labels=['label'])
+    dataloader = dgl.dataloading.DataLoader(
+            g_wrapped, torch.arange(g.num_nodes()), sampler, batch_size=40)
+    for input_nodes, output_nodes, blocks in dataloader:
+        pass
+
+if __name__ == '__main__':
+    test_custom_graph_storage()
