9915ca5b6f0958d7b0f600492adebd3830dcc723
lmh <823148190@qq.com>
2022-09-17 12:39:18 +0800

    del metapath2vec files

--- examples/pytorch/metapath2vec/reading_data.py
+++ examples/pytorch/metapath2vec/reading_data.py
@@ -1,96 +1,127 @@
-import dgl
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from download import AminerDataset
+np.random.seed(12345)
+
+class DataReader:
+    NEGATIVE_TABLE_SIZE = 1e8
+
+    def __init__(self, dataset, min_count, care_type):
+
+        self.negatives = []
+        self.discards = []
+        self.negpos = 0
+        self.care_type = care_type
+        self.word2id = dict()
+        self.id2word = dict()
+        self.sentences_count = 0
+        self.token_count = 0
+        self.word_frequency = dict()
+        self.inputFileName = dataset.fn
+        self.read_words(min_count)
+        self.initTableNegatives()
+        self.initTableDiscards()
+
+    def read_words(self, min_count):
+        word_frequency = dict()
+        for line in open(self.inputFileName, encoding="ISO-8859-1"):
+            line = line.split()
+            if len(line) > 1:
+                self.sentences_count += 1
+                for word in line:
+                    if len(word) > 0:
+                        self.token_count += 1
+                        word_frequency[word] = word_frequency.get(word, 0) + 1
+
+                        if self.token_count % 1000000 == 0:
+                            print("Read " + str(int(self.token_count / 1000000)) + "M words.")
+
+        wid = 0
+        for w, c in word_frequency.items():
+            if c < min_count:
+                continue
+            self.word2id[w] = wid
+            self.id2word[wid] = w
+            self.word_frequency[wid] = c
+            wid += 1
+
+        self.word_count = len(self.word2id)
+        print("Total embeddings: " + str(len(self.word2id)))
+
+    def initTableDiscards(self):
+        # get a frequency table for sub-sampling. Note that the frequency is adjusted by
+        # sub-sampling tricks.
+        t = 0.0001
+        f = np.array(list(self.word_frequency.values())) / self.token_count
+        self.discards = np.sqrt(t / f) + (t / f)
+
+    def initTableNegatives(self):
+        # get a table for negative sampling, if word with index 2 appears twice, then 2 will be listed
+        # in the table twice.
+        pow_frequency = np.array(list(self.word_frequency.values())) ** 0.75
+        words_pow = sum(pow_frequency)
+        ratio = pow_frequency / words_pow
+        count = np.round(ratio * DataReader.NEGATIVE_TABLE_SIZE)
+        for wid, c in enumerate(count):
+            self.negatives += [wid] * int(c)
+        self.negatives = np.array(self.negatives)
+        np.random.shuffle(self.negatives)
+        self.sampling_prob = ratio
+
+    def getNegatives(self, target, size):  # TODO check equality with target
+        if self.care_type == 0:
+            response = self.negatives[self.negpos:self.negpos + size]
+            self.negpos = (self.negpos + size) % len(self.negatives)
+            if len(response) != size:
+                return np.concatenate((response, self.negatives[0:self.negpos]))
+        return response
 
 
-class MetaPathGenerator:
-	def __init__(self):
-		self.id_author = dict()
-		self.id_conf = dict()
-		self.author_coauthorlist = dict()
-		self.conf_authorlist = dict()
-		self.author_conflist = dict()
-		self.paper_author = dict()
-		self.author_paper = dict()
-		self.conf_paper = dict()
-		self.paper_conf = dict()
+# -----------------------------------------------------------------------------------------------------------------
 
-	def read_data(self, dirpath):
-		with open(dirpath + "/id_author.txt") as adictfile:
-			for line in adictfile:
-				toks = line.strip().split("\t")
-				if len(toks) == 2:
-					self.id_author[int(toks[0])] = toks[1].replace(" ", "")
+class Metapath2vecDataset(Dataset):
+    def __init__(self, data, window_size):
+        # read in data, window_size and input filename
+        self.data = data
+        self.window_size = window_size
+        self.input_file = open(data.inputFileName, encoding="ISO-8859-1")
 
-		#print "#authors", len(self.id_author)
+    def __len__(self):
+        # return the number of walks
+        return self.data.sentences_count
 
-		with open(dirpath + "/id_conf.txt") as cdictfile:
-			for line in cdictfile:
-				toks = line.strip().split("\t")
-				if len(toks) == 2:
-					newconf = toks[1].replace(" ", "")
-					self.id_conf[int(toks[0])] = newconf
+    def __getitem__(self, idx):
+        # return the list of pairs (center, context, 5 negatives)
+        while True:
+            line = self.input_file.readline()
+            if not line:
+                self.input_file.seek(0, 0)
+                line = self.input_file.readline()
 
-		#print "#conf", len(self.id_conf)
+            if len(line) > 1:
+                words = line.split()
 
-		with open(dirpath + "/paper_author.txt") as pafile:
-			for line in pafile:
-				toks = line.strip().split("\t")
-				if len(toks) == 2:
-					p, a = toks[0], toks[1]
-					if p not in self.paper_author:
-						self.paper_author[p] = []
-					self.paper_author[p].append(a)
-					if a not in self.author_paper:
-						self.author_paper[a] = []
-					self.author_paper[a].append(p)
+                if len(words) > 1:
+                    word_ids = [self.data.word2id[w] for w in words if
+                                w in self.data.word2id and np.random.rand() < self.data.discards[self.data.word2id[w]]]
 
-		with open(dirpath + "/paper_conf.txt") as pcfile:
-			for line in pcfile:
-				toks = line.strip().split("\t")
-				if len(toks) == 2:
-					p, c = toks[0], toks[1]
-					self.paper_conf[p] = c
-					if c not in self.conf_paper:
-						self.conf_paper[c] = []
-					self.conf_paper[c].append(p)
-
-		sumpapersconf, sumauthorsconf = 0, 0
-		conf_authors = dict()
-		for conf in self.conf_paper:
-			papers = self.conf_paper[conf]
-			sumpapersconf += len(papers)
-			for paper in papers:
-				if paper in self.paper_author:
-					authors = self.paper_author[paper]
-					sumauthorsconf += len(authors)
-
-		print ("#confs  ", len(self.conf_paper))
-		print ("#papers ", sumpapersconf,  "#papers per conf ", sumpapersconf / len(self.conf_paper))
-		print ("#authors", sumauthorsconf, "#authors per conf", sumauthorsconf / len(self.conf_paper))
+                    pair_catch = []
+                    for i, u in enumerate(word_ids):
+                        for j, v in enumerate(
+                                word_ids[max(i - self.window_size, 0):i + self.window_size]):
+                            assert u < self.data.word_count
+                            assert v < self.data.word_count
+                            if i == j:
+                                continue
+                            pair_catch.append((u, v, self.data.getNegatives(v,5)))
+                    return pair_catch
 
 
-	def generate_random_aca(self):
-		for conf in self.conf_paper:
-			self.conf_authorlist[conf] = []
-			for paper in self.conf_paper[conf]:
-				if paper not in self.paper_author: continue
-				for author in self.paper_author[paper]:
-					self.conf_authorlist[conf].append(author)
-					if author not in self.author_conflist:
-						self.author_conflist[author] = []
-					self.author_conflist[author].append(conf)
+    @staticmethod
+    def collate(batches):
+        all_u = [u for batch in batches for u, _, _ in batch if len(batch) > 0]
+        all_v = [v for batch in batches for _, v, _ in batch if len(batch) > 0]
+        all_neg_v = [neg_v for batch in batches for _, _, neg_v in batch if len(batch) > 0]
 
-		print ("author-conf list done")
-
-		con_list=[]
-		author_list=[]
-		for conf in self.conf_authorlist:
-			for a in self.conf_authorlist[conf]:
-				con_list.append(int(conf))
-				author_list.append(int(a))
-
-		print("list construct done")
-
-		hg = dgl.heterograph({
-			('conference', 'ca', 'author'): (con_list,author_list),
-			('author', 'ac', 'conference'): (author_list, con_list)})
-		return hg,self.id_conf,self.id_author
+        return torch.LongTensor(all_u), torch.LongTensor(all_v), torch.LongTensor(all_neg_v)
--- examples/pytorch/metapath2vec/test.py
+++ examples/pytorch/metapath2vec/test.py
@@ -1,25 +1,16 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
-import tqdm
-import logging
-import sys
-import os
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler(sys.stdout))
-log_file = '{}-{}.log'.format("dgl_test", "aminer")
-logger.addHandler(logging.FileHandler("log/" + log_file))
 
-if __name__ == "__main__":
+if __name__ == "__main__":   
     venue_count = 133
     author_count = 246678
     experiment_times = 1
-    percents = np.arange(0.1, 1, 0.1)
-    file = open("./out/myout0.txt")
-    file_1 = open("data/label/googlescholar.8area.venue.label.txt")
-    file_2 = open("data/label/googlescholar.8area.author.label.txt")
+    percent = 0.05
+    file = open(".../output_file_path/...")
+    file_1 = open(".../label 2/googlescholar.8area.venue.label.txt")
+    file_2 = open(".../label 2/googlescholar.8area.author.label.txt")
     check_venue = {}
     check_author = {}
     for line in file_1:
@@ -45,75 +36,73 @@ if __name__ == "__main__":
                 author_embed_dict[embed[0]].append(float(embed[j]))
     #get venue embeddings
     print("reading finished")
-    for percent in percents:
-        venues = list(venue_embed_dict.keys())
-        authors = list(author_embed_dict.keys())
-        macro_average_venue = 0
-        micro_average_venue = 0
-        macro_average_author = 0
-        micro_average_author = 0
-        for time in range(experiment_times):
-            print("one more time")
-            np.random.shuffle(venues)
-            np.random.shuffle(authors)
-            venue_embedding = np.zeros((len(venues),128))
-            author_embedding = np.zeros((len(authors),128))
-            print("collecting venue embeddings")
-            index=0
-            for venue in tqdm.tqdm(venues):
-                temp = np.array(venue_embed_dict[venue])
-                venue_embedding[index,:]=temp
-                index+=1
-            print("collecting author embeddings")
-            index=0
-            for author in tqdm.tqdm(authors):
-                # print("one more author " + str(count))
-                temp_1 = np.array(author_embed_dict[author])
-                author_embedding[index, :] = temp_1
-                index += 1
-            # split data into training and testing
-            print("splitting")
-            venue_split = int(venue_count * percent)
-            venue_training = venue_embedding[:venue_split,:]
-            venue_testing = venue_embedding[venue_split:,:]
-            author_split = int(author_count * percent)
-            author_training = author_embedding[:author_split,:]
-            author_testing = author_embedding[author_split:,:]
-            # split label into training and testing
-            venue_label = []
-            venue_true = []
-            author_label = []
-            author_true = []
-            for i in range(len(venues)):
-                if i < venue_split:
-                    venue_label.append(check_venue[venues[i]])
-                else:
-                    venue_true.append(check_venue[venues[i]])
-            venue_label = np.array(venue_label)
-            venue_true = np.array(venue_true)
-            for j in range(len(authors)):
-                if j < author_split:
-                    author_label.append(check_author[authors[j]])
-                else:
-                    author_true.append(check_author[authors[j]])
-            author_label = np.array(author_label)
-            author_true = np.array(author_true)
-            file.close()
-            print("beging predicting")
-            clf_venue = LogisticRegression(random_state=0, solver="lbfgs", multi_class="multinomial").fit(venue_training,venue_label)
-            y_pred_venue = clf_venue.predict(venue_testing)
-            clf_author = LogisticRegression(random_state=0, solver="lbfgs", multi_class="multinomial").fit(author_training,author_label)
-            y_pred_author = clf_author.predict(author_testing)
-            macro_average_venue += f1_score(venue_true, y_pred_venue, average="macro")
-            micro_average_venue += f1_score(venue_true, y_pred_venue, average="micro")
-            macro_average_author += f1_score(author_true, y_pred_author, average="macro")
-            micro_average_author += f1_score(author_true, y_pred_author, average="micro")
-        print(macro_average_venue/float(experiment_times))
-        print(micro_average_venue/float(experiment_times))
-        print(macro_average_author / float(experiment_times))
-        print(micro_average_author / float(experiment_times))
-        logger.info(">>>>>>>>>>"+str(percent)+ ">>>>>>>>>>>>>")
-        logger.info("macro_average_venue: " + str(macro_average_venue/float(experiment_times)))
-        logger.info("micro_average_venue: " + str(micro_average_venue/float(experiment_times)))
-        logger.info("macro_average_author: " +str(macro_average_author / float(experiment_times)))
-        logger.info("micro_average_author: " +str(micro_average_author/ float(experiment_times)))
+    venues = list(venue_embed_dict.keys())
+    authors = list(author_embed_dict.keys())
+    macro_average_venue = 0
+    micro_average_venue = 0
+    macro_average_author = 0
+    micro_average_author = 0
+    for time in range(experiment_times):
+        print("one more time")
+        np.random.shuffle(venues)
+        np.random.shuffle(authors)
+        venue_embedding = np.array([])
+        author_embedding = np.array([])
+        print("collecting venue embeddings")
+        for venue in venues:
+            temp = np.array(venue_embed_dict[venue])
+            if len(venue_embedding) == 0:
+                venue_embedding = temp
+            else:
+                venue_embedding = np.vstack((venue_embedding, temp))
+        print("collecting author embeddings")
+        count = 0
+        for author in authors:
+            count += 1
+            # print("one more author " + str(count))
+            temp_1 = np.array(author_embed_dict[author])
+            if len(author_embedding) == 0:
+                author_embedding = temp_1
+            else:
+                author_embedding = np.vstack((author_embedding, temp_1))
+        # split data into training and testing
+        print("splitting")
+        venue_split = int(venue_count * percent)
+        venue_training = venue_embedding[:venue_split,:]
+        venue_testing = venue_embedding[venue_split:,:]
+        author_split = int(author_count * percent)
+        author_training = author_embedding[:author_split,:]
+        author_testing = author_embedding[author_split:,:]
+        # split label into training and testing
+        venue_label = []
+        venue_true = []
+        author_label = []
+        author_true = []
+        for i in range(len(venues)):
+            if i < venue_split:
+                venue_label.append(check_venue[venues[i]])
+            else:
+                venue_true.append(check_venue[venues[i]])
+        venue_label = np.array(venue_label)
+        venue_true = np.array(venue_true)
+        for j in range(len(authors)):
+            if j < author_split:
+                author_label.append(check_author[authors[j]])
+            else:
+                author_true.append(check_author[authors[j]])
+        author_label = np.array(author_label)
+        author_true = np.array(author_true)
+        file.close()
+        print("beging predicting")
+        clf_venue = LogisticRegression(random_state=0, solver="lbfgs", multi_class="multinomial").fit(venue_training,venue_label)
+        y_pred_venue = clf_venue.predict(venue_testing)
+        clf_author = LogisticRegression(random_state=0, solver="lbfgs", multi_class="multinomial").fit(author_training,author_label)
+        y_pred_author = clf_author.predict(author_testing)
+        macro_average_venue += f1_score(venue_true, y_pred_venue, average="macro")
+        micro_average_venue += f1_score(venue_true, y_pred_venue, average="micro")
+        macro_average_author += f1_score(author_true, y_pred_author, average="macro")
+        micro_average_author += f1_score(author_true, y_pred_author, average="micro")
+    print(macro_average_venue/float(experiment_times))
+    print(micro_average_venue/float(experiment_times))
+    print(macro_average_author / float(experiment_times))
+    print(micro_average_author / float(experiment_times))
