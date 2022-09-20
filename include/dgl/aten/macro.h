3ab84c0b0755d840eaadadf3020a3759d319d9da
Quan Gan <coin2028@hotmail.com>
2022-09-20 17:37:23 +0000

    fix

--- include/dgl/aten/macro.h
+++ include/dgl/aten/macro.h
@@ -172,16 +172,16 @@
 } while (0)
 
 #define ATEN_FLOAT_BOOL_TYPE_SWITCH(val, DType, val_name, ...) do {     \
-  if ((val).code == kDLInt && (val).bits == 8) {              \
+  if ((val).code == kDGLInt && (val).bits == 8) {              \
     typedef int8_t DType;                                     \
     {__VA_ARGS__}                                             \
-  } else if ((val).code == kDLUInt && (val).bits == 8) {      \
+  } else if ((val).code == kDGLUInt && (val).bits == 8) {      \
     typedef uint8_t DType;                                    \
     {__VA_ARGS__}                                             \
-  } else if ((val).code == kDLFloat && (val).bits == 32) {    \
+  } else if ((val).code == kDGLFloat && (val).bits == 32) {    \
     typedef float DType;                                      \
     {__VA_ARGS__}                                             \
-  } else if ((val).code == kDLFloat && (val).bits == 64) {    \
+  } else if ((val).code == kDGLFloat && (val).bits == 64) {    \
     typedef double DType;                                     \
     {__VA_ARGS__}                                             \
   } else {                                                    \
