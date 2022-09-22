cd67b31be7998a665aeca7bc9b0e8c81dd2ddb34
Vibhu Jawa <vibhujawa@gmail.com>
2022-09-22 16:17:01 -0700

    Update test_basics.py

--- tests/cugraph/test_basics.py
+++ tests/cugraph/test_basics.py
@@ -1,10 +1,11 @@
+import cugraph
 import backend as F
 import dgl
 import numpy as np
 from dgl import DGLGraph
 import unittest
 import pytest
-import cugraph
+
 
 def test_dummy():
     cg = cugraph.Graph()
