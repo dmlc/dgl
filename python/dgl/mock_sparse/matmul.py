a4f9b34f7e5bed63465dc14ed72ae593de68c421
Ubuntu <ubuntu@ip-172-31-9-26.ap-northeast-1.compute.internal>
2022-09-26 10:23:17 +0000

    Update

--- python/dgl/mock_sparse/matmul.py
+++ python/dgl/mock_sparse/matmul.py
@@ -13,10 +13,8 @@ __all__ = [
     'bspspmm'
 ]
 
-# internal functions
-
-def sparse_dense_mm(A: SparseMatrix, X: torch.Tensor) -> torch.Tensor:
-    """Multiply a sparse matrix by a dense matrix
+def _sparse_dense_mm(A: SparseMatrix, X: torch.Tensor) -> torch.Tensor:
+    """Internal function for multiplying a sparse matrix by a dense matrix
 
     Parameters
     ----------
@@ -32,8 +30,8 @@ def sparse_dense_mm(A: SparseMatrix, X: torch.Tensor) -> torch.Tensor:
     """
     return torch.matmul(A.adj, X)
 
-def sparse_sparse_mm(A1: SparseMatrix, A2: SparseMatrix) -> SparseMatrix:
-    """Multiply a sparse matrix by a sparse matrix
+def _sparse_sparse_mm(A1: SparseMatrix, A2: SparseMatrix) -> SparseMatrix:
+    """Internal function for multiplying a sparse matrix by a sparse matrix
 
     Parameters
     ----------
@@ -54,8 +52,8 @@ def sparse_sparse_mm(A1: SparseMatrix, A2: SparseMatrix) -> SparseMatrix:
                            val=result.values(),
                            shape=result.size())
 
-def diag_diag(A1: DiagMatrix, A2: DiagMatrix) -> DiagMatrix:
-    """Multiply a diagonal matrix by a diagonal matrix
+def _diag_diag_mm(A1: DiagMatrix, A2: DiagMatrix) -> DiagMatrix:
+    """Internal function for multiplying a diagonal matrix by a diagonal matrix
 
     Parameters
     ----------
@@ -77,11 +75,9 @@ def diag_diag(A1: DiagMatrix, A2: DiagMatrix) -> DiagMatrix:
     diag_val[:common_diag_len] = A1.val[:common_diag_len] * A2.val[:common_diag_len]
     return diag(diag_val.to(A1.device), (M, P))
 
-# internal functions for bspmm, bspspmm
-
-def unbatch_tensor(A: Union[torch.Tensor, SparseMatrix, DiagMatrix])\
+def _unbatch_tensor(A: Union[torch.Tensor, SparseMatrix, DiagMatrix])\
                    -> Union[List[torch.Tensor], List[SparseMatrix], List[DiagMatrix]]:
-    """Unbatch a tensor, sparse matrix, or diagonal matrix
+    """Internal function for unbatching a tensor, sparse matrix, or diagonal matrix
 
     Parameters
     ----------
@@ -102,9 +98,9 @@ def unbatch_tensor(A: Union[torch.Tensor, SparseMatrix, DiagMatrix])\
     else:
         return [diag(A.val[:, i], A.shape) for i in range(A.val.shape[-1])]
 
-def batch_tensor(A_list: Union[List[torch.Tensor], List[SparseMatrix], List[DiagMatrix]])\
+def _batch_tensor(A_list: Union[List[torch.Tensor], List[SparseMatrix], List[DiagMatrix]])\
                  -> Union[torch.Tensor, SparseMatrix, DiagMatrix]:
-    """Batch a list of tensors, sparse matrices, or diagonal matrices
+    """Internal function for batching a list of tensors, sparse matrices, or diagonal matrices
 
     Parameters
     ----------
@@ -126,111 +122,6 @@ def batch_tensor(A_list: Union[List[torch.Tensor], List[SparseMatrix], List[Diag
     else:
         return diag(
             val=torch.stack([A_list[i].val for i in range(len(A_list))], dim=-1), shape=A.shape)
-
-# internal functions to overload __matmul__
-
-def mm_sp(A1: SparseMatrix, A2: Union[torch.Tensor, SparseMatrix, DiagMatrix])\
-          -> Union[torch.Tensor, SparseMatrix]:
-    """Multiply a sparse matrix by a dense/sparse/diagonal matrix
-
-    Parameters
-    ----------
-    A1 : SparseMatrix
-        Matrix of shape (N, M), with values of shape (nnz1)
-    A2 : torch.Tensor, SparseMatrix, or DiagMatrix
-        Matrix of shape (M, P). If it is a SparseMatrix or DiagMatrix,
-        it should have values of shape (nnz2)
-
-    Returns
-    -------
-    torch.Tensor or SparseMatrix
-        The result of multiplication.
-
-        * It is a dense torch tensor if :attr:`A2` is so.
-        * It is a SparseMatrix object otherwise.
-
-    Examples
-    --------
-
-    >>> row = torch.tensor([0, 1, 1])
-    >>> col = torch.tensor([1, 0, 1])
-    >>> val = torch.randn(len(row))
-    >>> A1 = create_from_coo(row, col, val)
-    >>> A2 = torch.randn(2, 3)
-    >>> result = A1 @ A2
-    """
-    assert isinstance(A2, (torch.Tensor, SparseMatrix, DiagMatrix)), \
-        f'Expect arg2 to be a torch Tensor, SparseMatrix, or DiagMatrix object, got {type(A2)}'
-
-    val_dim = len(A1.val.shape)
-    assert val_dim == 1, f'Expect arg1.val to be a 1D tensor, got {val_dim}D'
-
-    assert A1.shape[1] == A2.shape[0], \
-        f'Expect arg1.shape[1] == arg2.shape[0], got {A1.shape[1]} and {A2.shape[0]}'
-
-    if isinstance(A2, torch.Tensor):
-        return sparse_dense_mm(A1, A2)
-
-    val_dim = len(A2.val.shape)
-    assert val_dim == 1, f'Expect arg2.val to be a 1D tensor, got {val_dim}D'
-
-    if isinstance(A2, DiagMatrix):
-        A2 = A2.as_sparse()
-    return sparse_sparse_mm(A1, A2)
-
-def mm_diag(A1: DiagMatrix, A2: Union[torch.Tensor, SparseMatrix, DiagMatrix])\
-            -> Union[torch.Tensor, SparseMatrix, DiagMatrix]:
-    """Multiply a diagonal matrix by a dense/sparse/diagonal matrix
-
-    Parameters
-    ----------
-    A1 : DiagMatrix
-        Matrix of shape (N, M), with values of shape (nnz1)
-    A2 : torch.Tensor, SparseMatrix, or DiagMatrix
-        Matrix of shape (M, P). If it is a SparseMatrix or DiagMatrix,
-        it should have values of shape (nnz2).
-
-    Returns
-    -------
-    torch.Tensor or DiagMatrix or SparseMatrix
-        The result of multiplication.
-
-        * It is a dense torch tensor if :attr:`A2` is so.
-        * It is a DiagMatrix object if :attr:`A2` is so.
-        * It is a SparseMatrix object otherwise.
-
-    Examples
-    --------
-
-    >>> val = torch.randn(3)
-    >>> A1 = diag(val)
-    >>> A2 = torch.randn(3, 2)
-    >>> result = A1 @ A2
-    """
-    assert isinstance(A2, (torch.Tensor, SparseMatrix, DiagMatrix)), \
-        f'Expect arg2 to be a torch Tensor, SparseMatrix, or DiagMatrix object, got {type(A2)}'
-
-    val_dim = len(A1.val.shape)
-    assert val_dim == 1, f'Expect arg1.val to be a 1D tensor, got {val_dim}D'
-
-    assert A1.shape[1] == A2.shape[0], \
-        f'Expect arg1.shape[1] == arg2.shape[0], got {A1.shape[1]} and {A2.shape[0]}'
-
-    if isinstance(A2, torch.Tensor):
-        return sparse_dense_mm(A1.as_sparse(), A2)
-
-    val_dim = len(A2.val.shape)
-    assert val_dim == 1, f'Expect arg2.val to be a 1D tensor, got {val_dim}D'
-
-    if isinstance(A2, DiagMatrix):
-        return diag_diag(A1, A2)
-    else:
-        return sparse_sparse_mm(A1.as_sparse(), A2)
-
-SparseMatrix.__matmul__ = mm_sp
-DiagMatrix.__matmul__ = mm_diag
-
-# external functions
 
 def spmm(A: Union[SparseMatrix, DiagMatrix], X: torch.Tensor) -> torch.Tensor:
     """Multiply a sparse matrix by a dense matrix
@@ -257,13 +148,20 @@ def spmm(A: Union[SparseMatrix, DiagMatrix], X: torch.Tensor) -> torch.Tensor:
     >>> X = torch.randn(2, 3)
     >>> result = A @ X
     """
-    assert isinstance(X, torch.Tensor), f'Expect X to be a torch.Tensor, got {type(X)}'
+    assert isinstance(A, (SparseMatrix, DiagMatrix)), \
+        f'Expect arg1 to be a SparseMatrix or DiagMatrix object, got {type(A)}'
+    assert isinstance(X, torch.Tensor), f'Expect arg2 to be a torch.Tensor, got {type(X)}'
+    assert A.shape[1] == X.shape[0], \
+        f'Expect arg1.shape[1] == arg2.shape[0], got {A.shape[1]} and {X.shape[0]}'
+    val_dim = len(A.val.shape)
+    assert val_dim == 1, f'Expect arg1.val to be a 1D tensor, got {val_dim}D'
+    val_dim = len(X.shape)
+    assert val_dim <= 2, f'Expect arg2 to be a 1D/2D tensor, got {val_dim}D'
+
     if isinstance(A, SparseMatrix):
-        return sparse_dense_mm(A, X)
-    elif isinstance(A, DiagMatrix):
-        return sparse_dense_mm(A.as_sparse(), X)
+        return _sparse_dense_mm(A, X)
     else:
-        raise ValueError(f'Expect A to be a SparseMatrix or DiagMatrix, got {type(A)}')
+        return _sparse_dense_mm(A.as_sparse(), X)
 
 def spspmm(A1: Union[SparseMatrix, DiagMatrix], A2: Union[SparseMatrix, DiagMatrix])\
            -> Union[SparseMatrix, DiagMatrix]:
@@ -309,16 +207,94 @@ def spspmm(A1: Union[SparseMatrix, DiagMatrix], A2: Union[SparseMatrix, DiagMatr
 
     if isinstance(A1, SparseMatrix):
         if isinstance(A2, SparseMatrix):
-            return sparse_sparse_mm(A1, A2)
+            return _sparse_sparse_mm(A1, A2)
         else:
-            return sparse_sparse_mm(A1, A2.as_sparse())
+            return _sparse_sparse_mm(A1, A2.as_sparse())
     else:
         if isinstance(A2, SparseMatrix):
-            return sparse_sparse_mm(A1.as_sparse(), A2)
+            return _sparse_sparse_mm(A1.as_sparse(), A2)
         else:
-            return diag_diag(A1, A2)
+            return _diag_diag(A1, A2)
 
-def bspmm(A1: Union[SparseMatrix, DiagMatrix], X: torch.Tensor)\
+def mm_sp(A1: SparseMatrix, A2: Union[torch.Tensor, SparseMatrix, DiagMatrix])\
+          -> Union[torch.Tensor, SparseMatrix]:
+    """Internal function for multiplying a sparse matrix by a dense/sparse/diagonal matrix
+
+    Parameters
+    ----------
+    A1 : SparseMatrix
+        Matrix of shape (N, M), with values of shape (nnz1)
+    A2 : torch.Tensor, SparseMatrix, or DiagMatrix
+        Matrix of shape (M, P). If it is a SparseMatrix or DiagMatrix,
+        it should have values of shape (nnz2)
+
+    Returns
+    -------
+    torch.Tensor or SparseMatrix
+        The result of multiplication.
+
+        * It is a dense torch tensor if :attr:`A2` is so.
+        * It is a SparseMatrix object otherwise.
+
+    Examples
+    --------
+
+    >>> row = torch.tensor([0, 1, 1])
+    >>> col = torch.tensor([1, 0, 1])
+    >>> val = torch.randn(len(row))
+    >>> A1 = create_from_coo(row, col, val)
+    >>> A2 = torch.randn(2, 3)
+    >>> result = A1 @ A2
+    """
+    assert isinstance(A2, (torch.Tensor, SparseMatrix, DiagMatrix)), \
+        f'Expect arg2 to be a torch Tensor, SparseMatrix, or DiagMatrix object, got {type(A2)}'
+
+    if isinstance(A2, torch.Tensor):
+        return spmm(A1, A2)
+    else:
+        return spspmm(A1, A2)
+
+def mm_diag(A1: DiagMatrix, A2: Union[torch.Tensor, SparseMatrix, DiagMatrix])\
+            -> Union[torch.Tensor, SparseMatrix, DiagMatrix]:
+    """Multiply a diagonal matrix by a dense/sparse/diagonal matrix
+
+    Parameters
+    ----------
+    A1 : DiagMatrix
+        Matrix of shape (N, M), with values of shape (nnz1)
+    A2 : torch.Tensor, SparseMatrix, or DiagMatrix
+        Matrix of shape (M, P). If it is a SparseMatrix or DiagMatrix,
+        it should have values of shape (nnz2).
+
+    Returns
+    -------
+    torch.Tensor or DiagMatrix or SparseMatrix
+        The result of multiplication.
+
+        * It is a dense torch tensor if :attr:`A2` is so.
+        * It is a DiagMatrix object if :attr:`A2` is so.
+        * It is a SparseMatrix object otherwise.
+
+    Examples
+    --------
+
+    >>> val = torch.randn(3)
+    >>> A1 = diag(val)
+    >>> A2 = torch.randn(3, 2)
+    >>> result = A1 @ A2
+    """
+    assert isinstance(A2, (torch.Tensor, SparseMatrix, DiagMatrix)), \
+        f'Expect arg2 to be a torch Tensor, SparseMatrix, or DiagMatrix object, got {type(A2)}'
+
+    if isinstance(A2, torch.Tensor):
+        return spmm(A1, A2)
+    else:
+        return spspmm(A1, A2)
+
+SparseMatrix.__matmul__ = mm_sp
+DiagMatrix.__matmul__ = mm_diag
+
+def bspmm(A: Union[SparseMatrix, DiagMatrix], X: torch.Tensor)\
           -> torch.Tensor:
     """Batched multiplication of a sparse matrix by a dense matrix,
     with the last dimension being the batch dimension
@@ -330,13 +306,13 @@ def bspmm(A1: Union[SparseMatrix, DiagMatrix], X: torch.Tensor)\
 
         result = []
         for i in range(H):
-            # If A2 is a 2D torch Tensor, then this will be A2[:, i]
-            result.append(A1[:, :, i] @ A2[:, :, i])
+            # If X is a 2D torch Tensor, then this will be X[:, i]
+            result.append(A[:, :, i] @ X[:, :, i])
         result = torch.stack(result, dim=-1)
 
     Parameters
     ----------
-    A1 : SparseMatrix or DiagMatrix
+    A : SparseMatrix or DiagMatrix
         Matrix of shape (N, M), with values of shape (nnz1, H)
     X : torch.Tensor
         Matrix of shape (M, P)
@@ -353,29 +329,29 @@ def bspmm(A1: Union[SparseMatrix, DiagMatrix], X: torch.Tensor)\
     >>> col = torch.tensor([1, 0, 1])
     >>> H = 4
     >>> val = torch.randn(len(row), H)
-    >>> A1 = create_from_coo(row, col, val)
+    >>> A = create_from_coo(row, col, val)
     >>> X = torch.randn(2, 3, H)
-    >>> result = bspmm(A1, X)
+    >>> result = bspmm(A, X)
     >>> result.shape
     (2, 3, 4)
     """
-    assert isinstance(A1, (SparseMatrix, DiagMatrix)), \
-        f'Expect A1 to be a SparseMatrix or DiagMatrix object, got {type(A1)}'
-    assert isinstance(A2, torch.Tensor), f'Expect A2 to be a torch Tensor, got {type(A2)}'
+    assert isinstance(A, (SparseMatrix, DiagMatrix)), \
+        f'Expect A to be a SparseMatrix or DiagMatrix object, got {type(A)}'
+    assert isinstance(X, torch.Tensor), f'Expect X to be a torch Tensor, got {type(X)}'
 
-    val_dim = len(A1.val.shape)
-    assert val_dim == 2, f'Expect A1.val to be a 2D tensor, got {val_dim}D'
-    H1 = A1.val.shape[-1]
+    val_dim = len(A.val.shape)
+    assert val_dim == 2, f'Expect A.val to be a 2D tensor, got {val_dim}D'
+    H1 = A.val.shape[-1]
 
-    val_dim = len(A2.shape)
-    assert val_dim in [2, 3], f'Expect to be a 2D/3D tensor, got {val_dim}D'
-    H2 = A2.shape[-1]
-    assert H1 == H2, f'Expect A1.val.shape[-1] == A2.shape[-1], got {H1} and {H2}'
+    val_dim = len(X.shape)
+    assert val_dim in [2, 3], f'Expect X to be a 2D/3D tensor, got {val_dim}D'
+    H2 = X.shape[-1]
+    assert H1 == H2, f'Expect A.val.shape[-1] == X.shape[-1], got {H1} and {H2}'
 
-    A1_unbatched = unbatch_tensor(A1)
-    A2_unbatched = unbatch_tensor(A2)
-    results = [spmm(A1_unbatched[i], A2_unbatched[i]) for i in range(H1)]
-    return batch_tensor(results)
+    A_unbatched = _unbatch_tensor(A)
+    X_unbatched = _unbatch_tensor(X)
+    results = [spmm(A_unbatched[i], X_unbatched[i]) for i in range(H1)]
+    return _batch_tensor(results)
 
 def bspspmm(A1: Union[SparseMatrix, DiagMatrix], A2: Union[SparseMatrix, DiagMatrix])\
             -> Union[SparseMatrix, DiagMatrix]:
@@ -438,7 +414,7 @@ def bspspmm(A1: Union[SparseMatrix, DiagMatrix], A2: Union[SparseMatrix, DiagMat
     H2 = A2.val.shape[-1]
     assert H1 == H2, f'Expect A1.val.shape[-1] == A2.val.shape[-1], got {H1} and {H2}'
 
-    A1_unbatched = unbatch_tensor(A1)
-    A2_unbatched = unbatch_tensor(A2)
+    A1_unbatched = _unbatch_tensor(A1)
+    A2_unbatched = _unbatch_tensor(A2)
     results = [spspmm(A1_unbatched[i], A2_unbatched[i]) for i in range(H1)]
-    return batch_tensor(results)
+    return _batch_tensor(results)
