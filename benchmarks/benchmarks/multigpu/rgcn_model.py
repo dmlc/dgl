a9f2acf3238dfb4d31ac5306bf5e75780ae50a78
Hongzhi (Steve), Chen <chenhongzhi.nkcs@gmail.com>
2022-09-26 21:46:32 +0800

    [Misc] Black auto fix. (#4641)
    * [Misc] Black auto fix.
    
    * sort
    
    Co-authored-by: Steve <ubuntu@ip-172-31-34-29.ap-northeast-1.compute.internal>

--- benchmarks/benchmarks/multigpu/rgcn_model.py
+++ benchmarks/benchmarks/multigpu/rgcn_model.py
@@ -3,10 +3,20 @@ import torch.nn as nn
 
 import dgl
 
+
 class BaseRGCN(nn.Module):
-    def __init__(self, num_nodes, h_dim, out_dim, num_rels, num_bases,
-                 num_hidden_layers=1, dropout=0,
-                 use_self_loop=False, use_cuda=False):
+    def __init__(
+        self,
+        num_nodes,
+        h_dim,
+        out_dim,
+        num_rels,
+        num_bases,
+        num_hidden_layers=1,
+        dropout=0,
+        use_self_loop=False,
+        use_cuda=False,
+    ):
         super(BaseRGCN, self).__init__()
         self.num_nodes = num_nodes
         self.h_dim = h_dim
@@ -50,9 +60,11 @@ class BaseRGCN(nn.Module):
             h = layer(g, h, r, norm)
         return h
 
+
 def initializer(emb):
     emb.uniform_(-1.0, 1.0)
     return emb
+
 
 class RelGraphEmbedLayer(nn.Module):
     r"""Embedding layer for featureless heterograph.
@@ -74,16 +86,19 @@ class RelGraphEmbedLayer(nn.Module):
     dgl_sparse : bool, optional
         If true, use dgl.nn.NodeEmbedding otherwise use torch.nn.Embedding
     """
-    def __init__(self,
-                 dev_id,
-                 num_nodes,
-                 node_tids,
-                 num_of_ntype,
-                 input_size,
-                 embed_size,
-                 dgl_sparse=False):
+
+    def __init__(
+        self,
+        dev_id,
+        num_nodes,
+        node_tids,
+        num_of_ntype,
+        input_size,
+        embed_size,
+        dgl_sparse=False,
+    ):
         super(RelGraphEmbedLayer, self).__init__()
-        self.dev_id = th.device(dev_id if dev_id >= 0 else 'cpu')
+        self.dev_id = th.device(dev_id if dev_id >= 0 else "cpu")
         self.embed_size = embed_size
         self.num_nodes = num_nodes
         self.dgl_sparse = dgl_sparse
@@ -96,10 +111,16 @@ class RelGraphEmbedLayer(nn.Module):
         for ntype in range(num_of_ntype):
             if isinstance(input_size[ntype], int):
                 if dgl_sparse:
-                    self.node_embeds[str(ntype)] = dgl.nn.NodeEmbedding(input_size[ntype], embed_size, name=str(ntype),
-                        init_func=initializer)
+                    self.node_embeds[str(ntype)] = dgl.nn.NodeEmbedding(
+                        input_size[ntype],
+                        embed_size,
+                        name=str(ntype),
+                        init_func=initializer,
+                    )
                 else:
-                    sparse_emb = th.nn.Embedding(input_size[ntype], embed_size, sparse=True)
+                    sparse_emb = th.nn.Embedding(
+                        input_size[ntype], embed_size, sparse=True
+                    )
                     nn.init.uniform_(sparse_emb.weight, -1.0, 1.0)
                     self.node_embeds[str(ntype)] = sparse_emb
             else:
@@ -110,8 +131,7 @@ class RelGraphEmbedLayer(nn.Module):
 
     @property
     def dgl_emb(self):
-        """
-        """
+        """ """
         if self.dgl_sparse:
             embs = [emb for emb in self.node_embeds.values()]
             return embs
@@ -137,15 +157,23 @@ class RelGraphEmbedLayer(nn.Module):
             embeddings as the input of the next layer
         """
         tsd_ids = node_ids.to(self.dev_id)
-        embeds = th.empty(node_ids.shape[0], self.embed_size, device=self.dev_id)
+        embeds = th.empty(
+            node_ids.shape[0], self.embed_size, device=self.dev_id
+        )
         for ntype in range(self.num_of_ntype):
             loc = node_tids == ntype
             if isinstance(features[ntype], int):
                 if self.dgl_sparse:
-                    embeds[loc] = self.node_embeds[str(ntype)](type_ids[loc], self.dev_id)
+                    embeds[loc] = self.node_embeds[str(ntype)](
+                        type_ids[loc], self.dev_id
+                    )
                 else:
-                    embeds[loc] = self.node_embeds[str(ntype)](type_ids[loc]).to(self.dev_id)
+                    embeds[loc] = self.node_embeds[str(ntype)](
+                        type_ids[loc]
+                    ).to(self.dev_id)
             else:
-                embeds[loc] = features[ntype][type_ids[loc]].to(self.dev_id) @ self.embeds[str(ntype)].to(self.dev_id)
+                embeds[loc] = features[ntype][type_ids[loc]].to(
+                    self.dev_id
+                ) @ self.embeds[str(ntype)].to(self.dev_id)
 
         return embeds
