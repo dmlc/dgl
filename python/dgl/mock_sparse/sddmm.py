af700681a6d8c3a649d304a209fe40b0a6d8d081
Israt Nisa <nisisrat@amazon.com>
2022-09-26 19:37:54 +0000

    drop support for vector shaped non zero elements

--- python/dgl/mock_sparse/sddmm.py
+++ python/dgl/mock_sparse/sddmm.py
@@ -7,27 +7,22 @@ __all__ = ["sddmm"]
 
 
 def sddmm(
-    A: SparseMatrix, mat1: torch.tensor, mat2: torch.tensor
+    A: SparseMatrix, mat1: torch.Tensor, mat2: torch.Tensor
 ) -> SparseMatrix:
     r"""Sampled-Dense-Dense Matrix Multiplication (sddmm). `sddmm` multiplies
     two dense matrices :attr:`mat1` and :attr:`mat2` at the nonzero locations
-    of sparse matrix :attr:`A`.
+    of sparse matrix :attr:`A`. Values of :attr:`A` is added to the resulting
+    matrix.
 
     Mathematically `sddmm` is formulated as:
 
     .. math::
-        out = (mat1 @ mat2) * A
-
-    :attr:`A` can have scalar-shaped `(nnz)` or vector-shaped `(nnz, D)`
-    nonzero values. For vector-shaped nonzero values, each `d` dimension in
-    `D` performs a sddmm operation on :attr:`mat1` and :attr:`mat2`.
-
+        out = (mat1 @ mat2) * spy(A) + A
 
     Parameters
     ----------
     A : SparseMatrix
-        Sparse matrix of shape (M, N). Values can have `(nnz)` or `(nnz, D)`
-        shape.
+        Sparse matrix of shape `(M, N)`.
     mat1 : Tensor
         Dense matrix of shape `(M, K)`
     mat2 : Tensor
@@ -36,15 +31,13 @@ def sddmm(
     Returns
     -------
     SparseMatrix
-        Sparse matrix of shape `(M, N)`. Values can have `(nnz)` or `(nnz, D)`
-        shape depending on the input :attr:`A`.
+        Sparse matrix of shape `(M, N)`.
 
     Examples
     --------
-    Case 1: Sparse matrix with scalar values. `A.val.shape` is (nnz).
 
-    >>> row = torch.tensor([1, 1, 2])
-    >>> col = torch.tensor([2, 3, 3])
+    >>> row = torch.Tensor([1, 1, 2])
+    >>> col = torch.Tensor([2, 3, 3])
     >>> val = torch.arange(1, 4).float()
     >>> A = SparseMatrix(row, col, val, (3, 4))
     >>> mat1 = torch.randn(3, 5)
@@ -54,37 +47,11 @@ def sddmm(
             [2, 3, 3]]),
     values=tensor([1.8035, 2.3375, 3.1255]),
     shape=(3, 4), nnz=3)
-
-    Case 2: Sparse matrix with vectors values. `A.val.shape` is (nnz, D).
-
-    >>> v = torch.arange(1, 7).float().reshape(A.nnz, -1)
-    >>> A = SparseMatrix(row, col, v)
-    >>> dgl.mock_sparse.sddmm(A, mat1, mat2)
-    SparseMatrix(indices=tensor([[1, 1, 2],
-            [2, 3, 3]]),
-    values=tensor([[1.8035, 2.8035],
-            [3.3375, 4.3375],
-            [5.1255, 6.1255]]),
-    shape=(3, 4), nnz=3)
     """
+    assert A.val.dim() == 1, (
+        "Nonzero elements have values of shape ({}). Expects "
+        "scalar values. ".format(A.val.shape[1])
+    )
     # PyTorch's sddmm operator only supports CSR format.
-    if len(A.val.shape) == 1:
-        # A.val is of shape (nnz).
-        vals = torch.sparse.sampled_addmm(
-            A.adj.to_sparse_csr(), mat1, mat2
-        ).values()
-    else:
-        # A.val is of shape (nnz, D).
-        _, dim = A.val.shape
-        vals = []
-        coo_index = A.adj.indices()
-        a_csr = torch.sparse_coo_tensor(coo_index, A.val[:, 0]).to_sparse_csr()
-        row_ptr = a_csr.crow_indices()
-        col_indices = a_csr.col_indices()
-        for i in range(dim):
-            val_i = A.val[:, i].contiguous().float()
-            adj_i = torch.sparse_csr_tensor(row_ptr, col_indices, val_i)
-            result_i = torch.sparse.sampled_addmm(adj_i, mat1, mat2)
-            vals.append(result_i.values())
-        vals = torch.stack(vals, dim=-1)
-    return SparseMatrix(A.row, A.col, vals, A.adj.shape)
+    res = torch.sparse.sampled_addmm(A.adj.to_sparse_csr(), mat1, mat2)
+    return SparseMatrix(A.row, A.col, res.values(), A.adj.shape)
--- tests/pytorch/mock_sparse/test_sddmm.py
+++ tests/pytorch/mock_sparse/test_sddmm.py
@@ -1,5 +1,5 @@
-import pytest
 import dgl
+import pytest
 import torch
 from dgl.mock_sparse import SparseMatrix
 
@@ -22,36 +22,11 @@ def all_close_sparse(A, B):
 def test_sddmm(idtype, dtype):
     row = torch.tensor([1, 0, 2, 9, 1])
     col = torch.tensor([0, 49, 2, 1, 7])
-    M = 10
-    N = 50
-    K = 20
-    matB = torch.rand(M, K)
-    matC = torch.rand(K, N)
+    val = torch.arange(1, 6).float()
+    A = SparseMatrix(row, col, val, shape=(10, 50))
+    matB = torch.rand(10, 5)
+    matC = torch.rand(5, 50)
 
-    # A.val shape (nnz)
-    val_scalar = torch.arange(1, 6).float()
-    A = SparseMatrix(row, col, val_scalar, shape=(M, N))
     dgl_result = dgl.mock_sparse.sddmm(A, matB, matC)
     th_result = torch.sparse.sampled_addmm(A.adj.to_sparse_csr(), matB, matC)
     all_close_sparse(dgl_result.adj, th_result.to_sparse_coo())
-
-    # A.val shape (nnz, D)
-    v = torch.arange(1, 11).float().reshape(A.nnz, -1)
-    A = SparseMatrix(row, col, v)
-    dgl_result = dgl.mock_sparse.sddmm(A, matB, matC)
-
-    th_result_i = []
-    for i in range(A.val.shape[1]):
-        A_coo_i = torch.sparse_coo_tensor(
-            A.adj.indices(), A.val[:, i].contiguous().float()
-        )
-        A_csr_i = A_coo_i.to_sparse_csr()
-        th_result_i.append(
-            torch.sparse.sampled_addmm(A_csr_i, matB, matC).values()
-        )
-    th_result = torch.stack(th_result_i, dim=-1)
-    torch.allclose(dgl_result.val, th_result)
-
-
-if __name__ == "__main__":
-    test_sddmm()
