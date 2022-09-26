6f3bc849e15a56f0edde1d9495327dce06e2ecd9
Quan Gan <coin2028@hotmail.com>
2022-09-26 05:56:01 +0000

    add test

---
+++ tests/distributed/test_dist_tensor.py
@@ -0,0 +1,53 @@
+import dgl
+import os
+import backend as F
+import unittest
+import pytest
+import operator
+from utils import reset_envs, generate_ip_config
+
+dist_g = None
+
+def rand_mask(shape, dtype):
+    return F.randn(shape) > 0
+
+def setup_module():
+    global dist_g
+
+    reset_envs()
+    os.environ['DGL_DIST_MODE'] = 'standalone'
+
+    g = create_random_graph(10000)
+    # Partition the graph
+    num_parts = 1
+    graph_name = 'dist_graph_test_3'
+    g.ndata['features'] = F.unsqueeze(F.arange(0, g.number_of_nodes()), 1)
+    g.edata['features'] = F.unsqueeze(F.arange(0, g.number_of_edges()), 1)
+    partition_graph(g, graph_name, num_parts, '/tmp/dist_graph')
+
+    dgl.distributed.initialize("kv_ip_config.txt")
+    dist_g = dgl.distributed.DistGraph(
+            graph_name, part_config='/tmp/dist_graph/{}.json'.format(graph_name))
+    dist_g.edata['mask1'] = dgl.distributed.DistTensor(
+            (g.num_edges(),), F.bool, 'mask1', init_func=rand_mask)
+    dist_g.edata['mask2'] = dgl.distributed.DistTensor(
+            (g.num_edges(),), F.bool, 'mask2', init_func=rand_mask)
+
+def check_binary_op(dist_g, key1, key2, key3, op):
+    for i in range(0, dist_g.num_edges(), 1000):
+        i_end = min(i + 1000, dist_g.num_edges())
+        assert F.array_equal(
+                g.edata[key3][i:i_end],
+                op(g.edata[key1][i:i_end], g.edata[key2][i:i_end]))
+
+@unittest.skipIf(dgl.backend.backend_name == "tensorflow", reason="TF doesn't support some of operations in DistGraph")
+@unittest.skipIf(dgl.backend.backend_name == "mxnet", reason="Turn off Mxnet support")
+def test_op():
+    dist_g.edata['mask3'] = dist_g.edata['mask1'] | dist_g.edata['mask2']
+    check_binary_op('mask1', 'mask2', 'mask3', operator.or_)
+
+def teardown_module():
+    dgl.distributed.exit_client() # this is needed since there's two test here in one process
+
+if __name__ == '__main__':
+    test_op()
