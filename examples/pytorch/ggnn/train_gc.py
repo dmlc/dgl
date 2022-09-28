f19f05ce66c47c87295ac57201d68f30b1cabfcd
Hongzhi (Steve), Chen <chenhongzhi.nkcs@gmail.com>
2022-09-28 10:28:04 +0800

    [Misc] Black auto fix. (#4651)
    Co-authored-by: Steve <ubuntu@ip-172-31-34-29.ap-northeast-1.compute.internal>

--- examples/pytorch/ggnn/train_gc.py
+++ examples/pytorch/ggnn/train_gc.py
@@ -3,34 +3,38 @@ Training and testing for graph classification tasks in bAbI
 """
 
 import argparse
+
+import numpy as np
+import torch
 from data_utils import get_babi_dataloaders
 from ggnn_gc import GraphClsGGNN
 from torch.optim import Adam
-import torch
-import numpy as np
 
 
 def main(args):
     out_feats = {18: 3}
     n_etypes = {18: 2}
 
-    train_dataloader, dev_dataloader, test_dataloaders = \
-        get_babi_dataloaders(batch_size=args.batch_size,
-                             train_size=args.train_num,
-                             task_id=args.task_id,
-                             q_type=args.question_id)
+    train_dataloader, dev_dataloader, test_dataloaders = get_babi_dataloaders(
+        batch_size=args.batch_size,
+        train_size=args.train_num,
+        task_id=args.task_id,
+        q_type=args.question_id,
+    )
 
-    model = GraphClsGGNN(annotation_size=2,
-                         out_feats=out_feats[args.task_id],
-                         n_steps=5,
-                         n_etypes=n_etypes[args.task_id],
-                         num_cls=2)
+    model = GraphClsGGNN(
+        annotation_size=2,
+        out_feats=out_feats[args.task_id],
+        n_steps=5,
+        n_etypes=n_etypes[args.task_id],
+        num_cls=2,
+    )
     opt = Adam(model.parameters(), lr=args.lr)
 
-    print(f'Task {args.task_id}, question_id {args.question_id}')
+    print(f"Task {args.task_id}, question_id {args.question_id}")
 
-    print(f'Training set size: {len(train_dataloader.dataset)}')
-    print(f'Dev set size: {len(dev_dataloader.dataset)}')
+    print(f"Training set size: {len(train_dataloader.dataset)}")
+    print(f"Dev set size: {len(dev_dataloader.dataset)}")
 
     # training and dev stage
     for epoch in range(args.epochs):
@@ -42,7 +46,7 @@ def main(args):
             loss.backward()
             opt.step()
             if epoch % 20 == 0:
-                print(f'Epoch {epoch}, batch {i} loss: {loss.data}')
+                print(f"Epoch {epoch}, batch {i} loss: {loss.data}")
 
         if epoch % 20 != 0:
             continue
@@ -62,7 +66,7 @@ def main(args):
 
     # test stage
     for i, dataloader in enumerate(test_dataloaders):
-        print(f'Test set {i} size: {len(dataloader.dataset)}')
+        print(f"Test set {i} size: {len(dataloader.dataset)}")
 
     test_acc_list = []
     for dataloader in test_dataloaders:
@@ -83,23 +87,29 @@ def main(args):
     test_acc_mean = np.mean(test_acc_list)
     test_acc_std = np.std(test_acc_list)
 
-    print(f'Mean of accuracy in 10 test datasets: {test_acc_mean}, std: {test_acc_std}')
+    print(
+        f"Mean of accuracy in 10 test datasets: {test_acc_mean}, std: {test_acc_std}"
+    )
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Gated Graph Neural Networks for graph classification tasks in bAbI')
-    parser.add_argument('--task_id', type=int, default=18,
-                        help='task id from 1 to 20')
-    parser.add_argument('--question_id', type=int, default=0,
-                        help='question id for each task')
-    parser.add_argument('--train_num', type=int, default=950,
-                        help='Number of training examples')
-    parser.add_argument('--batch_size', type=int, default=50,
-                        help='batch size')
-    parser.add_argument('--lr', type=float, default=1e-3,
-                        help='learning rate')
-    parser.add_argument('--epochs', type=int, default=200,
-                        help='number of training epochs')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Gated Graph Neural Networks for graph classification tasks in bAbI"
+    )
+    parser.add_argument(
+        "--task_id", type=int, default=18, help="task id from 1 to 20"
+    )
+    parser.add_argument(
+        "--question_id", type=int, default=0, help="question id for each task"
+    )
+    parser.add_argument(
+        "--train_num", type=int, default=950, help="Number of training examples"
+    )
+    parser.add_argument("--batch_size", type=int, default=50, help="batch size")
+    parser.add_argument("--lr", type=float, default=1e-3, help="learning rate")
+    parser.add_argument(
+        "--epochs", type=int, default=200, help="number of training epochs"
+    )
 
     args = parser.parse_args()
 
