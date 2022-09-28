254a49f8eb136c816533c3f3278f9e10f028a22d
Ubuntu <ubuntu@ip-172-31-9-26.ap-northeast-1.compute.internal>
2022-09-28 13:41:43 +0000

    Update

--- python/dgl/nn/pytorch/network_emb.py
+++ python/dgl/nn/pytorch/network_emb.py
@@ -7,6 +7,8 @@ from torch.nn import init
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import numpy as np
+
+from ...random import choice
 from ...sampling import random_walk
 
 __all__ = ['DeepWalk']
@@ -57,6 +59,9 @@ class DeepWalk(nn.Module):
     >>> model = DeepWalk(g)
     >>> dataloader = DataLoader(torch.arange(g.num_nodes()), batch_size=128,
     ...                         shuffle=True, collate_fn=model.sample)
+    >>> num_epochs = 10
+    >>> for epoch in range(num_epochs):
+    ...     for batch_walk in dataloader:
     """
     def __init__(self,
                  g,
@@ -121,12 +126,12 @@ class DeepWalk(nn.Module):
 
         Parameters
         ----------
-        indices : Tensor
+        indices : torch.Tensor
             Nodes from which we perform random walk
 
         Returns
         -------
-        Tensor
+        torch.Tensor
             Random walks in the form of node ID sequences. The Tensor
             is of shape :attr:`(len(indices), walk_length)`.
         """
@@ -137,9 +142,14 @@ class DeepWalk(nn.Module):
 
         Parameters
         ----------
-        batch_walk : Tensor
+        batch_walk : torch.Tensor
             Random walks in the form of node ID sequences. The Tensor
             is of shape :attr:`(batch_size, walk_length)`.
+
+        Returns
+        -------
+        torch.Tensor
+            Loss value
         """
         batch_size = len(batch_walk)
         device = batch_walk.device
@@ -148,92 +158,37 @@ class DeepWalk(nn.Module):
         batch_context_embed = self.context_embed(batch_walk).view(-1, self.emb_dim)
 
         batch_idx_list_offset = torch.arange(batch_size) * self.walk_length
-        batch_idx_list_offset = batch_idx_list_offset.to(device).unsqueeze(1)
-        batch_idx_list_src = batch_idx_list_offset + self.idx_list_src.unsqueeze(0)
-        batch_idx_list_dst = batch_idx_list_offset + self.idx_list_dst.unsqueeze(0)
-        batch_idx_list_src = batch_idx_list_src.view(-1)
-        batch_idx_list_dst = batch_idx_list_dst.view(-1)
+        batch_idx_list_offset = batch_idx_list_offset.unsqueeze(1)
+        idx_list_src = batch_idx_list_offset + self.idx_list_src.unsqueeze(0)
+        idx_list_dst = batch_idx_list_offset + self.idx_list_dst.unsqueeze(0)
+        idx_list_src = idx_list_src.view(-1).to(device)
+        idx_list_dst = idx_list_dst.view(-1).to(device)
 
-        pos_src_emb = batch_node_embed[batch_idx_list_src]
-        pos_dst_emb = batch_context_embed[batch_idx_list_dst]
+        pos_src_emb = batch_node_embed[idx_list_src]
+        pos_dst_emb = batch_context_embed[idx_list_dst]
 
-    def _generate_sample(self, batch_data):
-        if flag:
-            self.index_emb_negu, self.index_emb_negv = self._init_emb2neg_index(
-                self.walk_length,
-                self.window_size,
-                self.negative_size,
-                self.batch_size,
-                device)
+        neg_idx_list_src = idx_list_dst.unsqueeze(1) + torch.zeros(
+            self.negative_size).unsqueeze(0)
+        neg_idx_list_src = neg_idx_list_src.view(-1)
+        neg_src_emb = batch_node_embed[neg_idx_list_src]
 
-        ## Negative
-        emb_neg_u = torch.index_select(emb_u, 0, self.index_emb_negu)
         if self.fast_neg:
-            emb_neg_v = torch.index_select(emb_v, 0, self.index_emb_negv)
+            neg_idx_list_dst = list(range(batch_size * self.walk_length)) \
+                * (self.negative_size * self.window_size * 2)
+            random.shuffle(neg_idx_list_dst)
+            neg_idx_list_dst = neg_idx_list_dst[:len(neg_idx_list_src)]
+            neg_idx_list_dst = torch.LongTensor(neg_idx_list_dst).to(device)
+            neg_dst_emb = batch_context_embed[neg_idx_list_dst]
         else:
-            neg_nodes = torch.LongTensor(
-                np.random.choice(self.neg_table,
-                                 self.batch_size * self.num_pos * self.negative_size,
-                                 replace=True))
-            emb_neg_v = self.context_embed(neg_nodes)
+            neg_dst = choice(self.g.num_nodes(), size=len(neg_src_emb), prob=self.neg_prob)
+            neg_dst_emb = self.context_embed(neg_dst)
 
-        return emb_pos_u, emb_pos_v, emb_neg_u, emb_neg_v
-
-    def forward(self, emb_pos_u, emb_pos_v, emb_neg_u, emb_neg_v):
-        """
-        Return the loss score given center nodes, positive context nodes, negative samples
-        and corresponding context nodes.
-
-        Parameters
-        ----------
-        emb_pos_u : Tensor
-            center nodes embedding id
-        emb_pos_v : Tensor
-            positive context nodes embedding id
-        emb_neg_u : Tensor
-            negative samples embedding id
-        emb_neg_v : Tensor
-            context nodes corresponding to negative samples
-
-        Returns
-        ------
-        torch.Tensor
-        return the SkipGram model loss.
-        """
-        pos_score = torch.sum(torch.mul(emb_pos_u, emb_pos_v),dim=1)
+        pos_score = torch.sum(torch.mul(pos_src_emb, pos_dst_emb), dim=1)
         pos_score = torch.clamp(pos_score, max=6, min=-6)
         pos_score = torch.mean(-F.logsigmoid(pos_score))
 
-        # [batch_size * walk_length * negative, dim]
-        neg_score = torch.sum(torch.mul(emb_neg_u, emb_neg_v),dim=1)
+        neg_score = torch.sum(torch.mul(neg_src_emb, neg_dst_emb), dim=1)
         neg_score = torch.clamp(neg_score, max=6, min=-6)
-        # [batch_size * walk_length * negative, 1]
         neg_score = torch.mean(-F.logsigmoid(-neg_score)) * self.negative_size * self.neg_weight
 
-        loss = torch.mean(pos_score + neg_score)
-        return loss
-
-    def _init_emb2neg_index(self, walk_length, window_size, negative, batch_size, device):
-        '''select embedding of negative nodes from a batch of node embeddings
-        for fast negative sampling
-        '''
-        idx_list_u = []
-        for b in range(batch_size):
-            for i in range(walk_length):
-                for j in range(i - window_size, i):
-                    if j >= 0:
-                        idx_list_u += [i + b * walk_length] * negative
-                for j in range(i + 1, i + 1 + window_size):
-                    if j < walk_length:
-                        idx_list_u += [i + b * walk_length] * negative
-
-        idx_list_v = list(range(batch_size * walk_length)) \
-                     * negative * window_size * 2
-        random.shuffle(idx_list_v)
-        idx_list_v = idx_list_v[:len(idx_list_u)]
-
-        # [bs * walk_length * negative]
-        index_emb_negu = torch.LongTensor(idx_list_u).to(device)
-        index_emb_negv = torch.LongTensor(idx_list_v).to(device)
-
-        return index_emb_negu, index_emb_negv
+        return torch.mean(pos_score + neg_score)
