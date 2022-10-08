a208e8868b77ae6e0c18dee197caa659a00e9a0e
Hongzhi (Steve), Chen <chenhongzhi.nkcs@gmail.com>
2022-10-08 14:15:56 +0800

    [Misc] Black auto fix. (#4680)
    * [Misc] Black auto fix.
    
    * fix pylint disable
    
    Co-authored-by: Steve <ubuntu@ip-172-31-34-29.ap-northeast-1.compute.internal>

--- python/dgl/_ffi/streams.py
+++ python/dgl/_ffi/streams.py
@@ -5,13 +5,13 @@ For applications, please use PyTorch's stream management, of which DGL is aware.
 from __future__ import absolute_import
 
 import ctypes
-from .base import _LIB, check_call, _FFI_MODE
+
+from .base import _FFI_MODE, _LIB, check_call
 from .runtime_ctypes import DGLStreamHandle
 
-IMPORT_EXCEPT = RuntimeError if _FFI_MODE == "cython" else ImportError
 
 def to_dgl_stream_handle(cuda_stream):
-    """ Convert torch.cuda.Stream to DGL stream handle
+    """Convert torch.cuda.Stream to DGL stream handle
 
     Parameters
     ----------
@@ -23,6 +23,7 @@ def to_dgl_stream_handle(cuda_stream):
         DGLStreamHandle of the input ``cuda_stream``.
     """
     return ctypes.c_void_p(cuda_stream.cuda_stream)
+
 
 def _dgl_get_stream(ctx):
     """Get the current CUDA stream of the given DGL context.
@@ -37,6 +38,9 @@ def _dgl_get_stream(ctx):
         DGLStreamHandle of the current CUDA stream.
     """
     current_cuda_stream = DGLStreamHandle()
-    check_call(_LIB.DGLGetStream(
-        ctx.device_type, ctx.device_id, ctypes.byref(current_cuda_stream)))
+    check_call(
+        _LIB.DGLGetStream(
+            ctx.device_type, ctx.device_id, ctypes.byref(current_cuda_stream)
+        )
+    )
     return current_cuda_stream
