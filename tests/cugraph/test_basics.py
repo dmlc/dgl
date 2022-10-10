89a4cc4db214347fb6add23dbf373485f6a57499
Hongzhi (Steve), Chen <chenhongzhi.nkcs@gmail.com>
2022-10-11 00:12:30 +0800

    [Misc] Black auto fix. (#4694)
    Co-authored-by: Steve <ubuntu@ip-172-31-34-29.ap-northeast-1.compute.internal>

--- tests/cugraph/test_basics.py
+++ tests/cugraph/test_basics.py
@@ -1,28 +1,32 @@
 # NOTE(vibwu): Currently cugraph must be imported before torch to avoid a resource cleanup issue.
 #    See https://github.com/rapidsai/cugraph/issues/2718
-import cugraph
-import backend as F
-import dgl
-import numpy as np
-from dgl import DGLGraph
 import unittest
+
+import backend as F
+import cugraph
+import numpy as np
 import pytest
+
+import dgl
+from dgl import DGLGraph
 
 
 def test_dummy():
     cg = cugraph.Graph()
     assert cg is not None
 
+
 def test_to_cugraph_conversion():
-    g = dgl.graph((F.tensor([0, 1, 2, 3]), F.tensor([1, 0, 3, 2]))).to('cuda')
+    g = dgl.graph((F.tensor([0, 1, 2, 3]), F.tensor([1, 0, 3, 2]))).to("cuda")
     cugraph_g = g.to_cugraph()
 
-    assert cugraph_g.number_of_nodes()==g.number_of_nodes()
-    assert cugraph_g.number_of_edges()==g.number_of_edges()
+    assert cugraph_g.number_of_nodes() == g.number_of_nodes()
+    assert cugraph_g.number_of_edges() == g.number_of_edges()
 
     assert cugraph_g.has_edge(0, 1)
     assert cugraph_g.has_edge(1, 0)
     assert cugraph_g.has_edge(3, 2)
+
 
 def test_from_cugraph_conversion():
     # cudf is a dependency of cugraph
@@ -30,14 +34,13 @@ def test_from_cugraph_conversion():
 
     # directed graph conversion test
     cugraph_g = cugraph.Graph(directed=True)
-    df = cudf.DataFrame({"source":[0, 1, 2, 3],
-                     "destination":[1, 2, 3, 2]})
+    df = cudf.DataFrame({"source": [0, 1, 2, 3], "destination": [1, 2, 3, 2]})
 
     cugraph_g.from_cudf_edgelist(df)
 
     g = dgl.from_cugraph(cugraph_g)
 
-    assert g.device.type == 'cuda'
+    assert g.device.type == "cuda"
     assert g.number_of_nodes() == cugraph_g.number_of_nodes()
     assert g.number_of_edges() == cugraph_g.number_of_edges()
 
@@ -50,14 +53,13 @@ def test_from_cugraph_conversion():
 
     # undirected graph conversion test
     cugraph_g = cugraph.Graph(directed=False)
-    df = cudf.DataFrame({"source":[0, 1, 2, 3],
-                     "destination":[1, 2, 3, 2]})
+    df = cudf.DataFrame({"source": [0, 1, 2, 3], "destination": [1, 2, 3, 2]})
 
     cugraph_g.from_cudf_edgelist(df)
 
     g = dgl.from_cugraph(cugraph_g)
 
-    assert g.device.type == 'cuda'
+    assert g.device.type == "cuda"
     assert g.number_of_nodes() == cugraph_g.number_of_nodes()
     # assert reverse edges are present
     assert g.has_edges_between(0, 1)
