ea48ce7a3742c5d4ee7c37279be741db6b45bf50
Hongzhi (Steve), Chen <chenhongzhi.nkcs@gmail.com>
2022-10-11 13:31:13 +0800

    [Misc] Black auto fix. (#4697)
    Co-authored-by: Steve <ubuntu@ip-172-31-34-29.ap-northeast-1.compute.internal>

--- tools/dispatch_data.py
+++ tools/dispatch_data.py
@@ -1,12 +1,13 @@
 """Launching distributed graph partitioning pipeline """
+import argparse
+import json
+import logging
 import os
 import sys
-import argparse
-import logging
-import json
+
 from partition_algo.base import load_partition_meta
 
-INSTALL_DIR = os.path.abspath(os.path.join(__file__, '..'))
+INSTALL_DIR = os.path.abspath(os.path.join(__file__, ".."))
 LAUNCH_SCRIPT = "distgraphlaunch.py"
 PIPELINE_SCRIPT = "distpartitioning/data_proc_pipeline.py"
 
@@ -23,6 +24,7 @@ LARG_IPCONF = "ip_config"
 LARG_MASTER_PORT = "master_port"
 LARG_SSH_PORT = "ssh_port"
 
+
 def get_launch_cmd(args) -> str:
     cmd = sys.executable + " " + os.path.join(INSTALL_DIR, LAUNCH_SCRIPT)
     cmd = f"{cmd} --{LARG_SSH_PORT} {args.ssh_port} "
@@ -34,7 +36,7 @@ def get_launch_cmd(args) -> str:
 
 
 def submit_jobs(args) -> str:
-    #read the json file and get the remaining argument here.
+    # read the json file and get the remaining argument here.
     schema_path = "metadata.json"
     with open(os.path.join(args.in_dir, schema_path)) as schema:
         schema_map = json.load(schema)
@@ -49,17 +51,22 @@ def submit_jobs(args) -> str:
         part_meta = load_partition_meta(partition_path)
         num_parts = part_meta.num_parts
     if num_parts > num_chunks:
-        raise Exception('Number of partitions should be less/equal than number of chunks.')
+        raise Exception(
+            "Number of partitions should be less/equal than number of chunks."
+        )
 
     # verify ip_config
-    with open(args.ip_config, 'r') as f:
+    with open(args.ip_config, "r") as f:
         num_ips = len(f.readlines())
-        assert num_ips == num_parts, \
-            f'The number of lines[{args.ip_config}] should be equal to num_parts[{num_parts}].'
+        assert (
+            num_ips == num_parts
+        ), f"The number of lines[{args.ip_config}] should be equal to num_parts[{num_parts}]."
 
     argslist = ""
     argslist += "--world-size {} ".format(num_parts)
-    argslist += "--partitions-dir {} ".format(os.path.abspath(args.partitions_dir))
+    argslist += "--partitions-dir {} ".format(
+        os.path.abspath(args.partitions_dir)
+    )
     argslist += "--input-dir {} ".format(os.path.abspath(args.in_dir))
     argslist += "--graph-name {} ".format(graph_name)
     argslist += "--schema {} ".format(schema_path)
@@ -75,28 +82,73 @@ def submit_jobs(args) -> str:
     udf_cmd = f"{args.python_path} {pipeline_cmd} {argslist}"
 
     launch_cmd = get_launch_cmd(args)
-    launch_cmd += '\"'+udf_cmd+'\"'
+    launch_cmd += '"' + udf_cmd + '"'
 
     print(launch_cmd)
     os.system(launch_cmd)
 
-def main():
-    parser = argparse.ArgumentParser(description='Dispatch edge index and data to partitions', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--in-dir', type=str, help='Location of the input directory where the dataset is located')
-    parser.add_argument('--partitions-dir', type=str, help='Location of the partition-id mapping files which define node-ids and their respective partition-ids, relative to the input directory')
-    parser.add_argument('--out-dir', type=str, help='Location of the output directory where the graph partitions will be created by this pipeline')
-    parser.add_argument('--ip-config', type=str, help='File location of IP configuration for server processes')
-    parser.add_argument('--master-port', type=int, default=12345, help='port used by gloo group to create randezvous point')
-    parser.add_argument('--log-level', type=str, default="info", help='To enable log level for debugging purposes. Available options: (Critical, Error, Warning, Info, Debug, Notset)')
-    parser.add_argument('--python-path', type=str, default=sys.executable, help='Path to the Python executable on all workers')
-    parser.add_argument('--ssh-port', type=int, default=22, help='SSH Port.') 
-    parser.add_argument('--process-group-timeout', type=int, default=1800,
-                        help='timeout[seconds] for operations executed against the process group')
-    parser.add_argument('--save-orig-nids', action='store_true',
-                        help='Save original node IDs into files')
-    parser.add_argument('--save-orig-eids', action='store_true',
-                        help='Save original edge IDs into files')
+def main():
+    parser = argparse.ArgumentParser(
+        description="Dispatch edge index and data to partitions",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--in-dir",
+        type=str,
+        help="Location of the input directory where the dataset is located",
+    )
+    parser.add_argument(
+        "--partitions-dir",
+        type=str,
+        help="Location of the partition-id mapping files which define node-ids and their respective partition-ids, relative to the input directory",
+    )
+    parser.add_argument(
+        "--out-dir",
+        type=str,
+        help="Location of the output directory where the graph partitions will be created by this pipeline",
+    )
+    parser.add_argument(
+        "--ip-config",
+        type=str,
+        help="File location of IP configuration for server processes",
+    )
+    parser.add_argument(
+        "--master-port",
+        type=int,
+        default=12345,
+        help="port used by gloo group to create randezvous point",
+    )
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        default="info",
+        help="To enable log level for debugging purposes. Available options: (Critical, Error, Warning, Info, Debug, Notset)",
+    )
+    parser.add_argument(
+        "--python-path",
+        type=str,
+        default=sys.executable,
+        help="Path to the Python executable on all workers",
+    )
+    parser.add_argument("--ssh-port", type=int, default=22, help="SSH Port.")
+    parser.add_argument(
+        "--process-group-timeout",
+        type=int,
+        default=1800,
+        help="timeout[seconds] for operations executed against the process group",
+    )
+    parser.add_argument(
+        "--save-orig-nids",
+        action="store_true",
+        help="Save original node IDs into files",
+    )
+    parser.add_argument(
+        "--save-orig-eids",
+        action="store_true",
+        help="Save original edge IDs into files",
+    )
 
     args, udf_command = parser.parse_known_args()
 
@@ -109,7 +161,8 @@ def main():
     tokens = sys.executable.split(os.sep)
     submit_jobs(args)
 
-if __name__ == '__main__':
-    fmt = '%(asctime)s %(levelname)s %(message)s'
+
+if __name__ == "__main__":
+    fmt = "%(asctime)s %(levelname)s %(message)s"
     logging.basicConfig(format=fmt, level=logging.INFO)
     main()
