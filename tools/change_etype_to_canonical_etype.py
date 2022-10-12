6a222ff30c74604bf5c5de54f5d8f97557635ed1
peizhou001 <peizhou@amazon.com>
2022-10-12 09:22:15 +0000

    fix format

--- tools/change_etype_to_canonical_etype.py
+++ tools/change_etype_to_canonical_etype.py
@@ -1,40 +1,48 @@
+import argparse
 import json
+import logging
+import os
+import time
+
+import torch
+
 import dgl
 from dgl._ffi.base import DGLError
-from dgl.distributed import load_partition_book
-import time
-import argparse
-import os
-import logging
-import torch
 from dgl.data.utils import load_graphs
+from dgl.distributed import load_partition_book
 
-etypes_key = 'etypes'
-edge_map_key = 'edge_map'
-canonical_etypes_delimiter = ':'
+etypes_key = "etypes"
+edge_map_key = "edge_map"
+canonical_etypes_delimiter = ":"
+
 
 def convert_conf(part_config):
-    with open(part_config, 'r+', encoding='utf-8') as f:
+    with open(part_config, "r+", encoding="utf-8") as f:
         config = json.load(f)
-        logging.info('Checking if the provided json file need to be changed.')
+        logging.info("Checking if the provided json file need to be changed.")
         if is_old_version(config):
-            logging.info('Changing the partition configuration file.')
+            logging.info("Changing the partition configuration file.")
             canonical_etypes = etype2canonical_etype(part_config)
             # convert edge_map key from etype -> c_etype
             new_edge_map = {}
             for e_type, range in config[edge_map_key].items():
                 eid = config[etypes_key][e_type]
-                c_etype = [key for key in canonical_etypes if canonical_etypes[key] == eid][0]
+                c_etype = [
+                    key
+                    for key in canonical_etypes
+                    if canonical_etypes[key] == eid
+                ][0]
                 new_edge_map[c_etype] = range
-            config[edge_map_key] = new_edge_map  
+            config[edge_map_key] = new_edge_map
             config[etypes_key] = canonical_etypes
-            logging.info('Dumping the content to disk.')
+            logging.info("Dumping the content to disk.")
             f.seek(0)
             json.dump(config, f, indent=4)
             f.truncate()
 
+
 def etype2canonical_etype(part_config):
-    gpb, _, _, etypes =  load_partition_book(part_config=part_config, part_id=0)
+    gpb, _, _, etypes = load_partition_book(part_config=part_config, part_id=0)
     eid = []
     etype_id = []
     for etype in etypes:
@@ -45,23 +53,37 @@ def etype2canonical_etype(part_config):
     etype_id = torch.IntTensor(etype_id)
     partition_id = gpb.eid2partid(eid)
     canonical_etypes = {}
-    part_ids = [part_id for part_id in range(gpb.num_partitions()) if part_id in partition_id]
+    part_ids = [
+        part_id
+        for part_id in range(gpb.num_partitions())
+        if part_id in partition_id
+    ]
     for part_id in part_ids:
         seed_edges = torch.masked_select(eid, partition_id == part_id)
         seed_edge_tids = torch.masked_select(etype_id, partition_id == part_id)
-        c_etype = _find_c_etypes_in_partition(seed_edges, seed_edge_tids, part_id, part_config)
+        c_etype = _find_c_etypes_in_partition(
+            seed_edges, seed_edge_tids, part_id, part_config
+        )
         canonical_etypes.update(c_etype)
     return canonical_etypes
 
-def _find_c_etypes_in_partition(seed_edges, seed_edge_tids, part_id, part_config):
+
+def _find_c_etypes_in_partition(
+    seed_edges, seed_edge_tids, part_id, part_config
+):
     folder = os.path.dirname(os.path.realpath(part_config))
     partition_book = {}
     local_g = dgl.DGLGraph()
     try:
-        local_g = load_graphs(f'{folder}/part{part_id}/graph.dgl')[0][0]
-        partition_book = load_partition_book(part_config=part_config, part_id=part_id)[0]
+        local_g = load_graphs(f"{folder}/part{part_id}/graph.dgl")[0][0]
+        partition_book = load_partition_book(
+            part_config=part_config, part_id=part_id
+        )[0]
     except DGLError as e:
-        logging.fatal(f'Partition {part_id} data folder does not exist, which is required by edge types {seed_edge_tids.tolist()}.\n')
+        logging.fatal(
+            f"Partition {part_id} data folder does not exist, "
+            "which is requested by edge types {seed_edge_tids.tolist()}.\n"
+        )
         raise e
 
     ntypes, etypes = partition_book.ntypes, partition_book.etypes
@@ -77,6 +99,7 @@ def _find_c_etypes_in_partition(seed_edges, seed_edge_tids, part_id, part_config
         canonical_etypes[canonical_etypes_delimiter.join(c_etype)] = etype_id
     return canonical_etypes
 
+
 def _find_edges(local_g, partition_book, seed_edges):
     local_eids = partition_book.eid2localeid(seed_edges, partition_book.partid)
     local_src, local_dst = local_g.find_edges(local_eids)
@@ -85,21 +108,29 @@ def _find_edges(local_g, partition_book, seed_edges):
     global_dst = global_nid_mapping[local_dst]
     return global_src, global_dst
 
+
 def is_old_version(config):
     first_etype = list(config[etypes_key].keys())[0]
     etype_tuple = first_etype.split(canonical_etypes_delimiter)
     return len(etype_tuple) == 1
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert etypes in partition json config file, which are \
-        generated by old version partition code, to canonical etypes.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--part-config', type=str, help='The file of the partition config')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Change edge type in config file from format (str)"
+        " to (str,str,str), the original file will be overwritten",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--part-config", type=str, help="The file of the partition config"
+    )
     args, _ = parser.parse_known_args()
-    assert args.part_config is not None, \
-            'A user has to specify a partition configuration file with --part_config.'
-    
+    assert (
+        args.part_config is not None
+    ), "A user has to specify a partition config file with --part_config."
+
     start = time.time()
     convert_conf(args.part_config)
     end = time.time()
-    logging.info(f'elplased time in seconds: {end - start}')
+    logging.info(f"elplased time in seconds: {end - start}")
