24ac85c9bb2a28e756d251e40d1508d4ab56123a
Minjie Wang <wmjlyjemaine@gmail.com>
2022-11-02 07:42:37 +0000

    fix lint

--- python/dgl/typing.py
+++ python/dgl/typing.py
@@ -9,13 +9,15 @@ try:
 
     NData = torch.Tensor
     EData = torch.Tensor
-except:
+except ImportError:
     # When PyTorch is not available, create two class stubs to represent
     # EData and NData. TODO(minjie): remove this when DGL becomes PyTorch native.
     class NData:
+        """Type hint class to represent node data."""
         pass
 
     class EData:
+        """Type hint class to represent edge data."""
         pass
 
 
