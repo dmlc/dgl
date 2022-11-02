e09782f7ed8b9512f71a2ecb28e5c31569f90f0f
Minjie Wang <wmjlyjemaine@gmail.com>
2022-11-02 04:22:29 +0000

    black formatting

--- python/dgl/typing.py
+++ python/dgl/typing.py
@@ -1,18 +1,23 @@
 """DGL type hints"""
 
-__all__ = ['NData', 'EData', 'NType', 'EType']
+__all__ = ["NData", "EData", "NType", "EType"]
 
 from typing import Union, Tuple
 
 try:
     import torch
+
     NData = torch.Tensor
     EData = torch.Tensor
 except:
     # When PyTorch is not available, create two class stubs to represent
     # EData and NData. TODO(minjie): remove this when DGL becomes PyTorch native.
-    class NData: pass
-    class EData: pass
+    class NData:
+        pass
+
+    class EData:
+        pass
+
 
 NType = str
 EType = Union[str, Tuple[str, str, str]]
