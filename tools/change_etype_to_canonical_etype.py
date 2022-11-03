4659b031410aa9a6c26c59d06e63a64ce7f1a94e
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2022-11-03 13:45:38 +0000

    fix issue

--- tools/change_etype_to_canonical_etype.py
+++ tools/change_etype_to_canonical_etype.py
@@ -11,9 +11,11 @@ from dgl._ffi.base import DGLError
 from dgl.data.utils import load_graphs
 from dgl.utils import toindex
 
-etypes_key = "etypes"
-edge_map_key = "edge_map"
-canonical_etypes_delimiter = ":"
+ETYPES_KEY = "etypes"
+EDGE_MAP_KEY = "edge_map"
+NTYPES_KEY = "ntypes"
+NUM_PARTS_KEY = "num_parts"
+CANONICAL_ETYPE_DELIMITER = ":"
 
 
 def convert_conf(part_config):
@@ -23,23 +25,23 @@ def convert_conf(part_config):
         if is_old_version(config):
             logging.info("Changing the partition configuration file.")
             canonical_etypes = {}
-            if len(config["ntypes"]) == 1:
-                ntype = list(config["ntypes"].keys())[0]
+            if len(config[NTYPES_KEY]) == 1:
+                ntype = list(config[NTYPES_KEY].keys())[0]
                 canonical_etypes = {
-                    canonical_etypes_delimiter.join((ntype, etype, ntype)): eid
-                    for etype, eid in config[etypes_key].items()
+                    CANONICAL_ETYPE_DELIMITER.join((ntype, etype, ntype)): eid
+                    for etype, eid in config[ETYPES_KEY].items()
                 }
             else:
                 canonical_etypes = etype2canonical_etype(part_config, config)
             reverse_c_etypes = {v: k for k, v in canonical_etypes.items()}
-            # convert edge_map key from etype -> c_etype
+            # Convert edge_map keys from etype -> c_etype.
             new_edge_map = {}
-            for e_type, range in config[edge_map_key].items():
-                eid = config[etypes_key][e_type]
+            for e_type, range in config[EDGE_MAP_KEY].items():
+                eid = config[ETYPES_KEY][e_type]
                 c_etype = reverse_c_etypes[eid]
                 new_edge_map[c_etype] = range
-            config[edge_map_key] = new_edge_map
-            config[etypes_key] = canonical_etypes
+            config[EDGE_MAP_KEY] = new_edge_map
+            config[ETYPES_KEY] = canonical_etypes
             logging.info("Dumping the content to disk.")
             f.seek(0)
             json.dump(config, f, indent=4)
@@ -47,48 +49,43 @@ def convert_conf(part_config):
 
 
 def etype2canonical_etype(part_config, config):
-    try:
-        num_parts = config["num_parts"]
-        edge_map = config[edge_map_key]
-        etypes = list(edge_map.keys())
-        # get part id for each seed edges
-        partition_ids = []
-        for _, bound in edge_map.items():
-            for i in range(num_parts):
-                if bound[i][1] > bound[i][0]:
-                    partition_ids.append(i)
-                    break
-        partition_ids = torch.tensor(partition_ids)
-
-        # start index of each partition
-        shifts = []
+    num_parts = config[NUM_PARTS_KEY]
+    edge_map = config[EDGE_MAP_KEY]
+    etypes = list(edge_map.keys())
+    # Get part id of each seed edge.
+    partition_ids = []
+    for _, bound in edge_map.items():
         for i in range(num_parts):
-            shifts.append(edge_map[etypes[0]][i][0])
-        shifts = torch.tensor(shifts)
+            if bound[i][1] > bound[i][0]:
+                partition_ids.append(i)
+                break
+    partition_ids = torch.tensor(partition_ids)
 
-        canonical_etypes = {}
-        part_ids = [
-            part_id for part_id in range(num_parts) if part_id in partition_ids
+    # Get starting index of each partition.
+    shifts = []
+    for i in range(num_parts):
+        shifts.append(edge_map[etypes[0]][i][0])
+    shifts = torch.tensor(shifts)
+
+    canonical_etypes = {}
+    part_ids = [
+        part_id for part_id in range(num_parts) if part_id in partition_ids
+    ]
+    for part_id in part_ids:
+        seed_etypes = [
+            etypes[i] for i in range(len(etypes)) if partition_ids[i] == part_id
         ]
-        for part_id in part_ids:
-            seed_etypes = [
-                etypes[i]
-                for i in range(len(etypes))
-                if partition_ids[i] == part_id
-            ]
-            c_etype = _find_c_etypes_in_partition(
-                part_id,
-                seed_etypes,
-                config[etypes_key],
-                config["ntypes"],
-                edge_map,
-                shifts,
-                part_config,
-            )
-            canonical_etypes.update(c_etype)
-        return canonical_etypes
-    except KeyError as e:
-        print(e)
+        c_etype = _find_c_etypes_in_partition(
+            part_id,
+            seed_etypes,
+            config[ETYPES_KEY],
+            config[NTYPES_KEY],
+            edge_map,
+            shifts,
+            part_config,
+        )
+        canonical_etypes.update(c_etype)
+    return canonical_etypes
 
 
 def _find_c_etypes_in_partition(
@@ -113,7 +110,7 @@ def _find_c_etypes_in_partition(
         dst_ntypes = [ntypes[ntid.item()] for ntid in dst_ntids]
         c_etypes = list(zip(src_ntypes, seed_etypes, dst_ntypes))
         c_etypes = [
-            canonical_etypes_delimiter.join(c_etype) for c_etype in c_etypes
+            CANONICAL_ETYPE_DELIMITER.join(c_etype) for c_etype in c_etypes
         ]
         return {k: etypes[v] for (k, v) in zip(c_etypes, seed_etypes)}
     except DGLError as e:
@@ -124,8 +121,8 @@ def _find_c_etypes_in_partition(
 
 
 def is_old_version(config):
-    first_etype = list(config[etypes_key].keys())[0]
-    etype_tuple = first_etype.split(canonical_etypes_delimiter)
+    first_etype = list(config[ETYPES_KEY].keys())[0]
+    etype_tuple = first_etype.split(CANONICAL_ETYPE_DELIMITER)
     return len(etype_tuple) == 1
 
 
