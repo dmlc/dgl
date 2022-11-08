615a2c98b18528aeb67d09b657987bb098711875
Steve <ubuntu@ip-172-31-34-29.ap-northeast-1.compute.internal>
2022-11-08 03:14:10 +0000

    blabl

--- src/array/cpu/spmm_blocking_libxsmm.h
+++ src/array/cpu/spmm_blocking_libxsmm.h
@@ -210,31 +210,41 @@ inline libxsmm_meltwfunction_opreduce_vecs_idx SpMMCreateLibxsmmKernel(
   // the first operand. So, if we need to copy rhs, we need to set that as the
   // first operand. For rhs, we also set whether to use implicit indices or
   // provided indices.
+  // TODO(Steve): fix this long line in a separate PR.
   if (std::is_same<Op, op::CopyLhs<DType>>::value) {
-    opredop_flags |= LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_OPORDER_VECIDX_VECIN;
+    opredop_flags =
+        (libxsmm_meltw_opreduce_vecs_flags)(opredop_flags | LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_OPORDER_VECIDX_VECIN);  // NOLINT
   } else if (std::is_same<Op, op::CopyRhs<DType>>::value) {
-    opredop_flags |= LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_OPORDER_VECIN_VECIDX;
+    opredop_flags =
+        (libxsmm_meltw_opreduce_vecs_flags)(opredop_flags | LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_OPORDER_VECIN_VECIDX);  // NOLINT
     if (!has_idx) {
-      opredop_flags |= LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_IMPLICIT_INDEXED_VECIDX;
+      opredop_flags =
+          (libxsmm_meltw_opreduce_vecs_flags)(opredop_flags | LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_IMPLICIT_INDEXED_VECIDX);  // NOLINT
     }
   } else {
-    opredop_flags |= LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_OPORDER_VECIDX_VECIN;
+    opredop_flags =
+        (libxsmm_meltw_opreduce_vecs_flags)(opredop_flags | LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_OPORDER_VECIDX_VECIN);  // NOLINT
     if (has_idx) {
-      opredop_flags |= LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_INDEXED_VEC;
+      opredop_flags =
+          (libxsmm_meltw_opreduce_vecs_flags)(opredop_flags | LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_INDEXED_VEC);  // NOLINT
     } else {
-      opredop_flags |= LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_IMPLICIT_INDEXED_VEC;
+      opredop_flags =
+          (libxsmm_meltw_opreduce_vecs_flags)(opredop_flags | LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_IMPLICIT_INDEXED_VEC);  // NOLINT
     }
   }
   // Third, we set the Redop in the opredop_flags
-  opredop_flags |= redop_flag;
+  opredop_flags =
+      (libxsmm_meltw_opreduce_vecs_flags)(opredop_flags | redop_flag);
   // Fourth, in case of Cmp Redop, set whether to record argmax/argmin for
   // lhs/rhs
   if (is_cmp) {
     if (Op::use_lhs) {
-      opredop_flags |= LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_RECORD_ARGOP_OFF_VEC_0;
+      opredop_flags =
+          (libxsmm_meltw_opreduce_vecs_flags)(opredop_flags | LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_RECORD_ARGOP_OFF_VEC_0);  // NOLINT
     }
     if (Op::use_rhs) {
-      opredop_flags |= LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_RECORD_ARGOP_OFF_VEC_1;
+      opredop_flags =
+          (libxsmm_meltw_opreduce_vecs_flags)(opredop_flags | LIBXSMM_MELTW_FLAG_OPREDUCE_VECS_RECORD_ARGOP_OFF_VEC_1);  // NOLINT
     }
   }
   libxsmm_meltwfunction_opreduce_vecs_idx kernel = nullptr;
