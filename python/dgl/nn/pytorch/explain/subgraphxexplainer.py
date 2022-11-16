aae5c1412afe33da43b665a043e893e4749a2e76
kxm180046 <kxm180046@utdallas.edu>
2022-11-16 17:01:09 -0600

    example explainer code added

--- python/dgl/nn/pytorch/explain/subgraphxexplainer.py
+++ python/dgl/nn/pytorch/explain/subgraphxexplainer.py
@@ -237,6 +237,56 @@ class SubgraphXExplainer(nn.Module):
         -------
         list
             Return list of nodes that represent the most important subgraph
+
+        Examples
+        --------
+
+        >>> import dgl.function as fn
+        >>> import torch
+        >>> import torch.nn as nn
+        >>> from dgl.data import GINDataset
+        >>> from dgl.dataloading import GraphDataLoader
+        >>> from dgl.nn import AvgPooling, SubgraphXExplainer
+
+        >>> # Load dataset
+        >>> data = GINDataset('MUTAG', self_loop=True)
+        >>> dataloader = GraphDataLoader(data, batch_size=64, shuffle=True)
+
+        >>> # Define a model
+        >>> class Model(nn.Module):
+        ...     def __init__(self, in_feats, out_feats):
+        ...         super(Model, self).__init__()
+        ...         self.linear = nn.Linear(in_feats, out_feats)
+        ...         self.pool = AvgPooling()
+        ...
+        ...     def forward(self, graph, feat, eweight=None):
+        ...         with graph.local_scope():
+        ...             feat = self.linear(feat)
+        ...             graph.ndata['h'] = feat
+        ...             if eweight is None:
+        ...                 graph.update_all(fn.copy_u('h', 'm'), fn.sum('m', 'h'))
+        ...             else:
+        ...                 graph.edata['w'] = eweight
+        ...                 graph.update_all(fn.u_mul_e('h', 'w', 'm'), fn.sum('m', 'h'))
+        ...             return self.pool(graph, graph.ndata['h'])
+
+        >>> # Train the model
+        >>> feat_size = data[0][0].ndata['attr'].shape[1]
+        >>> model = Model(feat_size, data.gclasses)
+        >>> criterion = nn.CrossEntropyLoss()
+        >>> optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+        >>> for bg, labels in dataloader:
+        ...     logits = model(bg, bg.ndata['attr'])
+        ...     loss = criterion(logits, labels)
+        ...     optimizer.zero_grad()
+        ...     loss.backward()
+        ...     optimizer.step()
+
+        >>> # Explain the prediction for graph 0
+        >>> explainer = SubgraphXExplainer(model, hyperparam=6, pruning_action="high2low")
+        >>> g_nodes_explain = explainer.explain_graph(graph, M=50, N_min=6, features=features)
+        >>> g_nodes_explain
+        tensor([10, 11, 12, 13, 14])
         """
         self.value_func = self.get_value_func(features, **kwargs)
 
