2daebf0b4404aa0a9a44cf415041f819586aaf0f
Steve <ubuntu@ip-172-31-34-29.ap-northeast-1.compute.internal>
2022-11-16 09:24:13 +0000

    blabla

--- examples/sparse/sign.py
+++ examples/sparse/sign.py
@@ -75,10 +75,8 @@ if __name__ == "__main__":
 
     loss_func = nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=3e-3)
-    best_val_acc = 0
-    best_test_acc = 0
 
-    for e in range(100):
+    for e in range(10):
         # Forward.
         logits = model(X_sign).to(dev)
 
@@ -93,20 +91,12 @@ if __name__ == "__main__":
         val_acc = (pred[val_mask] == labels[val_mask]).float().mean()
         test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
 
-        # Save the best validation accuracy and the corresponding test
-        # accuracy.
-        if best_val_acc < val_acc:
-            best_val_acc = val_acc
-            best_test_acc = test_acc
-
         # Backward.
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        if e % 5 == 0:
-            print(
-                f"In epoch {e}, loss: {loss:.3f}, val acc: {val_acc:.3f} (best "
-                f"{best_val_acc:.3f}), test acc: {test_acc:.3f} (best "
-                f"{best_test_acc:.3f})"
-            )
+        print(
+            f"In epoch {e}, loss: {loss:.3f}, val acc: {val_acc:.3f}, test acc"
+            f": {test_acc:.3f}"
+        )
