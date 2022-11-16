02f47dae89b657235b27f02d4eac85e1339db17e
Steve <ubuntu@ip-172-31-34-29.ap-northeast-1.compute.internal>
2022-11-16 08:31:51 +0000

    add sign example

---
+++ examples/sparse/sign.py
@@ -0,0 +1,99 @@
+"""
+[SIGN: Scalable Inception Graph Neural Networks]
+(https://arxiv.org/abs/2004.11198)
+
+This example shows a simplified version of SIGN: a precomputed 2-hops diffusion
+operators on top of symmetrically normalized adjacency matrix A.
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from dgl.data import CoraGraphDataset
+from dgl.mock_sparse import create_from_coo, diag, identity
+from torch.optim import Adam
+
+# If CUDA is available, use GPU to accelerate the training, otherwise, use
+# CPU for the training.
+dev = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Perform the r-hops diffusion operation.
+def sign_diffusion(A, X, r):
+  X_sign = [X]
+  for _ in range(r):
+    X = A @ X
+    X_sign.append(X)
+  return X_sign
+
+class Sign(nn.Module):
+  def __init__(self, in_size, num_classes, r, hidden_size=64):
+    super().__init__()
+    self.linear = nn.ModuleList(
+            [nn.Linear(in_size, hidden_size) for _ in range(r + 1)]
+        )
+    self.pred = nn.Linear(hidden_size * (r + 1), num_classes)
+
+  def forward(self, X_sign):
+    results = []
+    for i in range(len(X_sign)):
+      results.append(self.linear[i](X_sign[i]))
+    Z = F.relu(torch.cat(results, dim=1))
+    return F.sigmoid(self.pred(Z))
+
+
+if __name__ == '__main__':
+  # Create graph from the existing dataset.
+  dataset = CoraGraphDataset()
+  g = dataset[0].to(dev)
+  
+  # Create the sparse adjacent matrix A (note that W was used as notion for
+  # adjacent matrix in the original paper).
+  src, dst = g.edges()
+  N = g.num_nodes()
+  A = create_from_coo(dst, src, shape=(N, N))
+
+  # 2-hops diffusion operators.
+  r = 2
+  X = g.ndata["feat"]
+  X_sign = sign_diffusion(A, X, r)
+  model = Sign(X.shape[1], dataset.num_classes, r)
+
+  labels = g.ndata["label"]
+  train_mask = g.ndata["train_mask"]
+  val_mask = g.ndata["val_mask"]
+  test_mask = g.ndata["test_mask"]
+
+  loss_func = nn.CrossEntropyLoss()
+  optimizer = Adam(model.parameters(), lr=3e-3)
+  best_val_acc = 0
+  best_test_acc = 0
+
+  for e in range(100):
+    # Forward.
+    logits = model(X_sign)
+
+    # Compute prediction.
+    pred = logits.argmax(1)
+
+    # Compute loss with nodes in training set.
+    loss = F.cross_entropy(logits[train_mask], labels[train_mask])
+
+    # Compute accuracy on training/validation/test
+    train_acc = (pred[train_mask] == labels[train_mask]).float().mean()
+    val_acc = (pred[val_mask] == labels[val_mask]).float().mean()
+    test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
+
+    # Save the best validation accuracy and the corresponding test
+    # accuracy.
+    if best_val_acc < val_acc:
+      best_val_acc = val_acc
+      best_test_acc = test_acc
+
+    # Backward
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if e % 5 == 0:
+      print(f"In epoch {e}, loss: {loss:.3f}, val acc: {val_acc:.3f} (best "
+      f"{best_val_acc:.3f}), test acc: {test_acc:.3f} (best {best_test_acc:.3f})")
