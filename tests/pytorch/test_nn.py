6d13349df8be2ff0f33ce587a5c5c12019ca05ea
kxm180046 <kxm180046@utdallas.edu>
2022-11-17 11:07:47 -0600

    revert test_nn.py change

--- tests/pytorch/test_nn.py
+++ tests/pytorch/test_nn.py
@@ -1559,6 +1559,20 @@ def test_hgt(idtype, in_size, num_heads):
     sorted_x = sorted_g.ndata['x']
     sorted_y = m(sorted_g, sorted_x, sorted_ntype, sorted_etype, presorted=False)
     assert sorted_y.shape == (g.num_nodes(), head_size * num_heads)
+    # mini-batch
+    train_idx = th.randint(0, 100, (10, ), dtype = idtype)
+    sampler = dgl.dataloading.NeighborSampler([-1])
+    train_loader = dgl.dataloading.DataLoader(g, train_idx.to(dev), sampler,
+                                            batch_size=8, device=dev,
+                                            shuffle=True)
+    (input_nodes, output_nodes, block) = next(iter(train_loader))
+    block = block[0]
+    x = x[input_nodes.to(th.long)]
+    ntype = ntype[input_nodes.to(th.long)]
+    edge = block.edata[dgl.EID]
+    etype = etype[edge.to(th.long)]
+    y = m(block, x, ntype, etype)
+    assert y.shape == (block.number_of_dst_nodes(), head_size * num_heads)
     # TODO(minjie): enable the following check
     #assert th.allclose(y, sorted_y[rev_idx], atol=1e-4, rtol=1e-4)
 
@@ -1762,6 +1776,26 @@ def test_DeepWalk():
     loss.backward()
     optim.step()
 
+@pytest.mark.parametrize('max_degree', [2, 6])
+@pytest.mark.parametrize('embedding_dim', [8, 16])
+@pytest.mark.parametrize('direction', ['in', 'out', 'both'])
+def test_degree_encoder(max_degree, embedding_dim, direction):
+    g = dgl.graph((
+        th.tensor([0, 0, 0, 1, 1, 2, 3, 3]),
+        th.tensor([1, 2, 3, 0, 3, 0, 0, 1])
+    ))
+    # test heterograph
+    hg = dgl.heterograph({
+        ('drug', 'interacts', 'drug'): (th.tensor([0, 1]), th.tensor([1, 2])),
+        ('drug', 'interacts', 'gene'): (th.tensor([0, 1]), th.tensor([2, 3])),
+        ('drug', 'treats', 'disease'): (th.tensor([1]), th.tensor([2]))
+    })
+    model = nn.DegreeEncoder(max_degree, embedding_dim, direction=direction)
+    de_g = model(g)
+    de_hg = model(hg)
+    assert de_g.shape == (4, embedding_dim)
+    assert de_hg.shape == (10, embedding_dim)
+
 @parametrize_idtype
 def test_MetaPath2Vec(idtype):
     dev = F.ctx()
@@ -1775,3 +1809,24 @@ def test_MetaPath2Vec(idtype):
     model = model.to(dev)
     embeds = model.node_embed.weight
     assert embeds.shape[0] == g.num_nodes()
+
+@pytest.mark.parametrize('num_layer', [1, 4])
+@pytest.mark.parametrize('k', [3, 5])
+@pytest.mark.parametrize('lpe_dim', [4, 16])
+@pytest.mark.parametrize('n_head', [1, 4])
+@pytest.mark.parametrize('batch_norm', [True, False])
+@pytest.mark.parametrize('num_post_layer', [0, 1, 2])
+def test_LaplacianPosEnc(num_layer, k, lpe_dim, n_head, batch_norm, num_post_layer):
+    ctx = F.ctx()
+    num_nodes = 4
+
+    EigVals = th.randn((num_nodes, k)).to(ctx)
+    EigVecs = th.randn((num_nodes, k)).to(ctx)
+
+    model = nn.LaplacianPosEnc("Transformer", num_layer, k, lpe_dim, n_head,
+                               batch_norm, num_post_layer).to(ctx)
+    assert model(EigVals, EigVecs).shape == (num_nodes, lpe_dim)
+
+    model = nn.LaplacianPosEnc("DeepSet", num_layer, k, lpe_dim,
+                               batch_norm=batch_norm, num_post_layer=num_post_layer).to(ctx)
+    assert model(EigVals, EigVecs).shape == (num_nodes, lpe_dim)
