179d610e78b04de7b562a52ec554ee8e147aa823
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2022-11-24 08:09:03 +0000

    code styple format

--- tests/tools/test_utils.py
+++ tests/tools/test_utils.py
@@ -1,23 +1,23 @@
-import tempfile
 import os
-import pytest
+import tempfile
+
 import numpy as np
+import pytest
 from distpartitioning import array_readwriter
 
-@pytest.mark.parametrize("shape", [[500], [300,10], [200,5,5], [100,5,5,5]])
-@pytest.mark.parametrize("format", ['numpy', 'parquet'])
+
+@pytest.mark.parametrize(
+    "shape", [[500], [300, 10], [200, 5, 5], [100, 5, 5, 5]]
+)
+@pytest.mark.parametrize("format", ["numpy", "parquet"])
 def test_array_readwriter(format, shape):
     original_arr = np.random.rand(*shape)
     fmt_meta = {"name": format}
-    
+
     with tempfile.TemporaryDirectory() as test_dir:
-        path = os.path.join(test_dir, f'nodes.{format}')
-        array_readwriter.get_array_parser(
-            **fmt_meta
-        ).write(path, original_arr)
-        arr = array_readwriter.get_array_parser(
-            **fmt_meta
-        ).read(path)
+        path = os.path.join(test_dir, f"nodes.{format}")
+        array_readwriter.get_array_parser(**fmt_meta).write(path, original_arr)
+        arr = array_readwriter.get_array_parser(**fmt_meta).read(path)
 
         assert original_arr.shape == arr.shape
         assert np.array_equal(original_arr, arr)
