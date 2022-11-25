43b3ad1710d20fe318867f0d4c43654fe71b2b82
xiny <xiny@nvidia.com>
2022-11-25 11:19:43 +0800

    update cmake and adapt to tot

--- examples/pytorch/graphsage/advanced/train_sampling_unsupervised_cache.py
+++ examples/pytorch/graphsage/advanced/train_sampling_unsupervised_cache.py
@@ -1,53 +1,50 @@
+import argparse
 import os
-import dgl
+import sys
+import time
+
 import numpy as np
 import torch as th
+import torch.multiprocessing as mp
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torch.multiprocessing as mp
-import dgl.function as fn
-import dgl.nn.pytorch as dglnn
-import time
-import argparse
+from model import SAGE
+from model import compute_acc_unsupervised as compute_acc
+from negative_sampler import NegativeSampler
 from torch.nn.parallel import DistributedDataParallel
 
-from model import SAGE, compute_acc_unsupervised as compute_acc
-from negative_sampler import NegativeSampler
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from load_graph import load_reddit, load_ogb
+import dgl
+import dgl.function as fn
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from load_graph import load_ogb, load_reddit
+
 from dgl.storages import GPUCachedTensorStorage
 
-class CachedDGLGraph(dgl.DGLGraph):
-    def __init__(self, g, node_feats, edge_feats={}):
-        super().__init__(g._graph, g._ntypes, g._etypes, g._node_frames, g._edge_frames)
-        self.node_feats = {(ntype, feat): GPUCachedTensorStorage(g.ndata[ntype][feat] if len(self.ntypes) > 1 else g.ndata[feat], size) for ntype, d in node_feats.items() for feat, size in d.items()}
-        self.edge_feats = {(etype, feat): GPUCachedTensorStorage(g.edata[etype][feat] if len(self.ntypes) > 1 else g.edata[feat], size) for etype, d in edge_feats.items() for feat, size in d.items()}
-    
-    def get_node_storage(self, key, ntype):
-        return self.node_feats.get((ntype, key), super().get_node_storage(key, ntype))
-    
-    def get_edge_storage(self, key, etype):
-        return self.edge_feats.get((etype, key), super().get_edge_storage(key, etype))
 
 class CrossEntropyLoss(nn.Module):
     def forward(self, block_outputs, pos_graph, neg_graph):
         with pos_graph.local_scope():
-            pos_graph.ndata['h'] = block_outputs
-            pos_graph.apply_edges(fn.u_dot_v('h', 'h', 'score'))
-            pos_score = pos_graph.edata['score']
+            pos_graph.ndata["h"] = block_outputs
+            pos_graph.apply_edges(fn.u_dot_v("h", "h", "score"))
+            pos_score = pos_graph.edata["score"]
         with neg_graph.local_scope():
-            neg_graph.ndata['h'] = block_outputs
-            neg_graph.apply_edges(fn.u_dot_v('h', 'h', 'score'))
-            neg_score = neg_graph.edata['score']
+            neg_graph.ndata["h"] = block_outputs
+            neg_graph.apply_edges(fn.u_dot_v("h", "h", "score"))
+            neg_score = neg_graph.edata["score"]
 
         score = th.cat([pos_score, neg_score])
-        label = th.cat([th.ones_like(pos_score), th.zeros_like(neg_score)]).long()
+        label = th.cat(
+            [th.ones_like(pos_score), th.zeros_like(neg_score)]
+        ).long()
         loss = F.binary_cross_entropy_with_logits(score, label.float())
         return loss
 
-def evaluate(model, g, nfeat, labels, train_nids, val_nids, test_nids, device, args):
+
+def evaluate(
+    model, g, nfeat, labels, train_nids, val_nids, test_nids, device, args
+):
     """
     Evaluate the model on the validation set specified by ``val_mask``.
     g : The entire graph.
@@ -60,12 +57,17 @@ def evaluate(model, g, nfeat, labels, train_nids, val_nids, test_nids, device, a
     with th.no_grad():
         # single gpu
         if isinstance(model, SAGE):
-            pred = model.inference(g, nfeat, device, args.batch_size, args.num_workers)
+            pred = model.inference(
+                g, nfeat, device, args.batch_size, args.num_workers
+            )
         # multi gpu
         else:
-            pred = model.module.inference(g, nfeat, device, args.batch_size, args.num_workers)
+            pred = model.module.inference(
+                g, nfeat, device, args.batch_size, args.num_workers
+            )
     model.train()
     return compute_acc(pred, labels, train_nids, val_nids, test_nids)
+
 
 #### Entry point
 def run(proc_id, n_gpus, args, devices, data):
@@ -74,16 +76,17 @@ def run(proc_id, n_gpus, args, devices, data):
     if n_gpus > 0:
         th.cuda.set_device(device)
     if n_gpus > 1:
-        dist_init_method = 'tcp://{master_ip}:{master_port}'.format(
-            master_ip='127.0.0.1', master_port='12345')
+        dist_init_method = "tcp://{master_ip}:{master_port}".format(
+            master_ip="127.0.0.1", master_port="12345"
+        )
         world_size = n_gpus
-        th.distributed.init_process_group(backend="nccl",
-                                          init_method=dist_init_method,
-                                          world_size=world_size,
-                                          rank=proc_id)
+        th.distributed.init_process_group(
+            backend="nccl",
+            init_method=dist_init_method,
+            world_size=world_size,
+            rank=proc_id,
+        )
     train_nid, val_nid, test_nid, n_classes, g, nfeat, labels = data
-    g.ndata['features'] = nfeat
-    g = CachedDGLGraph(g, {g._ntypes[0]: {'features': 1000000}})
 
     in_feats = nfeat.shape[1]
 
@@ -91,41 +94,65 @@ def run(proc_id, n_gpus, args, devices, data):
     n_edges = g.num_edges()
     train_seeds = th.arange(n_edges)
 
-    if args.graph_device == 'gpu':
+    if args.graph_device == "gpu":
         train_seeds = train_seeds.to(device)
         g = g.to(device)
         args.num_workers = 0
-    elif args.graph_device == 'uva':
+    elif args.graph_device == "uva":
         train_seeds = train_seeds.to(device)
         g.pin_memory_()
         args.num_workers = 0
 
     # Create sampler
     sampler = dgl.dataloading.NeighborSampler(
-        [int(fanout) for fanout in args.fan_out.split(',')], prefetch_node_feats=['features'])
+        [int(fanout) for fanout in args.fan_out.split(",")],
+        prefetch_node_feats=["features"],
+    )
     sampler = dgl.dataloading.as_edge_prediction_sampler(
-        sampler, exclude='reverse_id',
+        sampler,
+        exclude="reverse_id",
         # For each edge with ID e in Reddit dataset, the reverse edge is e ± |E|/2.
-        reverse_eids=th.cat([
-            th.arange(n_edges // 2, n_edges),
-            th.arange(0, n_edges // 2)]).to(train_seeds),
-        negative_sampler=NegativeSampler(g, args.num_negs, args.neg_share,
-                                         device if args.graph_device == 'uva' else None))
+        reverse_eids=th.cat(
+            [th.arange(n_edges // 2, n_edges), th.arange(0, n_edges // 2)]
+        ).to(train_seeds),
+        negative_sampler=NegativeSampler(
+            g,
+            args.num_negs,
+            args.neg_share,
+            device if args.graph_device == "uva" else None,
+        ),
+    )
     dataloader = dgl.dataloading.EdgeDataLoader(
-        g, train_seeds, sampler,
+        g,
+        train_seeds,
+        sampler,
         device=device,
         use_ddp=n_gpus > 1,
         batch_size=args.batch_size,
         shuffle=True,
         drop_last=False,
         num_workers=args.num_workers,
-        use_uva=args.graph_device == 'uva')
+        use_uva=args.graph_device == "uva",
+    )
+
+    dataloader.attach_ndata(
+        "features", GPUCachedTensorStorage(nfeat, args.cache_size)
+    )
 
     # Define model and optimizer
-    model = SAGE(in_feats, args.num_hidden, args.num_hidden, args.num_layers, F.relu, args.dropout)
+    model = SAGE(
+        in_feats,
+        args.num_hidden,
+        args.num_hidden,
+        args.num_layers,
+        F.relu,
+        args.dropout,
+    )
     model = model.to(device)
     if n_gpus > 1:
-        model = DistributedDataParallel(model, device_ids=[device], output_device=device)
+        model = DistributedDataParallel(
+            model, device_ids=[device], output_device=device
+        )
     loss_fcn = CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
@@ -143,9 +170,11 @@ def run(proc_id, n_gpus, args, devices, data):
         # Loop over the dataloader to sample the computation dependency graph as a list of
         # blocks.
         tic_step = time.time()
-        for step, (input_nodes, pos_graph, neg_graph, blocks) in enumerate(dataloader):
+        for step, (input_nodes, pos_graph, neg_graph, blocks) in enumerate(
+            dataloader
+        ):
             input_nodes = input_nodes.to(device)
-            batch_inputs = blocks[0].srcdata['features']
+            batch_inputs = blocks[0].srcdata["features"]
             blocks = [block.int() for block in blocks]
             d_step = time.time()
 
@@ -164,48 +193,80 @@ def run(proc_id, n_gpus, args, devices, data):
             iter_d.append(d_step - tic_step)
             iter_t.append(t - d_step)
             if step % args.log_every == 0 and proc_id == 0:
-                gpu_mem_alloc = th.cuda.max_memory_allocated() / 1000000 if th.cuda.is_available() else 0
-                print('[{}]Epoch {:05d} | Step {:05d} | Loss {:.4f} | Speed (samples/sec) {:.4f}|{:.4f} | Load {:.4f}| train {:.4f} | GPU {:.1f} MB'.format(
-                    proc_id, epoch, step, loss.item(), np.mean(iter_pos[3:]), np.mean(iter_neg[3:]), np.mean(iter_d[3:]), np.mean(iter_t[3:]), gpu_mem_alloc))
+                gpu_mem_alloc = (
+                    th.cuda.max_memory_allocated() / 1000000
+                    if th.cuda.is_available()
+                    else 0
+                )
+                print(
+                    "[{}]Epoch {:05d} | Step {:05d} | Loss {:.4f} | Speed (samples/sec) {:.4f}|{:.4f} | Load {:.4f}| train {:.4f} | GPU {:.1f} MB".format(
+                        proc_id,
+                        epoch,
+                        step,
+                        loss.item(),
+                        np.mean(iter_pos[3:]),
+                        np.mean(iter_neg[3:]),
+                        np.mean(iter_d[3:]),
+                        np.mean(iter_t[3:]),
+                        gpu_mem_alloc,
+                    )
+                )
             tic_step = time.time()
 
         toc = time.time()
         if proc_id == 0:
-            print('Epoch Time(s): {:.4f}'.format(toc - tic))
+            print("Epoch Time(s): {:.4f}".format(toc - tic))
             if epoch >= 5:
                 avg += toc - tic
             if (epoch + 1) % args.eval_every == 0:
-                eval_acc, test_acc = evaluate(model, g, nfeat, labels, train_nid, val_nid, test_nid, device, args)
-                print('Eval Acc {:.4f} Test Acc {:.4f}'.format(eval_acc, test_acc))
+                eval_acc, test_acc = evaluate(
+                    model,
+                    g,
+                    nfeat,
+                    labels,
+                    train_nid,
+                    val_nid,
+                    test_nid,
+                    device,
+                    args,
+                )
+                print(
+                    "Eval Acc {:.4f} Test Acc {:.4f}".format(eval_acc, test_acc)
+                )
                 if eval_acc > best_eval_acc:
                     best_eval_acc = eval_acc
                     best_test_acc = test_acc
-                print('Best Eval Acc {:.4f} Test Acc {:.4f}'.format(best_eval_acc, best_test_acc))
+                print(
+                    "Best Eval Acc {:.4f} Test Acc {:.4f}".format(
+                        best_eval_acc, best_test_acc
+                    )
+                )
 
         if n_gpus > 1:
             th.distributed.barrier()
 
     if proc_id == 0:
-        print('Avg epoch time: {}'.format(avg / (epoch - 4)))
+        print("Avg epoch time: {}".format(avg / (epoch - 4)))
+
 
 def main(args):
-    devices = list(map(int, args.gpu.split(',')))
+    devices = list(map(int, args.gpu.split(",")))
     n_gpus = len(devices)
 
     # load dataset
-    if args.dataset == 'reddit':
+    if args.dataset == "reddit":
         g, n_classes = load_reddit(self_loop=False)
-    elif args.dataset == 'ogbn-products':
-        g, n_classes = load_ogb('ogbn-products')
+    elif args.dataset == "ogbn-products":
+        g, n_classes = load_ogb("ogbn-products")
     else:
-        raise Exception('unknown dataset')
+        raise Exception("unknown dataset")
 
-    train_nid = g.ndata.pop('train_mask').nonzero().squeeze()
-    val_nid = g.ndata.pop('val_mask').nonzero().squeeze()
-    test_nid = g.ndata.pop('test_mask').nonzero().squeeze()
+    train_nid = g.ndata.pop("train_mask").nonzero().squeeze()
+    val_nid = g.ndata.pop("val_mask").nonzero().squeeze()
+    test_nid = g.ndata.pop("test_mask").nonzero().squeeze()
 
-    nfeat = g.ndata.pop('features')
-    labels = g.ndata.pop('labels')
+    nfeat = g.ndata.pop("features")
+    labels = g.ndata.pop("labels")
 
     # Create csr/coo/csc formats before launching training processes with multi-gpu.
     # This avoids creating certain formats in each sub-process, which saves memory and CPU.
@@ -213,55 +274,81 @@ def main(args):
 
     # this to avoid competition overhead on machines with many cores.
     # Change it to a proper number on your machine, especially for multi-GPU training.
-    os.environ['OMP_NUM_THREADS'] = str(mp.cpu_count() // 2 // n_gpus)
+    os.environ["OMP_NUM_THREADS"] = str(mp.cpu_count() // 2 // n_gpus)
 
     # Pack data
     data = train_nid, val_nid, test_nid, n_classes, g, nfeat, labels
 
     if devices[0] == -1:
-        assert args.graph_device == 'cpu', \
-               f"Must have GPUs to enable {args.graph_device} sampling."
-        assert args.data_device == 'cpu', \
-               f"Must have GPUs to enable {args.data_device} feature storage."
-        run(0, 0, args, ['cpu'], data)
+        assert (
+            args.graph_device == "cpu"
+        ), f"Must have GPUs to enable {args.graph_device} sampling."
+        assert (
+            args.data_device == "cpu"
+        ), f"Must have GPUs to enable {args.data_device} feature storage."
+        run(0, 0, args, ["cpu"], data)
     elif n_gpus == 1:
         run(0, n_gpus, args, devices, data)
     else:
         mp.spawn(run, args=(n_gpus, args, devices, data), nprocs=n_gpus)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     argparser = argparse.ArgumentParser("multi-gpu training")
-    argparser.add_argument("--gpu", type=str, default='0',
-                           help="GPU, can be a list of gpus for multi-gpu training,"
-                                " e.g., 0,1,2,3; -1 for CPU")
-    argparser.add_argument('--dataset', type=str, default='reddit',
-                           choices=('reddit', 'ogbn-products'))
-    argparser.add_argument('--num-epochs', type=int, default=20)
-    argparser.add_argument('--num-hidden', type=int, default=16)
-    argparser.add_argument('--num-layers', type=int, default=2)
-    argparser.add_argument('--num-negs', type=int, default=1)
-    argparser.add_argument('--neg-share', default=False, action='store_true',
-                           help="sharing neg nodes for positive nodes")
-    argparser.add_argument('--fan-out', type=str, default='10,25')
-    argparser.add_argument('--batch-size', type=int, default=10000)
-    argparser.add_argument('--log-every', type=int, default=20)
-    argparser.add_argument('--eval-every', type=int, default=5)
-    argparser.add_argument('--lr', type=float, default=0.003)
-    argparser.add_argument('--dropout', type=float, default=0.5)
-    argparser.add_argument('--num-workers', type=int, default=0,
-                           help="Number of sampling processes. Use 0 for no extra process.")
-    argparser.add_argument('--graph-device', choices=('cpu', 'gpu', 'uva'), default='cpu',
-                           help="Device to perform the sampling. "
-                                "Must have 0 workers for 'gpu' and 'uva'")
-    argparser.add_argument('--data-device', choices=('cpu', 'gpu', 'uva'), default='gpu',
-                           help="By default the script puts all node features and labels "
-                                "on GPU when using it to save time for data copy. This may "
-                                "be undesired if they cannot fit in GPU memory at once. "
-                                "Use 'cpu' to keep the features on host memory and "
-                                "'uva' to enable UnifiedTensor (GPU zero-copy access on "
-                                "pinned host memory).")
-    argparser.add_argument('--cache-size', type=int, default=0)
+    argparser.add_argument(
+        "--gpu",
+        type=str,
+        default="0",
+        help="GPU, can be a list of gpus for multi-gpu training,"
+        " e.g., 0,1,2,3; -1 for CPU",
+    )
+    argparser.add_argument(
+        "--dataset",
+        type=str,
+        default="reddit",
+        choices=("reddit", "ogbn-products"),
+    )
+    argparser.add_argument("--num-epochs", type=int, default=20)
+    argparser.add_argument("--num-hidden", type=int, default=16)
+    argparser.add_argument("--num-layers", type=int, default=2)
+    argparser.add_argument("--num-negs", type=int, default=1)
+    argparser.add_argument(
+        "--neg-share",
+        default=False,
+        action="store_true",
+        help="sharing neg nodes for positive nodes",
+    )
+    argparser.add_argument("--fan-out", type=str, default="10,25")
+    argparser.add_argument("--batch-size", type=int, default=10000)
+    argparser.add_argument("--log-every", type=int, default=20)
+    argparser.add_argument("--eval-every", type=int, default=5)
+    argparser.add_argument("--lr", type=float, default=0.003)
+    argparser.add_argument("--dropout", type=float, default=0.5)
+    argparser.add_argument(
+        "--num-workers",
+        type=int,
+        default=0,
+        help="Number of sampling processes. Use 0 for no extra process.",
+    )
+    argparser.add_argument(
+        "--graph-device",
+        choices=("cpu", "gpu", "uva"),
+        default="cpu",
+        help="Device to perform the sampling. "
+        "Must have 0 workers for 'gpu' and 'uva'",
+    )
+    argparser.add_argument(
+        "--data-device",
+        choices=("cpu", "gpu", "uva"),
+        default="gpu",
+        help="By default the script puts all node features and labels "
+        "on GPU when using it to save time for data copy. This may "
+        "be undesired if they cannot fit in GPU memory at once. "
+        "Use 'cpu' to keep the features on host memory and "
+        "'uva' to enable UnifiedTensor (GPU zero-copy access on "
+        "pinned host memory).",
+    )
+    argparser.add_argument("--cache-size", type=int, default=0)
     args = argparser.parse_args()
 
     main(args)
--- src/runtime/cuda/gpu_cache.cu
+++ src/runtime/cuda/gpu_cache.cu
@@ -20,17 +20,18 @@
 #ifndef DGL_RUNTIME_CUDA_GPU_CACHE_H_
 #define DGL_RUNTIME_CUDA_GPU_CACHE_H_
 
-#include <dgl/packed_func_ext.h>
-#include <dgl/runtime/registry.h>
-#include <dgl/runtime/object.h>
+#include <cuda_runtime.h>
 #include <dgl/array.h>
 #include <dgl/aten/array_ops.h>
+#include <dgl/packed_func_ext.h>
 #include <dgl/runtime/container.h>
 #include <dgl/runtime/device_api.h>
-#include <cuda_runtime.h>
-#include "../../runtime/cuda/cuda_common.h"
+#include <dgl/runtime/object.h>
+#include <dgl/runtime/registry.h>
 
 #include <nv_gpu_cache.hpp>
+
+#include "../../runtime/cuda/cuda_common.h"
 
 namespace dgl {
 namespace runtime {
@@ -43,120 +44,123 @@ class GpuCache : public runtime::Object {
   constexpr static int bucket_size = WARP_SIZE * set_associativity;
 
  public:
-  static constexpr const char* _type_key =
+  static constexpr const char *_type_key =
       sizeof(key_t) == 4 ? "cuda.GpuCache32" : "cuda.GpuCache64";
   DGL_DECLARE_OBJECT_TYPE_INFO(GpuCache, Object);
 
-  GpuCache(size_t num_items, size_t num_feats) : num_feats(num_feats),
-      cache((num_items + bucket_size - 1) / bucket_size, num_feats) {}
+  GpuCache(size_t num_items, size_t num_feats)
+      : num_feats(num_feats),
+        cache((num_items + bucket_size - 1) / bucket_size, num_feats) {}
 
   std::tuple<NDArray, IdArray, IdArray> Query(IdArray keys) {
-    const auto& ctx = keys->ctx;
-    cudaStream_t stream = dgl::runtime::CUDAThreadEntry::ThreadLocal()->stream;
+    const auto &ctx = keys->ctx;
+    cudaStream_t stream = dgl::runtime::getCurrentCUDAStream();
     auto device = dgl::runtime::DeviceAPI::Get(ctx);
-    CHECK_EQ(keys->ndim, 1) << "The tensor of requested indices must be of dimension one.";
-    NDArray values = NDArray::Empty({keys->shape[0], (int64_t)num_feats},
-                                    DLDataType{kDLFloat, 32, 1}, ctx);
+    CHECK_EQ(keys->ndim, 1)
+        << "The tensor of requested indices must be of dimension one.";
+    NDArray values = NDArray::Empty(
+        {keys->shape[0], (int64_t)num_feats}, DGLDataType{kDGLFloat, 32, 1},
+        ctx);
     IdArray missing_index = aten::NewIdArray(keys->shape[0], ctx, 64);
-    IdArray missing_keys = aten::NewIdArray(keys->shape[0], ctx, sizeof(key_t) * 8);
-    size_t * missing_len = static_cast<size_t *>(
-        device->AllocWorkspace(ctx, sizeof(size_t)));
+    IdArray missing_keys =
+        aten::NewIdArray(keys->shape[0], ctx, sizeof(key_t) * 8);
+    size_t *missing_len =
+        static_cast<size_t *>(device->AllocWorkspace(ctx, sizeof(size_t)));
     cache.Query(
-        static_cast<const key_t *>(keys->data),
-        keys->shape[0],
+        static_cast<const key_t *>(keys->data), keys->shape[0],
         static_cast<float *>(values->data),
         static_cast<uint64_t *>(missing_index->data),
-        static_cast<key_t *>(missing_keys->data),
-        missing_len, stream);
+        static_cast<key_t *>(missing_keys->data), missing_len, stream);
     size_t missing_len_host;
-    device->CopyDataFromTo(missing_len, 0, &missing_len_host, 0,
-        sizeof(missing_len_host),
-        ctx,
-        DGLContext{kDLCPU, 0},
-        keys->dtype,
-        stream);
+    device->CopyDataFromTo(
+        missing_len, 0, &missing_len_host, 0, sizeof(missing_len_host), ctx,
+        DGLContext{kDGLCPU, 0}, keys->dtype);
     device->FreeWorkspace(ctx, missing_len);
-    missing_index = missing_index.CreateView({(int64_t)missing_len_host}, missing_index->dtype);
-    missing_keys = missing_keys.CreateView({(int64_t)missing_len_host}, keys->dtype);
+    missing_index = missing_index.CreateView(
+        {(int64_t)missing_len_host}, missing_index->dtype);
+    missing_keys =
+        missing_keys.CreateView({(int64_t)missing_len_host}, keys->dtype);
     return std::make_tuple(values, missing_index, missing_keys);
   }
 
   void Replace(IdArray keys, NDArray values) {
-    cudaStream_t stream = dgl::runtime::CUDAThreadEntry::ThreadLocal()->stream;
-    CHECK_EQ(keys->shape[0], values->shape[0]) \
+    cudaStream_t stream = dgl::runtime::getCurrentCUDAStream();
+    CHECK_EQ(keys->shape[0], values->shape[0])
         << "First dimensions of keys and values must match";
     CHECK_EQ(values->shape[1], num_feats) << "Embedding dimension must match";
     cache.Replace(
-        static_cast<const key_t *>(keys->data),
-        keys->shape[0],
-        static_cast<const float *>(values->data),
-        stream);
+        static_cast<const key_t *>(keys->data), keys->shape[0],
+        static_cast<const float *>(values->data), stream);
   }
 
  private:
   size_t num_feats;
-  gpu_cache::gpu_cache<key_t, uint64_t, std::numeric_limits<key_t>::max(),
-                       set_associativity, WARP_SIZE> cache;
+  gpu_cache::gpu_cache<
+      key_t, uint64_t, std::numeric_limits<key_t>::max(), set_associativity,
+      WARP_SIZE>
+      cache;
 };
 
 DGL_DEFINE_OBJECT_REF(GpuCacheRef32, GpuCache<unsigned int>);
-DGL_DEFINE_OBJECT_REF(GpuCacheRef64, GpuCache<long long>); // NOLINT
+DGL_DEFINE_OBJECT_REF(GpuCacheRef64, GpuCache<long long>);  // NOLINT
 
 /* CAPI **********************************************************************/
 
 using namespace dgl::runtime;
 
 DGL_REGISTER_GLOBAL("cuda._CAPI_DGLGpuCacheCreate")
-.set_body([] (DGLArgs args, DGLRetValue* rv) {
-  const size_t num_items = args[0];
-  const size_t num_feats = args[1];
-  const int num_bits = args[2];
+    .set_body([](DGLArgs args, DGLRetValue *rv) {
+      const size_t num_items = args[0];
+      const size_t num_feats = args[1];
+      const int num_bits = args[2];
 
-  if (num_bits == 32)
-    *rv = GpuCacheRef32(std::make_shared<GpuCache<unsigned int>>(num_items, num_feats));
-  else
-    *rv = GpuCacheRef64(std::make_shared<GpuCache<long long>>(num_items, num_feats)); // NOLINT
-});
+      if (num_bits == 32)
+        *rv = GpuCacheRef32(
+            std::make_shared<GpuCache<unsigned int>>(num_items, num_feats));
+      else
+        *rv = GpuCacheRef64(std::make_shared<GpuCache<long long>>(
+            num_items, num_feats));  // NOLINT
+    });
 
 DGL_REGISTER_GLOBAL("cuda._CAPI_DGLGpuCacheQuery")
-.set_body([] (DGLArgs args, DGLRetValue* rv) {
-  IdArray keys = args[1];
+    .set_body([](DGLArgs args, DGLRetValue *rv) {
+      IdArray keys = args[1];
 
-  List<ObjectRef> ret;
-  if (keys->dtype.bits == 32) {
-    GpuCacheRef32 cache = args[0];
-    auto result = cache->Query(keys);
+      List<ObjectRef> ret;
+      if (keys->dtype.bits == 32) {
+        GpuCacheRef32 cache = args[0];
+        auto result = cache->Query(keys);
 
-    ret.push_back(Value(MakeValue(std::get<0>(result))));
-    ret.push_back(Value(MakeValue(std::get<1>(result))));
-    ret.push_back(Value(MakeValue(std::get<2>(result))));
-  } else {
-    GpuCacheRef64 cache = args[0];
-    auto result = cache->Query(keys);
+        ret.push_back(Value(MakeValue(std::get<0>(result))));
+        ret.push_back(Value(MakeValue(std::get<1>(result))));
+        ret.push_back(Value(MakeValue(std::get<2>(result))));
+      } else {
+        GpuCacheRef64 cache = args[0];
+        auto result = cache->Query(keys);
 
-    ret.push_back(Value(MakeValue(std::get<0>(result))));
-    ret.push_back(Value(MakeValue(std::get<1>(result))));
-    ret.push_back(Value(MakeValue(std::get<2>(result))));
-  }
+        ret.push_back(Value(MakeValue(std::get<0>(result))));
+        ret.push_back(Value(MakeValue(std::get<1>(result))));
+        ret.push_back(Value(MakeValue(std::get<2>(result))));
+      }
 
-  *rv = ret;
-});
+      *rv = ret;
+    });
 
 DGL_REGISTER_GLOBAL("cuda._CAPI_DGLGpuCacheReplace")
-.set_body([] (DGLArgs args, DGLRetValue* rv) {
-  IdArray keys = args[1];
-  NDArray values = args[2];
+    .set_body([](DGLArgs args, DGLRetValue *rv) {
+      IdArray keys = args[1];
+      NDArray values = args[2];
 
-  if (keys->dtype.bits == 32) {
-    GpuCacheRef32 cache = args[0];
-    cache->Replace(keys, values);
-  } else {
-    GpuCacheRef64 cache = args[0];
-    cache->Replace(keys, values);
-  }
+      if (keys->dtype.bits == 32) {
+        GpuCacheRef32 cache = args[0];
+        cache->Replace(keys, values);
+      } else {
+        GpuCacheRef64 cache = args[0];
+        cache->Replace(keys, values);
+      }
 
-  *rv = List<ObjectRef>{};
-});
+      *rv = List<ObjectRef>{};
+    });
 
 }  // namespace cuda
 }  // namespace runtime
