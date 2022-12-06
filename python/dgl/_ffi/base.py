b593c6fe8ea784002eb27308d402b3626ab8803a
xiny <xiny@nvidia.com>
2022-12-06 11:22:03 +0800

    turn off use_alternate_stream when tensoradaptor is not enabled

--- python/dgl/_ffi/base.py
+++ python/dgl/_ffi/base.py
@@ -148,3 +148,7 @@ def load_tensor_adapter(backend, version):
     if not tensor_adapter_loaded:
         logger = logging.getLogger("dgl-core")
         logger.debug("Memory optimization with PyTorch is not enabled.")
+
+
+def is_tensor_adaptor_enabled() -> bool:
+    return tensor_adapter_loaded
