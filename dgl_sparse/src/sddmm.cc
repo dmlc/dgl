a117810f0afda81de429cc274693afa79d349fcf
czkkkkkk <zekucai@gmail.com>
2022-12-20 09:06:52 +0000

    Minor fix

--- dgl_sparse/src/sddmm.cc
+++ dgl_sparse/src/sddmm.cc
@@ -27,19 +27,23 @@ class SDDMMAutoGrad : public Function<SDDMMAutoGrad> {
 void _SDDMMSanityCheck(
     const c10::intrusive_ptr<SparseMatrix>& sparse_mat, torch::Tensor mat1,
     torch::Tensor mat2) {
-  CHECK_EQ(mat1.dim(), 2)
-      << "SDDMM: the first dense matrix should be two-dimensional.";
-  CHECK_EQ(mat2.dim(), 2)
-      << "SDDMM: the second dense matrix should be two-dimensional.";
+  const int64_t mat1_dim = mat1.dim();
+  const int64_t mat2_dim = mat2.dim();
+  CHECK_EQ(mat1_dim, mat2_dim)
+      << "SDDMM: the two dense matrices should have the same dimensions.";
+  CHECK_LE(mat1_dim, 2)
+      << "SDDMM: the first dense matrix should have at most two dimensions.";
   CHECK_EQ(sparse_mat->shape()[0], mat1.size(0))
       << "SDDMM: the first dense matrix should have the same first dimension "
          "as the sparse matrix";
-  CHECK_EQ(sparse_mat->shape()[1], mat2.size(1))
-      << "SDDMM: the second dense matrix should have the same second dimension "
+  CHECK_EQ(sparse_mat->shape()[1], mat2.size(mat2_dim - 1))
+      << "SDDMM: the second dense matrix should have the same last dimension "
          "as the sparse matrix";
-  CHECK_EQ(mat1.size(1), mat2.size(0))
-      << "SDDMM: the second dimension of the first dense matrix should be "
-         "equal to the first dimension of the second dense matrix.";
+  if (mat1_dim == 2) {
+    CHECK_EQ(mat1.size(1), mat2.size(0))
+        << "SDDMM: the second dimension of the first dense matrix should be "
+           "equal to the first dimension of the second dense matrix.";
+  }
   CHECK_EQ(mat1.dtype(), mat2.dtype())
       << "SDDMM: the two dense matrices should have the same dtype.";
   CHECK_EQ(mat1.device(), mat2.device())
@@ -89,11 +93,11 @@ tensor_list SDDMMAutoGrad::backward(
 c10::intrusive_ptr<SparseMatrix> SDDMM(
     const c10::intrusive_ptr<SparseMatrix>& sparse_mat, torch::Tensor mat1,
     torch::Tensor mat2) {
-  _SDDMMSanityCheck(sparse_mat, mat1, mat2);
   if (mat1.dim() == 1) {
     mat1 = mat1.view({mat1.size(0), 1});
     mat2 = mat2.view({1, mat2.size(0)});
   }
+  _SDDMMSanityCheck(sparse_mat, mat1, mat2);
   auto val = SDDMMAutoGrad::apply(sparse_mat, mat1, mat2);
   val = val * sparse_mat->value();
   return CreateValLike(sparse_mat, val);
