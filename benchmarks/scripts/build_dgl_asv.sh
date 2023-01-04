0d9a4196ed99af59a8a59cd78c9d6570ab9b22cd
RhettYing <rhett_ying@qq.com>
2023-01-04 06:33:57 +0000

    update build arguments

--- benchmarks/scripts/build_dgl_asv.sh
+++ benchmarks/scripts/build_dgl_asv.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-# . /opt/conda/etc/profile.d/conda.sh
-# conda activate pytorch-ci
 # Default building only with cpu
 DEVICE=${DGL_BENCH_DEVICE:-cpu}
 
@@ -11,10 +9,9 @@ pip install -r /asv/torch_gpu_pip.txt
 pip install pandas rdflib ogb
 
 # build
-if [[ $DEVICE == "cpu" ]]; then
-    CMAKE_VARS=""
-else
-    CMAKE_VARS="-DUSE_CUDA=ON"
+CMAKE_VARS="-DUSE_OPENMP=ON"
+if [[ $DEVICE == "gpu" ]]; then
+    CMAKE_VARS="-DUSE_CUDA=ON -DUSE_NCCL=ON $CMAKE_VARS"
 fi
 arch=`uname -m`
 if [[ $arch == *"x86"* ]]; then
@@ -25,5 +22,3 @@ pushd build
 cmake -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda -DBUILD_TORCH=ON $CMAKE_VARS ..
 make -j
 popd
-
-# conda deactivate
