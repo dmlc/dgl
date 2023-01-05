bb58b0145e9f89c26eee6df814d97b11afd891a4
RhettYing <rhett_ying@qq.com>
2023-01-05 02:58:41 +0000

    update build

--- benchmarks/scripts/build_dgl_asv.sh
+++ benchmarks/scripts/build_dgl_asv.sh
@@ -6,10 +6,9 @@ set -e
 DEVICE=${DGL_BENCH_DEVICE:-cpu}
 
 pip install -r /asv/torch_gpu_pip.txt
-pip install pandas rdflib ogb
 
 # build
-CMAKE_VARS="-DUSE_OPENMP=ON -DBUILD_TORCH=ON -DBUILD_SPARSE=ON -DTORCH_PYTHON_INTERPS=/opt/conda/envs/bin/python"
+CMAKE_VARS="-DUSE_OPENMP=ON -DBUILD_TORCH=ON -DBUILD_SPARSE=ON -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda"
 if [[ $DEVICE == "gpu" ]]; then
     CMAKE_VARS="-DUSE_CUDA=ON -DUSE_NCCL=ON $CMAKE_VARS"
 fi
@@ -19,6 +18,6 @@ if [[ $arch == *"x86"* ]]; then
 fi
 mkdir -p build
 pushd build
-cmake -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda -DBUILD_TORCH=ON $CMAKE_VARS ..
-make -j
+cmake $CMAKE_VARS ..
+make -j8
 popd
