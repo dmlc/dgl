ab0c0ec68f73680cb224526b2d29f2e29a936138
peizhou001 <110809584+peizhou001@users.noreply.github.com>
2023-01-06 14:45:52 +0800

    [API Deprecation] Deprecate candidates in convert module (#4988) (#5115)

--- python/dgl/nn/mxnet/conv/nnconv.py
+++ python/dgl/nn/mxnet/conv/nnconv.py
@@ -75,7 +75,7 @@ class NNConv(nn.Block):
     >>> # Case 2: Unidirectional bipartite graph
     >>> u = [0, 1, 0, 0, 1]
     >>> v = [0, 1, 2, 3, 2]
-    >>> g = dgl.bipartite((u, v))
+    >>> g = dgl.heterograph({('_N', '_E', '_N'):(u, v)})
     >>> u_feat = mx.nd.random.randn(2, 10)
     >>> v_feat = mx.nd.random.randn(4, 10)
     >>> conv = NNConv(10, 2, edge_func, 'mean')
