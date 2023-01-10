91ef37f3a13eae0168873c4502aaa87c7d025a2e
RhettYing <rhett_ying@qq.com>
2023-01-10 00:55:18 +0000

    update gpu_count

--- benchmarks/benchmarks/utils.py
+++ benchmarks/benchmarks/utils.py
@@ -534,7 +534,9 @@ def skip_if_not_4gpu():
     """skip if DGL_BENCH_DEVICE is gpu"""
 
     def _wrapper(func):
-        if GPU_COUNT < 4:
+        gpu_counts = torch.cuda.device_count()
+        print(f"----------- gpu_counts: {gpu_counts}")
+        if gpu_counts < 4:
             # skip if not enabled
             print("Skip {}".format(func.__name__))
             func.benchmark_name = "skip_" + func.__name__
