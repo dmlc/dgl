15a2f7e6a1092e4ed32ac118ff8edb60c4e45975
Chang Liu <chang.liu@utexas.edu>
2023-01-10 16:46:55 -0800

    Fix lint

--- tests/compute/test_data.py
+++ tests/compute/test_data.py
@@ -1,18 +1,18 @@
 import gzip
-import tarfile
-import os
 import io
+import os
+import tarfile
 import tempfile
 import unittest
 
 import backend as F
+
+import dgl
+import dgl.data as data
 import numpy as np
 import pandas as pd
 import pytest
 import yaml
-
-import dgl
-import dgl.data as data
 from dgl import DGLError
 
 
@@ -386,14 +386,15 @@ def test_extract_archive():
         tar_file = "tar_archive"
         tar_path = os.path.join(src_dir, tar_file + ".tar")
         # default encode to utf8
-        content = 'test extract archive tar\n'.encode()
-        info = tarfile.TarInfo(name='tar_archive')
+        content = "test extract archive tar\n".encode()
+        info = tarfile.TarInfo(name="tar_archive")
         info.size = len(content)
         with tarfile.open(tar_path, "w") as f:
             f.addfile(info, io.BytesIO(content))
         with tempfile.TemporaryDirectory() as dst_dir:
             data.utils.extract_archive(tar_path, dst_dir, overwrite=True)
             assert os.path.exists(os.path.join(dst_dir, tar_file))
+
 
 def _test_construct_graphs_node_ids():
     from dgl.data.csv_dataset_base import (
