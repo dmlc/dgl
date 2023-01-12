711c756f03a36a0f1c74bd21d0a10506ff8aa9dc
Quan Gan <coin2028@hotmail.com>
2023-01-12 08:57:17 +0000

    format

--- python/dgl/sparse/elementwise_op_diag.py
+++ python/dgl/sparse/elementwise_op_diag.py
@@ -194,6 +194,7 @@ def diag_power(D: DiagMatrix, scalar: Union[float, int]) -> DiagMatrix:
         else NotImplemented
     )
 
+
 DiagMatrix.__add__ = diag_add
 DiagMatrix.__radd__ = diag_add
 DiagMatrix.__sub__ = diag_sub
