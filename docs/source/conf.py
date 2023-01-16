0868f1796d9bfe4713c046e670df3e323999c074
Minjie Wang <wmjlyjemaine@gmail.com>
2023-01-16 18:59:19 +0800

    [Doc] sparse lib tutorial and colab links (#5181)
    * Created using Colaboratory
    
    * nbsphinx & nblink
    
    * Created using Colaboratory
    
    * update notebook
    
    * minor update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,8 @@ extensions = [
     'sphinx.ext.graphviz',
     'sphinx_gallery.gen_gallery',
     'sphinx_copybutton',
+    'nbsphinx',
+    'nbsphinx_link',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
---
+++ docs/source/notebooks/sparse/index.rst
@@ -0,0 +1,10 @@
+🆕 Tutorials: dgl.sparse
+========================
+
+TODO(minjie): intro for the new library.
+
+.. toctree::
+  :maxdepth: 2
+  :titlesonly:
+
+  quickstart.nblink
