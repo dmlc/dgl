203359edb49755698d7d7a461c88812716822296
Steve <ubuntu@ip-172-31-34-29.ap-northeast-1.compute.internal>
2023-01-19 09:14:52 +0000

    revert

--- python/dgl/sparse/sparse_matrix.py
+++ python/dgl/sparse/sparse_matrix.py
@@ -496,15 +496,15 @@ def spmatrix(
 
     Case1: Sparse matrix with row and column indices without values.
 
-    >>> indices = torch.tensor([[1, 1, 2],
-    >>>                         [2, 4, 3]])
-    >>> A = dglsp.spmatrix(indices)
+    >>> dst = torch.tensor([1, 1, 2])
+    >>> src = torch.tensor([2, 4, 3])
+    >>> A = dglsp.from_coo(dst, src)
     SparseMatrix(indices=tensor([[1, 1, 2],
                                  [2, 4, 3]]),
                  values=tensor([1., 1., 1.]),
                  shape=(3, 5), nnz=3)
     >>> # Specify shape
-    >>> A = dglsp.spmatrix(indices, shape=(5, 5))
+    >>> A = dglsp.from_coo(dst, src, shape=(5, 5))
     SparseMatrix(indices=tensor([[1, 1, 2],
                                  [2, 4, 3]]),
                  values=tensor([1., 1., 1.]),
@@ -525,10 +525,10 @@ def spmatrix(
 
     Case3: Sparse matrix with vector values.
 
-    >>> indices = torch.tensor([[1, 1, 2],
-    >>>                         [2, 4, 3]])
+    >>> dst = torch.tensor([1, 1, 2])
+    >>> src = torch.tensor([2, 4, 3])
     >>> val = torch.tensor([[1., 1.], [2., 2.], [3., 3.]])
-    >>> A = dglsp.spmatrix(indices, val)
+    >>> A = dglsp.from_coo(dst, src, val)
     SparseMatrix(indices=tensor([[1, 1, 2],
                                  [2, 4, 3]]),
                  values=tensor([[1., 1.],
