abfc4c31f56f4b896a28a9fbed7d643b9f37e9a7
Quan (Andy) Gan <coin2028@hotmail.com>
2023-01-20 14:31:33 +0800

    [Sparse] Clarification on dimensions for sparse reduction and softmax docstrings (#5197)
    * fix sparse reduce and softmax doc
    
    * Update softmax.py

--- python/dgl/sparse/softmax.py
+++ python/dgl/sparse/softmax.py
@@ -9,7 +9,10 @@ __all__ = ["softmax"]
 
 
 def softmax(input: SparseMatrix) -> SparseMatrix:
-    """Apples row-wise softmax to the non-zero elements of the sparse matrix.
+    """Applies row-wise softmax to the non-zero elements of the sparse matrix.
+
+    Equivalently, applies softmax to the non-zero elements of the sparse
+    matrix along the column (``dim=1``) dimension.
 
     If :attr:`input.val` takes shape :attr:`(nnz, D)`, then the output matrix
     :attr:`output` and :attr:`output.val` take the same shape as :attr:`input`
