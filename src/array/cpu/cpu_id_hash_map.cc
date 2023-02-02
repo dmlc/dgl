62ba9abaa789b89326169bf2f8c1aae5275ebacf
Ubuntu <ubuntu@ip-172-31-25-242.ap-northeast-1.compute.internal>
2023-02-02 01:45:31 +0000

    lint

--- src/array/cpu/cpu_id_hash_map.cc
+++ src/array/cpu/cpu_id_hash_map.cc
@@ -4,12 +4,12 @@
  * @brief Class about CPU id hash map
  */
 
-#include <cmath>
+#include "cpu_id_hash_map.h"
 
 #include <dgl/runtime/parallel_for.h>
 #include <dgl/runtime/device_api.h>
 
-#include "cpu_id_hash_map.h"
+#include <cmath>
 
 using namespace dgl::runtime;
 
@@ -22,147 +22,148 @@ CpuIdHashMap<IdType>::CpuIdHashMap() : _hmap(nullptr) {
 
 template <typename IdType>
 size_t CpuIdHashMap<IdType>::Init(IdArray ids, IdArray unique_ids) {
-    const IdType* ids_data = ids.Ptr<IdType>();
-    const size_t num = ids->shape[0];
+  const IdType* ids_data = ids.Ptr<IdType>();
+  const size_t num = ids->shape[0];
 
-    CHECK_GT(num, 0);
+  CHECK_GT(num, 0);
 
-    size_t capcacity = 1 << static_cast<size_t>(1 + std::log2(num * 3));
-    _mask =  static_cast<IdType>(capcacity - 1);
+  size_t capcacity = 1 << static_cast<size_t>(1 + std::log2(num * 3));
+  _mask =  static_cast<IdType>(capcacity - 1);
 
-    DGLContext ctx = DGLContext{kDGLCPU, 0};
-    auto device = DeviceAPI::Get(ctx);
-    _hmap = static_cast<Mapping*>(
-        device->AllocWorkspace(ctx, sizeof(Mapping) * capcacity));
-    memset(_hmap, -1, sizeof(Mapping) * capcacity);
+  DGLContext ctx = DGLContext{kDGLCPU, 0};
+  auto device = DeviceAPI::Get(ctx);
+  _hmap = static_cast<Mapping*>(
+      device->AllocWorkspace(ctx, sizeof(Mapping) * capcacity));
+  memset(_hmap, -1, sizeof(Mapping) * capcacity);
 
-    return fillInIds(num, ids_data, unique_ids);
+  return fillInIds(num, ids_data, unique_ids);
 }
 
 template <typename IdType>
-size_t CpuIdHashMap<IdType>::fillInIds(size_t num_ids, const IdType* ids_data, IdArray unique_ids) {
-    // Use `short` instead of `bool` here. As vector<bool> is an exception that updating
-    // different elements from different threads is unsafe for it.
-    // see https://en.cppreference.com/w/cpp/container#Thread_safety.
-    std::vector<short> valid(num_ids);
-    auto thread_num = compute_num_threads(0, num_ids, grain_size);
-    std::vector<size_t> block_offset(thread_num + 1, 0);
-    IdType* unique_ids_data = unique_ids.Ptr<IdType>();
+size_t CpuIdHashMap<IdType>::fillInIds(size_t num_ids,
+    const IdType* ids_data, IdArray unique_ids) {
+  // Use `int16_t` instead of `bool` here. As vector<bool> is an exception
+  // for whom updating different elements from different threads is unsafe.
+  // see https://en.cppreference.com/w/cpp/container#Thread_safety.
+  std::vector<int16_t> valid(num_ids);
+  auto thread_num = compute_num_threads(0, num_ids, grain_size);
+  std::vector<size_t> block_offset(thread_num + 1, 0);
+  IdType* unique_ids_data = unique_ids.Ptr<IdType>();
 
-    // Insert all elements in this loop. 
-    parallel_for(0, num_ids, grain_size, [&](int64_t s, int64_t e) {
-        size_t count = 0;
-        for (int64_t i = s; i < e; i++) {
-            insert_cas(ids_data[i], valid, i);
-            if (valid[i]) {
-                count++;
-            }
-        }
+  // Insert all elements in this loop.
+  parallel_for(0, num_ids, grain_size, [&](int64_t s, int64_t e) {
+      size_t count = 0;
+      for (int64_t i = s; i < e; i++) {
+          insert_cas(ids_data[i], &valid, i);
+          if (valid[i]) {
+              count++;
+          }
+      }
 
-        auto tid = omp_get_thread_num();
-        block_offset[tid + 1] = count;
-    });
-    
-    // Get ExclusiveSum of each block.
-    for (size_t i = 1; i <= thread_num; i++) {
-        block_offset[i] += block_offset[i - 1];
-    }
+      auto tid = omp_get_thread_num();
+      block_offset[tid + 1] = count;
+  });
 
-    // Get unique array from ids and set value for hash map.
-    parallel_for(0, num_ids, grain_size, [&](int64_t s, int64_t e) {
-        auto tid = omp_get_thread_num();
-        auto pos = block_offset[tid];
-        for (int64_t i = s; i < e; i++) {
-            if (valid[i]) {
-                unique_ids_data[pos] = ids_data[i];
-                set_value(ids_data[i], pos);
-                pos = pos + 1;
-            }
-        }
-    });
-    
-    return block_offset[thread_num];
+  // Get ExclusiveSum of each block.
+  for (size_t i = 1; i <= thread_num; i++) {
+      block_offset[i] += block_offset[i - 1];
+  }
+
+  // Get unique array from ids and set value for hash map.
+  parallel_for(0, num_ids, grain_size, [&](int64_t s, int64_t e) {
+      auto tid = omp_get_thread_num();
+      auto pos = block_offset[tid];
+      for (int64_t i = s; i < e; i++) {
+          if (valid[i]) {
+              unique_ids_data[pos] = ids_data[i];
+              set_value(ids_data[i], pos);
+              pos = pos + 1;
+          }
+      }
+  });
+
+  return block_offset[thread_num];
 }
 
 template <typename IdType>
 CpuIdHashMap<IdType>::~CpuIdHashMap() {
-    if (_hmap != nullptr) {
-        DGLContext ctx = DGLContext{kDGLCPU, 0};
-        auto device = DeviceAPI::Get(ctx);
-        device->FreeWorkspace(ctx, _hmap);
-    }
+  if (_hmap != nullptr) {
+      DGLContext ctx = DGLContext{kDGLCPU, 0};
+      auto device = DeviceAPI::Get(ctx);
+      device->FreeWorkspace(ctx, _hmap);
+  }
 }
 
 template <typename IdType>
 void CpuIdHashMap<IdType>::Map(IdArray ids,
     IdType default_val, IdArray new_ids) const {
-    const IdType* ids_data = ids.Ptr<IdType>();
-    const size_t len = static_cast<size_t>(ids->shape[0]);
-    IdType* values_data = new_ids.Ptr<IdType>();
+  const IdType* ids_data = ids.Ptr<IdType>();
+  const size_t len = static_cast<size_t>(ids->shape[0]);
+  IdType* values_data = new_ids.Ptr<IdType>();
 
-    parallel_for(0, len, grain_size, [&](int64_t s, int64_t e) {
-        for (int64_t i = s; i < e; i++) {
-            values_data[i] = map(ids_data[i], default_val);
-        }
-    });
+  parallel_for(0, len, grain_size, [&](int64_t s, int64_t e) {
+      for (int64_t i = s; i < e; i++) {
+          values_data[i] = map(ids_data[i], default_val);
+      }
+  });
 }
 
 template <typename IdType>
 IdType CpuIdHashMap<IdType>::map(IdType id, IdType default_val) const {
-    IdType pos = (id & _mask);
-    IdType delta = 1;
-    while (_hmap[pos].key != kEmptyKey && _hmap[pos].key != id) {
-        next(pos, delta);
-    }
+  IdType pos = (id & _mask);
+  IdType delta = 1;
+  while (_hmap[pos].key != kEmptyKey && _hmap[pos].key != id) {
+      next(&pos, &delta);
+  }
 
-    if (_hmap[pos].key == kEmptyKey) {
-        return default_val;
-    } else {
-        return _hmap[pos].value;
-    }
+  if (_hmap[pos].key == kEmptyKey) {
+      return default_val;
+  } else {
+      return _hmap[pos].value;
+  }
 }
 
 template <typename IdType>
-void CpuIdHashMap<IdType>::next(IdType& pos, IdType&delta) const {
-    pos = ((pos + delta*delta) & _mask);
-    delta += 1;
+void CpuIdHashMap<IdType>::next(IdType* pos, IdType* delta) const {
+  *pos = (*pos + (*delta) * (*delta)) & _mask;
+  *delta = *delta + 1;
 }
 
 template <typename IdType>
-void CpuIdHashMap<IdType>::insert_cas(IdType id, std::vector<short>& valid, size_t index) {
-    IdType pos = (id & _mask), delta = 1;
-    while (!attempt_insert_at(pos, id, valid, index)) {
-        next(pos, delta);
-    }
+void CpuIdHashMap<IdType>::insert_cas(IdType id,
+    std::vector<int16_t>* valid,
+    size_t index) {
+  IdType pos = (id & _mask), delta = 1;
+  while (!attempt_insert_at(pos, id, valid, index)) {
+      next(&pos, &delta);
+  }
 }
-  
+
 template <typename IdType>
 void CpuIdHashMap<IdType>::set_value(IdType k, IdType v) {
-    IdType pos = (k & _mask), delta = 1;
-    while (_hmap[pos].key != k) {
-        next(pos, delta);
-    }
+  IdType pos = (k & _mask), delta = 1;
+  while (_hmap[pos].key != k) {
+      next(&pos, &delta);
+  }
 
-    _hmap[pos].value = v;
+  _hmap[pos].value = v;
 }
 
 template <typename IdType>
-bool CpuIdHashMap<IdType>::attempt_insert_at(int64_t pos,
-    IdType key, std::vector<short>& valid, size_t index) {
-    IdType empty_key = kEmptyKey;
-    
-    IdType old_val = CAS(&(_hmap[pos].key), empty_key, key);
+bool CpuIdHashMap<IdType>::attempt_insert_at(int64_t pos, IdType key,
+    std::vector<int16_t>* valid, size_t index) {
+  IdType empty_key = kEmptyKey;
+  IdType old_val = CAS(&(_hmap[pos].key), empty_key, key);
 
-    if (old_val == empty_key) {
-        valid[index] = true;
-        return true;
-    }
-    else {
-        if(old_val == key)
-            return true;
-        else
-            return false; 
-    }
+  if (old_val == empty_key) {
+      (*valid)[index] = true;
+      return true;
+  } else {
+      if (old_val == key)
+          return true;
+      else
+          return false;
+  }
 }
 
 template class CpuIdHashMap<int32_t>;
--- src/array/cpu/cpu_id_hash_map.h
+++ src/array/cpu/cpu_id_hash_map.h
@@ -4,12 +4,12 @@
  * @brief Class about CPU id hash map
  */
 
-#ifndef DGL_ARRAY_CPU_ID_HASH_MAP_H_
-#define DGL_ARRAY_CPU_ID_HASH_MAP_H_
-
-#include <vector>
+#ifndef DGL_ARRAY_CPU_CPU_ID_HASH_MAP_H_
+#define DGL_ARRAY_CPU_CPU_ID_HASH_MAP_H_
 
 #include <dgl/aten/types.h>
+
+#include <vector>
 
 #ifdef _MSC_VER
 # include <intrin.h>
@@ -31,46 +31,48 @@ namespace aten {
 template <typename IdType>
 class CpuIdHashMap {
  public:
-    struct Mapping {
-        IdType key;
-        IdType value;
-    };
-    
-    explicit CpuIdHashMap();
+  struct Mapping {
+      IdType key;
+      IdType value;
+  };
 
-    CpuIdHashMap(const CpuIdHashMap& other) = delete;
-    CpuIdHashMap& operator=(const CpuIdHashMap& other) = delete;
+  CpuIdHashMap();
 
-    size_t Init(IdArray ids, IdArray unique_ids);
+  CpuIdHashMap(const CpuIdHashMap& other) = delete;
+  CpuIdHashMap& operator=(const CpuIdHashMap& other) = delete;
 
-    void Map(IdArray ids, IdType default_val, IdArray new_ids) const;
+  size_t Init(IdArray ids, IdArray unique_ids);
 
-    ~CpuIdHashMap();
-   
-    // Return the new id of the given id. If the given id is not contained
-    // in the hash map, returns the default_val instead.
-    IdType map(IdType id, IdType default_val) const;
-    
-    size_t fillInIds(size_t num_ids, const IdType* ids_data, IdArray unique_ids);
+  void Map(IdArray ids, IdType default_val, IdArray new_ids) const;
 
-    void next(IdType& pos, IdType&delta) const;
+  ~CpuIdHashMap();
 
-    void insert_cas(IdType id, std::vector<short>& valid, size_t index);
-    
-    // Key must exist.
-    void set_value(IdType k, IdType v);
+  // Return the new id of the given id. If the given id is not contained
+  // in the hash map, returns the default_val instead.
+  IdType map(IdType id, IdType default_val) const;
 
-    bool attempt_insert_at(int64_t pos, IdType key, std::vector<short>& valid, size_t index);
+  size_t fillInIds(size_t num_ids,
+    const IdType* ids_data, IdArray unique_ids);
+
+  void next(IdType* pos, IdType* delta) const;
+
+  void insert_cas(IdType id, std::vector<int16_t>* valid, size_t index);
+
+  // Key must exist.
+  void set_value(IdType k, IdType v);
+
+  bool attempt_insert_at(int64_t pos, IdType key,
+    std::vector<int16_t>* valid, size_t index);
 
  private:
-    static constexpr IdType kEmptyKey = static_cast<IdType>(-1);
-    static constexpr int grain_size = 1024;
+  static constexpr IdType kEmptyKey = static_cast<IdType>(-1);
+  static constexpr int grain_size = 1024;
 
-    Mapping* _hmap;
-    IdType _mask;
+  Mapping* _hmap;
+  IdType _mask;
 };
 
 }  // namespace aten
 }  // namespace dgl
 
-#endif  // DGL_ARRAY_CPU_ID_HASH_MAP_H_
+#endif  // DGL_ARRAY_CPU_CPU_ID_HASH_MAP_H_
