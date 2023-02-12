3cc918ad5d7515d676aa7220d4948dfc8fb3649d
Quan (Andy) Gan <coin2028@hotmail.com>
2023-02-12 17:48:53 +0800

    Update __init__.py

--- python/dgl/sparse/__init__.py
+++ python/dgl/sparse/__init__.py
@@ -34,8 +34,7 @@ def load_dgl_sparse():
     dirname = os.path.dirname(libinfo.find_lib_path()[0])
     path = os.path.join(dirname, "dgl_sparse", basename)
     if not os.path.exists(path):
-        raise FileNotFoundError(
-            f"Cannot find DGL C++ sparse library at {path}")
+        raise FileNotFoundError(f"Cannot find DGL C++ sparse library at {path}")
 
     try:
         torch.classes.load_library(path)
