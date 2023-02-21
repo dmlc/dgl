0e82ecada7456b7336ac83e67b7248d1fa060597
Ubuntu <ubuntu@ip-172-31-28-63.ap-northeast-1.compute.internal>
2023-02-21 04:20:29 +0000

    update

--- tests/scripts/ci_report/status.py
+++ tests/scripts/ci_report/status.py
@@ -9,20 +9,29 @@ COMMIT = os.getenv("GIT_COMMIT")
 
 job_link = os.environ["BUILD_URL"]
 response = requests.get("{}wfapi".format(job_link), verify=False).json()
-print(response)
-status = "✅ CI test succeeded"
-for v in response["stages"]:
-    # https://javadoc.jenkins.io/plugin/pipeline-graph-analysis/org/jenkinsci/plugins/workflow/pipelinegraphanalysis/GenericStatus.html
-    if v["status"] in ["FAILED", "ABORTED"]:
-        status = "❌ CI test failed in Stage [{}].".format(v["name"])
-        break
 
-comment = f""" Commit ID: {COMMIT}\n
+# List of status of entire job.
+# https://javadoc.jenkins.io/hudson/model/Result.html
+status = response["status"]
+if status == "SUCCESS":
+    status_output = "✅ CI test succeeded"
+elif status == "ABORTED":
+    status_output = "⚪️ CI test aborted"
+else:
+    for v in response["stages"]:
+        # List of status of individual stage.
+        # https://javadoc.jenkins.io/plugin/pipeline-graph-analysis/org/jenkinsci/plugins/workflow/pipelinegraphanalysis/GenericStatus.html
+        if v["status"] in ["FAILED", "ABORTED"]:
+            stage = v["name"]
+            status_output = f"❌ CI test [{status}] in Stage [{name}]."
+            break
+
+comment = f"""
+Commit ID: {COMMIT}\n
 Build ID: {BUILD_ID}\n
-Status: {status} \n
+Status: {status_output} \n
 Report path: [link](https://dgl-ci-result.s3.us-west-2.amazonaws.com/{JOB_NAME}/{BUILD_NUMBER}/{BUILD_ID}/logs/report.html) \n
-Full logs path: [link](https://dgl-ci-result.s3.us-west-2.amazonaws.com/{JOB_NAME}/{BUILD_NUMBER}/{BUILD_ID}/logs/cireport.log) \n
-Build URL: {job_link}
+Full logs path: [link](https://dgl-ci-result.s3.us-west-2.amazonaws.com/{JOB_NAME}/{BUILD_NUMBER}/{BUILD_ID}/logs/cireport.log)
 """
 
 print(comment)
