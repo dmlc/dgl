0127a2ea4d9775c706509b25349ecbbf47ce2ccd
Hongzhi (Steve), Chen <chenhongzhi.nkcs@gmail.com>
2023-02-21 12:29:00 +0800

    [Misc] Update Jenkins status. (#5345)
    * update
    
    * polish
    
    * polish
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-28-63.ap-northeast-1.compute.internal>

--- tests/scripts/ci_report/status.py
+++ tests/scripts/ci_report/status.py
@@ -6,23 +6,31 @@ JOB_NAME = os.getenv("JOB_NAME")
 BUILD_NUMBER = os.getenv("BUILD_NUMBER")
 BUILD_ID = os.getenv("BUILD_ID")
 COMMIT = os.getenv("GIT_COMMIT")
+JOB_LINK = os.environ["BUILD_URL"]
+response = requests.get("{}wfapi".format(JOB_LINK), verify=False).json()
 
-job_link = os.environ["BUILD_URL"]
-response = requests.get("{}wfapi".format(job_link), verify=False).json()
-print(response)
-status = "✅ CI test succeeded"
-for v in response["stages"]:
-    # https://javadoc.jenkins.io/plugin/pipeline-graph-analysis/org/jenkinsci/plugins/workflow/pipelinegraphanalysis/GenericStatus.html
-    if v["status"] in ["FAILED", "ABORTED"]:
-        status = "❌ CI test failed in Stage [{}].".format(v["name"])
-        break
+# List of status of entire job.
+# https://javadoc.jenkins.io/hudson/model/Result.html
+status = response["status"]
+if status == "SUCCESS":
+    status_output = "✅ CI test succeeded"
+elif status == "ABORTED":
+    status_output = "⚪️ CI test aborted"
+else:
+    for stage in response["stages"]:
+        # List of status of individual stage.
+        # https://javadoc.jenkins.io/plugin/pipeline-graph-analysis/org/jenkinsci/plugins/workflow/pipelinegraphanalysis/GenericStatus.html
+        if stage["status"] in ["FAILED", "ABORTED"]:
+            stage_name = stage["name"]
+            status_output = f"❌ CI test [{status}] in Stage [{stage_name}]."
+            break
 
-comment = f""" Commit ID: {COMMIT}\n
+comment = f"""
+Commit ID: {COMMIT}\n
 Build ID: {BUILD_ID}\n
-Status: {status} \n
-Report path: [link](https://dgl-ci-result.s3.us-west-2.amazonaws.com/{JOB_NAME}/{BUILD_NUMBER}/{BUILD_ID}/logs/report.html) \n
-Full logs path: [link](https://dgl-ci-result.s3.us-west-2.amazonaws.com/{JOB_NAME}/{BUILD_NUMBER}/{BUILD_ID}/logs/cireport.log) \n
-Build URL: {job_link}
+Status: {status_output}\n
+Report path: [link](https://dgl-ci-result.s3.us-west-2.amazonaws.com/{JOB_NAME}/{BUILD_NUMBER}/{BUILD_ID}/logs/report.html)\n
+Full logs path: [link](https://dgl-ci-result.s3.us-west-2.amazonaws.com/{JOB_NAME}/{BUILD_NUMBER}/{BUILD_ID}/logs/cireport.log)
 """
 
 print(comment)
