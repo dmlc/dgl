618c96368b3f35cd910c5e4d4ff2d0e236d48e26
Ubuntu <ubuntu@ip-172-31-36-188.ap-northeast-1.compute.internal>
2023-02-23 09:53:16 +0000

    lint

--- tests/python/common/test_homophily.py
+++ tests/python/common/test_homophily.py
@@ -9,6 +9,8 @@ def test_node_homophily(idtype):
     # NOTE: If you want to update this test case, remember to update the
     # docstring example too.
     device = F.ctx()
-    graph = dgl.graph(([1, 2, 0, 4], [0, 1, 2, 3]), idtype=idtype, device=device)
+    graph = dgl.graph(
+        ([1, 2, 0, 4], [0, 1, 2, 3]), idtype=idtype, device=device
+    )
     y = F.tensor([0, 0, 0, 0, 1])
     assert dgl.node_homophily(graph, y) == 0.6000000238418579
