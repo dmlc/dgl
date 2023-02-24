5ffd2a022c3bc2b37a47d460d5f32d034f61d81e
czkkkkkk <zekucai@gmail.com>
2023-02-24 10:03:43 +0800

    [Sparse] Support column-wise softmax (#5377)
    * [Sparse] Support column-wise softmax
    
    * Update python/dgl/sparse/softmax.py
    
    Co-authored-by: Mufei Li <mufeili1996@gmail.com>
    
    ---------
    
    Co-authored-by: Mufei Li <mufeili1996@gmail.com>

--- dgl_sparse/include/sparse/softmax.h
+++ dgl_sparse/include/sparse/softmax.h
@@ -12,17 +12,20 @@ namespace dgl {
 namespace sparse {
 
 /**
- * @brief Apply row-wise softmax to the non-zero entries of the sparse matrix.
+ * @brief Apply softmax to the non-zero entries of the sparse matrix on the
+ * dimension dim. dim = 0 or 1 indicates column-wise or row-wise softmax
+ * respectively.
  *
  * This function supports autograd for the sparse matrix, but it does not
  * support higher order gradient.
  *
  * @param sparse_mat The sparse matrix
+ * @param dim The dimension to apply softmax
  *
  * @return Sparse matrix
  */
 c10::intrusive_ptr<SparseMatrix> Softmax(
-    const c10::intrusive_ptr<SparseMatrix>& sparse_mat);
+    const c10::intrusive_ptr<SparseMatrix>& sparse_mat, int64_t dim);
 
 }  // namespace sparse
 }  // namespace dgl
