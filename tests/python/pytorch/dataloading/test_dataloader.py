85136ef01bb279eea36f57ef33867632ec3b8582
Quan Gan <coin2028@hotmail.com>
2023-03-03 10:14:50 +0000

    fix duplicate worker_init_fn

--- tests/python/pytorch/dataloading/test_dataloader.py
+++ tests/python/pytorch/dataloading/test_dataloader.py
@@ -91,7 +91,6 @@ def test_saint(num_workers, mode):
     for sg in dataloader:
         pass
 
-
 @parametrize_idtype
 @pytest.mark.parametrize(
     "mode", ["cpu", "uva_cuda_indices", "uva_cpu_indices", "pure_gpu"]
@@ -621,6 +620,24 @@ def test_edge_dataloader_excludes(
         if i == 10:
             break
 
+
+def dummy_worker_init_fn(worker_id):
+    pass
+
+def test_dataloader_worker_init_fn():
+    dataset = dgl.data.CoraFullDataset()
+    g = dataset[0]
+    sampler = dgl.dataloading.MultiLayerNeighborSampler([2])
+    dataloader = dgl.dataloading.DataLoader(
+        g,
+        torch.arange(100),
+        sampler,
+        batch_size=4,
+        num_workers=4,
+        worker_init_fn=dummy_worker_init_fn
+    )
+    for _ in dataloader:
+        pass
 
 if __name__ == "__main__":
     # test_node_dataloader(F.int32, 'neighbor', None)
