166882052c5e0382d3b0bd06b7298e58abec5eaa
ZhenyuLU_Heliodore <74746069+ZhenyuLU-Heliodore@users.noreply.github.com>
2023-03-09 17:11:33 +0800

    Add files via upload

--- python/dgl/data/zinc.py
+++ python/dgl/data/zinc.py
@@ -5,67 +5,67 @@ from .utils import _get_dgl_url, load_graphs
 
 
 class ZINCDataset(DGLBuiltinDataset):
-    r""" ZINC dataset for graph regression task.
+    r""" ZINC dataset for the graph regression task.
 
-   A subset (12K) of ZINC molecular graphs (250K) dataset is used to
-   regress a molecular property known as the constrained solubility.
-   For each molecular graph, the node features are the types of heavy
-   atoms, between which the edge features are the types of bonds.
-   Each graph contains 9-37 nodes and 16-84 edges.
+    A subset (12K) of ZINC molecular graphs (250K) dataset is used to
+    regress a molecular property known as the constrained solubility.
+    For each molecular graph, the node features are the types of heavy
+    atoms, between which the edge features are the types of bonds.
+    Each graph contains 9-37 nodes and 16-84 edges.
 
-   Reference `<https://arxiv.org/pdf/2003.00982.pdf>`_
+    Reference `<https://arxiv.org/pdf/2003.00982.pdf>`_
 
-   Statistics:
+    Statistics:
 
-   Train examples: 10,000
-   Valid examples: 1,000
-   Test examples: 1,000
-   Average number of nodes: 23.16
-   Average number of edges: 39.83
-   Number of atom types: 28
-   Number of bond types: 4
+    Train examples: 10,000
+    Valid examples: 1,000
+    Test examples: 1,000
+    Average number of nodes: 23.16
+    Average number of edges: 39.83
+    Number of atom types: 28
+    Number of bond types: 4
 
-   Parameters
-   ----------
-   mode : str, optional
-       Should be chosen from ["train", "valid", "test"]
-       Default: "train".
-   raw_dir : str
-       Raw file directory to download/contains the input data directory.
-       Default: "~/.dgl/".
-   force_reload : bool
-       Whether to reload the dataset.
-       Default: False.
-   verbose : bool
-       Whether to print out progress information.
-       Default: False.
-   transform : callable, optional
-       A transform that takes in a :class:`~dgl.DGLGraph` object and returns
-       a transformed version. The :class:`~dgl.DGLGraph` object will be
-       transformed before every access.
+    Parameters
+    ----------
+    mode : str, optional
+        Should be chosen from ["train", "valid", "test"]
+        Default: "train".
+    raw_dir : str
+        Raw file directory to download/contains the input data directory.
+        Default: "~/.dgl/".
+    force_reload : bool
+        Whether to reload the dataset.
+        Default: False.
+    verbose : bool
+        Whether to print out progress information.
+        Default: False.
+    transform : callable, optional
+        A transform that takes in a :class:`~dgl.DGLGraph` object and returns
+        a transformed version. The :class:`~dgl.DGLGraph` object will be
+        transformed before every access.
 
-   Attributes
-   ----------
-   num_atom_types : int
-       Number of atom types.
-   num_bond_types : int
-       Number of bond types.
+    Attributes
+    ----------
+    num_atom_types : int
+        Number of atom types.
+    num_bond_types : int
+        Number of bond types.
 
-   Examples
-   ---------
-   >>> from dgl.data import ZINCDataset
-   >>>
-   >>> training_set = ZINCDataset(mode="train")
-   >>> training_set.num_atom_types
-   28
-   >>> len(training_set)
-   10000
-   >>> graph, label = training_set[0]
-   >>> graph
-   Graph(num_nodes=29, num_edges=64,
-         ndata_schemes={'feat': Scheme(shape=(), dtype=torch.int64)}
-         edata_schemes={'feat': Scheme(shape=(), dtype=torch.int64)})
-   """
+    Examples
+    ---------
+    >>> from dgl.data import ZINCDataset
+
+    >>> training_set = ZINCDataset(mode="train")
+    >>> training_set.num_atom_types
+    28
+    >>> len(training_set)
+    10000
+    >>> graph, label = training_set[0]
+    >>> graph
+    Graph(num_nodes=29, num_edges=64,
+        ndata_schemes={'feat': Scheme(shape=(), dtype=torch.int64)}
+        edata_schemes={'feat': Scheme(shape=(), dtype=torch.int64)})
+    """
 
     def __init__(
             self,
@@ -76,9 +76,6 @@ class ZINCDataset(DGLBuiltinDataset):
             transform=None,
     ):
         self._url = _get_dgl_url("dataset/ZINK12k.zip")
-        self.graph_path = os.path.join(
-            self.save_path, "ZincDGL_{}.bin".format(self.mode)
-        )
         self.mode = mode
 
         super(ZINCDataset, self).__init__(
@@ -88,6 +85,10 @@ class ZINCDataset(DGLBuiltinDataset):
             force_reload=force_reload,
             verbose=verbose,
             transform=transform,
+        )
+
+        self.graph_path = os.path.join(
+            self.save_path, "ZincDGL_{}.bin".format(self.mode)
         )
 
     def process(self):
