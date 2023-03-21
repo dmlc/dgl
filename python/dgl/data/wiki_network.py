96fdbf9c2ae33ca3ab552304faee52d181e4d7d4
Ubuntu <ubuntu@ip-172-31-36-188.ap-northeast-1.compute.internal>
2023-03-21 08:53:03 +0000

    update

--- python/dgl/data/wiki_network.py
+++ python/dgl/data/wiki_network.py
@@ -2,7 +2,14 @@
 Wikipedia page-page networks on three topics: chameleons, crocodiles and
 squirrels
 """
+import os
+
+import numpy as np
+
+from .. import backend as F
+from ..convert import graph
 from .dgl_dataset import DGLBuiltinDataset
+from .utils import _get_dgl_url
 
 
 class WikiNetworkDataset(DGLBuiltinDataset):
@@ -25,31 +32,103 @@ class WikiNetworkDataset(DGLBuiltinDataset):
         a transformed version. The :class:`~dgl.DGLGraph` object will be
         transformed before every access.
     """
-    def __init__(
-        self,
-        name,
-        raw_dir,
-        force_reload,
-        verbose,
-        transform
-    ):
+
+    def __init__(self, name, raw_dir, force_reload, verbose, transform):
         # TODO: provide url
         super(WikiNetworkDataset, self).__init__(
             name=name,
-            url=None,
+            url=_get_dgl_url(f"dataset/{name}.zip"),
             raw_dir=raw_dir,
             force_reload=force_reload,
             verbose=verbose,
-            transform=transform
+            transform=transform,
         )
 
     def process(self):
         """Load and process the data."""
+        # node features and labels
+        with open(f"{self.raw_path}/out1_node_feature_label.txt", "r") as f:
+            data = f.read().split("\n")[1:-1]
+        features = [
+            [float(v) for v in r.split("\t")[1].split(",")] for r in data
+        ]
+        features = F.tensor(features, dtype=F.data_type_dict["float32"])
+        labels = [int(r.split("\t")[2]) for r in data]
+        self._num_classes = max(labels) + 1
+        labels = F.tensor(labels, dtype=F.data_type_dict["int64"])
+
+        # graph structure
+        with open(f"{self.raw_path}/out1_graph_edges.txt", "r") as f:
+            data = f.read().split("\n")[1:-1]
+            data = [[int(v) for v in r.split("\t")] for r in data]
+        COO = F.swapaxes(F.tensor(data, dtype=F.data_type_dict["int64"]), 0, 1)
+        dst = F.squeeze(F.narrow_row(COO, start=0, stop=1), dim=0)
+        src = F.squeeze(F.narrow_row(COO, start=1, stop=2), dim=0)
+
+        self._g = graph((src, dst), num_nodes=F.shape(features)[0])
+        self._g.ndata["feat"] = features
+        self._g.ndata["label"] = labels
+
+        # split
+        train_masks, val_masks, test_masks = [], [], []
+        for i in range(10):
+            filepath = f"{self.raw_path}/{self.name}_split_0.6_0.2_{i}.npz"
+            f = np.load(filepath)
+            train_masks += [F.zerocopy_from_numpy(f["train_mask"])]
+            val_masks += [F.zerocopy_from_numpy(f["val_mask"])]
+            test_masks += [F.zerocopy_from_numpy(f["test_mask"])]
+        self._g.ndata["train_mask"] = F.astype(
+            F.stack(train_masks, dim=1), F.data_type_dict["bool"]
+        )
+        self._g.ndata["val_mask"] = F.astype(
+            F.stack(val_masks, dim=1), F.data_type_dict["bool"]
+        )
+        self._g.ndata["test_mask"] = F.astype(
+            F.stack(test_masks, dim=1), F.data_type_dict["bool"]
+        )
+
+    def has_cache(self):
+        return os.path.exists(self.raw_path)
+
+    def load(self):
+        self.process()
+
+    def __getitem__(self, idx):
+        assert idx == 0, "This dataset has only one graph."
+        if self._transform is None:
+            return self._g
+        else:
+            return self._transform(self._g)
+
+    def __len__(self):
+        return 1
+
+    @property
+    def num_classes(self):
+        return self._num_classes
 
 
 class ChameleonDataset(WikiNetworkDataset):
     """Wikipedia page-page network on chameleons from `Multi-scale Attributed
-    Node Embedding <https://arxiv.org/abs/1909.13021>`__
+    Node Embedding <https://arxiv.org/abs/1909.13021>`__ and later processed by
+    `Geom-GCN: Geometric Graph Convolutional Networks
+    <https://arxiv.org/abs/2002.05287>`
+
+    Nodes represent articles from the English Wikipedia, edges reflect mutual
+    links between them. Node features indicate the presence of particular nouns
+    in the articles. The nodes were classified into 5 classes in terms of their
+    average monthly traffic.
+
+    Statistics:
+
+    - Nodes: 2277
+    - Edges: 36101
+    - Number of Classes: 5
+    - 10 splits with 60/20/20 train/val/test ratio
+
+        - Train: 1092
+        - Val: 729
+        - Test: 456
 
     Parameters
     ----------
@@ -65,20 +144,42 @@ class ChameleonDataset(WikiNetworkDataset):
         a transformed version. The :class:`~dgl.DGLGraph` object will be
         transformed before every access. Default: None
 
+    Attributes
+    ----------
+    num_classes : int
+        Number of node classes
+
+    Notes
+    -----
+    The graph does not come with edges for both directions.
+
     Examples
     --------
+
+    >>> from dgl.data import ChameleonDataset
+    >>> dataset = ChameleonDataset()
+    >>> g = dataset[0]
+    >>> num_classes = dataset.num_classes
+
+    >>> # get node features
+    >>> feat = g.ndata["feat"]
+
+    >>> # get data split
+    >>> train_mask = g.ndata["train_mask"]
+    >>> val_mask = g.ndata["val_mask"]
+    >>> test_mask = g.ndata["test_mask"]
+
+    >>> # get labels
+    >>> label = g.ndata['label']
     """
+
     def __init__(
-        self,
-        raw_dir=None,
-        force_reload=False,
-        verbose=True,
-        transform=None
+        self, raw_dir=None, force_reload=False, verbose=True, transform=None
     ):
         super(ChameleonDataset, self).__init__(
-            name='chameleon',
+            name="chameleon",
             raw_dir=raw_dir,
             force_reload=force_reload,
             verbose=verbose,
-            transform=transform
+            transform=transform,
         )
