5146cdffa4e7b01a663eb9c7cc3a684c51324acf
lspongebobjh <lspongebobjh@gmail.com>
2023-04-18 12:09:50 +0000

    lintrunner autoformat codes

--- tests/python/common/data/test_movielens.py
+++ tests/python/common/data/test_movielens.py
@@ -15,20 +15,40 @@ import dgl
 def test_actor():
     transform = dgl.AddSelfLoop(allow_duplicate=True)
 
-    movielens = dgl.data.MovieLensDataset(name='ml-100k', valid_ratio=0.2, test_ratio=0.1, force_reload=True)
+    movielens = dgl.data.MovieLensDataset(
+        name="ml-100k", valid_ratio=0.2, test_ratio=0.1, force_reload=True
+    )
     train_graph, valid_graph, test_graph = movielens[0]
-    assert train_graph.num_nodes() + valid_graph.num_nodes() + test_graph.num_nodes() == 943 + 1682
-    assert train_graph.num_edges() + valid_graph.num_edges() + test_graph.num_edges() == 100000 * 2
+    assert (
+        train_graph.num_nodes()
+        + valid_graph.num_nodes()
+        + test_graph.num_nodes()
+        == 943 + 1682
+    )
+    assert (
+        train_graph.num_edges()
+        + valid_graph.num_edges()
+        + test_graph.num_edges()
+        == 100000 * 2
+    )
 
-    movielens1 = dgl.data.MovieLensDataset(name='ml-100k', valid_ratio=0.2, test_ratio=0.1, force_reload=True, transform=transform)
+    movielens1 = dgl.data.MovieLensDataset(
+        name="ml-100k",
+        valid_ratio=0.2,
+        test_ratio=0.1,
+        force_reload=True,
+        transform=transform,
+    )
     train_graph1, valid_graph1, test_graph1 = movielens1[0]
-    assert train_graph1.num_edges() - train_graph.num_edges() == train_graph.num_nodes()
-    assert valid_graph1.num_edges() - valid_graph.num_edges() == valid_graph.num_nodes()
-    assert test_graph1.num_edges() - test_graph.num_edges() == test_graph.num_nodes()
-    
-
-
-    
-
-    
-
+    assert (
+        train_graph1.num_edges() - train_graph.num_edges()
+        == train_graph.num_nodes()
+    )
+    assert (
+        valid_graph1.num_edges() - valid_graph.num_edges()
+        == valid_graph.num_nodes()
+    )
+    assert (
+        test_graph1.num_edges() - test_graph.num_edges()
+        == test_graph.num_nodes()
+    )
