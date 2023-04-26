8bf96aa1be85f872856c0069fb4005b00dec6745
RhettYing <rhett_ying@qq.com>
2023-04-26 06:41:11 +0000

    fix

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -17,7 +17,6 @@ pip uninstall -y dgl
 export DGL_BENCH_DEVICE=$DEVICE
 echo "DGL_BENCH_DEVICE=$DGL_BENCH_DEVICE"
 pushd $ROOT/benchmarks
-cat environment.yml
 cat asv.conf.json
 asv machine --yes
 asv run -e -v
