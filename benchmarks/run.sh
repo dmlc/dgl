ebd854ab283d6f85b54511ba85986255481b7e08
RhettYing <rhett_ying@qq.com>
2023-04-26 03:16:09 +0000

    add pythonpath

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -17,6 +17,7 @@ pip uninstall -y dgl
 export DGL_BENCH_DEVICE=$DEVICE
 echo "DGL_BENCH_DEVICE=$DGL_BENCH_DEVICE"
 pushd $ROOT/benchmarks
+export PYTHONPATH=$ROOT/benchmarks:$PYTHONPATH
 cat environment.yml
 cat asv.conf.json
 asv machine --yes
