ddf176a56a98ba1db346500b9eee4d40e735dfeb
RhettYing <rhett_ying@qq.com>
2023-04-27 13:25:17 +0000

    add plugins

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -11,7 +11,7 @@ conda activate base
 pip install --upgrade pip
 # Newer asv version like 0.5.1 has different result format,
 # so we fix the version here. Or `generate_excel.py` has to be changed.
-pip install asv==0.4.2
+pip install asv==0.5.1
 pip uninstall -y dgl
 
 export DGL_BENCH_DEVICE=$DEVICE
@@ -21,6 +21,6 @@ cat asv.conf.json
 asv machine --yes
 # If --launch-method is specified as 'spawn', multigpu tests will crash with
 # "No module named 'benchmarks' is found".
-asv run -e -v --python=same
+asv run -e -v
 asv publish
 popd
