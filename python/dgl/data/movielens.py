4dc8f8a5502e903e85fabe2e0a4993a2c5e0ce9c
Jiahang Li <lspongebobjh@gmail.com>
2023-05-10 17:11:49 +0800

    Apply suggestions from code review
    Co-authored-by: Mufei Li <mufeili1996@gmail.com>

--- python/dgl/data/movielens.py
+++ python/dgl/data/movielens.py
@@ -130,7 +130,7 @@ class MovieLensDataset(DGLDataset):
     >>> train_g, valid_g, test_g, info = dataset[0]
     >>> train_g
     Graph(num_nodes=2625, num_edges=128000,
-        ndata_schemes={}
+         ndata_schemes={}
         edata_schemes={'rate': Scheme(shape=(), dtype=torch.float32)})
     >>> # get ratings of edges in the training graph
     >>> ratings = train_g.edata['rate']
