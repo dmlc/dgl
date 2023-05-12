5d7c911fad8160946fab2c0dd14b52ebc43815d9
Ubuntu <ubuntu@ip-172-31-25-242.ap-northeast-1.compute.internal>
2023-05-12 09:46:58 +0000

    add c++ graph

---
+++ graphbolt/src/csr_sampling_graph.cc
@@ -0,0 +1,50 @@
+/**
+ *  Copyright (c) 2023 by Contributors
+ * @file graphbolt/include/csr_sampling_graph.cc
+ * @brief Source file of sampling graph.
+ */
+
+#include "csr_sampling_graph.h"
+
+namespace graphbolt {
+namespace sampling {
+
+CSRSamplingGraph::CSRSamplingGraph(
+    int64_t num_rows, int64_t num_cols, torch::Tensor& indptr,
+    torch::Tensor& indices, const std::shared_ptr<HeteroInfo>& hetero_info)
+    : num_rows_(num_rows),
+      num_cols_(num_cols),
+      indptr_(indptr),
+      indices_(indices),
+      hetero_info_(hetero_info) {
+  TORCH_CHECK(num_rows >= 0);
+  TORCH_CHECK(num_cols >= 0);
+  TORCH_CHECK(indptr.dim() == 1);
+  TORCH_CHECK(indices.dim() == 1);
+  TORCH_CHECK(indptr.size(0) == num_rows + 1);
+  TORCH_CHECK(indptr.device() == indices.device());
+}
+
+c10::intrusive_ptr<CSRSamplingGraph> CSRSamplingGraph::FromCSR(
+    const std::vector<int64_t>& shape, torch::Tensor& indptr,
+    torch::Tensor& indices) {
+  return c10::make_intrusive<CSRSamplingGraph>(
+      shape[0], shape[1], indptr, indices, nullptr);
+}
+
+void CSRSamplingGraph::SetHeteroInfo(
+    const StringList& ntypes, const StringList& etypes,
+    torch::Tensor node_type_offset_, torch::Tensor type_per_edge_) {
+  TORCH_CHECK(node_type_offset_.size(0) > 0);
+  TORCH_CHECK(node_type_offset_.dim() == 1);
+  TORCH_CHECK(type_per_edge_.size(0) > 0);
+  TORCH_CHECK(type_per_edge_.dim() == 1);
+  TORCH_CHECK(node_type_offset_.device() == type_per_edge_.device());
+  TORCH_CHECK(type_per_edge_.device() == indices_.device());
+  TORCH_CHECK(!ntypes.empty());
+  TORCH_CHECK(!etypes.empty());
+  hetero_info_ = std::make_shared<HeteroInfo>(
+      ntypes, etypes, node_type_offset_, type_per_edge_);
+}
+}  // namespace sampling
+}  // namespace graphbolt
--- graphbolt/src/python_binding.cc
+++ graphbolt/src/python_binding.cc
@@ -4,23 +4,26 @@
  * @brief Graph bolt library Python binding.
  */
 
-#include "csr_graph.h"
+#include "csr_sampling_graph.h"
 
 namespace graphbolt {
 namespace sampling {
 
 TORCH_LIBRARY(graphbolt, m) {
-  m.class_<CSR>("CsrGraph")
-      .def("num_rows", &CSR::NumRows)
-      .def("num_cols", &CSR::NumCols)
-      .def("indptr", &CSR::IndPtr)
-      .def("indices", &CSR::Indices)
-      .def("set_hetero_info", &CSR::SetHeteroInfo)
-      .def("is_hetero", &CSR::IsHeterogeneous)
-      .def("node_types", &CSR::GetNodeTypes)
-      .def("edge_types", &CSR::GetEdgeTypes)
-      .def("per_edge_types", &CSR::GetPerEdgeTypes);
-  m.def("from_csr", &CSR::FromCSR);
+  m.class_<CSRSamplingGraph>("CsrGraph")
+      .def("num_rows", &CSRSamplingGraph::NumRows)
+      .def("num_cols", &CSRSamplingGraph::NumCols)
+      .def("num_nodes", &CSRSamplingGraph::NumNodes)
+      .def("num_edges", &CSRSamplingGraph::NumEdges)
+      .def("indptr", &CSRSamplingGraph::IndPtr)
+      .def("indices", &CSRSamplingGraph::Indices)
+      .def("set_hetero_info", &CSRSamplingGraph::SetHeteroInfo)
+      .def("is_hetero", &CSRSamplingGraph::IsHeterogeneous)
+      .def("node_types", &CSRSamplingGraph::NodeTypes)
+      .def("edge_types", &CSRSamplingGraph::EdgeTypes)
+      .def("per_edge_types", &CSRSamplingGraph::NodeTypeOffset)
+      .def("per_edge_types", &CSRSamplingGraph::PerEdgeType);
+  m.def("from_csr", &CSRSamplingGraph::FromCSR);
 }
 
 }  // namespace sampling
