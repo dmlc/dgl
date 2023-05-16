83c3c536962580cb9836951d0f4ed9b746e96231
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2023-05-16 09:49:30 +0000

    fix issue

--- graphbolt/include/csc_sampling_graph.h
+++ graphbolt/include/csc_sampling_graph.h
@@ -44,17 +44,22 @@ struct HeteroInfo {
         node_type_offset(node_type_offset),
         type_per_edge(type_per_edge) {}
 
-  /** @brief List of node types in the graph. */
+  /** @brief List of node types in the graph.*/
   StringList node_types;
 
   /** @brief List of edge types in the graph. */
   StringList edge_types;
 
-  /** @brief Offset array of node type. */
+  /**
+   * @brief Offset array of node type. The length of it is equal to number of
+   * node types.
+   */
   torch::Tensor node_type_offset;
 
-  /** @brief Type id of each edge, where type id is the corresponding index of
-   * edge_types. */
+  /**
+   * @brief Type id of each edge, where type id is the corresponding index of
+   * edge_types. The length of it is equal to the number of edges.
+   */
   torch::Tensor type_per_edge;
 };
 
