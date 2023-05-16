8482fbed00902ae42c773695d25ffdc98eb61d7f
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2023-05-16 08:18:17 +0000

    add python binding

---
+++ graphbolt/src/python_binding.cc
@@ -0,0 +1,30 @@
+/**
+ *  Copyright (c) 2023 by Contributors
+ * @file python_binding.cc
+ * @brief Graph bolt library Python binding.
+ */
+
+#include "csc_sampling_graph.h"
+
+namespace graphbolt {
+namespace sampling {
+
+TORCH_LIBRARY(graphbolt, m) {
+  m.class_<CSCSamplingGraph>("CSCSamplingGraph")
+      .def("num_rows", &CSCSamplingGraph::NumRows)
+      .def("num_cols", &CSCSamplingGraph::NumCols)
+      .def("num_nodes", &CSCSamplingGraph::NumNodes)
+      .def("num_edges", &CSCSamplingGraph::NumEdges)
+      .def("csc_indptr", &CSCSamplingGraph::CSCIndptr)
+      .def("indices", &CSCSamplingGraph::Indices)
+      .def("is_heterogeneous", &CSCSamplingGraph::IsHeterogeneous)
+      .def("node_types", &CSCSamplingGraph::NodeTypes)
+      .def("edge_types", &CSCSamplingGraph::EdgeTypes)
+      .def("node_type_offset", &CSCSamplingGraph::NodeTypeOffset)
+      .def("per_edge_type", &CSCSamplingGraph::PerEdgeType);
+  m.def("from_csr", &CSCSamplingGraph::FromCSC);
+  m.def("from_csr", &CSCSamplingGraph::FromCSCWithHeteroInfo);
+}
+
+}  // namespace sampling
+}  // namespace graphbolt
