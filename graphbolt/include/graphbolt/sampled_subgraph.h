32a4402700cf05eaf3da6f8c5760dfa8d94cc916
Ubuntu <ubuntu@ip-172-31-25-242.ap-northeast-1.compute.internal>
2023-05-24 03:51:29 +0000

    sort member

--- graphbolt/include/graphbolt/sampled_subgraph.h
+++ graphbolt/include/graphbolt/sampled_subgraph.h
@@ -45,26 +45,26 @@ struct SampledSubgraph : torch::CustomClassHolder {
    * @param indptr CSC format index pointer array.
    * @param indices CSC format index array.
    * @param reverse_row_node_ids Row's reverse node ids in the original graph.
-   * @param node_type_offset Offset array of node type.
-   * @param type_per_edge Type id of each edge.
    * @param reverse_column_node_ids Column's reverse node ids in the original
    * graph.
    * @param reverse_edge_ids Reverse edge ids in the original graph.
+   * @param node_type_offset Offset array of node type.
+   * @param type_per_edge Type id of each edge.
    */
   SampledSubgraph(
       torch::Tensor indptr, torch::Tensor indices,
       torch::Tensor reverse_row_node_ids,
-      torch::optional<torch::Tensor> node_type_offset = torch::nullopt,
-      torch::optional<torch::Tensor> type_per_edge = torch::nullopt,
       torch::optional<torch::Tensor> reverse_column_node_ids = torch::nullopt,
-      torch::optional<torch::Tensor> reverse_edge_ids = torch::nullopt)
+      torch::optional<torch::Tensor> reverse_edge_ids = torch::nullopt,
+      torch::optional<torch::Tensor> node_type_offset = torch::nullopt,
+      torch::optional<torch::Tensor> type_per_edge = torch::nullopt)
       : indptr(indptr),
         indices(indices),
         reverse_row_node_ids(reverse_row_node_ids),
-        node_type_offset(node_type_offset),
-        type_per_edge(type_per_edge),
         reverse_column_node_ids(reverse_column_node_ids),
-        reverse_edge_ids(reverse_edge_ids) {}
+        reverse_edge_ids(reverse_edge_ids),
+        node_type_offset(node_type_offset),
+        type_per_edge(type_per_edge) {}
 
   /**
    * @brief CSC format index pointer array, where the implicit node ids are
@@ -79,23 +79,6 @@ struct SampledSubgraph : torch::CustomClassHolder {
    * `reverse_column_node_ids` field.
    */
   torch::Tensor indices;
-
-  /**
-   * @brief Offset array of node type. The length of it is equal to the number
-   * of node types + 1. The tensor is in ascending order as nodes of the same
-   * type have continuous IDs, and larger node IDs are paired with larger node
-   * type IDs. Its first value is 0 and last value is the number of nodes. And
-   * nodes with ID between `node_type_offset_[i] ~ node_type_offset_[i+1]` are
-   * of type id `i`.
-   */
-  torch::optional<torch::Tensor> node_type_offset;
-
-  /**
-   * @brief Type id of each edge, where type id is the corresponding index of
-   * edge types. The length of it is equal to the number of edges in the
-   * subgraph.
-   */
-  torch::optional<torch::Tensor> type_per_edge;
 
   /**
    * @brief Row's reverse node ids in the original graph. A graph structure can
@@ -123,6 +106,23 @@ struct SampledSubgraph : torch::CustomClassHolder {
    * subgraph. This is useful when edge features are needed.
    */
   torch::optional<torch::Tensor> reverse_edge_ids;
+
+  /**
+   * @brief Offset array of node type. The length of it is equal to the number
+   * of node types + 1. The tensor is in ascending order as nodes of the same
+   * type have continuous IDs, and larger node IDs are paired with larger node
+   * type IDs. Its first value is 0 and last value is the number of nodes. And
+   * nodes with ID between `node_type_offset_[i] ~ node_type_offset_[i+1]` are
+   * of type id `i`.
+   */
+  torch::optional<torch::Tensor> node_type_offset;
+
+  /**
+   * @brief Type id of each edge, where type id is the corresponding index of
+   * edge types. The length of it is equal to the number of edges in the
+   * subgraph.
+   */
+  torch::optional<torch::Tensor> type_per_edge;
 };
 
 }  // namespace sampling
