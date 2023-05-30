65a47480e2a9d52387e511c25330bd723a7b7dd7
Nick Baker <ndbaker1@outlook.com>
2023-05-30 10:51:36 -0500

    inherit homogeneous `PGExplainer` impls. for `HeteroPGExplainer` and remove unused parameters

--- python/dgl/nn/pytorch/explain/pgexplainer.py
+++ python/dgl/nn/pytorch/explain/pgexplainer.py
@@ -64,7 +64,7 @@ class PGExplainer(nn.Module):
             nn.Linear(self.num_features, 64), nn.ReLU(), nn.Linear(64, 1)
         )
 
-    def set_masks(self, graph, feat, edge_mask=None):
+    def set_masks(self, graph, edge_mask=None):
         r"""Set the edge mask that plays a crucial role to explain the
         prediction made by the GNN for a graph. Initialize learnable edge
         mask if it is None.
@@ -73,16 +73,13 @@ class PGExplainer(nn.Module):
         ----------
         graph : DGLGraph
             A homogeneous graph.
-        feat : Tensor
-            The input feature of shape :math:`(N, D)`. :math:`N` is the
-            number of nodes, and :math:`D` is the feature size.
         edge_mask : Tensor, optional
             Learned importance mask of the edges in the graph, which is a tensor
             of shape :math:`(E)`, where :math:`E` is the number of edges in the
             graph. The values are within range :math:`(0, 1)`. The higher,
             the more important. Default: None.
         """
-        num_nodes, _ = feat.shape
+        num_nodes = graph.num_nodes()
         num_edges = graph.num_edges()
 
         init_bias = self.init_bias
@@ -351,7 +348,7 @@ class PGExplainer(nn.Module):
         edge_mask = (values + values[reverse_eids]) / 2
 
         self.clear_masks()
-        self.set_masks(graph, feat, edge_mask)
+        self.set_masks(graph, edge_mask)
 
         # the model prediction with the updated edge mask
         logits = self.model(graph, feat, edge_weight=self.edge_mask, **kwargs)
@@ -362,7 +359,7 @@ class PGExplainer(nn.Module):
         return (probs, edge_mask) if training else (probs.data, edge_mask)
 
 
-class HeteroPGExplainer(nn.Module):
+class HeteroPGExplainer(PGExplainer):
     r"""PGExplainer from `Parameterized Explainer for Graph Neural Network
     <https://arxiv.org/pdf/2011.04573>`__, adapted for heterogeneous graphs
 
@@ -399,156 +396,13 @@ class HeteroPGExplainer(nn.Module):
         coff_connect=5e-4,
         sample_bias=0.0,
     ):
-        super(HeteroPGExplainer, self).__init__()
-
-        self.model = model
-        self.num_features = num_features * 2
-
-        # training hyperparameters for PGExplainer
-        self.coff_budget = coff_budget
-        self.coff_connect = coff_connect
-        self.sample_bias = sample_bias
-
-        self.init_bias = 0.0
-
-        # Explanation network in PGExplainer
-        self.elayers = nn.Sequential(
-            nn.Linear(self.num_features, 64), nn.ReLU(), nn.Linear(64, 1)
+        super(HeteroPGExplainer, self).__init__(
+            model,
+            num_features,
+            coff_budget,
+            coff_connect,
+            sample_bias,
         )
-
-    def set_masks(self, graph, edge_mask=None):
-        r"""Set the edge mask that plays a crucial role to explain the
-        prediction made by the GNN for a homogenous graph. Initialize learnable
-        edge mask if it is None.
-
-        Parameters
-        ----------
-        graph : DGLGraph
-            A homogenous graph.
-        edge_mask : Tensor, optional
-            Learned importance mask of the edges in the graph, which is a tensor
-            of shape :math:`(E)`, where :math:`E` is the number of edges in the
-            graph. The values are within range :math:`(0, 1)`. The higher,
-            the more important. Default: None.
-        """
-        if edge_mask is None:
-            num_nodes = graph.num_nodes()
-            num_edges = graph.num_edges()
-
-            init_bias = self.init_bias
-            std = nn.init.calculate_gain("relu") * math.sqrt(
-                2.0 / (2 * num_nodes)
-            )
-
-            self.edge_mask = torch.randn(num_edges) * std + init_bias
-        else:
-            self.edge_mask = edge_mask
-
-        self.edge_mask = self.edge_mask.to(graph.device)
-
-    def clear_masks(self):
-        r"""Clear the edge mask that play a crucial role to explain the
-        prediction made by the GNN for a graph.
-        """
-        self.edge_mask = None
-
-    def parameters(self):
-        r"""
-        Returns an iterator over the `Parameter` objects of the `nn.Linear`
-        layers in the `self.elayers` sequential module. Each `Parameter`
-        object contains the weight and bias parameters of an `nn.Linear`
-        layer, as learned during training.
-
-        Returns
-        -------
-        iterator
-            An iterator over the `Parameter` objects of the `nn.Linear`
-            layers in the `self.elayers` sequential module.
-        """
-        return self.elayers.parameters()
-
-    def loss(self, prob, ori_pred):
-        r"""The loss function that is used to learn the edge
-        distribution.
-
-        Parameters
-        ----------
-        prob: Tensor
-            Tensor contains a set of probabilities for each possible
-            class label of some model for all the batched graphs,
-            which is of shape :math:`(B, L)`, where :math:`L` is the
-            different types of label in the dataset and :math:`B` is
-            the batch size.
-        ori_pred: Tensor
-            Tensor of shape ::math:`(B, 1)`, representing the original prediction
-            for the graph, where :math:`B` is the batch size.
-
-        Returns
-        -------
-        float
-            The function that returns the sum of the three loss components,
-            which is a scalar tensor representing the total loss.
-        """
-        target_prob = prob.gather(-1, ori_pred.unsqueeze(-1))
-        # 1e-6 added to prob to avoid taking the logarithm of zero
-        target_prob += 1e-6
-        # computing the log likelihood for a single prediction
-        pred_loss = torch.mean(-torch.log(target_prob))
-
-        # size
-        edge_mask = self.sparse_mask_values
-        if self.coff_budget <= 0:
-            size_loss = self.coff_budget * torch.sum(edge_mask)
-        else:
-            size_loss = self.coff_budget * F.relu(
-                torch.sum(edge_mask) - self.coff_budget
-            )
-
-        # entropy
-        scale = 0.99
-        edge_mask = self.edge_mask * (2 * scale - 1.0) + (1.0 - scale)
-        mask_ent = -edge_mask * torch.log(edge_mask) - (
-            1 - edge_mask
-        ) * torch.log(1 - edge_mask)
-        mask_ent_loss = self.coff_connect * torch.mean(mask_ent)
-
-        loss = pred_loss + size_loss + mask_ent_loss
-        return loss
-
-    def concrete_sample(self, w, beta=1.0, training=True):
-        r"""Sample from the instantiation of concrete distribution when training.
-
-        Parameters
-        ----------
-        w : Tensor
-            A tensor representing the log of the prior probability of choosing the edges.
-        beta : float, optional
-            Controls the degree of randomness in the output of the sigmoid function.
-        training : bool, optional
-            Randomness is injected during training.
-
-        Returns
-        -------
-        Tensor
-            If training is set to True, the output is a tensor of probabilities that
-            represent the probability of activating the gate for each input element.
-            If training is set to False, the output is also a tensor of probabilities,
-            but they are determined solely by the log_alpha values, without adding any
-            random noise.
-        """
-        if training:
-            bias = self.sample_bias
-            random_noise = torch.rand(w.size()).to(w.device)
-            random_noise = bias + (1 - 2 * bias) * random_noise
-            gate_inputs = torch.log(random_noise) - torch.log(
-                1.0 - random_noise
-            )
-            gate_inputs = (gate_inputs + w) / beta
-            gate_inputs = torch.sigmoid(gate_inputs)
-        else:
-            gate_inputs = torch.sigmoid(w)
-
-        return gate_inputs
 
     def train_step(self, graph, feat, tmp, **kwargs):
         r"""Compute the loss of the explanation network
@@ -577,12 +431,12 @@ class HeteroPGExplainer(nn.Module):
 
         pred = self.model(graph, feat, embed=False, **kwargs).argmax(-1).data
 
-        prob, edge_mask = self.explain_graph(
+        prob, hetero_edge_mask = self.explain_graph(
             graph, feat, tmp=tmp, training=True, **kwargs
         )
 
         # store edge mask in homogeneous format
-        self.edge_mask = torch.concat(list(edge_mask.values()))
+        self.edge_mask = torch.concat(list(hetero_edge_mask.values()))
 
         loss_tmp = self.loss(prob, pred)
         return loss_tmp
@@ -702,9 +556,9 @@ class HeteroPGExplainer(nn.Module):
         embed = self.model(graph, feat, embed=True, **kwargs)
         embed_flat = torch.concat(list(embed.values()))
 
-        g_homo = to_homogeneous(graph, store_type=True)
+        homo_graph = to_homogeneous(graph, store_type=True)
 
-        edge_idx = g_homo.edges()
+        edge_idx = homo_graph.edges()
 
         col, row = edge_idx
         col_emb = embed_flat[col.long()]
@@ -716,16 +570,16 @@ class HeteroPGExplainer(nn.Module):
         values = self.concrete_sample(values, beta=tmp, training=training)
         self.sparse_mask_values = values
 
-        reverse_eids = g_homo.edge_ids(row, col).long()
-        edge_mask_homo = (values + values[reverse_eids]) / 2
+        reverse_eids = homo_graph.edge_ids(row, col).long()
+        edge_mask = (values + values[reverse_eids]) / 2
 
         self.clear_masks()
-        self.set_masks(g_homo, edge_mask_homo)
+        self.set_masks(homo_graph, edge_mask)
 
         # convert the edge mask back into heterogeneous format
-        edge_mask = {
-            etype: edge_mask_homo[
-                (g_homo.edata[ETYPE] == graph.get_etype_id(etype))
+        hetero_edge_mask = {
+            etype: edge_mask[
+                (homo_graph.edata[ETYPE] == graph.get_etype_id(etype))
                 .nonzero()
                 .squeeze(1)
             ]
@@ -733,9 +587,13 @@ class HeteroPGExplainer(nn.Module):
         }
 
         # the model prediction with the updated edge mask
-        logits = self.model(graph, feat, edge_weight=edge_mask, **kwargs)
+        logits = self.model(graph, feat, edge_weight=hetero_edge_mask, **kwargs)
         probs = F.softmax(logits, dim=-1)
 
         self.clear_masks()
 
-        return (probs, edge_mask) if training else (probs.data, edge_mask)
+        return (
+            (probs, hetero_edge_mask)
+            if training
+            else (probs.data, hetero_edge_mask)
+        )
