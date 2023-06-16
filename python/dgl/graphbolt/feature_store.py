a86d5c1a36c037d4c9811ef2693abe36d8b46ea6
czkkkkkk <zekucai@gmail.com>
2023-06-16 08:48:50 +0000

    update

--- python/dgl/graphbolt/feature_store.py
+++ python/dgl/graphbolt/feature_store.py
@@ -8,7 +8,7 @@ class FeatureStore:
     def __init__(self):
         pass
 
-    def read_feature(self, key: str, ids: torch.Tensor = None):
+    def read(self, key: str, ids: torch.Tensor = None):
         """Read a feature from the feature store.
 
         Parameters
@@ -26,9 +26,7 @@ class FeatureStore:
         """
         raise NotImplementedError
 
-    def update_feature(
-        self, key: str, value: torch.Tensor, ids: torch.Tensor = None
-    ):
+    def update(self, key: str, value: torch.Tensor, ids: torch.Tensor = None):
         """Update a feature in the feature store.
 
         This function is used to update a feature in the feature store. The
@@ -42,8 +40,9 @@ class FeatureStore:
         value : torch.Tensor
             The updated value of the feature.
         ids : torch.Tensor, optional
-            The index of the feature. If specified, only the specified indices
-            of the feature will be updated. If None, the entire feature will be
+            The indices of the feature to update. If specified, only the
+            specified indices of the feature will be updated and the indices and
+            value must have the same length. If None, the entire feature will be
             updated.
         """
         raise NotImplementedError
@@ -75,15 +74,15 @@ class InMemoryFeatureStore(FeatureStore):
         ...     "rel": torch.arange(0, 6).view(2, 3),
         ... }
         >>> feature_store = InMemoryFeatureStore(feature_dict)
-        >>> feature_store.read_feature("user", torch.tensor([0, 1, 2]))
+        >>> feature_store.read("user", torch.tensor([0, 1, 2]))
         tensor([0, 1, 2])
-        >>> feature_store.read_feature("item", torch.tensor([0, 1, 2]))
+        >>> feature_store.read("item", torch.tensor([0, 1, 2]))
         tensor([0, 1, 2])
-        >>> feature_store.read_feature("rel", torch.tensor([0]))
+        >>> feature_store.read("rel", torch.tensor([0]))
         tensor([[0, 1, 2]])
-        >>> feature_store.update_feature("user",
+        >>> feature_store.update("user",
         ... torch.ones(3, dtype=torch.long), torch.tensor([0, 1, 2]))
-        >>> feature_store.read_feature("user", torch.tensor([0, 1, 2]))
+        >>> feature_store.read("user", torch.tensor([0, 1, 2]))
         tensor([1, 1, 1])
         """
         super(InMemoryFeatureStore, self).__init__()
@@ -102,7 +101,7 @@ class InMemoryFeatureStore(FeatureStore):
 
         self._feature_dict = feature_dict
 
-    def read_feature(self, key: str, ids: torch.Tensor = None):
+    def read(self, key: str, ids: torch.Tensor = None):
         """Read a feature from the feature store by index.
 
         Parameters
@@ -125,9 +124,7 @@ class InMemoryFeatureStore(FeatureStore):
             return self._feature_dict[key]
         return self._feature_dict[key][ids]
 
-    def update_feature(
-        self, key: str, value: torch.Tensor, ids: torch.Tensor = None
-    ):
+    def update(self, key: str, value: torch.Tensor, ids: torch.Tensor = None):
         """Update a feature in the feature store.
 
         This function is used to update a feature in the feature store. The
@@ -141,11 +138,19 @@ class InMemoryFeatureStore(FeatureStore):
         value : torch.Tensor
             The updated value of the feature.
         ids : torch.Tensor, optional
-            The index of the feature. If specified, only the specified indices
-            of the feature will be updated. If None, the entire feature will be
+            The indices of the feature to update. If specified, only the
+            specified indices of the feature will be updated and the indices and
+            value must have the same length. If None, the entire feature will be
             updated.
         """
         assert (
             key in self._feature_dict
         ), f"key {key} not in {self._feature_dict.keys()}"
-        self._feature_dict[key][ids] = value
+        if ids is None:
+            self._feature_dict[key] = value
+        else:
+            assert ids.shape[0] == value.shape[0], (
+                f"ids and value must have the same length, "
+                f"but got {ids.shape[0]} and {value.shape[0]}."
+            )
+            self._feature_dict[key][ids] = value
--- tests/python/pytorch/graphbolt/test_feature_store.py
+++ tests/python/pytorch/graphbolt/test_feature_store.py
@@ -2,33 +2,39 @@ import torch
 from dgl import graphbolt as gb
 
 
-def test_feature_store():
+def test_in_memory_feature_store():
     a = torch.tensor([1, 2, 3])
     b = torch.tensor([3, 4, 5])
     c = torch.tensor([[1, 2, 3], [4, 5, 6]])
     feature_store = gb.InMemoryFeatureStore({"a": a, "b": b, "c": c})
-    assert torch.equal(feature_store.read_feature("a"), torch.tensor([1, 2, 3]))
-    assert torch.equal(feature_store.read_feature("b"), torch.tensor([3, 4, 5]))
+    assert torch.equal(feature_store.read("a"), torch.tensor([1, 2, 3]))
+    assert torch.equal(feature_store.read("b"), torch.tensor([3, 4, 5]))
     assert torch.equal(
-        feature_store.read_feature("a", torch.tensor([0, 2])),
+        feature_store.read("a", torch.tensor([0, 2])),
         torch.tensor([1, 3]),
     )
     assert torch.equal(
-        feature_store.read_feature("a", torch.tensor([1, 1])),
+        feature_store.read("a", torch.tensor([1, 1])),
         torch.tensor([2, 2]),
     )
     assert torch.equal(
-        feature_store.read_feature("c", torch.tensor([1])),
+        feature_store.read("c", torch.tensor([1])),
         torch.tensor([[4, 5, 6]]),
     )
+    feature_store.write("a", torch.tensor([0, 1, 2]))
+    assert torch.equal(feature_store.read("a"), torch.tensor([0, 1, 2]))
+    assert torch.equal(
+        feature_store.read("a", torch.tensor([0, 2])),
+        torch.tensor([0, 2]),
+    )
     try:
-        feature_store.read_feature("d")
+        feature_store.read("d")
         assert False
     except AssertionError:
         pass
 
     try:
-        feature_store.read_feature("a", torch.tensor([0, 1, 2, 3]))
+        feature_store.read("a", torch.tensor([0, 1, 2, 3]))
         assert False
     except IndexError:
         pass
