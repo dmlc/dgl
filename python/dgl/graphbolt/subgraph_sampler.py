5e9b58eafcd40432cad63844ca311b1bf2205c20
Quan Gan <coin2028@hotmail.com>
2023-06-27 06:23:18 +0000

    address comments

--- python/dgl/graphbolt/subgraph_sampler.py
+++ python/dgl/graphbolt/subgraph_sampler.py
@@ -11,24 +11,21 @@ class SubgraphSampler(IterDataPipe):
     .. code:: python
 
        for data in dp:
-           yield sampler_func(graph, data)
+           yield sampler_func(data)
 
     Parameters
     ----------
     dp : DataPipe
         The datapipe.
-    graph : GraphStorage
-        The graph storage.
     sampler_func : callable
         The subgraph sampling function.
     """
 
-    def __init__(self, dp, graph, sampler_func):
+    def __init__(self, dp, sampler_func):
         super().__init__()
-        self.graph = graph
         self.dp = dp
         self.sampler_func = sampler_func
 
     def __iter__(self):
         for data in self.dp:
-            yield self.sampler_func(self.graph, data)
+            yield self.sampler_func(data)
