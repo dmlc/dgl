0d62a10c48b20c6e42f235d38889f85ed2938a01
Quan Gan <coin2028@hotmail.com>
2023-06-27 07:18:21 +0000

    use modern graph

--- tests/python/pytorch/graphbolt/test_subgraph_sampler.py
+++ tests/python/pytorch/graphbolt/test_subgraph_sampler.py
@@ -1,17 +1,25 @@
 import dgl
 import dgl.graphbolt
+import gb_test_utils
 import torch
 import torchdata.datapipes as dp
 
 
 def test_SubgraphSampler():
-    graph = dgl.add_reverse_edges(dgl.rand_graph(20, 60))
-    sampler = dgl.dataloading.NeighborSampler([2, 2])
+    graph = gb_test_utils.rand_csc_graph(20, 0.15)
 
     def sampler_func(data):
-        return sampler.sample(graph, data)
+        adjs = []
+        seeds = data
+
+        for hop in range(2):
+            sg = graph.sample_neighbors(seeds, torch.LongTensor([2]))
+            seeds = sg.indices
+            adjs.insert(0, sg)
+        return seeds, data, adjs
 
     minibatch_dp = dp.map.SequenceWrapper(torch.arange(10)).batch(2)
+    minibatch_dp = minibatch_dp.to_iter_datapipe().collate()
     sampler_dp = dgl.graphbolt.SubgraphSampler(minibatch_dp, sampler_func)
 
     assert len(list(sampler_dp)) == 5
