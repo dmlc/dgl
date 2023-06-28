0d0ac7a667a5166be92c4733d604ddf9502580f5
Quan Gan <coin2028@hotmail.com>
2023-06-28 06:59:41 +0000

    changes

--- tests/python/pytorch/graphbolt/test_copy_to.py
+++ tests/python/pytorch/graphbolt/test_copy_to.py
@@ -1,11 +1,11 @@
-import dgl.graphbolt
 import unittest
-import torch
-import backend as F
 
-@unittest.skipIf(
-    F._default_context_str == "cpu", "CopyTo needs GPU to test"
-)
+import backend as F
+import dgl.graphbolt
+import torch
+
+
+@unittest.skipIf(F._default_context_str == "cpu", "CopyTo needs GPU to test")
 def test_CopyTo():
     dp = dgl.graphbolt.MinibatchSampler(torch.randn(20), 4)
     dp = dgl.graphbolt.CopyTo(dp, "cuda")
