75b078f7d66b834e993d3c71d86189e9fec20bd5
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2023-06-29 07:38:39 +0000

    fix

--- python/dgl/graphbolt/negative_sampler.py
+++ python/dgl/graphbolt/negative_sampler.py
@@ -1,110 +1,32 @@
 """Negative samplers"""
 
-from functools import partial
 
 import torch
-from torchdata.datapipes.iter import IterDataPipe
 
 from .graph_storage import CSCSamplingGraph
 from .linked_data_format import LinkedDataFormat
 
-__all__ = ["NegativeSampler", "PerSourceUniformGenerator"]
+__all__ = ["PerSourceUniformSampler"]
 
 
-class NegativeSampler(IterDataPipe):
+class _BaseNegativeSampler:
     """
-    A negative sampler.
-
-    It is an iterator equivalent to the following:
-
-    .. code:: python
-
-    for data in data_pipe:
-        yield negative_sample_generator(data, graph, negative_ratio,
-            linked_data_format)
-
-    Parameters
-    ----------
-    data_pipe : IterDataPipe
-        The data pipe, which always refers to a minibatch sampler.
-    negative_sample_generator : callable
-        A callable used to generate negative samples.
-    graph : CSCSamplingGraph
-        The graph for negative sampling.
-    negative_ratio : int
-        The ratio of the number of negative samples to positive samples.
-    linked_data_format : LinkedDataFormat
-        The format of the output data:
-            - Conditioned format: The output data is organized as quadruples
-                `[u, v, [negative heads], [negative tails]]`. In this format,
-                'u' and 'v' represent the source and destination nodes of
-                positive edges, respectively. 'Negative heads' and
-                'negative tails' signify the source and destination nodes of
-                negative edges, respectively.
-            - Independent format: The output data is displayed as triples
-                `[u, v, label]`. Here, 'u' and 'v' denote the source and
-                destination nodes of edges. The 'label' is normally set to
-                0 or 1. A label of 0 signifies a negative edge, while 1
-                indicates a positive edge.
-    Examples
-    --------
-    >>> from dgl import graphbolt as gb
-    >>> indptr = torch.LongTensor([0, 2, 4, 5])
-    >>> indices = torch.LongTensor([1, 2, 0, 2, 0])
-    >>> graph = gb.from_csc(indptr, indices)
-    >>> pos_pairs = (torch.tensor([0, 1]), torch.tensor([1, 2]))
-    >>> item_set = gb.ItemSet(pos_pairs)
-    >>> minibatch_sampler = gb.MinibatchSampler(
-    ... item_set, batch_size=1, shuffle=True, drop_last=False)
-    >>> negative_sampler = gb.NegativeSampler(
-    ... minibatch_sampler, gb.negative_sampler.PerSourceUniformGenerator(),
-    ... graph, 2, gb.LinkedDataFormat.INDEPENDENT,)
-    >>> [pairs for pairs in negative_sampler]
-    [(tensor([0, 0, 0]), tensor([1, 0, 1]), tensor([1, 0, 0])),
-    (tensor([1, 1, 1]), tensor([2, 1, 1]), tensor([1, 0, 0]))]
+    A negative sampler used to generate negative samples and return
+    a mix of positive and negative samples, the format of the output
+    depends on the specified `linked_data_format`.
     """
 
     def __init__(
         self,
-        data_pipe: IterDataPipe,
-        negative_sample_generator: callable,
         graph: CSCSamplingGraph,
         negative_ratio: int,
         linked_data_format: LinkedDataFormat,
     ):
-        super().__init__()
-        self.data_pipe = data_pipe
-        self.negative_sample_generator = partial(
-            negative_sample_generator,
-            graph=graph,
-            negative_ratio=negative_ratio,
-            linked_data_format=linked_data_format,
-        )
-
-    def __iter__(self):
         """
-        Iterate over the data pipe and apply the negative sampling
-        function to each data item.
-        """
-        for data in self.data_pipe:
-            yield self.negative_sample_generator(data)
-
-
-class _BaseNegativeSampleGenerator:
-    def _generate(self, pos_pairs, graph, negative_ratio):
-        raise NotImplementedError
-
-    def __call__(self, pos_pairs, graph, negative_ratio, linked_data_format):
-        """
-        Generates a mix of positive and negative samples, the format of which
-        depends on the specified `linked_data_format`.
+        Initlization for a negative sampler.
 
         Parameters
         ----------
-        pos_pairs : Iterable[Tensor]
-            Represents source-destination positive node pairs.
-        graph : CSCSamplingGraph
-            The graph utilized for negative sampling.
         negative_ratio : int
             The proportion of negative samples to positive samples.
         linked_data_format : LinkedDataFormat
@@ -118,6 +40,29 @@ class _BaseNegativeSampleGenerator:
                 In this case, 'u' and 'v' are the source and destination nodes
                 of an edge, and 'label' indicates whether the edge is negative
                 (0) or positive (1).
+        """
+        super().__init__()
+        self.graph = graph
+        self.negative_ratio = negative_ratio
+        assert (
+            linked_data_format == LinkedDataFormat.CONDITIONED
+            or linked_data_format == LinkedDataFormat.INDEPENDENT
+        ), f"Unsupported data format: {self.linked_data_format}"
+        self.linked_data_format = linked_data_format
+
+    def _generate(self, pos_pairs):
+        raise NotImplementedError
+
+    def __call__(self, pos_pairs):
+        """
+        Generates a mix of positive and negative samples, the format of which
+        depends on the specified `linked_data_format`.
+
+        Parameters
+        ----------
+        pos_pairs : Iterable[Tensor]
+            Represents source-destination node pairs of positive edges, where
+            positive means the edge must exist in the graph.
 
         Returns
         -------
@@ -127,26 +72,26 @@ class _BaseNegativeSampleGenerator:
             `linked_data_format`.
         """
 
-        neg_src, neg_dst = self._generate(pos_pairs, graph, negative_ratio)
+        neg_src, neg_dst = self._generate(pos_pairs)
         pos_src, pos_dst = pos_pairs
-        if linked_data_format == LinkedDataFormat.INDEPENDENT:
+        if self.linked_data_format == LinkedDataFormat.INDEPENDENT:
             pos_labels = torch.ones_like(pos_src)
             neg_labels = torch.zeros_like(neg_src)
             src = torch.cat([pos_src, neg_src])
             dst = torch.cat([pos_dst, neg_dst])
             labels = torch.cat([pos_labels, neg_labels])
             return (src, dst, labels)
-        elif linked_data_format == LinkedDataFormat.CONDITIONED:
-            neg_src = neg_src.view(-1, negative_ratio)
-            neg_dst = neg_dst.view(-1, negative_ratio)
-            return (pos_src, pos_dst, neg_src, neg_dst)
         else:
-            raise ValueError(f"Unsupported data format: {linked_data_format}")
+            neg_src = neg_src.view(-1, self.negative_ratio)
+            neg_dst = neg_dst.view(-1, self.negative_ratio)
+            return (pos_src, pos_dst, neg_src, neg_dst)
 
 
-class PerSourceUniformGenerator(_BaseNegativeSampleGenerator):
-    """Negative samples generator that randomly chooses negative destination
-    nodes for each source node according to a uniform distribution.
+class PerSourceUniformSampler(_BaseNegativeSampler):
+    """Negative samplers randomly select negative destination nodes for each
+    source node based on a uniform distribution. It's important to note that
+    the term 'negative' refers to false negatives, indicating that the sampled
+    pairs are not ensured to be absent in the graph.
 
     For each edge ``(u, v)``, it is supposed to generate `negative_ratio` pairs
     of negative edges ``(u, v')``, where ``v'`` is chosen uniformly from all
@@ -158,16 +103,15 @@ class PerSourceUniformGenerator(_BaseNegativeSampleGenerator):
     >>> indptr = torch.LongTensor([0, 2, 4, 5])
     >>> indices = torch.LongTensor([1, 2, 0, 2, 0])
     >>> graph = gb.from_csc(indptr, indices)
-    >>> generator = gb.negative_sampler.PerSourceUniformGenerator()
     >>> pos_pairs = (torch.tensor([0, 1]), torch.tensor([1, 2]))
-    >>> generator(g, pos_pairs, 2, LinkedDataFormat.INDEPENDENT)
-    (tensor([0, 0, 1, 1]), tensor([1, 2, 0, 1]))
+    >>> linked_data_format = gb.LinkedDataFormat.INDEPENDENT
+    >>> neg_sampler = gb.PerSourceUniformSampler(graph, 1, linked_data_format)
+    >>> neg_sampler(pos_pairs)
+    (tensor([0, 1, 0, 1]), tensor([1, 2, 1, 0]), tensor([1, 1, 0, 0]))
     """
 
-    def _generate(self, pos_pairs, graph, negative_ratio):
-        src, _ = pos_pairs
-        shape, dtype = src.shape, src.dtype
-        shape = (shape[0] * negative_ratio,)
-        neg_src = src.repeat(negative_ratio)
-        neg_dst = torch.randint(0, graph.num_nodes, shape, dtype=dtype)
-        return (neg_src, neg_dst)
+    def _generate(self, pos_pairs):
+        return self.graph.sample_negative_per_source_uniform(
+            pos_pairs,
+            self.negative_ratio,
+        )
--- tests/python/pytorch/graphbolt/test_negative_sampler.py
+++ tests/python/pytorch/graphbolt/test_negative_sampler.py
@@ -17,35 +17,26 @@ def test_NegativeSampler_Independent_Format(negative_ratio):
     # Construct CSCSamplingGraph.
     graph = rand_graph(100, 500)
 
-    # Construct mini-batch sampler
-    num_seeds = 20
-    node_pairs = (
-        torch.arange(0, num_seeds),
-        torch.arange(num_seeds, num_seeds * 2),
-    )
-    item_set = gb.ItemSet(node_pairs)
-    batch_size = 4
-    minibatch_sampler = gb.MinibatchSampler(
-        item_set, batch_size=batch_size, shuffle=True, drop_last=False
-    )
-    negative_sampler = gb.NegativeSampler(
-        minibatch_sampler,
-        gb.negative_sampler.PerSourceUniformGenerator(),
+    # Construct NegativeSampler.
+    negative_sampler = gb.PerSourceUniformSampler(
         graph,
         negative_ratio,
         gb.LinkedDataFormat.INDEPENDENT,
     )
-    for i, (src, dst, labels) in enumerate(negative_sampler):
-        is_last = (i + 1) * batch_size >= num_seeds
-        if not is_last or num_seeds % batch_size == 0:
-            expected_batch_size = batch_size
-        else:
-            expected_batch_size = num_seeds % batch_size
-        assert len(src) == expected_batch_size * (negative_ratio + 1)
-        assert len(dst) == expected_batch_size * (negative_ratio + 1)
-        assert len(labels) == expected_batch_size * (negative_ratio + 1)
-        assert torch.all(torch.eq(labels[:expected_batch_size], 1))
-        assert torch.all(torch.eq(labels[expected_batch_size:], 0))
+    # Perform Negative sampling.
+    num_seeds = 20
+    pos_pairs = (
+        torch.arange(0, num_seeds),
+        torch.arange(num_seeds, num_seeds * 2),
+    )
+    src, dst, label = negative_sampler(pos_pairs)
+
+    # Assertation
+    assert len(src) == num_seeds * (negative_ratio + 1)
+    assert len(dst) == num_seeds * (negative_ratio + 1)
+    assert len(label) == num_seeds * (negative_ratio + 1)
+    assert torch.all(torch.eq(label[:num_seeds], 1))
+    assert torch.all(torch.eq(label[num_seeds:], 0))
 
 
 @pytest.mark.parametrize("negative_ratio", [1, 5, 10, 20])
@@ -53,38 +44,29 @@ def test_NegativeSampler_Conditioned_Format(negative_ratio):
     # Construct CSCSamplingGraph.
     graph = rand_graph(100, 500)
 
-    # Construct mini-batch sampler
-    num_seeds = 20
-    node_pairs = (
-        torch.arange(0, num_seeds),
-        torch.arange(num_seeds, num_seeds * 2),
-    )
-    item_set = gb.ItemSet(node_pairs)
-    batch_size = 4
-    minibatch_sampler = gb.MinibatchSampler(
-        item_set, batch_size=batch_size, shuffle=True, drop_last=False
-    )
-    negative_sampler = gb.NegativeSampler(
-        minibatch_sampler,
-        gb.negative_sampler.PerSourceUniformGenerator(),
+    # Construct NegativeSampler.
+    negative_sampler = gb.PerSourceUniformSampler(
         graph,
         negative_ratio,
         gb.LinkedDataFormat.CONDITIONED,
     )
-    for i, (pos_src, pos_dst, neg_src, neg_dst) in enumerate(negative_sampler):
-        is_last = (i + 1) * batch_size >= num_seeds
-        if not is_last or num_seeds % batch_size == 0:
-            expected_batch_size = batch_size
-        else:
-            expected_batch_size = num_seeds % batch_size
-        assert len(pos_src) == expected_batch_size
-        assert len(pos_dst) == expected_batch_size
-        assert len(neg_src) == expected_batch_size
-        assert len(neg_dst) == expected_batch_size
-        assert neg_src.numel() == expected_batch_size * negative_ratio
-        assert neg_dst.numel() == expected_batch_size * negative_ratio
-        expected_src = pos_src.repeat(negative_ratio).view(-1, negative_ratio)
-        assert torch.equal(expected_src, neg_src)
+    # Perform Negative sampling.
+    num_seeds = 20
+    pos_pairs = (
+        torch.arange(0, num_seeds),
+        torch.arange(num_seeds, num_seeds * 2),
+    )
+    pos_src, pos_dst, neg_src, neg_dst = negative_sampler(pos_pairs)
+
+    # Assertation
+    assert len(pos_src) == num_seeds
+    assert len(pos_dst) == num_seeds
+    assert len(neg_src) == num_seeds
+    assert len(neg_dst) == num_seeds
+    assert neg_src.numel() == num_seeds * negative_ratio
+    assert neg_dst.numel() == num_seeds * negative_ratio
+    expected_src = pos_src.repeat(negative_ratio).view(-1, negative_ratio)
+    assert torch.equal(expected_src, neg_src)
 
 
 if __name__ == "__main__":
