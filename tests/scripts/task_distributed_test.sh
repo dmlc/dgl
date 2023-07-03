4c2df8c0b8573ad699b538872ccb69a8a200fd09
RhettYing <rhett_ying@qq.com>
2023-07-03 23:47:11 +0000

    install pydantic_yaml into CI

--- tests/scripts/task_distributed_test.sh
+++ tests/scripts/task_distributed_test.sh
@@ -34,6 +34,9 @@ export PYTHONUNBUFFERED=1
 export OMP_NUM_THREADS=1
 export DMLC_LOG_DEBUG=1
 
+# Install required dependencies
+python3 -m pytest pydantic_yaml
+
 python3 -m pytest -v --capture=tee-sys --junitxml=pytest_distributed.xml --durations=100 tests/distributed/*.py || fail "distributed"
 
 PYTHONPATH=tools:tools/distpartitioning:$PYTHONPATH python3 -m pytest -v --capture=tee-sys --junitxml=pytest_tools.xml --durations=100 tests/tools/*.py || fail "tools"
