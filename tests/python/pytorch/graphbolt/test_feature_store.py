76f2cb2cb2bffa6aa5a700ea612108dacbc4624e
czkkkkkk <zekucai@gmail.com>
2023-07-03 07:34:51 +0000

    [Graphbolt] Remove keys in feature store.

--- tests/python/pytorch/graphbolt/test_feature_store.py
+++ tests/python/pytorch/graphbolt/test_feature_store.py
@@ -21,40 +21,39 @@ def to_on_disk_tensor(test_dir, name, t):
 def test_torch_based_feature_store(in_memory):
     with tempfile.TemporaryDirectory() as test_dir:
         a = torch.tensor([1, 2, 3])
-        b = torch.tensor([3, 4, 5])
-        c = torch.tensor([[1, 2, 3], [4, 5, 6]])
+        b = torch.tensor([[1, 2, 3], [4, 5, 6]])
         if not in_memory:
             a = to_on_disk_tensor(test_dir, "a", a)
             b = to_on_disk_tensor(test_dir, "b", b)
-            c = to_on_disk_tensor(test_dir, "c", c)
 
-        feature_store = gb.TorchBasedFeatureStore({"a": a, "b": b, "c": c})
-        assert torch.equal(feature_store.read("a"), torch.tensor([1, 2, 3]))
-        assert torch.equal(feature_store.read("b"), torch.tensor([3, 4, 5]))
+        feat_store_a = gb.TorchBasedFeatureStore(a)
+        feat_store_b = gb.TorchBasedFeatureStore(b)
+
+        assert torch.equal(feat_store_a.read(), torch.tensor([1, 2, 3]))
         assert torch.equal(
-            feature_store.read("a", torch.tensor([0, 2])),
+            feat_store_b.read(), torch.tensor([[1, 2, 3], [4, 5, 6]])
+        )
+        assert torch.equal(
+            feat_store_a.read(torch.tensor([0, 2])),
             torch.tensor([1, 3]),
         )
         assert torch.equal(
-            feature_store.read("a", torch.tensor([1, 1])),
+            feat_store_a.read(torch.tensor([1, 1])),
             torch.tensor([2, 2]),
         )
         assert torch.equal(
-            feature_store.read("c", torch.tensor([1])),
+            feat_store_b.read(torch.tensor([1])),
             torch.tensor([[4, 5, 6]]),
         )
-        feature_store.update("a", torch.tensor([0, 1, 2]))
-        assert torch.equal(feature_store.read("a"), torch.tensor([0, 1, 2]))
-        assert torch.equal(
-            feature_store.read("a", torch.tensor([0, 2])),
-            torch.tensor([0, 2]),
-        )
-        with pytest.raises(AssertionError):
-            feature_store.read("d")
+        feat_store_a.update(torch.tensor([0, 1, 2]), torch.tensor([0, 1, 2]))
+        assert torch.equal(feat_store_a.read(), torch.tensor([0, 1, 2]))
+        feat_store_a.update(torch.tensor([2, 0]), torch.tensor([0, 2]))
+        assert torch.equal(feat_store_a.read(), torch.tensor([2, 1, 0]))
 
         with pytest.raises(IndexError):
-            feature_store.read("a", torch.tensor([0, 1, 2, 3]))
+            feat_store_a.read(torch.tensor([0, 1, 2, 3]))
 
         # For windows, the file is locked by the numpy.load. We need to delete
         # it before closing the temporary directory.
-        a = b = c = feature_store = None
+        a = b = None
+        feat_store_a = feat_store_b = None
