17cfbab4abfd32f3019a97b001d243ebed535e03
Ubuntu <ubuntu@ip-172-31-6-31.ap-northeast-1.compute.internal>
2023-07-03 06:35:05 +0000

    use pyhton API for relabel_nodes

--- tests/python/common/test_subgraph.py
+++ tests/python/common/test_subgraph.py
@@ -54,12 +54,12 @@ def test_edge_subgraph():
     sg.edata["h"] = F.arange(0, sg.num_edges())
 
 
-def test_subgraph():
+def test_subgraph_relabel_nodes():
     g = generate_graph()
     h = g.ndata["h"]
     l = g.edata["l"]
     nid = [0, 2, 3, 6, 7, 9]
-    sg = g.subgraph(nid)
+    sg = g.subgraph(nid, relabel_nodes = True)
     eid = {2, 3, 4, 5, 10, 11, 12, 13, 16}
     assert set(F.asnumpy(sg.edata[dgl.EID])) == eid
     eid = sg.edata[dgl.EID]
@@ -92,6 +92,48 @@ def test_subgraph():
     # update the node/edge features on the subgraph should NOT
     # reflect to the parent graph.
     sg.ndata["h"] = F.zeros((6, D))
+    assert F.allclose(h, g.ndata["h"])
+
+
+def test_subgraph_not_relabel_nodes():
+    g = generate_graph()
+    h = g.ndata["h"]
+    l = g.edata["l"]
+    nid = [0, 2, 3, 6, 7, 9]
+    sg = g.subgraph(nid, relabel_nodes = False)
+    eid = {2, 3, 4, 5, 10, 11, 12, 13, 16}
+    assert set(F.asnumpy(sg.edata[dgl.EID])) == eid
+    eid = sg.edata[dgl.EID]
+    # the subgraph is empty initially except for NID/EID field
+    assert len(sg.edata) == 2
+    sh = sg.ndata["h"]
+    assert F.allclose(
+        F.gather_row(h, F.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])), sh
+    )
+    """
+    s, d, eid
+    0, 1, 0
+    1, 9, 1
+    0, 2, 2    1
+    2, 9, 3    1
+    0, 3, 4    1
+    3, 9, 5    1
+    0, 4, 6
+    4, 9, 7
+    0, 5, 8
+    5, 9, 9       3
+    0, 6, 10   1
+    6, 9, 11   1  3
+    0, 7, 12   1
+    7, 9, 13   1  3
+    0, 8, 14
+    8, 9, 15      3
+    9, 0, 16   1
+    """
+    assert F.allclose(F.gather_row(l, eid), sg.edata["l"])
+    # update the node/edge features on the subgraph should NOT
+    # reflect to the parent graph.
+    sg.ndata["h"] = F.zeros((10, D))
     assert F.allclose(h, g.ndata["h"])
 
 
@@ -838,6 +880,7 @@ def test_uva_subgraph(idtype, device):
 
 
 if __name__ == "__main__":
-    test_edge_subgraph()
-    test_uva_subgraph(F.int64, F.cpu())
-    test_uva_subgraph(F.int64, F.cuda())
+    # test_edge_subgraph()
+    # test_uva_subgraph(F.int64, F.cpu())
+    # test_uva_subgraph(F.int64, F.cuda())
+    test_subgraph_not_relabel_nodes()
