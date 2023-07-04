39890c0c5d35e13a53c641a8a9f5b8ccef7c7d91
Rhett Ying <85214957+Rhett-Ying@users.noreply.github.com>
2023-07-04 20:55:26 +0800

    [GraphBolt] add pydantic-based metadata for TVT (#5942)

--- tests/python/pytorch/graphbolt/test_dataset.py
+++ tests/python/pytorch/graphbolt/test_dataset.py
@@ -1,3 +1,7 @@
+import os
+import tempfile
+
+import pydantic
 import pytest
 from dgl import graphbolt as gb
 
@@ -14,3 +18,36 @@ def test_Dataset():
         _ = dataset.graph()
     with pytest.raises(NotImplementedError):
         _ = dataset.feature()
+
+
+def test_OnDiskDataset_TVTSet():
+    """Test OnDiskDataset with TVTSet."""
+    with tempfile.TemporaryDirectory() as test_dir:
+        yaml_content = """
+        train_set:
+          - - type_name: paper
+              format: torch
+              path: set/paper-train.pt
+            - type_name: 'paper:cites:paper'
+              format: numpy
+              path: set/cites-train.pt
+        """
+        yaml_file = os.path.join(test_dir, "test.yaml")
+        with open(yaml_file, "w") as f:
+            f.write(yaml_content)
+        _ = gb.OnDiskDataset(yaml_file)
+
+        # Invalid format.
+        yaml_content = """
+        train_set:
+          - - type_name: paper
+              format: torch_invalid
+              path: set/paper-train.pt
+            - type_name: 'paper:cites:paper'
+              format: numpy_invalid
+              path: set/cites-train.pt
+        """
+        with open(yaml_file, "w") as f:
+            f.write(yaml_content)
+        with pytest.raises(pydantic.ValidationError):
+            _ = gb.OnDiskDataset(yaml_file)
