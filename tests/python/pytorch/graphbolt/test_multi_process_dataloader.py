bab7dba42a5c14c17d759b987852cd3634a61005
Quan Gan <coin2028@hotmail.com>
2023-07-08 12:39:32 +0000

    make the functions picklable

--- tests/python/pytorch/graphbolt/test_multi_process_dataloader.py
+++ tests/python/pytorch/graphbolt/test_multi_process_dataloader.py
@@ -1,8 +1,23 @@
+from functools import partial
+
 import backend as F
 import dgl
 import dgl.graphbolt
 import gb_test_utils
 import torch
+
+
+def sampler_func(graph, data):
+    seeds = data
+    sampler = dgl.dataloading.NeighborSampler([2, 2])
+    return sampler.sample(graph, seeds)
+
+
+def fetch_func(features, labels, data):
+    input_nodes, output_nodes, adjs = data
+    input_features = features.read(input_nodes)
+    output_labels = labels.read(output_nodes)
+    return input_features, output_labels, adjs
 
 
 def test_DataLoader():
@@ -19,23 +34,15 @@ def test_DataLoader():
         torch.randint(0, 10, (200,))
     )
 
-    def sampler_func(data):
-        seeds = data
-        sampler = dgl.dataloading.NeighborSampler([2, 2])
-        return sampler.sample(graph, seeds)
-
-    def fetch_func(data):
-        input_nodes, output_nodes, adjs = data
-        input_features = features.read(input_nodes)
-        output_labels = labels.read(output_nodes)
-        return input_features, output_labels, adjs
-
     minibatch_sampler = dgl.graphbolt.MinibatchSampler(itemset, batch_size=B)
     subgraph_sampler = dgl.graphbolt.SubgraphSampler(
         minibatch_sampler,
-        sampler_func,
+        partial(sampler_func, graph),
     )
-    feature_fetcher = dgl.graphbolt.FeatureFetcher(subgraph_sampler, fetch_func)
+    feature_fetcher = dgl.graphbolt.FeatureFetcher(
+        subgraph_sampler,
+        partial(fetch_func, features, labels),
+    )
     device_transferrer = dgl.graphbolt.CopyTo(feature_fetcher, F.ctx())
 
     dataloader = dgl.graphbolt.MultiProcessDataLoader(
