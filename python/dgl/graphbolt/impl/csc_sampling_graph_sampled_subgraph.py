10055941a523da13f907a659a661d000f57cee4c
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2023-07-10 06:38:27 +0000

    refactor

--- python/dgl/graphbolt/impl/csc_sampling_graph_sampled_subgraph.py
+++ python/dgl/graphbolt/impl/csc_sampling_graph_sampled_subgraph.py
@@ -1,19 +1,24 @@
 from dataclasses import dataclass
-from typing import Dict, Tuple, Union
-
+from typing import Union, Dict, Tuple
 import torch
+
+from ..sampled_subgraph import SampledSubGraph
 
 
 @dataclass
-class CSCSamplingGraphSampledSubGraph:
-    r"""Class for sampled subgraph."""
+class CSCSamplingGraphSampledSubgraph(SampledSubGraph):
+    r"""Class for sampled subgraph specific for CSCSamplingGraph."""
 
     node_pairs: Union[
         Dict[Tuple[str, str, str], Tuple[torch.tensor, torch.tensor]],
         Tuple[torch.tensor, torch.tensor],
-    ]
-    reverse_column_node_ids: Union[Dict[str, torch.tensor], torch.tensor] = None
-    reverse_row_node_ids: Union[Dict[str, torch.tensor], torch.tensor] = None
+    ] = None
+    reverse_column_node_ids: Union[
+        Dict[str, torch.tensor], torch.tensor
+    ] = None
+    reverse_row_node_ids: Union[
+        Dict[str, torch.tensor], torch.tensor
+    ] = None
     reverse_edge_ids: Union[
         Dict[Tuple[str, str, str], torch.tensor], torch.tensor
     ] = None
@@ -31,12 +36,12 @@ class CSCSamplingGraphSampledSubGraph:
                     isinstance(pair, tuple) and len(pair) == 2
                 ), "Node pair should be a source-destination tuple (u, v)."
                 assert all(
-                    isinstance(item, str) for item in pair
-                ), "Nodes in pairs should be of type string."
+                    isinstance(item, torch.Tensor) for item in pair
+                ), "Nodes in pairs should be of type torch.Tensor."
         else:
             assert (
                 isinstance(self.node_pairs, tuple) and len(self.node_pairs) == 2
             ), "Node pair should be a source-destination tuple (u, v)."
             assert all(
-                isinstance(item, str) for item in self.node_pairs
-            ), "Nodes in pairs should be of type string."
+                isinstance(item, torch.Tensor) for item in self.node_pairs
+            ), "Nodes in pairs should be of type torch.Tensor."
