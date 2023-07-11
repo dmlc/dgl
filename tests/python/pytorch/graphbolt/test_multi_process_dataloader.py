ea9365e1200b4e942028090cc43b3f62162d99e6
Quan Gan <coin2028@hotmail.com>
2023-07-11 07:52:30 +0000

    sync

--- tests/python/pytorch/graphbolt/test_multi_process_dataloader.py
+++ tests/python/pytorch/graphbolt/test_multi_process_dataloader.py
@@ -27,10 +27,10 @@ def test_DataLoader():
     # TODO(BarclayII): temporarily using DGLGraph.  Should test using
     # GraphBolt's storage as well once issue #5953 is resolved.
     graph = dgl.add_reverse_edges(dgl.rand_graph(200, 6000))
-    features = dgl.graphbolt.feature_store.TorchBasedFeatureStore(
+    features = dgl.graphbolt.TorchBasedFeatureStore(
         torch.randn(200, 4)
     )
-    labels = dgl.graphbolt.feature_store.TorchBasedFeatureStore(
+    labels = dgl.graphbolt.TorchBasedFeatureStore(
         torch.randint(0, 10, (200,))
     )
 
