1a956d2181f2aef658c57c2e917203a3e461940c
Quan Gan <coin2028@hotmail.com>
2023-07-12 05:46:40 +0000

    remove 1.12 logic

--- python/dgl/graphbolt/datapipe_utils.py
+++ python/dgl/graphbolt/datapipe_utils.py
@@ -1,41 +1,11 @@
 """DataPipe utilities"""
 
-# pylint: disable=unused-import
-try:
-    from torchdata.dataloader2.graph import traverse_dps
 
-    DATAPIPE_COMPATIBILITY = "0.5"
-except ImportError:
-    # PyTorch 1.12-
-    from torchdata.dataloader2.graph import traverse
-
-    DATAPIPE_COMPATIBILITY = "0.4"
-
-    def traverse_dps(datapipe):
-        """Wrapper of PyTorch 1.12 ``traverse`` function to PyTorch 1.13
-        ``traverse_dps`` interface.
-        """
-        return traverse(datapipe, True)
-
-
-if DATAPIPE_COMPATIBILITY == "0.4":
-
-    def _get_parents(result_dict, datapipe_graph):
-        for k, parents in datapipe_graph.items():
-            if k not in result_dict:
-                result_dict[id(k)] = (
-                    k,
-                    [id(datapipe) for datapipe in parents.keys()],
-                )
-                _get_parents(result_dict, parents)
-
-else:
-
-    def _get_parents(result_dict, datapipe_graph):
-        for k, (v, parents) in datapipe_graph.items():
-            if k not in result_dict:
-                result_dict[k] = (v, list(parents.keys()))
-                _get_parents(result_dict, parents)
+def _get_parents(result_dict, datapipe_graph):
+    for k, (v, parents) in datapipe_graph.items():
+        if k not in result_dict:
+            result_dict[k] = (v, list(parents.keys()))
+            _get_parents(result_dict, parents)
 
 
 def datapipe_graph_to_adjlist(datapipe_graph):
