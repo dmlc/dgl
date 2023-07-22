a1d67e73e39177f662c7e31be6b67856683758a3
jwzhi <jingzhuu@umich.edu>
2023-07-21 19:27:12 -0700

    update

--- python/dgl/dataloading/base.py
+++ python/dgl/dataloading/base.py
@@ -314,6 +314,29 @@ def _find_exclude_eids(g, exclude_mode, eids, **kwargs):
         raise ValueError("unsupported mode {}".format(exclude_mode))
 
 class SpotTarget(object):
+    """Callable excluder object to exclude the edges from a graph according to the specified degree threshold
+
+    Besides excluding all the edges or given edges in the edge sampler ``dgl.dataloading.as_edge_prediction_sampler`` 
+    in link prediction training, this excluder can extend the exclusion function by only excluding the edges incident 
+    to low-degree nodes in the graph to bring the performance increase in training link prediction model. 
+    This function will exclude the edge if incident to at least one node with degree larger or equal to ``degree_threshold``. 
+    The performance boost by excluding the target edges incident to low-degree nodes can be found in this paper: https://arxiv.org/abs/2306.00899
+
+    Parameters
+    ----------
+    g : DGLGraph
+        The graph.
+    degree_threshold : int
+        The threshold of node degrees, if the source or target node of an edge incident to 
+        has larger or equal degrees than ``degree_threshold``, this edge will be excluded from 
+        the graph 
+    Examples
+    --------
+    .. code:: python
+        low_degree_excluder = SpotTarget(g, degree_threshold=10)
+        sampler = as_edge_prediction_sampler(sampler, exclude=low_degree_excluder, reverse_eids=reverse_eids,
+                                                        negative_sampler=negative_sampler.Uniform(1))
+    """
     def __init__(self, g, degree_threshold=10):
         self.g = g
         self.degree_threshold = degree_threshold
