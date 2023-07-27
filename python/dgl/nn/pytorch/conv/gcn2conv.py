416e2425b1e7a28d17e3645daebd196ca2786168
Hongzhi (Steve), Chen <chenhongzhi.nkcs@gmail.com>
2023-07-27 13:13:50 +0800

    [Misc] Further polish gcn2conv.py (#6046)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-28-63.ap-northeast-1.compute.internal>

--- python/dgl/nn/pytorch/conv/gcn2conv.py
+++ python/dgl/nn/pytorch/conv/gcn2conv.py
@@ -254,14 +254,14 @@ class GCN2Conv(nn.Module):
 
             # initial residual connection to the first layer
             feat_0 = feat_0[: feat.size(0)] * self.alpha
+            feat_sum = feat + feat_0
 
             if self._project_initial_features:
-                feat_sum = feat + feat_0
-                rst = (1 - self.beta) * feat_sum + self.beta * feat_sum @ self.weight1
+                feat_proj_sum = feat_sum @ self.weight1
             else:
-                feat_sum = feat + feat_0
                 feat_proj_sum = feat @ self.weight1 + feat_0 @ self.weight2
-                rst = (1 - self.beta) * feat_sum + self.beta * feat_proj_sum
+
+            rst = (1 - self.beta) * feat_sum + self.beta * feat_proj_sum
 
             if self._bias:
                 rst = rst + self.bias
