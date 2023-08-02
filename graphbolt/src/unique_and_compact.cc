641d13c0486f3d0a7b2203f77072df55000e8094
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2023-08-02 04:14:10 +0000

    add compact

---
+++ graphbolt/src/unique_and_compact.cc
@@ -0,0 +1,15 @@
+/**
+ *  Copyright (c) 2023 by Contributors
+ *
+ * @file unique_and_compact.cc
+ * @brief Unique and compact op.
+ */
+#ifndef GRAPHBOLT_UNIQUE_AND_COMPACT_H_
+#define GRAPHBOLT_UNIQUE_AND_COMPACT_H_
+
+namespace graphbolt {
+namespace sampling {
+    
+}
+}
+#endif GRAPHBOLT_UNIQUE_AND_COMPACT_H_
