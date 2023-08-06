fa26d3c54272f1cfdf8a35d4de719b3b8f0db2a8
Muhammed Fatih Balin <m.f.balin@gmail.com>
2023-08-06 14:39:26 -0400

    simplify labor example, add proper inference code

--- examples/pytorch/labor/model.py
+++ examples/pytorch/labor/model.py
@@ -1,3 +1,5 @@
+import tqdm
+
 import dgl
 import dgl.nn as dglnn
 import sklearn.linear_model as lm
@@ -5,85 +7,6 @@ import sklearn.metrics as skm
 import torch as th
 import torch.functional as F
 import torch.nn as nn
-
-from dgl.nn import GATv2Conv
-
-
-class GATv2(nn.Module):
-    def __init__(
-        self,
-        num_layers,
-        in_dim,
-        num_hidden,
-        num_classes,
-        heads,
-        activation,
-        feat_drop,
-        attn_drop,
-        negative_slope,
-        residual,
-    ):
-        super(GATv2, self).__init__()
-        self.num_layers = num_layers
-        self.gatv2_layers = nn.ModuleList()
-        self.activation = activation
-        # input projection (no residual)
-        self.gatv2_layers.append(
-            GATv2Conv(
-                in_dim,
-                num_hidden,
-                heads[0],
-                feat_drop,
-                attn_drop,
-                negative_slope,
-                False,
-                self.activation,
-                True,
-                bias=False,
-                share_weights=True,
-            )
-        )
-        # hidden layers
-        for l in range(1, num_layers - 1):
-            # due to multi-head, the in_dim = num_hidden * num_heads
-            self.gatv2_layers.append(
-                GATv2Conv(
-                    num_hidden * heads[l - 1],
-                    num_hidden,
-                    heads[l],
-                    feat_drop,
-                    attn_drop,
-                    negative_slope,
-                    residual,
-                    self.activation,
-                    True,
-                    bias=False,
-                    share_weights=True,
-                )
-            )
-        # output projection
-        self.gatv2_layers.append(
-            GATv2Conv(
-                num_hidden * heads[-2],
-                num_classes,
-                heads[-1],
-                feat_drop,
-                attn_drop,
-                negative_slope,
-                residual,
-                None,
-                True,
-                bias=False,
-                share_weights=True,
-            )
-        )
-
-    def forward(self, mfgs, h):
-        for l, mfg in enumerate(mfgs):
-            h = self.gatv2_layers[l](mfg, h)
-            h = h.flatten(1) if l < self.num_layers - 1 else h.mean(1)
-        return h
-
 
 class SAGE(nn.Module):
     def __init__(
@@ -124,87 +47,41 @@ class SAGE(nn.Module):
                 h = self.dropout(h)
         return h
 
-
-class RGAT(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        hidden_channels,
-        num_etypes,
-        num_layers,
-        num_heads,
-        dropout,
-        pred_ntype,
-    ):
-        super().__init__()
-        self.convs = nn.ModuleList()
-        self.norms = nn.ModuleList()
-        self.skips = nn.ModuleList()
-
-        self.convs.append(
-            nn.ModuleList(
-                [
-                    dglnn.GATConv(
-                        in_channels,
-                        hidden_channels // num_heads,
-                        num_heads,
-                        allow_zero_in_degree=True,
-                    )
-                    for _ in range(num_etypes)
-                ]
-            )
+    def inference(self, g, device, batch_size, num_workers, buffer_device=None):
+        # The difference between this inference function and the one in the official
+        # example is that the intermediate results can also benefit from prefetching.
+        g.ndata["h"] = g.ndata["features"]
+        sampler = dgl.dataloading.MultiLayerFullNeighborSampler(
+            1, prefetch_node_feats=["h"]
         )
-        self.norms.append(nn.BatchNorm1d(hidden_channels))
-        self.skips.append(nn.Linear(in_channels, hidden_channels))
-        for _ in range(num_layers - 1):
-            self.convs.append(
-                nn.ModuleList(
-                    [
-                        dglnn.GATConv(
-                            hidden_channels,
-                            hidden_channels // num_heads,
-                            num_heads,
-                            allow_zero_in_degree=True,
-                        )
-                        for _ in range(num_etypes)
-                    ]
-                )
-            )
-            self.norms.append(nn.BatchNorm1d(hidden_channels))
-            self.skips.append(nn.Linear(hidden_channels, hidden_channels))
-
-        self.mlp = nn.Sequential(
-            nn.Linear(hidden_channels, hidden_channels),
-            nn.BatchNorm1d(hidden_channels),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_channels, out_channels),
+        dataloader = dgl.dataloading.DataLoader(
+            g,
+            th.arange(g.num_nodes(), dtype=g.idtype, device=g.device),
+            sampler,
+            device=device,
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=num_workers,
+            persistent_workers=(num_workers > 0),
         )
-        self.dropout = nn.Dropout(dropout)
+        if buffer_device is None:
+            buffer_device = device
+        
+        self.train(False)
 
-        self.hidden_channels = hidden_channels
-        self.pred_ntype = pred_ntype
-        self.num_etypes = num_etypes
-
-    def forward(self, mfgs, x):
-        for i in range(len(mfgs)):
-            mfg = mfgs[i]
-            x_dst = x[mfg.dst_in_src]
-            for data in [mfg.srcdata, mfg.dstdata]:
-                for k in list(data.keys()):
-                    if k not in ["features", "labels"]:
-                        data.pop(k)
-            mfg = dgl.block_to_graph(mfg)
-            x_skip = self.skips[i](x_dst)
-            for j in range(self.num_etypes):
-                subg = mfg.edge_subgraph(
-                    mfg.edata["etype"] == j, relabel_nodes=False
-                )
-                x_skip += self.convs[i][j](subg, (x, x_dst)).view(
-                    -1, self.hidden_channels
-                )
-            x = self.norms[i](x_skip)
-            x = th.nn.functional.elu(x)
-            x = self.dropout(x)
-        return self.mlp(x)
+        for l, layer in enumerate(self.layers):
+            y = th.zeros(
+                g.num_nodes(),
+                self.n_hidden if l != len(self.layers) - 1 else self.n_classes,
+                device=buffer_device,
+            )
+            for input_nodes, output_nodes, blocks in tqdm.tqdm(dataloader):
+                x = blocks[0].srcdata["h"]
+                h = layer(blocks[0], x)
+                if l != len(self.layers) - 1:
+                    h = self.activation(h)
+                    h = self.dropout(h)
+                y[output_nodes] = h.to(buffer_device)
+            g.ndata["h"] = y
+        return y
