e3420519eccba43aff031d9ddec18674395b7311
keli-wen <whuwkl@gmail.com>
2023-08-07 06:39:34 +0000

    add a impl function to avoid too large with body

--- python/dgl/graphbolt/impl/ondisk_dataset.py
+++ python/dgl/graphbolt/impl/ondisk_dataset.py
@@ -46,6 +46,179 @@ def fix_dataset_dir(path):
         os.chdir(old_pwd)
 
 
+def preprocess_ondisk_dataset_impl(dataset_dir: str) -> str:
+    """The implementation of the preprocessing function."""
+    # 0. Check if the dataset is already preprocessed.
+    if os.path.exists("preprocessed/metadata.yaml"):
+        print("The dataset is already preprocessed.")
+        return os.path.join(dataset_dir, "preprocessed/metadata.yaml")
+
+    print("Start to preprocess the on-disk dataset.")
+    # Read the input config.
+    processed_dir_prefix = Path("preprocessed")
+
+    # Check if the metadata.yaml exists.
+    if not os.path.exists("metadata.yaml"):
+        raise RuntimeError("metadata.yaml does not exist.")
+    with open("metadata.yaml", "r") as f:
+        input_config = yaml.safe_load(f)
+
+    # 1. Make `processed_dir_prefix` directory if it does not exist.
+    os.makedirs(processed_dir_prefix, exist_ok=True)
+    output_config = deepcopy(input_config)
+
+    # 2. Load the edge data and create a DGLGraph.
+    if "graph" not in input_config:
+        raise RuntimeError("Invalid config: does not contain graph field.")
+    is_homogeneous = "type" not in input_config["graph"]["nodes"][0]
+    if is_homogeneous:
+        # Homogeneous graph.
+        num_nodes = input_config["graph"]["nodes"][0]["num"]
+        edge_data = pd.read_csv(
+            input_config["graph"]["edges"][0]["path"],
+            names=["src", "dst"],
+        )
+        src, dst = edge_data["src"].to_numpy(), edge_data["dst"].to_numpy()
+
+        g = dgl.graph((src, dst), num_nodes=num_nodes)
+    else:
+        # Heterogeneous graph.
+        # Construct the num nodes dict.
+        num_nodes_dict = {}
+        for node_info in input_config["graph"]["nodes"]:
+            num_nodes_dict[node_info["type"]] = node_info["num"]
+        # Construct the data dict.
+        data_dict = {}
+        for edge_info in input_config["graph"]["edges"]:
+            edge_data = pd.read_csv(edge_info["path"], names=["src", "dst"])
+            src = torch.tensor(edge_data["src"])
+            dst = torch.tensor(edge_data["dst"])
+            data_dict[tuple(edge_info["type"].split(":"))] = (src, dst)
+        # Construct the heterograph.
+        g = dgl.heterograph(data_dict, num_nodes_dict)
+
+    # 3. Load the sampling related node/edge features and add them to
+    # the sampling-graph.
+    if input_config["graph"].get("feature_data", None):
+        for graph_feature in input_config["graph"]["feature_data"]:
+            if graph_feature["domain"] == "node":
+                node_data = read_data(
+                    graph_feature["path"],
+                    graph_feature["format"],
+                    in_memory=graph_feature["in_memory"],
+                )
+                g.ndata[graph_feature["name"]] = node_data
+            if graph_feature["domain"] == "edge":
+                edge_data = read_data(
+                    graph_feature["path"],
+                    graph_feature["format"],
+                    in_memory=graph_feature["in_memory"],
+                )
+                g.edata[graph_feature["name"]] = edge_data
+
+    # 4. Convert the DGLGraph to a CSCSamplingGraph.
+    csc_sampling_graph = from_dglgraph(g)
+
+    # 5. Save the CSCSamplingGraph and modify the output_config.
+    output_config["graph_topology"] = {}
+    output_config["graph_topology"]["type"] = "CSCSamplingGraph"
+    output_config["graph_topology"]["path"] = str(
+        processed_dir_prefix / "csc_sampling_graph.tar"
+    )
+
+    save_csc_sampling_graph(
+        csc_sampling_graph,
+        str(output_config["graph_topology"]["path"]),
+    )
+    del output_config["graph"]
+
+    # 6. Load the node/edge features and do necessary conversion.
+    if input_config.get("feature_data", None):
+        for feature, out_feature in zip(
+            input_config["feature_data"], output_config["feature_data"]
+        ):
+            # Always save the feature in numpy format.
+            out_feature["format"] = "numpy"
+            out_feature["path"] = str(
+                processed_dir_prefix / feature["path"].replace("pt", "npy")
+            )
+
+            if feature["format"] == "numpy":
+                # If the original format is numpy, just copy the file.
+                os.makedirs(
+                    os.path.dirname(out_feature["path"]),
+                    exist_ok=True,
+                )
+                shutil.copyfile(
+                    feature["path"],
+                    out_feature["path"],
+                )
+            else:
+                # If the original format is not numpy, convert it to numpy.
+                data = read_data(
+                    feature["path"],
+                    feature["format"],
+                    in_memory=feature["in_memory"],
+                )
+                save_data(
+                    data,
+                    out_feature["path"],
+                    out_feature["format"],
+                )
+
+    # 7. Save the train/val/test split according to the output_config.
+    for set_name in ["train_sets", "validation_sets", "test_sets"]:
+        if set_name not in input_config:
+            continue
+        for intput_set_split, output_set_split in zip(
+            input_config[set_name], output_config[set_name]
+        ):
+            for input_set_per_type, output_set_per_type in zip(
+                intput_set_split, output_set_split
+            ):
+                for input_data, output_data in zip(
+                    input_set_per_type["data"], output_set_per_type["data"]
+                ):
+                    # Always save the feature in numpy format.
+                    output_data["format"] = "numpy"
+                    output_data["path"] = str(
+                        processed_dir_prefix
+                        / input_data["path"].replace("pt", "npy")
+                    )
+                    if input_data["format"] == "numpy":
+                        # If the original format is numpy, just copy the
+                        # file.
+                        os.makedirs(
+                            os.path.dirname(output_data["path"]),
+                            exist_ok=True,
+                        )
+                        shutil.copyfile(
+                            input_data["path"],
+                            output_data["path"],
+                        )
+                    else:
+                        # If the original format is not numpy, convert it
+                        # to numpy.
+                        input_set = read_data(
+                            input_data["path"],
+                            input_data["format"],
+                        )
+                        save_data(
+                            input_set,
+                            output_data["path"],
+                            output_set_per_type["format"],
+                        )
+
+    # 8. Save the output_config.
+    output_config_path = processed_dir_prefix / "metadata.yaml"
+    with open(output_config_path, "w") as f:
+        yaml.dump(output_config, f)
+    print("Finish preprocessing the on-disk dataset.")
+
+    # 9. Return the absolute path of the preprocessing yaml file.
+    return str(dataset_dir / output_config_path)
+
+
 def preprocess_ondisk_dataset(dataset_dir: str) -> str:
     """Preprocess the on-disk dataset. Parse the input config file,
     load the data, and save the data in the format that GraphBolt supports.
@@ -66,175 +239,7 @@ def preprocess_ondisk_dataset(dataset_dir: str) -> str:
 
     # Fix all paths under dataset_dir.
     with fix_dataset_dir(dataset_dir):
-        # 0. Check if the dataset is already preprocessed.
-        if os.path.exists("preprocessed/metadata.yaml"):
-            print("The dataset is already preprocessed.")
-            return os.path.join(dataset_dir, "preprocessed/metadata.yaml")
-
-        print("Start to preprocess the on-disk dataset.")
-        # Read the input config.
-        processed_dir_prefix = Path("preprocessed")
-
-        # Check if the metadata.yaml exists.
-        if not os.path.exists("metadata.yaml"):
-            raise RuntimeError("metadata.yaml does not exist.")
-        with open("metadata.yaml", "r") as f:
-            input_config = yaml.safe_load(f)
-
-        # 1. Make `processed_dir_prefix` directory if it does not exist.
-        os.makedirs(processed_dir_prefix, exist_ok=True)
-        output_config = deepcopy(input_config)
-
-        # 2. Load the edge data and create a DGLGraph.
-        if "graph" not in input_config:
-            raise RuntimeError("Invalid config: does not contain graph field.")
-        is_homogeneous = "type" not in input_config["graph"]["nodes"][0]
-        if is_homogeneous:
-            # Homogeneous graph.
-            num_nodes = input_config["graph"]["nodes"][0]["num"]
-            edge_data = pd.read_csv(
-                input_config["graph"]["edges"][0]["path"],
-                names=["src", "dst"],
-            )
-            src, dst = edge_data["src"].to_numpy(), edge_data["dst"].to_numpy()
-
-            g = dgl.graph((src, dst), num_nodes=num_nodes)
-        else:
-            # Heterogeneous graph.
-            # Construct the num nodes dict.
-            num_nodes_dict = {}
-            for node_info in input_config["graph"]["nodes"]:
-                num_nodes_dict[node_info["type"]] = node_info["num"]
-            # Construct the data dict.
-            data_dict = {}
-            for edge_info in input_config["graph"]["edges"]:
-                edge_data = pd.read_csv(edge_info["path"], names=["src", "dst"])
-                src = torch.tensor(edge_data["src"])
-                dst = torch.tensor(edge_data["dst"])
-                data_dict[tuple(edge_info["type"].split(":"))] = (src, dst)
-            # Construct the heterograph.
-            g = dgl.heterograph(data_dict, num_nodes_dict)
-
-        # 3. Load the sampling related node/edge features and add them to
-        # the sampling-graph.
-        if input_config["graph"].get("feature_data", None):
-            for graph_feature in input_config["graph"]["feature_data"]:
-                if graph_feature["domain"] == "node":
-                    node_data = read_data(
-                        graph_feature["path"],
-                        graph_feature["format"],
-                        in_memory=graph_feature["in_memory"],
-                    )
-                    g.ndata[graph_feature["name"]] = node_data
-                if graph_feature["domain"] == "edge":
-                    edge_data = read_data(
-                        graph_feature["path"],
-                        graph_feature["format"],
-                        in_memory=graph_feature["in_memory"],
-                    )
-                    g.edata[graph_feature["name"]] = edge_data
-
-        # 4. Convert the DGLGraph to a CSCSamplingGraph.
-        csc_sampling_graph = from_dglgraph(g)
-
-        # 5. Save the CSCSamplingGraph and modify the output_config.
-        output_config["graph_topology"] = {}
-        output_config["graph_topology"]["type"] = "CSCSamplingGraph"
-        output_config["graph_topology"]["path"] = str(
-            processed_dir_prefix / "csc_sampling_graph.tar"
-        )
-
-        save_csc_sampling_graph(
-            csc_sampling_graph,
-            str(output_config["graph_topology"]["path"]),
-        )
-        del output_config["graph"]
-
-        # 6. Load the node/edge features and do necessary conversion.
-        if input_config.get("feature_data", None):
-            for feature, out_feature in zip(
-                input_config["feature_data"], output_config["feature_data"]
-            ):
-                # Always save the feature in numpy format.
-                out_feature["format"] = "numpy"
-                out_feature["path"] = str(
-                    processed_dir_prefix / feature["path"].replace("pt", "npy")
-                )
-
-                if feature["format"] == "numpy":
-                    # If the original format is numpy, just copy the file.
-                    os.makedirs(
-                        os.path.dirname(out_feature["path"]),
-                        exist_ok=True,
-                    )
-                    shutil.copyfile(
-                        feature["path"],
-                        out_feature["path"],
-                    )
-                else:
-                    # If the original format is not numpy, convert it to numpy.
-                    data = read_data(
-                        feature["path"],
-                        feature["format"],
-                        in_memory=feature["in_memory"],
-                    )
-                    save_data(
-                        data,
-                        out_feature["path"],
-                        out_feature["format"],
-                    )
-
-        # 7. Save the train/val/test split according to the output_config.
-        for set_name in ["train_sets", "validation_sets", "test_sets"]:
-            if set_name not in input_config:
-                continue
-            for intput_set_split, output_set_split in zip(
-                input_config[set_name], output_config[set_name]
-            ):
-                for input_set_per_type, output_set_per_type in zip(
-                    intput_set_split, output_set_split
-                ):
-                    for input_data, output_data in zip(
-                        input_set_per_type["data"], output_set_per_type["data"]
-                    ):
-                        # Always save the feature in numpy format.
-                        output_data["format"] = "numpy"
-                        output_data["path"] = str(
-                            processed_dir_prefix
-                            / input_data["path"].replace("pt", "npy")
-                        )
-                        if input_data["format"] == "numpy":
-                            # If the original format is numpy, just copy the
-                            # file.
-                            os.makedirs(
-                                os.path.dirname(output_data["path"]),
-                                exist_ok=True,
-                            )
-                            shutil.copyfile(
-                                input_data["path"],
-                                output_data["path"],
-                            )
-                        else:
-                            # If the original format is not numpy, convert it
-                            # to numpy.
-                            input_set = read_data(
-                                input_data["path"],
-                                input_data["format"],
-                            )
-                            save_data(
-                                input_set,
-                                output_data["path"],
-                                output_set_per_type["format"],
-                            )
-
-        # 8. Save the output_config.
-        output_config_path = processed_dir_prefix / "metadata.yaml"
-        with open(output_config_path, "w") as f:
-            yaml.dump(output_config, f)
-        print("Finish preprocessing the on-disk dataset.")
-
-        # 9. Return the absolute path of the preprocessing yaml file.
-        return str(dataset_dir / output_config_path)
+        return preprocess_ondisk_dataset_impl(dataset_dir)
 
 
 class OnDiskDataset(Dataset):
