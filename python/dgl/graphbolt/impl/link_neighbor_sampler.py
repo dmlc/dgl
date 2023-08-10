abc93087939d318c12b3e35ae57d971c8b9333a3
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2023-08-10 08:56:55 +0000

    add link sampler

---
+++ python/dgl/graphbolt/impl/link_neighbor_sampler.py
@@ -0,0 +1,73 @@
+"""Uniform negative sampler for GraphBolt."""
+
+from ..subgraph_sampler import LinkSampler
+import torch
+
+class LinkNeighborSampler(LinkSampler):
+    def __init__(
+    self,
+    datapipe,
+    input_format,
+    graph,
+    fanouts,
+    replace=False,
+    prob_name=None,
+    ):
+        """
+        Initlization for a link neighbor subgraph sampler.
+
+        Parameters
+        ----------
+        datapipe : DataPipe
+            The datapipe.
+        input_format : LinkPredictionEdgeFormat
+            Determines the format of the input data.
+        graph : CSCSamplingGraph
+            The graph on which to perform subgraph sampling.
+        fanouts: list[list[int]]
+            The number of edges to be sampled for each node with or without
+            considering edge types.
+        replace: bool
+            Boolean indicating whether the sample is preformed with or
+            without replacement. If True, a value can be selected multiple
+            times. Otherwise, each value can be selected only once.
+        prob_name: str, optional
+            The name of an edge attribute used a. This
+            attribute tensor should contain (unnormalized) probabilities
+            corresponding to each neighboring edge of a node. It must be a 1D
+            floating-point or boolean tensor, with the number of elements
+            equalling the total number of edges.
+
+        Examples
+        --------
+        >>> from dgl import graphbolt as gb
+        >>> indptr = torch.LongTensor([0, 2, 4, 5])
+        >>> indices = torch.LongTensor([1, 2, 0, 2, 0])
+        >>> graph = gb.from_csc(indptr, indices)
+        >>> data_format = gb.LinkPredictionEdgeFormat.INDEPENDENT
+        >>> node_pairs = (torch.tensor([0, 1]), torch.tensor([1, 2]))
+        >>> item_set = gb.ItemSet(node_pairs)
+        >>> minibatch_sampler = gb.MinibatchSampler(
+            ...item_set, batch_size=1,
+            ...)
+        >>> neg_sampler = gb.UniformNegativeSampler(
+            ...minibatch_sampler, 2, data_format, graph)
+        >>> fanouts = [[5], [10], [15]]
+        >>> subgraph_sampler = gb.LinkNeighborSampler(
+            ...neg_sampler, data_format, fanouts)
+        >>> for data in subgraph_sampler:
+            ...  print(data)
+            ...
+        (tensor([0, 0, 0]), tensor([1, 1, 2]), tensor([1, 0, 0]))
+        (tensor([1, 1, 1]), tensor([2, 1, 2]), tensor([1, 0, 0]))
+        """
+        super().__init__(datapipe, input_format, fanouts, replace, prob_name)
+        self.graph = graph
+
+    def _sample_sub_graph(self, seeds, hop):                
+        return self.graph.sample_neighbors(
+            seeds,
+            torch.LongTensor(self.fanouts[hop]),
+            self.replace,
+            self.prob_name,
+        )
--- python/dgl/graphbolt/subgraph_sampler.py
+++ python/dgl/graphbolt/subgraph_sampler.py
@@ -6,6 +6,7 @@ from torchdata.datapipes.iter import Mapper
 from .data_format import LinkPredictionEdgeFormat
 from .utils import unique_and_compact_node_pairs
 
+
 class SubgraphSampler(Mapper):
     """A subgraph sampler.
 
@@ -28,8 +29,8 @@ class SubgraphSampler(Mapper):
         self,
         datapipe,
         fanouts,
-        replace,
-        prob_name,
+        replace=False,
+        prob_name=None,
     ):
         """
         Initlization for a subgraph sampler.
@@ -45,7 +46,7 @@ class SubgraphSampler(Mapper):
             Boolean indicating whether the sample is preformed with or
             without replacement. If True, a value can be selected multiple
             times. Otherwise, each value can be selected only once.
-        probs_name: str, optional
+        prob_name: str, optional
             The name of an edge attribute used a. This
             attribute tensor should contain (unnormalized) probabilities
             corresponding to each neighboring edge of a node. It must be a 1D
@@ -63,12 +64,15 @@ class SubgraphSampler(Mapper):
         seeds = data
         for hop in range(num_layers):
             subgraph = self._sample_sub_graph(
-                seeds, hop,
+                seeds,
+                hop,
             )
-            seeds, compacted_subgraph = unique_and_compact_node_pairs(subgraph.node_pairs, seeds)
+            seeds, compacted_subgraph = unique_and_compact_node_pairs(
+                subgraph.node_pairs, seeds
+            )
             adjs.insert(0, compacted_subgraph)
         return seeds, adjs
-                     
+
     def _sample_sub_graph(self, seeds, hop):
         raise NotImplemented
 
@@ -77,10 +81,10 @@ class LinkSampler(SubgraphSampler):
     def __init__(
         self,
         datapipe,
+        input_format,
         fanouts,
         replace,
-        probs_name,
-        input_format,
+        prob_name,
     ):
         """
         Initlization for a link prediction oriented subgraph sampler.
@@ -89,6 +93,8 @@ class LinkSampler(SubgraphSampler):
         ----------
         datapipe : DataPipe
             The datapipe.
+        input_format:  LinkPredictionEdgeFormat
+            Determines the edge format of the input data.
         fanouts: list[list[int]]
             The number of edges to be sampled for each node with or without
             considering edge types.
@@ -96,35 +102,32 @@ class LinkSampler(SubgraphSampler):
             Boolean indicating whether the sample is preformed with or
             without replacement. If True, a value can be selected multiple
             times. Otherwise, each value can be selected only once.
-        probs_name: str, optional
+        prob_name: str, optional
             The name of an edge attribute used a. This
             attribute tensor should contain (unnormalized) probabilities
             corresponding to each neighboring edge of a node. It must be a 1D
             floating-point or boolean tensor, with the number of elements
             equalling the total number of edges.
-        input_format:  LinkPredictionEdgeFormat
-            Determines the edge format of the input data.
         """
-        super.__init__(datapipe, fanouts, replace, probs_name)
+        super().__init__(datapipe, fanouts, replace, prob_name)
         self.input_format = input_format
 
         def _sample(self, data):
             seeds, compacted_pairs = self._preprocess(data)
             seeds, adjs = super._sample(seeds)
             return seeds, compacted_pairs, adjs
-                                    
+
         def _preprocess(self, data):
             u, v = data[:2]
-            if self.output_format == LinkPredictionEdgeFormat.CONDITIONED:
+            if self.input_format == LinkPredictionEdgeFormat.CONDITIONED:
                 neg_u, neg_v = data[2:4]
                 u = torch.cat((u, neg_u.view(-1)))
                 v = torch.cat((v, neg_v.view(-1)))
-            elif self.output_format == LinkPredictionEdgeFormat.HEAD_CONDITIONED:
+            elif self.input_format == LinkPredictionEdgeFormat.HEAD_CONDITIONED:
                 neg_u = data[2]
-                u = torch.cat((u, neg_u.view(-1)))        
-            elif self.output_format == LinkPredictionEdgeFormat.TAIL_CONDITIONED:
+                u = torch.cat((u, neg_u.view(-1)))
+            elif self.input_format == LinkPredictionEdgeFormat.TAIL_CONDITIONED:
                 neg_v = data[2]
                 v = torch.cat((v, neg_v.view(-1)))
             seeds, compacted_pairs = unique_and_compact_node_pairs((u, v))
             return seeds, compacted_pairs
-        
