793796eda7d6ed2086e6e63de7f43bbfb66f6117
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2023-08-11 08:56:13 +0000

    add unified data structure

--- python/dgl/graphbolt/impl/node_unified_data_struct.py
+++ python/dgl/graphbolt/impl/node_unified_data_struct.py
@@ -1,16 +1,14 @@
 """Unified data structure for input and ouput of all the stages in loading process, especially for node level task."""
 
 from dataclasses import dataclass
-from ..unified_data_struct import UnifiedDataStruct
-import torch
 from typing import Dict, Union
 
+import torch
+
+from ..unified_data_struct import UnifiedDataStruct
 
 
 @dataclass
 class NodeUnifiedDataStruct(UnifiedDataStruct):
-  seed_node: Union[torch.Tensor, Dict[str, torch.Tensor]]
-  label: Union[torch.Tensor, Dict[str, torch.Tensor]]
-    
-    
-                                
+    seed_node: Union[torch.Tensor, Dict[str, torch.Tensor]]
+    label: Union[torch.Tensor, Dict[str, torch.Tensor]]
--- python/dgl/graphbolt/unified_data_struct.py
+++ python/dgl/graphbolt/unified_data_struct.py
@@ -1,9 +1,11 @@
 """Unified data structure for input and ouput of all the stages in loading process."""
 
 from dataclasses import dataclass
-from .sampled_subgraph import SampledSubgraph
-import torch
 from typing import Dict, Tuple, Union
+
+import torch
+
+from .sampled_subgraph import SampledSubgraph
 
 __all__ = ["UnifiedDataStruct"]
 
@@ -13,6 +15,3 @@ class UnifiedDataStruct:
     sampled_subgraph: SampledSubgraph
     node_feature: Union[torch.Tensor, Dict[str, torch.Tensor]]
     edge_feature: Union[torch.Tensor, Dict[Tuple(str, str, str), torch.Tensor]]
-    
-    
-                                
