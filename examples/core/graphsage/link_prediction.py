8ec0d0ee8d55612acd85d227c2d55e14f19c6bc4
keli-wen <whuwkl@gmail.com>
2023-08-15 08:18:40 +0000

    sync comment with sampling

--- examples/core/graphsage/link_prediction.py
+++ examples/core/graphsage/link_prediction.py
@@ -10,31 +10,70 @@ from ogb.linkproppred import DglLinkPropPredDataset, Evaluator
 
 
 def to_bidirected_with_reverse_mapping(g):
-    """Makes a graph bidirectional, and returns a mapping array ``mapping``
-    where ``mapping[i]`` is the reverse edge of edge ID ``i``. Does not work
-    with graphs that have self-loops.
+    """Convert the graph to bidirectional and return the reverse mapping.
+
+    This function transforms the input graph into its bidirectional form. It
+    then returns the newly formed bidirectional graph and the mapping that
+    represents the reverse edges. The function does not work with graphs that
+    have self-loops.
+
+    Parameters:
+    ----------
+    g : DGLGraph
+        Input graph.
+
+    Returns:
+    -------
+    DGLGraph :
+        Bidirectional graph.
+    Tensor :
+        Mapping to reverse edges.
     """
+    # First, add reverse edges to the graph, effectively making it
+    # bidirectional. Then, simplify the resulting graph by merging any duplicate
+    # edges. The resulting simplified graph is stored in `g_simple`, and
+    # `mapping` provides information on how edges in `g_simple` correspond to
+    # edges in the original graph.
     g_simple, mapping = dgl.to_simple(
-        dgl.add_reverse_edges(g),
-        return_counts="count",
-        writeback_mapping=True,
+        dgl.add_reverse_edges(g), return_counts="count", writeback_mapping=True
     )
 
-    # Construct the reverse edge mapping.
+    # The `return_counts` option in `dgl.to_simple` returns the count of how
+    # many times each edge in the simplified graph corresponds to an edge in the
+    # original graph. This count is saved in the edge data of the returned
+    # graph with the key "count".
     c = g_simple.edata["count"]
     num_edges = g.num_edges()
+
+    # `mapping_offset` is an auxiliary tensor used to understand how edges in
+    # the simplified bidirectional graph (g_simple) relate to the edges in the
+    # original graph.
     mapping_offset = torch.zeros(
         g_simple.num_edges() + 1, dtype=g_simple.idtype
     )
+
+    # Calculate the cumulative sum of counts to determine boundaries for each
+    # unique edge.
     mapping_offset[1:] = c.cumsum(0)
+
+    # Sort the mapping tensor to group the same edge indices.
     idx = mapping.argsort()
+
+    # Using the previously computed `mapping_offset`, it extracts the first
+    # index of each group, which represents the unique edge indices from the
+    # sorted mapping.
     idx_uniq = idx[mapping_offset[:-1]]
+
+    # If an edge index is greater than or equal to the number of edges in the
+    # original graph, it indicates that this edge is a reversed edge, and the
+    # original edge index for it is (idx_uniq - num_edges). Otherwise, its
+    # reverse edge index is (idx_uniq + num_edges).
     reverse_idx = torch.where(
         idx_uniq >= num_edges, idx_uniq - num_edges, idx_uniq + num_edges
     )
     reverse_mapping = mapping[reverse_idx]
 
-    # Sanity check to ensure the bidirectionality.
+    # Sanity check to ensure valid mapping.
     src1, dst1 = g_simple.edges()
     src2, dst2 = g_simple.find_edges(reverse_mapping)
     assert torch.equal(src1, dst2)
@@ -90,37 +129,57 @@ class SAGE(nn.Module):
         return feat
 
 
+@torch.no_grad()
 def compute_mrr(
     model, evaluator, node_emb, src, dst, neg_dst, device, batch_size=500
 ):
-    """Compute Mean Reciprocal Rank (MRR) in batches."""
+    """Compute the Mean Reciprocal Rank (MRR) for given source and destination
+    nodes.
+
+    This function computes the MRR for a set of node pairs, dividing the task
+    into batches to handle potentially large graphs.
+    """
     rr = torch.zeros(src.shape[0])
+    # Loop over node pairs in batches.
     for start in tqdm.trange(0, src.shape[0], batch_size, desc="Evaluate"):
         end = min(start + batch_size, src.shape[0])
+
+        # Concatenate positive and negative destination nodes.
         all_dst = torch.cat([dst[start:end, None], neg_dst[start:end]], 1)
+
+        # Fetch embeddings for current batch of source and destination nodes.
         h_src = node_emb[src[start:end]][:, None, :].to(device)
         h_dst = node_emb[all_dst.view(-1)].view(*all_dst.shape, -1).to(device)
+
+        # Compute prediction scores using the model.
         pred = model.predictor(h_src * h_dst).squeeze(-1)
+
+        # Evaluate the predictions to obtain MRR values.
         input_dict = {"y_pred_pos": pred[:, 0], "y_pred_neg": pred[:, 1:]}
         rr[start:end] = evaluator.eval(input_dict)["mrr_list"]
     return rr.mean()
 
 
+@torch.no_grad()
 def evaluate(device, graph, edge_split, model):
+    """Evaluate the model on validation and test sets."""
     model.eval()
     evaluator = Evaluator(name="ogbl-citation2")
-    with torch.no_grad():
-        node_emb = model.inference(graph)
-        results = []
-        for split in ["valid", "test"]:
-            src = edge_split[split]["source_node"].to(node_emb.device)
-            dst = edge_split[split]["target_node"].to(node_emb.device)
-            neg_dst = edge_split[split]["target_node_neg"].to(node_emb.device)
-            results.append(
-                compute_mrr(
-                    model, evaluator, node_emb, src, dst, neg_dst, device
-                )
-            )
+
+    # Compute node embeddings for the entire graph.
+    node_emb = model.inference(graph)
+    results = []
+
+    # Loop over both validation and test sets.
+    for split in ["valid", "test"]:
+        src = edge_split[split]["source_node"].to(node_emb.device)
+        dst = edge_split[split]["target_node"].to(node_emb.device)
+        neg_dst = edge_split[split]["target_node_neg"].to(node_emb.device)
+
+        # Compute MRR values for the current split.
+        results.append(
+            compute_mrr(model, evaluator, node_emb, src, dst, neg_dst, device)
+        )
     return results
 
 
@@ -133,7 +192,7 @@ def construct_negative_graph(graph, k):
 
 
 def train(device, g, model):
-    opt = torch.optim.Adam(model.parameters(), lr=0.0005)
+    opt = torch.optim.Adam(model.parameters(), lr=0.005)
 
     negative_g = construct_negative_graph(g, 1)
     for epoch in range(10):
@@ -151,7 +210,7 @@ def train(device, g, model):
         loss.backward()
         opt.step()
 
-        print(f"Epoch {epoch + 1:05d} | Loss {loss.item():.4f}")
+        print(f"Epoch {epoch:05d} | Loss {loss.item():.4f}")
 
 
 def parse_args():
