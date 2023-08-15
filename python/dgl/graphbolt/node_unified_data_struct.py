6f5b6a067e0bb7362e80f34a03adc9f7885242c3
Ubuntu <ubuntu@ip-172-31-16-19.ap-northeast-1.compute.internal>
2023-08-15 09:56:31 +0000

    fix comments

--- python/dgl/graphbolt/node_unified_data_struct.py
+++ python/dgl/graphbolt/node_unified_data_struct.py
@@ -14,6 +14,13 @@ class NodeUnifiedDataStruct(UnifiedDataStruct):
     r"""A subclass of 'UnifiedDataStruct', specialized for handling node level
     tasks."""
 
+    seed_node: Union[torch.Tensor, Dict[str, torch.Tensor]]
+    """
+    Representation of seed nodes used for sampling in the graph.
+    - If `seed_node` is a tensor: It indicates the graph is homogeneous.
+    - If `seed_node` is a dictionary: The keys should be node type and the
+      value should be corresponding heterogeneous node ids.
+    """
     label: Union[torch.Tensor, Dict[str, torch.Tensor]] = None
     """
     Labels associated with seed nodes in the graph.
