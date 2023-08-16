4304b5bb6cd1ec3fd5e0eba22bf8beeef06fe0cc
xiangyuzhi <zhixy2021@mail.sustech.edu.cn>
2023-08-16 05:58:28 +0000

    modify accroding to review

--- examples/sparse/hetero-rgcn.py
+++ examples/sparse/hetero-rgcn.py
@@ -12,12 +12,7 @@ import torch.nn.functional as F
 
 from dgl.data.rdf import AIFBDataset, AMDataset, BGSDataset, MUTAGDataset
 
-
-from collections import defaultdict
-
 import dgl
-import dgl.function as fn
-import dgl.nn as dglnn
 import dgl.sparse as dglsp
 
 import torch as th
@@ -30,129 +25,64 @@ class RelGraphEmbed(nn.Module):
     r"""Embedding layer for featureless heterograph."""
 
     def __init__(
-        self, g, embed_size, embed_name="embed", activation=None, dropout=0.0
+        self, ntype_num, embed_size, embed_name="embed", activation=None, dropout=0.0
     ):
         super(RelGraphEmbed, self).__init__()
-        self.g = g
         self.embed_size = embed_size
         self.embed_name = embed_name
         self.activation = activation
         self.dropout = nn.Dropout(dropout)
 
-        # create weight embeddings for each node for each relation
+        # Create weight embeddings for each node for each relation.
         self.embeds = nn.ParameterDict()
-        for ntype in g.ntypes:
-            embed = nn.Parameter(th.Tensor(g.num_nodes(ntype), self.embed_size))
+        for ntype, num_nodes in ntype_num.items():
+            embed = nn.Parameter(th.Tensor(num_nodes, self.embed_size))
             nn.init.xavier_uniform_(embed, gain=nn.init.calculate_gain("relu"))
             self.embeds[ntype] = embed
 
-    def forward(self, block=None):
-        """Forward computation
-
-        Parameters
-        ----------
-        block : DGLGraph, optional
-            If not specified, directly return the full graph with embeddings stored in
-            :attr:`embed_name`. Otherwise, extract and store the embeddings to the block
-            graph and return.
-
-        Returns
-        -------
-        DGLGraph
-            The block graph fed with embeddings.
-        """
+    def forward(self):
         return self.embeds
 
 
-class HeteRGConvLayer(nn.Module):
-    r"""Relational graph convolution layer.
+class HeteroRelationalGraphConv(nn.Module):
+    r"""Relational graph convolution layer. 
 
     Parameters
     ----------
-    in_feat : int
+    in_size : int
         Input feature size.
-    out_feat : int
+    out_size : int
         Output feature size.
-    rel_names : list[str]
+    relation_names : list[str]
         Relation names.
-    num_bases : int, optional
-        Number of bases. If is none, use number of relations. Default: None.
-    weight : bool, optional
-        True if a linear layer is applied after message passing. Default: True
-    bias : bool, optional
-        True if bias is added. Default: True
-    activation : callable, optional
-        Activation function. Default: None
-    self_loop : bool, optional
-        True to include self loop message. Default: False
-    dropout : float, optional
-        Dropout rate. Default: 0.0
     """
 
     def __init__(
         self,
-        in_feat,
-        out_feat,
-        rel_names,
-        num_bases,
-        *,
-        weight=True,
-        bias=True,
-        activation=None,
-        self_loop=False,
-        dropout=0.0
+        in_size,
+        out_size,
+        relation_names,
     ):
-        super(HeteRGConvLayer, self).__init__()
-        self.in_feat = in_feat
-        self.out_feat = out_feat
-        self.rel_names = rel_names
-        self.num_bases = num_bases
-        self.bias = bias
-        self.activation = activation
-        self.self_loop = self_loop
+        super(HeteroRelationalGraphConv, self).__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.relation_names = relation_names
 
         self.conv = nn.ModuleDict(
             {
-                str(rel): nn.Linear(in_feat, out_feat)
-                for rel in rel_names
+                str(rel): nn.Linear(in_size, out_size)
+                for rel in relation_names
             }
         )
 
-        self.use_weight = weight
-        self.use_basis = num_bases < len(self.rel_names) and weight
-        if self.use_weight:
-            if self.use_basis:
-                self.basis = dglnn.WeightBasis(
-                    (in_feat, out_feat), num_bases, len(self.rel_names)
-                )
-            else:
-                self.weight = nn.Parameter(
-                    th.Tensor(len(self.rel_names), in_feat, out_feat)
-                )
-                nn.init.xavier_uniform_(
-                    self.weight, gain=nn.init.calculate_gain("relu")
-                )
+        self.dropout = nn.Dropout(0.0)
 
-        # bias
-        if bias:
-            self.h_bias = nn.Parameter(th.Tensor(out_feat))
-            nn.init.zeros_(self.h_bias)
-
-        # weight for self loop
-        if self.self_loop:
-            self.loop_weight = nn.Parameter(th.Tensor(in_feat, out_feat))
-            nn.init.xavier_uniform_(
-                self.loop_weight, gain=nn.init.calculate_gain("relu")
-            )
-
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, g, inputs):
+    def forward(self, A, inputs):
         """Forward computation
 
         Parameters
         ----------
-        g : DGLGraph
+        A : Hetero Sparse Matrix 
             Input graph.
         inputs : dict[str, torch.Tensor]
             Node feature for each node type.
@@ -162,130 +92,56 @@ class HeteRGConvLayer(nn.Module):
         dict[str, torch.Tensor]
             New node features for each node type.
         """
-        g = g.local_var()
-        if self.use_weight:
-            weight = self.basis() if self.use_basis else self.weight
-            wdict = {
-                self.rel_names[i]: {"weight": w.squeeze(0)}
-                for i, w in enumerate(th.split(weight, 1, dim=0))
-            }
-        else:
-            wdict = {}
-
-        if g.is_block:
-            inputs_src = inputs
-            inputs_dst = {
-                k: v[: g.number_of_dst_nodes(k)] for k, v in inputs.items()
-            }
-        else:
-            inputs_src = inputs_dst = inputs
-
-        A = {}
-        for stype, etype, dtype in g.canonical_etypes:
-            eg = g[stype, etype, dtype]
-            indices = th.stack(eg.edges('uv'))
-            A[(stype, etype, dtype)] = dglsp.spmatrix(indices, shape=(inputs[stype].shape[0], inputs[dtype].shape[0]))
-            # TODO: add self loop and normlize ...
-
-        hs = { ntype: th.zeros(inputs[ntype].shape[0], self.out_feat) for ntype in g.ntypes }
-        for rel in A :
-            src_type = rel[0]
-            dst_type = rel[2]
-            hs[dst_type] = hs[dst_type] + (A[rel].T @ self.conv[str(rel[1])](inputs[src_type]))
+        hs = {}
+        for rel in A:
+            src_type, edge_type, dst_type = rel
+            if dst_type not in hs:
+                hs[dst_type] = th.zeros(
+                    inputs[dst_type].shape[0], self.out_size)
+            hs[dst_type] = hs[dst_type] + \
+                (A[rel].T @ self.conv[str(edge_type)](inputs[src_type]))
             hs[dst_type] = F.relu(hs[dst_type])
 
-
-        def _apply(ntype, h):
-            if self.self_loop:
-                h = h + th.matmul(inputs_dst[ntype], self.loop_weight)
-            if self.bias:
-                h = h + self.h_bias
-            if self.activation:
-                h = self.activation(h)
-            return self.dropout(h)
-
-        return {ntype: _apply(ntype, h) for ntype, h in hs.items()}
-
-
+        return hs
 
 
 class EntityClassify(nn.Module):
     def __init__(
         self,
-        g,
-        h_dim,
+        in_size,
         out_dim,
-        num_bases,
-        num_hidden_layers=1,
-        dropout=0,
-        use_self_loop=False,
+        ntype_num,
+        relation_names,
     ):
         super(EntityClassify, self).__init__()
-        self.g = g
-        self.h_dim = h_dim
+        self.in_size = in_size
         self.out_dim = out_dim
-        self.rel_names = list(set(g.etypes))
-        self.rel_names.sort()
-        if num_bases < 0 or num_bases > len(self.rel_names):
-            self.num_bases = len(self.rel_names)
-        else:
-            self.num_bases = num_bases
-        self.num_hidden_layers = num_hidden_layers
-        self.dropout = dropout
-        self.use_self_loop = use_self_loop
+        self.relation_names = relation_names
+        self.relation_names.sort()
 
-        self.embed_layer = RelGraphEmbed(g, self.h_dim)
+        self.embed_layer = RelGraphEmbed(ntype_num, self.in_size)
         self.layers = nn.ModuleList()
-        # i2h
+        # Input to hidden.
         self.layers.append(
-            HeteRGConvLayer(
-                self.h_dim,
-                self.h_dim,
-                self.rel_names,
-                self.num_bases,
-                activation=F.relu,
-                self_loop=self.use_self_loop,
-                dropout=self.dropout,
-                weight=False,
+            HeteroRelationalGraphConv(
+                self.in_size,
+                self.in_size,
+                self.relation_names,
             )
         )
-        # h2h
-        for i in range(self.num_hidden_layers):
-            self.layers.append(
-                HeteRGConvLayer(
-                    self.h_dim,
-                    self.h_dim,
-                    self.rel_names,
-                    self.num_bases,
-                    activation=F.relu,
-                    self_loop=self.use_self_loop,
-                    dropout=self.dropout,
-                )
-            )
-        # h2o
+        # Hidden to output.
         self.layers.append(
-            HeteRGConvLayer(
-                self.h_dim,
+            HeteroRelationalGraphConv(
+                self.in_size,
                 self.out_dim,
-                self.rel_names,
-                self.num_bases,
-                activation=None,
-                self_loop=self.use_self_loop,
+                self.relation_names,
             )
         )
 
-    def forward(self, h=None, blocks=None):
-        if h is None:
-            # full graph training
-            h = self.embed_layer()
-        if blocks is None:
-            # full graph training
-            for layer in self.layers:
-                h = layer(self.g, h)
-        else:
-            # minibatch training
-            for layer, block in zip(self.layers, blocks):
-                h = layer(block, h)
+    def forward(self, A):
+        h = self.embed_layer()
+        for layer in self.layers:
+            h = layer(A, h)
         return h
 
     def inference(self, g, batch_size, device, num_workers, x=None):
@@ -336,9 +192,8 @@ class EntityClassify(nn.Module):
         return y
 
 
-
 def main(args):
-    # load graph data
+    # Load graph data.
     if args.dataset == "aifb":
         dataset = AIFBDataset()
     elif args.dataset == "mutag":
@@ -358,19 +213,15 @@ def main(args):
     train_idx = th.nonzero(train_mask, as_tuple=False).squeeze()
     test_idx = th.nonzero(test_mask, as_tuple=False).squeeze()
     labels = g.nodes[category].data.pop("labels")
-    category_id = len(g.ntypes)
-    for i, ntype in enumerate(g.ntypes):
-        if ntype == category:
-            category_id = i
 
-    # split dataset into train, validate, test
+    # Split dataset into train, validate, test.
     if args.validation:
         val_idx = train_idx[: len(train_idx) // 5]
-        train_idx = train_idx[len(train_idx) // 5 :]
+        train_idx = train_idx[len(train_idx) // 5:]
     else:
         val_idx = train_idx
 
-    # check cuda
+    # Check cuda.
     use_cuda = args.gpu >= 0 and th.cuda.is_available()
     if use_cuda:
         th.cuda.set_device(args.gpu)
@@ -379,26 +230,32 @@ def main(args):
         train_idx = train_idx.cuda()
         test_idx = test_idx.cuda()
 
-    # create model
+    # Create model.
     model = EntityClassify(
-        g,
         args.n_hidden,
         num_classes,
-        num_bases=args.n_bases,
-        num_hidden_layers=args.n_layers - 2,
-        dropout=args.dropout,
-        use_self_loop=args.use_self_loop,
+        {ntype: g.num_nodes(ntype) for ntype in g.ntypes},
+        list(set(g.etypes)),
     )
 
     if use_cuda:
         model.cuda()
 
-    # optimizer
+    # Optimizer.
     optimizer = th.optim.Adam(
         model.parameters(), lr=args.lr, weight_decay=args.l2norm
     )
 
-    # training loop
+    # Construct sparse metrix.
+    g = g.local_var()
+    A = {}
+    for stype, etype, dtype in g.canonical_etypes:
+        eg = g[stype, etype, dtype]
+        indices = th.stack(eg.edges('uv'))
+        A[(stype, etype, dtype)] = dglsp.spmatrix(
+            indices, shape=(g.num_nodes(stype), g.num_nodes(dtype)))
+
+    # Training loop.
     print("start training...")
     dur = []
     model.train()
@@ -406,7 +263,7 @@ def main(args):
         optimizer.zero_grad()
         if epoch > 5:
             t0 = time.time()
-        logits = model()[category]
+        logits = model(A)[category]
         loss = F.cross_entropy(logits[train_idx], labels[train_idx])
         loss.backward()
         optimizer.step()
@@ -422,21 +279,16 @@ def main(args):
             logits[val_idx].argmax(dim=1) == labels[val_idx]
         ).item() / len(val_idx)
         print(
-            "Epoch {:05d} | Train Acc: {:.4f} | Train Loss: {:.4f} | Valid Acc: {:.4f} | Valid loss: {:.4f} | Time: {:.4f}".format(
-                epoch,
-                train_acc,
-                loss.item(),
-                val_acc,
-                val_loss.item(),
-                np.average(dur),
-            )
+            f"Epoch {epoch:05d} | Train Acc: {train_acc:.4f} | "
+            f"Train Loss: {loss.item():.4f} | Valid Acc: {val_acc:.4f} | "
+            f"Valid loss: {val_loss.item():.4f} | Time: {np.average(dur):.4f}"
         )
     print()
     if args.model_path is not None:
         th.save(model.state_dict(), args.model_path)
 
     model.eval()
-    logits = model.forward()[category]
+    logits = model.forward(A)[category]
     test_loss = F.cross_entropy(logits[test_idx], labels[test_idx])
     test_acc = th.sum(
         logits[test_idx].argmax(dim=1) == labels[test_idx]
@@ -452,27 +304,15 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="RGCN")
     parser.add_argument(
-        "--dropout", type=float, default=0, help="dropout probability"
-    )
-    parser.add_argument(
         "--n-hidden", type=int, default=16, help="number of hidden units"
     )
     parser.add_argument("--gpu", type=int, default=-1, help="gpu")
     parser.add_argument("--lr", type=float, default=1e-2, help="learning rate")
     parser.add_argument(
-        "--n-bases",
-        type=int,
-        default=-1,
-        help="number of filter weight matrices, default: -1 [use all]",
-    )
-    parser.add_argument(
-        "--n-layers", type=int, default=2, help="number of propagation rounds"
-    )
-    parser.add_argument(
         "-e",
         "--n-epochs",
         type=int,
-        default=50,
+        default=10,
         help="number of training epochs",
     )
     parser.add_argument(
@@ -482,12 +322,8 @@ if __name__ == "__main__":
         "--model_path", type=str, default=None, help="path for save the model"
     )
     parser.add_argument("--l2norm", type=float, default=0, help="l2 norm coef")
-    parser.add_argument(
-        "--use-self-loop",
-        default=False,
-        action="store_true",
-        help="include self feature as a special relation",
-    )
+
+    # Only one of modes in this group is valid.
     fp = parser.add_mutually_exclusive_group(required=False)
     fp.add_argument("--validation", dest="validation", action="store_true")
     fp.add_argument("--testing", dest="validation", action="store_false")
