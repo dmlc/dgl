968c52dd7bde8325e85fc53fa7f38491f1399656
Muhammed Fatih BALIN <m.f.balin@gmail.com>
2023-08-25 02:14:41 -0400

    [Graphbolt] Gpu cache feature store (#6005)

--- python/dgl/graphbolt/impl/__init__.py
+++ python/dgl/graphbolt/impl/__init__.py
@@ -6,4 +6,5 @@ from .ondisk_dataset import *
 from .ondisk_metadata import *
 from .sampled_subgraph_impl import *
 from .torch_based_feature_store import *
+from .gpu_cached_feature import *
 from .uniform_negative_sampler import *
