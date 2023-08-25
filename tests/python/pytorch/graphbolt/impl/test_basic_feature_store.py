d3cd05840e46c61bbe3255825e5f789e4c4169d6
Ubuntu <ubuntu@ip-172-31-28-63.ap-northeast-1.compute.internal>
2023-08-25 03:32:16 +0000

    add-homo

--- tests/python/pytorch/graphbolt/impl/test_basic_feature_store.py
+++ tests/python/pytorch/graphbolt/impl/test_basic_feature_store.py
@@ -4,6 +4,31 @@ import torch
 from dgl import graphbolt as gb
 
 
+def test_basic_feature_store_homo():
+    a = torch.tensor([3, 2, 1])
+    b = torch.tensor([2, 5, 3])
+
+    features = {}
+    features[("node", None, "a")] = gb.TorchBasedFeature(a)
+    features[("node", None, "b")] = gb.TorchBasedFeature(b)
+
+    feature_store = gb.BasicFeatureStore(features)
+
+    # Test read the entire feature.
+    assert torch.equal(
+        feature_store.read("node", None, "a"), torch.tensor([3, 2, 1])
+    )
+    assert torch.equal(
+        feature_store.read("node", None, "b"), torch.tensor([2, 5, 3])
+    )
+
+    # Test read with ids.
+    assert torch.equal(
+        feature_store.read("node", None, "a", torch.tensor([0, 1])),
+        torch.tensor([3, 2]),
+    )
+
+
 def test_basic_feature_store_hetero():
     a = torch.tensor([3, 2, 1])
     b = torch.tensor([2, 5, 3])
