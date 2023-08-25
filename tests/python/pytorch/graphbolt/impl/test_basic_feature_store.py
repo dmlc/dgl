c05832d11f3e0cb783bab6c0e39c6a563129376e
Ubuntu <ubuntu@ip-172-31-28-63.ap-northeast-1.compute.internal>
2023-08-25 03:30:26 +0000

    update

--- tests/python/pytorch/graphbolt/impl/test_basic_feature_store.py
+++ tests/python/pytorch/graphbolt/impl/test_basic_feature_store.py
@@ -4,22 +4,31 @@ import torch
 from dgl import graphbolt as gb
 
 
-def test_basic_feature_store():
+def test_basic_feature_store_hetero():
     a = torch.tensor([3, 2, 1])
     b = torch.tensor([2, 5, 3])
+    c = torch.tensor([6, 8, 9])
 
     features = {}
     features[("node", "paper", "a")] = gb.TorchBasedFeature(a)
-    features[("edge", "paper:cites:paper", "b")] = gb.TorchBasedFeature(b)
+    features[("node", "author", "b")] = gb.TorchBasedFeature(b)
+    features[("edge", "paper:cites:paper", "c")] = gb.TorchBasedFeature(c)
 
     feature_store = gb.BasicFeatureStore(features)
+
+    # Test read the entire feature.
     assert torch.equal(
         feature_store.read("node", "paper", "a"), torch.tensor([3, 2, 1])
     )
     assert torch.equal(
-        feature_store.read("edge", "paper:cites:paper", "b"),
-        torch.tensor([2, 5, 3]),
+        feature_store.read("node", "paper", "b"), torch.tensor([2, 5, 3])
     )
+    assert torch.equal(
+        feature_store.read("edge", "paper:cites:paper", "c"),
+        torch.tensor([6, 8, 9]),
+    )
+
+    # Test read with ids.
     assert torch.equal(
         feature_store.read("node", "paper", "a", torch.tensor([0, 1])),
         torch.tensor([3, 2]),
