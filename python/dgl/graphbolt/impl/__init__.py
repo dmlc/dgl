54a535c040f0c7f0b2c4802a1211d0b948a022ca
Muhammed Fatih Balin <m.f.balin@gmail.com>
2023-08-28 12:36:02 -0400

    address reviews

--- python/dgl/graphbolt/impl/__init__.py
+++ python/dgl/graphbolt/impl/__init__.py
@@ -2,7 +2,6 @@
 from .basic_feature_store import *
 from .csc_sampling_graph import *
 from .neighbor_sampler import *
-from .layer_neighbor_sampler import *
 from .ondisk_dataset import *
 from .ondisk_metadata import *
 from .sampled_subgraph_impl import *
