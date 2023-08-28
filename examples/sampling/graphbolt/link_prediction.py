17f08a3e3fb453da77657c8944d0b247a9f9094c
keli-wen <whuwkl@gmail.com>
2023-08-28 07:51:30 +0000

    update the logic

--- examples/sampling/graphbolt/link_prediction.py
+++ examples/sampling/graphbolt/link_prediction.py
@@ -1,5 +1,4 @@
 import argparse
-from functools import partial
 
 import dgl
 import dgl.graphbolt as gb
@@ -38,42 +37,61 @@ class SAGE(nn.Module):
         return hidden_x
 
 
-def _fetch_func(data: gb.LinkPredictionBlock, features):
-    """Fetch data from disk."""
-    data.node_feature = features.read("node", None, "feat", data.input_nodes)
-    # Convert SampledSubgraphImpl to dgl.Block.
-    blocks = [
-        dgl.create_block(data.sampled_subgraphs[i].node_pairs)
-        for i in range(len(data.sampled_subgraphs))
-    ]
-    return (data.node_feature.float(), data.compacted_node_pair, blocks)
-
-
-def to_link_block(data):
+def to_link_block_train(data):
+    """Convert train ItemSet to a LinkPredictionBlock."""
     block = gb.LinkPredictionBlock(node_pair=data)
     return block
 
 
-def get_dataloader(args, graph, features, current_set, shuffle=True):
+def to_link_block_eval(data):
+    """Convert eval ItemSet to a LinkPredictionBlock."""
+    block = gb.LinkPredictionBlock(node_pair=data[:2], negative_tail=data[2])
+    return block
+
+
+def get_dataloader(args, graph, features, current_set, is_train=True):
     """Get a graphbolt-version dataloader."""
     minibatch_sampler = gb.MinibatchSampler(
-        current_set, batch_size=args.batch_size, shuffle=shuffle
+        current_set, batch_size=args.batch_size, shuffle=is_train
     )
-    data_block_converter = Mapper(minibatch_sampler, to_link_block)
+    data_block_converter = Mapper(
+        minibatch_sampler,
+        to_link_block_train if is_train else to_link_block_eval,
+    )
+    format = (
+        gb.LinkPredictionEdgeFormat.INDEPENDENT
+        if is_train
+        else gb.LinkPredictionEdgeFormat.TAIL_CONDITIONED
+    )
     negative_sampler = gb.UniformNegativeSampler(
         data_block_converter,
         args.neg_ratio,
-        gb.LinkPredictionEdgeFormat.INDEPENDENT,
+        format,
         graph,
     )
     subgraph_sampler = gb.NeighborSampler(negative_sampler, graph, args.fanout)
-    fetch_func = partial(_fetch_func, features=features)
-    feature_fetcher = gb.FeatureFetcher(subgraph_sampler, fetch_func)
-    device_transfer = gb.CopyTo(feature_fetcher, torch.device("cpu"))
+    feature_keys = [("node", None, "feat")]
+    feature_fetcher = gb.FeatureFetcher(
+        subgraph_sampler, features, feature_keys
+    )
     dataloader = gb.MultiProcessDataLoader(
-        device_transfer, num_workers=args.num_workers
+        gb.CopyTo(feature_fetcher, torch.device("cpu")),
+        num_workers=args.num_workers,
     )
     return dataloader
+
+
+def to_dgl_blocks(sampled_subgraphs):
+    """Convert sampled subgraphs to DGL blocks."""
+    blocks = [
+        dgl.create_block(
+            sampled_subgraph.node_pairs,
+            num_src_nodes=sampled_subgraph.reverse_column_node_ids.shape[0],
+            num_dst_nodes=sampled_subgraph.reverse_row_node_ids.shape[0],
+        )
+        for sampled_subgraph in range(sampled_subgraphs)
+    ]
+    return blocks
 
 
 @torch.no_grad()
@@ -84,25 +102,35 @@ def evaluate(args, graph, features, current_set, model):
     # of layers to 1 and the fanout to -1.
     args.fanout = [torch.LongTensor([-1])]
     dataloader = get_dataloader(
-        args, graph, features, current_set, shuffle=False
+        args, graph, features, current_set, is_train=False
     )
     pos_pred = []
     neg_pred = []
 
     model.eval()
-    for step, (input_features, compacted_pairs, blocks) in enumerate(
-        dataloader
-    ):
-        y = model(blocks, input_features)
-        score = (
+    for step, data in tqdm.tqdm(enumerate(dataloader)):
+        # Unpack LinkPredictionBlock.
+        compacted_pairs = data.compacted_node_pair
+        node_feature = data.node_feature[(None, "feat")].float()
+        blocks = to_dgl_blocks(data.sampled_subgraphs)
+
+        # Get the embeddings of the input nodes.
+        y = model(blocks, node_feature)
+        pos_score = (
             (model.predictor(y[compacted_pairs[0]] * y[compacted_pairs[1]]))
             .squeeze()
             .detach()
         )
-        num_batch = compacted_pairs[0].shape[0] // (args.neg_ratio + 1)
-        pos_pred.append(score[:num_batch])
-        neg_pred.append(score[num_batch:])
-
+        # We need to repeat the negative source nodes to match the number of
+        # negative destination nodes.
+        neg_src = compacted_pairs[0].repeat_interleave(args.neg_ratio, dim=0)
+        neg_score = (
+            model.predictor(y[neg_src] * y[data.compacted_negative_tail])
+            .squeeze()
+            .detach()
+        )
+        pos_pred.append(pos_score)
+        neg_pred.append(neg_score)
     pos_pred = torch.cat(pos_pred, dim=0)
     neg_pred = torch.cat(neg_pred, dim=0).view(-1, args.neg_ratio)
 
@@ -118,19 +146,18 @@ def train(args, graph, features, train_set, valid_set, model):
 
     for epoch in tqdm.trange(args.epochs):
         model.train()
-        for step, (input_features, compacted_pairs, blocks) in enumerate(
-            dataloader
-        ):
+        for step, data in enumerate(dataloader):
+            # Unpack LinkPredictionBlock.
+            labels = data.label
+            compacted_pairs = data.compacted_node_pair
+            node_feature = data.node_feature[(None, "feat")].float()
+            blocks = to_dgl_blocks(data.sampled_subgraphs)
+
             # Get the embeddings of the input nodes.
-            y = model(blocks, input_features)
+            y = model(blocks, node_feature.float())
             logits = model.predictor(
                 y[compacted_pairs[0]] * y[compacted_pairs[1]]
             ).squeeze()
-
-            # Construct the labels.
-            labels = torch.zeros_like(logits)
-            num_batch = compacted_pairs[0].shape[0] // (args.neg_ratio + 1)
-            labels[:num_batch] = 1.0
 
             # Compute loss.
             loss = F.binary_cross_entropy_with_logits(logits, labels.float())
@@ -143,12 +170,12 @@ def train(args, graph, features, train_set, valid_set, model):
                 f"Epoch {epoch:05d} | "
                 f"Step {step:05d} | "
                 f"Loss {(total_loss) / (step + 1):.4f}",
-                end="\r",
+                end="\n",
             )
 
         # Evaluate the model.
         print("Validation")
-        valid_mrr = evaluate(args, graph, features, valid_set, model)
+        _, valid_mrr = evaluate(args, graph, features, valid_set, model)
         print(f"Valid MRR {valid_mrr.item():.4f}")
 
 
@@ -156,7 +183,6 @@ def parse_args():
     parser = argparse.ArgumentParser(description="OGBL-Citation2 (GraphBolt)")
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--lr", type=float, default=0.0005)
-    parser.add_argument("--num-layers", type=int, default=3)
     parser.add_argument("--neg-ratio", type=int, default=1000)
     parser.add_argument("--batch-size", type=int, default=512)
     parser.add_argument("--num-workers", type=int, default=4)
@@ -168,7 +194,7 @@ def parse_args():
     )
     parser.add_argument(
         "--mode",
-        default="mixed",
+        default="cpu",
         choices=["cpu", "mixed", "puregpu"],
         help="Training mode. 'cpu' for CPU training, 'mixed' for CPU-GPU mixed "
         "training, 'puregpu' for pure-GPU training.",
