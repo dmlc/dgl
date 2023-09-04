ac49220cd43afba6f71fd4bbdde13b4002061ec9
peizhou001 <110809584+peizhou001@users.noreply.github.com>
2023-09-04 14:01:44 +0800

    [Graphbolt] Convert a data block to dgl graphs (#6228)

--- python/dgl/graphbolt/minibatch.py
+++ python/dgl/graphbolt/minibatch.py
@@ -5,6 +5,9 @@ from typing import Dict, List, Tuple, Union
 
 import torch
 
+import dgl
+
+from .base import etype_str_to_tuple
 from .sampled_subgraph import SampledSubgraph
 
 __all__ = ["MiniBatch"]
@@ -122,3 +125,90 @@ class MiniBatch:
     Representation of compacted nodes corresponding to 'negative_tail', where
     all node ids inside are compacted.
     """
+
+    def to_dgl_graphs(self):
+        """Transforming a data graph into DGL graphs necessitates constructing a
+        graphical structure and assigning features to the nodes and edges within
+        the graphs.
+        """
+        if not self.sampled_subgraphs:
+            return None
+
+        is_heterogeneous = isinstance(
+            self.sampled_subgraphs[0].node_pairs, Dict
+        )
+
+        if is_heterogeneous:
+            graphs = []
+            for subgraph in self.sampled_subgraphs:
+                graphs.append(
+                    dgl.heterograph(
+                        {
+                            etype_str_to_tuple(etype): v
+                            for etype, v in subgraph.node_pairs.items()
+                        }
+                    )
+                )
+        else:
+            graphs = [
+                dgl.graph(subgraph.node_pairs)
+                for subgraph in self.sampled_subgraphs
+            ]
+
+        if is_heterogeneous:
+            # Assign node features to the outermost layer's nodes.
+            if self.node_features:
+                for (
+                    node_type,
+                    feature_name,
+                ), feature in self.node_features.items():
+                    graphs[0].nodes[node_type].data[feature_name] = feature
+            # Assign edge features.
+            if self.edge_features:
+                for graph, edge_feature in zip(graphs, self.edge_features):
+                    for (
+                        edge_type,
+                        feature_name,
+                    ), feature in edge_feature.items():
+                        graph.edges[etype_str_to_tuple(edge_type)].data[
+                            feature_name
+                        ] = feature
+            # Assign reverse node ids to the outermost layer's nodes.
+            reverse_row_node_ids = self.sampled_subgraphs[
+                0
+            ].reverse_row_node_ids
+            if reverse_row_node_ids:
+                for node_type, reverse_ids in reverse_row_node_ids.items():
+                    graphs[0].nodes[node_type].data[dgl.NID] = reverse_ids
+            # Assign reverse edges ids.
+            for graph, subgraph in zip(graphs, self.sampled_subgraphs):
+                if subgraph.reverse_edge_ids:
+                    for (
+                        edge_type,
+                        reverse_ids,
+                    ) in subgraph.reverse_edge_ids.items():
+                        graph.edges[etype_str_to_tuple(edge_type)].data[
+                            dgl.EID
+                        ] = reverse_ids
+        else:
+            # Assign node features to the outermost layer's nodes.
+            if self.node_features:
+                for feature_name, feature in self.node_features.items():
+                    graphs[0].ndata[feature_name] = feature
+            # Assign edge features.
+            if self.edge_features:
+                for graph, edge_feature in zip(graphs, self.edge_features):
+                    for feature_name, feature in edge_feature.items():
+                        graph.edata[feature_name] = feature
+            # Assign reverse node ids.
+            reverse_row_node_ids = self.sampled_subgraphs[
+                0
+            ].reverse_row_node_ids
+            if reverse_row_node_ids is not None:
+                graphs[0].ndata[dgl.NID] = reverse_row_node_ids
+            # Assign reverse edges ids.
+            for graph, subgraph in zip(graphs, self.sampled_subgraphs):
+                if subgraph.reverse_edge_ids is not None:
+                    graph.edata[dgl.EID] = subgraph.reverse_edge_ids
+
+        return graphs
