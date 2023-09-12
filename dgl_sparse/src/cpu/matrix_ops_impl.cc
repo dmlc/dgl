4b9c0b73086c6f5a26e11c651543281893d86bc6
xiangyuzhi <zhixy2021@mail.sustech.edu.cn>
2023-09-12 02:18:36 +0000

    add cpu compile support

---
+++ dgl_sparse/src/cpu/matrix_ops_impl.cc
@@ -0,0 +1,13 @@
+/**
+ *  Copyright (c) 2023 by Contributors
+ * @file matrix_ops.cc
+ * @brief DGL C++ matrix operators.
+ */
+#include "./matrix_ops_impl.h"
+
+#include <sparse/matrix_ops.h>
+#include <torch/script.h>
+
+namespace dgl {
+namespace sparse {}  // namespace sparse
+}  // namespace dgl
---
+++ dgl_sparse/src/matrix_ops_impl.h
@@ -0,0 +1,17 @@
+/**
+ *  Copyright (c) 2023 by Contributors
+ * @file matrix_ops_impl.h
+ * @brief DGL C++ sparse matrix operator implementations.
+ */
+#ifndef DGL_SPARSE_MATRIX_OPS_IMPL_H_
+#define DGL_SPARSE_MATRIX_OPS_IMPL_H_
+
+#include <sparse/sparse_format.h>
+
+#include <tuple>
+
+namespace dgl {
+namespace sparse {}
+}  // namespace dgl
+
+#endif  // DGL_SPARSE_MATRIX_OPS_IMPL_H_
