da1d5de2f16d2f0dc04589c5569d7afaaf1f486b
xiangyuzhi <zhixy2021@mail.sustech.edu.cn>
2023-09-12 16:43:30 +0800

    [Sparse] Add cpu compilation support (#6310)

---
+++ dgl_sparse/src/cpu/matrix_ops_impl.cc
@@ -0,0 +1,10 @@
+/**
+ *  Copyright (c) 2023 by Contributors
+ * @file cpu/matrix_ops_impl.cc
+ * @brief DGL C++ matrix operators.
+ */
+#include "./matrix_ops_impl.h"
+
+namespace dgl {
+namespace sparse {}  // namespace sparse
+}  // namespace dgl
