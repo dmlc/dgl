cbc34705c4e5675cdc658d25084286172579caf6
xiangyuzhi <zhixy2021@mail.sustech.edu.cn>
2023-09-18 20:12:30 +0800

    [Sparse] Compact C++ API (#6334)

--- dgl_sparse/src/matrix_ops_impl.h
+++ dgl_sparse/src/matrix_ops_impl.h
@@ -6,8 +6,22 @@
 #ifndef DGL_SPARSE_MATRIX_OPS_IMPL_H_
 #define DGL_SPARSE_MATRIX_OPS_IMPL_H_
 
+#include <sparse/sparse_format.h>
+
+#include <tuple>
+
 namespace dgl {
-namespace sparse {}
+namespace sparse {
+
+template <c10::DeviceType XPU, typename IdType>
+std::tuple<c10::intrusive_ptr<SparseMatrix>, torch::Tensor> CompactImpl(
+    const c10::intrusive_ptr<SparseMatrix>& mat, int64_t dim,
+    torch::Tensor leading_indices) {
+  // Place holder only.
+  return {mat, leading_indices};
+}
+
+}  // namespace sparse
 }  // namespace dgl
 
 #endif  // DGL_SPARSE_MATRIX_OPS_IMPL_H_
