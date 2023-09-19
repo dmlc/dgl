57a68f14565d993445e1da983dde9cc83af1cd66
keli-wen <whuwkl@gmail.com>
2023-09-19 09:46:50 +0000

    fix lint check

--- python/dgl/graphbolt/__init__.py
+++ python/dgl/graphbolt/__init__.py
@@ -19,9 +19,9 @@ from .negative_sampler import *
 from .sampled_subgraph import *
 from .subgraph_sampler import *
 from .utils import (
-    full_nodes_itemset,
     add_reverse_edges,
     exclude_seed_edges,
+    full_nodes_itemset,
     unique_and_compact,
     unique_and_compact_node_pairs,
 )
