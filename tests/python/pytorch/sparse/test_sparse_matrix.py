bf3a8d46e76d2ff3b6980ad4a32f20875d4dda8b
Ubuntu <ubuntu@ip-172-31-0-133.us-west-2.compute.internal>
2023-09-21 10:00:34 +0000

    modify the expected result

--- tests/python/pytorch/sparse/test_sparse_matrix.py
+++ tests/python/pytorch/sparse/test_sparse_matrix.py
@@ -602,7 +602,8 @@ def test_print():
                              [2, 1, 3]]),
              values=tensor([1., 1., 2.]),
              shape=(4, 4), nnz=3)"""
-        ) if str(ctx) == "cpu"
+        )
+        if str(ctx) == "cpu"
         else """SparseMatrix(indices=tensor([[1, 1, 3],
                              [2, 1, 3]]),
              values=tensor([1., 1., 2.], device='cuda:0'),
@@ -625,7 +626,8 @@ def test_print():
                             [-0.4126,  0.7250],
                             [-0.5416, -0.7022]]),
              shape=(4, 4), nnz=3, val_size=(2,))"""
-        ) if str(ctx) == "cpu"
+        )
+        if str(ctx) == "cpu"
         else """SparseMatrix(indices=tensor([[1, 1, 3],
                              [2, 1, 3]]),
              values=tensor([[ 1.3080,  1.5984],
