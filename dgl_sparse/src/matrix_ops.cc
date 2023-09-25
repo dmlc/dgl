94d75a9c1c917216a835c1cdeb0331b6f5e9db9f
xiangyuzhi <zhixy2021@mail.sustech.edu.cn>
2023-09-25 01:56:19 +0000

    fix native op

--- dgl_sparse/src/matrix_ops.cc
+++ dgl_sparse/src/matrix_ops.cc
@@ -6,9 +6,6 @@
 #include <sparse/matrix_ops.h>
 #include <torch/script.h>
 
-#include "./macro.h"
-#include "./matrix_ops_impl.h"
-
 namespace dgl {
 namespace sparse {
 
@@ -58,13 +55,106 @@ std::tuple<std::shared_ptr<COO>, torch::Tensor, torch::Tensor> COOIntersection(
   return {ret_coo, lhs_indices, rhs_indices};
 }
 
+std::tuple<torch::Tensor, torch::Tensor> CompactIndices(
+    const torch::Tensor& row,
+    const torch::optional<torch::Tensor>& leading_indices) {
+  torch::Tensor sort_row, sort_idx;
+  std::tie(sort_row, sort_idx) = row.sort(-1);
+  torch::Tensor rev_sort_idx = torch::empty_like(sort_idx);
+  rev_sort_idx.index_put_({sort_idx}, torch::arange(0, sort_idx.numel()));
+
+  torch::Tensor uniqued, uniq_idx;
+  int64_t n_leading_indices = 0;
+  if (leading_indices.has_value()) {
+    n_leading_indices = leading_indices.value().numel();
+    std::tie(uniqued, uniq_idx) = torch::_unique(
+        torch::cat({leading_indices.value(), sort_row}), false, true);
+  } else {
+    std::tie(uniqued, uniq_idx) = torch::_unique(sort_row, false, true);
+  }
+
+  auto new_row =
+      torch::arange(uniqued.numel() - 1, -1, -1)
+          .index_select(
+              0, uniq_idx.slice(
+                     0, n_leading_indices, n_leading_indices + row.size(-1)))
+          .index_select(0, rev_sort_idx);
+  return {new_row, uniqued};
+}
+
+std::tuple<c10::intrusive_ptr<SparseMatrix>, torch::optional<torch::Tensor>>
+CompactCOO(
+    const c10::intrusive_ptr<SparseMatrix>& mat, int64_t dim,
+    torch::optional<torch::Tensor> leading_indices) {
+  torch::Tensor row, col;
+  auto coo = mat->COOTensors();
+  if (dim == 0)
+    std::tie(row, col) = coo;
+  else
+    std::tie(col, row) = coo;
+
+  torch::Tensor new_row, uniqued;
+  std::tie(new_row, uniqued) = CompactIndices(row, leading_indices);
+
+  if (dim == 0) {
+    auto ret = SparseMatrix::FromCOO(
+        torch::stack({new_row, col}, 0), mat->value(),
+        std::vector<int64_t>{uniqued.numel(), mat->shape()[1]});
+    auto ret_idx = torch::optional<torch::Tensor>(uniqued.flip(-1));
+    return {ret, ret_idx};
+  } else {
+    auto ret = SparseMatrix::FromCOO(
+        torch::stack({col, new_row}, 0), mat->value(),
+        std::vector<int64_t>{mat->shape()[0], uniqued.numel()});
+    auto ret_idx = torch::optional<torch::Tensor>(uniqued.flip(-1));
+    return {ret, ret_idx};
+  }
+}
+
+std::tuple<c10::intrusive_ptr<SparseMatrix>, torch::optional<torch::Tensor>>
+CompactCSC(
+    const c10::intrusive_ptr<SparseMatrix>& mat, int64_t dim,
+    torch::optional<torch::Tensor> leading_indices) {
+  std::shared_ptr<dgl::sparse::CSR> csr;
+  if (dim == 0)
+    csr = mat->CSCPtr();
+  else
+    csr = mat->CSRPtr();
+
+  torch::Tensor new_indices, uniqued;
+  std::tie(new_indices, uniqued) =
+      CompactIndices(csr->indices, leading_indices);
+
+  if (dim == 0) {
+    auto ret = SparseMatrix::FromCSC(
+        csr->indptr, new_indices, mat->value(),
+        std::vector<int64_t>{uniqued.numel(), mat->shape()[1]});
+    auto ret_idx = torch::optional<torch::Tensor>(uniqued.flip(-1));
+    return {ret, ret_idx};
+  } else {
+    auto ret = SparseMatrix::FromCSR(
+        csr->indptr, new_indices, mat->value(),
+        std::vector<int64_t>{mat->shape()[0], uniqued.numel()});
+    auto ret_idx = torch::optional<torch::Tensor>(uniqued.flip(-1));
+    return {ret, ret_idx};
+  }
+}
+
 std::tuple<c10::intrusive_ptr<SparseMatrix>, torch::optional<torch::Tensor>>
 Compact(
     const c10::intrusive_ptr<SparseMatrix>& mat, int64_t dim,
     torch::optional<torch::Tensor> leading_indices) {
-  DGL_SPARSE_MAT_SWITCH(mat, XPU, IdType, ValType, "Compact", {
-    return CompactImpl<XPU, IdType, ValType>(mat, dim, leading_indices);
-  });
+  if (dim == 0) {
+    if (mat->HasCSC())
+      return CompactCSC(mat, dim, leading_indices);
+    else
+      return CompactCOO(mat, dim, leading_indices);
+  } else {
+    if (mat->HasCSR())
+      return CompactCSC(mat, dim, leading_indices);
+    else
+      return CompactCOO(mat, dim, leading_indices);
+  }
 }
 
 }  // namespace sparse
