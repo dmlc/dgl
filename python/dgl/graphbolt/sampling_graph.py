2e9ab66b0cae31cb9f52592f320d6490fd1feebc
Ubuntu <ubuntu@ip-172-31-39-125.ap-northeast-1.compute.internal>
2023-09-27 07:01:32 +0000

    create SamplingGraph class

---
+++ python/dgl/graphbolt/sampling_graph.py
@@ -0,0 +1,42 @@
+"""Sampling Graphs."""
+
+from typing import Dict, Union
+
+
+class SamplingGraph:
+    r"""Class for sampling graph."""
+
+    def __init__(self):
+        pass
+
+    @property
+    def num_nodes(self) -> Union[int, Dict[str, int]]:
+        """
+        >   For a homogenous graph, returns the number of nodes in it.
+        >   For a Heterogenous graph, returns a dictionary indicating
+            the numbers of all types of nodes in it.
+
+        Returns
+        -------
+        Union[int, Dict[str, int]]
+            The number of nodes in the entire graph (homogenous), or
+            a dictionary indicating the numbers of all types of nodes
+            in the graph (heterogenous).
+        """
+        raise NotImplementedError
+
+    @property
+    def num_edges(self) -> Union[int, Dict[str, int]]:
+        """
+        >   For a homogenous graph, returns the number of edges in it.
+        >   For a Heterogenous graph, returns a dictionary indicating
+            the numbers of all types of edges in it.
+
+        Returns
+        -------
+        Union[int, Dict[str, int]]
+            The number of edges in the entire graph (homogenous), or
+            a dictionary indicating the numbers of all types of edges
+            in the graph (heterogenous).
+        """
+        raise NotImplementedError
