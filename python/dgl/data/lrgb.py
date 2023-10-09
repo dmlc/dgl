8c2833f4e6a4f9981df28f58c821e82c1ec06533
paoxiaode <1435709051@qq.com>
2023-10-09 07:39:11 +0000

    add raise error in __getitem__ && refine docstring

--- python/dgl/data/lrgb.py
+++ python/dgl/data/lrgb.py
@@ -805,16 +805,18 @@ class COCOSuperpixelsDataset(DGLDataset):
     DGL dataset of COCOSuperpixels which contains image superpixels
     and a semantic segmentation label for each node superpixel.
 
-    Based on the COCO 2017 dataset. Original source `here<https://cocodataset.org>`_
+    Based on the COCO 2017 dataset. Original source `<https://cocodataset.org>`_
 
     COCO categories:
-    person bicycle car motorcycle airplane bus train truck boat traffic light fire hydrant stop
-    sign parking meter bench bird cat dog horse sheep cow elephant bear zebra giraffe backpack
-    umbrella handbag tie suitcase frisbee skis snowboard sports ball kite baseball bat baseball
-    glove skateboard surfboard tennis racket bottle wine glass cup fork knife spoon bowl banana
-    apple sandwich orange broccoli carrot hot dog pizza donut cake chair couch potted plant bed
-    dining table toilet tv laptop mouse remote keyboard cell phone microwave oven toaster sink
-    refrigerator book clock vase scissors teddy bear hair drier toothbrush
+    person bicycle car motorcycle airplane bus train truck boat traffic light
+    fire hydrant stop sign parking meter bench bird cat dog horse sheep cow
+    elephant bear zebra giraffe backpack umbrella handbag tie suitcase frisbee
+    skis snowboard sports ball kite baseball bat baseball glove skateboard
+    surfboard tennis racket bottle wine glass cup fork knife spoon bowl banana
+    apple sandwich orange broccoli carrot hot dog pizza donut cake chair couch
+    potted plant bed dining table toilet tv laptop mouse remote keyboard cell
+    phone microwave oven toaster sink refrigerator book clock vase scissors
+    teddy bear hair drier toothbrush
 
     Reference `<https://arxiv.org/abs/2206.08164.pdf>`_
 
@@ -877,7 +879,8 @@ class COCOSuperpixelsDataset(DGLDataset):
         ndata_schemes={'feat': Scheme(shape=(14,), dtype=torch.float32), 'label': Scheme(shape=(), dtype=torch.uint8)}
         edata_schemes={'feat': Scheme(shape=(2,), dtype=torch.float32)})
 
-    >>> # accept tensor to be index, but will ignore transform parameter
+    >>> # support tensor to be index when transform is None
+    >>> # see details in __getitem__ function
     >>> import torch
     >>> idx = torch.tensor([0, 1, 2])
     >>> train_dataset_subset = train_dataset[idx]
@@ -984,90 +987,14 @@ class COCOSuperpixelsDataset(DGLDataset):
         os.unlink(path)
 
     def label_remap(self):
-        # Util function to remap the labels as the original label idxs are not contiguous
-
+        # Util function to remap the labels as the original label
+        # idxs are not contiguous
         original_label_idx = [
-            0,
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-            13,
-            14,
-            15,
-            16,
-            17,
-            18,
-            19,
-            20,
-            21,
-            22,
-            23,
-            24,
-            25,
-            27,
-            28,
-            31,
-            32,
-            33,
-            34,
-            35,
-            36,
-            37,
-            38,
-            39,
-            40,
-            41,
-            42,
-            43,
-            44,
-            46,
-            47,
-            48,
-            49,
-            50,
-            51,
-            52,
-            53,
-            54,
-            55,
-            56,
-            57,
-            58,
-            59,
-            60,
-            61,
-            62,
-            63,
-            64,
-            65,
-            67,
-            70,
-            72,
-            73,
-            74,
-            75,
-            76,
-            77,
-            78,
-            79,
-            80,
-            81,
-            82,
-            84,
-            85,
-            86,
-            87,
-            88,
-            89,
-            90,
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+            40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+            58, 59, 60, 61, 62, 63, 64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78,
+            79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90
         ]
         label_map = {}
         for i, key in enumerate(original_label_idx):
@@ -1129,7 +1056,7 @@ class COCOSuperpixelsDataset(DGLDataset):
         Parameters
         ---------
         idx : int or tensor
-            The sample index, if idx is tensor will ignore transform.
+            The sample index, support tensor idx when transform is None.
 
         Returns
         -------
@@ -1144,7 +1071,14 @@ class COCOSuperpixelsDataset(DGLDataset):
             Subset of the dataset at specified indices
         """
         if F.is_tensor(idx) and idx.dim() == 1:
-            return Subset(self, idx.cpu())
+            if self._transform is None:
+                return Subset(self, idx.cpu())
+            else:
+                raise ValueError(
+                    """
+                    Not support tensor idx when transform is not None
+                    """
+                )
 
         if self._transform is None:
             return self.graphs[idx]
