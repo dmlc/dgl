a2a3a9132579ef49929ed00d2ec8a681440cdb63
Rhett Ying <85214957+Rhett-Ying@users.noreply.github.com>
2023-10-11 16:41:42 +0800

    [GraphBolt][Doc] update examples and display methods on page (#6426)

--- python/dgl/graphbolt/impl/uniform_negative_sampler.py
+++ python/dgl/graphbolt/impl/uniform_negative_sampler.py
@@ -17,6 +17,35 @@ class UniformNegativeSampler(NegativeSampler):
     For each edge ``(u, v)``, it is supposed to generate `negative_ratio` pairs
     of negative edges ``(u, v')``, where ``v'`` is chosen uniformly from all
     the nodes in the graph.
+
+    Parameters
+    ----------
+    datapipe : DataPipe
+        The datapipe.
+    graph : CSCSamplingGraph
+        The graph on which to perform negative sampling.
+    negative_ratio : int
+        The proportion of negative samples to positive samples.
+
+    Examples
+    --------
+    >>> from dgl import graphbolt as gb
+    >>> indptr = torch.LongTensor([0, 2, 4, 5])
+    >>> indices = torch.LongTensor([1, 2, 0, 2, 0])
+    >>> graph = gb.from_csc(indptr, indices)
+    >>> node_pairs = (torch.tensor([0, 1]), torch.tensor([1, 2]))
+    >>> item_set = gb.ItemSet(node_pairs, names="node_pairs")
+    >>> item_sampler = gb.ItemSampler(
+        ...item_set, batch_size=1,
+        ...)
+    >>> neg_sampler = gb.UniformNegativeSampler(
+        ...item_sampler, graph, 2)
+    >>> for minibatch in neg_sampler:
+        ...  print(minibatch.negative_srcs)
+        ...  print(minibatch.negative_dsts)
+        ...
+    (tensor([0, 0, 0]), tensor([1, 1, 2]), tensor([1, 0, 0]))
+    (tensor([1, 1, 1]), tensor([2, 1, 2]), tensor([1, 0, 0]))
     """
 
     def __init__(
@@ -25,38 +54,6 @@ class UniformNegativeSampler(NegativeSampler):
         graph,
         negative_ratio,
     ):
-        """
-        Initlization for a uniform negative sampler.
-
-        Parameters
-        ----------
-        datapipe : DataPipe
-            The datapipe.
-        graph : CSCSamplingGraph
-            The graph on which to perform negative sampling.
-        negative_ratio : int
-            The proportion of negative samples to positive samples.
-
-        Examples
-        --------
-        >>> from dgl import graphbolt as gb
-        >>> indptr = torch.LongTensor([0, 2, 4, 5])
-        >>> indices = torch.LongTensor([1, 2, 0, 2, 0])
-        >>> graph = gb.from_csc(indptr, indices)
-        >>> node_pairs = (torch.tensor([0, 1]), torch.tensor([1, 2]))
-        >>> item_set = gb.ItemSet(node_pairs, names="node_pairs")
-        >>> item_sampler = gb.ItemSampler(
-            ...item_set, batch_size=1,
-            ...)
-        >>> neg_sampler = gb.UniformNegativeSampler(
-            ...item_sampler, graph, 2)
-        >>> for minibatch in neg_sampler:
-            ...  print(minibatch.negative_srcs)
-            ...  print(minibatch.negative_dsts)
-            ...
-        (tensor([0, 0, 0]), tensor([1, 1, 2]), tensor([1, 0, 0]))
-        (tensor([1, 1, 1]), tensor([2, 1, 2]), tensor([1, 0, 0]))
-        """
         super().__init__(datapipe, negative_ratio)
         self.graph = graph
 
