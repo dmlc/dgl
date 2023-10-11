6baa69bbdca9f7862ad601cf74dc557512e6721c
Ubuntu <ubuntu@ip-172-31-0-133.us-west-2.compute.internal>
2023-10-11 09:39:59 +0000

    modify doc

--- python/dgl/graphbolt/impl/neighbor_sampler.py
+++ python/dgl/graphbolt/impl/neighbor_sampler.py
@@ -44,7 +44,7 @@ class NeighborSampler(SubgraphSampler):
         probabilities corresponding to each neighboring edge of a node.
         It must be a 1D floating-point or boolean tensor, with the number
         of elements equalling the total number of edges.
-    deduplicated: bool
+    deduplicate: bool
         Boolean indicating whether seeds between hops will be deduplicated.
         If True, the same elements in seeds will be deleted to only one.
         Otherwise, the same elements will be remained.
@@ -175,7 +175,7 @@ class LayerNeighborSampler(NeighborSampler):
         probabilities corresponding to each neighboring edge of a node.
         It must be a 1D floating-point or boolean tensor, with the number
         of elements equalling the total number of edges.
-    deduplicated: bool
+    deduplicate: bool
             Boolean indicating whether seeds between hops will be deduplicated.
             If True, the same elements in seeds will be deleted to only one.
             Otherwise, the same elements will be remained.
