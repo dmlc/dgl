f072415dd5f3f1e7595f9b511ad11e8412140e83
Ubuntu <zhixy2021@mail.sustech.edu.cn>
2023-10-23 02:25:35 +0000

    concise code

--- examples/sparse/graphsage.py
+++ examples/sparse/graphsage.py
@@ -25,8 +25,50 @@ import dgl.sparse as dglsp
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from dgl import AddSelfLoop, graph
+from dgl import AddSelfLoop
 from dgl.data import CiteseerGraphDataset, CoraGraphDataset, PubmedGraphDataset
+
+
+class SAGEConv(nn.Module):
+    r"""GraphSAGE layer from `Inductive Representation Learning on
+    Large Graphs <https://arxiv.org/pdf/1706.02216.pdf>`__
+    """
+
+    def __init__(
+        self,
+        in_feats,
+        out_feats,
+    ):
+        super(SAGEConv, self).__init__()
+        self._in_src_feats, self._in_dst_feats = in_feats, in_feats
+        self._out_feats = out_feats
+
+        self.fc_neigh = nn.Linear(self._in_src_feats, out_feats, bias=False)
+        self.fc_self = nn.Linear(self._in_dst_feats, out_feats, bias=True)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        gain = nn.init.calculate_gain("relu")
+        nn.init.xavier_uniform_(self.fc_self.weight, gain=gain)
+        nn.init.xavier_uniform_(self.fc_neigh.weight, gain=gain)
+
+    def forward(self, A, feat):
+        feat_src = feat_dst = feat
+        feat_dst = feat_src[: A.shape[0]]
+
+        # Aggregator type: mean
+        h_self = feat_dst
+        srcdata, dstdata = {}, {}
+        srcdata["h"] = self.fc_neigh(feat_src)
+        # Divided by degree.
+        D_hat = dglsp.diag(A.sum(0)) ** -1
+        A_div = A @ D_hat
+        dstdata["neigh"] = A_div.T @ srcdata["h"]
+        h_neigh = dstdata["neigh"]
+
+        rst = self.fc_self(h_self) + h_neigh
+
+        return rst
 
 
 class SAGE(nn.Module):
@@ -34,8 +76,8 @@ class SAGE(nn.Module):
         super().__init__()
         self.layers = nn.ModuleList()
         # Two-layer GraphSAGE-gcn.
-        self.layers.append(SAGEConv(in_size, hidden_size, "gcn"))
-        self.layers.append(SAGEConv(hidden_size, out_size, "gcn"))
+        self.layers.append(SAGEConv(in_size, hidden_size))
+        self.layers.append(SAGEConv(hidden_size, out_size))
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, graph, x):
@@ -78,101 +120,6 @@ def train(A, features, labels, masks, model):
         print(
             f"Epoch {epoch:05d} | Loss {loss.item():.4f} | Accuracy {acc:.4f} "
         )
-
-
-class SAGEConv(nn.Module):
-    r"""GraphSAGE layer from `Inductive Representation Learning on
-    Large Graphs <https://arxiv.org/pdf/1706.02216.pdf>`__
-    """
-
-    def __init__(
-        self,
-        in_feats,
-        out_feats,
-        aggregator_type="gcn",
-        feat_drop=0.0,
-        bias=True,
-        norm=None,
-        activation=None,
-    ):
-        super(SAGEConv, self).__init__()
-        self._in_src_feats, self._in_dst_feats = in_feats, in_feats
-        self._out_feats = out_feats
-        self._aggre_type = aggregator_type
-        self.norm = norm
-        self.feat_drop = nn.Dropout(feat_drop)
-        self.activation = activation
-
-        # aggregator type: mean/gcn
-        self.fc_neigh = nn.Linear(self._in_src_feats, out_feats, bias=False)
-
-        if aggregator_type != "gcn":
-            self.fc_self = nn.Linear(self._in_dst_feats, out_feats, bias=bias)
-        elif bias:
-            self.bias = nn.parameter.Parameter(torch.zeros(self._out_feats))
-        else:
-            self.register_buffer("bias", None)
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        gain = nn.init.calculate_gain("relu")
-        if self._aggre_type != "gcn":
-            nn.init.xavier_uniform_(self.fc_self.weight, gain=gain)
-        nn.init.xavier_uniform_(self.fc_neigh.weight, gain=gain)
-
-    def forward(self, A, feat):
-        feat_src = feat_dst = self.feat_drop(feat)
-        feat_dst = feat_src[: A.shape[0]]
-
-        h_self = feat_dst
-        lin_before_mp = self._in_src_feats > self._out_feats
-        srcdata, dstdata = {}, {}
-        if self._aggre_type == "mean":
-            srcdata["h"] = (
-                self.fc_neigh(feat_src) if lin_before_mp else feat_src
-            )
-            # Div by degree
-            D_hat = dglsp.diag(A_hat.sum(0)) ** -1
-            A_div = A_hat @ D_hat
-            dstdata["neigh"] = A_div.T @ srcdata["h"]
-            h_neigh = graph.dstdata["neigh"]
-            if not lin_before_mp:
-                h_neigh = self.fc_neigh(h_neigh)
-        elif self._aggre_type == "gcn":
-            srcdata["h"] = (
-                self.fc_neigh(feat_src) if lin_before_mp else feat_src
-            )
-            # Add self loop
-            I = dglsp.identity(A.shape)
-            A_hat = A + I
-            # Div by degree
-            D_hat = dglsp.diag(A_hat.sum(0)) ** -1
-            A_div = A_hat @ D_hat
-            dstdata["neigh"] = A_div.T @ srcdata["h"]
-            h_neigh = dstdata["neigh"]
-            if not lin_before_mp:
-                h_neigh = self.fc_neigh(h_neigh)
-        else:
-            raise KeyError(
-                "Aggregator type {} not recognized.".format(self._aggre_type)
-            )
-
-        if self._aggre_type == "gcn":
-            rst = h_neigh
-            # add bias manually for GCN
-            if self.bias is not None:
-                rst = rst + self.bias
-        else:
-            rst = self.fc_self(h_self) + h_neigh
-
-        # activation
-        if self.activation is not None:
-            rst = self.activation(rst)
-        # normalization
-        if self.norm is not None:
-            rst = self.norm(rst)
-        return rst
 
 
 if __name__ == "__main__":
@@ -221,7 +168,7 @@ if __name__ == "__main__":
     out_size = data.num_classes
     model = SAGE(in_size, 16, out_size).to(device)
 
-    # Create sparse
+    # Create sparse.
     indices = torch.stack(g.edges())
     N = g.num_nodes()
     A = dglsp.spmatrix(indices, shape=(N, N))
