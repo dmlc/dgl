0733a93e69400f69dd09d29b58ffe739002fba81
Ubuntu <ubuntu@ip-172-31-28-63.ap-northeast-1.compute.internal>
2023-10-24 08:28:48 +0000

    blabla

--- examples/sampling/graphbolt/node_classification.py
+++ examples/sampling/graphbolt/node_classification.py
@@ -39,8 +39,6 @@ main
 """
 import argparse
 
-from typing import Literal
-
 import dgl.graphbolt as gb
 import dgl.nn as dglnn
 import torch
@@ -51,7 +49,7 @@ import tqdm
 
 
 def create_dataloader(
-    args, graph, features, itemset, job: Literal["train", "evaluate", "infer"]
+    args, graph, features, itemset, job
 ):
     """
     [HIGHLIGHT]
@@ -70,7 +68,7 @@ def create_dataloader(
         The node features.
     itemset : Union[ItemSet, ItemSetDict]
         Data to be sampled.
-    job : Literal["train", "evaluate", "infer"]
+    job : one of ["train", "evaluate", "infer"]
         The stage where dataloader is created, with options "train", "evaluate"
         and "infer".
     """
