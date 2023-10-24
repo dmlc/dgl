1b5b7de5daef4fbc93934e525ab3c0e8c7d029d1
czkkkkkk <zekucai@gmail.com>
2023-10-24 09:34:33 +0800

    [Graphbolt] Support UVA index select for TorchBasedFeature (#6417)

---
+++ graphbolt/src/index_select.cc
@@ -0,0 +1,24 @@
+/**
+ *  Copyright (c) 2023 by Contributors
+ * @file index_select.cc
+ * @brief Index select operators.
+ */
+#include "./index_select.h"
+
+#include "./macro.h"
+
+namespace graphbolt {
+namespace ops {
+
+torch::Tensor IndexSelect(torch::Tensor input, torch::Tensor index) {
+  if (input.is_pinned() &&
+      (index.is_pinned() || index.device().type() == c10::DeviceType::CUDA)) {
+    GRAPHBOLT_DISPATCH_CUDA_ONLY_DEVICE(
+        c10::DeviceType::CUDA, "UVAIndexSelect",
+        { return UVAIndexSelectImpl(input, index); });
+  }
+  return input.index({index.to(torch::kLong)});
+}
+
+}  // namespace ops
+}  // namespace graphbolt
