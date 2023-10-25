9801cc8cec020a3462572e81fed0128c4d5440a2
SinuoXu <147471236+SinuoXu@users.noreply.github.com>
2023-10-25 21:34:16 +0800

    Update utils.py

--- examples/pytorch/ogc/utils.py
+++ examples/pytorch/ogc/utils.py
@@ -5,24 +5,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+
 def sparse_mx_to_torch_sparse_tensor(sparse_mx):
     """Convert a scipy sparse matrix to a torch sparse tensor."""
     sparse_mx = sparse_mx.tocoo().astype(np.float32)
     indices = torch.from_numpy(
-        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64)
+    )
     values = torch.from_numpy(sparse_mx.data)
     shape = torch.Size(sparse_mx.shape)
     return torch.sparse.FloatTensor(indices, values, shape)
 
 
 def symmetric_normalize_adjacency(graph):
-    """ Symmetric normalize graph adjacency matrix. """
+    """Symmetric normalize graph adjacency matrix."""
     adj = graph.adjacency_matrix()
     in_degs = graph.in_degrees().float()
     in_norm = torch.pow(in_degs, -0.5).unsqueeze(-1)
     degi = torch.diag(torch.squeeze(torch.t(in_norm)))
     degi = sp.coo_matrix(degi.cpu()).tocsr()
-    adj = sp.csr_matrix((adj.val.cpu(), (adj.row.cpu(), adj.col.cpu())), shape=adj.shape)
+    adj = sp.csr_matrix(
+        (adj.val.cpu(), (adj.row.cpu(), adj.col.cpu())), shape=adj.shape
+    )
     adj = degi.dot(adj.dot(degi))
     return adj
 
@@ -35,32 +39,34 @@ class LinearNeuralNetwork(nn.Module):
     def forward(self, x):
         return self.W(x)
 
-    def test(self, U, g):
+    def test(self, embeds, g):
         self.eval()
         with torch.no_grad():
-            output = self(U)
+            output = self(embeds)
             pred = output.argmax(dim=-1)
             labels = g.ndata["label"]
             test_mask = g.ndata["test_mask"]
             tv_mask = g.ndata["train_mask"] + g.ndata["val_mask"]
-            loss_tv = F.mse_loss(output[tv_mask],
-                                 F.one_hot(labels).float()[tv_mask])
+            loss_tv = F.mse_loss(
+                output[tv_mask], F.one_hot(labels).float()[tv_mask]
+            )
             accs = []
             for mask in [tv_mask, test_mask]:
                 accs.append(
-                    float((pred[mask] == labels[mask]).sum()/mask.sum()))
+                    float((pred[mask] == labels[mask]).sum() / mask.sum())
+                )
         return loss_tv.item(), accs[0], accs[1], pred
 
-    def update_W(self, U, g, eta_W):
-        optimizer = optim.SGD(self.parameters(), lr=eta_W)
+    def update_weight(self, embeds, g, lr_clf):
+        optimizer = optim.SGD(self.parameters(), lr=lr_clf)
         self.train()
         optimizer.zero_grad()
-        output = self(U)
+        output = self(embeds)
         labels = g.ndata["label"]
         tv_mask = g.ndata["train_mask"] + g.ndata["val_mask"]
-        loss_tv = F.mse_loss(output[tv_mask],
-                             F.one_hot(labels).float()[tv_mask],
-                             reduction='sum')
+        loss_tv = F.mse_loss(
+            output[tv_mask], F.one_hot(labels).float()[tv_mask], reduction="sum"
+        )
         loss_tv.backward()
         optimizer.step()
-        return self(U).data, self.W.weight.data
+        return self(embeds).data, self.W.weight.data
