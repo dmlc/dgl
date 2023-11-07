a5a97a96b7a596df9b96baecda7b53d5c8603b3e
keli-wen <whuwkl@gmail.com>
2023-11-07 10:40:37 +0000

    Fix the useless variable and add some explanation

--- python/dgl/graphbolt/subgraph_sampler.py
+++ python/dgl/graphbolt/subgraph_sampler.py
@@ -106,10 +106,8 @@ class SubgraphSampler(MiniBatchTransformer):
             # Collect nodes from all types of input.
             nodes = list(node_pairs)
             if has_neg_src:
-                neg_src_shape = neg_src.shape
                 nodes.append(neg_src.view(-1))
             if has_neg_dst:
-                neg_dst_shape = neg_dst.shape
                 nodes.append(neg_dst.view(-1))
             # Unique and compact the collected nodes.
             seeds, compacted = unique_and_compact(nodes)
@@ -118,13 +116,16 @@ class SubgraphSampler(MiniBatchTransformer):
             compacted = compacted[2:]
             if has_neg_src:
                 compacted_negative_srcs = compacted.pop(0)
+                # Since we need to calculate the neg_ratio according to the
+                # compacted_negatvie_srcs shape, we need to reshape it back.
                 compacted_negative_srcs = compacted_negative_srcs.view(
-                    neg_src_shape
+                    neg_src.shape
                 )
             if has_neg_dst:
                 compacted_negative_dsts = compacted.pop(0)
+                # Same as above.
                 compacted_negative_dsts = compacted_negative_dsts.view(
-                    neg_dst_shape
+                    neg_dst.shape
                 )
         return (
             seeds,
