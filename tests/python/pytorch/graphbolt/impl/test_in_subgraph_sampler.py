fb53753cf16584e0bf6c80c56afcc080d9172836
Skeleton003 <799284168@qq.com>
2023-11-14 11:10:03 +0000

    nonrandom

--- tests/python/pytorch/graphbolt/impl/test_in_subgraph_sampler.py
+++ tests/python/pytorch/graphbolt/impl/test_in_subgraph_sampler.py
@@ -5,12 +5,22 @@ import torch
 
 
 def test_InSubgraphSampler_homo():
-    num_seeds = 100
-    item_set = gb.ItemSet(torch.arange(0, num_seeds), names="seed_nodes")
-    graph = gb_test_utils.rand_csc_graph(num_seeds, density=0.1)
+    """Original graph in COO:
+    1   0   1   0   1   0
+    1   0   0   1   0   1
+    0   1   0   1   0   0
+    0   1   0   0   1   0
+    1   0   0   0   0   1
+    0   0   1   0   1   0
+    """
+    indptr = torch.LongTensor([0, 3, 5, 7, 9, 12, 14])
+    indices = torch.LongTensor([0, 1, 4, 2, 3, 0, 5, 1, 2, 0, 3, 5, 1, 4])
+    graph = gb.from_fused_csc(indptr, indices)
 
-    batch_size = 10
-    item_sampler = gb.ItemSampler(item_set, batch_size=batch_size, shuffle=True)
+    item_set = gb.ItemSet([0, 1, 2])
+    batch_size = 1
+    item_sampler = gb.ItemSampler(item_set, batch_size=batch_size)
+
     in_subgraph_sampler = gb.InSubgraphSampler(item_sampler, graph)
 
     adjacency_list = [
@@ -19,7 +29,6 @@ def test_InSubgraphSampler_homo():
     ]
 
     for _, data in enumerate(in_subgraph_sampler):
-        assert len(data.seed_nodes) == batch_size
         sampled_subgraph = data.sampled_subgraphs[0]
         src = [
             sampled_subgraph.original_row_node_ids[id]
