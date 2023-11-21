4df2e3991effb4f9a17dcff02a2bcc9c7efefdfe
yxy235 <77922129+yxy235@users.noreply.github.com>
2023-11-21 16:29:31 +0800

    [GraphBolt] Add not_deduplication compact. (#6579)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-0-133.us-west-2.compute.internal>

--- python/dgl/graphbolt/__init__.py
+++ python/dgl/graphbolt/__init__.py
@@ -20,6 +20,7 @@ from .sampled_subgraph import *
 from .subgraph_sampler import *
 from .utils import (
     add_reverse_edges,
+    compact_csc_format,
     exclude_seed_edges,
     unique_and_compact,
     unique_and_compact_node_pairs,
