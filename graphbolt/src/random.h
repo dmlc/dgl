c0f4f8356cfae2aa80cbba1b2b7d6829ddac1d35
Ubuntu <ubuntu@ip-172-31-28-63.ap-northeast-1.compute.internal>
2023-11-24 04:03:49 +0000

    int64

--- graphbolt/src/random.h
+++ graphbolt/src/random.h
@@ -93,7 +93,7 @@ class RandomEngine {
   pcg32 rng_;
 };
 
-void SetSeed(uint64_t seed) {
+void SetSeed(int64_t seed) {
   int64_t num_threads = torch::get_num_threads();
   torch::parallel_for(0, num_threads, 1, [&](size_t start, size_t end) {
     for (auto i = start; i < end; ++i) {
