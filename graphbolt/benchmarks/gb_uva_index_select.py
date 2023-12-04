a0d2b5695621c774c845484a0faf7bcaad930b4b
Muhammed Fatih Balin <m.f.balin@gmail.com>
2023-12-04 15:59:05 +0000

    add more sizes

--- graphbolt/benchmarks/gb_uva_index_select.py
+++ graphbolt/benchmarks/gb_uva_index_select.py
@@ -68,7 +68,7 @@ def test_index_select_throughput(feature, indices):
 
 
 available_RAM = 10 * (2**30)  ## 10 GiB
-n_rows = [2000000, 20000000, 200000000]
+n_rows = [2000000 * factor for factor in [1, 10, 100, 1000]]
 feat_size = [1, 4, 47, 256, 353]
 num_indices = [1, 1000, 100000, 1000000]
 dtypes = [torch.float32, torch.int8]
