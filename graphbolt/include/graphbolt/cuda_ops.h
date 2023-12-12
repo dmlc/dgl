25812cc0af04c2b3dc6fd1303ad5a98ec5357dd4
Muhammed Fatih Balin <m.f.balin@gmail.com>
2023-12-12 22:50:59 +0000

    initial implementation using a different approach.

--- graphbolt/include/graphbolt/cuda_ops.h
+++ graphbolt/include/graphbolt/cuda_ops.h
@@ -10,7 +10,7 @@
 namespace graphbolt {
 namespace ops {
 
-std::pair<torch::Tensor, torch::Tensor> Sort(torch::Tensor input, int num_bits);
+std::pair<torch::Tensor, torch::Tensor> Sort(torch::Tensor input, int num_bits = 0);
 
 std::tuple<torch::Tensor, torch::Tensor> IndexSelectCSCImpl(
     torch::Tensor indptr, torch::Tensor indices, torch::Tensor nodes);
@@ -19,6 +19,10 @@ std::tuple<torch::Tensor, torch::Tensor> UVAIndexSelectCSCImpl(
     torch::Tensor indptr, torch::Tensor indices, torch::Tensor nodes);
 
 torch::Tensor UVAIndexSelectImpl(torch::Tensor input, torch::Tensor index);
+
+std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> UniqueAndCompact(
+    const torch::Tensor src_ids, const torch::Tensor dst_ids,
+    const torch::Tensor unique_dst_ids, int num_bits = 0);
 
 }  //  namespace ops
 }  //  namespace graphbolt
