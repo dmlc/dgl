196f7e0d8fad43a7797ab1aeb2175661b71b7bf6
Muhammed Fatih Balin <m.f.balin@gmail.com>
2023-12-20 00:07:43 +0000

    use F.ctx() for tests

--- tests/python/pytorch/graphbolt/test_graphbolt_utils.py
+++ tests/python/pytorch/graphbolt/test_graphbolt_utils.py
@@ -72,19 +72,15 @@ def test_find_reverse_edges_circual_reverse_types():
 
 
 def test_unique_and_compact_hetero():
-    device = torch.device(
-        "cpu" if F._default_context_str == "cpu" else "cuda:0"
-    )
-
     N1 = torch.tensor(
-        [0, 5, 2, 7, 12, 7, 9, 5, 6, 2, 3, 4, 1, 0, 9], device=device
+        [0, 5, 2, 7, 12, 7, 9, 5, 6, 2, 3, 4, 1, 0, 9], device=F.ctx()
     )
-    N2 = torch.tensor([0, 3, 3, 5, 2, 7, 2, 8, 4, 9, 2, 3], device=device)
-    N3 = torch.tensor([1, 2, 6, 6, 1, 8, 3, 6, 3, 2], device=device)
+    N2 = torch.tensor([0, 3, 3, 5, 2, 7, 2, 8, 4, 9, 2, 3], device=F.ctx())
+    N3 = torch.tensor([1, 2, 6, 6, 1, 8, 3, 6, 3, 2], device=F.ctx())
     expected_unique = {
-        "n1": torch.tensor([0, 5, 2, 7, 12, 9, 6, 3, 4, 1], device=device),
-        "n2": torch.tensor([0, 3, 5, 2, 7, 8, 4, 9], device=device),
-        "n3": torch.tensor([1, 2, 6, 8, 3], device=device),
+        "n1": torch.tensor([0, 5, 2, 7, 12, 9, 6, 3, 4, 1], device=F.ctx()),
+        "n2": torch.tensor([0, 3, 5, 2, 7, 8, 4, 9], device=F.ctx()),
+        "n3": torch.tensor([1, 2, 6, 8, 3], device=F.ctx()),
     }
     if N1.is_cuda:
         expected_reverse_id = {
@@ -93,7 +89,7 @@ def test_unique_and_compact_hetero():
         expected_unique = {k: v.sort()[0] for k, v in expected_unique.items()}
     else:
         expected_reverse_id = {
-            k: torch.arange(0, v.shape[0], device=device)
+            k: torch.arange(0, v.shape[0], device=F.ctx())
             for k, v in expected_unique.items()
         }
     nodes_dict = {
@@ -103,21 +99,21 @@ def test_unique_and_compact_hetero():
     }
     expected_nodes_dict = {
         "n1": [
-            torch.tensor([0, 1, 2, 3, 4], device=device),
-            torch.tensor([3, 5, 1, 6, 2], device=device),
-            torch.tensor([7, 8, 9, 0, 5], device=device),
+            torch.tensor([0, 1, 2, 3, 4], device=F.ctx()),
+            torch.tensor([3, 5, 1, 6, 2], device=F.ctx()),
+            torch.tensor([7, 8, 9, 0, 5], device=F.ctx()),
         ],
         "n2": [
-            torch.tensor([0, 1, 1, 2], device=device),
-            torch.tensor([3, 4, 3, 5], device=device),
-            torch.tensor([6, 7, 3, 1], device=device),
+            torch.tensor([0, 1, 1, 2], device=F.ctx()),
+            torch.tensor([3, 4, 3, 5], device=F.ctx()),
+            torch.tensor([6, 7, 3, 1], device=F.ctx()),
         ],
         "n3": [
-            torch.tensor([0, 1], device=device),
-            torch.tensor([2, 2], device=device),
-            torch.tensor([0, 3], device=device),
-            torch.tensor([4, 2], device=device),
-            torch.tensor([4, 1], device=device),
+            torch.tensor([0, 1], device=F.ctx()),
+            torch.tensor([2, 2], device=F.ctx()),
+            torch.tensor([0, 3], device=F.ctx()),
+            torch.tensor([4, 2], device=F.ctx()),
+            torch.tensor([4, 1], device=F.ctx()),
         ],
     }
 
@@ -135,28 +131,24 @@ def test_unique_and_compact_hetero():
 
 
 def test_unique_and_compact_homo():
-    device = torch.device(
-        "cpu" if F._default_context_str == "cpu" else "cuda:0"
-    )
-
     N = torch.tensor(
-        [0, 5, 2, 7, 12, 7, 9, 5, 6, 2, 3, 4, 1, 0, 9], device=device
+        [0, 5, 2, 7, 12, 7, 9, 5, 6, 2, 3, 4, 1, 0, 9], device=F.ctx()
     )
     expected_unique_N = torch.tensor(
-        [0, 5, 2, 7, 12, 9, 6, 3, 4, 1], device=device
+        [0, 5, 2, 7, 12, 9, 6, 3, 4, 1], device=F.ctx()
     )
     if N.is_cuda:
         expected_reverse_id_N = expected_unique_N.sort()[1]
         expected_unique_N = expected_unique_N.sort()[0]
     else:
         expected_reverse_id_N = torch.arange(
-            0, expected_unique_N.shape[0], device=device
+            0, expected_unique_N.shape[0], device=F.ctx()
         )
     nodes_list = N.split(5)
     expected_nodes_list = [
-        torch.tensor([0, 1, 2, 3, 4], device=device),
-        torch.tensor([3, 5, 1, 6, 2], device=device),
-        torch.tensor([7, 8, 9, 0, 5], device=device),
+        torch.tensor([0, 1, 2, 3, 4], device=F.ctx()),
+        torch.tensor([3, 5, 1, 6, 2], device=F.ctx()),
+        torch.tensor([7, 8, 9, 0, 5], device=F.ctx()),
     ]
 
     unique, compacted = gb.unique_and_compact(nodes_list)
@@ -169,58 +161,54 @@ def test_unique_and_compact_homo():
 
 
 def test_unique_and_compact_node_pairs_hetero():
-    device = torch.device(
-        "cpu" if F._default_context_str == "cpu" else "cuda:0"
-    )
-
     node_pairs = {
         "n1:e1:n2": (
-            torch.tensor([1, 3, 4, 6, 2, 7, 9, 4, 2, 6], device=device),
-            torch.tensor([2, 2, 2, 4, 1, 1, 1, 3, 3, 3], device=device),
+            torch.tensor([1, 3, 4, 6, 2, 7, 9, 4, 2, 6], device=F.ctx()),
+            torch.tensor([2, 2, 2, 4, 1, 1, 1, 3, 3, 3], device=F.ctx()),
         ),
         "n1:e2:n3": (
-            torch.tensor([5, 2, 6, 4, 7, 2, 8, 1, 3, 0], device=device),
-            torch.tensor([1, 3, 3, 3, 2, 2, 2, 7, 7, 7], device=device),
+            torch.tensor([5, 2, 6, 4, 7, 2, 8, 1, 3, 0], device=F.ctx()),
+            torch.tensor([1, 3, 3, 3, 2, 2, 2, 7, 7, 7], device=F.ctx()),
         ),
         "n2:e3:n3": (
-            torch.tensor([2, 5, 4, 1, 4, 3, 6, 0], device=device),
-            torch.tensor([1, 1, 3, 3, 2, 2, 7, 7], device=device),
+            torch.tensor([2, 5, 4, 1, 4, 3, 6, 0], device=F.ctx()),
+            torch.tensor([1, 1, 3, 3, 2, 2, 7, 7], device=F.ctx()),
         ),
     }
 
     expected_unique_nodes = {
-        "n1": torch.tensor([1, 3, 4, 6, 2, 7, 9, 5, 8, 0], device=device),
-        "n2": torch.tensor([1, 2, 3, 4, 5, 6, 0], device=device),
-        "n3": torch.tensor([1, 2, 3, 7], device=device),
+        "n1": torch.tensor([1, 3, 4, 6, 2, 7, 9, 5, 8, 0], device=F.ctx()),
+        "n2": torch.tensor([1, 2, 3, 4, 5, 6, 0], device=F.ctx()),
+        "n3": torch.tensor([1, 2, 3, 7], device=F.ctx()),
     }
     if expected_unique_nodes["n1"].is_cuda:
         expected_reverse_id = {
             "n1": expected_unique_nodes["n1"].sort()[1],
-            "n2": torch.tensor([0, 1, 2, 3, 6, 4, 5], device=device),
+            "n2": torch.tensor([0, 1, 2, 3, 6, 4, 5], device=F.ctx()),
             "n3": expected_unique_nodes["n3"].sort()[1],
         }
         expected_unique_nodes = {
             "n1": expected_unique_nodes["n1"].sort()[0],
-            "n2": torch.tensor([1, 2, 3, 4, 0, 5, 6], device=device),
+            "n2": torch.tensor([1, 2, 3, 4, 0, 5, 6], device=F.ctx()),
             "n3": expected_unique_nodes["n3"].sort()[0],
         }
     else:
         expected_reverse_id = {
-            k: torch.arange(0, v.shape[0], device=device)
+            k: torch.arange(0, v.shape[0], device=F.ctx())
             for k, v in expected_unique_nodes.items()
         }
     expected_node_pairs = {
         "n1:e1:n2": (
-            torch.tensor([0, 1, 2, 3, 4, 5, 6, 2, 4, 3], device=device),
-            torch.tensor([1, 1, 1, 3, 0, 0, 0, 2, 2, 2], device=device),
+            torch.tensor([0, 1, 2, 3, 4, 5, 6, 2, 4, 3], device=F.ctx()),
+            torch.tensor([1, 1, 1, 3, 0, 0, 0, 2, 2, 2], device=F.ctx()),
         ),
         "n1:e2:n3": (
-            torch.tensor([7, 4, 3, 2, 5, 4, 8, 0, 1, 9], device=device),
-            torch.tensor([0, 2, 2, 2, 1, 1, 1, 3, 3, 3], device=device),
+            torch.tensor([7, 4, 3, 2, 5, 4, 8, 0, 1, 9], device=F.ctx()),
+            torch.tensor([0, 2, 2, 2, 1, 1, 1, 3, 3, 3], device=F.ctx()),
         ),
         "n2:e3:n3": (
-            torch.tensor([1, 4, 3, 0, 3, 2, 5, 6], device=device),
-            torch.tensor([0, 0, 2, 2, 1, 1, 3, 3], device=device),
+            torch.tensor([1, 4, 3, 0, 3, 2, 5, 6], device=F.ctx()),
+            torch.tensor([0, 0, 2, 2, 1, 1, 3, 3], device=F.ctx()),
         ),
     }
 
@@ -241,20 +229,16 @@ def test_unique_and_compact_node_pairs_hetero():
 
 
 def test_unique_and_compact_node_pairs_homo():
-    device = torch.device(
-        "cpu" if F._default_context_str == "cpu" else "cuda:0"
-    )
-
-    dst_nodes = torch.tensor([1, 1, 3, 3, 5, 5, 2, 6, 6, 6, 6], device=device)
-    src_nodes = torch.tensor([2, 3, 1, 4, 5, 2, 5, 1, 4, 4, 6], device=device)
+    dst_nodes = torch.tensor([1, 1, 3, 3, 5, 5, 2, 6, 6, 6, 6], device=F.ctx())
+    src_nodes = torch.tensor([2, 3, 1, 4, 5, 2, 5, 1, 4, 4, 6], device=F.ctx())
     node_pairs = (src_nodes, dst_nodes)
 
-    expected_unique_nodes = torch.tensor([1, 2, 3, 5, 6, 4], device=device)
+    expected_unique_nodes = torch.tensor([1, 2, 3, 5, 6, 4], device=F.ctx())
     expected_dst_nodes = torch.tensor(
-        [0, 0, 2, 2, 3, 3, 1, 4, 4, 4, 4], device=device
+        [0, 0, 2, 2, 3, 3, 1, 4, 4, 4, 4], device=F.ctx()
     )
     expected_src_ndoes = torch.tensor(
-        [1, 2, 0, 5, 3, 1, 3, 0, 5, 5, 4], device=device
+        [1, 2, 0, 5, 3, 1, 3, 0, 5, 5, 4], device=F.ctx()
     )
     unique_nodes, compacted_node_pairs = gb.unique_and_compact_node_pairs(
         node_pairs
@@ -265,20 +249,16 @@ def test_unique_and_compact_node_pairs_homo():
     assert torch.equal(u, expected_src_ndoes)
     assert torch.equal(v, expected_dst_nodes)
     assert torch.equal(
-        unique_nodes[:5], torch.tensor([1, 2, 3, 5, 6], device=device)
+        unique_nodes[:5], torch.tensor([1, 2, 3, 5, 6], device=F.ctx())
     )
 
 
 def test_incomplete_unique_dst_nodes_():
-    device = torch.device(
-        "cpu" if F._default_context_str == "cpu" else "cuda:0"
-    )
-
     node_pairs = (
-        torch.arange(0, 50, device=device),
-        torch.arange(100, 150, device=device),
+        torch.arange(0, 50, device=F.ctx()),
+        torch.arange(100, 150, device=F.ctx()),
     )
-    unique_dst_nodes = torch.arange(150, 200, device=device)
+    unique_dst_nodes = torch.arange(150, 200, device=F.ctx())
     with pytest.raises(IndexError):
         gb.unique_and_compact_node_pairs(node_pairs, unique_dst_nodes)
 
