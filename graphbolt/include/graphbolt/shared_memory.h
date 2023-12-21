58f997a24b255b7f24f3f133a742cc65bb5a47e9
Ubuntu <ubuntu@ip-172-31-21-218.ap-northeast-1.compute.internal>
2023-12-21 13:50:45 +0000

    fix

--- graphbolt/include/graphbolt/shared_memory.h
+++ graphbolt/include/graphbolt/shared_memory.h
@@ -11,13 +11,17 @@
 // Add the macro to avoid MIN/MAX conflict.
 #ifndef NOMINMAX
 #define NOMINMAX
-#include <windows.h>
+#define GRAPHBOLT_WINDOWS_NOMINMAX_
 #endif  // NOMINMAX
-#undef NOMINMAX
+#include <windows.h>
 #endif  // _WIN32
 
 #include <memory>
 #include <string>
+
+#ifdef GRAPHBOLT_WINDOWS_NOMINMAX_
+#undef NOMINMAX
+#endif  // GRAPHBOLT_WINDOWS_NOMINMAX_
 
 namespace graphbolt {
 namespace sampling {
