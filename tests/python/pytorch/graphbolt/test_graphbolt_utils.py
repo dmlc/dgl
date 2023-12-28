547f41e656b79c0cbaf39f8daee99634018e0489
Muhammed Fatih Balin <m.f.balin@gmail.com>
2023-12-28 16:37:35 -0500

    remove duplicate test

--- tests/python/pytorch/graphbolt/test_graphbolt_utils.py
+++ tests/python/pytorch/graphbolt/test_graphbolt_utils.py
@@ -441,11 +441,3 @@ def test_compact_incorrect_indptr():
     # The number of seeds is not corresponding to indptr.
     with pytest.raises(AssertionError):
         gb.compact_csc_format(csc_formats, seeds)
-
-    seeds = torch.tensor([1, 3, 5, 2, 6])
-    indptr = torch.tensor([0, 2, 4, 6, 7, 11])
-    indices = torch.tensor([2, 3, 1, 4, 5, 2, 5, 1, 4, 4])
-    csc_formats = gb.CSCFormatBase(indptr=indptr, indices=indices)
-    # The value of last element in indptr is not corresponding to indices.
-    with pytest.raises(AssertionError):
-        gb.compact_csc_format(csc_formats, seeds)
