a436424595099c2a05837c36e2cc568d0650653c
Muhammed Fatih Balin <m.f.balin@gmail.com>
2024-01-06 15:38:26 -0500

    add implementation

---
+++ graphbolt/src/cuda/neighbor_sampler_int8.cu
@@ -0,0 +1,15 @@
+/**
+ *  Copyright (c) 2023 by Contributors
+ *  Copyright (c) 2023, GT-TDAlab (Muhammed Fatih Balin & Umit V. Catalyurek)
+ * @file cuda/neighbor_sampler_int8.cu
+ * @brief SampleNeighbor operator instantiation for kInt8 on CUDA.
+ */
+#include "./neighbor_sampler.cuh"
+
+namespace graphbolt {
+namespace ops {
+
+INSTANTIATE_NEIGHBOR_SAMPLER(c10::impl::ScalarTypeToCPPTypeT<torch::kInt8>);
+
+}  //  namespace ops
+}  //  namespace graphbolt
