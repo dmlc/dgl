b889a8046db76d33bf029ad45cd9f1d411ceeb63
Muhammed Fatih Balin <m.f.balin@gmail.com>
2024-04-02 21:42:43 -0400

    linting

--- examples/sampling/graphbolt/pyg/node_classification_advanced.py
+++ examples/sampling/graphbolt/pyg/node_classification_advanced.py
@@ -54,6 +54,7 @@ import time
 
 import dgl.graphbolt as gb
 import torch
+
 # Needed until https://github.com/pytorch/pytorch/issues/121197 is resolved to
 # use the `--torch-compile` cmdline option reliably.
 import torch._inductor.codecache
