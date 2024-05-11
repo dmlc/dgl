491cef0198ca3a31ed0feeedba9b2bae86c5525f
RhettYing <rhett_ying@qq.com>
2024-05-11 05:55:45 +0000

    try

--- tests/scripts/build_dgl.sh
+++ tests/scripts/build_dgl.sh
@@ -9,6 +9,10 @@ fi
 
 if [[ $1 != "cpu" ]]; then
     CMAKE_VARS="$CMAKE_VARS -DUSE_CUDA=ON"
+    if [[ $1 == "gpu" ]]; then
+        # Avoid error: Unknown CUDA Architecture Name 9.0a in CUDA_SELECT_NVCC_ARCH_FLAGS
+        export TORCH_CUDA_ARCH_LIST=7.5
+    fi
 fi
 
 # This is a semicolon-separated list of Python interpreters containing PyTorch.
@@ -18,9 +22,7 @@ if [ "$1" != "cugraph" ]; then
     # We do not build pytorch for cugraph because currently building
     # pytorch against all the supported cugraph versions is not supported
     # See issue: https://github.com/rapidsai/cudf/issues/8510
-    
-    # Build within conda env as we support PyTorch only for now.
-    conda activate pytorch-ci
+    CMAKE_VARS="$CMAKE_VARS -DTORCH_PYTHON_INTERPS=/opt/conda/envs/pytorch-ci/bin/python"
 else
     # Disable sparse build as cugraph docker image lacks cuDNN.
     CMAKE_VARS="$CMAKE_VARS -DBUILD_TORCH=OFF -DBUILD_SPARSE=OFF"
