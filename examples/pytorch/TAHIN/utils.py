7620f7c70ed1fcf385319efa86c0c82db285a6f3
Andrei Ivanov <andreii@nvidia.com>
2024-05-20 10:34:25 -0700

    Fix FutureWarning in `TAHIN` example.

--- examples/pytorch/TAHIN/utils.py
+++ examples/pytorch/TAHIN/utils.py
@@ -1,11 +1,7 @@
-import numpy as np
-import torch
 from sklearn.metrics import (
     accuracy_score,
-    average_precision_score,
     f1_score,
     log_loss,
-    ndcg_score,
     roc_auc_score,
 )
 
@@ -18,23 +14,19 @@ def evaluate_auc(pred, label):
 def evaluate_acc(pred, label):
     res = []
     for _value in pred:
-        if _value >= 0.5:
-            res.append(1)
-        else:
-            res.append(0)
+        res.append(1 if _value >= 0.5 else 0)
+
     return accuracy_score(y_pred=res, y_true=label)
 
 
 def evaluate_f1_score(pred, label):
     res = []
     for _value in pred:
-        if _value >= 0.5:
-            res.append(1)
-        else:
-            res.append(0)
+        res.append(1 if _value >= 0.5 else 0)
+        
     return f1_score(y_pred=res, y_true=label)
 
 
 def evaluate_logloss(pred, label):
-    res = log_loss(y_true=label, y_pred=pred, eps=1e-7, normalize=True)
+    res = log_loss(y_true=label, y_pred=pred, normalize=True)
     return res
