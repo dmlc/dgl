27db1911b7eb217cd5b9c281f24376d58badeda7
Ubuntu <ubuntu@ip-172-31-13-22.us-west-2.compute.internal>
2024-06-11 12:33:28 +0000

    use S3-fifo implemented by cachemonCache

--- cache.py
+++ cache.py
@@ -2,7 +2,8 @@ from collections import OrderedDict
 from collections import deque
 import sys
 import os
-from cachetools import *
+#from cachetools import *
+from cachemonCache import S3FIFO
 
 total = 0
 class _LRUCache:
@@ -28,77 +29,11 @@ class _LRUCache:
             self.cache[key] = value
 
 
-#implemented S3-fifo-cache in https://dl.acm.org/doi/abs/10.1145/3600006.3613147
-class _FIFOCache:
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {}  
-        self.freq = {}   
-        self.S = OrderedDict()# Small queue
-        self.M = OrderedDict()# Main queue
-        self.G = OrderedDict()# ghost queue
-        self.hit = 0
-        self._evict = 0
-
-    def read(self, x):
-        if x in self.S or x in self.M:
-            # Cache Hit
-            self.hit += 1
-            self.freq[x] = min(self.freq.get(x, 0) + 1, 3) 
-            return x
-        else:
-            # Cache Miss
-            self.insert(x)
-            self.freq[x] = 0
-            return -1
-
-    def insert(self, x):
-        while len(self.S) + len(self.M) >= self.capacity:
-            self.evict()
-        if x in self.G:
-            self.M[x] = x
-            self.M.move_to_end(x)
-        else:
-            self.S[x] = x
-            self.S.move_to_end(x)
-
-    def evict(self):
-        self._evict+=1
-        if len(self.S) >= 0.1 * self.capacity:
-            self.evict_S()
-        else:
-            self.evict_M()
-
-    def evict_S(self):
-        evicted = False
-        while not evicted and self.S:
-            t = self.S.popitem(last=False)
-            if self.freq.get(t, 0) > 1:
-                self.M[t] = t
-                self.M.move_to_end(t)
-                if len(self.M) > self.capacity:
-                    self.evict_M()
-                else:
-                    self.G[t] = t
-                    self.G.move_to_end(t)
-                evicted = True
-
-    def evict_M(self):
-        evicted = False
-        while not evicted and self.M:
-            t = self.M.popitem(last=False)
-            if self.freq.get(t, 0) > 0:
-                self.M[t] = t
-                self.M.move_to_end(t)
-            else:
-                evicted = True
-
-
 
 
 
 if __name__ == '__main__':
-    cache = FIFOCache(int(16777216*4/1.7))
+    S3FIFOcache = S3FIFO(cache_size=int(16777216/1.7)) #16GB   
     hit = 0
     import time
     for line in sys.stdin:
@@ -108,10 +43,11 @@ if __name__ == '__main__':
         time2 = 0
         for it in line:
             st_time = time.time()
-            if ((int(it)) in cache) :
+            if (int(it) in S3FIFOcache) :
                 hit += 1
             else:
-                cache.__setitem__(int(it),int(it))
+                S3FIFOcache.put(int(it),int(it))
+                #cache.__setitem__(int(it),int(it))
             end_time = time.time()
             total += 1
             time1 += (end_time-st_time)
