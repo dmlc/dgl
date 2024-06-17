f7388fea5c1758241d44908cd21765d296b8b08c
Skeleton003 <799284168@qq.com>
2024-06-17 02:48:30 +0000

    remove if

--- src/array/cpu/spmat_op_impl_coo.cc
+++ src/array/cpu/spmat_op_impl_coo.cc
@@ -746,9 +746,7 @@ inline COOToCSRAlg WhichCOOToCSR(const COOMatrix &coo) {
 
 template <DGLDeviceType XPU, typename IdType>
 CSRMatrix COOToCSR(COOMatrix coo) {
-  if constexpr (std::is_same<IdType, int32_t>::value) {
-    CHECK_NO_OVERFLOW(coo.row->dtype, coo.row->shape[0]);
-  }
+  CHECK_NO_OVERFLOW(coo.row->dtype, coo.row->shape[0]);
   switch (WhichCOOToCSR<IdType>(coo)) {
     case COOToCSRAlg::sorted:
       return SortedCOOToCSR<IdType>(coo);
