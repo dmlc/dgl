6b29acb5780f5a6216498bfc6ef7473b093f2fae
Muhammed Fatih Balin <m.f.balin@gmail.com>
2024-07-03 01:48:48 -0400

    [GraphBolt] Add temporal labor sampling to graph.

--- graphbolt/src/fused_csc_sampling_graph.cc
+++ graphbolt/src/fused_csc_sampling_graph.cc
@@ -910,7 +910,9 @@ FusedCSCSamplingGraph::TemporalSampleNeighbors(
     torch::optional<torch::Tensor> input_nodes_pre_time_window,
     torch::optional<torch::Tensor> probs_or_mask,
     torch::optional<std::string> node_timestamp_attr_name,
-    torch::optional<std::string> edge_timestamp_attr_name) const {
+    torch::optional<std::string> edge_timestamp_attr_name,
+    torch::optional<torch::Tensor> random_seed,
+    double seed2_contribution) const {
   torch::optional<std::vector<int64_t>> seed_offsets = torch::nullopt;
   // 1. Get probs_or_mask.
   if (probs_or_mask.has_value()) {
@@ -928,19 +930,45 @@ FusedCSCSamplingGraph::TemporalSampleNeighbors(
   auto edge_timestamp = this->EdgeAttribute(edge_timestamp_attr_name);
   // 4. Call SampleNeighborsImpl
   if (layer) {
-    const int64_t random_seed = RandomEngine::ThreadLocal()->RandInt(
-        static_cast<int64_t>(0), std::numeric_limits<int64_t>::max());
-    SamplerArgs<SamplerType::LABOR> args{indices_, random_seed, NumNodes()};
-    return SampleNeighborsImpl<TemporalOption::TEMPORAL>(
-        input_nodes, seed_offsets, fanouts, return_eids,
-        GetTemporalNumPickFn(
-            input_nodes_timestamp, this->indices_, fanouts, replace,
-            type_per_edge_, input_nodes_pre_time_window, probs_or_mask,
-            node_timestamp, edge_timestamp),
-        GetTemporalPickFn(
-            input_nodes_timestamp, this->indices_, fanouts, replace,
-            indptr_.options(), type_per_edge_, input_nodes_pre_time_window,
-            probs_or_mask, node_timestamp, edge_timestamp, args));
+    if (random_seed.has_value() && random_seed->numel() >= 2) {
+      SamplerArgs<SamplerType::LABOR_DEPENDENT> args{
+          indices_,
+          {random_seed.value(), static_cast<float>(seed2_contribution)},
+          NumNodes()};
+      return SampleNeighborsImpl<TemporalOption::TEMPORAL>(
+          input_nodes, seed_offsets, fanouts, return_eids,
+          GetTemporalNumPickFn(
+              input_nodes_timestamp, indices_, fanouts, replace, type_per_edge_,
+              input_nodes_pre_time_window, probs_or_mask, node_timestamp,
+              edge_timestamp),
+          GetTemporalPickFn(
+              input_nodes_timestamp, indices_, fanouts, replace,
+              indptr_.options(), type_per_edge_, input_nodes_pre_time_window,
+              probs_or_mask, node_timestamp, edge_timestamp, args));
+    } else {
+      auto args = [&] {
+        if (random_seed.has_value() && random_seed->numel() == 1) {
+          return SamplerArgs<SamplerType::LABOR>{
+              indices_, random_seed.value(), NumNodes()};
+        } else {
+          return SamplerArgs<SamplerType::LABOR>{
+              indices_,
+              RandomEngine::ThreadLocal()->RandInt(
+                  static_cast<int64_t>(0), std::numeric_limits<int64_t>::max()),
+              NumNodes()};
+        }
+      }();
+      return SampleNeighborsImpl<TemporalOption::TEMPORAL>(
+          input_nodes, seed_offsets, fanouts, return_eids,
+          GetTemporalNumPickFn(
+              input_nodes_timestamp, indices_, fanouts, replace, type_per_edge_,
+              input_nodes_pre_time_window, probs_or_mask, node_timestamp,
+              edge_timestamp),
+          GetTemporalPickFn(
+              input_nodes_timestamp, indices_, fanouts, replace,
+              indptr_.options(), type_per_edge_, input_nodes_pre_time_window,
+              probs_or_mask, node_timestamp, edge_timestamp, args));
+    }
   } else {
     SamplerArgs<SamplerType::NEIGHBOR> args;
     return SampleNeighborsImpl<TemporalOption::TEMPORAL>(
