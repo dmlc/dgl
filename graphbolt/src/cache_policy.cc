8624ca2aca01cf4ffab7df3e4770d9777467cbd0
Muhammed Fatih Balin <m.f.balin@gmail.com>
2024-07-03 15:56:32 -0400

    do not create a second map.

--- graphbolt/src/cache_policy.cc
+++ graphbolt/src/cache_policy.cc
@@ -94,14 +94,10 @@ torch::Tensor S3FifoCachePolicy::Replace(torch::Tensor keys) {
       }));
   if (static_cast<int64_t>(ghost_map_.size()) >= 2 * main_queue_.Capacity()) {
     // Here, we ensure that the ghost_map_ does not grow too much.
-    decltype(ghost_map_) filtered_map;
-    filtered_map.reserve(ghost_map_.size());
-    for (const auto [key, timestamp] : ghost_map_) {
-      if (ghost_q_time_ - timestamp <= main_queue_.Capacity()) {
-        filtered_map[key] = timestamp;
-      }
-    }
-    ghost_map_ = filtered_map;
+    phmap::priv::erase_if(ghost_map_, [&](const auto& key_value) {
+      const auto timestamp = key_value.second;
+      return ghost_q_time_ - timestamp > main_queue_.Capacity();
+    });
   }
   return positions;
 }
