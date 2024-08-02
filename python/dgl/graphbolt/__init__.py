c858838f753d0d6bb2173df8f7ad787adb9f3c8c
Muhammed Fatih Balin <m.f.balin@gmail.com>
2024-08-02 08:54:27 -0400

    simplify the code and delete used variables.

--- python/dgl/graphbolt/__init__.py
+++ python/dgl/graphbolt/__init__.py
@@ -12,31 +12,33 @@ and set the environment variable `PYTORCH_CUDA_ALLOC_CONF=expandable_segments:Fa
 if you want to disable it and set it True to acknowledge and disable the warning.
 """
 cuda_allocator_env = os.getenv("PYTORCH_CUDA_ALLOC_CONF")
-cuda_allocator_env_warning_str = None
-if cuda_allocator_env is None:
-    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
-    cuda_allocator_env_warning_str = CUDA_ALLOCATOR_ENV_WARNING_STR
-else:
-    configs = {
+WARNING_STR_TO_BE_SHOWN = None
+configs = (
+    {}
+    if cuda_allocator_env is None
+    else {
         kv_pair.split(":")[0]: kv_pair.split(":")[1]
         for kv_pair in cuda_allocator_env.split(",")
     }
-    if "expandable_segments" in configs:
-        if configs["expandable_segments"] != "True":
-            cuda_allocator_env_warning_str = (
-                "You should consider `expandable_segments:True` in the"
-                " environment variable `PYTORCH_CUDA_ALLOC_CONF` for lower"
-                " memory usage. See "
-                "https://pytorch.org/docs/stable/notes/cuda.html"
-                "#optimizing-memory-usage-with-pytorch-cuda-alloc-conf"
-            )
-    else:
-        configs["expandable_segments"] = "True"
-        os.environ["PYTORCH_CUDA_ALLOC_CONF"] = ",".join(
-            [k + ":" + v for k, v in configs.items()]
+)
+if "expandable_segments" in configs:
+    if configs["expandable_segments"] != "True":
+        WARNING_STR_TO_BE_SHOWN = (
+            "You should consider `expandable_segments:True` in the"
+            " environment variable `PYTORCH_CUDA_ALLOC_CONF` for lower"
+            " memory usage. See "
+            "https://pytorch.org/docs/stable/notes/cuda.html"
+            "#optimizing-memory-usage-with-pytorch-cuda-alloc-conf"
         )
-        cuda_allocator_env_warning_str = CUDA_ALLOCATOR_ENV_WARNING_STR
-
+else:
+    configs["expandable_segments"] = "True"
+    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = ",".join(
+        [k + ":" + v for k, v in configs.items()]
+    )
+    WARNING_STR_TO_BE_SHOWN = CUDA_ALLOCATOR_ENV_WARNING_STR
+del configs
+del cuda_allocator_env
+del CUDA_ALLOCATOR_ENV_WARNING_STR
 
 # pylint: disable=wrong-import-position, wrong-import-order
 import torch
@@ -102,8 +104,9 @@ if torch.cuda.is_available() and not built_with_cuda():
         "installation instructions at https://www.dgl.ai/pages/start.html"
     )
 
-if torch.cuda.is_available() and cuda_allocator_env_warning_str is not None:
-    gb_warning(cuda_allocator_env_warning_str)
+if torch.cuda.is_available() and WARNING_STR_TO_BE_SHOWN is not None:
+    gb_warning(WARNING_STR_TO_BE_SHOWN)
+del WARNING_STR_TO_BE_SHOWN
 
 torch.ops.graphbolt.set_num_io_uring_threads(
     min((torch.get_num_threads() + 1) // 2, 8)
