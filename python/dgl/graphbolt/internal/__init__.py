839f85e819512d8e4da2ce4ab810b3105339592b
Muhammed Fatih Balin <m.f.balin@gmail.com>
2024-08-12 23:30:58 -0400

    [GraphBolt] Refactor datapipes folder and add `to_graph`.

--- python/dgl/graphbolt/internal/__init__.py
+++ python/dgl/graphbolt/internal/__init__.py
@@ -1,5 +1,5 @@
 """Utility functions for GraphBolt."""
 from .utils import *
 from .sample_utils import *
-from .datapipe_utils import *
+from .datapipes import *
 from .item_sampler_utils import *
---
+++ python/dgl/graphbolt/internal/datapipes/__init__.py
@@ -0,0 +1,2 @@
+from .visualization import *
+from .utils import *
