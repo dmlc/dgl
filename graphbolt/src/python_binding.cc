f37f24c77c0e8c7e1792af40d7e0245f36395d31
Muhammed Fatih BALIN <m.f.balin@gmail.com>
2024-08-23 14:43:01 -0400

    [GraphBolt] Do not expose `CPUFeatureCache` storage tensor. (#7735)

--- graphbolt/src/python_binding.cc
+++ graphbolt/src/python_binding.cc
@@ -167,7 +167,8 @@ TORCH_LIBRARY(graphbolt, m) {
       "clock_cache_policy",
       &storage::PartitionedCachePolicy::Create<storage::ClockCachePolicy>);
   m.class_<storage::FeatureCache>("FeatureCache")
-      .def_readonly("tensor", &storage::FeatureCache::tensor_)
+      .def("is_pinned", &storage::FeatureCache::IsPinned)
+      .def_property("nbytes", &storage::FeatureCache::NumBytes)
       .def("index_select", &storage::FeatureCache::IndexSelect)
       .def("query", &storage::FeatureCache::Query)
       .def("query_async", &storage::FeatureCache::QueryAsync)
