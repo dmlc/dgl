105d79d73d0700da995f8d1e1cb5fff5c7dc96e1
Muhammed Fatih Balin <m.f.balin@gmail.com>
2024-09-06 05:30:02 -0400

    correct header of the file.

--- graphbolt/src/cuda/cat.cu
+++ graphbolt/src/cuda/cat.cu
@@ -14,7 +14,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  *
- * @file cuda/expand_indptr.cu
+ * @file cuda/cat.cu
  * @brief ExpandIndptr operator implementation on CUDA.
  */
 #include <cub/cub.cuh>
