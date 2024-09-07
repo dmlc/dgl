bbe00c04baf4ee82fd2e6a348ea09dfbffabb527
Muhammed Fatih BALIN <m.f.balin@gmail.com>
2024-09-07 15:43:19 -0400

    [GraphBolt][Dataset] Merging part of #7708, `igb-het` small datasets by Bowen Yao's (#7788)
    Co-authored-by: Bowen Yao <112051015+BowenYao18@users.noreply.github.com>
    Co-authored-by: BowenYao18 <by18@rice.edu>

--- python/dgl/graphbolt/impl/ondisk_dataset.py
+++ python/dgl/graphbolt/impl/ondisk_dataset.py
@@ -990,6 +990,16 @@ class BuiltinDataset(OnDiskDataset):
             Self edges are added to the original graph.
             Node features are stored as float32.
 
+    **igb-het-[tiny|small|medium]**
+        The igb-hom-[tiny|small|medium] dataset is a heterogeneous citation network,
+        which is designed for developers to train and evaluate GNN models with
+        high fidelity. See more details in `igb-het-[tiny|small|medium]
+        <https://github.com/IllinoisGraphBenchmark/IGB-Datasets>`_.
+
+        .. note::
+            Four Reverse edge types are added to the original graph.
+            Node features are stored as float32.
+
     Parameters
     ----------
     name : str
@@ -1019,6 +1029,10 @@ class BuiltinDataset(OnDiskDataset):
         "igb-hom-tiny-seeds",
         "igb-hom-small",
         "igb-hom-small-seeds",
+        "igb-het-tiny",
+        "igb-het-tiny-seeds",
+        "igb-het-small",
+        "igb-het-small-seeds",
     ]
     _large_datasets = [
         "ogb-lsc-mag240m",
@@ -1031,6 +1045,8 @@ class BuiltinDataset(OnDiskDataset):
         "igb-hom-large-seeds",
         "igb-hom",
         "igb-hom-seeds",
+        "igb-het-medium",
+        "igb-het-medium-seeds",
     ]
     _all_datasets = _datasets + _large_datasets
 
