bb3239751047a25f78a0452302b917486d7a62c3
Ubuntu <ubuntu@ip-172-31-52-174.us-west-2.compute.internal>
2024-09-10 08:41:59 +0000

    change partition

--- python/dgl/distributed/partition.py
+++ python/dgl/distributed/partition.py
@@ -1417,9 +1417,9 @@ def partition_graph(
                 for name in g.edges[etype].data:
                     if name in [EID, "inner_edge"]:
                         continue
-                    edge_feats[_etype_tuple_to_str(etype) + "/" + name] = (
-                        F.gather_row(g.edges[etype].data[name], local_edges)
-                    )
+                    edge_feats[
+                        _etype_tuple_to_str(etype) + "/" + name
+                    ] = F.gather_row(g.edges[etype].data[name], local_edges)
         else:
             for ntype in g.ntypes:
                 if len(g.ntypes) > 1:
@@ -1454,9 +1454,9 @@ def partition_graph(
                 for name in g.edges[etype].data:
                     if name in [EID, "inner_edge"]:
                         continue
-                    edge_feats[_etype_tuple_to_str(etype) + "/" + name] = (
-                        F.gather_row(g.edges[etype].data[name], local_edges)
-                    )
+                    edge_feats[
+                        _etype_tuple_to_str(etype) + "/" + name
+                    ] = F.gather_row(g.edges[etype].data[name], local_edges)
         # delete `orig_id` from ndata/edata
         del part.ndata["orig_id"]
         del part.edata["orig_id"]
@@ -1503,7 +1503,6 @@ def partition_graph(
             part_metadata["part-{}".format(part_id)][
                 "part_graph"
             ] = os.path.relpath(part_graph_file, out_path)
-            
             # save DGLGraph
             _save_dgl_graphs(
                 part_graph_file,
