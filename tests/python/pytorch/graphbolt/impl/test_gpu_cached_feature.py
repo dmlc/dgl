930518cb7dc6927d64983a1c2903b47b31790e78
Andrei Ivanov <andreii@nvidia.com>
2024-09-25 11:34:13 -0700

    Renaming two functions

--- tests/python/pytorch/graphbolt/impl/test_gpu_cached_feature.py
+++ tests/python/pytorch/graphbolt/impl/test_gpu_cached_feature.py
@@ -17,13 +17,13 @@ def to_on_disk_numpy(test_dir, name, t):
     return path
 
 
-def _skip_condition_CachedFeature():
+def _skip_condition_cached_feature():
     return (F._default_context_str != "gpu") or (
         torch.cuda.get_device_capability()[0] < 7
     )
 
 
-def _reason_to_skip_CachedFeature():
+def _reason_to_skip_cached_feature():
     if F._default_context_str != "gpu":
         return "GPUCachedFeature tests are available only when testing the GPU backend."
 
@@ -31,8 +31,8 @@ def _reason_to_skip_CachedFeature():
 
 
 @unittest.skipIf(
-    _skip_condition_CachedFeature(),
-    reason=_reason_to_skip_CachedFeature(),
+    _skip_condition_cached_feature(),
+    reason=_reason_to_skip_cached_feature(),
 )
 @pytest.mark.parametrize(
     "dtype",
@@ -128,8 +128,8 @@ def test_gpu_cached_feature(dtype, cache_size_a, cache_size_b):
 
 
 @unittest.skipIf(
-    _skip_condition_CachedFeature(),
-    reason=_reason_to_skip_CachedFeature(),
+    _skip_condition_cached_feature(),
+    reason=_reason_to_skip_cached_feature(),
 )
 @pytest.mark.parametrize(
     "dtype",
@@ -166,8 +166,8 @@ def test_gpu_cached_feature_read_async(dtype, pin_memory):
 
 
 @unittest.skipIf(
-    _skip_condition_CachedFeature(),
-    reason=_reason_to_skip_CachedFeature(),
+    _skip_condition_cached_feature(),
+    reason=_reason_to_skip_cached_feature(),
 )
 @unittest.skipIf(
     not torch.ops.graphbolt.detect_io_uring(),
