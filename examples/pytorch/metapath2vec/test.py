a7f502e85c0615afce6a833e06ea978029b1c070
Andrei Ivanov <andreii@nvidia.com>
2024-09-27 11:00:29 -0700

    Remove the usage of the deprecated parameter.

--- examples/pytorch/metapath2vec/test.py
+++ examples/pytorch/metapath2vec/test.py
@@ -95,11 +95,11 @@ if __name__ == "__main__":
         file.close()
         print("beging predicting")
         clf_venue = LogisticRegression(
-            random_state=0, solver="lbfgs", multi_class="multinomial"
+            random_state=0, solver="lbfgs"
         ).fit(venue_training, venue_label)
         y_pred_venue = clf_venue.predict(venue_testing)
         clf_author = LogisticRegression(
-            random_state=0, solver="lbfgs", multi_class="multinomial"
+            random_state=0, solver="lbfgs"
         ).fit(author_training, author_label)
         y_pred_author = clf_author.predict(author_testing)
         macro_average_venue += f1_score(
