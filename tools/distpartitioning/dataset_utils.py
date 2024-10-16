d77b911318530d797708a325f057a3c27cf6e863
Ubuntu <ubuntu@ip-172-31-52-174.us-west-2.compute.internal>
2024-10-16 03:02:15 +0000

    change dataset_utils

--- tools/distpartitioning/dataset_utils.py
+++ tools/distpartitioning/dataset_utils.py
@@ -547,9 +547,9 @@ def get_dataset(
                     autogenerate_column_names=True,
                 )
                 parse_options = pyarrow.csv.ParseOptions(delimiter=" ")
+                # if getsize() == 0, the file is empty, indicating that the partition doesn't have this attribute.
+                # The src_ids and dst_ids should remain empty.
                 if os.path.getsize(edge_file) == 0:
-                    # if getsize() == 0, the file is empty, indicating that the partition doesn't have this attribute.
-                    # The src_ids and dst_ids should remain empty.
                     continue
                 with pyarrow.csv.open_csv(
                     edge_file,
